7c666762baabe76a7daf37859b86efc9bb89dd4a
JeremyMcCormick <jermccormick@gmail.com>
2016-09-26 18:47:31 -0700

    Remove debug prints.

--- src/UserTrackingAction.cc
+++ src/UserTrackingAction.cc
@@ -13,18 +13,12 @@ UserTrackingAction::~UserTrackingAction() {
 }
 
 void UserTrackingAction::PreUserTrackingAction(const G4Track* aTrack) {
-
-    std::cout << "PreUserTrackingAction - track ID " << aTrack->GetTrackID() << std::endl;
-
     if (aTrack->GetUserInformation() == NULL) {
-        std::cout << "Creating new track info for track ID " << aTrack->GetTrackID() << std::endl;
         UserTrackInformation* trackInfo = new UserTrackInformation(aTrack);
-        std::cout << "Added track info pointer " << (void*) trackInfo << std::endl;
         const_cast<G4Track*>(aTrack)->SetUserInformation(trackInfo);
     }
 }
 
 void UserTrackingAction::PostUserTrackingAction(const G4Track* aTrack) {
-    std::cout << "PostUserTrackingAction - track ID " << aTrack->GetTrackID() << std::endl;
     ((UserTrackInformation*)aTrack->GetUserInformation())->getTrackSummary()->update(aTrack);
 }
