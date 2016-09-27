b647cf2c334ccf357545150121c6b073c9e3cf33
JeremyMcCormick <jermccormick@gmail.com>
2016-09-27 15:51:23 -0700

    Set track summary save flag depending on detector region setting.

--- SimApplication/src/UserTrackingAction.cc
+++ SimApplication/src/UserTrackingAction.cc
@@ -1,6 +1,7 @@
 #include "SimApplication/UserTrackingAction.h"
 
 // LDMX
+#include "SimApplication/UserRegionInformation.h"
 #include "SimApplication/UserTrackInformation.h"
 
 // STL
@@ -13,9 +14,21 @@ UserTrackingAction::~UserTrackingAction() {
 }
 
 void UserTrackingAction::PreUserTrackingAction(const G4Track* aTrack) {
+
+    // Setup the user track info.
     if (aTrack->GetUserInformation() == NULL) {
         UserTrackInformation* trackInfo = new UserTrackInformation(aTrack);
         const_cast<G4Track*>(aTrack)->SetUserInformation(trackInfo);
+    }
+
+    // Turn off save flag for tracks originating in a region where trajectories are not being saved.
+    UserRegionInformation* regionInfo =
+            (UserRegionInformation*) aTrack->GetLogicalVolumeAtVertex()->GetRegion()->GetUserInformation();
+    if (regionInfo != NULL) {
+        if (!regionInfo->getStoreSecondaries()) {
+            UserTrackInformation* trackInfo = (UserTrackInformation*) const_cast<G4Track*>(aTrack)->GetUserInformation();
+            trackInfo->getTrackSummary()->setSaveFlag(false);
+        }
     }
 }
 
