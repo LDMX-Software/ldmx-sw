0048ae1236799e01b7b78692444eb5d5561d673e
JeremyMcCormick <jermccormick@gmail.com>
2016-09-26 18:47:43 -0700

    Remove debug prints.

--- SimApplication/src/UserRunAction.cc
+++ SimApplication/src/UserRunAction.cc
@@ -10,15 +10,9 @@ UserRunAction::~UserRunAction() {
 }
 
 void UserRunAction::BeginOfRunAction(const G4Run* run) {
-
-    std::cout << "UserRunAction::BeginOfRunAction" << run->GetRunID() << std::endl;
-
     RootEventWriter::getInstance()->open();
 }
 
 void UserRunAction::EndOfRunAction(const G4Run* run) {
-
-    std::cout << "UserRunAction::EndOfRunAction - " << run->GetRunID() << std::endl;
-
     RootEventWriter::getInstance()->close();
 }
