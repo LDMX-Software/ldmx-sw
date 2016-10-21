e9746faa8ff04503fd7d1e16cb787de481c42a73
JeremyMcCormick <jermccormick@gmail.com>
2016-10-20 22:23:49 -0700

    Overhaul the ROOT IO in the sim module.

--- SimApplication/src/G4CalorimeterHit.cxx
+++ SimApplication/src/G4CalorimeterHit.cxx
@@ -10,30 +10,12 @@ namespace sim {
 
 G4Allocator<G4CalorimeterHit> G4CalorimeterHitAllocator;
 
-G4CalorimeterHit::G4CalorimeterHit() :
-        simCalorimeterHit(new SimCalorimeterHit()), trackID(-1) {
-}
-
-G4CalorimeterHit::G4CalorimeterHit(SimCalorimeterHit* theSimCalorimeterHit) :
-        simCalorimeterHit(theSimCalorimeterHit), trackID(-1) {
-}
-
-G4CalorimeterHit::~G4CalorimeterHit() {
-}
-
-SimCalorimeterHit* G4CalorimeterHit::getSimCalorimeterHit() {
-    return simCalorimeterHit;
-}
-
 void G4CalorimeterHit::Draw() {
 
     G4VVisManager* visManager = G4VVisManager::GetConcreteInstance();
 
     if(visManager) {
-        G4ThreeVector position(
-                simCalorimeterHit->getPosition()[0],
-                simCalorimeterHit->getPosition()[1],
-                simCalorimeterHit->getPosition()[2]);
+        G4ThreeVector position(x, y, z);
         G4Point3D p3D = G4Point3D(position);
         G4Circle chit(p3D);
         chit.SetScreenDiameter(3.0);
@@ -44,18 +26,6 @@ void G4CalorimeterHit::Draw() {
         chit.SetVisAttributes(G4VisAttributes(col));
         visManager->Draw(chit);
     }
-}
-
-void G4CalorimeterHit::Print() {
-    simCalorimeterHit->Print();
-}
-
-void G4CalorimeterHit::setTrackID(G4int aTrackID) {
-    trackID = aTrackID;
-}
-
-G4int G4CalorimeterHit::getTrackID() {
-    return trackID;
 }
 
 }
