dcd6cea40ca03f3add5aa3defbee78df0806f601
JeremyMcCormick <jermccormick@gmail.com>
2016-10-25 13:32:14 -0700

    Provide default implementations for methods.

--- SimPlugins/include/SimPlugins/UserActionPlugin.h
+++ SimPlugins/include/SimPlugins/UserActionPlugin.h
@@ -14,10 +14,10 @@ namespace sim {
  *
  * This class is a pure virtual interface defining a plugin to the Geant4
  * simulation engine which may be activated in the "user action" hooks.
- * An implementation class should provide concrete implementations of all
- * methods and must define "create" and "destroy" functions as entry points for
- * the dynamic library loading.  The <i>DummySimPlugin</i> class provides an
- * example implementation showing how to define these functions properly.
+ * An implementation class must define "create" and "destroy" functions
+ * as entry points for the dynamic library loading.  The <i>DummySimPlugin</i>
+ * class provides an example implementation showing how to define these
+ * functions properly.
  *
  * @see DummySimPlugin
  */
@@ -29,27 +29,27 @@ class UserActionPlugin {
 
         virtual std::string getName() = 0;
 
-        virtual bool hasRunAction() = 0;
+        virtual bool hasRunAction() { return false; }
 
-        virtual bool hasSteppingAction() = 0;
+        virtual bool hasSteppingAction() { return false; }
 
-        virtual bool hasTrackingAction() = 0;
+        virtual bool hasTrackingAction() { return false; }
 
-        virtual bool hasEventAction() = 0;
+        virtual bool hasEventAction() { return false; }
 
-        virtual void beginRun(const G4Run*) = 0;
+        virtual void beginRun(const G4Run*) {;}
 
-        virtual void endRun(const G4Run*) = 0;
+        virtual void endRun(const G4Run*) {;}
 
-        virtual void stepping(const G4Step*) = 0;
+        virtual void stepping(const G4Step*) {;}
 
-        virtual void preTracking(const G4Track*) = 0;
+        virtual void preTracking(const G4Track*) {;}
 
-        virtual void postTracking(const G4Track*) = 0;
+        virtual void postTracking(const G4Track*) {;}
 
-        virtual void beginEvent(const G4Event*) = 0;
+        virtual void beginEvent(const G4Event*) {;}
 
-        virtual void endEvent(const G4Event*) = 0;
+        virtual void endEvent(const G4Event*) {;}
 };
 
 }
