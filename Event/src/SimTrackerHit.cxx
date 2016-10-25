3e2ee45152d2f8009a63af86df7c4adfa2ecfde9
JeremyMcCormick <jermccormick@gmail.com>
2016-10-24 18:24:48 -0700

    Minor changes to comments and code formatting.

--- Event/src/SimTrackerHit.cxx
+++ Event/src/SimTrackerHit.cxx
@@ -4,42 +4,42 @@ ClassImp(event::SimTrackerHit)
 
 namespace event {
 
-    SimTrackerHit::SimTrackerHit() 
-        : TObject() {
-    }
+SimTrackerHit::SimTrackerHit()
+    : TObject() {
+}
 
-    SimTrackerHit::~SimTrackerHit() {
-        Clear();
-    }
+SimTrackerHit::~SimTrackerHit() {
+    Clear();
+}
 
-    void SimTrackerHit::Print(Option_t *option) const {
-        std::cout << "SimTrackerHit { " << "ID: " << std::bitset<32>(id) << ", " <<
-            "Layer ID: " << layer_id << ", " <<
-            "Position: ( " << x << ", " << y << ", " << z << " ), " <<
-            "dEdx: " << edep << ", " <<
-            "Time: " << time << ", " <<
-            "Momentum: ( " << px << ", " << py << ", " << pz << " )" <<
-            " }" << std::endl;
-    }
+void SimTrackerHit::Print(Option_t *option) const {
+    std::cout << "SimTrackerHit { " << "ID: " << std::bitset<32>(id) << ", " <<
+        "Layer ID: " << layer_id << ", " <<
+        "Position: ( " << x << ", " << y << ", " << z << " ), " <<
+        "dEdx: " << edep << ", " <<
+        "Time: " << time << ", " <<
+        "Momentum: ( " << px << ", " << py << ", " << pz << " )" <<
+        " }" << std::endl;
+}
 
-    void SimTrackerHit::Clear(Option_t*) {
-        TObject::Clear(); 
-    }
+void SimTrackerHit::Clear(Option_t*) {
+    TObject::Clear();
+}
 
-    SimParticle* SimTrackerHit::getSimParticle() const {
-        return static_cast<SimParticle*>(simParticle.GetObject());
-    }
+SimParticle* SimTrackerHit::getSimParticle() const {
+    return static_cast<SimParticle*>(simParticle.GetObject());
+}
 
-    void SimTrackerHit::setPosition(const float x, const float y, const float z) { 
-        this->x = x; 
-        this->y = y; 
-        this->z = z; 
-    }
+void SimTrackerHit::setPosition(const float x, const float y, const float z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
 
-    void SimTrackerHit::setMomentum(const float px, const float py, const float pz) {
-        this->px = px;
-        this->py = py; 
-        this->pz = pz; 
-    }
+void SimTrackerHit::setMomentum(const float px, const float py, const float pz) {
+    this->px = px;
+    this->py = py;
+    this->pz = pz;
+}
 
 }
