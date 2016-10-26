9f75911578025fc7bdad810b341ebb06c5772a6c
JeremyMcCormick <jermccormick@gmail.com>
2016-10-26 16:58:58 -0700

    Add collections to SimEvent for trigger pads and active target.

--- Event/include/Event/EventConstants.h
+++ Event/include/Event/EventConstants.h
@@ -19,6 +19,8 @@ namespace event {
     const static char* TAGGER_SIM_HITS = "TaggerSimHits";
     const static char* ECAL_SIM_HITS = "EcalSimHits";
     const static char* HCAL_SIM_HITS = "HcalSimHits";
+    const static char* TRIGGER_PAD_SIM_HITS = "TriggerPadSimHits";
+    const static char* TARGET_SIM_HITS = "TargetSimHits";
 
     /*
      * Default collection size for TClonesArray in event objects.
