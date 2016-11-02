396bb8b67c0d91f0008759ea1b444687eafc2c82
JeremyMcCormick <jermccormick@gmail.com>
2016-11-01 17:41:22 -0700

    Minor cleanup to event model classes.

--- Event/include/Event/SimTrackerHit.h
+++ Event/include/Event/SimTrackerHit.h
@@ -45,7 +45,7 @@ class SimTrackerHit: public TObject {
         /**
          * Get the geometric layer ID of the hit.
          */
-        int getLayerID() const { return layer_id; };
+        int getLayerID() const { return layerID; };
 
         /** Return the position of the hit in mm. */
         std::vector<float> getPosition() const { return {x, y, z}; };
@@ -60,7 +60,7 @@ class SimTrackerHit: public TObject {
          * Return the path length between the start and end points of the
          * hit in mm.
          */
-        float getPathLength() const { return path_length; };
+        float getPathLength() const { return pathLength; };
 
         /**
          * Return the momentum in GeV of the particle at the position at which
@@ -86,7 +86,7 @@ class SimTrackerHit: public TObject {
         /**
          * Set the path length of the hit in mm.
          */
-        void setPathLength(const float path_length) { this->path_length = path_length; };
+        void setPathLength(const float path_length) { this->pathLength = path_length; };
 
         /**
          * Set the momentum in GeV of the particle at the position at which
@@ -97,48 +97,25 @@ class SimTrackerHit: public TObject {
         /**
          * Set the geometric layer ID of the hit.
          */
-        void setLayerID(const int layer_id) { this->layer_id = layer_id; };
+        void setLayerID(const int layer_id) { this->layerID = layer_id; };
 
         /** Set the Monte Carlo particle that created the hit. */
         void setSimParticle(SimParticle* simParticle) { this->simParticle.SetObject(simParticle); };
 
     private:
 
-        /** */
         TRef simParticle{nullptr};
-
-        /** */
         int id{0};
-
-        /** */
-        int layer_id{0};
-
-        /** */
+        int layerID{0};
         float edep{0};
-
-        /** */
         float time{0};
-
-        /** */
         float px{0};
-
-        /** */
         float py{0};
-
-        /** */
         float pz{0};
-
-        /** */
         float x{0};
-
-        /** */
         float y{0};
-
-        /** */
         float z{0};
-
-        /** */
-        float path_length{0};
+        float pathLength{0};
 
         ClassDef(SimTrackerHit, 1);
 
