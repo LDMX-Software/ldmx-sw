452a96030e7224fc5e065e3919afac3d31c64803
JeremyMcCormick <jermccormick@gmail.com>
2016-11-07 15:53:03 -0800

    Add default implementation of DetectorID.

---
+++ DetDescr/src/DefaultDetectorID.cxx
@@ -0,0 +1,12 @@
+#include "DetDescr/DefaultDetectorID.h"
+
+namespace detdescr {
+
+DefaultDetectorID::DefaultDetectorID() : DetectorID() {
+    IDField::IDFieldList* fieldList = new IDField::IDFieldList();
+    fieldList->push_back(new IDField("subdet", 0, 0, 3));
+    fieldList->push_back(new IDField("layer", 1, 4, 11));
+    setFieldList(fieldList);
+}
+
+}
