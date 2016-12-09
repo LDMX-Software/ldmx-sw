893cb98527639e0e13bba8d97d6a562d7c097e91
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-12-09 07:00:37 -0800

    Including code to return the hex neighbors for a specific cellId
    Including code to return the hex neighbors for a specific cellId
    
    Including code to return the hex neighbors for a specific cellId
    
    Including code to return the hex neighbors for a specific cellId
    
    Including code to return the hex neighbors for a specific cellId

--- DetDescr/include/DetDescr/EcalHexReadout.h
+++ DetDescr/include/DetDescr/EcalHexReadout.h
@@ -26,10 +26,34 @@ class EcalHexReadout {
         inline std::pair<float,float> getCellCentroidXYPair(int cellId){
             std::pair<std::map<int,std::pair<float,float>>::iterator, bool> isInserted;
             if (!isInserted.second) {
-                std::cout << "This cellId does not exist, returning (-1,-1)" << std::endl;
-                return std::make_pair<float,float>(-1.,-1.);
+                std::cout << "This cellId does not exist, returning (-1e6,-1e6)" << std::endl;
+                return std::make_pair<float,float>(-1e6,-1e6);
             }
             return isInserted.first->second;
+        };
+        inline std::vector<int> getInnerRingCellIds(int cellId){
+            return {cellId - 1,cellId - 1 - 76,cellId - 1 + 77,
+                    cellId + 1,cellId + 1 - 77,cellId + 1 + 76};
+        };
+        inline std::vector<int> getOuterRingCellIds(int cellId){
+            return {cellId - 2 * 76,cellId - 2 * 76- 1, cellId - 2 * 76 - 2,
+                    cellId + 2 * 78,cellId + 2 * 78 - 1,cellId + 2 * 78 - 2,
+                    cellId + 2,cellId + 2 - 77,cellId + 2 + 76,
+                    cellId - 2,cellId - 2 - 76,cellId - 2 + 77};
+        };
+        inline bool isInShowerInnerRing(int centroidId, int cellId){
+            bool matched = false;
+            for (auto ringId : getInnerRingCellIds(centroidId) ){
+                if (cellId == ringId) matched = true;
+            }
+            return matched;
+        };
+        inline bool isInShowerOuterRing(int centroidId, int cellId){
+            bool matched = false;
+            for (auto ringId : getOuterRingCellIds(centroidId) ){
+                if (cellId == ringId) matched = true;
+            }
+            return matched;
         };
     private:
 
