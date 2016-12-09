b701e2df79ed2f0bd5c69155e3240d9d48b6d29d
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-12-09 08:31:45 -0800

    Ecal Veto Processor

---
+++ EventProc/include/EventProc/EcalVetoProcessor.h
@@ -0,0 +1,66 @@
+#include "TString.h"
+#include "TRandom.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TRandom2.h"
+
+#include "Event/SimEvent.h"
+#include "Event/SimCalorimeterHit.h"
+#include "DetDescr/DetectorID.h"
+#include "DetDescr/EcalDetectorID.h"
+#include "DetDescr/EcalHexReadout.h"
+#include "EventProc/EventProcessor.h"
+
+using event::SimEvent;
+using event::SimCalorimeterHit;
+using detdescr::DetectorID;
+using detdescr::EcalDetectorID;
+using detdescr::EcalHexReadout;
+
+
+
+namespace eventproc {
+
+
+class EcalVetoProcessor : public EventProcessor {
+
+public:
+    typedef std::pair<int, int>   layer_cell_pair;
+
+    typedef std::pair<int, float> cell_energy_pair;
+
+    EcalVetoProcessor(TTree* outputTree_,bool verbose_ = false) :
+        outputTree(outputTree_),
+        verbose(verbose_){};
+
+    void initialize();
+
+    void execute();
+
+    void finish();
+
+ private:
+
+    TTree* outputTree;
+    TRandom2 *noiseInjector;
+    std::vector<float> *EcalLayerEdepRaw_,*EcalLayerEdepReadout_,
+    *EcalLayerTime_,*EcalLayerIsoRaw_,*EcalLayerIsoReadout_;
+    bool verbose,doesPassVeto;
+    EcalDetectorID* detID;
+    EcalHexReadout* hexReadout;
+    static const int numEcalLayers,numLayersForMedCal,backEcalStartingLayer;
+    static const float meanNoise,readoutThreshold,
+    totalDepCut,totalIsoCut,backEcalCut,ratioCut;
+
+    inline layer_cell_pair hitToPair(SimCalorimeterHit* hit){
+        int detIDraw = hit->getID();
+        detID->setRawValue(detIDraw);
+        detID->unpack();
+        int layer = detID->getFieldValue("layer");
+        int cellid = detID->getFieldValue("cellid");
+        return (std::make_pair(layer, cellid));
+    };
+
+};
+
+}
