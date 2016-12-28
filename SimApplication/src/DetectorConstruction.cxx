b481f78e55c4cf55b057fedc3ed6f3436ac391dc
Omar Moreno <email@omarmoreno.net>
2016-12-28 14:18:31 -0800

    Instantiate the xsec biasing operator and attach it to the target.  This code will soon be changed so it's configurable.

--- SimApplication/src/DetectorConstruction.cxx
+++ SimApplication/src/DetectorConstruction.cxx
@@ -5,7 +5,7 @@ namespace sim {
 DetectorConstruction::DetectorConstruction(G4GDMLParser* theParser) :
     parser_(theParser),
     auxInfoReader_(new AuxInfoReader(theParser)) {
-}
+    }
 
 DetectorConstruction::~DetectorConstruction() {
     delete auxInfoReader_;
@@ -15,6 +15,22 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
     auxInfoReader_->readGlobalAuxInfo();
     auxInfoReader_->assignAuxInfoToVolumes();
     return parser_->GetWorldVolume();
+}
+
+void DetectorConstruction::ConstructSDandField() {
+
+    // TODO: This should be configurable from a macro
+    G4LogicalVolume* logicalVolume = G4LogicalVolumeStore::GetInstance()->GetVolume("target");
+
+    // Instantiate the biasing operator
+    PhotonuclearXsecBiasingOperator* xsecBiasing 
+        = new PhotonuclearXsecBiasingOperator("PhotonuclearXsecBiasingOperator");
+    
+    // Attach it to the volume that will be biased.
+    xsecBiasing->AttachTo(logicalVolume);
+    G4cout << " Attaching biasing operator " << xsecBiasing->GetName()
+           << " to logical volume " << logicalVolume->GetName()
+           << G4endl;
 }
 
 }
