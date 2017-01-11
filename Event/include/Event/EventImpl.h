7c1ba9f72ef8c2ddd97163076a2933055d86e6ee
JeremyMcCormick <jermccormick@gmail.com>
2017-01-11 15:31:48 -0800

    Add new event implementation classes from evtModelDev branch.

---
+++ Event/include/Event/EventImpl.h
@@ -0,0 +1,88 @@
+#ifndef EVENT_EVENTIMPL_H_
+#define EVENT_EVENTIMPL_H_
+
+// ROOT
+#include "TObject.h"
+#include "TClonesArray.h"
+
+class TTree;
+class TBranch;
+
+// STL
+#include <string>
+#include <map>
+
+namespace event {
+
+class EventImpl {
+
+    public:
+
+        static const char* TREENAME;
+
+        EventImpl(const std::string& thePassName);
+
+        void setITree(TTree* tree);
+
+        void setOTree(TTree* tree);
+
+        TTree* createTree();
+
+        std::string makeBranchName(const std::string& collectionName, const std::string& passName) const {
+            return collectionName + "_" + passName;
+        }
+        std::string makeBranchName(const std::string& collectionName) const {
+            return makeBranchName(collectionName, passName_);
+        }
+
+        virtual ~EventImpl();
+
+        /**
+         * Adds a clones array to the event/tree.
+         */
+        void add(const std::string& collectionName, TClonesArray*);
+
+        /**
+         * Adds a general object to the event/tree.
+         * Important notes: all objects _must_ implement/replace
+         * TObject::Clone to simply call "new" and create an empty
+         * new object and implement TObject::Copy to either copy the
+         * contents of the object or _swap_ them to the calling
+         * function, which is more efficient.
+         */
+        void add(const std::string& collectionName, TObject*);
+
+        /**
+         * These two methods only apply to the current pass of processing --
+         * it is not allowed to modify any object from a previous pass.  They will
+         * require null if there no object with that name in the current pass.
+         */
+        // TClonesArray* getMutable(const std::string& collectionName,const std::string& passName);
+        // TObject* getMutable(const std::string& collectionName, const std::string& passName);
+
+        const TObject* get(const std::string& collectionName, const std::string& passName);
+
+        bool nextEvent();
+
+        void onEndOfEvent();
+
+        void onEndOfFile();
+
+    private:
+
+        Long64_t entries_{ -1 };
+        Long64_t ientry_ { -1 };
+        std::string passName_;
+        TTree* otree_{nullptr};
+        TTree* itree_{nullptr};
+
+        mutable std::map<std::string, TBranch*> branches_;
+        mutable std::map<std::string, TObject*> objects_;
+        std::map<std::string, TObject*> objectsOwned_;
+
+        std::vector<TBranch*> newBranches_;
+};
+
+}
+
+#endif
---
+++ Event/src/EventFile.cxx
@@ -0,0 +1,141 @@
+#include "Event/EventFile.h"
+#include "Event/EventImpl.h"
+
+namespace event {
+
+EventFile::EventFile(const std::string& filename, bool isOutputFile) :
+        fileName_(filename), isOutputFile_(isOutputFile) {
+
+    if (isOutputFile_)
+        file_ = new TFile(filename.c_str(), "RECREATE");
+    else
+        file_ = new TFile(filename.c_str());
+
+    if (!file_->IsOpen()) {
+        // throw exception eventually?
+    }
+
+    // TODO: Make compression level an argument with default value.
+    if (isOutputFile_) {
+        file_->SetCompressionLevel(2);
+    }
+
+    if (!isOutputFile_) {
+        tree_ = (TTree*) (file_->Get(EventImpl::TREENAME));
+        entries_ = tree_->GetEntriesFast();
+    }
+}
+
+EventFile::EventFile(const std::string& filename, EventFile* cloneParent) :
+        fileName_(filename), isOutputFile_(true), parent_(cloneParent) {
+
+    file_ = new TFile(filename.c_str(), "RECREATE");
+
+    if (!file_->IsOpen()) {
+        // throw exception eventually?
+    }
+
+    parent_->tree_->SetBranchStatus("*", 1);
+
+    if (isOutputFile_)
+        file_->SetCompressionLevel(2);
+}
+
+void EventFile::addDrop(const std::string& rule) {
+
+    if (parent_ == 0)
+        return;
+
+    size_t i = rule.find("keep");
+    bool iskeep = i != std::string::npos;
+    if (!iskeep)
+        i = rule.find("drop");
+
+    if (i == std::string::npos)
+        return;
+
+    std::string srule = rule.substr(i + 4);
+    for (i = srule.find_first_of(" \t\n\r"); i != std::string::npos; i = srule.find_first_of(" \t\n\r"))
+        srule.erase(i, 1);
+    if (srule.length() == 0)
+        return;
+    if (srule.back() != '*')
+        srule += '*';
+
+    parent_->tree_->SetBranchStatus(srule.c_str(), (iskeep) ? (1) : (0));
+}
+
+bool EventFile::nextEvent() {
+
+    if (ientry_ < 0 && parent_) {
+        if (!parent_->tree_)
+            return false;
+        tree_ = parent_->tree_->CloneTree(0);
+        event_->setITree(parent_->tree_);
+        event_->setOTree(tree_);
+    }
+
+    // close up the last event
+    if (ientry_ >= 0) {
+        if (isOutputFile_) {
+            tree_->Fill(); // fill the clones...
+        }
+        if (event_) {
+            event_->onEndOfEvent();
+        }
+    }
+
+    if (parent_) {
+        if (!parent_->nextEvent())
+            return false;
+        parent_->tree_->GetEntry(parent_->ientry_);
+        ientry_ = parent_->ientry_;
+        event_->nextEvent();
+        entries_++;
+        return true;
+    } else {
+
+        // if we are reading, move the pointer
+        if (!isOutputFile_) {
+
+            if (ientry_ + 1 >= entries_)
+                return false;
+
+            ientry_++;
+            tree_->LoadTree(ientry_);
+
+            if (event_) {
+                event_->nextEvent();
+            }
+            return true;
+
+        } else {
+            ientry_++;
+            entries_++;
+            return true;
+        }
+    }
+    return false;
+}
+
+void EventFile::setupEvent(EventImpl* evt) {
+    event_ = evt;
+    if (isOutputFile_) {
+        if (!tree_ && !parent_) {
+            tree_ = event_->createTree();
+        }
+        if (parent_) {
+            event_->setITree(parent_->tree_);
+        }
+    } else {
+        event_->setITree(tree_);
+    }
+}
+
+void EventFile::Close() {
+    if (isOutputFile_)
+        tree_->Write();
+    file_->Close();
+}
+
+}
