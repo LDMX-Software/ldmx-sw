9b2d7b7f689937bebbc1b96aaa9d62152df7a504
JeremyMcCormick <jermccormick@gmail.com>
2017-01-12 17:02:37 -0800

    Fix ID field name.

--- EventProc/include/EventProc/EcalVetoProcessor.h
+++ EventProc/include/EventProc/EcalVetoProcessor.h
@@ -65,7 +65,7 @@ public:
         detID->setRawValue(detIDraw);
         detID->unpack();
         int layer = detID->getFieldValue("layer");
-        int cellid = detID->getFieldValue("cellid");
+        int cellid = detID->getFieldValue("cell");
         return (std::make_pair(layer, cellid));
     };
 
