69d34d8e947bc8905bbe1c14de7cb25b72e30f93
JeremyMcCormick <jermccormick@gmail.com>
2017-01-13 14:35:54 -0800

    Add CalorimeterHit class to resolve iss115.

--- EventProc/include/EventProc/ExampleCalHitProcessor.h
+++ EventProc/include/EventProc/ExampleCalHitProcessor.h
@@ -7,6 +7,7 @@
 #ifndef EVENTPROC_EXAMPLECALHITPROCESSOR_H_
 #define EVENTPROC_EXAMPLECALHITPROCESSOR_H_
 
+#include "Event/CalorimeterHit.h"
 #include "Event/SimCalorimeterHit.h"
 
 namespace eventproc {
@@ -27,23 +28,20 @@ class ExampleCalHitProcessor : public EventProcessor {
             auto ecalSimHits = event_->getCollection(event::EventConstants::ECAL_SIM_HITS);
             for (int i = 0; i < ecalSimHits->GetEntriesFast(); i++) {
                 auto simHit = (event::SimCalorimeterHit*) ecalSimHits->At(i);
-                auto calHit = (event::SimCalorimeterHit*) ecalHits_->ConstructedAt(ecalHits_->GetEntries());
+                auto calHit = (event::CalorimeterHit*) ecalHits_->ConstructedAt(ecalHits_->GetEntries());
                 calHit->setID(simHit->getID());
                 calHit->setEdep(simHit->getEdep());
                 calHit->setTime(simHit->getTime());
-                auto pos = simHit->getPosition();
-                calHit->setPosition(pos[0], pos[1], pos[2]);
             }
             event_->add("EcalCalHits", ecalHits_);
         }
 
         void finish() {
-            //delete ecalHits_;
         }
 
     public:
 
-        TClonesArray* ecalHits_{new TClonesArray("event::SimCalorimeterHit", 50)};
+        TClonesArray* ecalHits_{new TClonesArray("event::CalorimeterHit", 50)};
 };
 
 }
