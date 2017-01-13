38c886a2dbd2dd0ef12bd7d52d19c6d3ac7239d9
JeremyMcCormick <jermccormick@gmail.com>
2017-01-13 14:37:05 -0800

    Update event constants and class def with new event classes.

--- Event/src/EventConstants.cxx
+++ Event/src/EventConstants.cxx
@@ -11,12 +11,13 @@ namespace event {
      * Default Collection and object names in the event tree.
      */
     const std::string EventConstants::ECAL_SIM_HITS = "EcalSimHits";
+    const std::string EventConstants::EVENT_HEADER = "EventHeader";
     const std::string EventConstants::HCAL_SIM_HITS = "HcalSimHits";
     const std::string EventConstants::RECOIL_SIM_HITS = "RecoilSimHits";
     const std::string EventConstants::SIM_PARTICLES = "SimParticles";
     const std::string EventConstants::TAGGER_SIM_HITS = "TaggerSimHits";
     const std::string EventConstants::TARGET_SIM_HITS = "TargetSimHits";
     const std::string EventConstants::TRIGGER_PAD_SIM_HITS = "TriggerPadSimHits";
-    const std::string EventConstants::EVENT_HEADER = "EventHeader";
+    const std::string EventConstants::TRIGGER_RESULT = "TriggerResult";
 
 } // namespace event
