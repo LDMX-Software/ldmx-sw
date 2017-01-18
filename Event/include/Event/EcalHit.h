1a66e8b7a046925c995e80e349fa5e478523c512
Jeremiah Mans <jmmans@physics.umn.edu>
2017-01-18 15:33:44 -0600

    Add configuration of the outputfile from python Fix issues related with tests

--- Event/include/Event/EcalHit.h
+++ Event/include/Event/EcalHit.h
@@ -16,16 +16,11 @@ class EcalHit : public CalorimeterHit {
 
   int getLayer() const {   return layer_; }
   void setLayer(int layer) { layer_=layer; }
-  int getPE() const { return pe_; }
-  void setPE(int pe) { pe_=pe; }
-  float getZpos() const { return zpos_; }
-  void setZpos(float zpos) { zpos_=zpos; }
 
   private:
 
         int layer_{0};
-        int pe_{0};
-        float zpos_{0};
+
     /**
      * The ROOT class definition.
      */
--- Framework/src/Process.cxx
+++ Framework/src/Process.cxx
@@ -6,108 +6,117 @@
 
 namespace ldmxsw {
 
-  Process::Process(const std::string& passname) : passname_{passname} {
-  }
-
-
-  void Process::run(int eventlimit) {
-    try {
-      int n_events_processed=0;
-    
-      // first, notify everyone that we are starting
-      for (auto module : sequence_)
-	module->onProcessStart();
-      
-      // if we have no input files, but do have an event number, run for that number of events on an output file
-      if (inputFiles_.empty() && eventlimit>0) {
-	EventFile outFile(outputFileNameRule_,true);
-	
-	for (auto module : sequence_)
-	  module->onFileOpen();
-	
-	EventImpl theEvent(passname_);
-	outFile.setupEvent(&theEvent);
-	
-	while (n_events_processed<eventlimit) {
-	  event::EventHeader& eh=theEvent.getEventHeaderMutable();
-	  eh.setRun(runForGeneration_);
-	  eh.setEventNumber(n_events_processed+1);
-	  eh.setTimestamp(TTimeStamp());
-
-	  theEvent.getEventHeader()->Print();
-	  
-	  for (auto module : sequence_)
-	    if (dynamic_cast<Producer*>(module)) (dynamic_cast<Producer*>(module))->produce(theEvent);
-	    else if (dynamic_cast<Analyzer*>(module)) (dynamic_cast<Analyzer*>(module))->analyze(theEvent);
-
-	  outFile.nextEvent();
-	  theEvent.Clear();
-	  n_events_processed++;
-	}
-	
-	for (auto module : sequence_)	  
-	  module->onFileClose();
-
-	outFile.close();
-      } else {    
-	// next, loop through the files
-	for (auto infilename : inputFiles_) {
-	  EventFile inFile(infilename);
-	  EventFile* outFile(0);
-	  
-	  if (!outputFileNameRule_.empty()) {
-	    outFile=new EventFile(outputFileNameRule_,&inFile);
-
-	    for (auto rule : dropKeepRules_)
-	      outFile->addDrop(rule);
-	  }
-	  
-	  for (auto module : sequence_)
-	    module->onFileOpen();
-	  
-	  EventImpl theEvent(passname_);
-	  if (outFile) outFile->setupEvent(&theEvent);
-	  else inFile.setupEvent(&theEvent);
-
-	  EventFile* masterFile=(outFile)?(outFile):(&inFile);
-	  
-	  while (masterFile->nextEvent()) {	      
-	    for (auto module : sequence_)
-	      if (dynamic_cast<Producer*>(module)) (dynamic_cast<Producer*>(module))->produce(theEvent);
-	      else if (dynamic_cast<Analyzer*>(module)) (dynamic_cast<Analyzer*>(module))->analyze(theEvent);
-	    n_events_processed++;
-	    theEvent.Clear();
-	  }
-
-	  if (outFile) outFile->close();
-	  inFile.close();
-	  
-	  for (auto module : sequence_)	  
-	    module->onFileClose();
-	}
-      }
-      
-      // finally, notify everyone that we are stopping
-      for (auto module : sequence_)
-	module->onProcessEnd();
-    } catch (Exception& e) {
-      std::cerr << "Framework Error [" << e.name() << "] : " << e.message() << std::endl;
-      std::cerr << "  at " << e.module() <<":"<<e.line()<<" in " <<e.function() << std::endl;
+    Process::Process(const std::string& passname) : passname_{passname} {
     }
-    
-  }
 
-  void Process::addToSequence(EventProcessor* mod) {
-    sequence_.push_back(mod);
-  }
-  void Process::addFileToProcess(const std::string& filename) {
-    inputFiles_.push_back(filename);
-  }
-  void Process::addDropKeepRule(const std::string& rule) {
-    dropKeepRules_.push_back(rule);
-  }
-  void Process::setOutputFileTemplate(const std::string& filenameOut) {
-    outputFileNameRule_=filenameOut;
-  }
+    
+    void Process::run(int eventlimit) {
+	try {
+	    int n_events_processed=0;
+	    
+	    // first, notify everyone that we are starting
+	    for (auto module : sequence_)
+		module->onProcessStart();
+	    
+	    // if we have no input files, but do have an event number, run for that number of events on an output file
+	    if (inputFiles_.empty() && eventlimit>0) {
+		EventFile outFile(outputFiles_[0],true);
+		
+		for (auto module : sequence_)
+		    module->onFileOpen();
+		
+		EventImpl theEvent(passname_);
+		outFile.setupEvent(&theEvent);
+		
+		while (n_events_processed<eventlimit) {
+		    event::EventHeader& eh=theEvent.getEventHeaderMutable();
+		    eh.setRun(runForGeneration_);
+		    eh.setEventNumber(n_events_processed+1);
+		    eh.setTimestamp(TTimeStamp());
+		    
+		    theEvent.getEventHeader()->Print();
+		    
+		    for (auto module : sequence_)
+			if (dynamic_cast<Producer*>(module)) (dynamic_cast<Producer*>(module))->produce(theEvent);
+			else if (dynamic_cast<Analyzer*>(module)) (dynamic_cast<Analyzer*>(module))->analyze(theEvent);
+		    
+		    outFile.nextEvent();
+		    theEvent.Clear();
+		    n_events_processed++;
+		}
+		
+		for (auto module : sequence_)	  
+		    module->onFileClose();
+		
+		outFile.close();
+	    } else {
+		if (!outputFiles_.empty() && outputFiles_.size()!=inputFiles_.size()) {
+		    EXCEPTION_RAISE("Process","Unable to handle case of different number of input and output files (other than zero output files)");
+		}
+		// next, loop through the files
+		int ifile=0;
+		for (auto infilename : inputFiles_) {
+		    EventFile inFile(infilename);
+		    std::cout << "Process: Opening file " << infilename << std::endl;
+		    EventFile* outFile(0);
+		    
+		    if (!outputFiles_.empty()) {
+			outFile=new EventFile(outputFiles_[ifile],&inFile);
+			ifile++;		    
+		    
+			for (auto rule : dropKeepRules_)
+			    outFile->addDrop(rule);
+		    }
+		
+		    for (auto module : sequence_)
+			module->onFileOpen();
+		    
+		    EventImpl theEvent(passname_);
+		    if (outFile) outFile->setupEvent(&theEvent);
+		    else inFile.setupEvent(&theEvent);
+		    
+		    EventFile* masterFile=(outFile)?(outFile):(&inFile);
+		    
+		    while (masterFile->nextEvent()) {	      
+			for (auto module : sequence_)
+			    if (dynamic_cast<Producer*>(module)) (dynamic_cast<Producer*>(module))->produce(theEvent);
+			    else if (dynamic_cast<Analyzer*>(module)) (dynamic_cast<Analyzer*>(module))->analyze(theEvent);
+			n_events_processed++;
+		    }
+
+		    if (outFile) outFile->close();
+		    inFile.close();
+		    std::cout << "Process: Closing file " << infilename << std::endl;	  
+		    for (auto module : sequence_)	  
+			module->onFileClose();
+		}
+	    }
+      
+	    // finally, notify everyone that we are stopping
+	    for (auto module : sequence_)
+		module->onProcessEnd();
+	} catch (Exception& e) {
+	    std::cerr << "Framework Error [" << e.name() << "] : " << e.message() << std::endl;
+	    std::cerr << "  at " << e.module() <<":"<<e.line()<<" in " <<e.function() << std::endl;
+	}
+    
+    }
+
+    void Process::addToSequence(EventProcessor* mod) {
+	sequence_.push_back(mod);
+    }
+    void Process::addFileToProcess(const std::string& filename) {
+	inputFiles_.push_back(filename);
+    }
+    void Process::addDropKeepRule(const std::string& rule) {
+	dropKeepRules_.push_back(rule);
+    }
+    void Process::setOutputFileName(const std::string& filenameOut) {
+	outputFiles_.clear();
+	outputFiles_.push_back(filenameOut);
+    }
+    void Process::addOutputFileName(const std::string& filenameOut) {
+	outputFiles_.push_back(filenameOut);
+    }
   
 }
