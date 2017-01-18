cee5d64adfcf3be8af3bab8b9d36bcf18a4a0c7c
Jeremiah Mans <jmmans@physics.umn.edu>
2017-01-18 12:40:34 -0600

    Fix issue with shared library loading (simpler now)

--- src/ldmx_sim.cxx
+++ src/ldmx_sim.cxx
@@ -1,6 +1,9 @@
 #include "SimApplication/SimApplication.h"
+#include "Framework/EventProcessorFactory.h"
 
 #include <stdio.h>
+
+// this static instance must be here so that it comes into existance before any shared library is loaded
 
 using sim::SimApplication;
 
