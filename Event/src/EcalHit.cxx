5d29c5d7fd9eb23f0c18a15e6695b3489b7f988c
Jeremiah Mans <jmmans@physics.umn.edu>
2017-01-18 21:28:18 -0600

    Tune Print() method and add methods to extract layer and cell information

--- Event/src/EcalHit.cxx
+++ Event/src/EcalHit.cxx
@@ -8,8 +8,12 @@ ClassImp(event::EcalHit)
 namespace event {
 
     void EcalHit::Print(Option_t *option)  const {
-	std::cout << "EcalHit { " << "id: " << getID() << ",  energy: " << getEnergy()
+	std::cout << "EcalHit { " << "id: " << std::hex << getID() << std::dec << ",  energy: " << getEnergy()
 		  << "MeV, time: " << getTime() << "ns, amplitude: " << getAmplitude() << std::endl;
 	
+    }
+
+    int EcalHit::getCell() const {
+	return (getID()>>12); // depends on internal knowledge of Detector Id, but efficiency is important for this method
     }
 }
