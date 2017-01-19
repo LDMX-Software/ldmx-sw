ac2c5060c94f30e5398dd813cc085dfd86ebe137
JHiltbrand <hiltb002@umn.edu>
2017-01-19 15:35:21 -0600

    Code styling and documentation in progress

--- Framework/include/Framework/Process.h
+++ Framework/include/Framework/Process.h
@@ -1,44 +1,47 @@
-#ifndef ldmxsw_Framework_Process_h_included
-#define ldmxsw_Framework_Process_h_included 1
+#ifndef LDMXSW_FRAMEWORK_PROCESS_H_
+#define LDMXSW_FRAMEWORK_PROCESS_H_
 
 #include "Framework/Exception.h"
 #include <vector>
 
 namespace ldmxsw {
 
-  class EventProcessor;
-  class EventFile;
-  class EventImpl;
-  
-   /** Class which represents the process under execution.
-   */
-  class Process {
-      public:
-      Process(const std::string& passname);
-      
-      const std::string& passname() const { return passname_; }
-      
-      void addToSequence(EventProcessor* mod);
-      void addFileToProcess(const std::string& filename);
-      void addDropKeepRule(const std::string& rule);
-      void setOutputFileName(const std::string& filenameOut);
-      void addOutputFileName(const std::string& filenameOut);
-      
-      void setRunNumber(int run) { runForGeneration_=run; }
-      void setEventLimit(int limit) { eventLimit_=limit; }
-      void run();
+class EventProcessor;
+class EventFile;
+class EventImpl;
 
-  private:
-      std::string passname_;
-      int eventLimit_;
-      std::vector<EventProcessor*> sequence_;
-      std::vector<std::string> inputFiles_;
-      std::vector<std::string> outputFiles_;
-      std::vector<std::string> dropKeepRules_;
-      
-      int runForGeneration_{1};
-  };
+/** Class which represents the process under execution.
+ */
+class Process {
+
+    public:
+
+        Process(const std::string& passname);
+        
+        const std::string& passname() const { return passname_; }
+        
+        void addToSequence(EventProcessor* mod);
+        void addFileToProcess(const std::string& filename);
+        void addDropKeepRule(const std::string& rule);
+        void setOutputFileName(const std::string& filenameOut);
+        void addOutputFileName(const std::string& filenameOut);
+        
+        void setRunNumber(int run) { runForGeneration_=run; }
+        void setEventLimit(int limit) { eventLimit_=limit; }
+        void run();
+
+    private:
+
+        std::string passname_;
+        int eventLimit_;
+        std::vector<EventProcessor*> sequence_;
+        std::vector<std::string> inputFiles_;
+        std::vector<std::string> outputFiles_;
+        std::vector<std::string> dropKeepRules_;
+        
+        int runForGeneration_{1};
+};
     
 }
 
-#endif // ldmxsw_Framework_Process_h_included
+#endif
--- Framework/src/EventImpl.cxx
+++ Framework/src/EventImpl.cxx
@@ -19,12 +19,12 @@ EventImpl::~EventImpl() {
 
 void EventImpl::add(const std::string& collectionName, TClonesArray* tca) {
 
-  if (collectionName.find('_')!=std::string::npos) {
-    EXCEPTION_RAISE("IllegalName","The product name '"+collectionName+"' is illegal as it contains an underscore.");
-  }
-  
+    if (collectionName.find('_') != std::string::npos) {
+        EXCEPTION_RAISE("IllegalName","The product name '"+collectionName+"' is illegal as it contains an underscore.");
+    }
+    
     std::string branchName = makeBranchName(collectionName);
-
+  
     std::map<std::string, TObject*>::iterator ito = objects_.find(branchName);
     if (ito == objects_.end()) { // create a new branch
         ito = objects_.insert(std::pair<std::string, TObject*>(branchName, tca)).first;
@@ -32,23 +32,27 @@ void EventImpl::add(const std::string& collectionName, TClonesArray* tca) {
             TBranch* aBranch = outputTree_->Branch(branchName.c_str(), tca, 100000, 3);
             newBranches_.push_back(aBranch);
         }
-	branchNames_.push_back(branchName);
-	knownLookups_.clear(); // have to invalidate this cache
+
+        branchNames_.push_back(branchName);
+        knownLookups_.clear(); // have to invalidate this cache
     }
 }
 
 void EventImpl::add(const std::string& collectionName, TObject* to) {
 
-  if (collectionName.find('_')!=std::string::npos) {
-    EXCEPTION_RAISE("IllegalName","The product name '"+collectionName+"' is illegal as it contains an underscore.");
-  }
-  
+    if (collectionName.find('_')!=std::string::npos) {
+        EXCEPTION_RAISE("IllegalName","The product name '"+collectionName+"' is illegal as it contains an underscore.");
+    }
+    
     std::string branchName;
-    if (collectionName=="EventHeader") branchName=collectionName;
-    else branchName = makeBranchName(collectionName);
+    if (collectionName == "EventHeader") {
+        branchName = collectionName;
 
+    } else {
+        branchName = makeBranchName(collectionName);
+    }
     std::map<std::string, TObject*>::iterator ito = objects_.find(branchName);
-
+  
     if (ito == objects_.end()) { // create a new branch
         TObject* myCopy = to->Clone();
         ito = objects_.insert(std::pair<std::string, TObject*>(branchName, myCopy)).first;
@@ -57,87 +61,105 @@ void EventImpl::add(const std::string& collectionName, TObject* to) {
             TBranch* aBranch = outputTree_->Branch(branchName.c_str(), myCopy);
             newBranches_.push_back(aBranch);
         }
-	branchNames_.push_back(branchName);
-	knownLookups_.clear(); // have to invalidate this cache
+
+        branchNames_.push_back(branchName);
+        knownLookups_.clear(); // have to invalidate this cache
     }
     to->Copy(*ito->second);
 }
 
-  const TObject* EventImpl::getReal(const std::string& collectionName, const std::string& passName, bool mustExist) {
+const TObject* EventImpl::getReal(const std::string& collectionName, const std::string& passName, bool mustExist) {
 
-  std::string branchName;
-  if (collectionName=="EventHeader") branchName=collectionName;
-  else branchName = makeBranchName(collectionName, passName);
+    std::string branchName;
+    if (collectionName == "EventHeader") {
+        branchName = collectionName;
 
-  if (passName.empty() && collectionName!="EventHeader") {
-    auto ptr=knownLookups_.find(collectionName);
-    if (ptr!=knownLookups_.end()) branchName=ptr->second;
-    else {
-      std::vector<std::vector<std::string>::const_iterator> matches;
-      branchName=collectionName+"_";
-      for (std::vector<std::string>::const_iterator ptr=branchNames_.begin(); ptr!=branchNames_.end(); ptr++) {
-	if (!ptr->compare(0,branchName.size(),branchName)) matches.push_back(ptr);
-      }
-      if (matches.empty()) {
-	if (!mustExist) return nullptr;
-	EXCEPTION_RAISE("ProductNotFound","No product found for name '"+collectionName+"'");
-      } else if (matches.size()>1) {
-	std::string names;
-	for (auto strs : matches) {
-	  if (!names.empty()) names+=", ";
-	  names+=*strs;
-	}
-	if (!mustExist) return nullptr;
-	EXCEPTION_RAISE("ProductAmbiguous","Multiple products found for name '"+collectionName+"' without specified pass name ("+names+")");
-      } else {
-	branchName=*matches.front();
-	knownLookups_[collectionName]=branchName;
-      }
+    } else {
+        branchName = makeBranchName(collectionName, passName);
     }
-  }
+    
+    if (passName.empty() && collectionName!="EventHeader") {
+        auto ptr = knownLookups_.find(collectionName);
+        if (ptr != knownLookups_.end()) {
+            branchName = ptr->second;
 
-  
+        } else {
+            std::vector<std::vector<std::string>::const_iterator> matches;
+            branchName = collectionName+"_";
+            for (std::vector<std::string>::const_iterator ptr = branchNames_.begin(); ptr != branchNames_.end(); ptr++) {
+                if (!ptr->compare(0,branchName.size(),branchName)) {
+                    matches.push_back(ptr);
+                }
+            }
+            if (matches.empty()) {
+                if (!mustExist) {
+                    return nullptr;
+                }
+                EXCEPTION_RAISE("ProductNotFound","No product found for name '"+collectionName+"'");
+            } else if (matches.size()>1) {
+                std::string names;
+                for (auto strs : matches) {
+                    if (!names.empty()) {
+                        names += ", ";
+                    }
+                    names += *strs;
+                }
+                if (!mustExist) {
+                    return nullptr;
+                }
+                EXCEPTION_RAISE("ProductAmbiguous","Multiple products found for name '"+collectionName+"' without specified pass name ("+names+")");
+            } else {
+                branchName=*matches.front();
+                knownLookups_[collectionName]=branchName;
+            }
+        }
+    }
+    
+    
     if (inputTree_ == 0) {
         // check the objects map
         std::map<std::string, TObject*>::const_iterator ito = objects_.find(branchName);
-        if (ito != objects_.end())
+        if (ito != objects_.end()) {
             return ito->second;
-        else {
-	  EXCEPTION_RAISE("ProductNotFound","No product found for name '"+collectionName+"' and pass '"+passName_+"'");
-	  return 0;
-	}
-    }
 
+        } else {
+            EXCEPTION_RAISE("ProductNotFound","No product found for name '"+collectionName+"' and pass '"+passName_+"'");
+            return 0;
+        }
+    }
+    
     // find the active branch and update if necessary
     std::map<std::string, TBranch*>::const_iterator itb = branches_.find(branchName);
     if (itb != branches_.end()) {
-
+    
         std::map<std::string, TObject*>::iterator ito = objects_.find(branchName);
-
+    
         // update buffers if needed
         if (itb->second->GetReadEntry() != ientry_) {
-
+    
             TBranchElement* tbe = dynamic_cast<TBranchElement*>(itb->second);
-            if (!tbe)
+            if (!tbe) {
                 itb->second->SetAddress(ito->second);
+            }
             int nr = itb->second->GetEntry(ientry_, 1);
         }
-
+    
         // check the objects map
-
-        if (ito != objects_.end())
+    
+        if (ito != objects_.end()) {
             return ito->second;
-
+        }
+    
         // this case is hard to achieve
         return 0;
     } else {
-
+    
         // ok, maybe we've not loaded this yet, look for a branch
         TBranch* branch = inputTree_->GetBranch(branchName.c_str());
         if (branch == 0) {
-	  EXCEPTION_RAISE("ProductNotFound","No product found for name '"+collectionName+"' and pass '"+passName_+"'");
-	}
-
+            EXCEPTION_RAISE("ProductNotFound","No product found for name '"+collectionName+"' and pass '"+passName_+"'");
+        }
+    
         // ooh, new branch!
         TObject* top(0);
         branch->SetAutoDelete(false);
@@ -149,10 +171,10 @@ void EventImpl::add(const std::string& collectionName, TObject* to) {
         } else {
             branch->SetAddress(&top);
         }
-
+    
         branches_.insert(std::pair<std::string, TBranch*>(branchName, branch));
         objects_.insert(std::pair<std::string, TObject*>(branchName, top));
-
+    
         return top;
     }
 }
@@ -160,8 +182,8 @@ void EventImpl::add(const std::string& collectionName, TObject* to) {
 TTree* EventImpl::createTree() {
     outputTree_ = new TTree("LDMX_Events", "LDMX Events");
 
-    eventHeader_=new event::EventHeader();
-    add("EventHeader",eventHeader_);
+    eventHeader_ = new event::EventHeader();
+    add("EventHeader", eventHeader_);
     
     return outputTree_;
 }
@@ -174,12 +196,12 @@ void EventImpl::setInputTree(TTree* tree) {
     inputTree_ = tree;
     entries_ = inputTree_->GetEntries();
     branchNames_.clear();
-    eventHeader_=get<event::EventHeader*>("EventHeader");
+    eventHeader_ = get<event::EventHeader*>("EventHeader");
 
     // find the names of all the existing branches
-    TObjArray* branches=inputTree_->GetListOfBranches();
-    for (int i=0; i<branches->GetEntriesFast(); i++) {
-      branchNames_.push_back(branches->At(i)->GetName());
+    TObjArray* branches = inputTree_->GetListOfBranches();
+    for (int i = 0; i < branches->GetEntriesFast(); i++) {
+        branchNames_.push_back(branches->At(i)->GetName());
     }
 }
 
@@ -188,18 +210,21 @@ bool EventImpl::nextEvent() {
     return true;
 }
 
-  void EventImpl::beforeFill() {
-    if (inputTree_ == 0) add("EventHeader",eventHeader_);
-  }
-  void EventImpl::Clear() {
+void EventImpl::beforeFill() {
+    if (inputTree_ == 0) add("EventHeader", eventHeader_);
+}
+void EventImpl::Clear() {
     // clear the event objects
     for (auto obj : objects_)
-      obj.second->Clear("C");
-  }
-  void EventImpl::onEndOfEvent() {
-  }
+        obj.second->Clear("C");
+}
+
+void EventImpl::onEndOfEvent() {
+
+}
   
 void EventImpl::onEndOfFile() {
+
 }
 
 }
