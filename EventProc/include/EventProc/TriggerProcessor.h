563502672ddeee01505eaa5804860b4c939f0bd5
JHiltbrand <hiltb002@umn.edu>
2017-01-23 15:12:19 -0600

    Added documentation to TriggerProcessor

--- EventProc/include/EventProc/TriggerProcessor.h
+++ EventProc/include/EventProc/TriggerProcessor.h
@@ -15,29 +15,68 @@
 /**
  * @class TriggerProcessor
  * @brief Provides a trigger decision for recon using a TriggerResult object.
+ *
+ * @note 
+ * TriggerProcessor takes in a set of parameters to be used in defining 
+ * the trigger algorithm. An event is passed to the processor and the relevant
+ * algorithms are then run on the event (ECAL layer sum). A trigger decision is
+ * executed and the decision along with the algorithm name and relevant variables
+ * are stored in a TriggerResult object which is added to the collection.
  */
 class TriggerProcessor : public ldmxsw::Producer {
 
     public:
 
+        /**
+         * Class constructor.
+         */
         TriggerProcessor(const std::string& name, const ldmxsw::Process& process) :
                 ldmxsw::Producer(name, process) {
         }
 
+        /**
+         * Class destructor.
+         */
         virtual ~TriggerProcessor() {;}
 
+        /**
+         * Read in a set of user specified parameters to be used
+         * in defining aspects of the trigger.
+         * @param pSet A set containing parameters to be used
+         * by the trigger algorithm.
+         */
         virtual void configure(const ldmxsw::ParameterSet& pSet);
 
+        /**
+         * Run the trigger algorithm and create a TriggerResult
+         * object to contain info about the trigger decision
+         * such as pass/fail, number of saved variables,
+         * etc.
+         * param event The event to run trigger algorithm on.
+         */
         virtual void produce(event::Event& event);
 
     private:
 
+        /** The energy sum to make cut on. */
         float layerESumCut_{0};
+
+        /** The trigger mode to run in. Mode zero sums over
+         * all cells in layer, while in mode 1 only cells in 
+         * center module are summed over. (TODO)
+         */
         int mode_{0};
+
+        /** The first layer of layer sum. */
         int startLayer_{0};
+
+        /** The last layer of layer sum. */
         int endLayer_{0};
 
+        /** The name of the trigger algorithm used. */
         TString algoName_;
+
+        /** Object to hold trigger results and variables */
         event::TriggerResult result_;
 
 };
