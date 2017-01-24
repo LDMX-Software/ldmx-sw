c305955d60dd02fc5f12b0254b25b0f1e9ea1f61
Omar Moreno <omoreno1@ucsc.edu>
2017-01-24 11:03:19 -0800

    Simply biasing plugin. The plugin now assumes that the electron is being fired just upstream of the target.

--- SimPlugins/src/ECalPhotonuclearBiasingPlugin.cxx
+++ SimPlugins/src/ECalPhotonuclearBiasingPlugin.cxx
@@ -38,15 +38,15 @@ G4ClassificationOfNewTrack sim::ECalPhotonuclearBiasingPlugin::stackingClassifyN
     G4String particleName = track->GetParticleDefinition()->GetParticleName();
 
     /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: " << "\n" 
-        << "\tParticle " << particleName      << " ( PDG ID: " << pdgID << " ) : " << "\n"
-        << "\tTrack ID: " << track->GetTrackID()     << "\n" 
-        << std::endl;*/
+                << "\tParticle " << particleName      << " ( PDG ID: " << pdgID << " ) : " << "\n"
+                << "\tTrack ID: " << track->GetTrackID()     << "\n" 
+                << std::endl;*/
 
 
     // Use current classification by default so values from other plugins are not overridden.
     G4ClassificationOfNewTrack classification = currentTrackClass;
 
-    if (track != targetGamma) {
+    if (track->GetTrackID() == 1 && pdgID == 11) {
         /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: Pushing track to waiting stack." << std::endl;*/
         return fWaiting; 
     }
@@ -67,11 +67,6 @@ void sim::ECalPhotonuclearBiasingPlugin::stepping(const G4Step* step) {
     /*std::cout << "*   Step   *" << std::endl;*/
     /*std::cout << "************" << std::endl;*/ 
 
-
-    if (track->GetParentID() == 0 && track->GetCurrentStepNumber() == 1) { 
-        targetGamma = nullptr;
-    }
-
     // get the PDGID of the track.
     G4int pdgID = track->GetParticleDefinition()->GetPDGEncoding();
 
@@ -86,137 +81,126 @@ void sim::ECalPhotonuclearBiasingPlugin::stepping(const G4Step* step) {
     double incidentParticleEnergy = step->GetPreStepPoint()->GetTotalEnergy();
 
     /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]:\n" 
-        << "\tTotal energy of " << particleName  << ": " << incidentParticleEnergy << " MeV \n"
-        << "\tPDG ID: " << pdgID << "\n"
-        << "\tTrack ID: " << track->GetTrackID() << "\n" 
-        << "\tStep #: " << track->GetCurrentStepNumber() << "\n"
-        << "\tParent ID: " << track->GetParentID() << "\n"
-        << "\tParticle currently in " << volumeName  << std::endl;*/
+                << "\tTotal energy of " << particleName  << ": " << incidentParticleEnergy << " MeV \n"
+                << "\tPDG ID: " << pdgID << "\n"
+                << "\tTrack ID: " << track->GetTrackID() << "\n" 
+                << "\tStep #: " << track->GetCurrentStepNumber() << "\n"
+                << "\tParent ID: " << track->GetParentID() << "\n"
+                << "\tParticle currently in " << volumeName  << std::endl;*/
 
     // Get the particles daughters.
     const G4TrackVector* secondaries = step->GetSecondary();
 
     // process primary track
-    if ((track->GetParentID() == 0)
-            && (volumeName.compareTo(targetVolumeName_) == 0)) {
+    if (track->GetParentID() == 0 
+            && track->GetCurrentStepNumber() == 1
+            && volumeName.compareTo(targetVolumeName_) == 0) {
 
-        if (targetGamma == nullptr) {
-            // If the initial interaction didn't result in any secondaries e.g.
-            // a brem photon, don't bother processing the rest of the event.
-            if (secondaries->size() == 0) {     
-                track->SetTrackStatus(fKillTrackAndSecondaries); 
+        // If the initial interaction didn't result in any secondaries e.g.
+        // a brem photon, don't bother processing the rest of the event.
+        if (secondaries->size() == 0) {     
+            track->SetTrackStatus(fKillTrackAndSecondaries); 
 
-                /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "  
-                    << "Primary did not produce secondaries --> Killing primary track!" 
+            /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "  
+              << "Primary did not produce secondaries --> Killing primary track!" 
+              << std::endl;*/
+
+            G4RunManager::GetRunManager()->AbortEvent();
+            return;
+        }
+
+        G4String processName = secondaries->at(0)->GetCreatorProcess()->GetProcessName(); 
+
+        /*std::cout << "[ TargetPhotonuclearBiasingPlugin ]: "
+                    << "Incident electron produced " << secondaries->size() 
+                    << " particle via " << processName << " process." 
                     << std::endl;*/
 
-                G4RunManager::GetRunManager()->AbortEvent();
-                return;
-            }
+        // If secondaries were produced via a process other than brem, stop 
+        // tracking all tracks.
+        if (processName.compareTo("eBrem") != 0 || secondaries->size() != 1) {
 
-            for (G4Track* secondaryTrack : *secondaries) { 
-                /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: Secondary: "
-                    << secondaryTrack->GetParticleDefinition()->GetParticleName() 
-                    << " Volume: " << secondaryTrack->GetVolume()->GetName()
-                    << " Process: " << secondaryTrack->GetCreatorProcess()->GetProcessName()
-                    << " Track ID: " << secondaryTrack->GetTrackID()
-                    << std::endl;*/        
-                if (secondaryTrack->GetVolume()->GetName().compareTo(targetVolumeName_) == 0) { 
-                    G4String processName = secondaryTrack->GetCreatorProcess()->GetProcessName();
-                    if (secondaryTrack->GetParticleDefinition()->GetPDGEncoding() == 22
-                            && processName.compareTo("eBrem") == 0) { 
 
-                        /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: Found PN gamma." << std::endl;*/
-                        targetGamma = secondaryTrack; 
-                        break;
-                    }
-                }
-            }
+            /*std::cout << "[ TargetPhotonuclearBiasingPlugin ]: "
+                        << "The secondaries are not a result of Brem. --> Killing all tracks!"
+                        << std::endl;*/
 
-            if (targetGamma == nullptr) { 
-                /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "
-                    << "A PN gamma was not created in the target --> Killing all tracks!"
-                    << std::endl;*/
-
-                track->SetTrackStatus(fKillTrackAndSecondaries);
-
-                G4RunManager::GetRunManager()->AbortEvent();
-                return;
-            }
-
-            /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "
-                << targetGamma->GetParticleDefinition()->GetParticleName() 
-                << " with kinetic energy " << targetGamma->GetKineticEnergy()
-                << " MeV was produced." << std::endl;*/
-
-            if (targetGamma->GetKineticEnergy() < photonEnergyThreshold_) { 
-
-                /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "
-                    << "Brem photon failed the energy threshold cut." 
-                    << std::endl;*/
-
-                track->SetTrackStatus(fKillTrackAndSecondaries);
-
-                G4RunManager::GetRunManager()->AbortEvent();
-                return;
-            }
-
-            /*std::cout << "[ ECalPhotonuclerBiasingPlugin ]: "
-                << "Photon passed energy cut --> Suspending primary track!"
-                << std::endl;*/
-            track->SetTrackStatus(fSuspend);   
-        } else if (targetGamma != nullptr && secondaries->size() != 0) {
-            /*std::cout << "[ ECalPhotonuclerBiasingPlugin ]: "
-                      << "Multiple particles were created in the target --> Killing all tracks!"
-                      << std::endl;*/
-                
             track->SetTrackStatus(fKillTrackAndSecondaries);
 
             G4RunManager::GetRunManager()->AbortEvent();
             return;
         } 
-    } 
 
-    if (track == targetGamma) {
-        if (secondaries->size() != 0) { 
+        G4Track* secondaryTrack = secondaries->at(0);
+        /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "
+                    << secondaryTrack->GetParticleDefinition()->GetParticleName() 
+                    << " with kinetic energy " << secondaryTrack->GetKineticEnergy()
+                    << " MeV was produced." << std::endl;*/
+
+        if (secondaryTrack->GetKineticEnergy() < photonEnergyThreshold_) { 
+
+            /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "
+                        << "Brem photon failed the energy threshold cut." 
+                        << std::endl;*/
+
+            track->SetTrackStatus(fKillTrackAndSecondaries);
+
+            G4RunManager::GetRunManager()->AbortEvent();
+            return;
+        }
+
+        /*std::cout << "[ ECalPhotonuclerBiasingPlugin ]: "
+                    << "Photon passed energy cut --> Suspending primary track!"
+                    << std::endl;*/
+        track->SetTrackStatus(fSuspend);   
+    }
+
+    if (track->GetTrackID() == 2 && pdgID == 22) {
+
+        if (secondaries->size() != 0 
+                && (volumeName.contains("W") || volumeName.contains("Si")) 
+                && volumeName.contains("phys")) { 
+
             G4String processName = secondaries->at(0)->GetCreatorProcess()->GetProcessName(); 
             /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "
-                << "Brem photon produced " << secondaries->size() 
-                << " particles via " << processName << " process within "
-                << secondaries->at(0)->GetVolume()->GetName() << "." 
-                << std::endl;*/
-            if ((volumeName.contains("W") || volumeName.contains("Si")) && volumeName.contains("phys")) { 
-
-                // Only record photonuclear events
-                if (!processName.contains("photonNuclear")) {
-
-                    /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "
                         << "Brem photon produced " << secondaries->size() 
                         << " particles via " << processName << " process within "
                         << secondaries->at(0)->GetVolume()->GetName() << "." 
                         << std::endl;*/
 
+            // Only record photonuclear events
+            if (!processName.contains("photonNuclear")) {
+
+                    /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "
+                                << "Brem photon produced " << secondaries->size() 
+                                << " particles via " << processName << " process within "
+                                << secondaries->at(0)->GetVolume()->GetName() << "." 
+                                << std::endl;*/
+
                     track->SetTrackStatus(fKillTrackAndSecondaries);
 
                     G4RunManager::GetRunManager()->AbortEvent();
                     return;
-                }
+            }
 
                 std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "
-                          << "Brem photon produced " << secondaries->size() 
-                          << " particles via " << processName << " process within "
-                          << secondaries->at(0)->GetVolume()->GetName() << "." 
-                          << std::endl;
-            } else { 
+                    << "Brem photon produced " << secondaries->size() 
+                    << " particles via " << processName << " process within "
+                    << secondaries->at(0)->GetVolume()->GetName() << "." 
+                    << std::endl;
+        } else if (secondaries->size() != 0) { 
                 /*std::cout << "[ ECalPhotonuclearBiasingPlugin ]: "
-                    << "Secondaries were produced outside of the ECal --> Killing all tracks!"
-                    << std::endl;*/
+                            << "Secondaries were produced outside of the ECal --> Killing all tracks!"
+                            << std::endl;*/
 
                 track->SetTrackStatus(fKillTrackAndSecondaries);
                 G4RunManager::GetRunManager()->AbortEvent();
                 return;
-            }
-        } 
+        } else if (volumeName.contains("hcal")) { 
+                track->SetTrackStatus(fKillTrackAndSecondaries);
+                G4RunManager::GetRunManager()->AbortEvent();
+                return;
+        }
     }
 }
 
