a12fe4655ec91e57d63a2078e3d9ca331ed24cda
Jeremiah Mans <jmmans@physics.umn.edu>
2017-01-27 22:19:48 -0600

    Deal with initialization bug in gcc 4.8

--- Framework/src/EventProcessor.cxx
+++ Framework/src/EventProcessor.cxx
@@ -9,24 +9,24 @@
 
 namespace ldmx {
 
-    EventProcessor::EventProcessor(const std::string& name, Process& process) :
-        process_ { process }, name_ { name } {
-	}
+EventProcessor::EventProcessor(const std::string& name, Process& process) :
+    process_ (process ), name_ { name } {
+}
     
 
-    void EventProcessor::declare(const std::string& classname, int classtype,EventProcessorMaker* maker) {
-	EventProcessorFactory::getInstance().registerEventProcessor(classname,classtype, maker);
-    }
+void EventProcessor::declare(const std::string& classname, int classtype,EventProcessorMaker* maker) {
+    EventProcessorFactory::getInstance().registerEventProcessor(classname,classtype, maker);
+}
 
-    TDirectory* EventProcessor::getHistoDirectory() {
-	if (!histoDir_) {
-	    histoDir_=process_.makeHistoDirectory(name_);
-	}
-	histoDir_->cd(); // make this the current directory
-	return histoDir_;
+TDirectory* EventProcessor::getHistoDirectory() {
+    if (!histoDir_) {
+	histoDir_=process_.makeHistoDirectory(name_);
     }
+    histoDir_->cd(); // make this the current directory
+    return histoDir_;
+}
 
-    Producer::Producer(const std::string& name, Process& process) : EventProcessor(name,process) { }
-    Analyzer::Analyzer(const std::string& name, Process& process) : EventProcessor(name,process) { }
+Producer::Producer(const std::string& name, Process& process) : EventProcessor(name,process) { }
+Analyzer::Analyzer(const std::string& name, Process& process) : EventProcessor(name,process) { }
   
 }
