8c4b25d397f2a85be4c3b8d4d68ee45360630fbf
Omar Moreno <email@omarmoreno.net>
2017-02-03 11:25:55 -0800

    Add process, volume and xsec commands.

--- Biasing/src/BiasingMessenger.cxx
+++ Biasing/src/BiasingMessenger.cxx
@@ -7,10 +7,8 @@
 
 #include "Biasing/BiasingMessenger.h"
 
-ldmx::BiasingMessenger::BiasingMessenger(G4RunManager* runManager) { 
+ldmx::BiasingMessenger::BiasingMessenger() { 
     
-    runManager_ = static_cast<RunManager*>(runManager); 
-
     biasingDir_->SetGuidance("LDMX physics biasing commands.");
     
     enableBiasingCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit);
@@ -28,8 +26,9 @@ ldmx::BiasingMessenger::~BiasingMessenger() {
 
 void ldmx::BiasingMessenger::SetNewValue(G4UIcommand* command, G4String newValues) { 
 
-    if (command == enableBiasingCmd_) _biasingEnabled = true; 
-    else if (command == particleTypeCmd_) { 
-       //runManager_->setParticleTypeToBias(newValues);  
-    }
+    if (command == enableBiasingCmd_) biasingEnabled_ = true; 
+    else if (command == particleTypeCmd_) particleType_ = newValues;  
+    else if (command == processCmd_)      process_ = newValues;
+    else if (command == volumeCmd_)       volume_ = newValues; 
+    else if (command == xsecTransCmd_)    xsecTrans_ = G4UIcommand::ConvertToDouble(newValues); 
 }
