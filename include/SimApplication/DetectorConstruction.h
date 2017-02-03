c9a5a962da56c68746c4be08ed97c2f9b44c9022
Omar Moreno <email@omarmoreno.net>
2017-02-03 14:04:00 -0800

    Formatting.

--- include/SimApplication/DetectorConstruction.h
+++ include/SimApplication/DetectorConstruction.h
@@ -22,60 +22,62 @@
 
 namespace ldmx {
 
-/**
- * @class DetectorConstruction
- * @brief Implements the Geant4 detector construction
- *
- * @note
- * This class reads in a detector description from a GDML file
- * using the basic <i>G4GDMLParser</i> and instantiates supplemental
- * information using the AuxInfoReader.
- *
- * @see AuxInfoReader
- */
-class DetectorConstruction: public G4VUserDetectorConstruction {
+    /**
+     * @class DetectorConstruction
+     * @brief Implements the Geant4 detector construction
+     *
+     * @note
+     * This class reads in a detector description from a GDML file
+     * using the basic <i>G4GDMLParser</i> and instantiates supplemental
+     * information using the AuxInfoReader.
+     *
+     * @see AuxInfoReader
+     */
+    class DetectorConstruction: public G4VUserDetectorConstruction {
 
-    public:
+        public:
 
-        /**
-         * Class constructor.
-         * @param theParser GDML parser defining the geometry.
-         */
-        DetectorConstruction(G4GDMLParser* theParser);
+            /**
+             * Class constructor.
+             * @param theParser GDML parser defining the geometry.
+             */
+            DetectorConstruction(G4GDMLParser* theParser);
 
-        /**
-         * Class destructor.
-         */
-        virtual ~DetectorConstruction();
+            /**
+             * Class destructor.
+             */
+            virtual ~DetectorConstruction();
 
-        /**
-         * Construct the detector.
-         * @return The top volume of the detector.
-         */
-        G4VPhysicalVolume *Construct();
+            /**
+             * Construct the detector.
+             * @return The top volume of the detector.
+             */
+            G4VPhysicalVolume *Construct();
 
-        void ConstructSDandField();
-        
-        /**
-         * Get the detector header.
-         * @return The detector header.
-         */
-        ldmx::DetectorHeader* getDetectorHeader() {
-            return auxInfoReader_->getDetectorHeader();
-        }
+            /**
+             */
+            void ConstructSDandField();
 
-    private:
+            /**
+             * Get the detector header.
+             * @return The detector header.
+             */
+            ldmx::DetectorHeader* getDetectorHeader() {
+                return auxInfoReader_->getDetectorHeader();
+            }
 
-        /**
-         * The GDML parser defining the detector.
-         */
-        G4GDMLParser* parser_;
+        private:
 
-        /**
-         * The auxiliary GDML info reader.
-         */
-        AuxInfoReader* auxInfoReader_;
-};
+            /**
+             * The GDML parser defining the detector.
+             */
+            G4GDMLParser* parser_;
+
+            /**
+             * The auxiliary GDML info reader.
+             */
+            AuxInfoReader* auxInfoReader_;
+    };
 
 }
 
