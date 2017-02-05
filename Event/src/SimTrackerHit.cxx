129e9bf7241ce8ba50afdb9db6c8f276e541bb34
Omar Moreno <email@omarmoreno.net>
2017-02-04 23:57:07 -0800

    Formatting.

--- Event/src/SimTrackerHit.cxx
+++ Event/src/SimTrackerHit.cxx
@@ -4,56 +4,55 @@ ClassImp(ldmx::SimTrackerHit)
 
 namespace ldmx {
 
-SimTrackerHit::SimTrackerHit()
-    : TObject() {
-}
+    SimTrackerHit::SimTrackerHit() :
+        TObject() {
+    }
 
-SimTrackerHit::~SimTrackerHit() {
-    Clear();
-}
+    SimTrackerHit::~SimTrackerHit() {
+        Clear();
+    }
 
-void SimTrackerHit::Print(Option_t *option) const {
-    std::cout << "SimTrackerHit { " << "id: " << id_ << ", " <<
-        "layerID: " << layerID_ << ", " <<
-        "position: ( " << x_ << ", " << y_ << ", " << z_ << " ), " <<
-        "edep: " << edep_ << ", " <<
-        "time: " << time_ << ", " <<
-        "momentum: ( " << px_ << ", " << py_ << ", " << pz_ << " )" <<
-        " }" << std::endl;
-}
+    void SimTrackerHit::Print(Option_t *option) const {
+        std::cout << "SimTrackerHit { " << "id: " << id_ << ", " <<
+                     "layerID: " << layerID_ << ", " <<
+                     "position: ( " << x_ << ", " << y_ << ", " << z_ << " ), " <<
+                     "edep: " << edep_ << ", " <<
+                     "time: " << time_ << ", " <<
+                     "momentum: ( " << px_ << ", " << py_ << ", " << pz_ << " )" <<
+                     " }" << std::endl;
+    }
 
-void SimTrackerHit::Clear(Option_t*) {
-    TObject::Clear();
+    void SimTrackerHit::Clear(Option_t*) {
+        TObject::Clear();
 
-    id_ = 0;
-    layerID_ = 0;
-    edep_ = 0;
-    time_ = 0;
-    px_ = 0;
-    py_ = 0;
-    pz_ = 0;
-    x_ = 0;
-    y_ = 0;
-    z_ = 0;
-    pathLength_ = 0;
+        id_ = 0;
+        layerID_ = 0;
+        edep_ = 0;
+        time_ = 0;
+        px_ = 0;
+        py_ = 0;
+        pz_ = 0;
+        x_ = 0;
+        y_ = 0;
+        z_ = 0;
+        pathLength_ = 0;
 
-    simParticle_.SetObject(nullptr);
-}
+        simParticle_ = nullptr;
+    }
 
-SimParticle* SimTrackerHit::getSimParticle() const {
-    return static_cast<SimParticle*>(simParticle_.GetObject());
-}
+    SimParticle* SimTrackerHit::getSimParticle() const {
+        return static_cast<SimParticle*>(simParticle_.GetObject());
+    }
 
-void SimTrackerHit::setPosition(const float x, const float y, const float z) {
-    this->x_ = x;
-    this->y_ = y;
-    this->z_ = z;
-}
+    void SimTrackerHit::setPosition(const float x, const float y, const float z) {
+        this->x_ = x;
+        this->y_ = y;
+        this->z_ = z;
+    }
 
-void SimTrackerHit::setMomentum(const float px, const float py, const float pz) {
-    this->px_ = px;
-    this->py_ = py;
-    this->pz_ = pz;
-}
-
+    void SimTrackerHit::setMomentum(const float px, const float py, const float pz) {
+        this->px_ = px;
+        this->py_ = py;
+        this->pz_ = pz;
+    }
 }
