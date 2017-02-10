042f1262ef6b5cd3f86eb263f0bf67cbe7d4a9f3
Omar Moreno <email@omarmoreno.net>
2017-02-10 13:51:05 -0800

    Remove code block used to process primary particle.  This functionality was move to it's own plugin.

--- Biasing/src/EcalProcessFilter.cxx
+++ Biasing/src/EcalProcessFilter.cxx
@@ -1,5 +1,5 @@
 /**
- * @file ECalPhotonuclearBiasing.cxx
+ * @file EcalProcessFilter.cxx
  * @brief User action plugin that biases Geant4 to only process events which
  *        involve a photonuclear reaction in the ECal.
  * @author Omar Moreno
@@ -25,49 +25,14 @@ namespace ldmx {
     EcalProcessFilter::~EcalProcessFilter() { 
     }
 
-    G4ClassificationOfNewTrack EcalProcessFilter::stackingClassifyNewTrack(
-            const G4Track* track, 
-            const G4ClassificationOfNewTrack& currentTrackClass) {
-
-        /*std::cout << "********************************" << std::endl;*/ 
-        /*std::cout << "*   Track pushed to the stack  *" << std::endl;*/
-        /*std::cout << "********************************" << std::endl;*/ 
-
-        // get the PDGID of the track.
-        G4int pdgID = track->GetParticleDefinition()->GetPDGEncoding();
-
-        // Get the particle type.
-        G4String particleName = track->GetParticleDefinition()->GetParticleName();
-
-        /*std::cout << "[ EcalProcessFilter ]: " << "\n" 
-                    << "\tParticle " << particleName      << " ( PDG ID: " << pdgID << " ) : " << "\n"
-                    << "\tTrack ID: " << track->GetTrackID()     << "\n" 
-                    << std::endl;*/
-
-
-        // Use current classification by default so values from other plugins are not overridden.
-        G4ClassificationOfNewTrack classification = currentTrackClass;
-
-        if (track->GetTrackID() == 1 && pdgID == 11) {
-            /*std::cout << "[ EcalProcessFilter ]: Pushing track to waiting stack." << std::endl;*/
-            return fWaiting; 
-        }
-
-        return classification;
-    }
-
     void EcalProcessFilter::stepping(const G4Step* step) { 
-
-        // Get the track associated with this step.
-        G4Track* track = step->GetTrack();
-
-        // Only process the primary particle (parent ID == 0) and its daughters
-        // (parent ID == 1)
-        if (track->GetParentID() > 1) return;
 
         /*std::cout << "************" << std::endl;*/
         /*std::cout << "*   Step   *" << std::endl;*/
         /*std::cout << "************" << std::endl;*/ 
+
+        // Get the track associated with this step.
+        G4Track* track = step->GetTrack();
 
         // get the PDGID of the track.
         G4int pdgID = track->GetParticleDefinition()->GetPDGEncoding();
@@ -80,7 +45,7 @@ namespace ldmx {
         G4String volumeName = volume->GetName();
 
         // Get the kinetic energy of the particle.
-        double incidentParticleEnergy = step->GetPreStepPoint()->GetTotalEnergy();
+        //double incidentParticleEnergy = step->GetPreStepPoint()->GetTotalEnergy();
 
         /*std::cout << "[ EcalProcessFilter ]:\n" 
                     << "\tTotal energy of " << particleName  << ": " << incidentParticleEnergy << " MeV \n"
@@ -92,70 +57,6 @@ namespace ldmx {
 
         // Get the particles daughters.
         const G4TrackVector* secondaries = step->GetSecondary();
-
-        // process primary track
-        if (track->GetParentID() == 0 
-                && track->GetCurrentStepNumber() == 1
-                && volumeName.compareTo(targetVolumeName_) == 0) {
-
-            // If the initial interaction didn't result in any secondaries e.g.
-            // a brem photon, don't bother processing the rest of the event.
-            if (secondaries->size() == 0) {     
-                track->SetTrackStatus(fKillTrackAndSecondaries); 
-
-                /*std::cout << "[ EcalProcessFilter ]: "  
-                            << "Primary did not produce secondaries --> Killing primary track!" 
-                            << std::endl;*/
-
-                G4RunManager::GetRunManager()->AbortEvent();
-                return;
-            }
-
-            G4String processName = secondaries->at(0)->GetCreatorProcess()->GetProcessName(); 
-
-            /*std::cout << "[ TargetPhotonuclearBiasingPlugin ]: "
-                        << "Incident electron produced " << secondaries->size() 
-                        << " particle via " << processName << " process." 
-                        << std::endl;*/
-
-            // If secondaries were produced via a process other than brem, stop 
-            // tracking all tracks.
-            if (processName.compareTo("eBrem") != 0 || secondaries->size() != 1) {
-
-
-                /*std::cout << "[ TargetPhotonuclearBiasingPlugin ]: "
-                            << "The secondaries are not a result of Brem. --> Killing all tracks!"
-                            << std::endl;*/
-
-                track->SetTrackStatus(fKillTrackAndSecondaries);
-
-                G4RunManager::GetRunManager()->AbortEvent();
-                return;
-            } 
-
-            G4Track* secondaryTrack = secondaries->at(0);
-            /*std::cout << "[ EcalProcessFilter ]: "
-                        << secondaryTrack->GetParticleDefinition()->GetParticleName() 
-                        << " with kinetic energy " << secondaryTrack->GetKineticEnergy()
-                        << " MeV was produced." << std::endl;*/
-
-            if (secondaryTrack->GetKineticEnergy() < photonEnergyThreshold_) { 
-
-                /*std::cout << "[ EcalProcessFilter ]: "
-                            << "Brem photon failed the energy threshold cut." 
-                            << std::endl;*/
-
-                track->SetTrackStatus(fKillTrackAndSecondaries);
-
-                G4RunManager::GetRunManager()->AbortEvent();
-                return;
-            }
-
-            /*std::cout << "[ ECalPhotonuclerBiasingPlugin ]: "
-                        << "Photon passed energy cut --> Suspending primary track!"
-                        << std::endl;*/
-            track->SetTrackStatus(fSuspend);   
-        }
 
         if (track->GetTrackID() == 2 && pdgID == 22) {
 
@@ -171,7 +72,7 @@ namespace ldmx {
                             << std::endl;*/
 
                 // Only record photonuclear events
-                if (!processName.contains("photonNuclear")) {
+                if (!processName.contains(BiasingMessenger::getProcess())) {
 
                     /*std::cout << "[ EcalProcessFilter ]: "
                                 << "Brem photon produced " << secondaries->size() 
@@ -180,7 +81,6 @@ namespace ldmx {
                                 << std::endl;*/
 
                     track->SetTrackStatus(fKillTrackAndSecondaries);
-
                     G4RunManager::GetRunManager()->AbortEvent();
                     return;
                 }
