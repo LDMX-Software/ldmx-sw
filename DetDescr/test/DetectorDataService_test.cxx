9b72218c49e2d86d4c48e8a9e70aa3ca5786d409
JeremyMcCormick <jermccormick@gmail.com>
2017-02-14 18:27:05 -0800

    Print out DetectorElement positions in test.

--- DetDescr/test/DetectorDataService_test.cxx
+++ DetDescr/test/DetectorDataService_test.cxx
@@ -1,6 +1,6 @@
 /*
  * DetectorDataService_test.cxx
- * @brief Loads detector data and prints out DetectorElement information from the hierarchy
+ * @brief Test that loads detector data and prints out DetectorElement information from the hierarchy
  * @author JeremyMcCormick, SLAC
  */
 
@@ -14,8 +14,15 @@
 
 // STL
 #include <iostream>
+#include <vector>
 
 using namespace ldmx;
+using namespace std;
+
+ostream& operator<<(ostream& stream, const vector<double>& pos) {
+    stream << " at (" << pos[0]*10. << ", " << pos[1]*10. << ", " << pos[2]*10. << ")";
+    return stream;
+}
 
 int main(int, const char* argv[])  {
 
@@ -33,23 +40,26 @@ int main(int, const char* argv[])  {
 
     // Print ECal info.
     DetectorElement* ecal = top->findChild("Ecal");
-    std::cout << "Got 'Ecal' DE with support '" << ecal->getSupport()->GetName() << "'" << std::endl;
+    std::cout << "Got 'Ecal' DE with support '" << ecal->getSupport()->GetName() << "'"
+            << ecal->getGlobalPosition() << std::endl;
     for (auto ecalLayer : ecal->getChildren()) {
         std::cout << "  " << ecalLayer->getName() << " with support "
                 << ecalLayer->getSupport()->GetName() << " and layer num "
                 << ((EcalLayer*)ecalLayer)->getLayerNumber()
+                << ecalLayer->getGlobalPosition()
                 << std::endl;
     }
-
     std::cout << std::endl;
 
     // Print HCal info.
     DetectorElement* hcal = top->findChild("Hcal");
-    std::cout << "Got 'Hcal' DE with support '" << hcal->getSupport()->GetName() << "'" << std::endl;
+    std::cout << "Got 'Hcal' DE with support '" << hcal->getSupport()->GetName() << "'"
+            << hcal->getGlobalPosition() << std::endl;
     for (auto hcalStation : hcal->getChildren()) {
         std::cout << "  " << hcalStation->getName() << " with support "
                 << hcalStation->getSupport()->GetName() << " and station num "
                 << ((HcalStation*)hcalStation)->getStationNumber()
+                << hcalStation->getGlobalPosition()
                 << std::endl;
     }
 
@@ -57,11 +67,13 @@ int main(int, const char* argv[])  {
 
     // Print Tagger info.
     DetectorElement* tagger = top->findChild("Tagger");
-    std::cout << "Got 'Tagger' DE with support '" << tagger->getSupport()->GetName() << "'" << std::endl;
+    std::cout << "Got 'Tagger' DE with support '" << tagger->getSupport()->GetName() << "'"
+            << tagger->getGlobalPosition() << std::endl;
     for (auto taggerLayer : tagger->getChildren()) {
         std::cout << "  " << taggerLayer->getName() << " with support "
                         << taggerLayer->getSupport()->GetName() << " and layer num "
                         << ((TaggerLayer*)taggerLayer)->getLayerNumber()
+                        << taggerLayer->getGlobalPosition()
                         << std::endl;
     }
 
@@ -69,12 +81,14 @@ int main(int, const char* argv[])  {
 
     // Print Recoil Tracker info.
     DetectorElement* recoilTracker = top->findChild("RecoilTracker");
-    std::cout << "Got 'RecoilTracker' DE with support " << recoilTracker->getSupport()->GetName() << "'" << std::endl;
+    std::cout << "Got 'RecoilTracker' DE with support '" << recoilTracker->getSupport()->GetName() << "'"
+            << recoilTracker->getGlobalPosition() << std::endl;
     for (auto recoilTrackerLayer : recoilTracker->getChildren()) {
         if (recoilTrackerLayer->getSupport()) {
             std::cout << "  " << recoilTrackerLayer->getName() << " with support "
                     << recoilTrackerLayer->getSupport()->GetName() << " and layer num "
                     << ((TaggerLayer*)recoilTrackerLayer)->getLayerNumber()
+                    << recoilTrackerLayer->getGlobalPosition()
                     << std::endl;
         } else {
             std::cout << "  " << recoilTrackerLayer->getName() << " with layer num "
@@ -83,18 +97,21 @@ int main(int, const char* argv[])  {
                 std::cout << "    " << recoilSensor->getName() << " with support "
                         << recoilSensor->getSupport()->GetName() << " and sensor num "
                         << ((RecoilTrackerSensor*)recoilSensor)->getSensorNumber()
+                        << recoilSensor->getGlobalPosition()
                         << std::endl;
             }
         }
     }
+    std::cout << std::endl;
 
     // Print target info.
     DetectorElement* target = top->findChild("Target");
-    std::cout << "Got 'Target' DE with support " << target->getSupport()->GetName() << "'" << std::endl;
+    std::cout << "Got 'Target' DE with support " << target->getSupport()->GetName() << "'" << target->getGlobalPosition() << std::endl;
     std::cout << "  targetThickness = " << ((TargetDetectorElement*)target)->getTargetThickness() << std::endl;
 
-    // Destroy the service object, which will delete the DetectorElement tree and the ROOT geometry manager.
+    // Delete the service object, which will delete the DetectorElement tree and the ROOT geometry manager.
     delete svc;
 
+    std::cout << std::endl;
     std::cout << "Done running DetectorDataService test!" << std::endl;
 }
