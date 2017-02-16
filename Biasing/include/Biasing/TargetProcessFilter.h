16104a91ac6fb73c8ce24f038b7812d922b7e12b
Omar Moreno <omoreno1@ucsc.edu>
2017-02-16 01:47:23 -0800

    Complete rewrite to allow multiple gammas from the target.

--- Biasing/include/Biasing/TargetProcessFilter.h
+++ Biasing/include/Biasing/TargetProcessFilter.h
@@ -8,12 +8,18 @@
 #ifndef SIMPLUGINS_TARGETPROCESSFILTER_H_
 #define SIMPLUGINS_TARGETPROCESSFILTER_H_
 
+//----------------//
+//   C++ StdLib   //
+//----------------//
+#include <algorithm>
+
 // Geant4
 #include "G4RunManager.hh"
 
 // LDMX
 #include "SimPlugins/UserActionPlugin.h"
 #include "Biasing/BiasingMessenger.h"
+#include "Biasing/TargetBremFilter.h"
 
 namespace ldmx {
 
@@ -51,19 +57,39 @@ class TargetProcessFilter : public UserActionPlugin {
             return true;
         }
 
+            /**
+             * Get whether this plugin implements the stacking aciton.
+             * @return True to indicate this plugin implements the stacking action.
+             */
+            bool hasStackingAction() { 
+                return true;
+            }
+
         /**
          * Implementmthe stepping action which performs the target volume biasing.
          * @param step The Geant4 step.
          */
         void stepping(const G4Step* step);
 
+            /**
+             * Classify a new track which postpones track processing.
+             * Track processing resumes normally if a target PN interaction occurred.
+             * @param aTrack The Geant4 track.
+             * @param currentTrackClass The current track classification.
+             */
+            G4ClassificationOfNewTrack stackingClassifyNewTrack(const G4Track* aTrack, const G4ClassificationOfNewTrack& currentTrackClass);
+
     private:
+
+        /** Pointer to the current track being processed. */
+        G4Track* currentTrack_{nullptr};
 
         /** The volume name of the LDMX target. */
         G4String volumeName_{"target_PV"};
 
         /** Brem photon energy threshold. */
         double photonEnergyThreshold_{2500}; // MeV
+
 };
 
 }
