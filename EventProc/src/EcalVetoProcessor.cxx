da42a70e845ea4508a25ad1731ed1c5b49596506
Omar Moreno <omoreno1@ucsc.edu>
2017-02-21 14:59:59 -0800

    Make the vectors containing the raw hit info class variables and zero them out at the beginning of each event.  This avoids the memory corruption error that was causing the processor to crash.

--- EventProc/src/EcalVetoProcessor.cxx
+++ EventProc/src/EcalVetoProcessor.cxx
@@ -22,24 +22,31 @@ void EcalVetoProcessor::configure(const ParameterSet& ps) {
     totalIsoCut_ = ps.getDouble("total_iso_cut");
     backEcalCut_ = ps.getDouble("back_ecal_cut");
     ratioCut_ = ps.getDouble("ratio_cut");
+
+    EcalLayerEdepRaw_.resize(nEcalLayers_, 0);
+    EcalLayerEdepReadout_.resize(nEcalLayers_, 0);
+    EcalLayerIsoRaw_.resize(nEcalLayers_, 0);
+    EcalLayerIsoReadout_.resize(nEcalLayers_, 0);
+    EcalLayerTime_.resize(nEcalLayers_, 0);
+
 }
 
 void EcalVetoProcessor::produce(Event& event) {
-    
-    std::vector<float> EcalLayerEdepRaw(nEcalLayers_, 0);
-    std::vector<float> EcalLayerEdepReadout(nEcalLayers_, 0);
-    std::vector<float> EcalLayerIsoRaw(nEcalLayers_, 0);
-    std::vector<float> EcalLayerIsoReadout(nEcalLayers_, 0);
-    std::vector<float> EcalLayerTime(nEcalLayers_, 0);
+
+    std::fill(EcalLayerEdepRaw_.begin(), EcalLayerEdepRaw_.end(), 0);
+    std::fill(EcalLayerEdepReadout_.begin(), EcalLayerEdepReadout_.end(), 0);
+    std::fill(EcalLayerIsoRaw_.begin(), EcalLayerIsoRaw_.end(), 0);
+    std::fill(EcalLayerIsoReadout_.begin(), EcalLayerIsoReadout_.end(), 0);
+    std::fill(EcalLayerTime_.begin(), EcalLayerTime_.end(), 0); 
    
     // Get the collection of digitized Ecal hits from the event. 
     const TClonesArray* ecalDigis = event.getCollection("ecalDigis");
     int nEcalHits = ecalDigis->GetEntriesFast();
     
-    //std::cout << "[ EcalVetoProcessor ] : Got " << nEcalHits << " ECal digis in event " << event.getEventHeader()->getEventNumber() << std::endl;
+    std::cout << "[ EcalVetoProcessor ] : Got " << nEcalHits << " ECal digis in event " << event.getEventHeader()->getEventNumber() << std::endl;
 
     std::vector<CellEnergyPair> layerMaxCellId(nLayersMedCal_, std::make_pair(0, 0));
-
+    
     //First, we find layer-wise max cell ids
     for (int hitCounter = 0; hitCounter < nEcalHits; ++hitCounter) {
         
@@ -53,56 +60,55 @@ void EcalVetoProcessor::produce(Event& event) {
             }
         }
     }
-
-    //Sort the layer-wise max energy deposition cells by energy and then select the median
+    
     std::sort(layerMaxCellId.begin(), layerMaxCellId.end(), [](const CellEnergyPair & a, const CellEnergyPair & b)
     {
         return a.second > b.second;
     });
     int showerMedianCellId = layerMaxCellId[layerMaxCellId.size() / 2].first;
 
+    //Sort the layer-wise max energy deposition cells by energy and then select the median
+
     //Loop over the hits from the event to calculate the rest of the important quantities
     for (int iHit = 0; iHit < nEcalHits; iHit++) {
         //Layer-wise quantities
         EcalHit* hit = (EcalHit*) ecalDigis->At(iHit);
         LayerCellPair hit_pair = hitToPair(hit);
-
-        EcalLayerEdepRaw[hit_pair.first] += hit->getEnergy();
+        EcalLayerEdepRaw_[hit_pair.first] = EcalLayerEdepRaw_[hit_pair.first] + hit->getEnergy();
 
         if (hit->getEnergy() > 0) {
-            EcalLayerEdepReadout[hit_pair.first] += hit->getEnergy();
-            EcalLayerTime[hit_pair.first] += (hit->getEnergy()) * hit->getTime();
+            EcalLayerEdepReadout_[hit_pair.first] += hit->getEnergy();
+            EcalLayerTime_[hit_pair.first] += (hit->getEnergy()) * hit->getTime();
         }
         //Check iso
         if (!(hexReadout_->isInShowerInnerRing(showerMedianCellId, hit_pair.second)) && !(hexReadout_->isInShowerOuterRing(showerMedianCellId, hit_pair.second)) && !(hit_pair.second == showerMedianCellId)) {
 
-            EcalLayerIsoRaw[hit_pair.first] += hit->getEnergy();
+            EcalLayerIsoRaw_[hit_pair.first] += hit->getEnergy();
 
             if (hit->getEnergy() > 0)
-                EcalLayerIsoReadout[hit_pair.first] += hit->getEnergy();
+                EcalLayerIsoReadout_[hit_pair.first] += hit->getEnergy();
         }
-    } // end loop over sim hits
+    }
+       
+    // end loop over sim hits
     float summedDep = 0, summedIso = 0, backSummedDep = 0;
-    for (int iLayer = 0; iLayer < EcalLayerEdepReadout.size(); iLayer++) {
-        EcalLayerTime[iLayer] = EcalLayerTime[iLayer] / EcalLayerEdepReadout[iLayer];
-        summedDep += EcalLayerEdepReadout[iLayer];
-        summedIso += EcalLayerIsoReadout[iLayer];
+    for (int iLayer = 0; iLayer < EcalLayerEdepReadout_.size(); iLayer++) {
+        EcalLayerTime_[iLayer] = EcalLayerTime_[iLayer] / EcalLayerEdepReadout_[iLayer];
+        summedDep += EcalLayerEdepReadout_[iLayer];
+        summedIso += EcalLayerIsoReadout_[iLayer];
         if (iLayer > backEcalStartingLayer_)
-            backSummedDep += EcalLayerEdepReadout[iLayer];
+            backSummedDep += EcalLayerEdepReadout_[iLayer];
     }
     
-
-    /*
-     if(verbose){
-     std::cout << "EdepRaw[0] : " << (*EcalLayerEdepRaw_)[0] << std::endl;
-     std::cout << "EdepReadout[0] : " << (*EcalLayerEdepReadout_)[0] << std::endl;
-     std::cout << "EcalLayerIsoRaw[0]: " << (*EcalLayerIsoRaw_)[0] << std::endl;
-     std::cout << "EcalLayerIsoReadout[0]: " << (*EcalLayerIsoReadout_)[0] << std::endl;
-     std::cout << "EcalLayerTime[0]: " << (*EcalLayerTime_)[0] << std::endl;
-     
-     std::cout << "Shower Median : " << showerMedianCellId << std::endl;
-     }// end verbose
-     */
+    
+    /*std::cout << "[ EcalVetoProcessor ]:\n" 
+              << "\t EdepRaw[0] : " << EcalLayerEdepRaw_[0] << "\n"
+              << "\t EdepReadout[0] : " << EcalLayerEdepReadout_[0] << "\n"
+              << "\t EdepLayerIsoRaw[0] : " << EcalLayerIsoRaw_[0] << "\n"
+              << "\t EdepLayerIsoReadout[0] : " << EcalLayerIsoReadout_[0] << "\n" 
+              << "\t EdepLayerTime[0] : " << EcalLayerTime_[0] << "\n"
+              << "\t Shower Median: " << showerMedianCellId 
+              << std::endl;*/
 
     doesPassVeto_ = (summedDep < totalDepCut_ && summedIso < totalIsoCut_ && backSummedDep < backEcalCut_); // add ratio cut in at some point
     /*
