88281c00eed7e7b8df6bff200aed665cf6c60db0
Owen Colegrove <ocolegro@Owens-MBP.matterhorn>
2017-03-01 12:28:00 -0800

    fixed

--- EventProc/python/ecalVeto.py
+++ EventProc/python/ecalVeto.py
@@ -6,5 +6,8 @@ ecalVeto = ldmxcfg.Producer("EcalVeto","ldmx::EcalVetoProcessor")
 
 ecalVeto.parameters["num_ecal_layers"] = 50
 ecalVeto.parameters["back_ecal_starting_layers"] = 22
-ecalVeto.parameters["num_layers_for_med_cal"] = 20
-
+ecalVeto.parameters["num_layers_for_med_cal"] = 10
+ecalVeto.parameters["total_dep_cut"] = 25.
+ecalVeto.parameters["total_outer_cut"] = 15.
+ecalVeto.parameters["back_ecal_cut"] = 1.
+ecalVeto.parameters["ratio_cut"] = 10.
