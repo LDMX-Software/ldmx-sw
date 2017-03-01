f10e73ee12ccd4f524ce5ebdb5399faaf6db64dd
Omar Moreno <email@omarmoreno.net>
2017-02-28 20:54:04 -0800

    Remove annoying std out

--- EventProc/src/EcalVetoProcessor.cxx
+++ EventProc/src/EcalVetoProcessor.cxx
@@ -35,14 +35,11 @@ namespace ldmx {
     }
 
     void EcalVetoProcessor::produce(Event& event) {
-    	std::cout << "Processing new event" << std::endl;
     	for (int i = 0; i < nEcalLayers_; i++){
-    		//std::cout << "Releasing old maps..." << std::endl;
             cellMap_[i].clear();
             cellMapIso_[i].clear();
     	}
 
-    	std::cout << "Old maps released, continuing" << std::endl;
     	nReadoutHits_  = 0;
         nIsoHits_      = 0;
         maxIsoDep_     = 0;
@@ -66,7 +63,6 @@ namespace ldmx {
         fillHitMap(ecalDigis,cellMap_);
         fillIsolatedHitMap(ecalDigis,globalCentroid,cellMap_,cellMapIso_);
         fillMipTracks(globalCentroid,cellMapIso_,trackVector_);
-        std::cout << "The size of trackVector_ = " << trackVector_.size() << std::endl;
 
         //Loop over the hits from the event to calculate the rest of the important quantities
         for (int iHit = 0; iHit < nEcalHits; iHit++) {
@@ -95,8 +91,6 @@ namespace ldmx {
 
         for (int iLayer = 0; iLayer < EcalLayerEdepReadout_.size(); iLayer++) {
     		for (auto cell : cellMapIso_[iLayer]){
-    			//std::cout << "Looping through cell map" << std::endl;
-
     			if (cell.second > 0){
     				nIsoHits_++;
     				summedIso_ += cell.second;
@@ -106,14 +100,12 @@ namespace ldmx {
     				maxIsoDep_ = cell.second;
     			}
     		}
-    		//std::cout << "Loop successful" << std::endl;
-            EcalLayerTime_[iLayer] = EcalLayerTime_[iLayer] / EcalLayerEdepReadout_[iLayer];
+    		EcalLayerTime_[iLayer] = EcalLayerTime_[iLayer] / EcalLayerEdepReadout_[iLayer];
             summedDet += EcalLayerEdepReadout_[iLayer];
             summedOuter += EcalLayerOuterReadout_[iLayer];
             if (iLayer > backEcalStartingLayer_)
                 backSummedDet += EcalLayerEdepReadout_[iLayer];
         }
-        std::cout << "The number of isolated hits = " << nIsoHits_ << std::endl;
         std::cout << "[ EcalVetoProcessor ]:\n" <<
         		"The global centroid = " << globalCentroid << "\n" << std::endl;
         /*std::cout << "[ EcalVetoProcessor ]:\n" 
@@ -133,12 +125,9 @@ namespace ldmx {
         	}
         }
 
-        std::cout << "Setting Result " << std::endl;
         result_.setResult(true, globalCentroid, nReadoutHits_, nIsoHits_, nMipTracks_, mipTrackDep_, longestMipTrack_,
         		summedDet, summedOuter,summedIso_, backSummedDet, maxIsoDep_, EcalLayerEdepRaw_);
-        std::cout << "Result Set" << std::endl;
         event.addToCollection("EcalVeto", result_);
-        std::cout << "Collection added" << std::endl;
     }
 }
 
