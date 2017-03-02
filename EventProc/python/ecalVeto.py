cbe2779fe1929fb5ea7e3f78a2ac6a1895a1d585
ocolegro <ocolegro@u.rochester.edu>
2017-03-02 10:06:14 -0800

    Update ecalVeto.py

--- EventProc/python/ecalVeto.py
+++ EventProc/python/ecalVeto.py
@@ -5,4 +5,4 @@ from LDMX.Framework import ldmxcfg
 ecalVeto = ldmxcfg.Producer("EcalVeto","ldmx::EcalVetoProcessor")
 
 ecalVeto.parameters["num_ecal_layers"] = 50
-ecalVeto.parameters["back_ecal_starting_layer"] = 22
+ecalVeto.parameters["num_layers_for_med_cal"] = 22
