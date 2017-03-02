03320eaa3db539171f0eac16fa7914ee9f2c671d
Omar Moreno <omoreno1@ucsc.edu>
2017-03-01 22:49:05 -0800

    Revert files to versions on master.

--- EventProc/python/ecalVeto.py
+++ EventProc/python/ecalVeto.py
@@ -4,5 +4,11 @@ from LDMX.Framework import ldmxcfg
 
 ecalVeto = ldmxcfg.Producer("EcalVeto","ldmx::EcalVetoProcessor")
 
-ecalVeto.parameters["num_ecal_layers"] = 50
-ecalVeto.parameters["back_ecal_starting_layers"] = 22
+ecalVeto.parameters["num_ecal_layers"] = 33
+ecalVeto.parameters["back_ecal_starting_layers"] = 20 
+ecalVeto.parameters["num_layers_for_med_cal"] = 10
+ecalVeto.parameters["total_dep_cut"] = 25.
+ecalVeto.parameters["total_iso_cut"] = 15.
+ecalVeto.parameters["back_ecal_cut"] = 1.
+ecalVeto.parameters["ratio_cut"] = 10.
+
