35d38ab2a1b59c46cd42a333136d93454b841dc7
JeremyMcCormick <jermccormick@gmail.com>
2017-03-04 22:03:12 -0800

    Add plugin that tags all tracks for saving (mostly for testing).

---
+++ SimPlugins/src/TrackSaver.cxx
@@ -0,0 +1,19 @@
+#include "SimPlugins/TrackSaver.h"
+
+namespace ldmx { 
+
+    extern "C" TrackSaver* createTrackSaver() {
+        return new TrackSaver;
+    }
+
+    extern "C" void destroyTrackSaver(TrackSaver* object) {
+        delete object;
+    }
+
+    TrackSaver::TrackSaver() { 
+    }
+
+    TrackSaver::~TrackSaver() { 
+    }
+}
+
