c4434fe37a3a21d483625b8d21b2332076745eae
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2017-03-08 16:43:39 -0800

    debugging

--- Event/include/Event/EcalVetoResult.h
+++ Event/include/Event/EcalVetoResult.h
@@ -40,7 +40,7 @@ namespace ldmx {
              * Set the sim particle and 'is findable' flag.
              */
 
-	    void EcalVetoResult::setVariables(int nReadoutHits, int nLooseIsoHits, int nTightIsoHits,
+	    void setVariables(int nReadoutHits, int nLooseIsoHits, int nTightIsoHits,
                 float summedDet, int summedOuter, float backSummedDet,
                         float summedLooseIso, float maxLooseIsoDep,float summedTightIso, float maxTightIsoDep,
                 float maxCellDep, float showerRMS, std::vector<float> EcalLayerEdepReadout,
