67853ea6e4bb390cb4952951ff6428991c8c826b
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2017-03-08 17:35:53 -0800

    updated w/ latest veto proc

--- EventProc/python/ecalVeto.py
+++ EventProc/python/ecalVeto.py
@@ -5,4 +5,5 @@ from LDMX.Framework import ldmxcfg
 ecalVeto = ldmxcfg.Producer("EcalVeto","ldmx::EcalVetoProcessor")
 ecalVeto.parameters["num_ecal_layers"] = 50
 ecalVeto.parameters["num_layers_for_med_cal"] = 22
-
+ecalVeto.parameters["n_bdt_vars"] = 22
+ecalVeto.parameters["discCut"] = .5  
