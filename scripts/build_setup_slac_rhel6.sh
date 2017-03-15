0902b233a667346e0336df3e2bae593040775c65
JeremyMcCormick <jermccormick@gmail.com>
2017-03-15 11:42:54 -0700

    Add build setup script for SLAC.

---
+++ scripts/build_setup_slac_rhel6.sh
@@ -0,0 +1,6 @@
+source /nfs/slac/g/ldmx/software/gcc/rhel6-64/setup.sh
+source /nfs/slac/g/ldmx/software/cmake/rhel6-64/setup.sh
+source /nfs/slac/g/ldmx/software/xerces/rhel6-64/setup.sh
+source /nfs/slac/g/ldmx/software/python/rhel6-64/setup.sh
+source /nfs/slac/g/ldmx/software/geant4/rhel6-64/geant4.10.02.p02-install/bin/geant4.sh
+source /nfs/slac/g/ldmx/software/root/rhel6-64/root-6.06.08-install/bin/thisroot.sh
