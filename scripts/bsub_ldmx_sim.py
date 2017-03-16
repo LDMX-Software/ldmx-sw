8814b163a093e043343c20032a35cfdfbd15edb7
JeremyMcCormick <jermccormick@gmail.com>
2017-03-15 18:37:05 -0700

    Updates to batch scripts.

--- scripts/bsub_ldmx_sim.py
+++ scripts/bsub_ldmx_sim.py
@@ -4,20 +4,22 @@ import os
 import argparse
 import math
 import subprocess
+import sys
 
-DEFAULT_DETECTOR = "ldmx-det-full-v2-fieldmap"
-RUN_SCRIPT = "run_ldmx_sim.py"
+DEFAULT_DETECTOR = 'ldmx-det-full-v2-fieldmap'
+RUN_SCRIPT = 'run_ldmx_sim.py'
 
 def main():
 
-    parser = argparse.ArgumentParser(description="Submit ldmx-sim LSF jobs using a particle gun macro")
-    parser.add_argument('-m', nargs="+", help="macros", required=True)         
-    parser.add_argument('-d', nargs=1, help="detector name")                   
+    parser = argparse.ArgumentParser(description='Submit ldmx-sim LSF jobs')
+    parser.add_argument('-m', action='append', dest='m', default=[], help="macros to run", required=False) 
+    parser.add_argument('-d', nargs=1, help="detector name", required=False) 
     parser.add_argument('-n', nargs=1, help="number of events", required=True) 
     parser.add_argument('-p', nargs=1, help="output directory", required=True) 
     parser.add_argument('-o', nargs=1, help="output file base name", required=True)
-    parser.add_argument('-j', nargs=1, help="number of jobs", required=True)
-    parser.add_argument('-x', action='store_true', help="enable dry run with no job submission")
+    parser.add_argument('-i', action='append', dest='i', default=[], help="input files", required=False)
+    parser.add_argument('-j', nargs=1, help="number of jobs (cannot be used with -i)", required=False)
+    parser.add_argument('-x', action='store_true', help="enable dry run with no job submission", required=False)
     args = parser.parse_args()
 
     macros = args.m
@@ -28,26 +30,39 @@ def main():
     nevents = int(args.n[0])
     outputdir = os.path.abspath(args.p[0])
     filename = args.o[0]
-    jobs = int(args.j[0])
     dryrun = args.x
+    input_files = args.i
+
+    if args.j is not None:
+        if len(input_files):
+            raise Exception("ERROR: The -i and -j arguments cannot be used together.")
+        jobs = int(args.j[0])
+    else:
+        jobs = len(input_files)
 
     jobtime = 2*round(float(nevents)/60./ 60., 0)
     if jobtime == 0:
         jobtime = 1
 
     exe, err = subprocess.Popen(['which', RUN_SCRIPT], stdout=subprocess.PIPE).communicate()
-    if "no %s in" % RUN_SCRIPT in exe:
+    if 'no %s in' % RUN_SCRIPT in exe:
         raise Exception("ERROR: The %s script was not found!  (Is it in the path?)" % RUN_SCRIPT)
     exe = exe[:-1]
 
+    macrostr = ' '.join([os.path.abspath(m) for m in macros])
+
     for jobnum in xrange(1, jobs + 1):
-        output_file = "%s_%04d" % (filename, jobnum)
-        log_file = "%s/%s.log" % (outputdir, output_file)
+        output_file = '%s_%04d' % (filename, jobnum)
+        log_file = '%s/%s.log' % (outputdir, output_file)
         if os.path.exists(log_file):
-            subprocess.Popen("rm %s" % log_file, shell=True).wait()
-        macrostr = ' '.join([os.path.abspath(m) for m in macros]) 
-        cmd = "bsub -W %d:0 -q long -o %s -e %s python %s -o %s.root -m %s -d %s -n %d -p %s" % \
-                (jobtime, log_file, log_file, exe, output_file, macrostr, detector, nevents, outputdir)
+            subprocess.Popen('rm %s' % log_file, shell=True).wait()
+        cmd = 'bsub -W %d:0 -q long -o %s -e %s python %s -o %s.root -d %s -n %d -p %s' % \
+                (jobtime, log_file, log_file, exe, output_file, detector, nevents, outputdir)
+        if len(macrostr):
+            cmd = '%s -m %s' % (cmd, macrostr)
+        if len(input_files):
+            input_file = os.path.abspath(input_files[jobnum-1])
+            cmd = '%s -i %s' % (cmd, input_file)
         print cmd
         if not dryrun:
             subprocess.Popen(cmd, shell=True).wait()
@@ -55,5 +70,5 @@ def main():
     if dryrun:
         print "\nWARNING: Dry run was enabled.  No jobs were submitted!"
 
-if __name__ == "__main__" :
+if __name__ == '__main__' :
     main()
--- scripts/run_ldmx_sim.py
+++ scripts/run_ldmx_sim.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import argparse
 import random
 import time
@@ -8,39 +10,45 @@ import subprocess
 # FIXME: Hard-coded mag field map location.
 field_map = '/nfs/slac/g/ldmx/users/omoreno/production/fieldmap/BmapCorrected3D_13k_unfolded_scaled_1.15384615385.dat'
 
-scratch_dir = '/scratch/'+getpass.getuser()
+# Geant4 macro that will be created by this script
 macro_path = './run.mac'
 
 def main():
 
-    parser = argparse.ArgumentParser(description='Run an ldmx-sim job')
-    parser.add_argument('-o', nargs=1,   help="output file",      required=True) # output file
-    parser.add_argument('-m', nargs="+", help="macros",           required=True) # one or more macros for generating events
-    parser.add_argument('-n', nargs=1,   help="number of events", required=True) # number of events to generate
-    parser.add_argument('-d', nargs=1,   help="detector name",    required=True) # detector name
-    parser.add_argument('-p', nargs=1,   help="output directory", required=True) # output directory
+    parser = argparse.ArgumentParser(description="Run an ldmx-sim job")
+    parser.add_argument('-i', nargs=1, help="input file (ROOT or LHE)", required=False) # input file if applicable
+    parser.add_argument('-o', nargs=1, help="output file", required=True) # output file
+    parser.add_argument('-m', action='append', dest='m', default=[], help="macros to run", required=False) # macros for generating events
+    parser.add_argument('-n', nargs=1, help="number of events", required=True) # number of events to generate
+    parser.add_argument('-d', nargs=1, help="detector name", required=True) # detector name
+    parser.add_argument('-p', nargs=1, help="output directory", required=True) # output directory
     args = parser.parse_args()
 
-    if not os.environ.get("LDMXSW_DIR"):
+    if not os.environ.get('LDMXSW_DIR'):
         raise Exception("ERROR: LDMXSW_DIR is not set in the environment!")
-    ldmxsw = os.environ.get("LDMXSW_DIR")
+    ldmxsw = os.environ.get('LDMXSW_DIR')
 
     output_file = args.o[0]
     macros = args.m
     nevents = int(args.n[0])
     detector_name = args.d[0]
     output_dir = os.path.abspath(args.p[0])
+    if args.i is not None:
+        input_file = args.i[0]
+    else:
+        if len(macros) == 0 :
+            raise Exception("ERROR: An input file or at least one macro must be supplied.")
+        input_file = None
 
+    scratch_dir = '/scratch/'+getpass.getuser()
     if not os.path.exists(scratch_dir):
         os.makedirs(scratch_dir)
-
     tmp_dir = '%s/%s' % (scratch_dir, os.environ['LSB_JOBID'])
-    print 'Creating tmp directory %s' % tmp_dir
     if not os.path.exists(tmp_dir):
         os.makedirs(tmp_dir)
 
     os.chdir(tmp_dir)
-    detector_data_dir=ldmxsw+"/Detectors/data/"+detector_name+"/"
+    detector_data_dir=ldmxsw+'/Detectors/data/'+detector_name+'/'
     for item in os.listdir(detector_data_dir):
         os.symlink(detector_data_dir+item,tmp_dir+'/'+ item)
     os.symlink(field_map, os.path.basename(field_map)) 
@@ -48,20 +56,18 @@ def main():
     if os.path.exists(output_file):
         raise Exception("ERROR: The output file %s already exists!" % output_file)
 
-    create_macro(detector_name, output_file, macros, nevents, ldmxsw)
+    create_macro(detector_name, output_file, input_file, macros, nevents, ldmxsw)
 
-    print "---- Job macro ----"
-    subprocess.Popen("cat run.mac", shell=True).wait()
-    print
+    subprocess.Popen('echo ---- Job macro ----; cat run.mac; echo', shell=True).wait()
 
-    command = "ldmx-sim %s" % macro_path
+    command = 'ldmx-sim %s' % macro_path
     subprocess.Popen(command, shell=True).wait()
-
-    #subprocess.Popen("rm -rf %s" % tmp_dir, shell=True).wait()
 
     os.system('cp -r %s/%s %s' % (tmp_dir, output_file, output_dir))
 
-def create_macro(detector, output_file, macros, nevents, ldmxsw_dir):
+    subprocess.Popen('rm -rf %s' % tmp_dir, shell=True).wait()
+
+def create_macro(detector, output_file, input_file, macros, nevents, ldmxsw_dir):
 
     random.seed(time.time())
     seed1 = int(random.random()*10000)
@@ -70,10 +76,15 @@ def create_macro(detector, output_file, macros, nevents, ldmxsw_dir):
     macro_file = open(macro_path, 'w')
     macro_file.write('/persistency/gdml/read '+ldmxsw_dir+'/Detectors/data/'+detector+'/detector.gdml\n')
     macro_file.write('/run/initialize\n')
-    macro_file.flush()
+    macro_file.write('/random/setSeeds %d %d\n' % (seed1, seed2))
     for macro in macros:
         macro_file.write('/control/execute '+macro+'\n')
-    macro_file.flush()
+    if input_file is not None:
+        if input_file.lower().endswith('.root'):
+            macro_file.write('/ldmx/generators/root/open '+input_file+'\n')
+            macro_file.write('/ldmx/generators/root/useSeed\n')
+        elif input_file.lower().endswith('.lhe'):
+            macro_file.write('/ldmx/generators/lhe/open '+input_file+'\n')
     macro_file.write('/ldmx/persistency/root/verbose 2\n')
     macro_file.write('/ldmx/persistency/root/file '+output_file+'\n')
     macro_file.write('/ldmx/plugins/load EventPrintPlugin\n')
@@ -81,5 +92,5 @@ def create_macro(detector, output_file, macros, nevents, ldmxsw_dir):
 
     macro_file.close()
 
-if __name__ == "__main__" :
+if __name__ == '__main__' :
     main()
