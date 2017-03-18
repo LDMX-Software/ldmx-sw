5a1ec8d185388febc8144d62d02d5e16b95a555e
JeremyMcCormick <jermccormick@gmail.com>
2017-03-17 18:07:59 -0700

    Updates to batch scripts.

--- scripts/bsub_ldmx_app.py
+++ scripts/bsub_ldmx_app.py
@@ -12,45 +12,52 @@ def main():
 
     parser = argparse.ArgumentParser(description="Run an ldmx-app job on LSF")
     parser.add_argument('-i', nargs=1, help="input string for file glob", required=True)
-    parser.add_argument('-o', nargs=1, help="output file base name", required=True)
+    parser.add_argument('-o', nargs=1, help="string to append to file name")
     parser.add_argument('-t', nargs=1, help="recon python template file", required=True)
-    parser.add_argument('-d', nargs=1, help="base dir for input files and output directory", required=True)
+    parser.add_argument('-d', nargs=1, help="output directory", required=True)
     parser.add_argument('-x', action='store_true', help="enable dry run with no job submission", required=False)
+    parser.add_argument('-s', action='store_true', help="skip job if output file already exists", required=False)
     args = parser.parse_args()
 
     input_pattern = args.i[0]
-    input_dir = os.path.abspath(args.d[0])
-    output_basename = args.o[0]
+    if args.o is not None:
+        output_append = args.o[0]
+    else:
+        output_append = "recon"
     output_dir = os.path.abspath(args.d[0])
     template_file = os.path.abspath(args.t[0])
+    skip_existing = args.s
     dryrun = args.x
 
-    input_glob = glob.glob('%s/%s*.root' % (input_dir, input_pattern))
+    input_glob = glob.glob('%s*.root' % (input_pattern))
 
     if len(input_glob) == 0:
-        raise Exception("ERROR: No input files found matching '%s' in dir '%s'." % (input_pattern, input_dir))
+        raise Exception("ERROR: No input files found matching '%s'." % (input_pattern))
 
     exe, err = subprocess.Popen(['which', RUN_SCRIPT], stdout=subprocess.PIPE).communicate()
     if 'no %s in' % RUN_SCRIPT in exe:
         raise Exception("ERROR: The %s script was not found!  (Is it in the path?)" % RUN_SCRIPT)
     exe = exe[:-1]
 
-    for input_file in input_glob:
-
-        fnum = input_file[input_file.rfind('_')+1:input_file.rfind('.')]
-        output_file = '%s_%s' % (output_basename, fnum)
-        log_file = '%s/%s.log' % (output_dir, output_file)
-        if os.path.exists(log_file):
-            subprocess.Popen('rm %s' % log_file, shell=True).wait()
-        cmd = 'bsub -W %d:0 -q long -o %s -e %s python %s -o %s.root -i %s -t %s -d %s' % \
-                (12, log_file, log_file, exe, output_file, input_file, template_file, output_dir)
-        print cmd
-
-        if not dryrun:
-            subprocess.Popen(cmd, shell=True).wait()
-
+    for input_file in input_glob:                
+        output_file = os.path.splitext(os.path.basename(input_file))[0] + "_" + output_append
+        log_file = os.path.join(output_dir, output_file + ".log")
+        input_path = os.path.abspath(input_file)
+        if not (skip_existing and os.path.exists(os.path.join(output_dir, output_file))):
+            if os.path.exists(log_file):
+                subprocess.Popen('rm %s' % log_file, shell=True).wait()
+            cmd = 'bsub -W %d:0 -q long -o %s -e %s python %s -o %s.root -i %s -t %s -d %s' % \
+                    (12, log_file, log_file, exe, output_file, input_path, template_file, output_dir)
+            print cmd
+            if not dryrun:
+                subprocess.Popen(cmd, shell=True).wait()
+        else:
+            print "Skipping submission for '%s' which already exists in '%s'." % (output_file, output_dir)
+                
     if dryrun:
         print "\nWARNING: Dry run was enabled.  No jobs were submitted!"
+    else:
+        print "\nSubmitted %d LSF jobs." % len(input_glob)
 
 if __name__ == '__main__' :
     main()
--- scripts/bsub_ldmx_sim.py
+++ scripts/bsub_ldmx_sim.py
@@ -20,6 +20,7 @@ def main():
     parser.add_argument('-i', action='append', dest='i', default=[], help="input files", required=False)
     parser.add_argument('-j', nargs=1, help="number of jobs (cannot be used with -i)", required=False)
     parser.add_argument('-x', action='store_true', help="enable dry run with no job submission", required=False)
+    parser.add_argument('-s', action='store_true', help="skip job if output file already exists", required=False)
     args = parser.parse_args()
 
     macros = args.m
@@ -31,6 +32,7 @@ def main():
     outputdir = os.path.abspath(args.p[0])
     filename = args.o[0]
     dryrun = args.x
+    skip_existing = args.s
     input_files = args.i
 
     if args.j is not None:
@@ -54,21 +56,26 @@ def main():
     for jobnum in xrange(1, jobs + 1):
         output_file = '%s_%04d' % (filename, jobnum)
         log_file = '%s/%s.log' % (outputdir, output_file)
-        if os.path.exists(log_file):
-            subprocess.Popen('rm %s' % log_file, shell=True).wait()
-        cmd = 'bsub -W %d:0 -q long -o %s -e %s python %s -o %s.root -d %s -n %d -p %s' % \
+        if not (skip_existing and os.path.exists(os.path.join(output_dir, output_file + ".root"))):
+            if os.path.exists(log_file):
+                subprocess.Popen('rm %s' % log_file, shell=True).wait()                
+            cmd = 'bsub -W %d:0 -q long -o %s -e %s python %s -o %s.root -d %s -n %d -p %s' % \
                 (jobtime, log_file, log_file, exe, output_file, detector, nevents, outputdir)
-        if len(macrostr):
-            cmd = '%s -m %s' % (cmd, macrostr)
-        if len(input_files):
-            input_file = os.path.abspath(input_files[jobnum-1])
-            cmd = '%s -i %s' % (cmd, input_file)
-        print cmd
-        if not dryrun:
-            subprocess.Popen(cmd, shell=True).wait()
+            if len(macrostr):
+                cmd = '%s -m %s' % (cmd, macrostr)
+            if len(input_files):
+                input_file = os.path.abspath(input_files[jobnum-1])
+                cmd = '%s -i %s' % (cmd, input_file)
+            print cmd
+            if not dryrun:
+                subprocess.Popen(cmd, shell=True).wait()
+        else:
+            print "Skipping submission for '%s' which already exists in '%s'." % (output_file, output_dir)
 
     if dryrun:
         print "\nWARNING: Dry run was enabled.  No jobs were submitted!"
+    else:
+        print "\nSubmitted %d LSF jobs." % jobs
 
 if __name__ == '__main__' :
     main()
