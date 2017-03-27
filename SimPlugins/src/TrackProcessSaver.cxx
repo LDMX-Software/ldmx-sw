04f3d5846bed46bc4398de2b828b4ec6959c256a
JeremyMcCormick <jermccormick@gmail.com>
2017-03-27 16:51:14 -0700

    Add option on TrackProcessSaver for exact match on process name; default is partial match.

--- SimPlugins/src/TrackProcessSaver.cxx
+++ SimPlugins/src/TrackProcessSaver.cxx
@@ -18,8 +18,18 @@ namespace ldmx {
         auto trackInfo = dynamic_cast<UserTrackInformation*>(aTrack->GetUserInformation());
         const G4VProcess* process = aTrack->GetCreatorProcess();
         if (process) {
-            if (processNames_.find(process->GetProcessName()) != processNames_.end()) {
-                trackInfo->setSaveFlag(true);
+            for (const auto& processName : processNames_) {
+                if (exactMatch_[processName]) {
+                    if (!processName.compare(process->GetProcessName())) {
+                        trackInfo->setSaveFlag(true);
+                    }
+                } else {
+                    if (process->GetProcessName().find(processName) != std::string::npos) {
+                        trackInfo->setSaveFlag(true);
+                    }
+                }
+            }
+            if (trackInfo->getSaveFlag()) {
                 if (verbose_ > 1) {
                     std::cout << "[ TrackProcessSaver ] : saving track ID " << aTrack->GetTrackID()
                             << " with process '" << process->GetProcessName() << "'" << std::endl;
