75cfb0d1c6ceced6d2c324cf1cff966a8b818bb2
JeremyMcCormick <jermccormick@gmail.com>
2017-04-03 18:20:22 -0700

    Merge in changes from master

--- SimApplication/src/TrackMap.cxx
+++ SimApplication/src/TrackMap.cxx
@@ -9,32 +9,26 @@
 
 namespace ldmx {
 
-void TrackMap::addSecondary(G4int trackID, G4int parentID) {
-    trackIDMap_[trackID] = parentID;
-}
-
-G4VTrajectory* TrackMap::findTrajectory(const G4Event* anEvent, G4int trackID) {
-    G4TrajectoryContainer* trajCont = anEvent->GetTrajectoryContainer();
-    G4int currTrackID = trackID;
-    G4VTrajectory* traj = NULL;
-    for (;;) {
-        traj = Trajectory::findByTrackID(trajCont, currTrackID);
-        if (traj != NULL) {
-            break;
-        } else {
-            if (trackIDMap_.find(currTrackID) != trackIDMap_.end()) {
-                currTrackID = trackIDMap_[currTrackID];
-            } else {
+    G4VTrajectory* TrackMap::findTrajectory(G4int trackID) {
+        G4int currTrackID = trackID;
+        G4VTrajectory* traj = nullptr;
+        for (;;) {
+            traj = trajectoryMap_[currTrackID];
+            if (traj) {
                 break;
+            } else {
+                if (trackIDMap_.find(currTrackID) != trackIDMap_.end()) {
+                    currTrackID = trackIDMap_[currTrackID];
+                } else {
+                    break;
+                }
             }
         }
+        return traj;
     }
-    return traj;
-}
 
-void TrackMap::clear() {
-    trackIDMap_.clear();
+    void TrackMap::clear() {
+        trackIDMap_.clear();
+        trajectoryMap_.clear();
+    }
 }
-
-}
-
