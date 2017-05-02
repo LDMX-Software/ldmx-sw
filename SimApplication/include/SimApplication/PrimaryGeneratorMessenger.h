a237fc577b367d2add7ad309868cea0630e710d2
Nhan Tran <ntran@rhel6-64m.slac.stanford.edu>
2017-05-02 11:51:16 -0700

    first commit

--- SimApplication/include/SimApplication/PrimaryGeneratorMessenger.h
+++ SimApplication/include/SimApplication/PrimaryGeneratorMessenger.h
@@ -10,6 +10,7 @@
 // Geant4
 #include "G4UImessenger.hh"
 #include "G4UIcmdWithoutParameter.hh"
+#include "G4UIcmdWithAString.hh"
 
 // LDMX
 #include "SimApplication/PrimaryGeneratorAction.h"
@@ -46,8 +47,16 @@ namespace ldmx {
              */
             static bool useRootSeed() {
                 return useRootSeed_;
-            }
-            ;
+            };
+
+            /** Get the particle type (e.g. gamma, e-) for mpg */
+            static std::string getMPGParticleType() { return particleType_; };
+        
+            /** Get the particle energy for mpg */
+            static double getMPGParticleEnergy() { return particleEnergy_; };
+
+            /** Get the particle energy for mpg */
+            static int getMPGnInteractions() { return nInteractions_; };
 
         private:
 
@@ -76,15 +85,30 @@ namespace ldmx {
              */
             G4UIcommand* rootOpenCmd_;
 
-            /**
+            /** 
              * The command for opening Root files.
              */
             G4UIcmdWithoutParameter* rootUseSeedCmd_ {new G4UIcmdWithoutParameter {"/ldmx/generators/root/useSeed", this}};
+
+            /** The command for using the multiparticle gun. */
+            G4UIcmdWithoutParameter* enableMPGunCmd_ {new G4UIcmdWithoutParameter {"/ldmx/generators/mpgun/enable", this}};
+            /** Command allowing a user to specify what particle type to generate. */
+            G4UIcmdWithAString* mpgunParticleTypeCmd_{new G4UIcmdWithAString{"/ldmx/generators/mpgun/particle", this}};
+            /** Command allowing a user to specify what particle type to generate. */
+            G4UIcmdWithAString* mpgunEnergyCmd_{new G4UIcmdWithAString{"/ldmx/generators/mpgun/energy", this}};
+            /** Command allowing a user to specify what particle type to generate. */
+            G4UIcmdWithAString* mpgunNIntCmd_{new G4UIcmdWithAString{"/ldmx/generators/mpgun/nInteractions", this}};
 
             /**
              * FIXME: This should not be static.
              */
             static bool useRootSeed_;
+            // /** particle type */
+            static std::string particleType_;
+            // * Particle energy threshold. 
+            static double particleEnergy_;
+            // * Particle energy threshold. 
+            static int nInteractions_;
 
     };
 
