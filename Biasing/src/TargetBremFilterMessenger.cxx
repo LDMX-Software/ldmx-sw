c96faf737c0ba177b3c1bfb127086a5c6305b0d4
Omar Moreno <omoreno1@ucsc.edu>
2017-05-25 12:45:45 -0700

    Add volume and brem energy threshold commands.

--- Biasing/src/TargetBremFilterMessenger.cxx
+++ Biasing/src/TargetBremFilterMessenger.cxx
@@ -5,24 +5,53 @@
  */
 
 #include "Biasing/TargetBremFilterMessenger.h"
+#include <iostream>
 
 namespace ldmx { 
     
     TargetBremFilterMessenger::TargetBremFilterMessenger(TargetBremFilter* filter) :
         UserActionPluginMessenger(filter), filter_(filter) {
 
-            killRecoilElectronCmd_ 
-                = new G4UIcmdWithoutParameter{std::string(getPath() + "kill_electron").c_str(), this};
-            killRecoilElectronCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit);
-            killRecoilElectronCmd_->SetGuidance("Enable killing of the electron track that produces the brem."); 
+            bremEnergyThresholdCmd_ = new G4UIcmdWithAString{std::string(getPath() + "brem_threshold").c_str(), this};
+            bremEnergyThresholdCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit,
+                                              G4ApplicationState::G4State_Idle);
+            bremEnergyThresholdCmd_->SetGuidance("Minium energy that the brem electron should have."); 
+
+            killRecoilCmd_ 
+                = new G4UIcmdWithoutParameter{std::string(getPath() + "kill_recoil").c_str(), this};
+            killRecoilCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit,
+                    G4ApplicationState::G4State_Idle);
+            killRecoilCmd_->SetGuidance("Enable killing of the electron track that produces the brem."); 
+
+
+            recoilEnergyThresholdCmd_ = new G4UIcmdWithAString{std::string(getPath() + "recoil_threshold").c_str(), this};
+            recoilEnergyThresholdCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit,
+                                              G4ApplicationState::G4State_Idle);
+            recoilEnergyThresholdCmd_->SetGuidance("Energy threshold that the recoil electron must not exceed."); 
+
+            volumeCmd_ = new G4UIcmdWithAString{std::string(getPath() + "volume").c_str(), this};
+            volumeCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit,
+                                           G4ApplicationState::G4State_Idle);
+            volumeCmd_->SetGuidance("Volume to apply the filter to.");     
     }
 
     TargetBremFilterMessenger::~TargetBremFilterMessenger() {
-        delete killRecoilElectronCmd_;
+        delete bremEnergyThresholdCmd_;
+        delete killRecoilCmd_;
+        delete recoilEnergyThresholdCmd_;
+        delete volumeCmd_; 
     }
 
     void TargetBremFilterMessenger::SetNewValue(G4UIcommand* command, G4String newValue) {
         
-        if (command == killRecoilElectronCmd_) filter_->setKillRecoilElectron(true); 
+        if (command == killRecoilCmd_) { 
+            filter_->setKillRecoilElectron(true); 
+        } else if (command == volumeCmd_) {
+            filter_->setVolume(newValue);
+        } else if (command == recoilEnergyThresholdCmd_)  {
+            filter_->setRecoilEnergyThreshold(G4UIcommand::ConvertToDouble(newValue));
+        } else if (command == bremEnergyThresholdCmd_) {
+            filter_->setBremEnergyThreshold(G4UIcommand::ConvertToDouble(newValue));
+        }
     }
 }
