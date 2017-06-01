8c7892c559112802e1ef0e3a47929b31209f7b77
omar-moreno <email@omarmoreno.net>
2017-05-31 23:32:34 -0700

    Read auxiliary info and assign it to the corresponding volumes.

--- SimApplication/src/ParallelWorld.cxx
+++ SimApplication/src/ParallelWorld.cxx
@@ -1,7 +1,6 @@
 
 #include "SimApplication/ParallelWorld.h"
 
-
 ldmx::ParallelWorld::ParallelWorld(G4GDMLParser* parser, G4String worldName) 
     : G4VUserParallelWorld(worldName), parser_(parser), 
       auxInfoReader_(new AuxInfoReader(parser)) {
@@ -11,8 +10,6 @@ ldmx::ParallelWorld::~ParallelWorld() {
 }
 
 void ldmx::ParallelWorld::Construct() {
-    auxInfoReader_->readGlobalAuxInfo();
-    auxInfoReader_->assignAuxInfoToVolumes();
 }
 
 void ldmx::ParallelWorld::ConstructSD() {
@@ -21,10 +18,14 @@ void ldmx::ParallelWorld::ConstructSD() {
     G4LogicalVolume* worldLogical = worldPhysical->GetLogicalVolume();
     
     G4LogicalVolume* parallelWorldLogical = parser_->GetWorldVolume()->GetLogicalVolume();
+    auxInfoReader_->readGlobalAuxInfo();
+     
     for (int index = 0; index < parallelWorldLogical->GetNoDaughters(); index++) { 
+        G4VPhysicalVolume* physicalVol = parallelWorldLogical->GetDaughter(index); 
         std::cout << "[ ParallelWorld ]: Adding : " 
-                  << parallelWorldLogical->GetDaughter(index)->GetName() 
-                  << " to parallel world." << std::endl;
-        worldLogical->AddDaughter(parallelWorldLogical->GetDaughter(index));
+                  << physicalVol->GetName() << " to parallel world." << std::endl;
+        worldLogical->AddDaughter(physicalVol);
     }
+
+    auxInfoReader_->assignAuxInfoToVolumes();
 }
