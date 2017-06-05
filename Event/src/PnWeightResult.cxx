077ab0a93c1446d2abfdc4fb2aa61c4c18223c57
quinnanm <mkq5008@gmail.com>
2017-06-05 14:38:50 -0700

    output raw info pnWeight

--- Event/src/PnWeightResult.cxx
+++ Event/src/PnWeightResult.cxx
@@ -22,15 +22,18 @@ namespace ldmx {
     void PnWeightResult::Clear(Option_t *option) {
         TObject::Clear();
         weight_ = 1.;
+        test_ = 0.;
     }
 
     void PnWeightResult::Copy(TObject& object) const {
         PnWeightResult& result = (PnWeightResult&) object;
         result.weight_ = weight_;
+        result.test_ = test_;
     }
 
     void PnWeightResult::Print(Option_t *option) const {
         std::cout << "[ PnWeightResult ]:\n" << "\t PN Weight : " << weight_ << "\n" << std::endl;
+        std::cout << "[ PnWeightResult ]:\n" << "\t PN Test : " << test_ << "\n" << std::endl;
     }
 
 }
