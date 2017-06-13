b5f4f3b07c3fd8f4fafef4dc100f3db0d0b47741
omar-moreno <email@omarmoreno.net>
2017-06-13 15:57:01 -0700

    Add the polar angle and the kinetic energy of the particle used to calculate Wp.

--- Event/src/PnWeightResult.cxx
+++ Event/src/PnWeightResult.cxx
@@ -20,32 +20,42 @@ namespace ldmx {
         Clear();
     }
 
-    void PnWeightResult::setResult(double weight, double measuredWp, double wpFit) { 
+    void PnWeightResult::setResult(double fitWp, double ke, 
+                                   double measuredWp, double theta, 
+                                   double weight) {
         
-        weight_ = weight;
+        fitWp_ = fitWp;
+        ke_ = ke;  
         measuredWp_ = measuredWp;
-        fitWp_ = wpFit; 
+        theta_ = theta; 
+        weight_ = weight;
     }
 
     void PnWeightResult::Clear(Option_t *option) {
         TObject::Clear();
-        weight_ = 0.0;
+        fitWp_ = 0.0;
+        ke_ = 0; 
         measuredWp_ = 0.0;
-        fitWp_ = 0.0; 
+        theta_ = 0.0;
+        weight_ = 0.0;
     }
 
     void PnWeightResult::Copy(TObject& object) const { 
         PnWeightResult& result = (PnWeightResult&) object; 
-        result.weight_ = weight_; 
+        result.fitWp_ = fitWp_;
+        result.ke_ = ke_; 
+        result.theta_ = theta_;  
         result.measuredWp_ = measuredWp_; 
-        result.fitWp_ = fitWp_; 
+        result.weight_ = weight_; 
     }
 
     void PnWeightResult::Print(Option_t *option) const {
         std::cout << "[ PnWeightResult ]:\n" 
+                  << "\t W_p fit : "      << fitWp_ << "\n"
+                  << "\t PN daughter kinetic energy: " << ke_ << "\n"
+                  << "\t W_p measured : " << measuredWp_ << "\n"
+                  << "\t PN daughter polar angle: " << theta_ << "\n"
                   << "\t PN Weight : "    << weight_ << "\n" 
-                  << "\t W_p measured : " << measuredWp_ << "\n" 
-                  << "\t W_p fit : "      << fitWp_ << "\n" 
                   << std::endl;
     }
 }
