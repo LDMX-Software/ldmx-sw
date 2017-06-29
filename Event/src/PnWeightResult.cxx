b794351ab59d3094d789c0a535ccda26d5982ac3
quinnanm <mkq5008@gmail.com>
2017-06-28 19:41:31 -0700

    apply reweighting to either protons or neutrons

--- Event/src/PnWeightResult.cxx
+++ Event/src/PnWeightResult.cxx
@@ -20,79 +20,39 @@ namespace ldmx {
         Clear();
     }
 
-    void PnWeightResult::setResult(
-                           double fitWp, double ke, double wp, double theta, double weight,
-                           double fitWp_max, double ke_max, double wp_max, double theta_max, double weight_max,
-                           double fitWp_p, double ke_p, double wp_p, double theta_p, double weight_p
-                                  ){
-        fitWp_ = fitWp;
+    void PnWeightResult::setResult(double ke, double theta, double w, double fitW, double weight){
+        fitW_ = fitW;
         ke_ = ke;  
-        wp_ = wp;
+        w_ = w;
         theta_ = theta; 
         weight_ = weight;
-
-        fitWp_max_ = fitWp_max;
-        ke_max_ = ke_max;
-        wp_max_ = wp_max;
-        theta_max_ = theta_max;
-        weight_max_ = weight_max;
-
-        fitWp_p_ = fitWp_p;
-        ke_p_ = ke_p;
-        wp_p_ = wp_p;
-        theta_p_ = theta_p;
-        weight_p_ = weight_p;
     }
 
     void PnWeightResult::Clear(Option_t *option) {
         TObject::Clear();
 
-        fitWp_ = 0.0;
+        fitW_ = 0.0;
         ke_ = 0; 
-        wp_ = 0.0;
+        w_ = 0.0;
         theta_ = 0.0;
         weight_ = 0.0;
-
-        fitWp_max_ = 0.0;
-        ke_max_ = 0.0;
-        wp_max_ = 0.0;
-        theta_max_ = 0.0;
-        weight_max_ = 0.0;
-
-        fitWp_p_ = 0.0;
-        ke_p_ = 0.0;
-        wp_p_ = 0.0;
-        theta_p_ = 0.0;
-        weight_p_ = 0.0;
     }
 
     void PnWeightResult::Copy(TObject& object) const { 
         PnWeightResult& result = (PnWeightResult&) object; 
 
-        result.fitWp_ = fitWp_;
+        result.fitW_ = fitW_;
         result.ke_ = ke_; 
         result.theta_ = theta_;  
-        result.wp_ = wp_; 
+        result.w_ = w_; 
         result.weight_ = weight_; 
-
-        result.fitWp_max_ = fitWp_max_;
-        result.ke_max_ = ke_max_;
-        result.wp_max_ = wp_max_;
-        result.theta_max_ = theta_max_;
-        result.weight_max_ = weight_max_;
-
-        result.fitWp_p_ = fitWp_p_;
-        result.ke_p_ = ke_p_;
-        result.wp_p_ = wp_p_;
-        result.theta_p_ = theta_p_;
-        result.weight_p_ = weight_p_;
     }
 
     void PnWeightResult::Print(Option_t *option) const {
         std::cout << "[ PnWeightResult ]:\n" 
-                  << "\t W_p fit : "      << fitWp_ << "\n"
+                  << "\t W fit : "      << fitW_ << "\n"
                   << "\t PN daughter kinetic energy: " << ke_ << "\n"
-                  << "\t W_p measured : " << wp_ << "\n"
+                  << "\t W measured : " << w_ << "\n"
                   << "\t PN daughter polar angle: " << theta_ << "\n"
                   << "\t PN Weight : "    << weight_ << "\n" 
                   << std::endl;
