01d9acfd448ecb1d25bc694081287e8b65c52869
quinnanm <mkq5008@gmail.com>
2017-06-28 19:41:31 -0700

    apply reweighting to either protons or neutrons

--- EventProc/src/PnWeightProcessor.cxx
+++ EventProc/src/PnWeightProcessor.cxx
@@ -10,7 +10,7 @@
  * arising from photonNuclear reactions, and is intended to correct
  * the simulation in the case of high-momentum, backwards-going nucleons
  * arising from those reactions.
- *   fit variable W_p = 0.5*(p_tot + K)*(1.12-0.5*(p_z/p))
+ *   fit variable W = 0.5*(p_tot + K)*(1.12-0.5*(p_z/p))
  *   where p_tot = sqrt(K^2 + 2*K*m)
  *           K = kinetic energy of nucleon at PN vertex
  *           p, p_z = momentum, z-component of nucleon at PN vertex
@@ -21,46 +21,30 @@
 namespace ldmx {
 
     void PnWeightProcessor::configure(const ParameterSet& pSet) {
-        wpThreshold_ = pSet.getDouble("wp_threshold");
+        wThreshold_ = pSet.getDouble("w_threshold");
+        wTheta_ = pSet.getDouble("w_theta");
+        wPdgId_ = 2212; // demoted from pSet. use proton or neutron hists and fit curve.
+
+        // open data files
+        TString fName = "$LDMXSW_DIR/data/config/W_hists_Owen_Omar.root";
+        TString hName = "W_Omar_5GeV_Pb_";
+        hName += (wPdgId_ == 2212) ? "Protons" : "Neutrons";
+        wFile = TFile::Open(fName);
+        if(!wFile) throw std::invalid_argument(TString::Format("[PnWeightProcessor::configure] Cannot find data file %s",fName.Data()).Data());
+        wHist = (TH1F*)wFile->Get(hName);
+        if(!wHist) throw std::invalid_argument(TString::Format("[PnWeightProcessor::configure] Cannot hist %s in data file %s",hName.Data(),fName.Data()).Data());
+        std::cout << TString::Format("[PnWeightProcessor::configure] Hist %s from data file %s opened successfully",hName.Data(),fName.Data()) << std::endl;
     }
 
     void PnWeightProcessor::produce(Event& event) {
         result_.Clear();
+        bool verb = false;
 
         // Get the collection of sim particles from the event.  If the 
         // collection of sim particles is empty, don't process the
         // event.
         const TClonesArray *simParticles = event.getCollection("SimParticles");
         if (simParticles->GetEntriesFast() == 0) return; 
-
-        // Calculate max(Wp) over all daughers of PN reactions
-        // nucleon_max = PN daughter with highest Wp
-        //std::set<double> setWp;
-        double wp_max = -1.;
-        SimParticle* nucleon_max{nullptr};
-        double keNucleon_max{0};
-        double thetaNucleon_max{0};
-        for(int particleCount = 0; particleCount < simParticles->GetEntriesFast(); ++particleCount){ 
-            SimParticle* simParticle = static_cast<SimParticle*>(simParticles->At(particleCount));
-            if( (simParticle->getProcessType() == 121) && (simParticle->getParentCount() != 0) ){
-                double thisWp = this->calculateWp(simParticle);
-                if(thisWp > wp_max) {
-                  wp_max = thisWp;
-                  double ke = (simParticle->getEnergy() - simParticle->getMass());
-                  double px = simParticle->getMomentum()[0];
-                  double py = simParticle->getMomentum()[1];
-                  double pz = simParticle->getMomentum()[2];
-                  double p = sqrt(px*px + py*py + pz*pz);
-                  double theta = acos(pz/p)*180.0/3.14159;
-                  keNucleon_max = ke;
-                  nucleon_max = simParticle;
-                  thetaNucleon_max = theta;
-                }
-                //setWp.insert(thisWp);
-            }
-        }
-        //double wp_max_set = *setWp.rbegin();
-        //std::cout << TString::Format("wp_max %.3f, keNucleon_max %.3f, thetaNucleon_max %.3f", wp_max, keNucleon_max, thetaNucleon_max) << std::endl;
 
         // Loop through all of the particles and search for the recoil electron
         // i.e. an electron which doesn't have any parents.
@@ -73,13 +57,14 @@ namespace ldmx {
             // If the particle doesn't correspond to the recoil electron, 
             // continue to the next particle.
             if ((simParticle->getPdgID() == 11) && (simParticle->getParentCount() == 0)) {
-                //std::cout << "[ pnWeightProcessor ]: Recoil electron found." << std::endl;
+                if(verb) std::cout << "[ pnWeightProcessor ]: Recoil electron found." << std::endl;
                 recoilElectron = simParticle; 
                 break;
             }
         }
 
         // Search for the PN gamma and use it to get the PN daughters.
+        // TODO: Improve 121 (hadronInelastic) check with processType=="photonNuclear" check
         SimParticle* pnGamma{nullptr};
         for (int daughterCount = 0; daughterCount < recoilElectron->getDaughterCount(); ++daughterCount) {
             SimParticle* daughter = recoilElectron->getDaughter(daughterCount);
@@ -89,13 +74,13 @@ namespace ldmx {
                 break;
             }
         }
-        
+
         // For PN biased events, there should always be a gamma that 
         // underwent a PN reaction.
         if (pnGamma == nullptr) return; // throw a runtime exception 
-        
+
         // Loop over all PN daughters and find the highest energy 
-        // back-scattered neutron and proton
+        // back-scattered nucleon
        	SimParticle* nucleon{nullptr}, *nucleon_p{nullptr};
         double keNucleon{0}, keNucleon_p{0};
         double thetaNucleon{0}, thetaNucleon_p{0}; 
@@ -107,64 +92,51 @@ namespace ldmx {
             double pz = pnDaughter->getMomentum()[2];
             double p = sqrt(px*px + py*py + pz*pz); 
             double theta = acos(pz/p)*180.0/3.14159;
-            if ((pnDaughter->getPdgID() == 2112) && (ke > keNucleon)) { 
+            // can use == wPdgId_ here for exclusively neutrons or protons
+            if ((pnDaughter->getPdgID() == 2212 || pnDaughter->getPdgID() == 2112) && (ke > keNucleon)) {
                 keNucleon = ke;
                 nucleon = pnDaughter;
                 thetaNucleon = theta;  
             }
-            if ((pnDaughter->getPdgID() == 2212) && (ke > keNucleon)) { 
-                keNucleon_p = ke;
-                nucleon_p = pnDaughter;
-                thetaNucleon_p = theta;  
-            }
         }
 
+        if(nucleon && verb) std::cout << TString::Format("Nucleon ke %0.3f, pdgId %d, theta %0.3f", keNucleon, nucleon->getPdgID(), thetaNucleon) << std::endl;
         if (nucleon == nullptr) return; // throw a runtime exception
-        //if (nucleon_p == nullptr) return; // throw a runtime exception
 
-        // Calculate W_p
-        double wp = this->calculateWp(nucleon); 
-        double wp_p = (nucleon_p != nullptr) ? this->calculateWp(nucleon_p) : -1;
+        // Calculate W
+        double w = this->calculateW(nucleon);
 
-        // Calculate weight from Wp fit
-        // neutrons
+        // Calculate weight from W fit
         double weight = 1.0;
-        double wpFit = 0.0;
-        if ((wp >= wpThreshold_ ) && (thetaNucleon > 100)) {
-            wpFit = this->calculateFitWp(wp);  
-            weight = wpFit/wp; 
+        double wFit = 0.0;
+        double denomHist = 0.;
+        if ((w >= wThreshold_ ) && (thetaNucleon > wTheta_)) {
+            wFit = this->calculateFitW(w);
+            denomHist = wHist->Interpolate(w);
+            weight = wFit/denomHist;
         } 
-        //std::cout << "[ pnWeightProcessor ] : PN weight: " << result_.getWeight() << std::endl;
-        // protons
-        double weight_p = 1.0;
-        double wpFit_p = 0.0;
-        if ((wp_p >= wpThreshold_ ) && (thetaNucleon_p > 100)) {
-            wpFit_p = this->calculateFitWp(wp_p);  
-            weight_p = wpFit_p/wp_p; 
-        }
-        // max
-        double weight_max = 1.0;
-        double wpFit_max = 0.0;
-        if ((wp_max >= wpThreshold_ ) && (thetaNucleon_max > 100)) {
-            wpFit_max = this->calculateFitWp(wp_max);  
-            weight_max = wpFit_max/wp_max; 
-        }
-
-        //std::cout << TString::Format("wp_max %.3f wpFit_p %.3f keNucleon_p %.3f wp_p %.3f thetaNucleon_p %.3f weight_p %.3f wpFit_max %.3f weight_max %.3f",
-        //                              wp_max,     wpFit_p,     keNucleon_p,     wp_p,     thetaNucleon_p,     weight_p,     wpFit_max,     weight_max) << std::endl;
+        if(verb) std::cout << TString::Format("wThreshold_ %0.3f, w %0.6f, wFit %0.6f, denomHist %0.6f, weight %0.9f",
+                                               wThreshold_, w, wFit, denomHist, weight) << std::endl;
+        if(verb) std::cout << "[ pnWeightProcessor ] : PN weight: " << result_.getWeight() << std::endl;
 
         // Set the resulting weight.
-        result_.setResult(wpFit, keNucleon, wp, thetaNucleon, weight, wpFit_max, keNucleon_max, wp_max, thetaNucleon_max, weight_max, wpFit_p, keNucleon_p, wp_p, thetaNucleon_p, weight_p);
+        result_.setResult(keNucleon, thetaNucleon, w, wFit, weight);
 
         // Add the result to the collection     
         event.addToCollection("pnWeight", result_);
     }
 
-    double PnWeightProcessor::calculateFitWp(double wp) { 
-        return 1.78032e+04*exp(-8.07561e-03*(wp - 7.91244e+02)); 
+    double PnWeightProcessor::calculateFitW(double w) {
+        if(wPdgId_ == 2212){
+          return exp(3.66141e+00+-8.14167e-03*w); // proton curve
+        }else if(wPdgId_ == 2112){
+          return exp(3.79133e+00+-8.33342e-03*w); // neutron curve
+        }else{
+          throw std::invalid_argument(TString::Format("[PnWeightProcessor::calculateFitW] invalid wPdgId_ value of %d, expected 2212 or 2112",wPdgId_).Data());
+        }
     }
 
-    double PnWeightProcessor::calculateWp(SimParticle* particle) {
+    double PnWeightProcessor::calculateW(SimParticle* particle) {
         double px = particle->getMomentum()[0];
         double py = particle->getMomentum()[1];
         double pz = particle->getMomentum()[2];
