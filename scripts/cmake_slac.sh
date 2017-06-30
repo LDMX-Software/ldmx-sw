8fd42b15b9b062bae7b0946a678d0c4f07dcf943
JeremyMcCormick <jermccormick@gmail.com>
2017-06-29 17:49:21 -0700

    add setup scripts for cmake and gcc

--- scripts/cmake_slac.sh
+++ scripts/cmake_slac.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+. /nfs/slac/g/ldmx/software/cmake/rhel6-64/setup.sh
+. /nfs/slac/g/ldmx/software/gcc/rhel6-64/setup.sh
+
 Geant4_DIR=/nfs/slac/g/ldmx/software/geant4/rhel6-64/geant4.10.02.p02-install/lib64/Geant4-10.2.2/
 ROOT_DIR=/nfs/slac/g/ldmx/software/root/rhel6-64/root-6.06.08-install/
 XercesC_DIR=/nfs/slac/g/ldmx/software/xerces/rhel6-64/xerces-c-3.1.4/
