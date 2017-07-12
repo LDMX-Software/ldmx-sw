f96e89a2e0b96520c17c582ca200749a1d017931
JeremyMcCormick <jermccormick@gmail.com>
2017-07-12 16:32:02 -0700

    put script logic in functions

--- scripts/fix_gdml_names.py
+++ scripts/fix_gdml_names.py
@@ -5,7 +5,7 @@
   Fix GDML attributes written by the G4GDMLParser class.
 
     * strip pointer strings from name attributes
-    * strip pointer strings from ref attributs
+    * strip pointer strings from ref attributes
     * append copynumber to physvol names to make them unique
     * replace position and rotation names with one based on corrected physvol name
 
@@ -15,18 +15,10 @@
 
 """
 
+debug = True
+
 import sys
 import xml.etree.ElementTree
-
-if sys.argc == 1:
-    raise Exception("Missing name of GDML file")
-
-fname = sys.argv[1]
-
-et = xml.etree.ElementTree.parse(fname)
-root = et.getroot()
-
-debug = True
 
 def strip_pointer_string(name):
     try:
@@ -37,46 +29,56 @@ def strip_pointer_string(name):
     except:
         return name
 
-for child in root.iter():
-    if 'name' in child.attrib:
-
-        if debug:
-            print "old name: " + child.attrib['name']
-
-        # strip pointer strings from name attributes
-        child.attrib['name'] = strip_pointer_string(child.attrib['name'])
-
-        # append string to make PV names unique
-        if child.tag == "physvol":
-            child.attrib['name'] = child.attrib['name'] + "_physvol" 
-            if 'copynumber' in child.attrib:
-                child.attrib['name'] = child.attrib['name'] + child.attrib['copynumber']
-            for pv_child in child:
-                if pv_child.tag == "rotation":
-                    if debug:
-                        print "old rot name: " + pv_child.attrib['name']
-                    pv_child.attrib['name'] = child.attrib['name'] + "_rot"
-                    if debug:
-                        print "new rot name: " + pv_child.attrib['name']
-                if pv_child.tag == "position":
-                    if debug:
-                        print "old pos name: " + pv_child.attrib['name']
-                    pv_child.attrib['name'] = child.attrib['name'] + "_pos"
-                    if debug:
-                        print "new pos name: " + pv_child.attrib['name']
-
-        if debug:
-            print "new name: " + child.attrib['name']
+def fix_gdml_file(name):
+    et = xml.etree.ElementTree.parse(fname)
+    root = et.getroot()
+    for child in root.iter():
+        if 'name' in child.attrib:
     
-    if 'ref' in child.attrib:
+            if debug:
+                print "old name: " + child.attrib['name']
+    
+            # strip pointer strings from name attributes
+            child.attrib['name'] = strip_pointer_string(child.attrib['name'])
+    
+            # append string to make PV names unique
+            if child.tag == "physvol":
+                child.attrib['name'] = child.attrib['name'] + "_physvol" 
+                if 'copynumber' in child.attrib:
+                    child.attrib['name'] = child.attrib['name'] + child.attrib['copynumber']
+                for pv_child in child:
+                    if pv_child.tag == "rotation":
+                        if debug:
+                            print "old rot name: " + pv_child.attrib['name']
+                        pv_child.attrib['name'] = child.attrib['name'] + "_rot"
+                        if debug:
+                            print "new rot name: " + pv_child.attrib['name']
+                    if pv_child.tag == "position":
+                        if debug:
+                            print "old pos name: " + pv_child.attrib['name']
+                        pv_child.attrib['name'] = child.attrib['name'] + "_pos"
+                        if debug:
+                            print "new pos name: " + pv_child.attrib['name']
+    
+            if debug:
+                print "new name: " + child.attrib['name']
+        
+        if 'ref' in child.attrib:
+    
+            if debug:
+                print "old ref: " + child.attrib['ref']
+    
+            # strip pointer strings from ref attributes
+            child.attrib['ref'] = strip_pointer_string(child.attrib['ref'])
+    
+            if debug:
+                print "new ref: " + child.attrib['ref']
 
-        if debug:
-            print "old ref: " + child.attrib['ref']
+    et.write(fname)
 
-        # strip pointer strings from ref attributes
-        child.attrib['ref'] = strip_pointer_string(child.attrib['ref'])
+if __name__ == "__main__":
+    if sys.argc == 1:
+        raise Exception("Missing name of GDML file")
+    fname = sys.argv[1]
+    fix_gdml_file(fname)
 
-        if debug:
-            print "new ref: " + child.attrib['ref']
-
-et.write(fname) 
