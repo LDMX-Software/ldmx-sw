072a67862906767ca5889cd161f93c40a790e9b9
Alex James Rickman <arickman@rhel6-64j.slac.stanford.edu>
2017-07-24 16:24:06 -0700

    electronuclear biasing

---
+++ Biasing/include/Biasing/electroNuclearTargetProcessFilter.h
@@ -0,0 +1,110 @@
+/**
+ * @file electroNuclearTargetProcessFilter.h
+ * @brief Class defining a UserActionPlugin that biases Geant4 to only process events which
+ *        involve an electronuclear reaction in the target
+ * @author Omar Moreno--modified by Alex Rickman, SLAC National Accelerator Laboratory
+ */
+
+#ifndef BIASING_TARGETPROCESSFILTER_H_
+#define BIASING_TARGETPROCESSFILTER_H_
+
+//----------------//
+//   C++ StdLib   //
+//----------------//
+#include <algorithm>
+
+// Geant4
+#include "G4RunManager.hh"
+
+// LDMX
+#include "SimPlugins/UserActionPlugin.h"
+#include "Biasing/BiasingMessenger.h"
+#include "Biasing/TargetBremFilter.h"
+
+namespace ldmx {
+
+    /**
+     * @class electroNuclearTargetProcessFilter
+     * @brief Biases Geant4 to only process events where PN reaction occurred in the target
+     */
+    class electroNuclearTargetProcessFilter : public UserActionPlugin {
+
+        public:
+
+            /**
+             * Class constructor.
+             */
+            electroNuclearTargetProcessFilter();
+
+            /**
+             * Class destructor.
+             */
+            ~electroNuclearTargetProcessFilter();
+
+            /**
+             * Get the name of the plugin.
+             * @return The name of the plugin.
+             */
+            virtual std::string getName() {
+                return "electroNuclearTargetProcessFilter";
+            }
+
+            /**
+             * Get whether this plugin implements the event action.
+             * @return True if the plugin implements the event action.
+             */
+            virtual bool hasEventAction() { 
+                return true;
+            }
+
+            /**
+             * Get whether this plugin implements the stepping action.
+             * @return True to indicate this plugin implements the stepping action.
+             */
+            bool hasSteppingAction() {
+                return true;
+            }
+
+            /**
+             * Get whether this plugin implements the stacking aciton.
+             * @return True to indicate this plugin implements the stacking action.
+             */
+            bool hasStackingAction() { 
+                return true;
+            }
+
+            /**
+             * Implementmthe stepping action which performs the target volume biasing.
+             * @param step The Geant4 step.
+             */
+            void stepping(const G4Step* step);
+
+            /**
+             * End of event action.
+             */
+            virtual void endEvent(const G4Event*);
+
+            /**
+             * Classify a new track which postpones track processing.
+             * Track processing resumes normally if a target EN interaction occurred.
+             * @param aTrack The Geant4 track.
+             * @param currentTrackClass The current track classification.
+             */
+            G4ClassificationOfNewTrack stackingClassifyNewTrack(const G4Track* aTrack, const G4ClassificationOfNewTrack& currentTrackClass);
+
+        private:
+
+            /** Pointer to the current track being processed. */
+            G4Track* currentTrack_{nullptr};
+
+            /** The volume name of the LDMX target. */
+            G4String volumeName_{"target_PV"};
+
+            /** Flag indicating if the reaction of intereset occurred. */
+            bool reactionOccurred_{false};
+
+    };
+
+}
+
+#endif // BIASING_TARGETPROCESSFILTER_H__
