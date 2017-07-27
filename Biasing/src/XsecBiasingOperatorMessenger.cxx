512c74972b8b8febf1212b123f3f463462aed6a8
omar-moreno <email@omarmoreno.net>
2017-07-27 10:08:48 -0700

    Messenger for setting parameters on XsecBiasingOperator.

---
+++ Biasing/src/XsecBiasingOperatorMessenger.cxx
@@ -0,0 +1,65 @@
+/**
+ * @file XsecBiasingOperatorMessenger.cxx
+ * @brief Messenger for setting parameters on XsecBiasingOperator.
+ * @author Omar Moreno, SLAC National Accelerator Laboratory
+ */
+
+#include "Biasing/XsecBiasingOperatorMessenger.h"
+
+namespace ldmx { 
+    
+    XsecBiasingOperatorMessenger::XsecBiasingOperatorMessenger(XsecBiasingOperator* oprt) :
+        operator_(oprt) {
+    
+        biasingDir_ = new G4UIdirectory{"/ldmx/biasing/xsec/"};
+
+        biasAllCmd_ = new G4UIcmdWithoutParameter{"/ldmx/biasing/xsec/all", this};
+        biasAllCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit,
+                                        G4ApplicationState::G4State_Idle);
+        biasAllCmd_->SetGuidance("Bias all particles of the given type."); 
+
+        particleTypeCmd_ = new G4UIcmdWithAString{"/ldmx/biasing/xsec/particle", this};
+        particleTypeCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit,
+                                             G4ApplicationState::G4State_Idle);
+        particleTypeCmd_->SetGuidance("Set the particle type to bias."); 
+
+        processCmd_ = new G4UIcmdWithAString{"/ldmx/biasing/xsec/process", this};
+        processCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit,
+                                        G4ApplicationState::G4State_Idle);
+        processCmd_->SetGuidance("Set the process to bias."); 
+        
+        thresholdCmd_ = new G4UIcmdWithAString{"/ldmx/biasing/xsec/threshold", this};
+        thresholdCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit,
+                                          G4ApplicationState::G4State_Idle);
+        thresholdCmd_->SetGuidance("Set the minimum energy required to bias a particle."); 
+        
+        xsecTransCmd_ = new G4UIcmdWithAString{"/ldmx/biasing/xsec/factor", this};
+        xsecTransCmd_->AvailableForStates(G4ApplicationState::G4State_PreInit,  
+                                          G4ApplicationState::G4State_Idle);
+        xsecTransCmd_->SetGuidance("Set the factor by which the xsec will increase."); 
+    }
+
+    XsecBiasingOperatorMessenger::~XsecBiasingOperatorMessenger() {
+        delete biasingDir_;
+        delete biasAllCmd_;
+        delete particleTypeCmd_;
+        delete processCmd_;
+        delete thresholdCmd_;
+        delete xsecTransCmd_;
+    }
+
+    void XsecBiasingOperatorMessenger::SetNewValue(G4UIcommand* command, G4String newValue) {
+        
+        if (command == biasAllCmd_) { 
+            operator_->biasAll(); 
+        } else if (command == particleTypeCmd_)  {
+            operator_->setParticleType(newValue); 
+        } else if (command == processCmd_) {
+            operator_->setProcess(newValue); 
+        } else if (command == thresholdCmd_) { 
+            operator_->setThreshold(G4UIcommand::ConvertToDouble(newValue)); 
+        } else if (command == xsecTransCmd_) { 
+            operator_->setXsecFactor(G4UIcommand::ConvertToDouble(newValue));
+        }
+    }
+}
