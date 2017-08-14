666473843f20044c1ecaebe1675b732feef9b031
JeremyMcCormick <jermccormick@gmail.com>
2017-08-14 16:54:58 -0700

    correct macro dir

--- scripts/export_detector.py
+++ scripts/export_detector.py
@@ -11,10 +11,10 @@ det_in = sys.argv[1]
 det_out = sys.argv[2]
                 
 p = Popen(['ldmx-sim'], stdin=PIPE)
-p.stdin.write("/ldmx/persistency/gdml/removeModule magnet.gdml\n")
-p.stdin.write("/ldmx/persistency/gdml/read %s\n" % det_in)
+p.stdin.write("/ldmx/gdml/removeModule magnet.gdml\n")
+p.stdin.write("/ldmx/gdml/read %s\n" % det_in)
 p.stdin.write("/run/initialize\n")
-p.stdin.write("/ldmx/persistency/gdml/write %s\n" % det_out)
+p.stdin.write("/ldmx/gdml/write %s\n" % det_out)
 p.stdin.write("exit\n")
 p.stdin.close()
 p.wait()
