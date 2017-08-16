737b8f5a152ddcd60678c42f5bb814b9ede01096
JeremyMcCormick <jermccormick@gmail.com>
2017-08-15 20:01:10 -0700

    correct dumb copy pasta

--- DetDescr/include/DetDescr/HcalID.h
+++ DetDescr/include/DetDescr/HcalID.h
@@ -16,32 +16,35 @@ namespace ldmx {
      * @class EcalDetectorID
      * @brief Extension of DefaultDetectorID providing access to ECal cell number in a hex grid
      */
-    class EcalDetectorID : public DefaultDetectorID {
+    class HcalID : public DefaultDetectorID {
 
         public:
 
             /**
              * Adds a cell field and re-initializes the ID.
              */
-            EcalDetectorID() {
+            HcalID() {
                 this->getFieldList()->push_back(new IDField("section", 2, 12, 14));
                 this->getFieldList()->push_back(new IDField("strip", 3, 15, 20));
                 init();
             }
 
             /**
-             * Get the value of the cell field from the ID.
-             * @return The value of the cell field.
+             * Get the value of the 'section' field from the ID.
+             * @return The value of the 'strip' field.
              */
             int getSection() {
                 return this->getFieldValue(3);
             }
 
+            /**
+             * Get the value of the 'strip' field from the ID.
+             * @return The value of 'strip' field.
+             */
             int getStrip() {
                 return this->getFieldValue(4);
             }
     };
-
 }
 
 #endif
