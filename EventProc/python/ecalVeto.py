350d3b363a3c5a655aac92ede3ac840f3f6daeff
omar-moreno <email@omarmoreno.net>
2017-10-09 11:22:56 -0700

    Update recon config files.

--- EventProc/python/ecalVeto.py
+++ EventProc/python/ecalVeto.py
@@ -3,7 +3,7 @@
 from LDMX.Framework import ldmxcfg
 
 ecalVeto = ldmxcfg.Producer("EcalVeto","ldmx::EcalVetoProcessor")
-ecalVeto.parameters["num_ecal_layers"] = 50
-ecalVeto.parameters["num_layers_for_med_cal"] = 22
-ecalVeto.parameters["disc_cut"] = .5
-ecalVeto.parameters["bdt_file"] = "bdt.pkl" 
+ecalVeto.parameters["num_ecal_layers"] = 34
+ecalVeto.parameters["do_bdt"] = 1
+ecalVeto.parameters["bdt_file"] = "cal_bdt.pkl" 
+ecalVeto.parameters["disc_cut"] = 0.999672
