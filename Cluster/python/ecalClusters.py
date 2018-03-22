476f98828eb689f0f5938de3f7a0add83183c1b6
Joshua Hiltbrand <hiltbran@umn.edu>
2018-03-22 07:59:19 -0500

    Core clustering algorithm files

---
+++ Cluster/python/ecalClusters.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+# we need the ldmx configuration package to construct the object
+from LDMX.Framework import ldmxcfg
+
+ecalClusters = ldmxcfg.Producer("ecalClusters", "ldmx::EcalClusterProducer")
+
+# Cluster weight cutoff in appropriate units
+ecalClusters.parameters["cutoff"] = 10.0 
+
+# Seed threshold for clustering
+ecalClusters.parameters["seedThreshold"] = 100.0 # MeV
+
+# Pass name for ecal digis
+ecalClusters.parameters["digisPassName"] = "recon"
+
+# Name of the algo to save to the root file 
+ecalClusters.parameters["algoName"] = "MyClusterAlgo"
+
+# Name of the cluster collection to make
+ecalClusters.parameters["clusterCollName"] = "ecalClusters"
+
+# Name of the cluster algo collection to make
+ecalClusters.parameters["algoCollName"] = "ClusterAlgoResult"
