d1e7f140feda18d6d9671217b06bbc5116545df9
omar-moreno <email@omarmoreno.net>
2018-04-19 15:09:32 -0700

    Update hcalDigis config.

--- EventProc/python/hcalDigis.py
+++ EventProc/python/hcalDigis.py
@@ -4,9 +4,15 @@ from LDMX.Framework import ldmxcfg
 
 hcalDigis = ldmxcfg.Producer("hcalDigis", "ldmx::HcalDigiProducer")
 
-# set the mean noise in PE units
-hcalDigis.parameters["meanNoise"] = 0.2
-hcalDigis.parameters["mev_per_mip"] = 1.4
-hcalDigis.parameters["pe_per_mip"] = 13.5
-hcalDigis.parameters["doStrip"] = 0
+hcalDigis.parameters["meanNoise"] = 0.004
+hcalDigis.parameters["readoutThreshold"]= 1
+hcalDigis.parameters["strips_side_lr_per_layer"] = 3
+hcalDigis.parameters["num_side_lr_hcal_layers"] = 17
+hcalDigis.parameters["strips_side_tb_per_layer"] = 3
+hcalDigis.parameters["num_side_tb_hcal_layers"] = 17
+hcalDigis.parameters["strips_back_per_layer"] = 31
+hcalDigis.parameters["num_back_hcal_layers"] = 40
+hcalDigis.parameters["mev_per_mip"] = 1.4*20./6.
+hcalDigis.parameters["pe_per_mip"] = 50.
+hcalDigis.parameters["doStrip"] = True
 
