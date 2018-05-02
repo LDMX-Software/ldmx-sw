1135a377711b2107d4986f33a5690a4965544831
Gavin Niendorf <gavinniendorf@gmail.com>
2018-05-01 18:30:15 -0700

    Update bdt_skim.py

--- Configuration/python/bdt_skim.py
+++ Configuration/python/bdt_skim.py
@@ -15,17 +15,31 @@ p.libraries.append("libEventProc.so")
 ecalVeto = ldmxcfg.Producer("ecalVeto", "ldmx::EcalVetoProcessor")
 ecalVeto.parameters["num_ecal_layers"] = 34
 ecalVeto.parameters["do_bdt"] = 1
-ecalVeto.parameters["bdt_file"] = "cal_bdt.pkl"
-ecalVeto.parameters["disc_cut"] = 0.94
+ecalVeto.parameters["bdt_file"] = "fid_bdt.pkl"
+ecalVeto.parameters["cellxy_file"] = "cellxy.txt"
+ecalVeto.parameters["disc_cut"] = 0.95
+
+# Configure the Non-Fiducial Ecal veto processor
+NonFidecalVeto = ldmxcfg.Producer("NonFidecalVeto", "ldmx::NonFidEcalVetoProcessor")
+NonFidecalVeto.parameters["num_ecal_layers"] = 34
+NonFidecalVeto.parameters["do_bdt"] = 1
+#Files in order of increasing mass
+NonFidecalVeto.parameters["nf_bdt_files"] = ["p001_nf_bdt.pkl", "p01_nf_bdt.pkl", "p1_nf_bdt.pkl", "p0_nf_bdt.pkl"]
+NonFidecalVeto.parameters["cellxy_file"] = "cellxy.txt"
+#Disc cuts in order of increasing mass
+NonFidecalVeto.parameters["disc_cut"] = [0.99, 0.95, 0.94, 0.94]
 
 # Add the processor to the processing chain
-p.sequence=[ecalVeto]
+# If you are dropping fiducial or non-fiducial events you can delete one of these.
+p.sequence=[ecalVeto, NonFidecalVeto]
 
 # Default to dropping all events
 p.skimDefaultIsDrop()
 
 # Use the output of the Ecal veto processor to decide what events should be 
 # dropped
+# This option will also drop non-fiducial events. To drop fiducial events change this to 
+# NonFidecalVeto
 p.skimConsider("ecalVeto")
 
 # Set the input and output files to use
