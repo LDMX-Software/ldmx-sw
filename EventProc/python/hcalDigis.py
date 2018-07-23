8bbd32082665ba56c75dbed7ceb7a3c113f62a72
Nhan Tran <nhan.v.tran@gmail.com>
2018-07-23 14:28:50 -0700

    a new noise treatment for dual readout

--- EventProc/python/hcalDigis.py
+++ EventProc/python/hcalDigis.py
@@ -12,8 +12,8 @@ hcalDigis.parameters["strips_side_tb_per_layer"] = 3
 hcalDigis.parameters["num_side_tb_hcal_layers"] = 17
 hcalDigis.parameters["strips_back_per_layer"] = 62 # n strips correspond to 5 cm wide bars
 hcalDigis.parameters["num_back_hcal_layers"] = 40
-hcalDigis.parameters["super_strip_size"] = 4 # 1 = 5 cm readout, 2 = 10 cm readout, ...
+hcalDigis.parameters["super_strip_size"] = 1 # 1 = 5 cm readout, 2 = 10 cm readout, ...
 hcalDigis.parameters["mev_per_mip"] = 1.4*20./6.
-hcalDigis.parameters["pe_per_mip"] = 50. # PEs per MIP at 1m (assume 80% attentuation of 1m)
+hcalDigis.parameters["pe_per_mip"] = 68. # PEs per MIP at 1m (assume 80% attentuation of 1m)
 hcalDigis.parameters["doStrip"] = True
 
