756f7aa497f43fd9cadcaae19457c3d30e405d2e
Nhan Tran <nhan.v.tran@gmail.com>
2018-08-20 13:47:08 -0700

    adding the updated config for the new v9 geometry

--- EventProc/python/hcalDigis.py
+++ EventProc/python/hcalDigis.py
@@ -6,11 +6,11 @@ hcalDigis = ldmxcfg.Producer("hcalDigis", "ldmx::HcalDigiProducer")
 
 hcalDigis.parameters["meanNoise"] = 0.02
 hcalDigis.parameters["readoutThreshold"]= 1
-hcalDigis.parameters["strips_side_lr_per_layer"] = 3
-hcalDigis.parameters["num_side_lr_hcal_layers"] = 17
-hcalDigis.parameters["strips_side_tb_per_layer"] = 3
-hcalDigis.parameters["num_side_tb_hcal_layers"] = 17
-hcalDigis.parameters["strips_back_per_layer"] = 62 # n strips correspond to 5 cm wide bars
+hcalDigis.parameters["strips_side_lr_per_layer"] = 6
+hcalDigis.parameters["num_side_lr_hcal_layers"] = 31
+hcalDigis.parameters["strips_side_tb_per_layer"] = 6
+hcalDigis.parameters["num_side_tb_hcal_layers"] = 31
+hcalDigis.parameters["strips_back_per_layer"] = 60 # n strips correspond to 5 cm wide bars
 hcalDigis.parameters["num_back_hcal_layers"] = 40
 hcalDigis.parameters["super_strip_size"] = 1 # 1 = 5 cm readout, 2 = 10 cm readout, ...
 hcalDigis.parameters["mev_per_mip"] = 1.4*20./6.
