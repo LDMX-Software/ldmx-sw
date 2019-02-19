67f229e5cd67411c550d043cc2837093b2c990d2
omar-moreno <email@omarmoreno.net>
2019-02-19 13:59:07 -0800

    Wrap functions in Analysis namespace.

--- DQM/src/AnalysisUtils.cxx
+++ DQM/src/AnalysisUtils.cxx
@@ -23,33 +23,37 @@
 
 namespace ldmx {
 
+    namespace Analysis {
 
-    const SimParticle* searchForRecoil(const TClonesArray* particles, const int index) { 
-        
-        // Check that the index is within the bounds of the array. If not, 
-        // throw an exception.
-        if (index == particles->GetEntriesFast()) 
-            throw std::out_of_range("Index is beyond the size of the TClonesArray."); 
+        const SimParticle* searchForRecoil(const TClonesArray* particles, const int index) { 
 
-        const SimParticle* particle = static_cast<const SimParticle*>(particles->At(index));
+            // Check that the index is within the bounds of the array. If not, 
+            // throw an exception.
+            if (index == particles->GetEntriesFast()) 
+                throw std::out_of_range("Index is beyond the size of the TClonesArray."); 
 
-        if ((particle->getPdgID() == 11) && (particle->getGenStatus() == 1)) return particle;
-        
-        return searchForRecoil(particles, index + 1); 
-    }
+            const SimParticle* particle = static_cast<const SimParticle*>(particles->At(index));
 
-    const SimParticle* searchForPNGamma(const SimParticle* particle, const int index) { 
-        
-        // Check that the index is within the bounds of the array. If not, 
-        // throw an exception.
-        if (index == particle->getDaughterCount()) 
-            throw std::out_of_range("Index is beyond the size of the TClonesArray.");
+            if ((particle->getPdgID() == 11) && (particle->getGenStatus() == 1)) return particle;
 
-        const SimParticle* daughter = particle->getDaughter(index);
-        if ((daughter->getDaughterCount() > 0) 
-                && (daughter->getDaughter(0)->getProcessType() 
+            return searchForRecoil(particles, index + 1); 
+        }
+
+        const SimParticle* searchForPNGamma(const SimParticle* particle, const int index) { 
+
+            // Check that the index is within the bounds of the array. If not, 
+            // throw an exception.
+            if (index == particle->getDaughterCount()) 
+                throw std::out_of_range("Index is beyond the size of the TClonesArray.");
+
+            const SimParticle* daughter = particle->getDaughter(index);
+            if ((daughter->getDaughterCount() > 0) 
+                    && (daughter->getDaughter(0)->getProcessType() 
                         == SimParticle::ProcessType::photonNuclear)) return daughter;
 
-        return searchForPNGamma(particle, index + 1);  
-    }
-}
+            return searchForPNGamma(particle, index + 1);  
+        }
+
+    } // Analysis    
+
+} // ldmx
