e4be3317dc6df82e88472634a326a8759d99ef28
omar-moreno <email@omarmoreno.net>
2019-02-19 13:59:07 -0800

    Wrap functions in Analysis namespace.

--- DQM/include/DQM/AnalysisUtils.h
+++ DQM/include/DQM/AnalysisUtils.h
@@ -15,44 +15,48 @@
 // Forward declaration
 class TClonesArray;  
 
-namespace ldmx { 
+namespace ldmx {
 
     // Forward declaration for classes inside ldmx namespace
     class SimParticle;
 
-    /**
-     * Find and return the the sim particle associated with the recoil 
-     * electron.
-     *
-     * @param particles Collection of sim particles
-     * @param index Position along the TClonesArray to start the search for 
-     *              the recoil.  The default is to start at the 
-     *              beginning of the array. 
-     */
-    const SimParticle* searchForRecoil(const TClonesArray* particles, const int index = 0);
+    namespace Analysis {
 
-    /**
-     * Find and return the sim particle associated with the gamma that 
-     * underwent a photo-nuclear reaction.
-     *
-     * @param particle Sim particle that will be used to retrieve the list of 
-     *                 particles to search through.
-     * @param index Position along the TClonesArray to start the search for the
-     *              photo-nuclear gamma.  The default is to start at the 
-     *              beginning of the array.
-     */
-    const SimParticle* searchForPNGamma(const SimParticle* particle, const int index = 0);  
+        /**
+         * Find and return the the sim particle associated with the recoil 
+         * electron.
+         *
+         * @param particles Collection of sim particles
+         * @param index Position along the TClonesArray to start the search for 
+         *              the recoil.  The default is to start at the 
+         *              beginning of the array. 
+         */
+        const SimParticle* searchForRecoil(const TClonesArray* particles, const int index = 0);
 
-    /**
-     * Calculate the magnitude of a vector.
-     *
-     * @param v C++ std::vector
-     * @return Magnitude of the given vector.
-     */
-    template <class T>
-    double vectorMagnitude(std::vector<T> v) { 
-        return sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]); 
-    } 
+        /**
+         * Find and return the sim particle associated with the gamma that 
+         * underwent a photo-nuclear reaction.
+         *
+         * @param particle Sim particle that will be used to retrieve the list of 
+         *                 particles to search through.
+         * @param index Position along the TClonesArray to start the search for the
+         *              photo-nuclear gamma.  The default is to start at the 
+         *              beginning of the array.
+         */
+        const SimParticle* searchForPNGamma(const SimParticle* particle, const int index = 0);  
+
+        /**
+         * Calculate the magnitude of a vector.
+         *
+         * @param v C++ std::vector
+         * @return Magnitude of the given vector.
+         */
+        template <class T>
+        double vectorMagnitude(std::vector<T> v) { 
+                return sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]); 
+        }
+
+    } // Analysis
 
 } // ldmx
 
