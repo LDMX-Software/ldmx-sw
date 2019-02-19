d078c7d084f167bcad392b6c9a16a6581a77a3a4
omar-moreno <email@omarmoreno.net>
2019-02-19 13:52:45 -0800

    Add method to search for a particle that underwent a photo-nuclear reaction.  Add method to calculate the magnitude of a vector.

--- DQM/include/DQM/AnalysisUtils.h
+++ DQM/include/DQM/AnalysisUtils.h
@@ -7,6 +7,10 @@
 #ifndef _ANALYSIS_UTILS_H_
 #define _ANALYSIS_UTILS_H_
 
+//----------------//
+//   C++ StdLib   //
+//----------------//
+#include <vector>
 
 // Forward declaration
 class TClonesArray;  
@@ -22,10 +26,33 @@ namespace ldmx {
      *
      * @param particles Collection of sim particles
      * @param index Position along the TClonesArray to start the search for 
-     *              the recoil.  The default is to search start at the 
+     *              the recoil.  The default is to start at the 
      *              beginning of the array. 
      */
-    const SimParticle* searchForRecoil(const TClonesArray* particles, const int index = 0); 
+    const SimParticle* searchForRecoil(const TClonesArray* particles, const int index = 0);
+
+    /**
+     * Find and return the sim particle associated with the gamma that 
+     * underwent a photo-nuclear reaction.
+     *
+     * @param particle Sim particle that will be used to retrieve the list of 
+     *                 particles to search through.
+     * @param index Position along the TClonesArray to start the search for the
+     *              photo-nuclear gamma.  The default is to start at the 
+     *              beginning of the array.
+     */
+    const SimParticle* searchForPNGamma(const SimParticle* particle, const int index = 0);  
+
+    /**
+     * Calculate the magnitude of a vector.
+     *
+     * @param v C++ std::vector
+     * @return Magnitude of the given vector.
+     */
+    template <class T>
+    double vectorMagnitude(std::vector<T> v) { 
+        return sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]); 
+    } 
 
 } // ldmx
 
