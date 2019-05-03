d211fbffa408fc58ac43f8092f9102192eef6493
omar-moreno <email@omarmoreno.net>
2019-05-02 23:30:55 -0700

    Update the HCal veto config.

--- EventProc/python/hcalVeto.py
+++ EventProc/python/hcalVeto.py
@@ -2,5 +2,5 @@
 from LDMX.Framework import ldmxcfg 
 
 hcalVeto = ldmxcfg.Producer("HcalVeto", "ldmx::HcalVetoProcessor")
-hcalVeto.parameters['pe_threshold'] = 3.0
+hcalVeto.parameters['pe_threshold'] = 5.0
 hcalVeto.parameters['max_time'] = 50.0
