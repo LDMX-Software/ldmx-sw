ebb0d50823211d00caaeae65bb830d5d70e67fe8
omar-moreno <email@omarmoreno.net>
2019-05-24 13:05:39 -0700

    Update hcalVeto config.

--- EventProc/python/hcalVeto.py
+++ EventProc/python/hcalVeto.py
@@ -4,3 +4,4 @@ from LDMX.Framework import ldmxcfg
 hcalVeto = ldmxcfg.Producer("HcalVeto", "ldmx::HcalVetoProcessor")
 hcalVeto.parameters['pe_threshold'] = 5.0
 hcalVeto.parameters['max_time'] = 50.0
+hcalVeto.parameters['max_depth'] = 4000.0
