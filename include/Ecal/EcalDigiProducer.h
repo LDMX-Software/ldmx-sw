a50ae513c54aeccab6d4914700a19cc358a68ef9
tomeichlersmith <eichl008@umn.edu>
2019-11-07 10:27:52 -0800

    Compiling and running digi producer in Ecal module
    Renaming and commenting documentation
    
    compiling but segfault on execution of rec producer

--- include/Ecal/EcalDigiProducer.h
+++ include/Ecal/EcalDigiProducer.h
@@ -4,6 +4,7 @@
  * @author Owen Colegrove, UCSB
  * @author Omar Moreno, SLAC National Accelerator Laboratory
  * @author Cameron Bravo, SLAC National Accelerator Laboratory
+ * @author Tom Eichlersmith, University of Minnesota
  */
 
 #ifndef EVENTPROC_ECALDIGIPRODUCER_H_
@@ -43,16 +44,28 @@ namespace ldmx {
 
         public:
 
-            typedef std::pair<int, int> layer_cell_pair;
-
-            typedef std::pair<int, float> cell_energy_pair;
-
+            /**
+             * Constructor
+             * Makes unique noise generator and injector for this class
+             */
             EcalDigiProducer(const std::string& name, Process& process);
 
+            /**
+             * Destructor
+             * Deletes digi collection if it has been created
+             */
             virtual ~EcalDigiProducer();
 
+            /**
+             * Configure this producer from the python configuration.
+             * Sets event constants and configures the noise generator, noise injector, and pulse function.
+             * Creates digi collection
+             */
             virtual void configure(const ParameterSet&);
 
+            /**
+             * Simulates measurement of pulse and creates digi collection for input event.
+             */
             virtual void produce(Event& event);
 
         private:
@@ -67,66 +80,28 @@ namespace ldmx {
                 return noiseIntercept + noiseSlope*capacitance;
             } 
 
-            inline layer_cell_pair hitToPair(SimCalorimeterHit* hit) {
-                int detIDraw = hit->getID();
-                detID_.setRawValue(detIDraw);
-                detID_.unpack();
-                int layer = detID_.getFieldValue("layer");
-                int cellid = detID_.getFieldValue("cell");
-                return (std::make_pair(layer, cellid));
-            }
-
-            //evaluate the pulse given the input peakTime and amplitude parameters
-            //  the shape parameters were (and in the future should be) determined from a fit of the pulse shape
-            double evalPulse(const double evalTime, const double peakTime , const double amplitude ) const {
-                double f=amplitude/(1.0+exp(-0.345*(evalTime-70.6547+77.732-peakTime)))/(1.0+exp(0.140068*(evalTime-87.7649+77.732-peakTime)));
-            }
-
-            TF1 pulseFunc;
-
-        private:
-
+            //Universal Constants
+            //  Won't be changed by python configuration
+            
             /** Electrons per MIP. */
             static const double ELECTRONS_PER_MIP; 
 
             /** MIP response in MeV. */
             static const double MIP_SI_RESPONSE;
 
-            /** MIP response in MeV. */
+            /** Time interval for chip clock */
             static const double CLOCK_CYCLE;
 
-            /** Total number of Ecal layers. */
-            static const int NUM_ECAL_LAYERS{34};
-
-            /** Total number of hex modules per layer. */
-            static const int HEX_MODULES_PER_LAYER{7}; 
-
-            /** Total number of cells (channels) per hex module. */
-            static const int CELLS_PER_HEX_MODULE{397};
-
-            /** Total number of cells across all modules. */
-            static const int TOTAL_CELLS{NUM_ECAL_LAYERS*HEX_MODULES_PER_LAYER*CELLS_PER_HEX_MODULE};
-
-
-            std::unique_ptr<TRandom3> noiseInjector_;
-            TClonesArray* ecalDigis_{nullptr};
-            EcalDetectorID detID_;
-            std::unique_ptr<EcalHexReadout> hexReadout_;
-
-            /** Generator of noise hits. */ 
-            std::unique_ptr<NoiseGenerator> noiseGenerator_; 
-
-            /** The pedestal in ADC units. */
-            double pedestal_{1100.};
-
+            //Python Configuration Parameters
+            
             /** The gain in ADC units per MeV. */
             double gain_{2000.};
 
+            /** The pedestal in ADC units */
+            double pedestal_{1100.};
+
             /** Set the noise (in electrons) when the capacitance is 0. */
             double noiseIntercept_{900.};
-
-            /** Noise RMS in units of electrons. */
-            double noiseRMS_{0}; 
 
             /** Set the capacitative noise slope (electrons/pF). */
             double noiseSlope_{22.}; 
@@ -135,13 +110,50 @@ namespace ldmx {
             double padCapacitance_{27.56}; // pF 
 
             /** Depth of ADC buffer. */
-            int nADCs_{10}; // pF 
+            int nADCs_{10}; 
+
+            //Member Variables that are used for each event
 
             /** 
-             * Set the threshold for reading out a channel. Units are 
-             * multiples of RMS noise. 
+             * Noise RMS in units of electrons. 
+             * Calculated using the python config parameters and calculateNoise
+             */
+            double noiseRMS_{0}; 
+
+            /** 
+             * Set the threshold for reading out a channel.
+             * Units are multiples of RMS noise for python config
+             * Set to value of input config parameter times noiseRMS_
              */
             double readoutThreshold_{4.};
+
+            /**
+             * Helper Object that translates between realspace and cell/module IDs in the hexagonal layout
+             */
+            std::unique_ptr<EcalHexReadout> hexReadout_;
+
+            /**
+             * Generates noise hits based off of number of cells that are not hit
+             */
+            std::unique_ptr<NoiseGenerator> noiseGenerator_;
+
+            /**
+             * Generates Gaussian noise on top of real hits
+             */
+            std::unique_ptr<TRandom3> noiseInjector_;
+
+            /**
+             * Collection that will be put into event bus
+             */
+            TClonesArray *ecalDigis_;
+
+            /**
+             * Functional shape of signal pulse in time
+             * Shape parameters are hardcoded into the function currently.
+             * par[0] = time pulse reaches its peak
+             * par[1] = amplitude of pulse at its peak
+             */
+            TF1 pulseFunc_;
     };
 }
 
