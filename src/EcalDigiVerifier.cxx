c0b9ccbe5a88055c3a49aadd59c579c727ee02a4
tomeichlersmith <eichl008@umn.edu>
2019-11-28 11:42:15 -0600

    Python configurable collection and pass names for verification analyzer
    compiling verification analyzer with sorting and histogram filling

--- src/EcalDigiVerifier.cxx
+++ src/EcalDigiVerifier.cxx
@@ -10,12 +10,54 @@ namespace ldmx {
 
     void EcalDigiVerifier::configure(const ldmx::ParameterSet& ps) {
 
-
+        ecalSimHitColl_ = ps.getString( "ecalSimHitColl" , "EcalSimHits" );
+        ecalSimHitPass_ = ps.getString( "ecalSimHitPass" , "sim" );
+        ecalRecHitColl_ = ps.getString( "ecalRecHitColl" , "EcalRecHits" );
+        ecalRecHitPass_ = ps.getString( "ecalSimHitPass" , "" );
 
         return;
     }
 
     void EcalDigiVerifier::analyze(const ldmx::Event& event) {
+
+        //get truth information sorted into an ID based map
+        const TClonesArray *ecalSimHits = event.getCollection( ecalSimHitColl_ , ecalSimHitPass_ );
+        int numSimHits = ecalSimHits->GetEntriesFast();
+        std::map< int , std::vector< SimCalorimeterHit *> > id_simHit;
+        for ( int iHit = 0; iHit < numSimHits; iHit++ ) {
+            
+            SimCalorimeterHit *simHit = (SimCalorimeterHit *)(ecalSimHits->At( iHit ));
+
+            int  rawID = simHit->getID();
+            id_simHit[ rawID ].push_back( simHit );
+        }
+
+        const TClonesArray *ecalRecHits = event.getCollection( ecalRecHitColl_ , ecalRecHitPass_ );
+        int numRecHits = ecalRecHits->GetEntriesFast();
+        double totalRecEnergy = 0.;
+        for ( int iRecHit = 0; iRecHit < numRecHits; iRecHit++ ) {
+
+            EcalHit *recHit = (EcalHit *)(ecalRecHits->At( iRecHit ));
+
+            //skip anything that digi flagged as noise
+            if ( recHit->isNoise() ) continue;
+
+            int rawID = recHit->getID();
+
+            int numSimHits = id_simHit[rawID].size();
+            h_NumSimHitsPerCell_->Fill( numSimHits );
+
+            double totalSimEDep = 0.;
+            for ( int iSimHit = 0; iSimHit < numSimHits; iSimHit++ ) {
+                totalSimEDep += id_simHit[rawID][iSimHit]->getEdep();
+            }
+
+            h_SimEDep_RecAmplitude_->Fill( totalSimEDep , recHit->getAmplitude() );
+
+            totalRecEnergy += recHit->getEnergy();
+        }
+
+        h_TotalRecEnergy_->Fill( totalRecEnergy );
 
         return;
     }
@@ -39,7 +81,7 @@ namespace ldmx {
 
         h_NumSimHitsPerCell_ = new TH1F(
                 "h_NumSimHitsPerCell_",
-                ";Number SimHits per ECal Cell (excluding empty cells);Count",
+                ";Number SimHits per ECal Cell (excluding empty rec cells);Count",
                 20,0,20
                 );
 
