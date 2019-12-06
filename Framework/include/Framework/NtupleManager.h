604a62abb059e8b2b55824a767edf7c9525ee081
omar-moreno <email@omarmoreno.net>
2019-12-05 17:06:41 -0800

    More documentation.

--- Framework/include/Framework/NtupleManager.h
+++ Framework/include/Framework/NtupleManager.h
@@ -2,6 +2,9 @@
 #ifndef _NTUPLE_MANAGER_H_
 #define _NTUPLE_MANAGER_H_
 
+/*~~~~~~~~~~~~*/
+/*   StdLib   */
+/*~~~~~~~~~~~~*/
 #include <limits> 
 #include <iostream> 
 #include <map> 
@@ -9,12 +12,21 @@
 #include <typeinfo>
 #include <unordered_map>
 
+/*~~~~~~~~~~~*/
+/*   Boost   */
+/*~~~~~~~~~~~*/
 #include "boost/assign/list_of.hpp"
 #include "boost/variant.hpp"
 #include "boost/unordered_map.hpp"
 
+/*~~~~~~~~~~*/
+/*   ROOT   */
+/*~~~~~~~~~~*/
 #include "TTree.h" 
 
+/*~~~~~~~~~~*/
+/*   Core   */
+/*~~~~~~~~~~*/
 #include "Exception/Exception.h" 
 
 typedef boost::variant< short, int, float, double, long > vtype; 
@@ -34,6 +46,7 @@ namespace ldmx {
 
         public:
 
+            /// @return The NtupleManager instance 
             static NtupleManager* getInstance(); 
 
             /**
@@ -44,7 +57,13 @@ namespace ldmx {
             void create(const std::string& tname);
 
             /**
+             *  Add a variable of type T to the ROOT tree with name 'tname'.  
+             *  If the variable already exists in any tree or the requested 
+             *  tree does not exists, an exception is thrown.
              *
+             *  @param tname Name of the tree to add the variable to.
+             *  @param vname Name of the variable to add to the tree 
+             *  @throws exception
              */
             template <typename T>
             void addVar(const std::string& tname, const std::string& vname) {
@@ -56,7 +75,15 @@ namespace ldmx {
                             "NtupleManager", 
                             "A variable with name " + vname + " has already been defined."); 
                 } 
-               
+        
+                // Check if a tree named 'tname' has already been created.  If
+                // not, throw an exception.
+                if (trees_.count(tname) == 0) 
+                    EXCEPTION_RAISE(
+                        "NtupleManager", 
+                        "A tree with name " + tname + " has already been created." 
+                    ); 
+              
                 // Initialize the variable to the minimum limit of that type. 
                 variables_[vname] = T(std::numeric_limits<T>::min()); 
 
@@ -69,10 +96,18 @@ namespace ldmx {
             } 
 
             /**
+             *  Set the value of the variable named 'vname'.  If the variable
+             *  value is not set, the default value will be used when filling
+             *  the tree.  If the requested variable has not been created, 
+             *  a warning will be printed.  This allows a user to choose to 
+             *  make a subset of an ntuple by simply not adding the variable 
+             *  to the tree.
              *
+             *  @param vname Name of the variable 
+             *  @param value The value of the variable 
              */
             template <typename T>
-            void setVar(const std::string& vname, T value) {
+            void setVar(const std::string& vname, const T value) {
                 
                 // Check if the variable already exists in the map.  If it
                 // doesn't, warn the user and don't try to set the variable 
@@ -87,14 +122,10 @@ namespace ldmx {
                 variables_[vname] = value;      
             }
 
-            /**
-             *
-             */
+            // Fill all of the ROOT trees.
             void fill();
 
-            /**
-             *
-             */
+            /// Reset all of the variables to their limits.
             void clear(); 
 
         private: 
