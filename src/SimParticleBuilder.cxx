44a90ac9ac9e7f1f99b7630b9d496a5dab7cb86f
tomeichlersmith <eichl008@umn.edu>
2019-12-13 16:08:11 -0600

    Move collection of SimParticles to map
    EcalClusterProducer translated - not run
    
    EcalDigiProducer translated - UNRUN
    
    EcalVetoProcessor translation - UNRUN
    
    SEG VIO on run - first attempt to translate ldmx-sim to vector
    
    Remove TRef from SimTrackerHit, SimCalorimeterHit, and SimParticle
    
    COMPILES BUT DOES NOT RUN
    
    Transition to map of SimParticles in event bus
    
    Compiles and runs effectively! Still lots of checks and improvements to
    go though.
    
    Adding time, energy, and PDG information to SimTrackerHit
    
    fixup! Adding time, energy, and PDG information to SimTrackerHit
    
    Removed un-used SimParticle::findSimParticle

--- src/SimParticleBuilder.cxx
+++ src/SimParticleBuilder.cxx
@@ -22,90 +22,62 @@ namespace ldmx {
     SimParticleBuilder::SimParticleBuilder() :
             currentEvent_(nullptr) {
         trackMap_ = UserTrackingAction::getUserTrackingAction()->getTrackMap();
-        outputParticleColl_ = new TClonesArray(EventConstants::SIM_PARTICLE.c_str(), 50);
     }
 
-    SimParticleBuilder::~SimParticleBuilder() {
-        delete outputParticleColl_;
-    }
+    SimParticleBuilder::~SimParticleBuilder() { }
 
     void SimParticleBuilder::buildSimParticles(ldmx::Event* outputEvent) {
-
-        // Clear the output particle collection.
-        outputParticleColl_->Clear("C");
 
         // Get the trajectory container for the event.
         TrajectoryContainer* trajectories = (TrajectoryContainer*) (const_cast<G4Event*>(currentEvent_))->GetTrajectoryContainer();
 
         // Create empty SimParticle objects and create the map of track ID to particles.
-        buildParticleMap(trajectories, outputParticleColl_);
+        std::map< int , SimParticle > outputParticleMap;
+        for (auto trajectory : *trajectories->GetVector()) {
+            outputParticleMap[trajectory->GetTrackID()] = SimParticle();
+        }
 
         // Fill information into the particles.
         for (auto trajectory : *trajectories->GetVector()) {
-            buildSimParticle(static_cast<Trajectory*>(trajectory));
+            
+            Trajectory *traj = static_cast<Trajectory *>(trajectory);
+            
+            SimParticle* simParticle = &outputParticleMap[traj->GetTrackID()];
+    
+            simParticle->setGenStatus(traj->getGenStatus());
+            simParticle->setTrackID(traj->GetTrackID());
+            simParticle->setPdgID(traj->GetPDGEncoding());
+            simParticle->setCharge(traj->GetCharge());
+            simParticle->setMass(traj->getMass());
+            simParticle->setEnergy(traj->getEnergy());
+            simParticle->setTime(traj->getGlobalTime());
+            simParticle->setProcessType(traj->getProcessType());
+    
+            const G4ThreeVector& vertex = traj->getVertexPosition();
+            simParticle->setVertex(vertex[0], vertex[1], vertex[2]);
+    
+            const G4ThreeVector& momentum = traj->GetInitialMomentum();
+            simParticle->setMomentum(momentum[0], momentum[1], momentum[2]);
+    
+            const G4ThreeVector& endpMomentum = traj->getEndPointMomentum();
+            simParticle->setEndPointMomentum(endpMomentum[0], endpMomentum[1], endpMomentum[2]);
+    
+            G4ThreeVector endpoint = traj->getEndPoint();
+            simParticle->setEndPoint(endpoint[0], endpoint[1], endpoint[2]);
+    
+            if (traj->GetParentID() > 0) {
+                simParticle->addParent( traj->GetParentID() );
+                //check if parent track is being stored
+                auto parentParticle = outputParticleMap.find( traj->GetParentID() );
+                if ( parentParticle != outputParticleMap.end() ) {
+                    //this parent has been found in the particle map
+                    outputParticleMap[ traj->GetParentID() ].addDaughter( simParticle->getTrackID() );
+                }//check if parent exists in output map
+            }//check if particle has a parent
         }
 
         // Add the collection data to the output event.
-        outputEvent->add("SimParticles", outputParticleColl_);
-    }
-
-    void SimParticleBuilder::buildSimParticle(Trajectory* traj) {
-
-        SimParticle* simParticle = particleMap_[traj->GetTrackID()];
-
-        if (!simParticle) {
-            EXCEPTION_RAISE( "MissingInfo" , "SimParticle not found for Trajectory with track ID '" 
-                    + std::to_string(traj->GetTrackID()) + "'." );
-        }
-
-        simParticle->setGenStatus(traj->getGenStatus());
-        simParticle->setTrackID(traj->GetTrackID());
-        simParticle->setPdgID(traj->GetPDGEncoding());
-        simParticle->setCharge(traj->GetCharge());
-        simParticle->setMass(traj->getMass());
-        simParticle->setEnergy(traj->getEnergy());
-        simParticle->setTime(traj->getGlobalTime());
-        simParticle->setProcessType(traj->getProcessType());
-
-        const G4ThreeVector& vertex = traj->getVertexPosition();
-        simParticle->setVertex(vertex[0], vertex[1], vertex[2]);
-
-        const G4ThreeVector& momentum = traj->GetInitialMomentum();
-        simParticle->setMomentum(momentum[0], momentum[1], momentum[2]);
-
-        const G4ThreeVector& endpMomentum = traj->getEndPointMomentum();
-        simParticle->setEndPointMomentum(endpMomentum[0], endpMomentum[1], endpMomentum[2]);
-
-        G4ThreeVector endpoint = traj->getEndPoint();
-        simParticle->setEndPoint(endpoint[0], endpoint[1], endpoint[2]);
-
-        if (traj->GetParentID() > 0) {
-            SimParticle* parent = findSimParticle(traj->GetParentID());
-            if (parent != nullptr) {
-                simParticle->addParent(parent);
-                parent->addDaughter(simParticle);
-            } else {
-                // If the parent particle can not be found by its track ID, this is a fatal error!
-                EXCEPTION_RAISE( "MissingInfo" , "SimParticle with parent ID '" + std::to_string(traj->GetParentID()) 
-                        + "' not found for track ID '" + std::to_string(traj->GetTrackID()) + "'." );
-            }
-        }
-    }
-
-    void SimParticleBuilder::buildParticleMap(TrajectoryContainer* trajectories, TClonesArray* simParticleColl) {
-        particleMap_.clear();
-        for (auto trajectory : *trajectories->GetVector()) {
-            particleMap_[trajectory->GetTrackID()] = (SimParticle*) simParticleColl->ConstructedAt(simParticleColl->GetEntries());
-        }
-    }
-
-    SimParticle* SimParticleBuilder::findSimParticle(G4int trackID) {
-        G4VTrajectory* traj = trackMap_->findTrajectory(trackID);
-        if (traj != nullptr) {
-            return particleMap_[traj->GetTrackID()];
-        } else {
-            return nullptr;
-        }
+        outputEvent->add("SimParticles", outputParticleMap );
     }
 
 }
