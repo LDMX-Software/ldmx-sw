fd6bd21976288287d73964fdaffb32188926fa32
omar-moreno <email@omarmoreno.net>
2019-12-16 15:08:19 -0800

    Comment out persistency.  Use smart pointers and rearrange where some classes are called.

--- SimApplication/src/Simulator.cxx
+++ SimApplication/src/Simulator.cxx
@@ -2,44 +2,48 @@
  * @file Simulator.cxx
  * @brief Producer that runs Geant4 simulation inside of ldmx-app
  * @author Tom Eichlersmith, University of Minnesota
+ * @author Omar Moreno, SLAC National Accelerator Laboratory
  */
 
 #include "SimApplication/Simulator.h"
+
+#include "SimApplication/DetectorConstruction.h"
+#include "SimApplication/RunManager.h"
+
+#include "G4CascadeParameters.hh"
+#include "G4GDMLParser.hh"
+#include "G4GDMLMessenger.hh"
 
 namespace ldmx {
 
     Simulator::Simulator(const std::string& name, ldmx::Process& process) : Producer( name , process ) {
 
-        runManager_ = new RunManager();
+        runManager_ = std::make_unique<RunManager>();
 
-        parser_ = new G4GDMLParser();
-        gdmlMessenger_ = new G4GDMLMessenger(parser_);
-
-        new SimApplicationMessenger();
+        // Instantiate the GDML parser and corresponding messenger
+        parser_ = std::make_unique<G4GDMLParser>();
+        gdmlMessenger_ = std::make_unique<G4GDMLMessenger>(parser_.get()); 
 
         // Instantiate the class so cascade parameters can be set.
         G4CascadeParameters::Instance();  
 
-        runManager_->SetUserInitialization(new DetectorConstruction( parser_ ) );
+        runManager_->SetUserInitialization(new DetectorConstruction( parser_.get() ) );
+
         runManager_->SetRandomNumberStore( true );
-
-        uiManager_ = G4UImanager::GetUIpointer();
-
+        
+        /*
+        new SimApplicationMessenger();
         persistencyManager_ = new RootPersistencyManager();
         persistencyManager_->SetVerboseLevel( 3 );
+        */
     }
 
     Simulator::~Simulator() {
-
-        if ( runManager_ ) delete runManager_;
-        if ( parser_     ) delete parser_;
-        if ( persistencyManager_ ) delete persistencyManager_;
-
-        //Producer destructor called automatically
     }
 
 
     void Simulator::configure(const ldmx::ParameterSet& ps) {
+       
         return;
     }
 
@@ -47,39 +51,25 @@ namespace ldmx {
 
         runManager_->ProcessOneEvent( iEvent_++ );
         runManager_->GetCurrentEvent()->Print();
-//        persistencyManager_->buildEvent( runManager_->GetCurrentEvent() , &event );
-        persistencyManager_->writeHitsCollections( runManager_->GetCurrentEvent() , &event );
         runManager_->TerminateOneEvent();
-
+    
+        /*
+        persistencyManager_->buildEvent( runManager_->GetCurrentEvent() , &event );
+        persistencyManager_->writeHitsCollections( runManager_->GetCurrentEvent() , &event );
+        */
         return;
     }
     
     void Simulator::onProcessStart() {
-
-        iEvent_ = 0;
-
-        //read in detector from gdml
+        
+        // Parser the detector
+        // TODO: This should be configurable via a parameter. 
         uiManager_->ApplyCommand( "/persistency/gdml/read detector.gdml" );
-        //biasing would happen here
-
         uiManager_->ApplyCommand( "/run/initialize" );
 
-        //load plugins here
-
-        //setup basic particle gun
-        uiManager_->ApplyCommand("/gun/particle e-");
-        uiManager_->ApplyCommand("/gun/energy 4.0 GeV");
-        uiManager_->ApplyCommand("/gun/position 0 0 -.55 mm");
-        uiManager_->ApplyCommand("/gun/direction 0 0 4 GeV");
-
-        //turn off writing to output file
-
-        runManager_->ConstructScoringWorlds();
+        //runManager_->ConstructScoringWorlds();
         runManager_->RunInitialization();
         runManager_->InitializeEventLoop( 1 );
-        runManager_->Initialize();
-
-        persistencyManager_->setupHitsCollectionMap();
 
         return;
     }
@@ -88,10 +78,9 @@ namespace ldmx {
 
         runManager_->TerminateEventLoop();
         runManager_->RunTermination();
-
         return;
     }
 
 }
 
-DECLARE_PRODUCER_NS(ldmx, Simulator);
+DECLARE_PRODUCER_NS(ldmx, Simulator)
