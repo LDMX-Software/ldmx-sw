01169686da69ed1a6f6f07c09f746f1ade4bd96d
tomeichlersmith <eichl008@umn.edu>
2019-12-22 09:40:53 -0800

    Remove dependence on getDaughter(int) from SimParticle
    cleanup and exceptions for searchProducts regular expression inputs
    
    cleanup and renaming for clarity
    
    remove executable permission

--- Framework/include/Framework/Event.h
+++ Framework/include/Framework/Event.h
@@ -29,6 +29,7 @@
 namespace ldmx {
 
     /**
+     * @class clearPassenger
      * Clearing of event objects.
      *
      * This is necessary, so if a producer skips an event, then
@@ -38,13 +39,13 @@ namespace ldmx {
         public:
             
             /**
-             * All vector collections can be cleared in the same way.
+             * All vector passengers can be cleared in the same way.
              */
             template <typename T>
             void operator()(std::vector<T> &vec) const { vec.clear(); }
 
             /**
-             * All map collections can be cleared in the same way.
+             * All map passengers can be cleared in the same way.
              */
             template <typename Key, typename Val>
             void operator()(std::map<Key,Val> &m) const { m.clear(); }
@@ -59,6 +60,7 @@ namespace ldmx {
 
 
     /**
+     * @class printPassenger
      * Printing of event objects.
      *
      * This method requires all event objects to have a Print method defined.
@@ -72,7 +74,6 @@ namespace ldmx {
              * Sets verbosity
              */
             printPassenger(int verbosity) : verbosity_(verbosity) { }
-            printPassenger() : verbosity_(0) { }
 
             /**
              * Prints size and contents of all vectors depending on verbosity.
@@ -158,7 +159,7 @@ namespace ldmx {
             /**
              * Print event bus
              *
-             * Only prints collections that have been loaded into the member object.
+             * Only prints passengers that have been loaded into the member object.
              * This means what is printed depends on when this method is called.
              * For example, if this method is called after the Clear method, then
              * everything will be an empty object.
@@ -199,7 +200,9 @@ namespace ldmx {
                 if (collectionName.find('_') != std::string::npos) {
                     EXCEPTION_RAISE(
                             "IllegalName", 
-                            "The product name '" + collectionName + "' is illegal as it contains an underscore.");
+                            "The product name '" 
+                            + collectionName 
+                            + "' is illegal as it contains an underscore.");
                 }
         
                 std::string branchName;
@@ -209,16 +212,16 @@ namespace ldmx {
                 if (branchesFilled_.find(branchName) != branchesFilled_.end()) {
                     EXCEPTION_RAISE(
                             "ProductExists", 
-                            "A product named '" + collectionName + 
-                            "' already exists in the event (has been loaded by a previous producer in this process.");
+                            "A product named '" 
+                            + collectionName 
+                            + "' already exists in the event (has been loaded by a previous producer in this process.");
                 }
                 branchesFilled_.insert(branchName);
-                auto itCollection = collections_.find(branchName);
-                if (itCollection == collections_.end()) { 
+                if (passengers_.find(branchName) == passengers_.end()) { 
                     // create a new branch for this collection
-                    collections_[branchName] = EventBusPassenger( obj );
-                    T *passengerAddress = boost::get<T>(&collections_[branchName]);
-                    std::string tname = collections_[branchName].type().name();//type name (want to use branch element if possible)
+                    passengers_[branchName] = EventBusPassenger( obj );
+                    T *passengerAddress = boost::get<T>(&passengers_[branchName]);
+                    std::string tname = passengers_[branchName].type().name();//type name (want to use branch element if possible)
                     if (outputTree_ != 0) {
                         TBranch *outBranch = outputTree_->GetBranch( branchName.c_str() );
                         if ( outBranch ) {
@@ -240,13 +243,16 @@ namespace ldmx {
                 
                 //copy input contents into bus passenger
                 EventBusPassenger toAdd( obj );
-                if ( toAdd.which() == collections_[branchName].which() ) {
-                    collections_[branchName] = toAdd;
+                if ( toAdd.which() == passengers_[branchName].which() ) {
+                    passengers_[branchName] = toAdd;
                 } else {
                     EXCEPTION_RAISE(
                             "TypeMismatch",
-                            "Attempting to add an object whose type '" + std::string(toAdd.type().name()) + "' doesn't match the type stored in the collection '" +
-                            std::string(collections_[branchName].type().name()) + "'"
+                            "Attempting to add an object whose type '" 
+                            + std::string(toAdd.type().name()) 
+                            + "' doesn't match the type stored in the collection '" 
+                            + std::string(passengers_[branchName].type().name()) 
+                            + "'"
                             );
                 }
 
@@ -317,7 +323,6 @@ namespace ldmx {
              * Get an event passenger from the event bus (actual implementation)
              * @param collectionName name of collection you want
              * @param passName name of pass you want
-             * @param mustExist flag to say whether or not you require this collection to exist in the tree
              */
             template <typename T> 
             T getImpl(const std::string& collectionName, const std::string& passName) const {
@@ -342,7 +347,9 @@ namespace ldmx {
                             //no matches found
                             EXCEPTION_RAISE(
                                     "ProductNotFound",
-                                    "No product found for name '"+collectionName+"'");
+                                    "No product found for name '"
+                                    + collectionName
+                                    + "'");
                         } else if (matches.size()>1) {
                             //more than one branch found
                             std::string names;
@@ -352,7 +359,12 @@ namespace ldmx {
                             }
                             EXCEPTION_RAISE(
                                     "ProductAmbiguous",
-                                    "Multiple products found for name '"+collectionName+"' without specified pass name ("+names+")");
+                                    "Multiple products found for name '"
+                                    + collectionName
+                                    + "' without specified pass name ("
+                                    + names
+                                    + ")"
+                                    );
                         } else {
                             //exactly one branch found
                             branchName=*matches.front();
@@ -363,16 +375,22 @@ namespace ldmx {
         
                 //get iterators to branch and collection
                 auto itBranch = branches_.find(branchName);
-                auto itObject = collections_.find(branchName);
+                auto itPassenger = passengers_.find(branchName);
         
-                if (itObject != collections_.end()) {
+                if (itPassenger != passengers_.end()) {
                    if (itBranch != branches_.end())
                       itBranch->second->GetEntry(ientry_);
-                   return boost::get<T>(itObject->second);
+                   return boost::get<T>(itPassenger->second);
                 } else if (inputTree_ == 0) {
+                    //not found in loaded branches and there is not inputTree,
+                    // so no hope of finding an unloaded object
                     EXCEPTION_RAISE(
                             "ProductNotFound", 
-                            "No product found for name '" + collectionName + "' and pass '" + passName_ + "'");
+                            "No product found for name '" 
+                            + collectionName 
+                            + "' and pass '" 
+                            + passName_ 
+                            + "'");
                 }
         
                 // find the active branch and update if necessary
@@ -382,7 +400,7 @@ namespace ldmx {
                     if (itBranch->second->GetReadEntry() != ientry_) {
         
                         TBranchElement* tbe = dynamic_cast<TBranchElement*>(itBranch->second);
-                        T *passengerAddress = boost::get<T>( &itObject->second );
+                        T *passengerAddress = boost::get<T>( &itPassenger->second );
                         if (!tbe)
                             itBranch->second->SetAddress( &passengerAddress );
         
@@ -390,21 +408,28 @@ namespace ldmx {
                     }
         
                     // check the objects map
-                    if (itObject != collections_.end())
-                        return boost::get<T>(itObject->second);
+                    if (itPassenger != passengers_.end())
+                        return boost::get<T>(itPassenger->second);
         
-                    // this case is hard to achieve
+                    // this case is hard (impossible?) to achieve
                     EXCEPTION_RAISE(
                             "ProductNotFound", 
-                            "A branch mis-match occurred. I'm not sure how I got here!" );
+                            "A branch mis-match occurred. I'm not sure how I got here!" 
+                            );
                 } else {
         
                     // ok, maybe we've not loaded this yet, look for a branch
                     TBranch* branch = inputTree_->GetBranch(branchName.c_str());
                     if (branch == 0) {
+                        //inputTree doesn't have that branch
                         EXCEPTION_RAISE(
                                 "ProductNotFound", 
-                                "No product found for name '" + collectionName + "' and pass '" + passName_ + "'");
+                                "No product found for name '" 
+                                + collectionName 
+                                + "' and pass '" 
+                                + passName_ 
+                                + "'"
+                                );
                     }
                     // ooh, new branch!
                     //get address of object that will be the event passenger
@@ -422,11 +447,11 @@ namespace ldmx {
                     branch->SetStatus(1); //tell root this branch should be active
                     branch->GetEntry((ientry_<0)?(0):(ientry_)); //load in current entry
         
-                    //insert into collection maps
-                    collections_[branchName] = EventBusPassenger( *passengerAddress );
+                    //insert into maps of loaded branches and passengers
+                    passengers_[branchName] = EventBusPassenger( *passengerAddress );
                     branches_[branchName]    = branch;
         
-                    return boost::get<T>( collections_.at(branchName) );
+                    return boost::get<T>( passengers_.at(branchName) );
                 }
             }
 
@@ -486,12 +511,12 @@ namespace ldmx {
             void beforeFill();
 
             /**
-             * Clear this object's data.
+             * Clear this object's data (including passengers).
              */
             void Clear();
 
             /**
-             * Perform end of event action (clears the owned objects).
+             * Perform end of event action (doesn't do anything right now).
              */
             void onEndOfEvent();
 
@@ -511,7 +536,7 @@ namespace ldmx {
         private:
 
             /**
-             * The event header object (as pointer).
+             * The event header object.
              */
             EventHeader eventHeader_;
 
@@ -546,9 +571,9 @@ namespace ldmx {
             mutable std::map<std::string, TBranch*> branches_;
 
             /**
-             * Map of names to collections.
+             * Map of names to passengers.
              */
-            mutable std::map<std::string, EventBusPassenger > collections_; 
+            mutable std::map<std::string, EventBusPassenger > passengers_; 
 
             /**
              * List of new branches added.
