8d168789f408e0df1893dfec598c10d7737c488a
tomeichlersmith <eichl008@umn.edu>
2020-02-05 11:18:25 -0600

    change to default v9 layer weights

--- Ecal/python/ecalRecon.py
+++ Ecal/python/ecalRecon.py
@@ -11,18 +11,18 @@ ecalRecon.parameters[ "digiPassName" ] = ""
 ### The default layerWeights and secondOrderEnergyCorrection were calculated at least before v3.
 ### The other options are below, uncomment those lines if you wish to use those geometries.
 
-### v9
-### These layer weights work for all ecal designs between v3 and v9
-#ecalRecon.parameters[ "secondOrderEnergyCorrection" ] = 4000./4012.
-#                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+## v9
+## These layer weights work for all ecal designs between v3 and v9
+ecalRecon.parameters[ "secondOrderEnergyCorrection" ] = 4000./4012.
+#                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #   This correction was found by comparing the mean of 1M single 4GeV electron events with 4GeV.
-#
-#ecalRecon.parameters[ "layerWeights" ] = [
-#        1.019, 1.707, 3.381, 5.022, 6.679, 8.060, 8.613, 8.613, 8.613, 8.613, 8.613,
-#        8.613, 8.613, 8.613, 8.613, 8.613, 8.613, 8.613, 8.613, 8.613, 8.613, 8.613,
-#        8.613, 12.480, 16.347, 16.347, 16.347, 16.347, 16.347, 16.347, 16.347, 16.347,
-#        16.347, 8.334
-#        ]
+
+ecalRecon.parameters[ "layerWeights" ] = [
+        1.019, 1.707, 3.381, 5.022, 6.679, 8.060, 8.613, 8.613, 8.613, 8.613, 8.613,
+        8.613, 8.613, 8.613, 8.613, 8.613, 8.613, 8.613, 8.613, 8.613, 8.613, 8.613,
+        8.613, 12.480, 16.347, 16.347, 16.347, 16.347, 16.347, 16.347, 16.347, 16.347,
+        16.347, 8.334
+        ]
 
 ### v12
 ### These layer weights are for the most recent change to the ecal geometry where motherboards and more component space is added
