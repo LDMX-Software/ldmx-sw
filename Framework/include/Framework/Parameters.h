bc5a6f1f3d9a04a2d81987b3687f2898ad9e2723
omar-moreno <email@omarmoreno.net>
2020-02-12 17:40:56 -0800

    Class encapsulating parameters for configuring a processor. Also adds a templated getter for parameter retrieval.

---
+++ Framework/include/Framework/Parameters.h
@@ -0,0 +1,67 @@
+/**
+ * @file Parameters.h
+ * @brief Class encapsulating parameters for configuring a processor.
+ * @author Omar Moreno, SLAC National Accelerator Laboratory
+ */
+
+/*~~~~~~~~~~~~~~~~*/
+/*   C++ StdLib   */
+/*~~~~~~~~~~~~~~~~*/
+#include <any>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <string>
+#include <type_traits>
+#include <vector> 
+
+namespace ldmx {
+
+    /**
+     *
+     */
+    class Parameters {
+        
+        public: 
+
+            /// Constructor
+            Parameters();
+
+            /// Destructor
+            ~Parameters(); 
+
+            /**
+             * Set the mapping of parameter names to value.
+             *
+             * @param mapping between parameter names and the corresponding value.
+             */
+            void setParameters(std::map < std::string, std::any > parameters) { 
+                parameters_ = parameters; 
+            }
+
+            /**
+             *
+             */
+            template <typename T> 
+            T getParameter(const std::string& name) { 
+                
+                // Check if the variable exists in the map.  If it doesn't, 
+                // warn the user and set a default.
+                if (parameters_.count(name) == 0) { 
+                    std::cout << "[ Parameters ]: Parameter " << name
+                              << " hasn't been set. Using default value." << std::endl; 
+               
+                    if constexpr (std::is_integral<T>::value || std::is_floating_point<T>::value) {
+                        return std::numeric_limits<T>::lowest(); 
+                    } else return {};  
+                }
+
+                return std::any_cast< T >(parameters_[name]); 
+            }                
+
+        private:
+
+            /// Parameters 
+            std::map < std::string, std::any > parameters_; 
+    };
+}
