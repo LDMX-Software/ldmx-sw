bff2ba3c27cbebc839252f26c8752b388acacee2
tomeichlersmith <eichl008@umn.edu>
2020-02-14 09:13:16 -0600

    Compiles and runs without any tests specified
    Operational DetectorIDTest

---
+++ Test/src/DetectorIDTest.cxx
@@ -0,0 +1,36 @@
+/**
+ * @file DetectorIDTest.cxx
+ * @brief Test the operation of DetectorID class
+ */
+#include "Test/catch.hpp"
+
+#include "DetDescr/DetectorID.h"
+
+TEST_CASE( "Detector ID Test" , "[function]" ) {
+
+    using ldmx::IDField;
+    using ldmx::DetectorID;
+    
+    IDField::IDFieldList *fieldList = new IDField::IDFieldList();
+    fieldList->push_back(new IDField("subdet", 0, 0, 3));
+    fieldList->push_back(new IDField("layer", 1, 4, 11));
+    DetectorID detID(fieldList); //DetectorID owns fieldList and its members
+
+    int layer = 10;
+    int subdet = 2;
+
+    detID.setFieldValue("layer", layer);
+    detID.setFieldValue("subdet", subdet);
+    detID.pack();
+
+    DetectorID::RawValue rawVal = detID.getRawValue();
+
+    detID.setRawValue(rawVal);
+    const DetectorID::FieldValueList& fieldValues = detID.unpack();
+
+    REQUIRE( fieldValues[0] == subdet );
+    REQUIRE( detID.getFieldValue( "subdet" ) == subdet );
+    REQUIRE( fieldValues[1] == layer  );
+    REQUIRE( detID.getFieldValue( "layer" ) == layer );
+
+}
---
+++ Test/test/ldmx_test.cxx
@@ -0,0 +1,16 @@
+/**
+ * @file catch.cxx
+ * @brief File to implement unit testing program.
+ * @author Tom Eichlersmith, University of Minnesota
+ */
+
+/**
+ * This define statement tells the Catch2 code to pull together
+ * all of the macros throughout ldmx-sw and compile them into a
+ * single executable here.
+ *
+ * There can only be *one* instance of this definition.
+ * I have chosen to put it here for now.
+ */
+#define CATCH_CONFIG_MAIN
+#include "Test/catch.hpp"
