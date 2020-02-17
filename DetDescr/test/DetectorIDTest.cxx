b47d9d06a06fea7dbf5753f17bca40bf1673a4f8
tomeichlersmith <eichl008@umn.edu>
2020-02-17 10:42:35 -0600

    small improvements to DetectorID testing to show more Catch features

--- DetDescr/test/DetectorIDTest.cxx
+++ DetDescr/test/DetectorIDTest.cxx
@@ -2,35 +2,60 @@
  * @file DetectorIDTest.cxx
  * @brief Test the operation of DetectorID class
  */
-#include "Exception/catch.hpp"
+#include "Exception/catch.hpp" //for TEST_CASE, REQUIRE, and other Catch2 macros
 
-#include "DetDescr/DetectorID.h"
+#include "DetDescr/DetectorID.h" //headers defining what we will be testing
+#include "DetDescr/DefaultDetectorID.h" //headers defining what we will be testing
 
-TEST_CASE( "Detector ID Test" , "[function]" ) {
+/**
+ * Test for DetectorID function
+ *
+ * First argument is name of this test.
+ * Second argument is tags to group tests together.
+ */
+TEST_CASE( "Detector ID Test" , "[DetDescr][functionality]" ) {
 
     using ldmx::IDField;
     using ldmx::DetectorID;
+    using ldmx::DefaultDetectorID;
     
-    IDField::IDFieldList *fieldList = new IDField::IDFieldList();
-    fieldList->push_back(new IDField("subdet", 0, 0, 3));
-    fieldList->push_back(new IDField("layer", 1, 4, 11));
-    DetectorID detID(fieldList); //DetectorID owns fieldList and its members
+    SECTION( "General Detector ID" ) {
+        IDField::IDFieldList *fieldList = new IDField::IDFieldList();
+        fieldList->push_back(new IDField("subdet", 0, 0, 3));
+        fieldList->push_back(new IDField("layer", 1, 4, 11));
+        DetectorID detID(fieldList); //DetectorID owns fieldList and its members
+    
+        int layer = 10;
+        int subdet = 2;
+    
+        detID.setFieldValue("layer", layer);
+        detID.setFieldValue("subdet", subdet);
+        detID.pack(); //needs to happen to save new values
+    
+        DetectorID::RawValue rawVal = detID.getRawValue();
+    
+        detID.setRawValue(rawVal);
+        const DetectorID::FieldValueList& fieldValues = detID.unpack();
+    
+        REQUIRE( fieldValues[0] == subdet );
+        REQUIRE( detID.getFieldValue( "subdet" ) == subdet );
+        REQUIRE( fieldValues[1] == layer  );
+        REQUIRE( detID.getFieldValue( "layer" ) == layer );
+    }
 
-    int layer = 10;
-    int subdet = 2;
+    SECTION( "Default Detector ID" ) {
 
-    detID.setFieldValue("layer", layer);
-    detID.setFieldValue("subdet", subdet);
-    detID.pack();
+        DefaultDetectorID defaultID; //default ID handles IDFields internally
 
-    DetectorID::RawValue rawVal = detID.getRawValue();
+        int layer = 10;
+        int subdet = 2;
+    
+        defaultID.setFieldValue( "layer" , layer );
+        defaultID.setFieldValue( "subdet" , subdet );
+        defaultID.pack(); //needs to happen to save new values
+    
+        REQUIRE( defaultID.getSubdetID() == subdet );
+        REQUIRE( defaultID.getLayerID()  == layer  );
 
-    detID.setRawValue(rawVal);
-    const DetectorID::FieldValueList& fieldValues = detID.unpack();
-
-    REQUIRE( fieldValues[0] == subdet );
-    REQUIRE( detID.getFieldValue( "subdet" ) == subdet );
-    REQUIRE( fieldValues[1] == layer  );
-    REQUIRE( detID.getFieldValue( "layer" ) == layer );
-
+    }
 }
