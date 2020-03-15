254ffe71c1ccf1b5124d0956732d13630b0f8cc0
omar-moreno <email@omarmoreno.net>
2020-03-14 23:48:34 -0700

    Add classes for each of the actions.

--- SimApplication/src/UserAction.cxx
+++ SimApplication/src/UserAction.cxx
@@ -1,7 +1,18 @@
 
 #include "SimApplication/UserAction.h"
 
+/*~~~~~~~~~~~~~*/
+/*   SimCore   */
+/*~~~~~~~~~~~~~*/
 #include "SimApplication/UserActionManager.h"
+
+/*~~~~~~~~~~~~*/
+/*   Geant4   */
+/*~~~~~~~~~~~~*/
+#include "G4Event.hh"
+#include "G4Run.hh"
+#include "G4Step.hh"
+#include "G4Track.hh"
 
 namespace ldmx { 
 
@@ -12,9 +23,21 @@ namespace ldmx {
     UserAction::~UserAction() {} 
 
     void UserAction::declare(const std::string& className, UserActionBuilder* builder) {
+
+        std::cout << "UserAction::declare : Declaring class " << className << std::endl;
         UserActionManager::getInstance().registerAction(className, builder);      
     }
 
+    EventAction::EventAction(const std::string& name)       : UserAction(name) {}
+
+    RunAction::RunAction(const std::string& name)           : UserAction(name) {}
+
+    TrackingAction::TrackingAction(const std::string& name) : UserAction(name) {}
+
     SteppingAction::SteppingAction(const std::string& name) : UserAction(name) {}
+
+    StackingAction::StackingAction(const std::string& name) : UserAction(name) {}
+
+
 
 }
