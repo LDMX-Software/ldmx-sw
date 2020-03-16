233c6a612a24a03725ac5e39e75c18956541f4b2
omar-moreno <email@omarmoreno.net>
2020-03-16 11:58:20 -0700

    When creating a user action, register it with the corresponding class.

--- SimApplication/src/UserActionManager.cxx
+++ SimApplication/src/UserActionManager.cxx
@@ -18,20 +18,20 @@ namespace ldmx {
 
     UserActionManager::UserActionManager() { }
 
-    UserActionManager& UserActionManager::getInstance() { return instance_; }
-
-    actionVec UserActionManager::getActions() { 
+    actionMap UserActionManager::getActions() { 
         
-        if (actions_.empty()) { 
-            actions_.push_back(new UserRunAction()); 
-            actions_.push_back(new UserEventAction()); 
-            actions_.push_back(new UserTrackingAction()); 
-            actions_.push_back(new USteppingAction()); 
-            actions_.push_back(new UserStackingAction());
+        if (actions_.empty()) {
+            actions_[TYPE::RUN]      = new UserRunAction(); 
+            actions_[TYPE::EVENT]    = new UserEventAction(); 
+            actions_[TYPE::TRACKING] = new UserTrackingAction(); 
+            actions_[TYPE::STEPPING] = new USteppingAction();
+            actions_[TYPE::STACKING] = new UserStackingAction();   
         }
 
         return actions_; 
     }
+
+    UserActionManager& UserActionManager::getInstance() { return instance_; }
 
     void UserActionManager::registerAction(const std::string& className, UserActionBuilder* builder) { 
     
@@ -41,18 +41,34 @@ namespace ldmx {
         }
 
         ActionInfo info; 
-        info.className_ = className; 
+        info.className_ = className;
         info.builder_ = builder;
         
         actionInfo_[className] = info; 
     }
 
-    UserAction* UserActionManager::createAction(const std::string& className, const std::string& instanceName) { 
+    void UserActionManager::createAction(const std::string& className, const std::string& instanceName) { 
     
         auto it{actionInfo_.find(className)}; 
-        if (it == actionInfo_.end()) return 0; 
+        if (it == actionInfo_.end()) {
+            EXCEPTION_RAISE("UserActionException", "Failed to create " + className); 
+        } 
 
-        return it->second.builder_(instanceName); 
+        auto act{it->second.builder_(instanceName)};
+
+        if (act->getType() == TYPE::RUN) 
+            std::get< UserRunAction* >(actions_[act->getType()])->registerAction(static_cast<RunAction*>(act)); 
+        else if (act->getType() == TYPE::EVENT) 
+            std::get< UserEventAction* >(actions_[act->getType()])->registerAction(static_cast<EventAction*>(act)); 
+        else if (act->getType() == TYPE::TRACKING) 
+            std::get< UserTrackingAction* >(actions_[act->getType()])->registerAction(static_cast<TrackingAction*>(act)); 
+        else if (act->getType() == TYPE::STEPPING) 
+            std::get< USteppingAction* >(actions_[act->getType()])->registerAction(static_cast<SteppingAction*>(act)); 
+        else if (act->getType() == TYPE::STACKING) 
+            std::get< UserStackingAction* >(actions_[act->getType()])->registerAction(static_cast<StackingAction*>(act)); 
+        else {
+            EXCEPTION_RAISE("UserActionException", "Action type  doesn't exist."); 
+        }
     }
 
 } // ldmx
