2eb6750ac44689de776f048e27fb466a0965f2bf
omar-moreno <email@omarmoreno.net>
2020-03-17 23:18:48 -0700

    Configure the xsec biasing operator.

--- src/DetectorConstruction.cxx
+++ src/DetectorConstruction.cxx
@@ -1,5 +1,10 @@
 #include "SimApplication/DetectorConstruction.h"
 
+/*~~~~~~~~~~~~~~~*/
+/*   Exception   */
+/*~~~~~~~~~~~~~~~*/
+#include "Exception/Exception.h" 
+
 namespace ldmx {
 
     DetectorConstruction::DetectorConstruction(G4GDMLParser* theParser, Parameters& parameters) :
@@ -23,19 +28,38 @@ namespace ldmx {
         if (biasingEnabled) {
 
             auto biasingProcess{parameters_.getParameter< std::string >("biasing.process")}; 
-            auto biasingVolume{parameters_.getParameter< std::string >("biasing.volume")}; 
+            auto biasingVolume{parameters_.getParameter< std::string >("biasing.volume")};
+            auto biasingParticle{parameters_.getParameter< std::string >("biasing.particle")}; 
+            auto biasAll{parameters_.getParameter< bool >("biasing.all")}; 
+            auto biasIncident{parameters_.getParameter< bool >("biasing.incident")}; 
+            auto disableEMBiasing{parameters_.getParameter< bool >("biasing.disableEMBiasing")};
+            auto biasThreshold{parameters_.getParameter< double >("biasing.threshold")}; 
+            auto biasFactor{parameters_.getParameter< int >("biasing.factor")}; 
 
             // Instantiate the biasing operator
             // TODO: At some point, this should be more generic i.e. operators should be
             //       similar to plugins.
-            XsecBiasingOperator* xsecBiasing = nullptr; 
+            XsecBiasingOperator* xsecBiasing{nullptr}; 
             if (biasingProcess.compare("photonNuclear") == 0) { 
                 xsecBiasing = new PhotoNuclearXsecBiasingOperator("PhotoNuclearXsecBiasingOperator");
             } else if (biasingProcess.compare("GammaToMuPair") == 0) { 
                 xsecBiasing = new GammaToMuPairXsecBiasingOperator("GammaToMuPairXsecBiasingOperator");
             } else if (biasingProcess.compare("electronNuclear") == 0) { 
                 xsecBiasing = new ElectroNuclearXsecBiasingOperator("ElectroNuclearXsecBiasingOperator");
+            } else {
+                EXCEPTION_RAISE("BiasingException", "Invalid particle type"); 
             }
+
+            // Configure the operator
+            xsecBiasing->setParticleType(biasingParticle);
+            xsecBiasing->setThreshold(biasThreshold); 
+            xsecBiasing->setBiasFactor(biasFactor); 
+
+            if (biasAll) xsecBiasing->biasAll(); 
+            else if (biasIncident) xsecBiasing->biasIncident();
+            
+            if (disableEMBiasing) xsecBiasing->disableBiasDownEM(); 
+
 
             for (G4LogicalVolume* volume : *G4LogicalVolumeStore::GetInstance()) {
                 G4String volumeName = volume->GetName();
