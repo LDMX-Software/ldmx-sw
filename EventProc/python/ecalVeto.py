4c42fee61d4ed0ba63c132e2a63a372bd28ff1c1
omar-moreno <email@omarmoreno.net>
2020-03-25 11:43:26 -0700

    Change do_bdt flag to a bool.

--- EventProc/python/ecalVeto.py
+++ EventProc/python/ecalVeto.py
@@ -3,7 +3,7 @@ from LDMX.Framework import ldmxcfg
 
 ecalVeto = ldmxcfg.Producer("EcalVeto","ldmx::EcalVetoProcessor")
 ecalVeto.parameters["num_ecal_layers"] = 34
-ecalVeto.parameters["do_bdt"] = 1
+ecalVeto.parameters["do_bdt"] = True
 ecalVeto.parameters["bdt_file"] = "gabrielle.pkl"
 ecalVeto.parameters["cellxy_file"] = "cellxy.txt" 
 ecalVeto.parameters["disc_cut"] = 0.99
