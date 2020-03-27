7b4ec570ffed89ffa31f16b685daeefc42b34f0d
tomeichlersmith <eichl008@umn.edu>
2020-03-27 09:59:24 -0500

    python interaction with generators

--- SimApplication/python/basicOneElectron.py
+++ SimApplication/python/basicOneElectron.py
@@ -11,12 +11,6 @@ basicOneElectron.parameters[ "runNumber" ] = 1
 basicOneElectron.parameters[ "verbosity" ] = 3
 basicOneElectron.parameters[ "description"] = "One 4GeV electron shot from far upstream."
 
-from LDMX.SimApplication import simcfg
+from LDMX.SimApplication import generators
 
-farUpstreamElectron = simcfg.PrimaryGenerator( 'farUpstreamGenerator' , 'ldmx::ParticleGun' )
-farUpstreamElectron.parameters[ 'particle'  ] = 'e-'
-farUpstreamElectron.parameters[ 'position'  ] = [ -27.926, 5, -700 ] #mm
-farUpstreamElectron.parameters[ 'direction' ] = [ 313.8 / 4000 , 0, 3987.7/4000 ] #unitless
-farUpstreamElectron.parameters[ 'energy'    ] = 4.0 #GeV
-
-basicOneElectron.parameters[ "generators" ] = [ farUpstreamElectron ]
+basicOneElectron.parameters[ "generators" ] = [ generators.farUpstreamSingleElectron() ]
---
+++ SimApplication/python/generators.py
@@ -0,0 +1,71 @@
+
+##################################################################################
+# @file generators.py
+# Define some helpful functions and standardized generators for the simulation
+#
+# @author Tom Eichlersmith, University of Minnesota
+##################################################################################
+
+from LDMX.SimApplication import simcfg
+
+#############################################################
+# @function gun
+# @param name name of ParticleGun
+# @return PrimaryGenerator of class ldmx::ParticleGun
+#############################################################
+def gun( name ) :
+    return simcfg.PrimaryGenerator( name , "ldmx::ParticleGun" )
+
+#############################################################
+# @function multi
+# @param name name of MultiParticleGunPrimaryGenerator
+# @return PrimaryGenerator of class ldmx::MultiParticleGunPrimaryGenerator
+#############################################################
+def multi( name ) :
+    return simcfg.PrimaryGenerator( name , "ldmx::MultiParticleGunPrimaryGenerator" )
+
+#############################################################
+# @function lhe
+# @param name name of LHEPrimaryGenerator
+# @return PrimaryGenerator of class ldmx::LHEPrimaryGenerator
+#############################################################
+def lhe( name ) :
+    return simcfg.PrimaryGenerator( name , "ldmx::LHEPrimaryGenerator" )
+
+#############################################################
+# @function root
+# @param name name of RootPrimaryGenerator
+# @return PrimaryGenerator of class ldmx::RootPrimaryGenerator
+#############################################################
+def root( name ) :
+    return simcfg.PrimaryGenerator( name , "ldmx::RootPrimaryGenerator" )
+
+#############################################################
+# @function stdhep
+# @param name name of StdHepPrimaryGenerator
+# @return PrimaryGenerator of class ldmx::StdHepPrimaryGenerator
+#############################################################
+def stdhep( name ) :
+    print "stdhep PrimaryGenerator Not Implemented yet"
+    quit()
+
+#############################################################
+# @function gps
+# @param name name of GeneralParticleSource
+# @return PrimaryGenerator of class ldmx::GeneralParticleSource
+#############################################################
+def gps( name ) :
+    print "gps PrimaryGenerator Not Implemented yet"
+    return False
+
+#############################################################
+# @function farUpstreamSingleElectron
+# @return a ParticleGun with a single 4GeV electron fired from far upstream of target
+#############################################################
+def farUpstreamSingleElectron() :
+    farUpstreamElectron = gun( "farUpstreamSingleElectron" )
+    farUpstreamElectron.parameters[ 'particle'  ] = 'e-'
+    farUpstreamElectron.parameters[ 'position'  ] = [ -27.926, 5, -700 ] #mm
+    farUpstreamElectron.parameters[ 'direction' ] = [ 313.8 / 4000 , 0, 3987.7/4000 ] #unitless
+    farUpstreamElectron.parameters[ 'energy'    ] = 4.0 #GeV
+    return farUpstreamElectron
