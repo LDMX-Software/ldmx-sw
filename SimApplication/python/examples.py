0d0eea19ce62cefba8fdddfd19eaacc2a45e4831
tomeichlersmith <eichl008@umn.edu>
2020-03-28 15:18:06 -0500

    delete BiasingMessenger
    Add in examples and test python modules

---
+++ SimApplication/python/examples.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+################################################################################################
+# @file examples.py
+# Example Simulators using various generators
+#
+# @author Tom Eichlersmith, University of Minnesota
+################################################################################################
+
+from LDMX.Framework import ldmxcfg
+from LDMX.Detectors.makePath import makeDetectorPath
+from LDMX.SimApplication import generators
+
+################################################################################################
+# @func basicOneElectron
+# @return a simulation with one 4GeV electron shot from far upstream
+################################################################################################
+def basicOneElectron() :
+    sim = ldmxcfg.Producer( "basicOneElectron" , "ldmx::Simulator" )
+    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-full-v12-fieldmap-magnet" )
+    sim.parameters[ "runNumber" ] = 1
+    sim.parameters[ "verbosity" ] = 3
+    sim.parameters[ "description"] = "One 4GeV electron shot from far upstream."
+    sim.parameters[ "generators" ] = [ generators.farUpstreamSingle4GeVElectron() ]
+    return sim
+
+################################################################################################
+# @func lheExample
+# @param lheFile path to LHE file to use with generator
+# @return a simulator using the LHE generator
+################################################################################################
+def lheExample( lheFile ) :
+    sim = ldmxcfg.Producer( "lheSimulation" , "ldmx::Simulator" )
+    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-full-v12-fieldmap-magnet" )
+    sim.parameters[ "runNumber" ] = 1
+    sim.parameters[ "verbosity" ] = 3
+    sim.parameters[ "description"] = "Example of how to use LHE generator"
+    sim.parameters[ "generators" ] = [ generators.lhe( "LHEExample" , lheFile ) ]
+    return sim
+
+################################################################################################
+# @func gpsExample
+# @return a simulator using the General Particle Source
+################################################################################################
+def gpsExample( ) :
+    sim = ldmxcfg.Producer( "gpsExample" , "ldmx::Simulator" )
+    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-full-v12-fieldmap-magnet" )
+    sim.parameters[ "runNumber" ] = 1
+    sim.parameters[ "verbosity" ] = 3
+    sim.parameters[ "description"] = "Example of how to use GPS generator"
+    gpsGen = generators.gps( "GPSExample" )
+    gpsGen.parameters[ "initCommands" ] = [
+        "/gps/particle e-",
+        "/gps/pos/type Plane",
+        "/gps/pos/shape Square",
+        "/gps/pos/centre 0 0 0 mm",
+        "/gps/pos/halfx 40 mm",
+        "/gps/pos/halfy 80 mm",
+        "/gps/ang/type cos",
+        "/gps/ene/type Lin",
+        "/gps/ene/min 3 GeV",
+        "/gps/ene/max 4 GeV",
+        "/gps/ene/gradient 1",
+        "/gps/ene/intercept 1"
+        ]
+    sim.parameters[ "generators" ] = [ gpsGen ]
+    return sim
+
+################################################################################################
+# @func multiExample
+# @return a simulator using the MPG generator
+################################################################################################
+def multiExample( lheFile ) :
+    sim = ldmxcfg.Producer( "mpgExample" , "ldmx::Simulator" )
+    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-full-v12-fieldmap-magnet" )
+    sim.parameters[ "runNumber" ] = 1
+    sim.parameters[ "verbosity" ] = 3
+    sim.parameters[ "description"] = "Example of how to use MPG generator"
+    mpgGen = generators.multi( "MPGExample" )
+    mpgGen.parameters[ "vertex" ] = [ 0., 0., 0. ] #mm
+    mpgGen.parameters[ "nParticles" ] = 2
+    mpgGen.parameters[ "pdgID" ] = 11
+    mpgGen.parameters[ "enablePoisson" ] = True
+    mpgGen.parameters[ "momentum" ] = [ 0., 0., 4000. ] #MeV
+    sim.parameters[ "generators" ] = [ mpgGen ]
+    return sim
+
