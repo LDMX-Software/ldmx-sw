73bcbdb0356aca7c9536bf341925031c3322fc59
Lene Kristian Bryngemark <lene.bryngemark@cern.ch>
2020-04-16 14:42:01 -0700

    Change number of strips in trigscint reco config to 50
    This change makes the number of strips in the hit reconstruction equal
    the number of bars (strips) in the detector geometry.

--- EventProc/python/trigScintDigis.py
+++ EventProc/python/trigScintDigis.py
@@ -5,17 +5,17 @@ from LDMX.Framework import ldmxcfg
 trigScintDigis = ldmxcfg.Producer("trigScintDigis", "ldmx::TrigScintDigiProducer")
 
 trigScintDigis.parameters["mean_noise"] = 0.02
-trigScintDigis.parameters["number_of_strips"] = 62
+trigScintDigis.parameters["number_of_strips"] = 50
 trigScintDigis.parameters["number_of_arrays"] = 1
 trigScintDigis.parameters["mev_per_mip"] = 0.4
 trigScintDigis.parameters["pe_per_mip"] = 10.
 trigScintDigis.parameters["input_collection"]="TriggerPadUpSimHits"
-trigScintDigis.parameters["output_collection"]="trigScintDigis"
+trigScintDigis.parameters["output_collection"]="trigScintDigisDn"
 
 trigScintDigisDn = ldmxcfg.Producer("trigScintDigis", "ldmx::TrigScintDigiProducer")
 
 trigScintDigisDn.parameters["mean_noise"] = 0.02
-trigScintDigisDn.parameters["number_of_strips"] = 62
+trigScintDigisDn.parameters["number_of_strips"] = 50
 trigScintDigisDn.parameters["number_of_arrays"] = 1
 trigScintDigisDn.parameters["mev_per_mip"] = 0.4
 trigScintDigisDn.parameters["pe_per_mip"] = 10.
@@ -25,7 +25,7 @@ trigScintDigisDn.parameters["output_collection"]="trigScintDigisDn"
 trigScintDigisTag = ldmxcfg.Producer("trigScintDigis", "ldmx::TrigScintDigiProducer")
 
 trigScintDigisTag.parameters["mean_noise"] = 0.02
-trigScintDigisTag.parameters["number_of_strips"] = 62
+trigScintDigisTag.parameters["number_of_strips"] = 50
 trigScintDigisTag.parameters["number_of_arrays"] = 1
 trigScintDigisTag.parameters["mev_per_mip"] = 0.4
 trigScintDigisTag.parameters["pe_per_mip"] = 10.
