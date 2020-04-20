5f8abae0a3ec746fb7c144dd986da87d5d1f6190
omar-moreno <email@omarmoreno.net>
2020-04-20 00:31:46 -0700

    Update functions so users need to pass the detector.

--- Biasing/python/target.py
+++ Biasing/python/target.py
@@ -1,75 +1,74 @@
-###############################################################################
-# Templates for studying biased interactions in Target
-# Import with:
-#   from LDMX.Biasing import target
-#
-# @author Tom Eichlersmith, University of Minnesota
-###############################################################################
+""" Example configurations for producing biased interactions in the target. 
+
+    Example
+    -------
+        
+        from LDMX.Biasing import target
+"""
 
 from LDMX.Framework import ldmxcfg
-from LDMX.Detectors.makePath import * #both detector and scoring planes path
+from LDMX.Detectors.makePath import * 
 from LDMX.SimApplication import generators
 from LDMX.Biasing import event_filters, track_filters
 
-###############################################################################
-# @func electronNuclear
-# Biasing EN interactions in the target and use a basic one 4GeV electron
-#   generator
-# User:
-#   myTargetENSim = target.electronNuclear()
-###############################################################################
 def electro_nuclear( detector ) :
+    """Example configuration for producing electro-nuclear reactions in the target.  
+       
+    In this particular example, 4 GeV electrons are fired upstream of the 
+    tagger tracker. TargetENFilter filters out events that don't see an 
+    electro-nuclear reaction take places in the target.  
     
-    #
+    Parameters
+    ----------
+
+    detector : str
+        Path to the detector 
+
+    Returns
+    -------
+    Instance of the simulator configured for target electro-nuclear.
+
+    Example
+    -------
+
+        target_en_sim = target.electro_nuclear('ldmx-det-v12')
+
+    """
+    
     # Instantiate the simulator.  Before doing this, the shared library containing
     # the simulator needs to be loaded.  This is usually done from the top level
     # configure file.
-    #
     simulator = ldmxcfg.Producer("target_electronNuclear", "ldmx::Simulator")
     
-    #
     # Set the path to the detector to use.
     #
     # The detectors installed with ldmx-sw can be accessed using the makeDetectorPath function.
     # Otherwise, you can provide the full path yourself.
-    #
     simulator.parameters["detector"] = makeDetectorPath( detector )
     
-    #
     # Set run parameters
-    #
     simulator.parameters["runNumber"] = 0
     simulator.parameters["description"] = "Target electron-nuclear, xsec bias 1e8"
     simulator.parameters["randomSeeds"] = [ 1, 2 ]
     simulator.parameters["beamSpotSmear"] = [20., 80., 0.]
     
-    #
     # Fire an electron upstream of the tagger tracker
     #
     # A 4GeV single electron generator is so common that you
     # can pull it in from the generators module.
-    #
     simulator.parameters['generators'] = [ generators.farUpstreamSingle4GeVElectron() ]
     
-    #
     # Enable the scoring planes 
-    #
-    # Same comments about path to gdml as for the detectors
-    #
     simulator.parameters["scoringPlanes"] = makeScoringPlanesPath( detector )
     
-    #
     # Enable and configure the biasing
-    #
     simulator.parameters['biasing.enabled'] = True
     simulator.parameters['biasing.particle'] = 'e-'
     simulator.parameters['biasing.process'] = 'electronNuclear'
     simulator.parameters['biasing.volume'] = 'target'
     simulator.parameters['biasing.factor'] = 1e8
     
-    #
     # Configure the sequence in which user actions should be called.
-    #
     simulator.parameters["actions"] = [
             event_filters.targetENFilter(), #only consider events where an EN interaction happens in the target
             track_filters.keepENTracks()    #keep all EN children
@@ -77,56 +76,62 @@ def electro_nuclear( detector ) :
 
     return simulator
 
-###############################################################################
-# @func photonNuclear
-# Biasing PN interactions in the target and use a basic one 4GeV electron
-#   generator
-# User:
-#   myTargetPNSim = target.photonNuclear()
-###############################################################################
 def photo_nuclear( detector ) :
+    """Example configuration for producing photo-nuclear reactions in the ECal.  
+       
+    In this particular example, 4 GeV electrons are fired upstream of the 
+    tagger tracker.  The TargetBremFilter filters out all events that don't 
+    produced a brem in the target with an energy greater than 2.5 GeV. 
+    TargetBremFilter filters out events that don't see the brem photon undergo
+    a photo-nuclear reaction in the target. 
     
-    #
+    Parameters
+    ----------
+
+    detector : str
+        Path to the detector 
+
+    Returns
+    -------
+    Instance of the simulator configured for target photo-nuclear.
+
+    Example
+    -------
+
+        target_pn_sim = target.photo_nuclear('ldmx-det-v12')
+
+    """
+
+
     # Instantiate the simulator.  Before doing this, the shared library containing
     # the simulator needs to be loaded.  This is usually done from the top level
     # configure file.
-    #
     simulator = ldmxcfg.Producer("target_photonNuclear", "ldmx::Simulator")
     
-    #
     # Set the path to the detector to use.
     #
     # The detectors installed with ldmx-sw can be accessed using the makeDetectorPath function.
     # Otherwise, you can provide the full path yourself.
-    #
     simulator.parameters["detector"] = makeDetectorPath( detector )
     
-    #
     # Set run parameters
-    #
     simulator.parameters["runNumber"] = 0
     simulator.parameters["description"] = "ECal photo-nuclear, xsec bias 450"
     simulator.parameters["randomSeeds"] = [ 1, 2 ]
     simulator.parameters["beamSpotSmear"] = [20., 80., 0.]
     
-    #
     # Fire an electron upstream of the tagger tracker
     #
     # A 4GeV single electron generator is so common that you
     # can pull it in from the generators module.
-    #
     simulator.parameters['generators'] = [ generators.farUpstreamSingle4GeVElectron() ]
     
-    #
     # Enable the scoring planes 
     #
     # Same comments about path to gdml as for the detectors
-    #
     simulator.parameters["scoringPlanes"] = makeScoringPlanesPath( detector )
     
-    #
     # Enable and configure the biasing
-    #
     simulator.parameters['biasing.enabled'] = True
     simulator.parameters['biasing.particle'] = 'gamma'
     simulator.parameters['biasing.process'] = 'photonNuclear'
@@ -134,57 +139,73 @@ def photo_nuclear( detector ) :
     simulator.parameters['biasing.threshold'] = 2500.
     simulator.parameters['biasing.factor'] = 450
     
-    #
     # Save tracks of particles created in the photonuclear reaction
-    #
     track_process_filter = simcfg.UserAction('trackProcessFilter', 'ldmx::TrackProcessFilter')
     track_process_filter.parameters['process'] = ['photonNuclear']
     
-    #
     # Configure the sequence in which user actions should be called.
-    #
     simulator.parameters["actions"] = [
-            event_filters.targetBremFilter(), #only consider events where a hard brem occurs
-            event_filters.targetPNFilter(),   #only consider events where a PN reaction happnes in the target
-            track_filters.keepPNTracks()      #keep all PN children
+            event_filters.targetBremFilter(), 
+            event_filters.targetPNFilter(),   
+            track_filters.keepPNTracks()      
             ]
 
     return simulator
 
-###############################################################################
-# @func darkBrem
-# Biasing dark brem interactions in the target and use a basic one 4GeV electron
-#   generator
-# User:
-#       myTargetDarkBremSim = target.darkBrem( massAPrime , lheFile )
-#   where massAPrime is the mass of the A' in MeV and lheFile is the path
-#   to the LHE file to use as vertices of the dark brem
-###############################################################################
-def darkBrem( massAPrime , lheFile, detector ) :
-    darkBremOn = ldmxcfg.Producer( "darkBrem_" + str(massAPrime) + "_MeV" , "ldmx::Simulator")
+def dark_brem( ap_mass , lhe, detector ) :
+    """Example configuration for producing dark brem interactions in the target. 
+
+    This configures the simulator to fire a 4 GeV electron upstream of the 
+    tagger tracker.  The dark-photon production cross-section is biased up in 
+    the target.  Only events that result in a dark-photon being produced in the
+    target are kept. 
+
+    Parameters
+    ----------
+    ap_mass : float
+        The mass of the A' in MeV.
+    lhe : str
+        The path to the LHE file to use as vertices of the dark brem. 
+    detector : str
+        Path to the detector.
+
+    Return
+    ------
+    Instance of the simulator configured for dark-brem production in the target.
+
+    Example
+    -------
+
+        target_ap_sim = target.dark_brem(1000, 'path/to/lhe', 'ldmx-det-v12')
+
+
+    """
+    simulator = ldmxcfg.Producer( "darkBrem_" + str(massAPrime) + "_MeV" , "ldmx::Simulator")
     
-    darkBremOn.parameters[ "description" ] = "One e- fired far upstream with Dark Brem turned on and biased up in target"
-    darkBremOn.parameters[ "detector" ] = makeDetectorPath( detector )
-    darkBremOn.parameters[ "scoringPlanes" ] = makeScoringPlanesPath( detector )
-    darkBremOn.parameters[ "generators" ] = [ generators.farUpstreamSingle4GeVElectron() ]
+    simulator.parameters[ "description" ] = "One e- fired far upstream with Dark Brem turned on and biased up in target"
+    simulator.parameters[ "detector" ] = makeDetectorPath( detector )
+    simulator.parameters[ "scoringPlanes" ] = makeScoringPlanesPath( detector )
+    simulator.parameters[ "generators" ] = [ generators.farUpstreamSingle4GeVElectron() ]
     
     # Bias the electron dark brem process inside of the target
-    #   These commands allow us to restrict the dark brem process to a given volume.
-    darkBremOn.parameters[ "biasing.enabled" ] = True
-    darkBremOn.parameters[ "biasing.particle"] = "e-"
-    darkBremOn.parameters[ "biasing.process" ] = "eDBrem"
-    darkBremOn.parameters[ "biasing.volume"  ] = "target" #options: target, ecal
-    darkBremOn.parameters[ "biasing.factor"  ] = 1000000 #this factor is only applied in the volume defined in biasing
+    # These commands allow us to restrict the dark brem process to a given 
+    # volume.
+    simulator.parameters[ "biasing.enabled" ] = True
+    simulator.parameters[ "biasing.particle"] = "e-"
+    simulator.parameters[ "biasing.process" ] = "eDBrem"
+    # Options: target, ECal
+    simulator.parameters[ "biasing.volume"  ] = "target"
+    simulator.parameters[ "biasing.factor"  ] = 1000000 
     
-    darkBremOn.parameters[ "darkbrem.method" ] = 1 #Forward only
+    simulator.parameters[ "darkbrem.method" ] = 1 # Forward only
 
-    darkBremOn.parameters[ "APrimeMass" ] = massAPrime #MeV
-    darkBremOn.parameters[ "darkbrem.madgraphfilepath" ] = lheFile
+    simulator.parameters[ "APrimeMass" ] = massAPrime #MeV
+    simulator.parameters[ "darkbrem.madgraphfilepath" ] = lheFile
     
     # Then give the UserAction to the simulation so that it knows to use it
-    darkBremOn.parameters['actions'] = [ 
-            event_filters.targetDarkFilter() , #only keep events when a dark brem happens in the target
-            track_filters.keepDarkTracks()     #keep all eDBrem children
+    simulator.parameters['actions'] = [ 
+            event_filters.targetDarkFilter() , 
+            track_filters.keepDarkTracks()     
             ]
     
-    return darkBremOn
+    return simulator
