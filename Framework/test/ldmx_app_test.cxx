2553b545cf6837415ac66a4e9a33db6512f34936
tomeichlersmith <eichl008@umn.edu>
2020-04-23 12:19:02 -0500

    outline tests with SECTION macros

--- Framework/test/ldmx_app_test.cxx
+++ Framework/test/ldmx_app_test.cxx
@@ -17,4 +17,57 @@
  */
 TEST_CASE( "ldmx-app Functionality Test" , "[Framework][functionality]" ) {
 
+    //test python configuration
+    //  run script
+    //  read in parameters and give them to process and processors
+
+
+    SECTION( "Production Mode" ) {
+        //no input files, only output files
+        
+        SECTION( "with Analyses" ) {
+
+        }
+
+        SECTION( "with drop/keep rules" ) {
+
+        }
+
+    }
+
+    SECTION( "Analysis Mode" ) {
+        //no output files, only histogram output
+
+        SECTION( "with drop/keep rules" ) {
+
+        }
+
+    }
+
+    SECTION( "Merge Mode" ) {
+        //many input files to one output file
+
+        SECTION( "with Analyses" ) {
+
+        }
+
+        SECTION( "with drop/keep rules" ) {
+
+        }
+
+    }
+
+    SECTION( "N to N Mode" ) {
+        //many input files to many output files
+
+        SECTION( "with Analyses" ) {
+
+        }
+
+        SECTION( "with drop/keep rules" ) {
+
+        }
+
+    }
+
 }
