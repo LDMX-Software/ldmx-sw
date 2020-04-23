247044c2029cca87e87bf250b79d612414253eab
tomeichlersmith <eichl008@umn.edu>
2020-04-23 12:03:50 -0500

    skeleton testing file

---
+++ Framework/test/ldmx_app_test.cxx
@@ -0,0 +1,20 @@
+/**
+ * @file ldmx_app_test.cxx
+ * @brief Test the operation of ldmx-app program
+ *
+ * @author Tom Eichlersmith, University of Minnesota
+ */
+#include "Exception/catch.hpp" //for TEST_CASE, REQUIRE, and other Catch2 macros
+
+#include "Framework/Process.h"
+#include "Framework/ConfigurePython.h"
+
+/**
+ * Test for ldmx-app function
+ *
+ * First argument is name of this test.
+ * Second argument is tags to group tests together.
+ */
+TEST_CASE( "ldmx-app Functionality Test" , "[Framework][functionality]" ) {
+
+}
