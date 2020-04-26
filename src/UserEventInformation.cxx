92adfbd9b77150dc5e9e502ba83271ca7445c5b5
omar-moreno <email@omarmoreno.net>
2020-04-26 00:00:47 -0700

    Add event weight.

--- src/UserEventInformation.cxx
+++ src/UserEventInformation.cxx
@@ -12,7 +12,7 @@ namespace ldmx {
     UserEventInformation::~UserEventInformation() {}
 
     void UserEventInformation::Print() const { 
-        std::cout << "Event weight: " << eventWeight_ << "\n"
+        std::cout << "Event weight: " << weight_ << "\n"
                   << "Brem candidate count: " << bremCandidateCount_ << "\n"
                   << std::endl;  
     }
