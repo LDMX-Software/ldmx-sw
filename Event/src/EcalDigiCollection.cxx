5ea197d1ab4576f66628a6b38c14f0e108ba4482
Tom E <tom.eichlersmith@gmail.com>
2020-05-04 16:57:43 -0500

    merge digicoll into ecaldigicoll

--- Event/src/EcalDigiCollection.cxx
+++ Event/src/EcalDigiCollection.cxx
@@ -1,25 +1,77 @@
+/**
+ * @file EcalDigiCollection.cxx
+ * @author Tom Eichlersmith, University of Minnesota
+ */
+
 #include "Event/EcalDigiCollection.h"
 
 ClassImp(ldmx::EcalDigiCollection)
 
 namespace ldmx {
 
+    void EcalDigiCollection::Clear() {
+
+        channelIDs_.clear();
+        samples_.clear();
+
+        return;
+    }
+
+    void EcalDigiCollection::Print() const {
+
+        printf( "Num Channel IDs:      %d\n" , channelIDs_.size() );
+        printf( "Num Samples:          %d\n" , samples_.size() );
+        printf( "Num Samples Per Digi: %d\n" , numSamplesPerDigi_ );
+
+        return;
+    }
+
     std::vector< EcalDigiSample > EcalDigiCollection::getDigi( unsigned int digiIndex ) const {
         
         std::vector< EcalDigiSample > digi;
         for ( unsigned int sampleIndex = 0; sampleIndex < this->getNumSamplesPerDigi(); sampleIndex++ ) {
-            digi.push_back( this->getSample( digiIndex , sampleIndex ) );
+    
+            EcalDigiSample sample;
+    
+            sample.rawID_ = channelIDs_.at( digiIndex );
+    
+            int32_t word = samples_.at( digiIndex*numSamplesPerDigi_ + sampleIndex );
+    
+            //this is where the word --> measurements translation occurs
+    
+            bool firstFlag = ONE_BIT_MASK & ( word >> FIRSTFLAG_POS );
+            bool seconFlag = ONE_BIT_MASK & ( word >> SECONFLAG_POS );
+            int  firstMeas = TEN_BIT_MASK & ( word >> FIRSTMEAS_POS );
+            int  seconMeas = TEN_BIT_MASK & ( word >> SECONMEAS_POS );
+            int  lastMeas  = TEN_BIT_MASK & ( word );
+    
+            //the chip returns flags that determine what the three measurements are
+            //  I (Tom E) don't know right now what that mapping is, so I will not use them.
+    
+            sample.adc_t_ = firstMeas;
+            sample.tot_   = seconMeas;
+            sample.toa_   = lastMeas;
+            sample.adc_tm1_ = -99;
+
+            digi.push_back( sample );
         }
 
         return digi;
     }
 
     void EcalDigiCollection::addDigi( std::vector< EcalDigiSample > newSamples ) {
+
+        if ( newSamples.size() != this->getNumSamplesPerDigi() ) {
+            std::cerr << "[ WARN ] [ EcalDigiCollection ] Input list of samples has size '"
+                << newSamples.size() << "' that does not match the number of samples per digi '"
+                << this->getNumSamplesPerDigi() << "'!." << std::endl;
+            return;
+        }
         
-        std::vector< EcalDigiSample >::iterator it_newSamples;
         int channelID = newSamples.at(0).rawID_;
-        std::vector< int32_t > words;
-        for ( it_newSamples = newSamples.begin(); it_newSamples != newSamples.end(); ++it_newSamples ) {
+        channelIDs_.push_back( channelID );
+
+        for ( auto const &sample : newSamples ) {
             
             int32_t word;
 
@@ -27,9 +79,9 @@ namespace ldmx {
 
             //check if over largest number possible ==> set to largest if over
             //  don't want wrapping
-            int adc_t = (it_newSamples->adc_t_ > TEN_BIT_MASK) ? TEN_BIT_MASK : it_newSamples->adc_t_;
-            int tot   = (it_newSamples->tot_   > TEN_BIT_MASK) ? TEN_BIT_MASK : it_newSamples->tot_;
-            int toa   = (it_newSamples->toa_   > TEN_BIT_MASK) ? TEN_BIT_MASK : it_newSamples->toa_;
+            int adc_t = (sample.adc_t_ > TEN_BIT_MASK) ? TEN_BIT_MASK : sample.adc_t_;
+            int tot   = (sample.tot_   > TEN_BIT_MASK) ? TEN_BIT_MASK : sample.tot_;
+            int toa   = (sample.toa_   > TEN_BIT_MASK) ? TEN_BIT_MASK : sample.toa_;
 
             //the chip returns flags that determine what the three measurements are
             //  I (Tom E) don't know right now what that mapping is, so I will not use them.
@@ -41,38 +93,11 @@ namespace ldmx {
                  + ( (tot   & TEN_BIT_MASK) << SECONMEAS_POS ) 
                  + ( toa & TEN_BIT_MASK );
             
-            words.push_back( word );
+            samples_.push_back( word );
         }
 
-        DigiCollection::addDigi( channelID , words );
 
         return;
     }
 
-    EcalDigiSample EcalDigiCollection::getSample( unsigned int digiIndex , unsigned int sampleIndex ) const {
-
-        EcalDigiSample sample;
-
-        sample.rawID_ = this->getChannelID( digiIndex );
-
-        int32_t word = DigiCollection::getSampleWord( digiIndex , sampleIndex  );
-
-        //this is where the word --> measurements translation occurs
-
-        bool firstFlag = ONE_BIT_MASK & ( word >> FIRSTFLAG_POS );
-        bool seconFlag = ONE_BIT_MASK & ( word >> SECONFLAG_POS );
-        int  firstMeas = TEN_BIT_MASK & ( word >> FIRSTMEAS_POS );
-        int  seconMeas = TEN_BIT_MASK & ( word >> SECONMEAS_POS );
-        int  lastMeas  = TEN_BIT_MASK & ( word );
-
-        //the chip returns flags that determine what the three measurements are
-        //  I (Tom E) don't know right now what that mapping is, so I will not use them.
-
-        sample.adc_t_ = firstMeas;
-        sample.tot_   = seconMeas;
-        sample.toa_   = lastMeas;
-        sample.adc_tm1_ = -99;
-
-        return sample;
-    }
-}
+} //ldmx
