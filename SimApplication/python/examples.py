5914c1956a400a885146f4005eb4afee893fbcb2
omar-moreno <email@omarmoreno.net>
2020-05-08 11:24:16 -0700

    Update examples.

--- SimApplication/python/examples.py
+++ SimApplication/python/examples.py
@@ -1,49 +1,28 @@
-#!/usr/bin/python
-
-################################################################################################
-# @file examples.py
-# Example Simulators using various generators
-#
-# @author Tom Eichlersmith, University of Minnesota
-################################################################################################
 
 from LDMX.Framework import ldmxcfg
 from LDMX.Detectors.makePath import makeDetectorPath
 from LDMX.SimApplication import generators
 
-################################################################################################
-# @func basicOneElectron
-# @return a simulation with one 4GeV electron shot from far upstream
-################################################################################################
-def basicOneElectron() :
-    sim = ldmxcfg.Producer( "basicOneElectron" , "ldmx::Simulator" )
-    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-full-v12-fieldmap-magnet" )
+def inclusive_single_e() :
+    sim = ldmxcfg.Producer( "inclusive_single_e" , "ldmx::Simulator" )
+    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-v12" )
     sim.parameters[ "runNumber" ] = 1
     sim.parameters[ "description"] = "One 4GeV electron shot from far upstream."
-    sim.parameters[ "generators" ] = [ generators.farUpstreamSingle4GeVElectron() ]
+    sim.parameters[ "generators" ] = [ generators.single_4gev_e_upstream_tagger() ]
     return sim
 
-################################################################################################
-# @func lheExample
-# @param lheFile path to LHE file to use with generator
-# @return a simulator using the LHE generator
-################################################################################################
 def lheExample( lheFile ) :
     sim = ldmxcfg.Producer( "lheSimulation" , "ldmx::Simulator" )
-    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-full-v12-fieldmap-magnet" )
+    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-v12" )
     sim.parameters[ "runNumber" ] = 1
     sim.parameters[ "verbosity" ] = 3
     sim.parameters[ "description"] = "Example of how to use LHE generator"
     sim.parameters[ "generators" ] = [ generators.lhe( "LHEExample" , lheFile ) ]
     return sim
 
-################################################################################################
-# @func gpsExample
-# @return a simulator using the General Particle Source
-################################################################################################
 def gpsExample( ) :
     sim = ldmxcfg.Producer( "gpsExample" , "ldmx::Simulator" )
-    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-full-v12-fieldmap-magnet" )
+    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-v12" )
     sim.parameters[ "runNumber" ] = 1
     sim.parameters[ "verbosity" ] = 3
     sim.parameters[ "description"] = "Example of how to use GPS generator"
@@ -65,13 +44,9 @@ def gpsExample( ) :
     sim.parameters[ "generators" ] = [ gpsGen ]
     return sim
 
-################################################################################################
-# @func multiExample
-# @return a simulator using the MPG generator
-################################################################################################
 def multiExample( ) :
     sim = ldmxcfg.Producer( "mpgExample" , "ldmx::Simulator" )
-    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-full-v12-fieldmap-magnet" )
+    sim.parameters[ "detector"  ] = makeDetectorPath( "ldmx-det-v12" )
     sim.parameters[ "runNumber" ] = 1
     sim.parameters[ "verbosity" ] = 3
     sim.parameters[ "description"] = "Example of how to use MPG generator"
