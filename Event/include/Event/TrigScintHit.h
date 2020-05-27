b2ebeb58bd842221d57d370a504775ebfda395ef
omar-moreno <email@omarmoreno.net>
2020-05-27 11:59:40 -0700

    Fix formatting.

--- Event/include/Event/TrigScintHit.h
+++ Event/include/Event/TrigScintHit.h
@@ -57,48 +57,48 @@ namespace ldmx {
             /// Get the module/pad associated with the hit from the ID. */
             int getModule() const { return moduleID_; }
 
-	    /**
+            /**
              * Set the hit time. 
              *
              * @param time The hit time.
              */
             void setTime(const float time) { time_ = time; }
 
-	    /** 
+            /** 
              * Set hit bar ID.
              *
              * @param barID The bar ID of the hit.
              */
-	    void setBarID(const int barID) { barID_ = barID; };
+            void setBarID(const int barID) { barID_ = barID; };
 
-	    /** 
+            /** 
              * Set hit module ID.
              *
              * @param moduleID The module ID of the hit.
              */
-	    void setModuleID(const int moduleID) { moduleID_ = moduleID; };
+            void setModuleID(const int moduleID) { moduleID_ = moduleID; };
 
-	    /** 
+            /** 
              * Set beam energy fraction of hit. 
              *
              * @param beamEfrac The beam energy fraction of the hit.
              */
-	    void setBeamEfrac(const float beamEfrac) { beamEfrac_ = beamEfrac; };
-	  
+            void setBeamEfrac(const float beamEfrac) { beamEfrac_ = beamEfrac; };
+
         private:
 
             /// The time estimated for this hit. 
             float time_{0};
-	    // bar/channel number associated with the hit 
-	    int barID_{-1};
-	    // module/pad number associated with the hit
-	    int moduleID_{-1};
-	    
-	    /// The fraction of energy associated with beam electrons. 
-	    float beamEfrac_{0};
-	    
+            // bar/channel number associated with the hit 
+            int barID_{-1};
+            // module/pad number associated with the hit
+            int moduleID_{-1};
+
+            /// The fraction of energy associated with beam electrons. 
+            float beamEfrac_{0};
+
             ClassDef(TrigScintHit, 1);
-    
+
     }; // TrigScintHit
 
 } // ldmx
