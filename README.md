5e2651e63eba22d2f3daa293079bfbd7680735d0
Tom E <tom.eichlersmith@gmail.com>
2020-05-30 23:44:35 -0500

    more small improvements

--- README.md
+++ README.md
@@ -9,9 +9,9 @@
 - [Install the docker engine](https://docs.docker.com/engine/install/)
 - [Manage docker as non-root user](https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user)
 - Clone the repo: `git clone https://github.com/LDMX-Software/ldmx-sw.git`
-- Setup the environment: `/bin/bash source ldmx-sw/scripts/ldmx-env.sh`
-- Make a build directory: `cd ldmx-sw; mkdir ldmx-sw; mkdir build; cd build;`
-- Configure the build: `ldmx-cmake`
+- Setup the environment: `/bin/bash -c source ldmx-sw/scripts/ldmx-env.sh`
+- Make a build directory: `cd ldmx-sw; mkdir build; cd build;`
+- Configure the build: `ldmx cmake -DCMAKE_INSTALL_PREFIX=../install ..`
 - Build and Install: `ldmx make install -j2`
 - Now you can run any processor in _ldmx-sw_ through `ldmx app`
 
--- scripts/ldmx-env.sh
+++ scripts/ldmx-env.sh
@@ -7,7 +7,6 @@
 #   2. Can run docker as a non-root user
 ###############################################################################
 
-
 ###############################################################################
 # Parse CLI Arguments
 #   $1 - base directory for your ldmx work
@@ -19,7 +18,11 @@ _ldmx_base="$1"
 if [ -z ${_ldmx_base} ]; then
     _ldmx_base=$(pwd)
 fi
-export LDMX_BASE=${_ldmx_base}
+
+# this makes sure we get the full path
+cd ${_ldmx_base}
+export LDMX_BASE=$(pwd)
+cd - &> /dev/null
 
 _dock_image="$2"
 if [ -z ${_dock_image} ]; then
@@ -29,30 +32,22 @@ export LDMX_DOCKER_TAG=${_dock_image}
 
 #this is the name of the dockerhub repository
 #   it shouldn't change
-export _docker_hub_repo="ldmx" #"ldmx/dev"
+export _docker_hub_repo="ldmx/dev"
 
 ###############################################################################
 # Make sure we have the latest docker container
 ###############################################################################
-#docker pull ${_docker_hub_repo}:${LDMX_DOCKER_TAG}
+docker pull ${_docker_hub_repo}:${LDMX_DOCKER_TAG}
 
 ###############################################################################
 # ldmx
 #   Launch the user into the ldmx docker container environment, passing
 #   any argument along as a command to be executed.
+#
+#   The container is set up so that it goes to the first argument and then
+#   executes the rest. This alias sets the first argument to $(pwd) so that
+#   the container runs the users command from the same place that the user
+#   intended.
 ###############################################################################
 alias ldmx='docker run -i -e LDMX_BASE -v $LDMX_BASE:$LDMX_BASE ${_docker_hub_repo}:$LDMX_DOCKER_TAG $(pwd)'
 
-
-###############################################################################
-# ldmx-cmake
-#   The cmake command is still rather nasty for ldmx-sw, so we provide
-#   a helpful alias for that here. Notice that the paths are with
-#   reference to inside the container.
-#   
-#   If you want to provide additional cmake parameters than the ones given here,
-#   simply execute cmake twice:
-#       $ ldmx-cmake
-#       $ ldmx cmake -DBUILD_TESTS=ON ..
-###############################################################################
-alias ldmx-cmake='ldmx cmake -DONNXRUNTIME_ROOT=/deps/onnxruntime -DBUILD_EVE=OFF -DCMAKE_INSTALL_PREFIX=../install ../'
