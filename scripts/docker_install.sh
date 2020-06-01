92597712021824ed5265f049b1fca63bca3d3492
Tom E <tom.eichlersmith@gmail.com>
2020-06-01 11:35:50 -0500

    operational production image

---
+++ scripts/docker_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+###############################################################################
+# docker_install.sh
+#   This bash script is meant to run as the installation script for ldmx-sw
+#   while building the production docker image.
+###############################################################################
+
+# setup environment within container
+source $G4DIR/bin/geant4.sh
+source $ROOTDIR/bin/thisroot.sh
+
+# go to source directory
+cd /code
+
+mkdir build
+cd build
+
+# configure the build
+#   leave install prefix undefined so it ends up in PATH naturally
+cmake \
+    -DXercesC_DIR=$XercesC_DIR \
+    -DONNXRUNTIME_ROOT=$ONNX_DIR \
+    ..
+
+# build and install
+make install
+
