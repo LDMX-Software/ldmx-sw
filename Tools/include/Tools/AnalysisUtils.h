f766d9f45bf5ba876363d283c9ca6346ed51cdaf
omar-moreno <email@omarmoreno.net>
2020-06-05 17:55:30 -0700

    Remove deprecated method. Add energy threshold to PN gamma search.

--- Tools/include/Tools/AnalysisUtils.h
+++ Tools/include/Tools/AnalysisUtils.h
@@ -32,8 +32,9 @@ namespace ldmx {
         /**
          * Find and return the sim particle associated with the recoil electron.
          *
-         * @param particleMap map of sim particles
-         * @return pointer to sim particle labeled as recoil electron (nullptr if not found)
+         * @param[in] particleMap map of sim particles
+         *
+         * @return[out] Pointer to sim particle labeled as recoil electron (nullptr if not found)
          */
         std::tuple<int, const SimParticle*> getRecoil(const std::map<int,SimParticle> &particleMap);
 
@@ -41,22 +42,19 @@ namespace ldmx {
          * Get a pointer to the sim particle associated with the photon that
          * underwent a photo-nuclear reaction.
          *
-         * Returns the first particle in the map that underwent a PN interaction.
-         * If more than one particle undergoes a PN interaction, this will NOT notice.
+         * Returns the first particle in the map that underwent a PN interaction
+         * and has an energy above the threshold. If more than one particle 
+         * satisfied this condition, this will NOT notice.
          *
-         * @param particleMap map of sim particles
-         * @return pointer to sim particle labeled as PN Gamma photon (nullptr if not found)
-         */
-        const SimParticle* getPNGamma(const std::map<int,SimParticle> &particleMap);
-
-        /**
-         * Get a pointer to the sim particle associated with the photon daughter
-         * of the recoil electron that underwent a photo-nuclear reaction.
+         * @param[in] particleMap Map of sim particles
+         * @param[in] recoil The recoil electron 
+         * @param[in] energyThreshold The energy that the photon energy must be 
+         *      greater than.
          *
-         * @param particleMap map of sim particles
-         * @return pointer to sim particle (nullptr if not found)
+         * @return[out] Pointer to sim particle labeled as PN Gamma photon (nullptr if not found)
          */
-        const SimParticle* getRecoilPNGamma(const std::map<int,SimParticle> &particleMap);
+        const SimParticle* getPNGamma(const std::map<int,SimParticle>& particleMap, 
+               const SimParticle* recoil, const float& energyThreshold);
 
         /**
          * Sort tracks depending on how finable they are.
