87df890a00706963cb2538ae0d9f2e31cb3e5279
Tom E <tom.eichlersmith@gmail.com>
2020-06-16 07:18:10 -0500

    remove need for map by using toupper

--- Framework/include/Framework/NtupleManager.h
+++ Framework/include/Framework/NtupleManager.h
@@ -11,13 +11,7 @@
 #include <string>
 #include <typeinfo>
 #include <unordered_map>
-
-/*~~~~~~~~~~~*/
-/*   Boost   */
-/*~~~~~~~~~~~*/
-#include "boost/assign/list_of.hpp"
-#include "boost/variant.hpp"
-#include "boost/unordered_map.hpp"
+#include <variant>
 
 /*~~~~~~~~~~*/
 /*   ROOT   */
@@ -29,13 +23,7 @@
 /*~~~~~~~~~~*/
 #include "Exception/Exception.h" 
 
-typedef boost::variant< short, int, float, double, long > vtype; 
-
 namespace ldmx {
-
-    struct cmpStr {
-        bool operator()(char const *a, char const *b) const { return std::strcmp(a, b) < 0; }
-    };
 
     /**
      * @class NtupleManager
@@ -88,11 +76,18 @@ namespace ldmx {
                 variables_[vname] = T(std::numeric_limits<T>::min()); 
 
                 // Get the type name
-                auto typeName = typeid(boost::get<T>(variables_[vname])).name(); 
+                std::string typeName = typeid(std::get<T>(variables_[vname])).name();
+
+                //  The type name in C++ for basic types are lowercase:
+                //      s, i, f, d, l
+                //  while in ROOT it is uppercase:
+                //      S, I, F, D, L
+                //  so we need the call to toupper
+                for ( char& c : typeName ) c = toupper(c);
 
                 // Add the variable to the ROOT tree
-                trees_[tname]->Branch(vname.c_str(), &boost::get<T>(variables_[vname]), 
-                        (vname + "/" + rtype_.at(typeName)).c_str()); 
+                trees_[tname]->Branch(vname.c_str(), &std::get<T>(variables_[vname]), 
+                        (vname + "/" + typeName).c_str()); 
             } 
 
             /**
@@ -136,21 +131,20 @@ namespace ldmx {
 
         private: 
 
+            /// The variables these trees can hold
+            typedef std::variant<
+                short,
+                int,
+                float,
+                double,
+                long
+                > variable_type;
+
             /// Container for ROOT trees 
             std::unordered_map< std::string, TTree* > trees_; 
 
             /// Container for tree leaves 
-            std::unordered_map< std::string, vtype > variables_; 
-
-            /// Map from variable type to string representation used by ROOT.
-            const std::map< const char*, std::string, cmpStr> rtype_ = 
-            { 
-                {"s", "S"}, 
-                {"i", "I"},
-                {"f", "F"},
-                {"d", "D"},
-                {"l", "L"},
-            };
+            std::unordered_map< std::string, variable_type > variables_; 
 
             /// Private constructor to prevent instantiation 
             NtupleManager(); 
