c106ba38a4b47fc2b2d60e59854a5b4b4769dff8
Tom E <tom.eichlersmith@gmail.com>
2020-06-16 09:15:52 -0500

    update DQM python to new histogram interface

--- DQM/python/recoilTracker.py
+++ DQM/python/recoilTracker.py
@@ -16,19 +16,19 @@ from LDMX.Framework import ldmxcfg
 
 recoilTracker = ldmxcfg.Analyzer("RecoilTracker", "ldmx::RecoilTrackerDQM")
 
-recoilTracker.build1DHistogram("track_count", "Track Multiplicity", 10, 0, 10)
-recoilTracker.build1DHistogram("loose_track_count", "Track Multiplicity", 10, 0, 10)
-recoilTracker.build1DHistogram("axial_track_count", "Track Multiplicity", 10, 0, 10)
+recoilTracker.buildHistogram("track_count", "Track Multiplicity", 10, 0, 10)
+recoilTracker.buildHistogram("loose_track_count", "Track Multiplicity", 10, 0, 10)
+recoilTracker.buildHistogram("axial_track_count", "Track Multiplicity", 10, 0, 10)
 
-recoilTracker.build1DHistogram("recoil_vx", "Recoil e^{-} Vertex x (mm)", 120, -30, 30) 
-recoilTracker.build1DHistogram("recoil_vy", "Recoil e^{-} Vertex y (mm)", 200, -100, 100) 
-recoilTracker.build1DHistogram("recoil_vz", "Recoil e^{-} Vertex z (mm)", 40, -2, 0)
+recoilTracker.buildHistogram("recoil_vx", "Recoil e^{-} Vertex x (mm)", 120, -30, 30) 
+recoilTracker.buildHistogram("recoil_vy", "Recoil e^{-} Vertex y (mm)", 200, -100, 100) 
+recoilTracker.buildHistogram("recoil_vz", "Recoil e^{-} Vertex z (mm)", 40, -2, 0)
 
 titles = ['', '_track_veto', '_bdt', '_hcal', '_track_bdt', '_vetoes']
 for t in titles: 
-    recoilTracker.build1DHistogram("tp%s" % t,  "Recoil e^{-} Truth p (MeV)", 255, -50, 2500)
-    recoilTracker.build1DHistogram("tpt%s" % t, "Recoil e^{-} Truth p_{t} (MeV)", 300, -50, 100)
-    recoilTracker.build1DHistogram("tpx%s" % t, "Recoil e^{-} Truth p_{x} (MeV)", 100, -10, 10)
-    recoilTracker.build1DHistogram("tpy%s" % t, "Recoil e^{-} Truth p_{y} (MeV)", 100, -10, 10)
-    recoilTracker.build1DHistogram("tpz%s" % t, "Recoil e^{-} Truth p_{z} (MeV)", 260, -100, 2500)
+    recoilTracker.buildHistogram("tp%s" % t,  "Recoil e^{-} Truth p (MeV)", 255, -50, 2500)
+    recoilTracker.buildHistogram("tpt%s" % t, "Recoil e^{-} Truth p_{t} (MeV)", 300, -50, 100)
+    recoilTracker.buildHistogram("tpx%s" % t, "Recoil e^{-} Truth p_{x} (MeV)", 100, -10, 10)
+    recoilTracker.buildHistogram("tpy%s" % t, "Recoil e^{-} Truth p_{y} (MeV)", 100, -10, 10)
+    recoilTracker.buildHistogram("tpz%s" % t, "Recoil e^{-} Truth p_{z} (MeV)", 260, -100, 2500)
 
