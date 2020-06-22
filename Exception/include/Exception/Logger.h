ab2598679d39fca016a031caa1f55c756ffbb42b
omar-moreno <email@omarmoreno.net>
2020-06-22 16:38:04 -0700

    operational with main function

--- Exception/include/Exception/Logger.h
+++ Exception/include/Exception/Logger.h
@@ -19,17 +19,30 @@
 #include <boost/log/core.hpp> //core logging service
 #include <boost/log/sinks/sync_frontend.hpp> //syncronous sink frontend
 #include <boost/log/sinks/text_ostream_backend.hpp> //output stream sink backend
-#include <boost/log/trivial.hpp> //for trivial logging levels
-#include <boost/log/sources/logger.hpp> //for our logger type
+#include <boost/log/sources/severity_feature.hpp> //for the severity feature in a logger
+#include <boost/log/sources/severity_channel_logger.hpp> //for the severity logger
 #include <boost/log/sources/global_logger_storage.hpp> //for global logger default
-
-#include <boost/log/expressions.hpp>
-#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/expressions.hpp> //for attributes and expressions
 #include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
+
+//TODO check which headers are required
+#include <boost/log/utility/setup/file.hpp>
 #include <boost/log/sources/record_ostream.hpp>
 
 namespace ldmx {
+
+    /**
+     * Severity/Logging levels
+     *
+     * Out in this namespace so its easier for everyone to use.
+     */
+    enum level {
+        debug = 0, //0
+        info,  //1
+        warn,  //2 -> default
+        error, //3
+        fatal  //4 
+    };
 
     namespace logging {
 
@@ -38,50 +51,41 @@ namespace ldmx {
          */
         namespace log = boost::log;
         namespace sinks = boost::log::sinks;
-        namespace level = log::trivial;
+
 
         /**
          * Define the type of logger we will be using in ldmx-sw
          */
-        typedef log::sources::logger_mt logger;
+        typedef log::sources::severity_channel_logger_mt< level, std::string > logger;
+
+        /**
+         * Gets a logger for the user
+         *
+         * Returns a logger type with some extra initialization procedures done.
+         * Should _only be called ONCE_ during a run.
+         *
+         * @param name name of this logging channel (e.g. processor name)
+         * @return logger with the input channel name
+         */
+        logger makeLogger( const std::string& name );
     
         /**
-         * Initialize the logging
+         * Initialize the logging backend
          *
          * This function setups up the terminal and file sinks.
          * Sets their format and filtering level for this run.
          *
-         * Boost Logging Trivial Levels:
-         * logging::level::
-         *      trace
-         *      debug
-         *      info
-         *      warning
-         *      error
-         *      fatal
-         *
-         * @param termV logging level to output to terminal
-         * @param fileV logging level to output to file
-         * @param fileName name of log file if file logging is enabled
-         * 
-         * Defaults to batch setup where only fatal messages are allowed through the filters.
+         * @param loggingArgs list of command line parameters passed
          */
-        void open(int termV, int fileV, const std::string& fileName = "");
+        void open(const std::vector<std::string>& loggingArgs);
 
         /**
          * Close up the logging
-         *
-         * TODO need this before end of main()
          */
         void close();
 
     } //logging
 
 } //ldmx
-
-/**
- * Declare a global logger for all non-EventProcessors
- */
-BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT( ldmx_log , ldmx::logging::logger )
 
 #endif //EXCEPTION_LOGGER_H
