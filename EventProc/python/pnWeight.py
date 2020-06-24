bf0200aa546f8ba8a66912b31cabfda267466bda
Tom E <tom.eichlersmith@gmail.com>
2020-06-24 10:31:30 -0500

    configuration for PN weight processor

---
+++ EventProc/python/pnWeight.py
@@ -0,0 +1,23 @@
+"""Configuration for processing pn weights"""
+
+from LDMX.Framework import ldmxcfg
+
+class PnWeightProcessor(ldmxcfg.Producer) :
+    """Process the PN Weights into something more helpful
+
+    Attributes
+    ----------
+    w_threshold : float
+        Threshold above which to apply reweighting [MeV]
+    theta_threshold : float
+        Minimum angle for backwards going hadrons [degrees]
+    """
+
+    def __init__(self,name='pnWeight') :
+        super().__init__(name , 'ldmx::PnWeightProcessor')
+
+        from LDMX.EventProc import include
+        include.librar()
+
+        self.w_threshold     = 1150.
+        self.theta_threshold = 100.
