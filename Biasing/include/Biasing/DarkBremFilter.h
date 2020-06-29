b04081c55562f7d86bab79971e538cbf3c6d1df1
Tom E <tom.eichlersmith@gmail.com>
2020-06-29 11:29:47 -0500

    update header include names

--- Biasing/include/Biasing/DarkBremFilter.h
+++ Biasing/include/Biasing/DarkBremFilter.h
@@ -32,6 +32,22 @@
 
 namespace ldmx {
 
+    /**
+     * @class DarkBremFilter
+     *
+     * This class is meant to filter for events that produce a dark brem matching
+     * the input parameters:
+     *      volume: A' originates inside of the input volume (target or ecal)
+     *      nGensFromPrimary: A' produced by electron removed from primary by <= input n generations
+     *
+     * The general idea for this filter is to force Geant4 to
+     * simulate the tracks in generation order. This is done by
+     * pushing all new tracks to the waiting stack and incrementing
+     * the current generation by one when the urgent stack is empty.
+     * Checking if the A' is produced is done in the PostTrackingAction,
+     * and the event is aborted if we reach a generation later than the input
+     * generation and no A' has been found.
+     */
     class DarkBremFilter : public UserAction {
 
         public:
@@ -46,56 +62,109 @@ namespace ldmx {
             /**
              * Class destructor.
              */
-            ~DarkBremFilter();
+            ~DarkBremFilter() { }
 
             /**
              * Get the types of actions this class can do
              */
             std::vector< TYPE > getTypes() final override {
-                return { TYPE::STACKING , TYPE::STEPPING };
+                return { TYPE::STACKING , TYPE::EVENT , TYPE::TRACKING };
             }
 
             /**
-             * Kills events that don't contain a DarkBrem produced by the primary electron within the desired volume.
-             *
-             * @param step The Geant4 step.
+             * Reset generation counter and flag on if A' has been found
              */
-            void stepping(const G4Step* step);
+            void BeginOfEventAction(const G4Event* ) final override;
 
             /**
              * Classify a new track which postpones track processing.
-             * Track processing resumes normally if an interaction occurred.
+             *
+             * Push all new tracks to fWaiting
+             *
+             * This forces all tracks in a given generation to be simulated
+             * before the next generation of tracks is started.
+             *
+             * Checks a new track for being an A'
+             *  if it is an A', checks if it is in correct volume and sets foundAp_ member
+             * 
              * @param aTrack The Geant4 track.
              * @param currentTrackClass The current track classification.
              */
             G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track* aTrack, 
-                    const G4ClassificationOfNewTrack& currentTrackClass) final override;
+                    const G4ClassificationOfNewTrack& currentTrackClass) final override; 
+
+            /**
+             * Increment the generation counter
+             *
+             * This function is called when the urgent stack is empty
+             * and the waiting stack is transferred to the urgent stack.
+             *
+             * With all new tracks being pushed to the waiting stack,
+             * this only occurs when a new generation has begun.
+             *
+             * When a new generation has begun, if the new generation is more
+             * than the input generation limit, we check if the A' was found.
+             */
+            void NewStage() final override;
+
+            /**
+             * Make sure A' is saved.
+             *
+             * If passed track is A', set save status to true
+             * Aborts event if A' does not originate in desired volume
+             */
+            void PostUserTrackingAction(const G4Track* ) final override;
 
         private:
 
             /**
-             * Checks if the secondaries given has an A Prime.
+             * Check if input volume is in the desired volume name
              *
-             * @param secondaries list to search
-             * @return true if A Prime in secondaries
+             * @param pointer to track to check
+             * @return true if originated in desired volume
              */
-            bool hasAPrime(const G4TrackVector *secondaries) const;
+            bool inDesiredVolume(const G4Track* ) const;
 
         private:
-            
-            /** 
-             * Level of verbosity for this filter
-             *
-             * Parameter Name: 'verbosity'
-             */
-            int verbosity_;
 
             /** 
-             * The volume that the filter will be applied to.
+             * The volumes that the filter will be applied to.
+             *
+             * Default: 'target'
              *
              * Parameter Name: 'volume'
+             *  Searched for in PhysicalVolumeStore
              */
-            std::string volumeName_;
+            std::vector< G4LogicalVolume* > volumes_;
+
+            /**
+             * Number of generations away from primary
+             * to allow to dark brem.
+             *
+             * Default: 0 (only primary itself)
+             *
+             * Parameter Name: 'nGensFromPrimary'
+             */
+            int nGensFromPrimary_;
+
+            /**
+             * Minimum energy [MeV] that the A' should have to keep the event.
+             * 
+             * Default: 0 (allow all energies)
+             *
+             * Parameter Name: 'minApEnergy'
+             */
+            double minApEnergy_;
+
+            /**
+             * Have we found the A' yet?
+             */
+            bool foundAp_;
+
+            /**
+             * The current generation removed from the primary electron
+             */
+            int currentGen_;
 
     }; // DarkBremFilter
 }
