956f4a721b0c89e0f195c656b33c43d3cf4800c6
Tom E <tom.eichlersmith@gmail.com>
2020-07-01 16:03:06 -0500

    don't update submodules when installing mag field map

--- cmake/Scripts/InstallMagFieldMap.cmake
+++ cmake/Scripts/InstallMagFieldMap.cmake
@@ -1,6 +1,4 @@
 message(STATUS "Installing field map files ...")
-execute_process(COMMAND git submodule init WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/../..)
-execute_process(COMMAND git submodule update WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/../..)
 file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/data/fieldmap)
 file(GLOB fieldmap_files ${CMAKE_CURRENT_LIST_DIR}/../../MagFieldMap/*.tar.gz)
 foreach(fieldmap_file ${fieldmap_files})
