0b615a4bf72ba0c772ecccd781ec7bffb7bbc872
Tom E <tom.eichlersmith@gmail.com>
2020-07-02 12:06:18 -0500

    logging configurable from python and in Process

--- Framework/src/fire.cxx
+++ Framework/src/fire.cxx
@@ -1,6 +1,3 @@
-/** @file ldmx-app.cxx
- * Definition of ldmx-app executable main
- */
 
 //----------------//
 //   C++ StdLib   //
@@ -35,6 +32,12 @@ if (p) p->requestFinish();
 }
 */
 
+/**
+ * @func printUsage
+ *
+ * Print how to use this executable to the terminal.
+ */
+void printUsage();
 
 /**
  * @mainpage
@@ -51,8 +54,8 @@ if (p) p->requestFinish();
 int main(int argc, char* argv[]) {
 
     if (argc < 2) {
-        printf("Usage: ldmx-app [application arguments] {configuration_script.py} [arguments to configuration script]\n");
-        return 0;
+        printUsage();
+        return 1;
     }
 
     int ptrpy = 1;
@@ -62,45 +65,53 @@ int main(int argc, char* argv[]) {
     }
 
     if (ptrpy == argc) {
-        printf("Usage: ldmx-app [application arguments] {configuration_script.py} [arguments to configuration script]\n");
-        printf("  ** No python script provided. **\n");
-        return 0;
+        printUsage();
+        std::cout << " ** No python configuration script provided (must end in '.py'). ** " << std::endl;
+        return 1;
     }
 
     ProcessHandle p;
+    std::cout << "---- LDMXSW: Loading configuration --------" << std::endl;
     try {
-        std::cout << "---- LDMXSW: Loading configuration --------" << std::endl;
         
         ConfigurePython cfg(argv[ptrpy], argv + ptrpy + 1, argc - ptrpy - 1);
         p = cfg.makeProcess();
-
-        std::cout << "---- LDMXSW: Configuration load complete  --------" << std::endl;
-
-        // If Ctrl-c is used, immediately exit the application.
-        struct sigaction act;
-        memset (&act, '\0', sizeof(act));
-        if (sigaction(SIGINT, &act, NULL) < 0) {
-            perror ("sigaction");
-            return 1;
-        }
-
-        // See comment above for reason why this code is commented out.
-        /* Use the sa_sigaction field because the handles has two additional parameters */
-        //act.sa_sigaction = &softFinish;
-
-        /* The SA_SIGINFO flag tells sigaction() to use the sa_sigaction field, not sa_handler. */
-        //act.sa_flags = SA_SIGINFO;
-
-        std::cout << "---- LDMXSW: Starting event processing --------" << std::endl;
-        
-        p->run();
-        
-        std::cout << "---- LDMXSW: Event processing complete  --------" << std::endl;
-
     } catch (Exception& e) {
-        std::cerr << "Framework Error [" << e.name() << "] : " << e.message() << std::endl;
+        std::cerr << "Configuration Error [" << e.name() << "] : " << e.message() << std::endl;
         std::cerr << "  at " << e.module() << ":" << e.line() << " in " << e.function() << std::endl;
+        return 1;
     }
+
+    std::cout << "---- LDMXSW: Configuration load complete  --------" << std::endl;
+
+    // If Ctrl-c is used, immediately exit the application.
+    struct sigaction act;
+    memset (&act, '\0', sizeof(act));
+    if (sigaction(SIGINT, &act, NULL) < 0) {
+        perror ("sigaction");
+        return 1;
+    }
+
+    // See comment above for reason why this code is commented out.
+    /* Use the sa_sigaction field because the handles has two additional parameters */
+    //act.sa_sigaction = &softFinish;
+
+    /* The SA_SIGINFO flag tells sigaction() to use the sa_sigaction field, not sa_handler. */
+    //act.sa_flags = SA_SIGINFO;
+
+    std::cout << "---- LDMXSW: Starting event processing --------" << std::endl;
+    
+    //process has its own try-catch branch for any exceptions
+    p->run();
+    
+    std::cout << "---- LDMXSW: Event processing complete  --------" << std::endl;
 
     return 0;
 }
+
+void printUsage() {
+    std::cout << "Usage: fire {configuration_script.py} [arguments to configuration script]" << std::endl;
+    std::cout << "     configuration_script.py  (required) python script to configure the processing" << std::endl;
+    std::cout << "     arguments                (optional) passed to configuration script when run in python" << std::endl;
+}
+
