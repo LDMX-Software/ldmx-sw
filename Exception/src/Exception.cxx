05b343e94c9e5e9875743b9a25e99081278b67e8
omar-moreno <email@omarmoreno.net>
2020-07-06 13:26:00 -0700

    Remove Exception module.

--- Exception/src/Exception.cxx
+++
@@ -1,6 +0,0 @@
-
-#include "Exception/Exception.h"
-
-// This file is required so that a library for this module is created by make which can be linked
-// in the same way that all the other modules are linked. It is easier to have this extra source
-// file than it is to compile/link this part differently to the program.
