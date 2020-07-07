0410b57ecb05ec6a53042039d04b61b7ee00e270
omar-moreno <email@omarmoreno.net>
2020-07-07 11:46:47 -0700

    Update location of Exception in unit tests.

---
+++ test/ldmx_test.cxx
@@ -0,0 +1,12 @@
+/**
+ * This define statement tells the Catch2 code to pull together
+ * all of the macros throughout ldmx-sw and compile them into a
+ * single executable here.
+ *
+ * Check out the Catch2 [documentation](https://github.com/catchorg/Catch2/blob/master/docs/tutorial.md#top) to learn more about how to write a test.
+ *
+ * There can only be *one* instance of this definition.
+ * I have chosen to put it here for now.
+ */
+#define CATCH_CONFIG_MAIN
+#include "Framework/catch.hpp"
