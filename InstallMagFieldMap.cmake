54ec1bece270cd270c035e1c7606e094c2e840dc
omar-moreno <email@omarmoreno.net>
2020-07-13 12:02:14 -0700

    Macro used to isntall the B field map.

---
+++ InstallMagFieldMap.cmake
@@ -0,0 +1,7 @@
+message(STATUS "Installing field map files ...")
+file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/data/fieldmap)
+file(GLOB fieldmap_files ${CMAKE_CURRENT_LIST_DIR}/../../MagFieldMap/*.tar.gz)
+foreach(fieldmap_file ${fieldmap_files})
+  execute_process(COMMAND tar -C ${CMAKE_INSTALL_PREFIX}/data/fieldmap -zxvf ${fieldmap_file})
+endforeach()
+message(STATUS "Done installing field map files.")
