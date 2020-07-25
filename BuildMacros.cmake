9c21178700b09bae9683fd909f488f2c274a75af
omar-moreno <email@omarmoreno.net>
2020-07-25 00:00:04 -0700

    Clean up python directory installation.

--- BuildMacros.cmake
+++ BuildMacros.cmake
@@ -72,7 +72,7 @@ endmacro()
 
 macro(setup_library)
 
-    set(oneValueArgs name)
+    set(oneValueArgs name python_install_path)
     set(multiValueArgs dependencies sources)
     cmake_parse_arguments(setup_library "${options}" "${oneValueArgs}"
                           "${multiValueArgs}" ${ARGN} )
@@ -106,22 +106,19 @@ macro(setup_library)
     # If the python directory exists, initialize the package and copy over the 
     # python modules.
     if (EXISTS ${PROJECT_SOURCE_DIR}/python)
-        file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/python/__init__.py 
-             "\"\"\"Python module to configure the LDMX module ${setup_library_name}\"\"\"")
+       
+        # Initilaize the python package
+        file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/python/__init__.py "") 
         install(FILES ${CMAKE_CURRENT_BINARY_DIR}/python/__init__.py 
-                DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/python/LDMX/${setup_library_name})
+                DESTINATION ${setup_library_python_install_path}/${setup_library_name})
 
-        # write an include file for this module
-        file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/python/include.py
-             "\"\"\"Include this module\"\"\"\ndef library() :\n\ \ \ \ \"\"\"Attach the name of ${setup_library_name} library to the process\"\"\"\n\ \ \ \ from LDMX.Framework.ldmxcfg import Process\n\ \ \ \ Process.addLibrary('${CMAKE_INSTALL_PREFIX}/lib/lib${setup_library_name}.so')")
-        install(FILES ${CMAKE_CURRENT_BINARY_DIR}/python/include.py DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/python/LDMX/${setup_library_name})
-        # install python scripts
+        # Install the python modules
         file(GLOB py_scripts CONFIGURE_DEPENDS ${PROJECT_SOURCE_DIR}/python/*.py)
         foreach(pyscript ${py_scripts})
             string(REPLACE ".in" "" script_output ${pyscript})
             get_filename_component(script_output ${script_output} NAME)
             configure_file(${pyscript} ${CMAKE_CURRENT_BINARY_DIR}/python/${script_output})
-            install(FILES ${CMAKE_CURRENT_BINARY_DIR}/python/${script_output} DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/python/LDMX/${setup_library_name})
+            install(FILES ${CMAKE_CURRENT_BINARY_DIR}/python/${script_output} DESTINATION ${setup_library_python_install_path}/${setup_library_name})
         endforeach()
 
     endif()
