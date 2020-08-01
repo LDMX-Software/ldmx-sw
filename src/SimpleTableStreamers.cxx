9bf92d0b449d891181facf76457d326e006920df
Jeremiah Mans <jmmans@umn.edu>
2020-08-01 13:55:05 -0500

    CSV peristence works, except not yet able to load files with id columns but invalid DetID

--- src/SimpleTableStreamers.cxx
+++ src/SimpleTableStreamers.cxx
@@ -14,8 +14,9 @@ namespace ldmx {
 		s << ",\"" << field->getFieldName() << '"';
 	    }
 	}
-    
-	void SimpleTableStreamerCSV::store(const IntegerTableCondition& t, std::ostream& s, bool expandIds) {
+
+	template <class T, class V>
+	void storeT(const T& t, std::ostream& s, bool expandIds) {
 	    char buffer[100];
 	    // write the header line
 	    s << "\"DetID\"";
@@ -28,48 +29,26 @@ namespace ldmx {
 	    s << std::endl;
 	    // write the data rows
 	    for (unsigned int irow=0; irow<t.getRowCount(); irow++) {
-		std::pair<unsigned int, std::vector<int> > row = t.getRow(irow);
+		std::pair<unsigned int, std::vector<V> > row = t.getRow(irow);
 		// write the id in hex
 		s << boost::format("0x%08x") % row.first;
 		if (expandIds) {
 		    DetectorIDInterpreter did(row.first);
 
 		    for (int i=0; i<did.getFieldCount(); i++) 
-			s << ',' << did.getFieldValue(i);
+			s << ',' << std::setprecision(10) << did.getFieldValue(i);
 		}
 		for (auto col : row.second) 
 		    s << ',' << col;      
 		s << std::endl;      
 	    }
+	}
+	void SimpleTableStreamerCSV::store(const IntegerTableCondition& t, std::ostream& s, bool expandIds) {
+	    storeT<IntegerTableCondition,int>(t,s,expandIds);
 	}
 	void SimpleTableStreamerCSV::store(const DoubleTableCondition& t, std::ostream& s, bool expandIds) {
-	    char buffer[100];
-	    // write the header line
-	    s << "\"DetId\"";
-	    if (expandIds && t.getRowCount()>0) {
-		storeIdFields(t.getRowId(0),s);
-	    }
-	    for (auto name: t.getColumnNames()) {
-		s << ",\"" << name << "\"";
-	    }
-	    s << std::endl;
-	    // write the data rows
-	    for (unsigned int irow=0; irow<t.getRowCount(); irow++) {
-		std::pair<unsigned int, std::vector<double> > row = t.getRow(irow);
-		// write the id in hex
-		s << boost::format("0x%08x") % row.first;
-		if (expandIds) {
-		    DetectorIDInterpreter did(row.first);
-
-		    for (int i=0; i<did.getFieldCount(); i++) 
-			s << ',' << std::setprecision(10) <<  did.getFieldValue(i);
-		}
-		for (auto col : row.second) 
-		    s << ',' << col;      
-		s << std::endl;      
-	    }
+	    storeT<DoubleTableCondition,double>(t,s,expandIds);
 	}
- 
       
 	static std::vector<std::string> splitCSV(const std::string& s) {
 	    std::vector<std::string> rv;
@@ -79,7 +58,10 @@ namespace ldmx {
 		if (chr=='"') {
 		    inquote=!inquote;
 		} else if ((chr==',' || chr=='\t') && !inquote) {
-		    if (!field.empty()) rv.push_back(field);
+		    if (!field.empty()) {
+			//			std::cout << "Field: '" << field << "'\n";
+			rv.push_back(field);
+		    }
 		    field.clear();
 		} else if (isspace(chr) && !inquote) { // do not add spaces		    
 		} else if (chr=='#' && !inquote) {
@@ -88,7 +70,10 @@ namespace ldmx {
 		    field+=chr;
 		}	
 	    }
-	    if (!field.empty()) rv.push_back(field);
+	    if (!field.empty()) {
+		rv.push_back(field);
+		//		std::cout << "Field: '" << field << "'\n";
+	    }
 	    return rv;
 	}
 	static std::vector<std::string>::const_iterator find(const std::vector<std::string>& v, const std::string& a) {
@@ -97,23 +82,29 @@ namespace ldmx {
 	    }
 	    return v.end();
 	}
-    
-	void SimpleTableStreamerCSV::load(IntegerTableCondition& table, std::istream& is) {
+
+	template <class T, class V>
+	    void loadT(T& table, std::istream& is) {
 	    table.clear();
 	    // first work with the header line
 	    std::string line;
 	    std::vector<std::string> split;
 	    int iDetID=-1;
-
+	    int iline=0;
+	    size_t ncolin;
+	    
 	    while (!is.eof()) {
+		iline++;
 		std::getline(is,line);
 		split=splitCSV(line);
-		if (split.size()<1+table.getColumnCount()) {
+		if (split.size()<1) {
 		    split.clear();
 		    continue; // need at least an id column and as many columns as requested
 		}
 		if (find(split,"DetID")==split.end() && find(split,"subdetector")==split.end()) {
 		    EXCEPTION_RAISE("ConditionsException","Malformed CSV file with no DetId or subdetector column");
+		} else {
+		    break;
 		}
 	    }
 	    if (is.eof()) {
@@ -124,9 +115,45 @@ namespace ldmx {
 	    if (id!=split.end()) {
 		// good this is simpler...
 		iDetID=int(id-split.begin());
+	    } else {
+		EXCEPTION_RAISE("MissingFeatureException","Cannot actually load CSV file without valid DetID column at this point");
+	    }
+	    ncolin=split.size();
+	    // check for columns which match all those requested in the table
+	    std::map<unsigned int, int> table_to_csv;
+	    for (unsigned int ic=0; ic!=table.getColumnCount(); ic++) {
+		auto fc = find(split,table.getColumnName(ic));
+		if (fc==split.end()) {
+		    EXCEPTION_RAISE("ConditionsException","Missing column '"+table.getColumnName(ic)+"' in CSV table load");
+		}
+		table_to_csv[ic]=int(fc-split.begin());
+	    }
+	    
+	    // processing additional lines
+	    while (!is.eof()) {
+		iline++;
+		std::getline(is,line);
+		split=splitCSV(line);
+		if (split.size()==0) continue; // ignore comment lines
+		if (split.size()!=ncolin) {
+		    EXCEPTION_RAISE("ConditionsException","Mismatched number of columns (" +
+				    std::to_string(split.size()) + "!=" + std::to_string(ncolin) + ") on line " + std::to_string(iline));
+		}
+		unsigned int id(0);
+		std::vector<V> values(table_to_csv.size(),0);
+		if (iDetID>=0) id=strtoul(split[iDetID].c_str(),0,0);
+		for (auto icopy : table_to_csv) {
+		    values[icopy.first]=strtoul(split[icopy.second].c_str(),0,0);		   
+		}
+		if (id!=0) {
+		    table.add(id,values);
+		}
 	    }
 	}
-    
+	
+	void SimpleTableStreamerCSV::load(IntegerTableCondition& table, std::istream& is) {
+											 loadT<IntegerTableCondition,int>(table,is);
+											 }
     }  
 }
 
