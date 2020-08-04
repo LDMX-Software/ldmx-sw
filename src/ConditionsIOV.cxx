e1fbf938b5fb7a6cf1c9b9ae1cc038c472d241d6
Jeremiah Mans <jmmans@umn.edu>
2020-08-04 10:30:16 -0500

    Move check of validity out of header to remove requirement for EventHeader in users, add check of overlaps with other IOVs

--- src/ConditionsIOV.cxx
+++ src/ConditionsIOV.cxx
@@ -1,21 +1,46 @@
 #include "Framework/ConditionsIOV.h"
+#include "Event/EventHeader.h"
+
 #include <iostream>
 #include <sstream>
 
+std::ostream& operator<<(std::ostream& s, const ldmx::ConditionsIOV& iov) {
+    iov.stream(s);
+    return s;
+}  
+
 namespace ldmx {
-    void ConditionsIOV::Print() const {
-	std::cout << ToString() << std::endl;
+    
+    bool ConditionsIOV::validForEvent(const EventHeader& eh) const {
+	return (eh.getRun()>=firstRun_ || firstRun_==-1) && (eh.getRun()<=lastRun_ || lastRun_==-1) && ((eh.isRealData())?(validForData_):(validForMC_));
     }
 
-    std::string ConditionsIOV::ToString() const {
-	std::stringstream s;
+    bool ConditionsIOV::overlaps(const ConditionsIOV& iov) const {
+	if (iov.validForData_!=validForData_ && iov.validForMC_!=validForMC_) return false;
+	if (iov.firstRun_<lastRun_) return false; // starts after this IOV
+	if (iov.lastRun_<firstRun_) return false; // ends before this IOV
+	return true;
+    }
+
+    void ConditionsIOV::stream(std::ostream& s) const {
 	s << "IOV(" << firstRun_ << "->";
 	if (lastRun_==-1) s << "[all runs]";
 	else s << lastRun_;
 	if (validForData_) s << ",Data";
 	if (validForMC_) s << ",MC";
 	s << ")";
+    }  
 
+    
+    
+    void ConditionsIOV::Print() const {
+	stream(std::cout);
+	std::cout << std::endl;
+    }
+    
+    std::string ConditionsIOV::ToString() const {
+	std::stringstream s;
+	stream(s);
 	return s.str();
     }
 
