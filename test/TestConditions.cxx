244464275163b71f80aeec7a8b5e52fb1d51329b
Jeremiah Mans <jmmans@umn.edu>
2020-08-04 10:35:37 -0500

    Working, tested for integer and double simple csv tables stored in both http and files

--- test/TestConditions.cxx
+++ test/TestConditions.cxx
@@ -2,21 +2,45 @@
 
 #include "Conditions/SimpleTableCondition.h"
 #include "Conditions/SimpleTableStreamers.h"
+#include "Conditions/SimpleCSVTableProvider.h"
+#include "Framework/ConfigurePython.h"
 #include "DetDescr/EcalID.h"
+#include "DetDescr/HcalID.h"
 #include <sstream>
+#include <fstream>
+#include <stdlib.h>
 
 namespace ldmx {
   namespace test {
 
-
-    void matchesAll(const ldmx::IntegerTableCondition&a, const ldmx::IntegerTableCondition& b) {
+    template <class T>
+    void matchesMeta(const T& a, const T& b) {
       REQUIRE( a.getColumnCount() == b.getColumnCount() );
       REQUIRE( a.getRowCount() == b.getRowCount() );
       REQUIRE( a.getColumnNames() == b.getColumnNames() );
+
+    }
+
+    void matchesAll(const ldmx::DoubleTableCondition& a, const ldmx::DoubleTableCondition& b) {
+      matchesMeta(a,b);
+      for (unsigned int i=0; i<a.getRowCount(); i++) {
+	std::pair<unsigned int, std::vector<double>> ar=a.getRow(i);
+	std::pair<unsigned int, std::vector<double>> br=b.getRow(i);
+	REQUIRE( ar.first==br.first );
+	for (unsigned int ic=0; ic<a.getColumnCount(); ic++) {
+	  //	  std::cout << i << ',' << ic << "=>" << ar.second[ic] << " " << br.second[ic] << std::endl;
+	  REQUIRE( fabs(ar.second[ic]-br.second[ic])/std::max(1e-5,(ar.second[ic]+br.second[ic])/2) < 1e-5 ); // limited precision of course
+	}
+      }
+    }
+
+
+    void matchesAll(const ldmx::IntegerTableCondition& a, const ldmx::IntegerTableCondition& b) {
+      matchesMeta(a,b);
       for (unsigned int i=0; i<a.getRowCount(); i++)
 	REQUIRE( a.getRow(i) == b.getRow(i) );
     }
-    
+
     using Catch::Matchers::Contains;
     
     /**
@@ -45,6 +69,19 @@ namespace ldmx {
 	itable.add(id.raw(),vals);
       }
 
+      std::vector<std::string> columnsd({"SQRT","EXP","LOG"});
+      DoubleTableCondition dtable("DTable",columnsd);
+
+      for (int key=1; key<8; key+=2) {
+	HcalID id(1,1,key);
+	std::vector<double> vals;
+	vals.push_back(sqrt(key));
+	vals.push_back(exp(key*5));
+	vals.push_back(log(key));
+	dtable.add(id.raw(),vals);
+      }
+
+      
       SECTION("Testing simple table construction") {
 
 	
@@ -77,11 +114,11 @@ namespace ldmx {
 	ldmx::utility::SimpleTableStreamerCSV::store(itable, ss, true);
 	
 	std::string image1=ss.str();
-	const char* expected1="\"DetID\",\"subdetector\",\"layer\",\"module\",\"cell\",\"A\",\"Q\",\"V\"\n0x1402100a,5,1,1,10,20,5,100\n0x14021014,5,1,1,20,40,10,400\n0x1402101e,5,1,1,30,60,15,900\n0x14021028,5,1,1,40,80,20,1600\n0x14021032,5,1,1,50,100,25,2500\n0x1402103c,5,1,1,60,120,30,3600\n0x14021046,5,1,1,70,140,35,4900\n0x14021050,5,1,1,80,160,40,6400\n0x1402105a,5,1,1,90,180,45,8100\n0x14021064,5,1,1,100,200,50,10000\n";
+	const char* expected1="\"DetID\",id:\"subdetector\",id:\"layer\",id:\"module\",id:\"cell\",\"A\",\"Q\",\"V\"\n0x1402100a,5,1,1,10,20,5,100\n0x14021014,5,1,1,20,40,10,400\n0x1402101e,5,1,1,30,60,15,900\n0x14021028,5,1,1,40,80,20,1600\n0x14021032,5,1,1,50,100,25,2500\n0x1402103c,5,1,1,60,120,30,3600\n0x14021046,5,1,1,70,140,35,4900\n0x14021050,5,1,1,80,160,40,6400\n0x1402105a,5,1,1,90,180,45,8100\n0x14021064,5,1,1,100,200,50,10000\n";
 
 	CHECK( image1 == expected1 );
 	
-	std::cout << image1;
+	//std::cout << image1;
 
 	std::stringstream ss_read1(image1);
 	IntegerTableCondition itable2("ITable",columns);
@@ -104,6 +141,45 @@ namespace ldmx {
 	std::string image4("DetID,A,Q,V\n0x1402100a,20,5,100\n0x14021014,40,10\n0x1402101e,60,15,900\n");
 	std::stringstream ss_read4(image4);
 	REQUIRE_THROWS_WITH( ldmx::utility::SimpleTableStreamerCSV::load(itable2, ss_read4) , Contains("Mismatched number of columns (3!=4) on line 3") );
+      }
+
+      SECTION( "Testing file loading" ) {
+	std::ofstream fs("/tmp/dump_double.csv");
+	std::stringstream ss;
+	ldmx::utility::SimpleTableStreamerCSV::store(dtable, fs, true);
+		ldmx::utility::SimpleTableStreamerCSV::store(dtable, ss, true);
+	fs.close();
+	//	std::cout << "Step 1" << std::endl << ss.str();
+	
+        const char* cfg="#!/usr/bin/python\n\nimport sys\n\nfrom LDMX.Framework import ldmxcfg\nfrom LDMX.Conditions import SimpleCSVTableProvider\n\np=ldmxcfg.Process(\"test\")\np.testMode=True\ncolumns=[\"SQRT\",\"EXP\",\"LOG\"]\ncop=SimpleCSVTableProvider.SimpleCSVTableProvider(\"tableSource\",\"TEST_MODE\")\ncop.provideDoubleTable(\"test_table_file1\",\"file:///tmp/dump_double.csv\",columns)\ncop.provideDoubleTable(\"test_table_file2\",\"/tmp/dump_double.csv\",columns)\np.declareConditionsObjectProvider(cop)\np.libraries.append(\"libConditions.so\")\n";
+
+	FILE* f=fopen("/tmp/test_cond.py","w");
+	fputs(cfg,f);
+	fclose(f);
+
+	ldmx::ConfigurePython cp("/tmp/test_cond.py",0,0);
+	ldmx::ProcessHandle hp=cp.makeProcess();
+	EventHeader cxt;
+	
+	const DoubleTableCondition& fTable1=hp->getConditions().getCondition<DoubleTableCondition>("test_table_file1",cxt);
+	const DoubleTableCondition& fTable2=hp->getConditions().getCondition<DoubleTableCondition>("test_table_file2",cxt);
+	matchesAll(dtable,fTable1);
+	matchesAll(dtable,fTable2);
+      }
+
+      SECTION( "Testing HTTP loading" ) {
+        const char* cfg="#!/usr/bin/python\n\nimport sys\n\nfrom LDMX.Framework import ldmxcfg\nfrom LDMX.Conditions import SimpleCSVTableProvider\n\np=ldmxcfg.Process(\"test\")\np.testMode=True\ncolumns=[\"A\",\"Q\",\"V\"]\ncop=SimpleCSVTableProvider.SimpleCSVTableProvider(\"tableSource\",\"TEST_MODE\")\ncop.provideIntegerTable(\"test_table_http\",\"http://webusers.physics.umn.edu/~jmmans/test_table.csv\",columns)\np.declareConditionsObjectProvider(cop)\np.libraries.append(\"libConditions.so\")\n";
+
+	FILE* f=fopen("/tmp/test_cond.py","w");
+	fputs(cfg,f);
+	fclose(f);
+
+	ldmx::ConfigurePython cp("/tmp/test_cond.py",0,0);
+	ldmx::ProcessHandle hp=cp.makeProcess();
+	EventHeader cxt;
+	
+	const IntegerTableCondition& httpTable=hp->getConditions().getCondition<IntegerTableCondition>("test_table_http",cxt);
+	matchesAll(httpTable,itable);
       }
     }
   }
