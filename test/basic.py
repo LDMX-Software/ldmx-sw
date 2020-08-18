c9ae80612743a4c112fd1327cc5e9c2a3d7b4a6d
Tom E <tom.eichlersmith@gmail.com>
2020-08-18 13:24:18 -0500

    add basic simulation test file

---
+++ test/basic.py
@@ -0,0 +1,23 @@
+from LDMX.Framework import ldmxcfg
+# create my process object
+p = ldmxcfg.Process( "test" )
+# how many events to process?
+p.maxEvents = 10
+# we want to see every event
+p.logFrequency = 1
+p.termLogLevel = 0
+# we also only have an output file
+p.outputFiles = [ "justSim_" + str(p.maxEvents) + "_events.root" ]
+from LDMX.SimCore import simulator as sim
+mySim = sim.simulator( "mySim" )
+mySim.setDetector( 'ldmx-det-v12' )
+# Set a run number
+mySim.runNumber = 9001
+# Get a pre-written generator
+from LDMX.SimCore import generators as gen
+mySim.generators.append( gen.single_4gev_e_upstream_tagger() )
+# add your configured simulation to the sequence
+mySim.description = 'Basic test Simulation'
+mySim.randomSeeds = [ 1 , 2 ]
+
+p.sequence.append( mySim )
