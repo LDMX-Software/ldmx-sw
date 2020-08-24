f2214ac3c0a38bf0ff6ba8e36c52d43fe7fd08a5
Omar Moreno <email@omarmoreno.net>
2020-08-24 10:37:32 -0700

    Formatting

--- Event/include/Event/TrigScintCluster.h
+++ Event/include/Event/TrigScintCluster.h
@@ -1,11 +1,5 @@
-/**
- * @file TrigScintCluster.h
- * @brief Class that stores cluster information from the Trigger Scintillator
- * @author Lene Kristian Bryngemark, Stanford University : adaptation to Trigger Scintillator
- */
-
-#ifndef EVENT_TRIGSCINTCLUSTER_H_
-#define EVENT_TRIGSCINTCLUSTER_H_
+#ifndef EVENT_TRIGSCINTCLUSTER_H
+#define EVENT_TRIGSCINTCLUSTER_H
 
 // ROOT
 #include "TObject.h"
@@ -22,154 +16,155 @@
 
 namespace ldmx {
 
-  /**
-   * @class TrigScintCluster 
-   * @brief Stores cluster information from the trigger scintillator pads. Adds on the ECal cluster functionality
-   */
-  class TrigScintCluster : public EcalCluster {
-
-  public:
-    
     /**
-     * Class constructor.
+     * @class TrigScintCluster 
+     * @brief Stores cluster information from the trigger scintillator pads. 
+     *  Adds on the ECal cluster functionality
      */
-    TrigScintCluster();
+    class TrigScintCluster : public EcalCluster {
 
-    /**
-     * Class destructor.
-     */
-    virtual ~TrigScintCluster();
+        public:
 
-    /**
-     * Print a description of this object.
-     */
-    void Print(Option_t *option = "") const ; //override;
+            /**
+             * Class constructor.
+             */
+            TrigScintCluster();
 
-    /**
-     * Reset the TrigScintCluster object.
-     */
-    void Clear(Option_t *option = "") ; // override;
+            /**
+             * Class destructor.
+             */
+            virtual ~TrigScintCluster();
 
+            /**
+             * Print a description of this object.
+             */
+            void Print(Option_t *option = "") const ; //override;
 
-    /**
-     * Take in the hits that make up the cluster.
-     * @param hit The digi hit's entry number in the events digi 
-     * collection.
-     */
-
-    void addHit(uint idx, const TrigScintHit* hit);
-    
-    void setSeed( int idx ) { seed_ = idx ; } 
-	
-	void setEnergy(double energy) {
-      energy_ = energy;
-	}
-	
-	void setPE(int PE) {
-      PE_ = PE;
-	}
-	
-	
-	void setNHits(int nHits) {
-      nHits_ = nHits;
-	}
-	
-	void setIDs(std::vector<unsigned int>& hitIDs) {
-      hitIDs_ = hitIDs;
-	}
-	
-	void setCentroidXYZ(double x, double y, double z) {
-      centroidX_ = x;
-      centroidY_ = y;
-      centroidZ_ = z;
-	}
-	
-	
-    /**
-     * @param centroid The channel ID centroid 
-     */
-	
-    void setCentroid(double centroid) {
-      centroid_ = centroid;
-    }
-	
-	
-    /** Set time of hit. */
-    void setTime(float t){
-      time_ = t;
-    }
-	
-    /** Get time of hit. */
-    float getTime() const {
-      return time_;
-    }
-
-    /** Set beam energy fraction of hit. */
-    void setBeamEfrac(float e){
-      beamEfrac_ = e;
-    }
-
-    /** Get beam energy fraction of hit. */
-    float getBeamEfrac() const {
-      return beamEfrac_;
-    }
-
-    int getSeed() const { return seed_ ; } 
+            /**
+             * Reset the TrigScintCluster object.
+             */
+            void Clear(Option_t *option = "") ; // override;
 
 
-	double getEnergy() const {
-      return energy_;
-	}
-	
-	double getPE() const {
-      return PE_;
-	}
-	
-	int getNHits() const {
-      return nHits_;
-	}
+            /**
+             * Take in the hits that make up the cluster.
+             * @param hit The digi hit's entry number in the events digi 
+             * collection.
+             */
 
-	double getCentroidX() const {
-      return centroidX_;
-	}
+            void addHit(uint idx, const TrigScintHit* hit);
 
-	double getCentroidY() const {
-      return centroidY_;
-	}
-            
-	double getCentroidZ() const {
-      return centroidZ_;
-	}
+            void setSeed( int idx ) { seed_ = idx ; } 
 
-	const std::vector<unsigned int>& getHitIDs() const {
-      return hitIDs_;
-	}
-    
+            void setEnergy(double energy) {
+                energy_ = energy;
+            }
 
-    double getCentroid() const {
-      return centroid_;
-    }
-    
-     
-
-  private:
-
-    std::vector<unsigned int> hitIDs_;
-    double energy_{0};
-    int nHits_{0};
-    int PE_{0};
-    int seed_{-1};
-    double centroid_{0};
-    double centroidX_{0};
-    double centroidY_{0};
-    double centroidZ_{0};
+            void setPE(int PE) {
+                PE_ = PE;
+            }
 
 
-    float beamEfrac_{0.};
-    float time_{0.};
-      
-    ClassDef(TrigScintCluster, 1);
-  };
+            void setNHits(int nHits) {
+                nHits_ = nHits;
+            }
+
+            void setIDs(std::vector<unsigned int>& hitIDs) {
+                hitIDs_ = hitIDs;
+            }
+
+            void setCentroidXYZ(double x, double y, double z) {
+                centroidX_ = x;
+                centroidY_ = y;
+                centroidZ_ = z;
+            }
+
+
+            /**
+             * @param centroid The channel ID centroid 
+             */
+
+            void setCentroid(double centroid) {
+                centroid_ = centroid;
+            }
+
+
+            /** Set time of hit. */
+            void setTime(float t){
+                time_ = t;
+            }
+
+            /** Get time of hit. */
+            float getTime() const {
+                return time_;
+            }
+
+            /** Set beam energy fraction of hit. */
+            void setBeamEfrac(float e){
+                beamEfrac_ = e;
+            }
+
+            /** Get beam energy fraction of hit. */
+            float getBeamEfrac() const {
+                return beamEfrac_;
+            }
+
+            int getSeed() const { return seed_ ; } 
+
+
+            double getEnergy() const {
+                return energy_;
+            }
+
+            double getPE() const {
+                return PE_;
+            }
+
+            int getNHits() const {
+                return nHits_;
+            }
+
+            double getCentroidX() const {
+                return centroidX_;
+            }
+
+            double getCentroidY() const {
+                return centroidY_;
+            }
+
+            double getCentroidZ() const {
+                return centroidZ_;
+            }
+
+            const std::vector<unsigned int>& getHitIDs() const {
+                return hitIDs_;
+            }
+
+
+            double getCentroid() const {
+                return centroid_;
+            }
+
+
+
+        private:
+
+            std::vector<unsigned int> hitIDs_;
+            double energy_{0};
+            int nHits_{0};
+            int PE_{0};
+            int seed_{-1};
+            double centroid_{0};
+            double centroidX_{0};
+            double centroidY_{0};
+            double centroidZ_{0};
+
+
+            float beamEfrac_{0.};
+            float time_{0.};
+
+            ClassDef(TrigScintCluster, 1);
+    };
 }
 
-#endif /* EVENT_TRIGSCINTCLUSTER_H_ */
+#endif /* EVENT_TRIGSCINTCLUSTER_H */
--- Event/src/TrigScintCluster.cxx
+++ Event/src/TrigScintCluster.cxx
@@ -4,32 +4,31 @@ ClassImp(ldmx::TrigScintCluster)
 
 namespace ldmx {
 
-  TrigScintCluster::TrigScintCluster() :  EcalCluster() {
-    
-  }
-  
-  TrigScintCluster::~TrigScintCluster() {
-    Clear();
-    
-  }
-  
-  void TrigScintCluster::Print(Option_t *option) const {
-    
-    std::cout << "TrigScintCluster { " << "Energy or PE: " << energy_ << ", " << "Number of hits: " << nHits_ << ", " << "Seed channel " << seed_ << ", Channel centroid: " << centroid_ << " }" << std::endl;
-    std::cout << "  --  Constituent hit channel ids: {  ";
-    for (const auto& idx : getHitIDs() )
-      std::cout  << idx << "  ";
-    std::cout << "}"<< std::endl;
+    TrigScintCluster::TrigScintCluster() :  EcalCluster() {
+    }
 
-  }
-  
-  void TrigScintCluster::Clear(Option_t*) {
-    
-    EcalCluster::Clear();
-    setCentroid(0); 
-    setSeed(-1);    
-  }
-  
-  
+    TrigScintCluster::~TrigScintCluster() {
+        Clear();
+    }
+
+    void TrigScintCluster::Print(Option_t *option) const {
+
+        std::cout << "TrigScintCluster { " 
+                  << "Energy or PE: " << energy_ << ", " 
+                  << "Number of hits: " << nHits_ << ", " 
+                  << "Seed channel " << seed_  << ", "
+                  << "Channel centroid: " << centroid_ << " }" << std::endl;
+        std::cout << "  --  Constituent hit channel ids: {  ";
+        
+        for (const auto& idx : getHitIDs() ) std::cout  << idx << "  ";
+        
+        std::cout << "}"<< std::endl;
+    }
+
+    void TrigScintCluster::Clear(Option_t*) {
+        EcalCluster::Clear();
+        setCentroid(0); 
+        setSeed(-1);    
+    }
 }
 
