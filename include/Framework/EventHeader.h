f72e0cac248d74379ce4deae2e5a0c6792f3f297
Tom E <tom.eichlersmith@gmail.com>
2020-08-24 08:57:57 -0500

    move EventHeader to Framework

---
+++ include/Framework/EventHeader.h
@@ -0,0 +1,247 @@
+/**
+ * @file EventHeader.h
+ * @brief Class that provides header information about an event such as event number and timestamp
+ * @author Jeremy McCormick, SLAC National Accelerator Laboratory
+ */
+
+#ifndef EVENT_EVENTHEADER_H_
+#define EVENT_EVENTHEADER_H_
+
+// ROOT
+#include "TObject.h" //For ClassDef
+#include "TTimeStamp.h"
+
+// STL
+#include <iostream>
+#include <string>
+#include <map>
+
+namespace ldmx {
+
+    /**
+     * @class EventHeader
+     * @brief Provides header information an event such as event number and timestamp
+     */
+    class EventHeader {
+
+        public:
+
+            /**
+             * Class constructor.
+             */
+            EventHeader() {
+            }
+
+            /**
+             * Class destructor.
+             */
+            virtual ~EventHeader() {
+            }
+
+            /**
+             * Clear information from this object.
+             */
+            void Clear(Option_t* = "") {
+                eventNumber_ = -1;
+                run_ = -1;
+                timestamp_ = TTimeStamp(0, 0);
+                weight_ = 1.0;
+                isRealData_ = false;
+                intParameters_.clear();
+                floatParameters_.clear();
+                stringParameters_.clear();
+            }
+
+            /**
+             * Print this object.
+             */
+            void Print(Option_t* = "") const {
+                std::cout << "EventHeader {" << " eventNumber: " << eventNumber_ << ", run: "
+                        << run_ << ", timestamp: " << timestamp_ << ", weight: " << weight_;
+                if (isRealData_)
+                    std::cout << ", DATA";
+                else
+                    std::cout << ", MC";
+                std::cout << " }" << std::endl;
+            }
+
+            /**
+             * Return the event number.
+             * @return The event number.
+             */
+            int getEventNumber() const {
+                return eventNumber_;
+            }
+
+            /**
+             * Return the run number.
+             * @return The run number.
+             */
+            int getRun() const {
+                return run_;
+            }
+
+            /**
+             * Get the event's timestamp.
+             * This currently returns second's since the epoch for simulated events.
+             * @return The event's timestamp.
+             * @note The returned object has a possible resolution of nanoseconds.
+             */
+            const TTimeStamp& getTimestamp() const {
+                return timestamp_;
+            }
+
+            /**
+             * Get the event weight (default of 1.0).
+             * @return The event weight.
+             */
+            double getWeight() const {
+                return weight_;
+            }
+
+            /**
+             * Is this a real data event?
+             * @return True if this is a real data event.
+             */
+            bool isRealData() const {
+                return isRealData_;
+            }
+
+            /**
+             * Set the event number.
+             * @param eventNumber The event number.
+             */
+            void setEventNumber(int eventNumber) {
+                this->eventNumber_ = eventNumber;
+            }
+
+            /**
+             * Set the run number.
+             * @param run The run number.
+             */
+            void setRun(int run) {
+                this->run_ = run;
+            }
+
+            /**
+             * Set the timestamp.
+             * @param timestamp The timestamp.
+             */
+            void setTimestamp(const TTimeStamp& timestamp) {
+                this->timestamp_ = timestamp;
+            }
+
+            /**
+             * Set the event weight.
+             * @param weight The event weight.
+             */
+            void setWeight(double weight) {
+                this->weight_ = weight;
+            }
+
+            /**
+             * Get an int parameter value.
+             * @param name The name of the parameter.
+             * @return The parameter value.
+             */
+            int getIntParameter(const std::string& name) {
+                return intParameters_[name];
+            }
+
+            /**
+             * Set an int parameter value.
+             * @param name The name of the parameter.
+             * @param value The value of the parameter.
+             * @return The parameter value.
+             */
+            void setIntParameter(const std::string& name, int value) {
+                intParameters_[name] = value;
+            }
+
+            /**
+             * Get a float parameter value.
+             * @param name The name of the parameter.
+             * @return value The parameter value.
+             */
+            float getFloatParameter(const std::string& name) {
+                return floatParameters_[name];
+            }
+
+            /**
+             * Set a float parameter value.
+             * @param name The name of the parameter.
+             * @return value The parameter value.
+             */
+            void setFloatParameter(const std::string& name, float value) {
+                floatParameters_[name] = value;
+            }
+
+            /**
+             * Get a string parameter value.
+             * @param name The name of the parameter.
+             * @return value The parameter value.
+             */
+            std::string getStringParameter(const std::string& name) {
+                return stringParameters_[name];
+            }
+
+            /**
+             * Set a string parameter value.
+             * @param name The name of the parameter.
+             * @return value The parameter value.
+             */
+            void setStringParameter(const std::string& name, std::string value) {
+                stringParameters_[name] = value;
+            }
+
+        protected:
+
+            /**
+             * The event number.
+             */
+            int eventNumber_{-1};
+
+            /**
+             * The run number.
+             */
+            int run_{-1};
+
+            /**
+             * The event timestamp
+             */
+            TTimeStamp timestamp_{0, 0};
+
+            /**
+             * The event weight.
+             */
+            double weight_{1.0};
+
+            /**
+             * Is this event real data?
+             */
+            bool isRealData_{false};
+
+            /**
+             * The int parameters.
+             */
+            std::map<std::string, int> intParameters_;
+
+            /**
+             * The float parameters.
+             */
+            std::map<std::string, float> floatParameters_;
+
+            /**
+             * The string parameters.
+             */
+            std::map<std::string, std::string> stringParameters_;
+
+            /**
+             * ROOT class definition.
+             */
+            ClassDef(EventHeader, 1);
+    };
+
+}
+
+#endif /* EVENT_EVENTHEADER_H_ */
