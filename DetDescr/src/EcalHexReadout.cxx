fa0a462d28907e145fc56c3a01ae1cbfcd81da4b
Tom E <tom.eichlersmith@gmail.com>
2020-08-24 16:52:22 -0500

    redo projection algorithm
    now does all "straddling" cells, projecting any
    vertices inside that have a neighbor outside
    onto the border and adds that new vertex as
    an additional vertex

--- DetDescr/src/EcalHexReadout.cxx
+++ DetDescr/src/EcalHexReadout.cxx
@@ -131,8 +131,9 @@ namespace ldmx {
 
             if(numVerticesInside > 1){
                 // Include this cell if more than one of its vertices is inside the module hexagon
-
-                if ( numVerticesInside == 5 ) {
+                double actual_x[8], actual_y[8];
+                int num_vertices{0};
+                if ( numVerticesInside < 6 ) {
                     // This cell is stradling the edge of the module
                     // and is NOT cleanly cut by module edge
 
@@ -195,21 +196,47 @@ namespace ldmx {
                                 + (vertex_y[i]-edge_origin_y)*edge_slope_y )
                                 / (edge_slope_x*edge_slope_x + edge_slope_y*edge_slope_y);
     
-                            vertex_x[i] = edge_origin_x + projection_factor*edge_slope_x;
-                            vertex_y[i] = edge_origin_y + projection_factor*edge_slope_y;
+                            double proj_x = edge_origin_x + projection_factor*edge_slope_x;
+                            double proj_y = edge_origin_y + projection_factor*edge_slope_y;
+
+                            if ( not isinside[up] ) {
+                                //the next point is outside
+                                actual_x[num_vertices] = vertex_x[i];
+                                actual_y[num_vertices] = vertex_y[i];
+                                actual_x[num_vertices+1] = proj_x;
+                                actual_y[num_vertices+1] = proj_y;
+                            } else {
+                                //the previous point was outside
+                                actual_x[num_vertices] = proj_x;
+                                actual_y[num_vertices] = proj_y;
+                                actual_x[num_vertices+1] = vertex_x[i];
+                                actual_y[num_vertices+1] = vertex_y[i];
+                            }
+                            num_vertices += 2;
     
                             if(verbose_>2) {
                                 std::cout << "New Vertex " << i 
                                     << " : (" << vertex_x[i] << "," << vertex_y[i] << ")" << std::endl;
                             }
-                        } //if inside and adjacent to a vertex outside module
+                        } else {
+                            actual_x[num_vertices] = vertex_x[i];
+                            actual_y[num_vertices] = vertex_y[i];
+                            num_vertices++;
+                        } //should we project or not
                     } //loop through vertices
-                } //if numVerticesInside is equal to 5
+                } else {
+                    //all 6 inside, just copy the vertices over
+                    num_vertices = 6;
+                    for ( int i = 0; i < 6; i++ ) {
+                        actual_x[i] = vertex_x[i];
+                        actual_y[i] = vertex_y[i];
+                    }
+                } //if numVerticesInside is less than 5
 
                 //ecalMap_ needs to have its own copy of the polygon TGraph
                 //  otherwise, we get a seg fault when EcalHexReadout is destructed
                 //  because the polygon that was copied over from gridMap is deleted at the end of this function
-                ecalMap_.AddBin( 6 , vertex_x , vertex_y );
+                ecalMap_.AddBin( num_vertices , actual_x , actual_y );
                 
                 double x = (polyBin->GetXMax() + polyBin->GetXMin()) / 2.;
                 double y = (polyBin->GetYMax() + polyBin->GetYMin()) / 2.;
