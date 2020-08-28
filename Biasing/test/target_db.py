d0efd05970d52a1f37e51df951317afa367017c2
Tom E <tom.eichlersmith@gmail.com>
2020-08-28 14:00:15 -0500

    add DB tests and make sure other tests are working

---
+++ Biasing/test/target_db.py
@@ -0,0 +1,15 @@
+from LDMX.Framework import ldmxcfg
+p = ldmxcfg.Process('target_db')
+from LDMX.Biasing import target
+from LDMX.SimCore import makePath
+p.sequence = [
+    target.dark_brem( 
+        10., #MeV - mass of A'
+        makePath.makeLHEPath(10.), #str - full path to directory containing LHE vertex files
+        'ldmx-det-v12' , #name of geometry to use
+        )
+    ]
+p.maxEvents = 1000
+p.termLogLevel = 0
+p.logFrequency = 10
+p.outputFiles = [ 'target_db.root' ]
