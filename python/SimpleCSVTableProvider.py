96c2f4c052e0491f6d69a7efc0e7f7147b8f91ad
Jeremiah Mans <jmmans@umn.edu>
2020-09-16 15:43:58 -0500

    Change to single-data type per provider

--- python/SimpleCSVTableProvider.py
+++ python/SimpleCSVTableProvider.py
@@ -5,13 +5,17 @@ Specialization of ConditionsObjectProvider for simple tables indexed by detid
 
 from LDMX.Framework import ldmxcfg
 
-class SimpleCSVTableItem:
-    def __init__(self, name, url, dataType, columns):
-        self.name=name
+class SimpleCSVTableEntry:
+    def __init__(self, url):
         self.URL=url
-        self.dataType=dataType;
-        self.columns=columns
+        self.firstRun=-1
+        self.lastRun=-1
+        self.runType="any"
 
+    def setIOV(self, firstRun, lastRun):
+        self.firstRun=firstRun
+        self.lastRun=lastRun
+        
     def __str__(self) :
         """Stringify this ConditionsObjectProvider, creates a message with all the internal parameters.
 
@@ -30,29 +34,28 @@ class SimpleCSVTableItem:
         return msg 
 
 class SimpleCSVTableProvider(ldmxcfg.ConditionsObjectProvider):
-    def __init__(self,instanceName, tagName):
-        super().__init__(instanceName,"ldmx::SimpleCSVTableProvider",tagName)
-        self.provides=[]
+    def __init__(self,objName, tagName, dataType, columns):
+        super().__init__(objName,"ldmx::SimpleCSVTableProvider",tagName)
+        self.dataType="double"
+        self.columns=columns
+        self.entries=[]
 
-    def provideIntegerTable(self, name, url, columns):
-        self.provides.append(SimpleCSVTableItem(name,url,"int",columns))
+    def validForever(self, url):
+        self.entries.append(SimpleCSVTableEntry(url))
 
-    def provideDoubleTable(self, name, url, columns):
-        self.provides.append(SimpleCSVTableItem(name,url,"double",columns))
+    def validForRuns(self, url, firstRun, lastRun):
+        entry=SimpleCSVTableEntry(url)
+        entry.setIOV(firstRun,lastRun)
+        self.entries.append(entry)
+        
+class SimpleCSVDoubleTableProvider(SimpleCSVTableProvider):
+    def __init__(self,objName, tagName, columns):
+        super().__init__(objName, tagName,"double",columns)
+        
+class SimpleCSVIntegerTableProvider(SimpleCSVTableProvider):
+    def __init__(self,objName, tagName, columns):
+        super().__init__(objName, tagName,"int",columns)
 
-    def __str__(self) :
-        """Stringify this ConditionsObjectProvider, creates a message with all the internal parameters.
 
-        Returns
-        -------
-        str
-            A message with all the parameters and member variables in a human readable format
-        """
 
-        msg = "\n  ConditionsObjectProvider(%s of class %s, tag='%s')"%(self.instanceName,self.className,self.tagName)
-        if len(self.__dict__)>0:
-            msg += "\n   Parameters:"
-            for k, v in self.__dict__.items():
-                msg += "\n    " + str(k) + " : " + str(v)
 
-        return msg
