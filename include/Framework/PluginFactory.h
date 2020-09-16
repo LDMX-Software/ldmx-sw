5b34f9842d0d2274e88419e40e88e20521ee6b81
Jeremiah Mans <jmmans@umn.edu>
2020-09-16 13:28:37 -0500

    Switch to single-object-per-provider and google style

--- include/Framework/PluginFactory.h
+++ include/Framework/PluginFactory.h
@@ -18,101 +18,101 @@
 
 namespace ldmx {
 
-    /**
-     * @class PluginFactory
-     * @brief Singleton module factory that creates EventProcessor objects.
-     */
-    class PluginFactory {
+/**
+ * @class PluginFactory
+ * @brief Singleton module factory that creates EventProcessor objects.
+ */
+class PluginFactory {
 
-        public:
+ public:
 
-            /**
-             * Get the factory instance.
-             * @return The factory.
-             */
-            static PluginFactory& getInstance() {
-                return theFactory_;
-            }
+  /**
+   * Get the factory instance.
+   * @return The factory.
+   */
+  static PluginFactory& getInstance() {
+    return theFactory_;
+  }
 
-            /**
-             * Register the event processor.
-             * @param classname The name of the class associated with processor.
-             * @param classtype The type of class associated with processor.
-             * @param maker TODO.
-             */
-            void registerEventProcessor(const std::string& classname, int classtype, EventProcessorMaker* maker);
+  /**
+   * Register the event processor.
+   * @param classname The name of the class associated with processor.
+   * @param classtype The type of class associated with processor.
+   * @param maker TODO.
+   */
+  void registerEventProcessor(const std::string& classname, int classtype, EventProcessorMaker* maker);
 
-            /**
-             * Register a conditions object provider
-             * @param classname The name of the class associated with the conditions object provider.
-             * @param classtype The type of class associated with conditions object provider.
-             * @param maker TODO.
-             */
-            void registerConditionsObjectProvider(const std::string& classname, int classtype, ConditionsObjectProviderMaker* maker);
+  /**
+   * Register a conditions object provider
+   * @param classname The name of the class associated with the conditions object provider.
+   * @param classtype The type of class associated with conditions object provider.
+   * @param maker TODO.
+   */
+  void registerConditionsObjectProvider(const std::string& classname, int classtype, ConditionsObjectProviderMaker* maker);
       
-            /**
-             * Get the classes associated with the processor.
-             * @return a vector of strings corresponding to processor classes.
-             */
-            std::vector<std::string> getEventProcessorClasses() const;
+  /**
+   * Get the classes associated with the processor.
+   * @return a vector of strings corresponding to processor classes.
+   */
+  std::vector<std::string> getEventProcessorClasses() const;
 
-            /**
-             * Get the class type for the event processor.
-             * @param TODO.
-             */
-            int getEventProcessorClasstype(const std::string&) const;
+  /**
+   * Get the class type for the event processor.
+   * @param TODO.
+   */
+  int getEventProcessorClasstype(const std::string&) const;
 
-            /**
-             * Make an event processor.
-             * @param classname Class name of event processor.
-             * @param moduleInstanceName TODO.
-             * @param process The process handle
-             */
-            EventProcessor* createEventProcessor(const std::string& classname, const std::string& moduleInstanceName, Process& process);
+  /**
+   * Make an event processor.
+   * @param classname Class name of event processor.
+   * @param moduleInstanceName TODO.
+   * @param process The process handle
+   */
+  EventProcessor* createEventProcessor(const std::string& classname, const std::string& moduleInstanceName, Process& process);
 
-            /**
-             * Make a conditions object provider
-             * @param classname Class name of conditions object provider
-             * @param moduleInstanceName 
-             * @param params Parameters for the conditoons object provider
-             * @param process The process handle
-             */
-            ConditionsObjectProvider* createConditionsObjectProvider(const std::string& classname, const std::string& moduleInstanceName, const std::string& tagname, const Parameters& params, Process& process);
+  /**
+   * Make a conditions object provider
+   * @param classname Class name of conditions object provider
+   * @param objName Name of the object provided (may be overriden internally!)
+   * @param params Parameters for the conditoons object provider
+   * @param process The process handle
+   */
+  ConditionsObjectProvider* createConditionsObjectProvider(const std::string& classname, const std::string& objName, const std::string& tagname, const Parameters& params, Process& process);
 
-            /**
-             * Load a library.
-             * @param libname The library to load.
-             */
-            void loadLibrary(const std::string& libname);
+  /**
+   * Load a library.
+   * @param libname The library to load.
+   */
+  void loadLibrary(const std::string& libname);
 
-        private:
+ private:
 
-            /**
-             * Constructor
-             */
-            PluginFactory();
+  /**
+   * Constructor
+   */
+  PluginFactory();
 
-            /**
-             * @struct PluginInfo
-             * @brief info container to hold classname, class type and maker.
-             */
-            struct PluginInfo {
-                std::string classname;
-                int classtype;
-                EventProcessorMaker* ep_maker;
-                ConditionsObjectProviderMaker* cop_maker;
-            };
+  /**
+   * @struct PluginInfo
+   * @brief info container to hold classname, class type and maker.
+   */
+  struct PluginInfo {
+    std::string classname;
+    int classtype;
+    EventProcessorMaker* ep_maker;
+    ConditionsObjectProviderMaker* cop_maker;
+  };
 
-            /** A map of names to processor containers. */
-            std::map<std::string, PluginInfo> moduleInfo_;
+  /** A map of names to processor containers. */
+  std::map<std::string, PluginInfo> moduleInfo_;
 
-            /** A set of names of loaded libraries. */
-            std::set<std::string> librariesLoaded_;
+  /** A set of names of loaded libraries. */
+  std::set<std::string> librariesLoaded_;
 
-            /** Factory for creating the plugin objects. */
-            static PluginFactory theFactory_;
-    };
+  /** Factory for creating the plugin objects. */
+  static PluginFactory theFactory_;
+};
 
-}
+} // namespace ldmx
 
 #endif
--- src/PluginFactory.cxx
+++ src/PluginFactory.cxx
@@ -6,81 +6,81 @@ ldmx::PluginFactory ldmx::PluginFactory::theFactory_ __attribute((init_priority(
 
 namespace ldmx {
 
-    PluginFactory::PluginFactory() {
-    }
+PluginFactory::PluginFactory() {
+}
     
-    void PluginFactory::registerEventProcessor(const std::string& classname, int classtype, EventProcessorMaker* maker) {
-        auto ptr = moduleInfo_.find(classname);
-        if (ptr != moduleInfo_.end()) {
-            EXCEPTION_RAISE("ExistingEventProcessorDefinition", "Already have a module registered with the classname '" + classname + "'");
-        }
-        PluginInfo mi;
-        mi.classname = classname;
-        mi.classtype = classtype;
-        mi.ep_maker = maker;
-        mi.cop_maker = 0;
-        moduleInfo_[classname] = mi;
-    }
+void PluginFactory::registerEventProcessor(const std::string& classname, int classtype, EventProcessorMaker* maker) {
+  auto ptr = moduleInfo_.find(classname);
+  if (ptr != moduleInfo_.end()) {
+    EXCEPTION_RAISE("ExistingEventProcessorDefinition", "Already have a module registered with the classname '" + classname + "'");
+  }
+  PluginInfo mi;
+  mi.classname = classname;
+  mi.classtype = classtype;
+  mi.ep_maker = maker;
+  mi.cop_maker = 0;
+  moduleInfo_[classname] = mi;
+}
     
-    void PluginFactory::registerConditionsObjectProvider(const std::string& classname, int classtype, ConditionsObjectProviderMaker* maker) {
-        auto ptr = moduleInfo_.find(classname);
-        if (ptr != moduleInfo_.end()) {
-            EXCEPTION_RAISE("ExistingEventProcessorDefinition", "Already have a module registered with the classname '" + classname + "'");
-        }
-        PluginInfo mi;
-        mi.classname = classname;
-        mi.classtype = classtype;
-        mi.cop_maker = maker;
-        mi.ep_maker = 0;
-        moduleInfo_[classname] = mi;
-    }
+void PluginFactory::registerConditionsObjectProvider(const std::string& classname, int classtype, ConditionsObjectProviderMaker* maker) {
+  auto ptr = moduleInfo_.find(classname);
+  if (ptr != moduleInfo_.end()) {
+    EXCEPTION_RAISE("ExistingEventProcessorDefinition", "Already have a module registered with the classname '" + classname + "'");
+  }
+  PluginInfo mi;
+  mi.classname = classname;
+  mi.classtype = classtype;
+  mi.cop_maker = maker;
+  mi.ep_maker = 0;
+  moduleInfo_[classname] = mi;
+}
 
-    std::vector<std::string> PluginFactory::getEventProcessorClasses() const {
-        std::vector<std::string> classes;
-        for (auto ptr : moduleInfo_) {
-        if (ptr.second.ep_maker!=0)
-        classes.push_back(ptr.first);
-        }
-        return classes;
-    }
+std::vector<std::string> PluginFactory::getEventProcessorClasses() const {
+  std::vector<std::string> classes;
+  for (auto ptr : moduleInfo_) {
+    if (ptr.second.ep_maker!=0)
+      classes.push_back(ptr.first);
+  }
+  return classes;
+}
 
-    int PluginFactory::getEventProcessorClasstype(const std::string& ct) const {
-        auto ptr = moduleInfo_.find(ct);
-        if (ptr == moduleInfo_.end() || ptr->second.ep_maker==0) {
-            return 0;
+int PluginFactory::getEventProcessorClasstype(const std::string& ct) const {
+  auto ptr = moduleInfo_.find(ct);
+  if (ptr == moduleInfo_.end() || ptr->second.ep_maker==0) {
+    return 0;
 
-        } else {
-            return ptr->second.classtype;
-        }
-    }
+  } else {
+    return ptr->second.classtype;
+  }
+}
 
-    EventProcessor* PluginFactory::createEventProcessor(const std::string& classname, const std::string& moduleInstanceName, Process& process) {
-        auto ptr = moduleInfo_.find(classname);
-        if (ptr == moduleInfo_.end() || ptr->second.ep_maker==0) {
-            return 0;
-        }
-        return ptr->second.ep_maker(moduleInstanceName, process);
-    }
+EventProcessor* PluginFactory::createEventProcessor(const std::string& classname, const std::string& moduleInstanceName, Process& process) {
+  auto ptr = moduleInfo_.find(classname);
+  if (ptr == moduleInfo_.end() || ptr->second.ep_maker==0) {
+    return 0;
+  }
+  return ptr->second.ep_maker(moduleInstanceName, process);
+}
 
-    ConditionsObjectProvider* PluginFactory::createConditionsObjectProvider(const std::string& classname, const std::string& moduleInstanceName, const std::string& tagname, const Parameters& params, Process& process) {
-        auto ptr = moduleInfo_.find(classname);
-        if (ptr == moduleInfo_.end() || ptr->second.cop_maker==0) {
-            return 0;
-        }
-        return ptr->second.cop_maker(moduleInstanceName, tagname, params, process);
-    }
+ConditionsObjectProvider* PluginFactory::createConditionsObjectProvider(const std::string& classname, const std::string& objName, const std::string& tagname, const Parameters& params, Process& process) {
+  auto ptr = moduleInfo_.find(classname);
+  if (ptr == moduleInfo_.end() || ptr->second.cop_maker==0) {
+    return 0;
+  }
+  return ptr->second.cop_maker(objName, tagname, params, process);
+}
 
-    void PluginFactory::loadLibrary(const std::string& libname) {
-        if (librariesLoaded_.find(libname) != librariesLoaded_.end()) {
-            return; // already loaded
-        }
+void PluginFactory::loadLibrary(const std::string& libname) {
+  if (librariesLoaded_.find(libname) != librariesLoaded_.end()) {
+    return; // already loaded
+  }
 
-        void* handle = dlopen(libname.c_str(), RTLD_NOW);
-        if (handle == nullptr) {
-            EXCEPTION_RAISE("LibraryLoadFailure", "Error loading library '" + libname + "':" + dlerror());
-        }
+  void* handle = dlopen(libname.c_str(), RTLD_NOW);
+  if (handle == nullptr) {
+    EXCEPTION_RAISE("LibraryLoadFailure", "Error loading library '" + libname + "':" + dlerror());
+  }
 
-        librariesLoaded_.insert(libname);
-    }
+  librariesLoaded_.insert(libname);
+}
 
 }
