fc29e46842c2a508a07ca2a7dbed27c9c0d656a0
Jeremiah Mans <jmmans@umn.edu>
2020-09-16 15:43:58 -0500

    Change to single-data type per provider

--- include/Conditions/SimpleCSVTableProvider.h
+++ include/Conditions/SimpleCSVTableProvider.h
@@ -10,46 +10,55 @@
 
 namespace ldmx {
 
-    /**
-     * @class ConditionsObjectProvider which can be configured to load conditions objects from
-     *  CSV files.
-     *
-     * Configuration via Python uses a parameter "provides" which is a list of elements each of which must contain
-     * "name","dataType", "columns", and "URL".  The parameter set _may_ contain "firstRun", "lastRun", and "runType".
-     * "DataType may be "int" or "double".  The "columns" parameter is the list of names of data columns to be read out.
-     * If "firstRun" is not provided, -1 is assumed.  If "lastRun" is not provided, -1 is assumed.
-     * The parameter "runType" could have the value "data", "MC", or "any".
-     * If "runType" is not provided, "any" is assumed.
-     * 
-     * In any URL, envrironment variables in the format ${VARNAME} will be expanded.  Also, the variable ${LDMX_CONDITION_TAG} will be replaced with the 
-     * tagname provided in the constructor.
-     */
-    class SimpleCSVTableProvider : public ConditionsObjectProvider {
-	public:
+/**
+ * @class ConditionsObjectProvider which can be configured to load conditions objects from
+ *  CSV files.
+ *
+ * Configuration via Python uses the following parameters:
+ * "dataType", "columns", "entries"
+ * "DataType may be "int" or "double".  
+ * The "columns" parameter is the list of names of data columns to be read out.
+
+ * "entries" is an array of parameter sets.
+ * Each entry must contain a "URL"
+ * The entry _may_ contain "firstRun", "lastRun", "runType", and "values".
+ * If "firstRun" is not provided, -1 is assumed.  If "lastRun" is not provided, -1 is assumed.
+ * The parameter "runType" could have the value "data", "MC", or "any".
+ * If "runType" is not provided, "any" is assumed.
+ * The parameter "values" is only used when the URL is "python:".  In this case, the value for each
+ * column will be taken from the values array and the configuration is expected to be independend of id.
+ * 
+ * In any URL, envrironment variables in the format ${VARNAME} will be expanded.  Also, the variable ${LDMX_CONDITION_TAG} will be replaced with the 
+ * tagname provided in the constructor.
+ */
+class SimpleCSVTableProvider : public ConditionsObjectProvider {
+ public:
 	
-	SimpleCSVTableProvider(const std::string& name, const std::string& tagname, const Parameters& parameters, Process& process);
+  SimpleCSVTableProvider(const std::string& name, const std::string& tagname, const Parameters& parameters, Process& process);
 
-	virtual ~SimpleCSVTableProvider();
+  virtual ~SimpleCSVTableProvider();
 
-	virtual std::pair<const ConditionsObject*,ConditionsIOV> getCondition(const std::string& condition_name, const EventHeader& context);
+  virtual std::pair<const ConditionsObject*,ConditionsIOV> getCondition(const EventHeader& context);
 
-        static void runTest(const std::string& host, const std::string& path, std::string& ss);
-	
-	private:
-	
-	struct Item {
-	    std::string objectName_;
-	    enum { OBJ_unknown, OBJ_int, OBJ_double } objectType_;
-	    ConditionsIOV iov_;
-	    std::string url_;
-	    std::vector<std::string> columns_;
-	};
-	std::vector<Item> items_;
+ private:
 
-	/** 
-	 * Utility for expanding environment variables
-	 */
-	std::string expandEnv(const std::string& s) const;
-    };
-}
+  enum { OBJ_unknown, OBJ_int, OBJ_double } objectType_;
+  std::vector<std::string> columns_;
+
+  struct Entry {
+    ConditionsIOV iov_;
+    std::string url_;
+    std::vector<int> ivalues_;
+    std::vector<double> dvalues_;
+  };
+
+  std::vector<Entry> entries_;
+
+  /** 
+   * Utility for expanding environment variables
+   */
+  std::string expandEnv(const std::string& s) const;
+};
+
+} // namespace ldmx
 #endif
--- src/BaseTableCondition.cxx
+++ src/BaseTableCondition.cxx
@@ -4,13 +4,15 @@
 namespace ldmx {
     
     std::size_t BaseTableCondition::findKey(unsigned int id) const {
-	std::vector<unsigned int>::const_iterator ptr=std::lower_bound(keys_.begin(), keys_.end(), id);
-	if (ptr==keys_.end() || *ptr!=id) return keys_.size();
+        unsigned int effid=id&idMask_;
+	std::vector<unsigned int>::const_iterator ptr=std::lower_bound(keys_.begin(), keys_.end(), effid);
+	if (ptr==keys_.end() || *ptr!=effid) return keys_.size();
 	else return std::distance(keys_.begin(),ptr);
     }
     
     std::size_t BaseTableCondition::findKeyInsert(unsigned int id) const {
-	std::vector<unsigned int>::const_iterator ptr=std::lower_bound(keys_.begin(), keys_.end(), id);
+        unsigned int effid=id&idMask_;
+	std::vector<unsigned int>::const_iterator ptr=std::lower_bound(keys_.begin(), keys_.end(), effid);
 	return std::distance(keys_.begin(),ptr);
     }
 
