a88612e93cabcb5ec3c23b3995dbdf47c1f5a139
Jeremiah Mans <jmmans@umn.edu>
2020-09-16 19:43:30 -0500

    Give both to-and-from types

--- include/Framework/Parameters.h
+++ include/Framework/Parameters.h
@@ -103,7 +103,7 @@ namespace ldmx {
                     parameter = std::any_cast< T >(parameters_.at(name));
                 } catch(const std::bad_any_cast& e) {
                     EXCEPTION_RAISE( "BadTypeParam",
-                                    "Parameter '" + name + "' is being cast to incorrect type '" + typeid(T).name() + "'."); 
+                                     "Parameter '" + name + "' of type '"+parameters_.at(name).type().name()+"' is being cast to incorrect type '" + typeid(T).name() + "'."); 
                 }
 
                 return parameter; 
