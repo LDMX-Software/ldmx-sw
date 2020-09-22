ec88d11b271115bdb7d031d9071ef56e3742bc66
Omar Moreno <email@omarmoreno.net>
2020-09-22 16:10:42 -0700

    Update paths.

--- src/Ecal/EcalDigiProducer.cxx
+++ src/Ecal/EcalDigiProducer.cxx
@@ -6,7 +6,6 @@
  */
 
 #include "Ecal/EcalDigiProducer.h"
-#include "Framework/RandomNumberSeedService.h"
 
 namespace ldmx {
 
@@ -56,23 +55,17 @@ namespace ldmx {
         noiseGenerator_->setNoise(hgcrocParams.getParameter<double>("noiseRMS")); //rms noise in mV
         noiseGenerator_->setPedestal(gain_*pedestal_); //mean noise amplitude (if using Gaussian Model for the noise) in mV
         noiseGenerator_->setNoiseThreshold(readoutThreshold_); //threshold for readout in mV
+
+        //The noise injector is used to place smearing on top
+        //of energy depositions and hit times before doing
+        //the digitization procedure.
+        int seed = ps.getParameter<int>("randomSeed");
+        noiseInjector_ = std::make_unique<TRandom3>(seed);
+        noiseGenerator_->setSeed(seed);
+
     }
 
     void EcalDigiProducer::produce(Event& event) {
-
-        // Need to handle seeding on the first event
-        if (!noiseGenerator_->hasSeed()) {
-            const auto& rseed = getCondition<RandomNumberSeedService>(RandomNumberSeedService::CONDITIONS_OBJECT_NAME);
-            noiseGenerator_->seedGenerator(rseed.getSeed("EcalDigiProducer::NoiseGenerator"));
-        }
-        if (noiseInjector_.get()==nullptr) {
-            const auto& rseed = getCondition<RandomNumberSeedService>(RandomNumberSeedService::CONDITIONS_OBJECT_NAME);
-            noiseInjector_ = std::make_unique<TRandom3>(rseed.getSeed("EcalDigiProducer::NoiseInjector"));
-        }
-        if(!hgcroc_->hasSeed()) {
-            const auto& rseed = getCondition<RandomNumberSeedService>(RandomNumberSeedService::CONDITIONS_OBJECT_NAME);
-            hgcroc_->seedGenerator(rseed.getSeed("EcalDigiProducer::HgcrocEmulator"));
-        }
 
         //Empty collection to be filled
         HgcrocDigiCollection ecalDigis;
@@ -89,7 +82,7 @@ namespace ldmx {
         //  the class EcalHitIO in the SimApplication module handles the translation from G4CalorimeterHits to SimCalorimeterHits
         //  this class ensures that only one SimCalorimeterHit is generated per cell, but
         //  multiple "contributions" are still handled within SimCalorimeterHit 
-        auto ecalSimHits{event.getCollection<SimCalorimeterHit>("EcalSimHits")};
+        auto ecalSimHits{event.getCollection<SimCalorimeterHit>(EventConstants::ECAL_SIM_HITS)};
 
         /* debug printout
         std::cout << "Energy to Voltage Conversion: " << MeV_ << " mV/MeV" << std::endl;
