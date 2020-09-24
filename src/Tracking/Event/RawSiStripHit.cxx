aed792c8d5402c8ce96f122b9ee1cf0efac113b1
Omar Moreno <email@omarmoreno.net>
2020-09-24 13:46:23 -0700

    Formatting and bug fix.

--- src/Tracking/Event/RawSiStripHit.cxx
+++ src/Tracking/Event/RawSiStripHit.cxx
@@ -1,12 +1,16 @@
 
 #include "Tracking/Event/RawSiStripHit.h"
 
-std::ostream& operator << (std::ostream& output, const RawSiStripHit& hit) {
-    output << "[ RawSiStripHit ]: Samples: { "; 
-    for(auto isample{0}; isample < (hit.samples_.size() - i); ++isample) 
-       output << hit.samples_[isample] << ", "; 
-    output << hit.samples_[hit.samples_.size() - 1] << " } " 
-           << "Time: " << hit.time_ << std::endl;
+namespace ldmx {
 
-    return output;  
-} 
+std::ostream &operator<<(std::ostream &output, const RawSiStripHit &hit) {
+  output << "[ RawSiStripHit ]: Samples: { ";
+  for (auto isample{0}; isample < (hit.samples_.size() - 1); ++isample)
+    output << hit.samples_[isample] << ", ";
+  output << hit.samples_[hit.samples_.size() - 1] << " } "
+         << "Time: " << hit.time_ << std::endl;
+
+  return output;
+}
+
+} // namespace ldmx
