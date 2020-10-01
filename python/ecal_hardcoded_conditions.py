404d569c75d806b2b62236c4c8e77144eb90b5f8
Tom E <tom.eichlersmith@gmail.com>
2020-10-01 13:29:28 -0500

    more cleanup, make sure global instances are created upon import

--- python/ecal_hardcoded_conditions.py
+++ python/ecal_hardcoded_conditions.py
@@ -5,13 +5,10 @@ from LDMX.Framework.ldmxcfg import Process
 from LDMX.Conditions.SimpleCSVTableProvider import SimpleCSVIntegerTableProvider
 
 EcalTrigPrimConditionsHardcode=SimpleCSVIntegerTableProvider("EcalTrigPrimDigiConditions",["ADC_PEDESTAL","ADC_THRESHOLD","TOT_PEDESTAL","TOT_THRESHOLD","TOT_GAIN"])
-           
+
 EcalTrigPrimConditionsHardcode.validForAllRows([ 50 , # ADC_PEDESTAL -- should match value from HgcrocEmulator
                                                  5 , # ADC_THRESHOLD -- current noise is 
                                                  0,  # TOT_PEDESTAL -- currently zero
                                                  100,  # TOT_THRESHOLD -- rather large value...
                                                  8 ] # TOT_GAIN, would ideally be 7.8, but....
                                                )
-
-Process.declareConditionsObjectProvider(EcalTrigPrimConditionsHardcode)
-    
