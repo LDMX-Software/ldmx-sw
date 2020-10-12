cd7cda0ad9c99c88eab6deb1296fdfc38c5730d4
Lene Kristian Bryngemark <lene.bryngemark@cern.ch>
2020-10-12 09:16:44 -0700

    Add in overlay config and Ecal changes
    Overlay config with defaults. Most likely the exact parameters needed will
    change a lot and some might already be obsolete.
    
    The Ecal changes are only making collection names settable in digi and reco

---
+++ EventProc/python/overlay.py
@@ -0,0 +1,38 @@
+"""Configuration for overlay """
+
+
+
+from LDMX.Framework import ldmxcfg
+
+class OverlayProducer(ldmxcfg.Producer) :
+    """Configuration for pileup overlay
+
+        Sets all parameters to reasonable defaults.
+
+    Examples
+    --------
+        from LDMX.EventProc.overlay import OverlayProducer
+        p.sequence.append( OverlayProducer() )
+    """
+
+    def __init__(self,name = 'overlay') :
+        super().__init__(name,'ldmx::OverlayProducer','EventProc')
+
+
+        self.overlayFileName = "ldmx_upstreamMultiElectron_events.root"
+        self.overlayTreeName = "LDMX_Events"
+        self.numberOverlaidInteractions = 1.
+        self.timeSpread = 0.
+        self.doPoisson = 0
+        self.overlayProcessName = "inclusive"
+        self.overlayHitCollections=[ "TriggerPadUpSimHits", "EcalSimHits"]
+        self.randomSeed=0
+        self.verbosity=3
+
+        self.pass_name = "sim"
+        self.overlay_pass_name = "inclusive"
+        self.collection_name = "inclusive"
+
+#        import time
+#        self.randomSeed = int(time.time())
+        
