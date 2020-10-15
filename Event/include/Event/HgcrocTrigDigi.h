76fc0a50a89bba19aab6d1acb6a6367f01fc263f
Omar Moreno <email@omarmoreno.net>
2020-10-15 11:49:07 -0700

    Fix formatting and apply requested changes.

--- Event/include/Event/HgcrocTrigDigi.h
+++ Event/include/Event/HgcrocTrigDigi.h
@@ -10,19 +10,18 @@
 namespace ldmx {
 
 /**
- * @class HgcrocTrigDigi 
+ * @class HgcrocTrigDigi
  * @brief Contains the trigger output for a single trigger hgcroc channel
  */
 class HgcrocTrigDigi {
- public:
-
+public:
   /**
    * Default Constructor
    *
    * Needed for ROOT dictionary definition,
    * suggested to not use this constructor.
    */
-  HgcrocTrigDigi();
+  HgcrocTrigDigi() = default;
 
   /**
    * Preferred Constructor
@@ -33,7 +32,7 @@ class HgcrocTrigDigi {
    * @param[in] tid raw trigger group ID
    * @param[in] tp trigger primitive value
    */
-  HgcrocTrigDigi(uint32_t tid, uint8_t tp=0);
+  HgcrocTrigDigi(uint32_t tid, uint8_t tp = 0);
 
   /**
    * Destructor
@@ -42,7 +41,7 @@ class HgcrocTrigDigi {
    * dictionary definition, does
    * nothing right now.
    */
-  virtual ~HgcrocTrigDigi();
+  virtual ~HgcrocTrigDigi() = default;
 
   /**
    * Sort the collection to trig digis
@@ -51,43 +50,33 @@ class HgcrocTrigDigi {
    * @param[in] d another digi to compare against
    * @returns true if this ID is less than the other ID
    */
-  bool operator<(const HgcrocTrigDigi &d) {
-    return tid_<d.tid_;
-  }
+  bool operator<(const HgcrocTrigDigi &digi) { return tid_ < digi.tid_; }
 
-  /** 
-   * Get the id of the digi 
+  /**
+   * Get the id of the digi
    * @returns raw trigger ID
    */
-  uint32_t getId() const {
-    return tid_;
-  }
-  
+  uint32_t getId() const { return tid_; }
+
   /**
    * Set the trigger primitive (7 bits) for the given link on a channel
    * @params[in] tp the value of the trigger primitive
    */
-  void setPrimitive(uint8_t tp) {
-    tp_=tp;
-  }
+  void setPrimitive(uint8_t tp) { tp_ = tp; }
 
   /**
    * Get the trigger primitive (7 bits) for the given link on a channel
    * @returns the value of the trigger primitive
    */
-  uint8_t getPrimitive() const {
-    return tp_;
-  }
+  uint8_t getPrimitive() const { return tp_; }
 
-  /** 
+  /**
    * Get the linearized value of the trigger primitive
    * @returns the linearized (unpacked) trigger primitive value
    */
-  uint32_t linearPrimitive() const {
-    return compressed2Linear(getPrimitive());
-  }
-	
-  /** 
+  uint32_t linearPrimitive() const { return compressed2Linear(getPrimitive()); }
+
+  /**
    * Static conversion from 18b linear -> compressed
    *
    * @param[in] lin linearized 18bit ADC value
@@ -95,58 +84,59 @@ class HgcrocTrigDigi {
    */
   static uint8_t linear2Compressed(uint32_t lin);
 
-  /** 
+  /**
    * Static conversion from compressed -> linear 18b
    *
    * @param[in] comp compressed 7bit ADC value
    * @returns equivalent linearized 18bit ADC value
    */
   static uint32_t compressed2Linear(uint8_t comp);
-	
+
   /**
    * Print a description of this object.
    */
   void Print() const;
-  
- private:
+
+  /**
+   * Stream the input digi
+   *
+   * In one line, prints out the ID (in hex),
+   * the primitive (in hex), and the linearized
+   * primitive (in dec).
+   *
+   * @param[in] o ostream to write to
+   * @param[in] d digi to write out
+   * @returns modified ostream
+   */
+  friend std::ostream &operator<<(std::ostream &o, const HgcrocTrigDigi &d);
+
+  /**
+   * Stream the input digi collection
+   *
+   * Prints out each digi member of the collection
+   * on a new line.
+   *
+   * @param[in] o ostream to write to
+   * @param[in] c collection to write out
+   * @returns modified ostream
+   */
+  friend std::ostream &operator<<(std::ostream &o,
+                                  const HgcrocTrigDigiCollection &c);
+
+private:
   /// the raw ID for this trigger channel
   uint32_t tid_{0};
   /// the compressed 7bit trigger primitive value for this channel
   uint8_t tp_{0};
   /// ROOT Dictionary class definition macro
   ClassDef(HgcrocTrigDigi, 1);
-}; 
+};
 
 /**
  * Define the type of collection for trig digis
  */
 typedef std::vector<HgcrocTrigDigi> HgcrocTrigDigiCollection;
 
-} //ldmx
-
-/**
- * Stream the input digi
- *
- * In one line, prints out the ID (in hex),
- * the primitive (in hex), and the linearized
- * primitive (in dec).
- *
- * @param[in] o ostream to write to
- * @param[in] d digi to write out
- * @returns modified ostream
- */
-std::ostream& operator<<(std::ostream& o, const ldmx::HgcrocTrigDigi& d);
-
-/**
- * Stream the input digi collection
- *
- * Prints out each digi member of the collection
- * on a new line.
- *
- * @param[in] o ostream to write to
- * @param[in] c collection to write out
- * @returns modified ostream
- */
-std::ostream& operator<<(std::ostream& o, const ldmx::HgcrocTrigDigiCollection& c);
+} // namespace ldmx
 
 #endif // EVENT_HGCROCTRIGDIGI_H_
--- Tools/include/Tools/HgcrocTriggerCalculations.h
+++ Tools/include/Tools/HgcrocTriggerCalculations.h
@@ -1,37 +1,39 @@
 /**
  * @file HgcrocTriggerCalculations.h
- * @brief Class that contains the Hgcroc Trigger algorithms, used for both Ecal and Hcal
+ * @brief Class that contains the Hgcroc Trigger algorithms, used for both Ecal
+ * and Hcal
  * @author Jeremiah Mans, University of Minnesota
  */
 
-#ifndef HGCROCTRIGGERCALCULATIONS_H_
-#define HGCROCTRIGGERCALCULATIONS_H_
+#ifndef TOOLS_HGCROCTRIGGERCALCULATIONS_H_
+#define TOOLS_HGCROCTRIGGERCALCULATIONS_H_
 
-#include <map>
 #include "Conditions/SimpleTableCondition.h"
+#include <map>
 
 namespace ldmx {
 
 /**
  * Class to wrap around an integer table of conditions.
- * 
- * This hardcodes the column numbers and checks that 
+ *
+ * This hardcodes the column numbers and checks that
  * the hardcoded numbers match the imported columns
  * during construction.
  */
 class HgcrocTriggerConditions {
+
  public:
-  ///column index for ADC pedestal
-  static const unsigned int IADC_PEDESTAL  = 0;
-  ///column index for ADC threshold
+  /// column index for ADC pedestal
+  static const unsigned int IADC_PEDESTAL = 0;
+  /// column index for ADC threshold
   static const unsigned int IADC_THRESHOLD = 1;
-  ///column index for TOT pedestal
-  static const unsigned int ITOT_PEDESTAL  = 2;
-  ///column index for TOT threshold
+  /// column index for TOT pedestal
+  static const unsigned int ITOT_PEDESTAL = 2;
+  /// column index for TOT threshold
   static const unsigned int ITOT_THRESHOLD = 3;
-  ///column index for TOT gain
-  static const unsigned int ITOT_GAIN      = 4;
- 
+  /// column index for TOT gain
+  static const unsigned int ITOT_GAIN = 4;
+
   /**
    * Constructor
    *
@@ -41,53 +43,62 @@ class HgcrocTriggerConditions {
    *
    * @raises Exception if any of the column names from the passed table
    * do not match the hardcoded indices.
-   */  
-  HgcrocTriggerConditions(const IntegerTableCondition&, bool validate=true);
+   */
+  HgcrocTriggerConditions(const IntegerTableCondition &, bool validate = true);
 
-  /** 
-   * get the ADC pedestal 
+  /**
+   * get the ADC pedestal
    *
    * @param[in] id raw ID for specific chip
    * @returns the ADC pedestal for that chip
    */
-  int adc_pedestal(unsigned int id) const { return ict_.get(id,IADC_PEDESTAL); }
+  int adcPedestal(unsigned int id) const {
+    return ict_.get(id, IADC_PEDESTAL);
+  }
 
-  /** 
-   * get the ADC threshold 
+  /**
+   * get the ADC threshold
    *
    * @param[in] id raw ID for specific chip
    * @returns the ADC threshold for that chip
    */
-  int adc_threshold(unsigned int id) const { return ict_.get(id,IADC_THRESHOLD); }
+  int adcThreshold(unsigned int id) const {
+    return ict_.get(id, IADC_THRESHOLD);
+  }
 
-  /** 
-   * get the TOT pedestal 
+  /**
+   * get the TOT pedestal
    *
    * @param[in] id raw ID for specific chip
    * @returns the TOT pedestal for that chip
    */
-  int tot_pedestal(unsigned int id) const { return ict_.get(id,ITOT_PEDESTAL); }
+  int totPedestal(unsigned int id) const {
+    return ict_.get(id, ITOT_PEDESTAL);
+  }
 
-  /** 
-   * get the TOT threshold 
+  /**
+   * get the TOT threshold
    *
    * @param[in] id raw ID for specific chip
    * @returns the TOT threshold for that chip
    */
-  int tot_threshold(unsigned int id) const { return ict_.get(id,ITOT_THRESHOLD); }
+  int totThreshold(unsigned int id) const {
+    return ict_.get(id, ITOT_THRESHOLD);
+  }
 
-  /** 
-   * get the TOT gain 
+  /**
+   * get the TOT gain
    *
    * @param[in] id raw ID for specific chip
    * @returns the TOT gain for that chip
    */
-  int tot_gain(unsigned int id) const { return ict_.get(id,ITOT_GAIN); }
+  int totGain(unsigned int id) const { return ict_.get(id, ITOT_GAIN); }
+
  private:
-  ///reference to the table of conditions storing the chip conditions
-  const IntegerTableCondition& ict_;
-}; //HgcrocTriggerConditions
-    
+  /// reference to the table of conditions storing the chip conditions
+  const IntegerTableCondition &ict_;
+}; // HgcrocTriggerConditions
+
 /**
  * @class HgcrocTriggerCalculations
  * @brief Contains the core logic for the Hgcroc trigger calculations
@@ -100,21 +111,24 @@ class HgcrocTriggerConditions {
  */
 class HgcrocTriggerCalculations {
  public:
-
   /**
-   * Calculates the linear trigger charge for a single precision channel (before 2x2 or 3x3 summing)
-   * @param adc         ADC measurement for the channel 
+   * Calculates the linear trigger charge for a single precision channel (before
+   * 2x2 or 3x3 summing)
+   * @param adc         ADC measurement for the channel
    * @param tot         TOT measurement for the channel or zero if no TOT
    * @param adc_ped     Trigger pedestal to subtract (8 bits, per channel)
-   * @param adc_thresh  ADC threshold for noise-suppression in the trigger path (5 bits, global for ROC)
+   * @param adc_thresh  ADC threshold for noise-suppression in the trigger path
+   * (5 bits, global for ROC)
    * @param tot_ped     TOT pedestal (7 bits, group-of-nine)
-   * @param tot_thresh  TOT threshold, use this fixed value as "TOT" if the TOT is below this value (8 bits, group-of-nine)
-   * @param tot_gain    TOT to charge multiplicative factor (5 bits, integer math, global for ROC)
+   * @param tot_thresh  TOT threshold, use this fixed value as "TOT" if the TOT
+   * is below this value (8 bits, group-of-nine)
+   * @param tot_gain    TOT to charge multiplicative factor (5 bits, integer
+   * math, global for ROC)
    * @returns linear trigger charge from a single precision channel
    */
-  static unsigned int singleChannelCharge(int adc, int tot,
-                                          int adc_ped, int adc_thresh,
-                                          int tot_ped, int tot_thresh, int tot_gain);
+  static unsigned int singleChannelCharge(int adc, int tot, int adc_ped,
+                                          int adc_thresh, int tot_ped,
+                                          int tot_thresh, int tot_gain);
 
   /**
    * Construct the chip trigger calculator
@@ -128,13 +142,15 @@ class HgcrocTriggerCalculations {
    *
    * @param[in] ict table of chip conditions
    */
-  HgcrocTriggerCalculations(const IntegerTableCondition& ict);
+  HgcrocTriggerCalculations(const IntegerTableCondition &ict);
 
-  /** 
-   * Determine the linear charge for the given channel, using the calibration information, 
-   * and add it to the map of trigger channel IDs to linear charge measurements.
+  /**
+   * Determine the linear charge for the given channel, using the calibration
+   * information, and add it to the map of trigger channel IDs to linear charge
+   * measurements.
    *
-   * @see singleChannelCharge for how the precision channel measurement is converted to a linear trig-digi charge
+   * @see singleChannelCharge for how the precision channel measurement is
+   * converted to a linear trig-digi charge
    * @param id Precision channel id (used to lookup in the conditions table)
    * @param tid Trigger channel id
    * @param adc ADC measurement of precision channel if not TOT complete
@@ -142,29 +158,34 @@ class HgcrocTriggerCalculations {
    */
   void addDigi(unsigned int id, unsigned int tid, int adc, int tot);
 
-
-  /** 
-   * Convert the linear charges to compressed charges, with a division depending on the number of cells summed by HGCROC 
+  /**
+   * Convert the linear charges to compressed charges, with a division depending
+   * on the number of cells summed by HGCROC
    *
    * Fills the map of trigger channel IDs to compressed charge measurements.
    * Some of the lowest order bits are dropped during compression in order
    * to effectively reach the necessary dynamic range. The number of these
    * bits that are dropped depends on the number of cells in each trigger
    * grouping.
-   *  - 9 --> drop 8 lowest order bits --> multiply unpacked charge by 8 to get full amount
-   *  - 4 --> drop 4 lowest order bits --> mutliply unpacked charge by 4 to get full amount
+   *  - 9 --> drop 8 lowest order bits --> multiply unpacked charge by 8 to get
+   * full amount
+   *  - 4 --> drop 4 lowest order bits --> mutliply unpacked charge by 4 to get
+   * full amount
    *
    * @raises Exception if the input cells_per_trig is not 4 or 9.
    * @param cells_per_trig Valid values are 4 or 9
    */
   void compressDigis(int cells_per_trig);
 
-  /** 
+  /**
    * Access the map of trigger ids to compressed energies
-   * @returns const reference to the map of trigger channel ID to compressed charge measurements
+   * @returns const reference to the map of trigger channel ID to compressed
+   * charge measurements
    */
-  const std::map<unsigned int, uint8_t>& compressedEnergies() const { return compressedCharge_; }
-  
+  const std::map<unsigned int, uint8_t> &compressedEnergies() const {
+    return compressedCharge_;
+  }
+
  private:
   /** The conditions to be used */
   HgcrocTriggerConditions conditions_;
@@ -172,8 +193,8 @@ class HgcrocTriggerCalculations {
   std::map<unsigned int, unsigned int> linearCharge_;
   /** A map of trigger channel id to compressed charge */
   std::map<unsigned int, uint8_t> compressedCharge_;
-}; //HgcrocTriggerCalculations
+}; // HgcrocTriggerCalculations
 
-} //ldmx
+} // namespace ldmx
 
-#endif // HGCROCTRIGGERCALCULATIONS_H_
+#endif // TOOLS_HGCROCTRIGGERCALCULATIONS_H_
