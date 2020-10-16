1a7dae9db7344da50973fb24e1b24d9868d47267
Omar Moreno <email@omarmoreno.net>
2020-10-15 17:10:26 -0700

    Change library name to TrigScint.

--- python/trigScint.py
+++ python/trigScint.py
@@ -4,9 +4,9 @@ Sets all parameters to reasonable defaults.
 
 Examples
 --------
-    from LDMX.EventProc.trigScint import TrigScintDigiProducer
+    from LDMX.TrigScint.trigScint import TrigScintDigiProducer
     p.sequence.extend([ TrigScintDigiProducer.up() , TrigScintDigiProducer.down() , TrigScintDigiProducer.tagger() ])
-    from LDMX.EventProc.trigScint import TrigScintClusterProducer
+    from LDMX.TrigScint.trigScint import TrigScintClusterProducer
     p.sequence.extend([ TrigScintClusterProducer.up() , TrigScintClusterProducer.down() , TrigScintClusterProducer.tagger() ])
 
 """
@@ -17,7 +17,7 @@ class TrigScintDigiProducer(ldmxcfg.Producer) :
     """Configuration for digitizer for Trigger Scintillators"""
 
     def __init__(self,name) :
-        super().__init__(name,'ldmx::TrigScintDigiProducer','EventProc')
+        super().__init__(name,'ldmx::TrigScintDigiProducer','TrigScint')
 
         self.mean_noise = 0.02
         self.number_of_strips = 50
@@ -57,7 +57,7 @@ class TrigScintClusterProducer(ldmxcfg.Producer) :
     """Configuration for cluster producer for Trigger Scintillators"""
 
     def __init__(self,name) :
-        super().__init__(name,'ldmx::TrigScintClusterProducer','EventProc')
+        super().__init__(name,'ldmx::TrigScintClusterProducer','TrigScint')
 
         self.max_cluster_width = 2
         self.clustering_threshold = 0.  #to add in neighboring channels
@@ -95,7 +95,7 @@ class TrigScintTrackProducer(ldmxcfg.Producer) :
     """Configuration for track producer for Trigger Scintillators"""
 
     def __init__(self,name) :
-        super().__init__(name,'ldmx::TrigScintTrackProducer','EventProc')
+        super().__init__(name,'ldmx::TrigScintTrackProducer','TrigScint')
 
         self.delta_max = 1.0
         self.tracking_threshold = 0.  #to add in neighboring channels
