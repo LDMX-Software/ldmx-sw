ec740a8a0362edd5360526ad53bef78f725dd731
GNiramay <niramaygogate@gmail.com>
2020-10-23 21:54:54 +0530

    Moved QIE digi classes to TrigScint

--- Event/include/Event/TrigScintQIEDigis.h
+++ Event/include/Event/TrigScintQIEDigis.h
@@ -7,104 +7,105 @@
 #ifndef EVENT_TRIGSCINTQIEDIGIS_H
 #define EVENT_TRIGSCINTQIEDIGIS_H
 
-#include"SimQIE.h"
 #include "TObject.h" //For ClassDef
 
 namespace ldmx {
 
-/**
- * @class SimQIE
- * @brief class for simulating QIE chip output
- * @note This should be initialized only once per simulation
- */
-class TrigScintQIEDigis
-{
- public:
-
   /**
-   * constructor
-   * @param maxTS_ no. of time samples to be digitized
-   * @param pl instance of pulse or its daughters
-   * @param pd pedestal level
-   * @param ns noise level
-   * 
-   * @note SimQIE instance is initialized automatically
+   * @class TrigScintQIEDigis
+   * @brief class for storing QIE output
    */
-  TrigScintQIEDigis(int maxTS_,QIEInputPulse* pl, float pd, float ns);
+  class TrigScintQIEDigis
+  {
+  public:
 
-  /**
-   * Defaut constructor
-   * @param maxTS_ no. of time samples to be digitized
-   * @param pl instance of pulse or its daughters
-   * @param sm instance of SimQIE
-   *
-   * @note The most preferred way of initialization
-   */
-  TrigScintQIEDigis(int maxTS_,QIEInputPulse* pl, SimQIE* sm);
+    /**
+     * constructor
+     * @param maxTS_ no. of time samples to be digitized
+     */
+    TrigScintQIEDigis(int maxTS_);
 
-  /**
-   * Default constructor
-   */
-  TrigScintQIEDigis();
-
-  /**
-   * Default constructor
-   */
-  ~TrigScintQIEDigis(){};
-
-  /**
-   * Get ADCs of all time samples
-   */
-  std::vector<int> GetADC(){return(ADCs);}
-
-  /**
-   * Get TDCs of all time samples
-   */
-  std::vector<int> GetTDC(){return(TDCs);}
-
-  /**
-   * Get Cap IDs of all time samples
-   */
-  std::vector<int> GetCID(){return(CIDs);}
-
+    /**
+     * Default constructor
+     */
+    TrigScintQIEDigis(){};
   
-  /**
-   * Print ifo about the class
-   * @note required by Event/include/Event/EventDef.h
-   */
-  void Print(Option_t *option = "") const;
+    /**
+     * Default destructor
+     */
+    ~TrigScintQIEDigis(){};
+  
+    /**
+     * Print ifo about the class
+     * @note required by Event/include/Event/EventDef.h
+     */
+    void Print(Option_t *option = "") const;
 
-  /**
-   * A dummy function
-   * @note required by Event/include/Event/EventDef.h
-   */
-  void Clear(Option_t *option = "");
+    /**
+     * A dummy function
+     * @note required by Event/include/Event/EventDef.h
+     */
+    void Clear(Option_t *option = "");
 
-  /**
-   * A dummy operator overloading
-   * @note required for declaring std::vector<> in Event/include/Event/EventDef.h
-   */  
-  bool operator < ( const TrigScintQIEDigis &rhs ) const
-  { return this->chanID < rhs.chanID;}
+    /**
+     * A dummy operator overloading
+     * @note required for declaring std::vector<> in Event/include/Event/EventDef.h
+     */  
+    bool operator < ( const TrigScintQIEDigis &rhs ) const
+    { return this->chanID < rhs.chanID;}
 
-  // private:
-  // no. of time samples stored
-  int maxTS;
-  // channel ID
-  int chanID;
-  // Net input no. of PEs
-  int truePE;
-  // Whether or not
-  bool IsNoisy; 
+    /// no. of time samples stored
+    int maxTS;
 
-  // analog to digital counts
-  std::vector<int> ADCs;
-  // Time to Digital counts
-  std::vector<int> TDCs;
-  // capacitor IDs
-  std::vector<int> CIDs;
+    /**
+     * Get ADCs of all time samples
+     */
+    std::vector<int> GetADC(){return(ADCs);}
 
-  ClassDef(TrigScintQIEDigis,1);
-};
+    /**
+     * Get TDCs of all time samples
+     */
+    std::vector<int> GetTDC(){return(TDCs);}
+
+    /**
+     * Get Cap IDs of all time samples
+     */
+    std::vector<int> GetCID(){return(CIDs);}
+
+    /**
+     * Store ADCs of all time samples
+     * @param adc_ array of ADCs
+     */
+    void SetADC(int* adc_);
+
+    /**
+     * Store TDCs of all time samples
+     * @param tdc_ array of TDCs
+     */
+    void SetTDC(int* tdc_);
+
+    /**
+     * Store CIDs of all time samples
+     * @param cid_ array of CIDs
+     */
+    void SetCID(int* cid_);
+
+    // private:
+    /// channel ID
+    int chanID;
+    /// Net input no. of PEs
+    int truePE;
+    /// Whether or not there is noise
+    bool IsNoisy; 
+
+    /// analog to digital counts
+    std::vector<int> ADCs;
+    /// Time to Digital counts
+    std::vector<int> TDCs;
+    /// capacitor IDs
+    std::vector<int> CIDs;
+
+    ClassDef(TrigScintQIEDigis,1);
+  };
 }
 #endif
--- Event/src/TrigScintQIEDigis.cxx
+++ Event/src/TrigScintQIEDigis.cxx
@@ -11,67 +11,38 @@ ClassImp(ldmx::TrigScintQIEDigis);
 
 namespace ldmx {
 
-TrigScintQIEDigis::TrigScintQIEDigis()
-{
-}
+  TrigScintQIEDigis::TrigScintQIEDigis(int maxTS_=5){
+    maxTS = maxTS_;
+  }
 
-// TrigScintQIEDigis::~TrigScintQIEDigis(){}
+  void TrigScintQIEDigis::SetADC(int* adc_){
+    for(int i=0;i<maxTS;i++){
+      ADCs[i] = adc_[i];
+    }
+  }
+  
+  void TrigScintQIEDigis::SetTDC(int* tdc_){
+    for(int i=0;i<maxTS;i++){
+      TDCs[i] = tdc_[i];
+    }
+  }
+  
+  void TrigScintQIEDigis::SetCID(int* cid_){
+    for(int i=0;i<maxTS;i++){
+      CIDs[i] = cid_[i];
+    }
+  }
+  
+  void TrigScintQIEDigis::Print(Option_t* option) const {
+    std::cout<<"TrigScintQIEDigis { "
+	     <<"maxTS= "<<maxTS<<", "
+	     <<"chanID= "<<chanID<<", "
+	     <<"ADC[0]= "<<ADCs[0]<<", "
+	     <<"TDC[0]= "<<TDCs[0]<<", "
+	     <<"CID[0]= "<<TDCs[0]<<", "
+	     <<"}\n";
+  }
 
-TrigScintQIEDigis::TrigScintQIEDigis(int maxTS_,QIEInputPulse* pl, float pd=0, float ns=0)
-{
-  SimQIE* smq;
-  if(ns==0) smq = new SimQIE();
-  else smq = new SimQIE(pd,ns);
-  smq->SetGain();		// by default, 1e+6
-  smq->SetFreq();		// by default, 40
-  maxTS = maxTS_;
-  // ADCs = smq->Out_ADC(pl,maxTS);
-  // TDCs = smq->Out_TDC(pl,maxTS);
-  // CIDs = smq->CapID(pl,maxTS);
-  int * ADC_ = smq->Out_ADC(pl,maxTS);
-  int * TDC_ = smq->Out_TDC(pl,maxTS);
-  int * CID_ = smq->CapID(pl,maxTS);
-
-  std::vector<int>temp1(ADC_,ADC_+maxTS);
-  std::vector<int>temp2(TDC_,TDC_+maxTS);
-  std::vector<int>temp3(CID_,CID_+maxTS);
-
-  ADCs = temp1;
-  TDCs = temp2;
-  CIDs = temp3;
-}
-
-TrigScintQIEDigis::TrigScintQIEDigis(int maxTS_,QIEInputPulse* pl, SimQIE* sm)
-{
-  maxTS = maxTS_;
-  // ADCs = sm->Out_ADC(pl,maxTS);
-  // TDCs = sm->Out_TDC(pl,maxTS);
-  // CIDs = sm->CapID(pl,maxTS);
-
-  int * ADC_ = sm->Out_ADC(pl,maxTS);
-  int * TDC_ = sm->Out_TDC(pl,maxTS);
-  int * CID_ = sm->CapID(pl,maxTS);
-
-  std::vector<int>temp1(ADC_,ADC_+maxTS);
-  std::vector<int>temp2(TDC_,TDC_+maxTS);
-  std::vector<int>temp3(CID_,CID_+maxTS);
-
-  ADCs = temp1;
-  TDCs = temp2;
-  CIDs = temp3;
-}
-
-
-void TrigScintQIEDigis::Print(Option_t* option) const {
-  std::cout<<"TrigScintQIEDigis { "
-           <<"maxTS= "<<maxTS<<", "
-           <<"chanID= "<<chanID<<", "
-           <<"ADC[0]= "<<ADCs[0]<<", "
-           <<"TDC[0]= "<<TDCs[0]<<", "
-           <<"CID[0]= "<<TDCs[0]<<", "
-           <<"}\n";
-}
-
-void TrigScintQIEDigis::Clear(Option_t* option){}
+  void TrigScintQIEDigis::Clear(Option_t* option){}
 }
 // DECLARE_PRODUCER_NS(ldmx, TrigScintQIEDigis);
