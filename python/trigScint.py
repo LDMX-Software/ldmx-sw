c037736ee09d54e99c8f3d3142268b801e79a2e8
GNiramay <niramaygogate@gmail.com>
2020-10-28 18:49:47 +0530

    Fixing the seeding for noise simulation

--- python/trigScint.py
+++ python/trigScint.py
@@ -53,6 +53,46 @@ class TrigScintDigiProducer(ldmxcfg.Producer) :
         return digi
 
 
+class TrigScintQIEDigiProducer(ldmxcfg.Producer) :
+    """Configuration for digitizer for Trigger Scintillators's QIE chip"""
+
+    def __init__(self,name) :
+        super().__init__(name,'ldmx::TrigScintQIEDigiProducer','TrigScint')
+
+        self.mean_noise = 0.02
+        self.number_of_strips = 50
+        self.number_of_arrays = 1
+        self.mev_per_mip = 0.4
+        self.pe_per_mip = 100.
+        self.input_collection="TriggerPadUpSimHits"
+        self.input_pass_name="" #take any pass
+        self.output_collection="trigScintQIEDigisUp"
+        import time
+        self.randomSeed = int(time.time())
+        self.verbose = False
+
+    def up() :
+        """Get the digitizer for the trigger pad upstream of target"""
+        digi = TrigScintQIEDigiProducer( 'trigScintQIEDigisUp' )
+        digi.input_collection = 'TriggerPadUpSimHits'
+        digi.output_collection= 'trigScintQIEDigisUp'
+        return digi
+
+    def down() :
+        """Get the digitizer for the trigger pad downstream of target"""
+        digi = TrigScintQIEDigiProducer( 'trigScintQIEDigisDn' )
+        digi.input_collection = 'TriggerPadDownSimHits'
+        digi.output_collection= 'trigScintQIEDigisDn'
+        return digi
+
+    def tagger() :
+        """Get the digitizer for the trigger pad upstream of tagger"""
+        digi = TrigScintQIEDigiProducer( 'trigScintQIEDigisTag' )
+        digi.input_collection = 'TriggerPadTaggerSimHits'
+        digi.output_collection= 'trigScintQIEDigisTag'
+        return digi
+
+
 class TrigScintClusterProducer(ldmxcfg.Producer) :
     """Configuration for cluster producer for Trigger Scintillators"""
 
