d00fbd3e469a6e98e31370133d339852473fb349
Omar Moreno <email@omarmoreno.net>
2020-11-01 23:18:23 -0800

    Format.

--- include/SimCore/DetectorConstruction.h
+++ include/SimCore/DetectorConstruction.h
@@ -6,84 +6,83 @@
 #include "AuxInfoReader.h"
 
 // Biasing
+#include "SimCore/DarkBremXsecBiasingOperator.h"
 #include "SimCore/ElectroNuclearXsecBiasingOperator.h"
 #include "SimCore/GammaToMuPairXsecBiasingOperator.h"
 #include "SimCore/PhotoNuclearXsecBiasingOperator.h"
-#include "SimCore/DarkBremXsecBiasingOperator.h" 
 
 // Geant4
-#include "G4VUserDetectorConstruction.hh"
 #include "G4GDMLParser.hh"
 #include "G4LogicalVolume.hh"
 #include "G4LogicalVolumeStore.hh"
+#include "G4VUserDetectorConstruction.hh"
 
 /*~~~~~~~~~~~~~~~*/
 /*   Framework   */
 /*~~~~~~~~~~~~~~~*/
-#include "Framework/Configure/Parameters.h" 
+#include "Framework/Configure/Parameters.h"
 
 namespace ldmx {
 
-    /**
-     * @class DetectorConstruction
-     * @brief Implements the Geant4 detector construction
-     *
-     * @note
-     * This class reads in a detector description from a GDML file
-     * using the basic <i>G4GDMLParser</i> and instantiates supplemental
-     * information using the AuxInfoReader.
-     *
-     * @see AuxInfoReader
-     */
-    class DetectorConstruction : public G4VUserDetectorConstruction {
+/**
+ * @class DetectorConstruction
+ * @brief Implements the Geant4 detector construction
+ *
+ * @note
+ * This class reads in a detector description from a GDML file
+ * using the basic <i>G4GDMLParser</i> and instantiates supplemental
+ * information using the AuxInfoReader.
+ *
+ * @see AuxInfoReader
+ */
+class DetectorConstruction : public G4VUserDetectorConstruction {
 
-        public:
+public:
+  /**
+   * Class constructor.
+   * @param theParser GDML parser defining the geometry.
+   */
+  DetectorConstruction(G4GDMLParser *theParser, Parameters &parameters,
+                       ConditionsInterface &ci);
 
-            /**
-             * Class constructor.
-             * @param theParser GDML parser defining the geometry.
-             */
-            DetectorConstruction(G4GDMLParser* theParser, Parameters& parameters, ConditionsInterface& ci);
+  /**
+   * Class destructor.
+   */
+  virtual ~DetectorConstruction();
 
-            /**
-             * Class destructor.
-             */
-            virtual ~DetectorConstruction();
+  /**
+   * Construct the detector.
+   * @return The top volume of the detector.
+   */
+  G4VPhysicalVolume *Construct();
 
-            /**
-             * Construct the detector.
-             * @return The top volume of the detector.
-             */
-            G4VPhysicalVolume *Construct();
+  /**
+   */
+  void ConstructSDandField();
 
-            /**
-             */
-            void ConstructSDandField();
+  /**
+   * Get the detector header.
+   * @return The detector header.
+   */
+  ldmx::DetectorHeader *getDetectorHeader() {
+    return auxInfoReader_->getDetectorHeader();
+  }
 
-            /**
-             * Get the detector header.
-             * @return The detector header.
-             */
-            ldmx::DetectorHeader* getDetectorHeader() {
-                return auxInfoReader_->getDetectorHeader();
-            }
+private:
+  /**
+   * The GDML parser defining the detector.
+   */
+  G4GDMLParser *parser_;
 
-        private:
+  /**
+   * The auxiliary GDML info reader.
+   */
+  AuxInfoReader *auxInfoReader_;
 
-            /**
-             * The GDML parser defining the detector.
-             */
-            G4GDMLParser* parser_;
+  /// The set of parameters used to configure this class
+  Parameters parameters_;
+};
 
-            /**
-             * The auxiliary GDML info reader.
-             */
-            AuxInfoReader* auxInfoReader_;
-            
-            /// The set of parameters used to configure this class
-            Parameters parameters_; 
-    };
-
-}
+} // namespace ldmx
 
 #endif
