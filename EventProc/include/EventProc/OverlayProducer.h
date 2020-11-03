2d5de7e35c6695a9395006ff1b81c9945f70d75b
Lene Kristian Bryngemark <lene.bryngemark@cern.ch>
2020-11-02 16:34:51 -0800

    Add in capability to overlay pileup from neighbouring bunches
    Allows for specifying the max number of electron bunches before and
    after the sim event bunch (a single number, applied forward and
    backward in time relative to the sim event bunch) that a pileup
    event can be located in, and the bunch spacing.

--- EventProc/include/EventProc/OverlayProducer.h
+++ EventProc/include/EventProc/OverlayProducer.h
@@ -41,13 +41,7 @@ namespace ldmx {
 
 	virtual void produce(Event& event) final override;
 
-	//	virtual void onFileOpen();
-
-	//virtual void onFileClose(EventFile&);
-
 	virtual void onProcessStart(); 
-
-	//	virtual void onProcessEnd();
 
   private:
 
@@ -62,28 +56,30 @@ namespace ldmx {
 	 */
 	Event overlayEvent_;
 
-	std::unique_ptr<TTree> overlayTree_;      // overlay events input tree
+	//	std::unique_ptr<TTree> overlayTree_;      // overlay events input tree
 	std::string overlayFileName_;   // overlay events input file name
-	std::string overlayTreeName_;   // overlay events input tre name
+	//	std::string overlayTreeName_;   // overlay events input tre name
 	std::string overlayPassName_;   // overlay events input file name
-	//	    std::unique_ptr<EventFile> simFile_;      // sim file: the file being generated, to be overlaid 
-	EventFile *simFile_;      // sim file: the file being generated, to be overlaid 
+
+	//	EventFile *simFile_;      // sim file: the file being generated, to be overlaid 
 	int lastEvent_{0};         // book keeping of used events
 	int doPoisson_{0};         // or not
 	double poissonMu_{0.};       // av. nOverlay from the given process
-	double timeSigma_{0.};       // spread in time for bunches
-	double timeMean_{0.};       // spread in time for bunches
+	double timeSigma_{0.};       // width of pileup bunch spread in time
+	double timeMean_{0.};        // average position in time of pileup bunches
+	double bunchSpacing_{0.};    // spacing in time between electron bunches 
+	int nBunchesToSample_{0};    // number of bunches before and after the sim event to sample (0 --> all events occur in the same bunch)
+
 	int nEventsTot_{0};        // in the input tree 
 	std::vector <std::string> collections_;     // to loop over and add hits from
-	std::string overlayProcessName_;  // to use for naming the collection where nOverlaidEvents is relayed
-	std::string passName_;          // to use for naming the event bus passengers, mostly a disambiguation
-	std::string simParticleCollName_;          // to use for naming the event bus passengers, mostly a disambiguation
-	std::string simParticlePassName_;          // to use for naming the event bus passengers, mostly a disambiguation
+	//	std::string overlayProcessName_;  // to use for naming the collection where nOverlaidEvents is relayed
+	std::string simPassName_;          // to use for naming the event bus passengers, mostly a disambiguation
+	//	std::string simParticleCollName_;          // to use for naming the event bus passengers, mostly a disambiguation
+	//	std::string simParticlePassName_;          // to use for naming the event bus passengers, mostly a disambiguation
 	int verbosity_;            // control verbosity 
 	std::unique_ptr<TRandom2> rndm_;  // random number generator. TRandom2 slightly (~10%) faster than TRandom3; shorter period but our input files will have way shorter period anyway. 
 	std::unique_ptr<TRandom2> rndmTime_;  // random number generator. TRandom2 slightly (~10%) faster than TRandom3; shorter period but our input files will have way shorter period anyway. 
-	//	    TRandom2 * rndm_{NULL};     
-	int seed_{0};              // random number generator seed. not clear if this needs to be reproducibly independent or can just be clock seeded (using 0).
+	int seed_{0};              // random number generator seed, gets set by central random number generator seeding service 
 
 	// for Ecal, overlay hits need to be added as contribs.
 	// but these are required to be unique, by the Ecal rconstruction code 
