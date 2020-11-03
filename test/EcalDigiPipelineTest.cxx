a78c17b0f51fbf566313fa975e15633d2698c49f
Tom E <tom.eichlersmith@gmail.com>
2020-11-03 12:25:36 -0600

    allow for ntuplizing the test values, test TP generation as well

--- test/EcalDigiPipelineTest.cxx
+++ test/EcalDigiPipelineTest.cxx
@@ -16,6 +16,7 @@
 
 namespace ldmx {
 namespace test {
+namespace ecal {
 
 /**
  * Maximum percent error that a single hit
@@ -37,7 +38,14 @@ static const double MAX_ENERGY_PERCENT_ERROR=2.5;
  * the parameters of EcalFakeSimHits), we know
  * how "fine-grained" the test is.
  */
-static const int NUM_TEST_SIM_HITS=61;
+static const int NUM_TEST_SIM_HITS=795;
+//static const int NUM_TEST_SIM_HITS=61;
+
+/**
+ * Should the sim/rec/tp energies be ntuplized
+ * for your viewing?
+ */
+static const bool NTUPLIZE_ENERGIES=true;
 
 /**
  * @class FakeSimHits
@@ -50,13 +58,15 @@ static const int NUM_TEST_SIM_HITS=61;
 class EcalFakeSimHits : public Producer {
 
         /// Maximum energy to make a simulated hit for [MeV]
-        const double maxEnergy_  = 12.6; //80.;
+        const double maxEnergy_  = 80.;
+        //const double maxEnergy_  = 12.6; 
 
         /**
          * Minimum energy to make a sim hit for [MeV]
          * Needs to be above readout threshold (after internal EcalDigi's calculation)
          */
-        const double minEnergy_ = 6.5; //0.5; 
+        const double minEnergy_ = 0.5;
+        //const double minEnergy_ = 6.5;
 
         /// Difference between energies to make a sim hit for [MeV]
         const double energyStep_ = (maxEnergy_-minEnergy_)/(NUM_TEST_SIM_HITS); //MeV
@@ -110,13 +120,28 @@ class EcalCheckEnergyReconstruction : public Analyzer {
         EcalCheckEnergyReconstruction(const std::string& name,Process& p) : Analyzer( name , p ) { }
         ~EcalCheckEnergyReconstruction() { }
 
+        void onProcessStart() final override {
+
+            getHistoDirectory();
+            ntuple_.create("EcalDigiTest");
+            ntuple_.addVar<float>("EcalDigiTest","SimEnergy");
+            ntuple_.addVar<float>("EcalDigiTest","RecEnergy");
+            ntuple_.addVar<float>("EcalDigiTest","TrigPrimEnergy");
+
+            ntuple_.addVar<int>("EcalDigiTest","DaqDigi");
+            ntuple_.addVar<int>("EcalDigiTest","TrigPrimDigiEncoded");
+            ntuple_.addVar<int>("EcalDigiTest","TrigPrimDigiLinear" );
+
+        }
+
         void analyze(const Event& event) final override {
 
-            std::vector<SimCalorimeterHit> simHits;
-            REQUIRE_NOTHROW(simHits = event.getCollection<SimCalorimeterHit>( "EcalSimHits" ));
+            const auto simHits = event.getCollection<SimCalorimeterHit>( "EcalSimHits" );
 
-            std::vector<EcalHit> recHits;
-            REQUIRE_NOTHROW(recHits = event.getCollection<EcalHit>( "EcalRecHits" ));
+            float truth_energy = simHits.at(0).getEdep();
+            ntuple_.setVar<float>("SimEnergy",truth_energy);
+
+            const auto recHits = event.getCollection<EcalHit>( "EcalRecHits" );
 
             bool found_real_hit = false;
             for ( const auto& hit : recHits ) {
@@ -125,7 +150,8 @@ class EcalCheckEnergyReconstruction : public Analyzer {
                     //not a noise hit
                     //argument to epsilon is maximum relative difference allowed
                     CHECK_FALSE( hit.isNoise() );
-                    CHECK( hit.getAmplitude() == Approx( simHits.at(0).getEdep() ).epsilon(MAX_ENERGY_PERCENT_ERROR/100) );
+                    CHECK( hit.getAmplitude() == Approx(truth_energy).epsilon(MAX_ENERGY_PERCENT_ERROR/100) );
+                    ntuple_.setVar<float>("RecEnergy",hit.getAmplitude());
                     found_real_hit = true;
                 } else {
                     //should be flagged as noise
@@ -136,15 +162,34 @@ class EcalCheckEnergyReconstruction : public Analyzer {
             // did we lose any energies during the processing?
             CHECK( found_real_hit );
 
+            const auto trigDigis{event.getObject<HgcrocTrigDigiCollection>("ecalTrigDigis")};
+            for (const auto& digi : trigDigis ) {
+                //TODO make this work when we include noise in testing
+                
+                float tp_energy = 8 * digi.linearPrimitive() * 320./1024 * 0.130 / (37000.*(0.162/1000));
+                //CHECK( tp_energy == Approx(truth_energy).epsilon(MAX_ENERGY_PERCENT_ERROR/100) );
+                ntuple_.setVar<float>("TrigPrimEnergy",tp_energy);
+                ntuple_.setVar<int>("TrigPrimDigiEncoded",digi.getPrimitive());
+                ntuple_.setVar<int>("TrigPrimDigiLinear",digi.linearPrimitive());
+            }
+
+            const auto digis{event.getObject<HgcrocDigiCollection>("EcalDigis")};
+            for ( unsigned int iDigi = 0; iDigi < digis.getNumDigis(); iDigi++ ) {
+                //TODO make this work when we include noise in testing
+                auto digi = digis.getDigi( iDigi );
+                ntuple_.setVar<int>("DaqDigi",digi.soi().raw());
+            }
+
             return;
         }
 };//EcalCheckEnergyReconstruction
 
+} //ecal
 } //test
 } //ldmx
 
-DECLARE_PRODUCER_NS(ldmx::test,EcalFakeSimHits)
-DECLARE_ANALYZER_NS(ldmx::test,EcalCheckEnergyReconstruction)
+DECLARE_PRODUCER_NS(ldmx::test::ecal,EcalFakeSimHits)
+DECLARE_ANALYZER_NS(ldmx::test::ecal,EcalCheckEnergyReconstruction)
 
 /**
  * Test for the Ecal Digi Pipeline
@@ -167,17 +212,19 @@ TEST_CASE( "Ecal Digi Pipeline test" , "[Ecal][functionality]" ) {
 
     cf << "from LDMX.Framework import ldmxcfg" << std::endl;
     cf << "p = ldmxcfg.Process( 'testEcalDigis' )" << std::endl;
-    cf << "p.maxEvents = " << ldmx::test::NUM_TEST_SIM_HITS << std::endl;
+    cf << "p.maxEvents = " << ldmx::test::ecal::NUM_TEST_SIM_HITS << std::endl;
     cf << "from LDMX.Ecal import ecal_hardcoded_conditions" << std::endl;
-    cf << "from LDMX.Ecal import digi" << std::endl;
+    cf << "from LDMX.Ecal import digi, ecal_trig_digi" << std::endl;
     cf << "p.outputFiles = [ '/tmp/ecal_digi_pipeline_test.root' ]" << std::endl;
+    cf << "p.histogramFile = '" << (ldmx::test::ecal::NTUPLIZE_ENERGIES ? "" : "/tmp/") << "ecal_digi_pipeline_test.root'" << std::endl;
     cf << "from LDMX.Ecal import EcalGeometry" << std::endl;
     cf << "geom = EcalGeometry.EcalGeometryProvider.getInstance()" << std::endl;
     cf << "p.sequence = [" << std::endl;
-    cf << "    ldmxcfg.Producer('fakeSimHits','ldmx::test::EcalFakeSimHits','Ecal')," << std::endl;
+    cf << "    ldmxcfg.Producer('fakeSimHits','ldmx::test::ecal::EcalFakeSimHits','Ecal')," << std::endl;
     cf << "    digi.EcalDigiProducer()," << std::endl;
+    cf << "    ecal_trig_digi.EcalTrigPrimDigiProducer()," << std::endl;
     cf << "    digi.EcalRecProducer()," << std::endl;
-    cf << "    ldmxcfg.Analyzer('checkEcalHits','ldmx::test::EcalCheckEnergyReconstruction','Ecal')," << std::endl;
+    cf << "    ldmxcfg.Analyzer('checkEcalHits','ldmx::test::ecal::EcalCheckEnergyReconstruction','Ecal')," << std::endl;
     cf << "    ]" << std::endl;
 
     /* debug printing during run
