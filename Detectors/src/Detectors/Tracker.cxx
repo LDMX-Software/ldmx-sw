e054e3851906ad500c1fc0d099680e613b99d63b
Omar Moreno <email@omarmoreno.net>
2020-11-06 10:43:57 -0800

    Add DetElement for sensors.

--- Detectors/src/Detectors/Tracker.cxx
+++ Detectors/src/Detectors/Tracker.cxx
@@ -6,8 +6,14 @@ using namespace dd4hep;
 static Ref_t create_tracker(Detector &lcdd, xml::Handle_t xml_handle,
                             SensitiveDetector sens_det) {
 
+  // Set the sensitive detector type
+  sens_det.setType("tracker"); 
+
   // Detector xml handle
   xml::DetElement det_handle = xml_handle;
+  
+  // Create the tracker detector element
+  DetElement tracker(det_handle.nameStr(), det_handle.id());
 
   // Get the dimensions of the tracker envelope and construct a box shape made
   // out of air.  This volume will be used to contain the tracker.
@@ -34,6 +40,10 @@ static Ref_t create_tracker(Detector &lcdd, xml::Handle_t xml_handle,
     for (xml::Collection_t ilayer(xml_module, _U(layer)); ilayer; ++ilayer) {
 
       xml::Component xml_layer(ilayer);
+
+      // Create the detector element for the sensor
+      DetElement layer(tracker, _toString(xml_layer.id()), xml_layer.id()); 
+      layer.setType("si_sensor");  
 
       // Create the box shape representing the sensor.  If a box can't be
       // created, throw an exception.
@@ -63,6 +73,9 @@ static Ref_t create_tracker(Detector &lcdd, xml::Handle_t xml_handle,
       pv = module_assembly.placeVolume(
           layer_vol, Transform3D(rotation, Position(position.x(), position.y(),
                                                     position.z())));
+      pv.addPhysVolID("layer", xml_module.id()); 
+      layer.setPlacement(pv);
+       
     }
 
     // Get the position of the module and place it inside of the tracker
@@ -72,9 +85,6 @@ static Ref_t create_tracker(Detector &lcdd, xml::Handle_t xml_handle,
                              Position(module_position.x(), module_position.y(),
                                       module_position.z()));
   }
-
-  // Create the tracker detector element
-  DetElement tracker(det_handle.nameStr(), det_handle.id());
 
   // Get the global position of the tracker envelope and place it in the mother
   // volume.
