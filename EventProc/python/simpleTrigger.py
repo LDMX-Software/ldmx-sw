137be59c3e49b45202fe8a1173b20aacae1beb6e
Lene Kristian Bryngemark <lene.bryngemark@cern.ch>
2020-11-24 14:47:30 -0800

    Adding multielectron capabilities to the existing TriggerProcessor
    This puts the multielectron stuff into the existing TriggerProcessor
    rather than in its own class.

--- EventProc/python/simpleTrigger.py
+++ EventProc/python/simpleTrigger.py
@@ -2,6 +2,29 @@
 
 Sets all parameters to reasonable defaults.
 
+Attributes:
+------------- 
+beamEnergy : float
+    The beam energy in MeV
+thresholds : list of floats
+    The upper limit on Ecal reconstructed hit energy sum (in MeV) allowed for the event to pass the trigger. 
+    Calculated as a sum over the specified number of layers. 
+    The processor assumes that the first element in the list is for 1e, 2nd is for 2e, etc:
+    [ my_cut_for_1e, my_cut_for_2e, ... ]
+    If the electron count exceeds the number of elements, the 1e threshold and a simple formula subtracting a multiple of the beam energy is used. 
+mode : int
+    Legacy parameter for which energy calculation mode to use. Defaults to 0 (layer sums). 
+    mode = 1 is for tower sums, currently not implemented. 
+start_layer : int
+    First layer used in the Ecal energy sum over layers.
+end_layer : int
+    First layer not used in the Ecal energy sum over layers. So, the last used layer is end_layer-1. 
+input_collection : string
+    Name of the Ecal hit collection used as input
+trigger_collection : string
+    Name of the output collection containing the trigger result
+
+
 Examples
 --------
     from LDMX.EventProc.simpleTrigger import simpleTrigger
@@ -11,12 +34,13 @@ Examples
 from LDMX.Framework import ldmxcfg
 
 class TriggerProcessor(ldmxcfg.Producer) :
-    """Configuration for the simple trigger on the ECal"""
+    """Configuration for the (multi-electron aware but simple) trigger on the ECal reco hits"""
 
     def __init__(self,name) :
         super().__init__(name,'ldmx::TriggerProcessor','EventProc')
 
-        self.threshold = 1500.0
+        self.beamEnergy = 4000.
+        self.thresholds = [ 1500.0, 1000. + self.beamEnergy, 500. + 2*self.beamEnergy, 100. + 3*self.beamEnergy ]  #toy something up 
         self.mode = 0
         self.start_layer = 1
         self.end_layer = 20
