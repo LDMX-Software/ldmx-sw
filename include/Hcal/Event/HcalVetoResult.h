95305bf8e9a3c7aac427d2fc2891ec0586975bda
Omar Moreno <email@omarmoreno.net>
2020-12-14 13:18:43 -0800

    Move HcalHit and HcalVetoResult to Hcal module.

---
+++ include/Hcal/Event/HcalVetoResult.h
@@ -0,0 +1,73 @@
+/**
+ * @file HcalVetoResult.h
+ * @brief Class used to encapsulate the results obtained from 
+ *        HcalVetoProcessor.
+ * @author Omar Moreno, SLAC National Accelerator Laboratory
+ */
+
+#ifndef HCAL_EVENT_HCALVETORESULT_H_
+#define HCAL_EVENT_HCALVETORESULT_H_
+
+//----------//
+//   ROOT   //
+//----------//
+#include "TObject.h" //For ClassDef
+
+//----------//
+//   LDMX   //
+//----------//
+#include "Hcal/Event/HcalHit.h"
+
+namespace ldmx { 
+
+    class HcalVetoResult { 
+        
+        public: 
+
+            /** Constructor */
+            HcalVetoResult(); 
+
+            /** Destructor */
+            ~HcalVetoResult(); 
+
+            /** Reset the object. */
+            void Clear(); 
+            
+            /** Print out the object */
+            void Print() const;
+
+            /** Checks if the event passes the Hcal veto. */
+            bool passesVeto() const { return passesVeto_; };
+
+            /** @return The maximum PE HcalHit. */
+            inline HcalHit getMaxPEHit() const { return maxPEHit_; } 
+
+            /**
+             * Sets whether the Hcal veto was passed or not.
+             *
+             * @param passesVeto Veto result. 
+             */
+            inline void setVetoResult(const bool& passesVeto = true) { passesVeto_ = passesVeto; } 
+
+            /**
+             * Set the maximum PE hit.
+             *
+             * @param maxPEHit The maximum PE HcalHit
+             */
+            inline void setMaxPEHit(const HcalHit maxPEHit) { maxPEHit_ = maxPEHit; } 
+
+        private:
+            
+            /** Reference to max PE hit. */
+            HcalHit maxPEHit_; 
+
+            /** Flag indicating whether the event passes the Hcal veto. */
+            bool passesVeto_{false};
+
+        ClassDef(HcalVetoResult, 2); 
+
+    }; // HcalVetoResult
+}
+
+
+#endif // HCAL_EVENT_HCALVETORESULT_H_
