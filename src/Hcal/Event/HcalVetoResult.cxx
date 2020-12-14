8e27748b7407cd69dfb7a0e67962ffbdbdd7b4fe
Omar Moreno <email@omarmoreno.net>
2020-12-14 13:18:43 -0800

    Move HcalHit and HcalVetoResult to Hcal module.

---
+++ src/Hcal/Event/HcalVetoResult.cxx
@@ -0,0 +1,37 @@
+/**
+ * @file HcalVetoResult.h
+ * @brief Class used to encapsulate the results obtained from 
+ *        HcalVetoProcessor.
+ * @author Omar Moreno, SLAC National Accelerator Laboratory
+ */
+
+#include "Hcal/Event/HcalVetoResult.h"
+
+//----------------//
+//   C++ StdLib   //
+//----------------//
+#include <iostream>
+
+//-------------//
+//   ldmx-sw   //
+//-------------//
+#include "Hcal/Event/HcalHit.h"
+
+ClassImp(ldmx::HcalVetoResult)
+
+namespace ldmx {
+            
+    HcalVetoResult::HcalVetoResult() {}
+
+    HcalVetoResult::~HcalVetoResult() {}
+
+    void HcalVetoResult::Clear() { 
+        passesVeto_ = false;
+    }
+
+    void HcalVetoResult::Print() const { 
+        std::cout << "[ HcalVetoResult ]: Passes veto : " 
+                  << " Passes veto: " << passesVeto_ << std::endl;
+        maxPEHit_.Print(); 
+    }
+}
