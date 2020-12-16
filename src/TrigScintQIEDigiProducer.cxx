05ad19bbdbd9cf4e56c2ea13962cce2b3c4b30c3
GNiramay <niramaygogate@gmail.com>
2020-12-16 15:43:44 +0530

    QIE noise, TDC now configurable

--- src/TrigScintQIEDigiProducer.cxx
+++ src/TrigScintQIEDigiProducer.cxx
@@ -6,7 +6,8 @@
 
 namespace ldmx {
 
-  TrigScintQIEDigiProducer::TrigScintQIEDigiProducer(const std::string& name, Process& process) :
+  TrigScintQIEDigiProducer::TrigScintQIEDigiProducer
+  (const std::string& name, Process& process) :
     Producer(name, process) {    
   }
 
@@ -16,23 +17,47 @@ namespace ldmx {
   void TrigScintQIEDigiProducer::configure(Parameters& parameters) {
 
     // Configure this instance of the producer
-    stripsPerArray_   = parameters.getParameter< int >("number_of_strips");
-    numberOfArrays_   = parameters.getParameter< int >("number_of_arrays");
-    meanNoise_        = parameters.getParameter< double >("mean_noise");
-    mevPerMip_        = parameters.getParameter< double >("mev_per_mip");
-    pePerMip_         = parameters.getParameter< double >("pe_per_mip");
-    inputCollection_  = parameters.getParameter< std::string >("input_collection");
-    inputPassName_    = parameters.getParameter< std::string >("input_pass_name" );
-    outputCollection_ = parameters.getParameter< std::string >("output_collection");
-    verbose_          = parameters.getParameter< bool >("verbose");
+    stripsPerArray_   =
+      parameters.getParameter< int >("number_of_strips");
+    numberOfArrays_   =
+      parameters.getParameter< int >("number_of_arrays");
+    meanNoise_        =
+      parameters.getParameter< double >("mean_noise");
+    mevPerMip_        =
+      parameters.getParameter< double >("mev_per_mip");
+    pePerMip_         =
+      parameters.getParameter< double >("pe_per_mip");
+    inputCollection_  =
+      parameters.getParameter< std::string >("input_collection");
+    inputPassName_    =
+      parameters.getParameter< std::string >("input_pass_name" );
+    outputCollection_ =
+      parameters.getParameter< std::string >("output_collection");
+    verbose_          =
+      parameters.getParameter< bool >("verbose");
 
-    toff_overall_     	= parameters.getParameter< double >("toff_overall");
-    maxts_ 	      	= parameters.getParameter< int >("maxts");
-    input_pulse_shape_ 	= parameters.getParameter< std::string >("input_pulse_shape");
+    // QIE specific parameters initialization
+    maxts_ 	      	=
+      parameters.getParameter< int >("maxts");
+    toff_overall_     	=
+      parameters.getParameter< double >("toff_overall");
+    input_pulse_shape_ 	=
+      parameters.getParameter< std::string >("input_pulse_shape");
+    tdc_thr 		=
+      parameters.getParameter< double >("tdc_thr");
+    pedestal 		=
+      parameters.getParameter< double >("pedestal");
+    elec_noise 		=
+      parameters.getParameter< double >("elec_noise");
+    sipm_gain 		=
+      parameters.getParameter< double >("sipm_gain");
+      
     if ( input_pulse_shape_ == "Expo") {
       pulse_params.clear();
-      pulse_params.push_back(parameters.getParameter< double >("expo_k"));
-      pulse_params.push_back(parameters.getParameter< double >("expo_tmax"));
+      pulse_params.push_back
+	(parameters.getParameter< double >("expo_k"));
+      pulse_params.push_back
+	(parameters.getParameter< double >("expo_tmax"));
     }
 
     noiseGenerator_ =std::make_unique<NoiseGenerator>(meanNoise_, false);
@@ -54,7 +79,7 @@ namespace ldmx {
 
   void TrigScintQIEDigiProducer::produce(Event& event) {
 
-    // Need to handle seeding on the first event                                                                     
+    // Need to handle seeding on the first event
     if (!noiseGenerator_->hasSeed()) {
       const auto& rseed = getCondition<RandomNumberSeedService>
 	(RandomNumberSeedService::CONDITIONS_OBJECT_NAME);
@@ -65,14 +90,15 @@ namespace ldmx {
       (RandomNumberSeedService::CONDITIONS_OBJECT_NAME);
       noiseGenerator_->seedGenerator(rseed.getSeed(outputCollection_
 						   +"NoiseGenerator"));
-      // noiseGenerator_->seedGenerator(rseed.getSeed("TrigScintQIEDigiProducer::NoiseGenerator"));
       const auto& rseed2 =
 	getCondition<RandomNumberSeedService>
 	(RandomNumberSeedService::CONDITIONS_OBJECT_NAME);
-      smq = new SimQIE(6,1.5,rseed2.getSeed(outputCollection_+"SimQIE"));
-      smq->SetGain();
+      smq = new SimQIE
+	(pedestal,elec_noise,rseed2.getSeed(outputCollection_+"SimQIE"));
+      smq->SetGain(sipm_gain);
       smq->SetFreq();
       smq->SetNTimeSamples(maxts_);
+      smq->SetTDCThreshold(tdc_thr);
     }
 
     std::map<TrigScintID, int> cellPEs;
@@ -88,8 +114,9 @@ namespace ldmx {
     }
     auto numRecHits{0};
 
-    // looper over sim hits and aggregate energy depositions for each detID
-    const auto simHits{event.getCollection< SimCalorimeterHit >(inputCollection_,inputPassName_)};
+    // loop over sim hits and aggregate energy depositions for each detID
+    const auto simHits{event.getCollection< SimCalorimeterHit >
+	(inputCollection_,inputPassName_)};
     auto particleMap{event.getMap< int, SimParticle >("SimParticles")};
 
     int module{-1};
@@ -98,9 +125,7 @@ namespace ldmx {
 
       TrigScintID id(simHit.getID());
 
-      // Just set the module ID to use for noise hits here.  Given that
-      // we are currently processing a single module at a time, setting
-      // it within the loop shouldn't matter.
+      // set module ID for Edep.
       module = id.module();
       std::vector<float> position = simHit.getPosition();
 
@@ -108,76 +133,38 @@ namespace ldmx {
 	std::cout << id << std::endl;
       }
 
-      // unsigned int detIDRaw = id.raw();
-
-      // // for now, we take am energy weighted average of the hit in each stip to simulate the hit position. 
-      // // AJW: these should be dropped, they are likely to lead to a problem since we can't measure them anyway
-      // // except roughly y and z, which is encoded in the ids.
       if (Edep.find(id) == Edep.end()) {
 
       	// first hit, initialize
       	Edep[id] = simHit.getEdep();
       	numRecHits++;
-
-      } else {
+      }
+      else {
 
       	// not first hit, aggregate, and store the largest radius hit
       	Edep[id] += simHit.getEdep();
       }
 
-      double PulseAmp = random_->Poisson(simHit.getEdep() / mevPerMip_ * pePerMip_);
+      double PulseAmp =
+	random_->Poisson(simHit.getEdep() / mevPerMip_ * pePerMip_);
       ex[id.bar()]->AddPulse(toff_overall_+simHit.getTime(),PulseAmp);
       TrueEdep[id.bar()]+=simHit.getEdep();
     }
 
-    // Create the container to hold the digitized trigger scintillator hits.
+    // A container to hold the digitized trigger scintillator hits.
     std::vector<TrigScintQIEDigis> QDigis;
-    
-    // // loop over detIDs and simulate number of PEs
-    // // int ihit = 0;
-    // for (std::map<TrigScintID, float>::iterator it = Edep.begin(); it != Edep.end(); ++it) {
-    //   TrigScintID id(it->first);
-
-    //   // double depEnergy    = Edep[id];
-    //   // double meanPE       = depEnergy / mevPerMip_ * pePerMip_;
-    //   // cellPEs[id]   = random_->Poisson(meanPE + meanNoise_);
-
-    //   // If a cell has a PE count above threshold, persit the hit.
-    //   if(smq->PulseCut(ex[id.bar()])){
-
-    // 	TrigScintQIEDigis QIEInfo(maxts_);
-    // 	QIEInfo.chanID = id.bar();
-
-    // 	QIEInfo.SetADC(smq->Out_ADC(ex[id.bar()]));
-    // 	QIEInfo.SetTDC(smq->Out_TDC(ex[id.bar()]));
-    // 	QIEInfo.SetCID(smq->CapID(ex[id.bar()]));
-    // 	QIEInfo.truePE = Edep[id] / mevPerMip_ * pePerMip_;
-    // 	QIEInfo.IsNoisy= false;
-
-    // 	QDigis.push_back(QIEInfo);
-    //   }
-
-    //   if (verbose_) {
-
-    // 	std::cout << id << std::endl;
-    // 	std::cout << "Edep: " << Edep[id] << std::endl;
-    // 	// std::cout << "numPEs: " << cellPEs[id] << std::endl;
-    //   }        // end verbose
-    // }
 
     double TotalNoise = meanNoise_*maxts_;
     double SamplingTime = 25;	// 1/sampling freq.
     
     for(int bar_id=0;bar_id<stripsPerArray_;bar_id++) {
 
-      // Noise simulation
+      // Dark current simulation
       int n_noise_pulses = random_->Poisson(TotalNoise);
-      std::cout<<"bar_id=\t"<<bar_id<<"\tnoise pulses=\t"<<n_noise_pulses<<std::endl;
-      // if(n_noise_pulses>0){
-	for(int i=0;i<n_noise_pulses;i++) {
-	  ex[bar_id]->AddPulse(random_->Uniform(0,maxts_*SamplingTime),1);
-	}
-      // }
+      	for(int i=0;i<n_noise_pulses;i++) {
+      	  ex[bar_id]->AddPulse
+	    (random_->Uniform(0,maxts_*SamplingTime),1);
+      	}
 
       // Storing the "good" digis
       if(smq->PulseCut(ex[bar_id])) {
@@ -191,44 +178,9 @@ namespace ldmx {
 
 	QIEInfo.IsNoisy= (TrueEdep[bar_id]==0);
 	// QIEInfo.IsNoisy= (ex[bar_id]->npulses==n_noise_pulses);
-	// std::cout<<"TruePE=\t"<<TrueEdep[bar_id]<<"ex->npulses=\t"<<ex[bar_id]->npulses<<"noise pulses=\t"<<n_noise_pulses<<std::endl;
 	QDigis.push_back(QIEInfo);
       }
     }
-    
-    // // ------------------------------- Noise simulation -------------------------------
-    // int numEmptyCells = stripsPerArray_ - numRecHits; // only simulating for single array until all arrays are merged into one collection
-    // std::vector<double> noiseHits_PE = noiseGenerator_->generateNoiseHits( numEmptyCells );
-
-    // TrigScintID tempID;
-
-    // for (auto& noiseHitPE : noiseHits_PE) {
-
-    //   TrigScintHit hit;
-    //   // generate random ID from remaining cells
-    //   do {
-    // 	tempID = generateRandomID(module);
-    //   } while( Edep.find(tempID) != Edep.end() || 
-    // 	       noiseHitIDs.find(tempID) != noiseHitIDs.end() );
-
-    //   TrigScintID noiseID=tempID;
-
-    //   // Expo* ex = new Expo(pulse_params[0],pulse_params[1],30,noiseHitPE);
-    //   Expo* ex = new Expo(pulse_params[0],pulse_params[1]);
-    //   ex->AddPulse(toff_overall_,noiseHitPE);
-    //   TrigScintQIEDigis QIEInfo(maxts_);
-
-    //   QIEInfo.chanID = noiseID.bar();
-
-    //   QIEInfo.SetADC(smq->Out_ADC(ex));
-    //   QIEInfo.SetTDC(smq->Out_TDC(ex));
-    //   QIEInfo.SetCID(smq->CapID(ex));
-    //   QIEInfo.truePE = noiseHitPE;
-    //   QIEInfo.IsNoisy= true;
-
-    //   QDigis.push_back(QIEInfo); 
-    // }
-    // // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     event.add(outputCollection_, QDigis);
   }
 
