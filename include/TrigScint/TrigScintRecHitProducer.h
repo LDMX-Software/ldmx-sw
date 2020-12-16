e64a129c956060fd31f239bab69f48860892ad12
awhitbeck <whitbeck.andrew@gmail.com>
2020-12-16 15:39:18 -0600

    initial commit of rechit producer

---
+++ include/TrigScint/TrigScintRecHitProducer.h
@@ -0,0 +1,82 @@
+/**
+ * @file TrigScintRecHitProducer.h
+ * @brief Class that builds recHits
+ * @author Andrew Whitbeck, TTU
+ */
+
+#ifndef EVENTPROC_TRIGSCINTDIGIPRODUCER_H
+#define EVENTPROC_TRIGSCINTDIGIPRODUCER_H
+
+/*~~~~~~~~~~*/
+/*   ROOT   */
+/*~~~~~~~~~~*/
+#include "TRandom3.h"
+
+// LDMX
+#include "DetDescr/TrigScintID.h"
+#include "Recon/Event/EventConstants.h"
+#include "TrigScint/Event/TrigScintHit.h"
+#include "SimCore/Event/SimCalorimeterHit.h"
+#include "Tools/NoiseGenerator.h"
+
+/*~~~~~~~~~~~~~~~*/
+/*   Framework   */
+/*~~~~~~~~~~~~~~~*/
+#include "Framework/EventProcessor.h"
+#include "Framework/Configure/Parameters.h" 
+
+namespace ldmx {
+
+/**
+ * @class TrigScintRecHitProducer
+ * @brief Organizes digis into TrigScintHits, linearizes TDC 
+ * and ADC info, and converts amplitudes to PEs
+ 
+ */
+class TrigScintRecHitProducer : public Producer {
+
+ public:
+
+  TrigScintRecHitProducer(const std::string& name, Process& process);
+
+  ~TrigScintRecHitProducer(); 
+
+  /**
+   * Callback for the processor to configure itself from the given set
+   * of parameters.
+   * 
+   * @param parameters ParameterSet for configuration.
+   */
+  void configure(Parameters& parameters) final override;
+
+  void produce(Event& event);
+
+ private:
+
+  /// Class to set the verbosity level.  
+  // TODO: Make use of the global verbose parameter. 
+  bool verbose_{false};
+
+  /// Name of the input collection containing the sim hits
+  std::string inputCollection_;
+
+  /// Name of the pass that the input collection is on (empty string means take any pass)
+  std::string inputPassName_;
+
+  /// Name of the output collection that will be used to stored the
+  /// digitized trigger scintillator hits
+  std::string outputCollection_;
+
+  /// Total MeV per MIP
+  double mevPerMip_{1.40};
+
+  /// Total number of photoelectrons per MIP
+  double pePerMip_{13.5};
+
+  
+
+};
+
+}
+
+#endif
