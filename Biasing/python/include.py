c3ce5a188734c965855c131c9f62a6e36b8007be
tomeichlersmith <eichl008@umn.edu>
2020-12-21 11:21:54 -0600

    storage controller updates and new addModule python function

--- Biasing/python/include.py
+++ Biasing/python/include.py
@@ -3,4 +3,4 @@
 def library() :
     """Attach the name of Biasing library to the process"""
     from LDMX.Framework.ldmxcfg import Process
-    Process.addLibrary('@CMAKE_INSTALL_PREFIX@/lib/libBiasing.so')
+    Process.addModule('Biasing')
