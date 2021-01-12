82eace5963b8b98bbae3773fd2f895afcf9103b1
Christian Herwig <therwig@users.noreply.github.com>
2021-01-11 19:55:36 -0800

    correct data types

--- Algo_HLS/src/data.h
+++ Algo_HLS/src/data.h
@@ -11,9 +11,9 @@ typedef ap_uint<7> tp_t;
 
 typedef ap_uint<18> lin_t;
 
-typedef ap_ufixed<16,2> e_t; // [MeV] (Up to at least 8 GeV)
-typedef ap_fixed<10,1> xy_t; // [mm] (-250 to 250, resolution = a few mm)
-typedef ap_fixed<16,2> pxy_t; // [MeV/c]
+typedef ap_ufixed<16,14> e_t; // [MeV] (Up to at least 8 GeV)
+typedef ap_fixed<10,9> xy_t; // [mm] (-250 to 250, resolution = a few mm)
+typedef ap_fixed<16,14> pxy_t; // [MeV/c]
 
 
 struct EcalTP {
