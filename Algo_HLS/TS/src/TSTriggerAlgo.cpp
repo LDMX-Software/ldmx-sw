47895bc178ec338bbe4d1b5e70193f8726493cb4
Marcelo Vicente <marcelo.vicente@cern.ch>
2021-02-09 15:06:20 -0800

    Split digis into 3 vectors

--- Algo_HLS/TS/src/TSTriggerAlgo.cpp
+++ Algo_HLS/TS/src/TSTriggerAlgo.cpp
@@ -1,25 +1,32 @@
 #include "TSTriggerAlgo.hpp"
 
+//void TSClustering(TSDigi digisUp[48], /* TODO */) {};
+
 // Top-level function
 // It will run every event and all digis from a single event are passed as an argument
-void TSTriggerAlgo(TSDigi digis[TS_PARAM_DIGIS_PER_EVENT], TSTrack tracks[TS_PARAM_MAX_TRACKS]) {
+void TSTriggerAlgo( TSDigi digisUp[TS_PARAM_DIGIS_UP],
+					TSDigi digisDown[TS_PARAM_DIGIS_DOWN],
+					TSDigi digisTag[TS_PARAM_DIGIS_TAG],
+					TSTrack tracks[TS_PARAM_MAX_TRACKS]) {
 #pragma HLS INTERFACE ap_fifo depth=16 port=digis
 #pragma HLS INTERFACE ap_fifo depth=16 port=tracks
 #pragma HLS ARRAY_PARTITION variable=digis complete dim=0
 #pragma HLS PIPELINE II=5
 //#pragma HLS LATENCY max=5
 
+	//TSClustering x3
+
 	// Exponent to linear
 	uint32_t qie_adc[TS_PARAM_DIGIS_PER_EVENT];
 	for (unsigned int i = 0; i < TS_PARAM_DIGIS_PER_EVENT; i++) {
-		#pragma HLS UNROLL
+		//#pragma HLS UNROLL
 		qie_adc[i] = digis[i].getDecodedAdc();
 	}
 
 	// Apply energy threshold cut and TDC must be valid
 	bool above_thr[TS_PARAM_DIGIS_PER_EVENT];
 	for (unsigned int i = 0; i < TS_PARAM_DIGIS_PER_EVENT; i++) {
-		#pragma HLS UNROLL
+		//#pragma HLS UNROLL
 		if (qie_adc[i] > 10000 && digis[i].isTdcValid()) above_thr[i] = true;
 		else above_thr[i] = false;
 	}
@@ -27,13 +34,13 @@ void TSTriggerAlgo(TSDigi digis[TS_PARAM_DIGIS_PER_EVENT], TSTrack tracks[TS_PAR
 	// Trigger if one QIE is above threshold
 	bool filter = false;
 	for (unsigned int i = 0; i < TS_PARAM_DIGIS_PER_EVENT; i++) {
-		#pragma HLS UNROLL
+		//#pragma HLS UNROLL
 		filter |= above_thr[i];
 	}
 
 	// Dummy code, just fills the variables with some data to generate datapath
 	for (unsigned int i = 0; i < TS_PARAM_MAX_TRACKS; i++) {
-		#pragma HLS UNROLL
+		//#pragma HLS UNROLL
 		tracks[i] = TSTrack(i, i, i, i * filter);
 	}
 }
