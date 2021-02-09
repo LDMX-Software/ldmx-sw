ad2c813c3822b1a44fd2ec5bec33f197f64a5833
Marcelo Vicente <marcelo.vicente@cern.ch>
2021-02-09 15:06:20 -0800

    Split digis into 3 vectors

--- Algo_HLS/TS/src/TSTriggerParameters.hpp
+++ Algo_HLS/TS/src/TSTriggerParameters.hpp
@@ -2,7 +2,9 @@
 #define __TS_TRIGGER_PARAMETERS_HPP__
 
 //! Number of input digis per event
-#define TS_PARAM_DIGIS_PER_EVENT 48
+#define TS_PARAM_DIGIS_UP   48
+#define TS_PARAM_DIGIS_DOWN 48
+#define TS_PARAM_DIGIS_TAG  48
 
 //! Maximum number of output tracks
 #define TS_PARAM_MAX_TRACKS 5
