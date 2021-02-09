768bddd6d5d99374420b1d0cb102bb4698c332a4
Tom E <tom.eichlersmith@gmail.com>
2021-02-09 13:03:55 -0600

    allow for PN bias operator to only bias children of primary
    This is done on trunk right now and so we need to have it as an option.

--- src/SimCore/BiasOperators/PhotoNuclear.cxx
+++ src/SimCore/BiasOperators/PhotoNuclear.cxx
@@ -11,6 +11,7 @@ PhotoNuclear::PhotoNuclear(std::string name, const framework::config::Parameters
   threshold_ = p.getParameter<double>("threshold");
   factor_ = p.getParameter<double>("factor");
   down_bias_conv_ = p.getParameter<bool>("down_bias_conv");
+  only_children_of_primary_ = p.getParameter<bool>("only_children_of_primary");
 }
 
 void PhotoNuclear::StartRun() {
@@ -31,6 +32,10 @@ G4VBiasingOperation* PhotoNuclear::ProposeOccurenceBiasingOperation(
             << "Kinetic energy: " << track->GetKineticEnergy()
             << " MeV" << std::endl;*/
 
+  // if we want to only bias children of primary, leave if this track is NOT a child of the primary
+  if (only_children_of_primary_ and track->GetParentID() != 1) return 0;
+
+  // is this track too low energy to be biased?
   if (track->GetKineticEnergy() < threshold_) return 0;
 
   /*std::cout << "[ PhotoNuclearXsecBiasingOperator ]: "
