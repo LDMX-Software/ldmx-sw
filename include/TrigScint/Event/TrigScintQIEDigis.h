5d049f877140cb5de0471bfcb2e64fd98a56778a
Omar Moreno <email@omarmoreno.net>
2021-02-17 17:16:59 -0800

    Format all code so it follows Google's C++ style.

--- include/TrigScint/Event/TrigScintQIEDigis.h
+++ include/TrigScint/Event/TrigScintQIEDigis.h
@@ -7,103 +7,102 @@
 #ifndef TRIGSCINT_EVENT_TRIGSCINTQIEDIGIS_H
 #define TRIGSCINT_EVENT_TRIGSCINTQIEDIGIS_H
 
-#include "TObject.h" //For ClassDef
+#include "TObject.h"  //For ClassDef
 
 namespace trigscint {
 
+/**
+ * @class TrigScintQIEDigis
+ * @brief class for storing QIE output
+ */
+class TrigScintQIEDigis {
+ public:
   /**
-   * @class TrigScintQIEDigis
-   * @brief class for storing QIE output
+   * Default constructor
    */
-  class TrigScintQIEDigis
-  {
-  public:
+  // TrigScintQIEDigis(){};
+  TrigScintQIEDigis() = default;
 
-    /**
-     * Default constructor
-     */
-    // TrigScintQIEDigis(){};
-    TrigScintQIEDigis() = default;
-  
-    /**
-     * Default destructor
-     */
-    ~TrigScintQIEDigis() = default;
-  
-    /**
-     * Print ifo about the class
-     * @note required by EventDef.h
-     */
-    void Print(Option_t *option = "") const;
+  /**
+   * Default destructor
+   */
+  ~TrigScintQIEDigis() = default;
 
-    /**
-     * A dummy function
-     * @note required by Event/include/Event/EventDef.h
-     */
-    void Clear(Option_t *option = "");
+  /**
+   * Print ifo about the class
+   * @note required by EventDef.h
+   */
+  void Print(Option_t *option = "") const;
 
-    /**
-     * A dummy operator overloading
-     * @note required for declaring std::vector<> in EventDef.h
-     */  
-    bool operator < ( const TrigScintQIEDigis &rhs ) const
-    { return this->chanID_ < rhs.chanID_;}
+  /**
+   * A dummy function
+   * @note required by Event/include/Event/EventDef.h
+   */
+  void Clear(Option_t *option = "");
 
-    /**
-     * Get channel ID
-     */
-    int GetChanID() const{return(chanID_);}
+  /**
+   * A dummy operator overloading
+   * @note required for declaring std::vector<> in EventDef.h
+   */
+  bool operator<(const TrigScintQIEDigis &rhs) const {
+    return this->chanID_ < rhs.chanID_;
+  }
 
-    /**
-     * Get ADCs of all time samples
-     */
-    std::vector<int> GetADC() const{return(adcs_);}
+  /**
+   * Get channel ID
+   */
+  int GetChanID() const { return (chanID_); }
 
-    /**
-     * Get tdcs of all time samples
-     */
-    std::vector<int> GetTDC() const{return(tdcs_);}
+  /**
+   * Get ADCs of all time samples
+   */
+  std::vector<int> GetADC() const { return (adcs_); }
 
-    /**
-     * Get Cap IDs of all time samples
-     */
-    std::vector<int> GetCID() const{return(cids_);}
+  /**
+   * Get tdcs of all time samples
+   */
+  std::vector<int> GetTDC() const { return (tdcs_); }
 
-    /**
-     * Store the channel ID
-     */
-    void SetChanID(int chanid) {chanID_ = chanid;}
+  /**
+   * Get Cap IDs of all time samples
+   */
+  std::vector<int> GetCID() const { return (cids_); }
 
-    /**
-     * Store adcs of all time samples
-     * @param adc_ array of adcs
-     */
-    void SetADC(std::vector<int> adc) {adcs_ = adc;}
+  /**
+   * Store the channel ID
+   */
+  void SetChanID(int chanid) { chanID_ = chanid; }
 
-    /**
-     * Store tdcs of all time samples
-     * @param tdc_ array of tdcs
-     */
-    void SetTDC(std::vector<int> tdc) {tdcs_ = tdc;}
+  /**
+   * Store adcs of all time samples
+   * @param adc_ array of adcs
+   */
+  void SetADC(std::vector<int> adc) { adcs_ = adc; }
 
-    /**
-     * Store cids of all time samples
-     * @param cid_ array of cids
-     */
-    void SetCID(std::vector<int> cid) {cids_ = cid;}
+  /**
+   * Store tdcs of all time samples
+   * @param tdc_ array of tdcs
+   */
+  void SetTDC(std::vector<int> tdc) { tdcs_ = tdc; }
 
-  private:
-    /// channel ID
-    int chanID_;
+  /**
+   * Store cids of all time samples
+   * @param cid_ array of cids
+   */
+  void SetCID(std::vector<int> cid) { cids_ = cid; }
 
-    /// analog to digital counts
-    std::vector<int> adcs_;
-    /// Time to Digital counts
-    std::vector<int> tdcs_;
-    /// capacitor IDs
-    std::vector<int> cids_;
+ private:
+  /// channel ID
+  int chanID_;
 
-    ClassDef(TrigScintQIEDigis,1);
-  };
-}
+  /// analog to digital counts
+  std::vector<int> adcs_;
+  /// Time to Digital counts
+  std::vector<int> tdcs_;
+  /// capacitor IDs
+  std::vector<int> cids_;
+
+  ClassDef(TrigScintQIEDigis, 1);
+};
+}  // namespace trigscint
 #endif
--- src/TrigScint/SimQIE.cxx
+++ src/TrigScint/SimQIE.cxx
@@ -3,163 +3,159 @@
  * @author Niramay Gogate, Texas Tech University
  */
 
-#include<iostream>
+#include "TrigScint/SimQIE.h"
 #include <exception>
-#include"TMath.h"
-#include"TrigScint/SimQIE.h"
+#include <iostream>
 #include "Framework/Exception/Exception.h"
+#include "TMath.h"
 
 namespace trigscint {
 
-  SimQIE::SimQIE() {}
+SimQIE::SimQIE() {}
 
-  SimQIE::SimQIE(float PD, float SG, uint64_t seed=0) {
-    isnoise_ = true;
-    if (seed==0) {
-      EXCEPTION_RAISE("RandomSeedException",
-		      "QIE Noise generator not seeded (seed=0)");
-    }
-    else{
-      rand_ptr = std::make_unique<TRandom3>(seed);
-      trg_ = rand_ptr.get();
-    }
-    mu_ = PD;
-    sg_ = SG;
+SimQIE::SimQIE(float PD, float SG, uint64_t seed = 0) {
+  isnoise_ = true;
+  if (seed == 0) {
+    EXCEPTION_RAISE("RandomSeedException",
+                    "QIE Noise generator not seeded (seed=0)");
+  } else {
+    rand_ptr = std::make_unique<TRandom3>(seed);
+    trg_ = rand_ptr.get();
   }
-
-  void SimQIE::SetTDCThreshold(float thr) {
-    tdc_thr_ = thr;
-  }
-  
-  void SimQIE::SetGain(float gg) {
-    gain_ = gg*16e-5;		// to convert from 1.6e-19 to fC
-  }
-
-  void SimQIE::SetFreq(float sf) {
-    tau_ = 1000/sf;		// 1/sf -> MHz to ns
-  }
-
-  void SimQIE::SetNTimeSamples(int maxts) {
-    maxts_ = maxts;
-  }
-  // Function to convert charge to ADC count
-  // Working: The method checks in which QIE subrange does the charge lie,
-  // applies a corresponding  gain to it and digitizes it.
-  int SimQIE::Q2ADC(float Charge) {
-    float qq = gain_*Charge;		    // including QIE gain
-    if (isnoise_) qq+=trg_->Gaus(mu_,sg_); // Adding gaussian random noise.
-
-    if (qq<=edges_[0]) return 0;
-    if (qq>=edges_[16]) return 255;
-
-    int ID=8;
-    int a=0;
-    int b=16;
-
-    // Binary search to find the subrange
-    while(b-a!=1) {
-      if (qq>edges_[(a+b)/2]) {
-	a=(a+b)/2;
-      }
-      else b=(a+b)/2;
-    }
-    return 64*(int)(a/4)+nbins_[a%4]+floor((qq-edges_[a])/sense_[a]);
-  }
-
-  // Function to convert ADCs back to charge
-  // The method checks to which QIE subrange does the ADC correspnd to
-  // and returns the mean charge of the correspnding bin in the subrange
-  float SimQIE::ADC2Q(int ADC) {
-    if (ADC<= 0) return -16;
-    if (ADC>= 255) return 350000;
-
-    int rr = ADC/64;		// range
-    int v1 = ADC%64;		// temp. var
-    int ss = 0;			// sub range
-  
-    for(int i=1;i<4;i++) {		// to get the subrange
-      if (v1>nbins_[i]) ss++;
-    }
-    int cc = 64*rr+nbins_[ss];
-    float temp =
-      edges_[4*rr+ss]+
-      (v1-nbins_[ss])*sense_[4*rr+ss]+
-      sense_[4*rr+ss]/2;
-    return(temp/gain_);
-  }
-
-  // Function to return the quantization error for given input charge
-  float SimQIE::QErr(float Q) {
-    if (Q<=edges_[0]) return 0;
-    if (Q>=edges_[16]) return 0;
-
-    int ID=8;
-    int a=0;
-    int b=16;
-    while(b-a!=1) {
-      if (Q>edges_[(a+b)/2]) a=(a+b)/2;
-      else b=(a+b)/2;
-    }
-    return(sense_[a]/(sqrt(12)*Q));
-  }
-
-  // Function that returns an array of ADCs each corresponding to
-  // one time sample
-  std::vector<int> SimQIE::Out_ADC(QIEInputPulse* pp) {
-    std::vector<int> OP;
-    
-    for(int i=0;i<maxts_;i++) {
-      float QQ = pp->Integrate(i*tau_,i*tau_+tau_);
-      OP.push_back(Q2ADC(QQ));
-    }
-    return OP;
-  }
-
-  // Function that returns the digitized time corresponding to
-  // current pulse crossing a specified current threshold
-  int SimQIE::TDC(QIEInputPulse* pp, float T0=0) {
-    float thr2=tdc_thr_/gain_;
-    if (pp->Eval(T0)>thr2) return 62;		// when pulse starts high
-    for(float tt=T0;tt<T0+tau_;tt+=0.1) {
-      if (pp->Eval(tt)>=thr2) return((int)(2*(tt-T0)));
-    }
-    return 63;			// when pulse remains low all along
-  }
-
-  // Function that returns an array of TDCs each corresponding to
-  // one time sample
-  std::vector<int> SimQIE::Out_TDC(QIEInputPulse* pp) {
-    std::vector<int> OP;
-
-    for(int i=0;i<maxts_;i++) {
-      OP.push_back(TDC(pp,tau_*i));
-    }
-    return OP;
-  }
-
-  // Function that returns an array of Caoacitor IDs
-  // each corresponding to one time sample
-  std::vector<int> SimQIE::CapID(QIEInputPulse* pp) {
-    std::vector<int> OP;
-
-    OP.push_back(trg_->Integer(4));
-    for(int i=0;i<maxts_;i++) {
-      OP.push_back((OP[i]+1)%4);
-    }
-    return OP;
-  }
-  
-  bool SimQIE::PulseCut(QIEInputPulse* pulse) {
-    // if(pulse->ampl_.size()==0) return false;
-    if(pulse->GetNPulses()==0) return false;
-
-    // Only keep the pulse if it produces 1 PE
-    // in atleast one of the time samples
-    float thr_in_pes=1.0;
-
-    for(int i=0;i<maxts_;i++){
-      if(pulse->Integrate(i*tau_,i*tau_+tau_)>=thr_in_pes) return true;
-    }
-    return false;
-  }
+  mu_ = PD;
+  sg_ = SG;
 }
+
+void SimQIE::SetTDCThreshold(float thr) { tdc_thr_ = thr; }
+
+void SimQIE::SetGain(float gg) {
+  gain_ = gg * 16e-5;  // to convert from 1.6e-19 to fC
+}
+
+void SimQIE::SetFreq(float sf) {
+  tau_ = 1000 / sf;  // 1/sf -> MHz to ns
+}
+
+void SimQIE::SetNTimeSamples(int maxts) { maxts_ = maxts; }
+// Function to convert charge to ADC count
+// Working: The method checks in which QIE subrange does the charge lie,
+// applies a corresponding  gain to it and digitizes it.
+int SimQIE::Q2ADC(float Charge) {
+  float qq = gain_ * Charge;                 // including QIE gain
+  if (isnoise_) qq += trg_->Gaus(mu_, sg_);  // Adding gaussian random noise.
+
+  if (qq <= edges_[0]) return 0;
+  if (qq >= edges_[16]) return 255;
+
+  int ID = 8;
+  int a = 0;
+  int b = 16;
+
+  // Binary search to find the subrange
+  while (b - a != 1) {
+    if (qq > edges_[(a + b) / 2]) {
+      a = (a + b) / 2;
+    } else
+      b = (a + b) / 2;
+  }
+  return 64 * (int)(a / 4) + nbins_[a % 4] +
+         floor((qq - edges_[a]) / sense_[a]);
+}
+
+// Function to convert ADCs back to charge
+// The method checks to which QIE subrange does the ADC correspnd to
+// and returns the mean charge of the correspnding bin in the subrange
+float SimQIE::ADC2Q(int ADC) {
+  if (ADC <= 0) return -16;
+  if (ADC >= 255) return 350000;
+
+  int rr = ADC / 64;  // range
+  int v1 = ADC % 64;  // temp. var
+  int ss = 0;         // sub range
+
+  for (int i = 1; i < 4; i++) {  // to get the subrange
+    if (v1 > nbins_[i]) ss++;
+  }
+  int cc = 64 * rr + nbins_[ss];
+  float temp = edges_[4 * rr + ss] + (v1 - nbins_[ss]) * sense_[4 * rr + ss] +
+               sense_[4 * rr + ss] / 2;
+  return (temp / gain_);
+}
+
+// Function to return the quantization error for given input charge
+float SimQIE::QErr(float Q) {
+  if (Q <= edges_[0]) return 0;
+  if (Q >= edges_[16]) return 0;
+
+  int ID = 8;
+  int a = 0;
+  int b = 16;
+  while (b - a != 1) {
+    if (Q > edges_[(a + b) / 2])
+      a = (a + b) / 2;
+    else
+      b = (a + b) / 2;
+  }
+  return (sense_[a] / (sqrt(12) * Q));
+}
+
+// Function that returns an array of ADCs each corresponding to
+// one time sample
+std::vector<int> SimQIE::Out_ADC(QIEInputPulse* pp) {
+  std::vector<int> OP;
+
+  for (int i = 0; i < maxts_; i++) {
+    float QQ = pp->Integrate(i * tau_, i * tau_ + tau_);
+    OP.push_back(Q2ADC(QQ));
+  }
+  return OP;
+}
+
+// Function that returns the digitized time corresponding to
+// current pulse crossing a specified current threshold
+int SimQIE::TDC(QIEInputPulse* pp, float T0 = 0) {
+  float thr2 = tdc_thr_ / gain_;
+  if (pp->Eval(T0) > thr2) return 62;  // when pulse starts high
+  for (float tt = T0; tt < T0 + tau_; tt += 0.1) {
+    if (pp->Eval(tt) >= thr2) return ((int)(2 * (tt - T0)));
+  }
+  return 63;  // when pulse remains low all along
+}
+
+// Function that returns an array of TDCs each corresponding to
+// one time sample
+std::vector<int> SimQIE::Out_TDC(QIEInputPulse* pp) {
+  std::vector<int> OP;
+
+  for (int i = 0; i < maxts_; i++) {
+    OP.push_back(TDC(pp, tau_ * i));
+  }
+  return OP;
+}
+
+// Function that returns an array of Caoacitor IDs
+// each corresponding to one time sample
+std::vector<int> SimQIE::CapID(QIEInputPulse* pp) {
+  std::vector<int> OP;
+
+  OP.push_back(trg_->Integer(4));
+  for (int i = 0; i < maxts_; i++) {
+    OP.push_back((OP[i] + 1) % 4);
+  }
+  return OP;
+}
+
+bool SimQIE::PulseCut(QIEInputPulse* pulse) {
+  // if(pulse->ampl_.size()==0) return false;
+  if (pulse->GetNPulses() == 0) return false;
+
+  // Only keep the pulse if it produces 1 PE
+  // in atleast one of the time samples
+  float thr_in_pes = 1.0;
+
+  for (int i = 0; i < maxts_; i++) {
+    if (pulse->Integrate(i * tau_, i * tau_ + tau_) >= thr_in_pes) return true;
+  }
+  return false;
+}
+}  // namespace trigscint
