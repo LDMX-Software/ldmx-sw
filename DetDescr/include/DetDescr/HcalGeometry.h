408beeb9dea29945b1a7e2ccefee9dc6fd82c0b2
cmantill <criss.ms7@gmail.com>
2021-02-20 12:41:41 -0600

    formatting DetDescr

--- DetDescr/include/DetDescr/HcalGeometry.h
+++ DetDescr/include/DetDescr/HcalGeometry.h
@@ -7,10 +7,10 @@
 #define DETDESCR_HCALGEOMETRY_H_
 
 // LDMX
-#include "Framework/Exception/Exception.h"
 #include "DetDescr/HcalID.h"
-#include "Framework/Configure/Parameters.h"
 #include "Framework/ConditionsObject.h"
+#include "Framework/Configure/Parameters.h"
+#include "Framework/Exception/Exception.h"
 
 // ROOT
 #include "TVector3.h"
@@ -24,135 +24,129 @@ class HcalGeometryProvider;
 
 namespace ldmx {
 
-    class HcalGeometryProvider;
-  
-    /**
-     * @class HcalGeometry
-     * @brief Implementation of HCal strip readout
-     *
-     */
-    class HcalGeometry : public framework::ConditionsObject {
+class HcalGeometryProvider;
 
-        public:
-            /**
-	     * Conditions object: 
-	     *  The name of the python configuration calling this class (Hcal/python/HcalGeometry.py)
-	     *  needs to match the CONDITIONS_OBJECT_NAME exactly.
-	     */
-            static constexpr const char* CONDITIONS_OBJECT_NAME{"HcalGeometry"};
+/**
+ * @class HcalGeometry
+ * @brief Implementation of HCal strip readout
+ *
+ */
+class HcalGeometry : public framework::ConditionsObject {
+ public:
+  /**
+   * Conditions object:
+   *  The name of the python configuration calling this class
+   * (Hcal/python/HcalGeometry.py) needs to match the CONDITIONS_OBJECT_NAME
+   * exactly.
+   */
+  static constexpr const char* CONDITIONS_OBJECT_NAME{"HcalGeometry"};
 
-            /**
-             * Class destructor.
-             *
-             * Does nothing because the stl containers clean up automatically.
-             */
-            virtual ~HcalGeometry() { }
+  /**
+   * Class destructor.
+   *
+   * Does nothing because the stl containers clean up automatically.
+   */
+  virtual ~HcalGeometry() {}
 
-            /**
-             * Get a strip center position from a combined hcal ID.
-             *
-             * @throw std::out_of_range if HcalID is not on map.
-             *
-             * @param HcalID 
-             * @return A TVector3 with the X, Y and Z position of the center of the bar.
-             */
-            TVector3 getStripCenterPosition(ldmx::HcalID id) const {
-	      return stripPositionMap_.at(id);
-            }
+  /**
+   * Get a strip center position from a combined hcal ID.
+   *
+   * @throw std::out_of_range if HcalID is not on map.
+   *
+   * @param HcalID
+   * @return A TVector3 with the X, Y and Z position of the center of the bar.
+   */
+  TVector3 getStripCenterPosition(ldmx::HcalID id) const {
+    return stripPositionMap_.at(id);
+  }
 
-            /**
-	     * Get the half total width for a given section(strip) for back(side) Hcal.
-	     * @param section
-	     * @return half total width [mm]
-	     */
-            double getHalfTotalWidth(int isection) const {
-	      return HalfTotalWidth_.at(isection);
-	    }
+  /**
+   * Get the half total width for a given section(strip) for back(side) Hcal.
+   * @param section
+   * @return half total width [mm]
+   */
+  double getHalfTotalWidth(int isection) const {
+    return HalfTotalWidth_.at(isection);
+  }
 
-            /**
-	     * Get the number of sections.
-	     */
-            int getNumSections() const {
-	      return NumSections_;
-	    }
+  /**
+   * Get the number of sections.
+   */
+  int getNumSections() const { return NumSections_; }
 
-            /**
-	     * Get the number of layers for that section.
-	     */
-            int getNumLayers(int isection) const {
-	      return NumLayers_.at(isection);
-	    }
+  /**
+   * Get the number of layers for that section.
+   */
+  int getNumLayers(int isection) const { return NumLayers_.at(isection); }
 
-            /**
-	     * Get the number of strips per layer for that section.
-	     */
-            int getNumStrips(int isection) const {
-	      return NumStrips_.at(isection);
-	    }
+  /**
+   * Get the number of strips per layer for that section.
+   */
+  int getNumStrips(int isection) const { return NumStrips_.at(isection); }
 
-      static HcalGeometry* debugMake(const framework::config::Parameters& p) { return new HcalGeometry(p); }
-    
-        private:
+  static HcalGeometry* debugMake(const framework::config::Parameters& p) {
+    return new HcalGeometry(p);
+  }
 
-            /**
-             * Class constructor, for use only by the provider
-             *
-             * @param ps Parameters to configure the HcalGeometry
-             */
-            HcalGeometry(const framework::config::Parameters &ps);
-            friend class hcal::HcalGeometryProvider;
+ private:
+  /**
+   * Class constructor, for use only by the provider
+   *
+   * @param ps Parameters to configure the HcalGeometry
+   */
+  HcalGeometry(const framework::config::Parameters& ps);
+  friend class hcal::HcalGeometryProvider;
 
-            /**
-	     * Map builder of HcalID and position.
-	     * To build the map we loop over the number of Hcal sections, layers and strips.
-	     * The Hcal sections range from 0 to 4. (We hard-code the number of sections as seen in HcalID)
-	     * The Hcal layers range from 1 to NumLayers_[section].
-	     * The Hcal strips range from 0 to NumStrips_[section].
-	     *
-	     * Odd layers have horizontal strips.
-	     * Even layers have vertical strips.
-	     */
-            void buildStripPositionMap();
+  /**
+   * Map builder of HcalID and position.
+   * To build the map we loop over the number of Hcal sections, layers and
+   * strips. The Hcal sections range from 0 to 4. (We hard-code the number of
+   * sections as seen in HcalID) The Hcal layers range from 1 to
+   * NumLayers_[section]. The Hcal strips range from 0 to NumStrips_[section].
+   *
+   * Odd layers have horizontal strips.
+   * Even layers have vertical strips.
+   */
+  void buildStripPositionMap();
 
-        private:
+ private:
+  /// Verbosity, not configurable but helpful if developing
+  int verbose_{0};
 
-            /// Verbosity, not configurable but helpful if developing
-            int verbose_{0};
+  /// Thickness of scintillator
+  double ThicknessScint_;
 
-	    /// Thickness of scintillator 
-	    double ThicknessScint_;
+  /// Width of Scintillator Strip [mm]
+  double WidthScint_;
 
-	    /// Width of Scintillator Strip [mm]
-            double WidthScint_;
+  /// Half Total Width of Strips [mm]
+  std::vector<double> HalfTotalWidth_;
 
-            /// Half Total Width of Strips [mm]
-            std::vector<double> HalfTotalWidth_;
-      
-            /// Front of HCal relative to world geometry for each section [mm]
-	    std::vector<double> ZeroLayer_;
+  /// Front of HCal relative to world geometry for each section [mm]
+  std::vector<double> ZeroLayer_;
 
-            /// The plane of the zero'th strip of each section [mm] 
-	    std::vector<double> ZeroStrip_;
+  /// The plane of the zero'th strip of each section [mm]
+  std::vector<double> ZeroStrip_;
 
-	    /// Thickness of the layers in each section [mm] 
-	    std::vector<double> LayerThickness_;
+  /// Thickness of the layers in each section [mm]
+  std::vector<double> LayerThickness_;
 
-	    /// Number of layers in each section 
-	    std::vector<int> NumLayers_;
+  /// Number of layers in each section
+  std::vector<int> NumLayers_;
 
-            /// Number of strips per layer in each section
-	    std::vector<int> NumStrips_;
+  /// Number of strips per layer in each section
+  std::vector<int> NumStrips_;
 
-            /// Number of sections
-            int NumSections_;
-      
-            /* 
-             Map of the HcalID position of strip centers relative to world geometry.
-	     The map is not configurable and is calculated by buildStripPositionMap().
-	     */ 
-            std::map<ldmx::HcalID, TVector3> stripPositionMap_;
-    };
-    
-}
+  /// Number of sections
+  int NumSections_;
+
+  /*
+   Map of the HcalID position of strip centers relative to world geometry.
+   The map is not configurable and is calculated by buildStripPositionMap().
+   */
+  std::map<ldmx::HcalID, TVector3> stripPositionMap_;
+};
+
+}  // namespace ldmx
 
 #endif
