8b1773ae1b39543377c2dd3c7ffaa49c3fc42cf3
cmantill <criss.ms7@gmail.com>
2021-02-20 14:58:39 -0600

    add noise and include attenuation/shift in simhits

--- include/Hcal/HcalDigiProducer.h
+++ include/Hcal/HcalDigiProducer.h
@@ -14,22 +14,21 @@
 //----------------//
 //   C++ StdLib   //
 //----------------//
-#include <memory> //for smart pointers
-#include <set> //for tracking used detector IDs
+#include <memory>  //for smart pointers
+#include <set>     //for tracking used detector IDs
 
 //----------//
 //   LDMX   //
 //----------//
-#include "Recon/Event/HgcrocDigiCollection.h"
-#include "Recon/Event/EventConstants.h"
-#include "SimCore/Event/SimCalorimeterHit.h"
-#include "DetDescr/HcalID.h"
 #include "DetDescr/HcalDigiID.h"
 #include "DetDescr/HcalGeometry.h"
+#include "DetDescr/HcalID.h"
 #include "Framework/EventProcessor.h"
-#include "Tools/NoiseGenerator.h"
+#include "Recon/Event/EventConstants.h"
+#include "Recon/Event/HgcrocDigiCollection.h"
+#include "SimCore/Event/SimCalorimeterHit.h"
 #include "Tools/HgcrocEmulator.h"
-
+#include "Tools/NoiseGenerator.h"
 
 namespace hcal {
 
@@ -39,88 +38,82 @@ namespace hcal {
  */
 class HcalDigiProducer : public framework::Producer {
  public:
-
   /**
    * Constructor
    * Makes unique noise generator and injector for this class
    */
   HcalDigiProducer(const std::string& name, framework::Process& process);
 
+  virtual ~HcalDigiProducer() = default;
+
   /**
-   * Destructor
-   * Deletes digi collection if it has been created
+   * Configure this producer from the python configuration.
+   * Sets event constants and configures the noise generator, noise injector,
+   * and pulse function. Creates digi collection
    */
-  virtual ~HcalDigiProducer();
+  virtual void configure(framework::config::Parameters&);
 
-            /**
-             * Configure this producer from the python configuration.
-             * Sets event constants and configures the noise generator, noise injector, and pulse function.
-             * Creates digi collection
-             */
-            virtual void configure(framework::config::Parameters&);
+  /**
+   * Simulates measurement of pulse and creates digi collection for input event.
+   */
+  virtual void produce(framework::Event& event);
 
-            /**
-             * Simulates measurement of pulse and creates digi collection for input event.
-             */
-            virtual void produce(framework::Event& event);
+ private:
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // Python Configuration Parameters
 
-        private:
+  /// input hit collection name
+  std::string inputCollName_;
 
-            ///////////////////////////////////////////////////////////////////////////////////////
-            //Python Configuration Parameters
+  /// input pass name
+  std::string inputPassName_;
 
-            /// input hit collection name
-            std::string inputCollName_;
+  /// output hit collection name
+  std::string digiCollName_;
 
-            /// input pass name
-            std::string inputPassName_;
+  /// Time interval for chip clock in ns
+  double clockCycle_;
 
-            /// output hit collection name
-            std::string digiCollName_;
-	  
-            /// Time interval for chip clock in ns
-            double clockCycle_;
+  /// The gain in ADC units per MeV.
+  double gain_;
 
-            /// The gain in ADC units per MeV. 
-            double gain_;
+  /// The pedestal in ADC units
+  double pedestal_;
 
-            /// The pedestal in ADC units 
-            double pedestal_;
+  /// Depth of ADC buffer.
+  int nADCs_;
 
-            /// Depth of ADC buffer. 
-            int nADCs_; 
+  /// Index for the Sample Of Interest in the list of digi samples
+  int iSOI_;
 
-            /// Index for the Sample Of Interest in the list of digi samples 
-            int iSOI_;
+  /// Readout threshold [ADCs]
+  double readoutThreshold_;
 
-            /// Readout threshold [ADCs]
-            double readoutThreshold_;
+  /// Conversion from energy in MeV to voltage in mV
+  double MeV_;
 
-            /// Conversion from energy in MeV to voltage in mV
-            double MeV_;
+  /// Strip attenuation length [m]
+  double attlength_;
 
-            /// Strip attenuation length [m]
-            double attlength_;
-  
-            ///////////////////////////////////////////////////////////////////////////////////////
-            // Other member variables
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // Other member variables
 
-            /// Put noise into empty channels, not configurable, only helpful in development
-            bool noise_{false};
+  /// Put noise into empty channels, not configurable, only helpful in
+  /// development
+  bool noise_{true};
 
-            /// Hgcroc Emulator to digitize analog voltage signals
-            std::unique_ptr<ldmx::HgcrocEmulator> hgcroc_;
+  /// Hgcroc Emulator to digitize analog voltage signals
+  std::unique_ptr<ldmx::HgcrocEmulator> hgcroc_;
 
-            /// Conversion from time in ns to ticks of the internal clock
-            double ns_;
+  /// Conversion from time in ns to ticks of the internal clock
+  double ns_;
 
-            /// Generates noise hits based off of number of cells that are not hit
-            std::unique_ptr<ldmx::NoiseGenerator> noiseGenerator_;
+  /// Generates noise hits based off of number of cells that are not hit
+  std::unique_ptr<ldmx::NoiseGenerator> noiseGenerator_;
 
-            /// Generates Gaussian noise on top of real hits
-            std::unique_ptr<TRandom3> noiseInjector_;
-
-    };
-}
+  /// Generates Gaussian noise on top of real hits
+  std::unique_ptr<TRandom3> noiseInjector_;
+};
+}  // namespace hcal
 
 #endif
