f73006154958d1a80817b9710992a1abe32ad283
cmantill <criss.ms7@gmail.com>
2021-02-19 23:08:40 -0600

    document geometry class

--- DetDescr/python/HcalGeometry.py
+++ DetDescr/python/HcalGeometry.py
@@ -13,20 +13,25 @@ class HcalReadoutGeometry() :
 
         # parameters that must align with the geometry
         self.detectors_valid = [ ]
-        self.hcalThicknessScint = 0.
-        self.hcalWidthScint = 0.
-        self.hcalZeroLayer = []
-        self.hcalZeroStrip = []
-        self.hcalLayerThickness = []
-        self.hcalNLayers = []
-        self.hcalNStrips = []
-        self.hcalHalfTotalWidthBack = 0.
+        self.ThicknessScint = 0.
+        self.WidthScint = 0.
+        self.ZeroLayer = []
+        self.ZeroStrip = []
+        self.LayerThickness = []
+        self.NumLayers = []
+        self.NumStrips = []
+        self.HalfTotalWidth = []
+        self.NumSections = 0
         self.verbose = 0
 
     def __str__(self) :
         """Stringify this configuration class"""
 
-        s = 'HcalReadoutGeometry'
+        s = 'HcalReadoutGeometry { Scintillator thickness: %.1f mm, width: %.1f mm, and half total width (back Hcal): %.1f mm  \n' % ( 
+                self.ThicknessScint , self.WidthScint , self.HalfTotalWidthBack )
+        s += 'Z positions of zero-th layer %s mm, Z positions of zero-th strip %s mm \n'%(','.join(str(x) for x in self.ZeroLayer),','.join(str(x) for x in self.ZeroStrip))
+        s += 'layer thickness %s mm \n'%(','.join(str(x) for x in self.LayerThickness))
+        s += 'nlayers %s, nstrips %s }'%(' '.join(str(x) for x in self.NumLayers), ' '.join(str(x) for x in self.NumStrips))
         return s
 
 class HcalGeometry() :
@@ -40,19 +45,58 @@ class HcalGeometry() :
     def make_v12(self) :
         """Create the HcalGeometry with the v12 geometry parameters
 
-        Only sets parameters that must align with the gdml
+        Only sets parameters that must align with the gdml constants given in ``.
+
+        - ThicknessScint: Scintillator thickness (in z).
+          In gdml see: `scint_thick`.
+        
+        - WidthScint: Scintillator width (in x).
+          Can be obtained by: hcal_length/n_strips.
+
+        - ZeroLayer: Position of the first layer 
+          For back Hcal: ecal_front_z + hcal_side_dz (in z)
+          For side Hcal: hcal_side_dz/2 (in x/y)
+          In gdml see: `ecal_front_z` and `hcal_side_dz`
+
+        - ZeroStrip: Position of the first strip.
+          For back Hcal: back_transverse_width/2 (in x)
+          For side Hcal: ecal_front_z (in z)  
+          n gdml see: `hcal_back_dx` and `ecal_front_z`
+
+        - LayerThickness: 
+          Layer thickness (in z) 
+          Can be obtained by: absorber_thickness + scint_thickness + 2.0*air_thickness
+          In gdml see: `side_abso_thick` and `back_abso2_thick` and `air_thick`  
+
+        - NumLayers:
+          Number of layers per section.
+
+        - NumStrips: 
+          Number of strips per layer.
+
+        - NumSections:
+          Number of sections. 
+          Encoded in HcalID.
+          Back, Top, Left, Right, Bottom.
+
+        - HalfTotalWidth: 
+          For back Hcal: nstrips * scint_width / 2 (in x/y)
+          For side Hcal: (nlayers * layer_thick + Ecal_dx(y)/2
+          In gdml see: sideTB_dx, sideTB_dy
         """
         self.v12=HcalReadoutGeometry()
 
-        self.v12.hcalThicknessScint = 20.0
-        self.v12.hcalWidthScint = 50.0
-        self.v12.hcalZeroLayer = [220.+600.,600./2,600./2,600./2,600./2]
-        self.v12.hcalZeroStrip = [3100./2,220.,220.,220.,220.]
-        self.v12.hcalLayerThickness = [25. + self.v12.hcalThicknessScint + 2*2.,
-                                       20. + self.v12.hcalThicknessScint + 2*2., 20. + self.v12.hcalThicknessScint + 2*2.,
-                                       20. + self.v12.hcalThicknessScint + 2*2., 20. + self.v12.hcalThicknessScint + 2*2.]
-        self.v12.hcalNLayers = [100,28,28,26,26]
-        self.v12.hcalNStrips = [62,12,12,12,12]
-        self.v12.hcalHalfTotalWidthBack = self.v12.hcalNStrips[0]*self.v12.hcalWidthScint/2.
+        self.v12.ThicknessScint = 20.0
+        self.v12.WidthScint = 50.0
+        self.v12.ZeroLayer = [240.+600.,600./2,600./2,600./2,600./2]
+        self.v12.ZeroStrip = [3100./2,240.,240.,240.,240.]
+        self.v12.LayerThickness = [25. + self.v12.ThicknessScint + 2*2.,
+                                   20. + self.v12.ThicknessScint + 2*2., 20. + self.v12.ThicknessScint + 2*2.,
+                                   20. + self.v12.ThicknessScint + 2*2., 20. + self.v12.ThicknessScint + 2*2.]
+        self.v12.NumLayers = [100,28,28,26,26]
+        self.v12.HalfTotalWidth = [(self.v12.NumStrips[0]*self.v12.WidthScint)/2
+                                   (self.v12.NumLayers[1]*self.v12.LayerThickness[1]+800)/2, (self.v12.NumLayers[2]*self.v12.LayerThickness[2]+800)/2,
+                                   (self.v12.NumLayers[3]*self.v12.LayerThickness[2]+800)/2, (self.v12.NumLayers[3]*self.v12.LayerThickness[2]+800)/2]
+        self.v12.NumStrips = [62,12,12,12,12]
+        self.v12.NumSections = 5
         self.v12.detectors_valid = ["ldmx-det-v12","ldmx-det-v12[.].*","ldmx-det-v9","ldmx-det-v10","ldmx-det-v11"]
-        print(self.v12)
