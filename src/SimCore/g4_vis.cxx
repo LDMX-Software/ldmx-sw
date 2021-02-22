f896252f70fec99a6e87afba1600c8ef2fbecc0b
Tom E <tom.eichlersmith@gmail.com>
2021-02-22 14:21:39 -0600

    compile and launching g4 vis from within container

--- src/SimCore/g4_vis.cxx
+++ src/SimCore/g4_vis.cxx
@@ -3,6 +3,7 @@
 #include "G4RunManager.hh"
 #include "G4GDMLParser.hh"
 #include "G4GeometryManager.hh"
+#include "G4PhysListFactory.hh"
 
 #include "Framework/Configure/Parameters.h"
 #include "Framework/EventProcessor.h"
@@ -26,10 +27,27 @@ int main(int argc, char* argv[]) {
   parser->Read("detector.gdml",false);
   runManager->DefineWorldVolume(parser->GetWorldVolume());
 
+  // required to define a physics list to complete initialization
+  G4PhysListFactory lists;
+  runManager->SetUserInitialization(lists.GetReferencePhysList("FTFP_BERT"));
+
+
+  runManager->Initialize();
+
   // Define (G)UI
   G4UIExecutive* ui = new G4UIExecutive(argc, argv);
   G4VisManager* visManager = new G4VisExecutive;
   visManager->Initialize();
+
+  /*
+  G4VVisManager* cli{G4VVisManager::GetConcreteInstance()};
+  if (cli) {
+    cli->ApplyCommand("/vis/open OGL");
+    cli->ApplyCommand("/vis/drawVolume");
+    cli->ApplyCommand("/vis/viewer/flush");
+  }
+  */
+
   ui->SessionStart();
 
   delete ui;
