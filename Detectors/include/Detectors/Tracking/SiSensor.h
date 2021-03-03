4646b8ac063130edf0eef78d2ab467fa27fb2810
Omar Moreno <email@omarmoreno.net>
2021-03-02 16:36:40 -0800

    Instead of extending dd4hep::DetElement, just make it this class a wrapper which extends the functionality.

--- Detectors/include/Detectors/Tracking/SiSensor.h
+++ Detectors/include/Detectors/Tracking/SiSensor.h
@@ -1,29 +1,48 @@
 #ifndef DETECTORS_TRACKING_SISENSOR_H_
 #define DETECTORS_TRACKING_SISENSOR_H_
 
-/*~~~~~~~~~~~~*/
-/*   DD4hep   */
-/*~~~~~~~~~~~~*/
+//---< DD4hep >---//
 #include "DD4hep/DetElement.h"
 
 namespace detectors {
 namespace tracker {
 
-/**
- */
-class SiSensor : public dd4hep::DetElement {
- public:
+class SiSensor {
+public:
   /**
    * Constructor
-   * @param[in] parent The parent detector element in which this element is
-   * defined in.
-   * @param[in] name The name of this sensor
-   * @param[in] id The detector ID of this sensor.
+   *
+   * @param[in] det_element The DD4hep detector element associated with
+   *  this sensor.
    */
-  SiSensor(dd4hep::DetElement parent, const std::string &name, int id);
+  SiSensor(const dd4hep::DetElement det_element);
 
   /// Destructor
   ~SiSensor() = default;
+
+  /**
+   * Get the ID associted with this sensor.  This is set during the translation
+   * of the DD4hep compact to dd4hep::DetElement objects. The ID is retrieved
+   * from the DetElement object.
+   *
+   * @return sensor ID
+   */
+  int id() const { return det_element_.id(); }
+
+  bool operator<(const SiSensor &rhs) const { return id() < rhs.id(); }
+
+  /// @return The thickness of the sensor in mm
+  double thickness() { return thickness_; }
+
+private:
+  /// DD4hep detector element associated with this sensor object.
+  dd4hep::DetElement det_element_;
+
+  /// The sensor thickness
+  double thickness_{0};
+
+  /// Conversion from cm to mm i.e. from DD4hep to Tracking
+  static float mm;
 };
 } // namespace tracker
 } // namespace detectors
--- Detectors/src/Detectors/Tracking/SiSensor.cxx
+++ Detectors/src/Detectors/Tracking/SiSensor.cxx
@@ -1,12 +1,22 @@
 
 #include "Detectors/Tracking/SiSensor.h"
 
+//---< DD4hep >---//
+#include "DD4hep/Shapes.h"
+
 namespace detectors {
 namespace tracker {
 
-SiSensor::SiSensor(dd4hep::DetElement parent, const std::string &name, int id)
-    : dd4hep::DetElement(parent, name, id) {
-  setType("si_sensor");
+float SiSensor::mm{10};
+
+SiSensor::SiSensor(const dd4hep::DetElement det_element)
+    : det_element_(det_element) {
+
+  // Set the sensor thicknesss. Doing this here once helps avoid the overhead
+  // involved in casting the solid to a box when the thickness is desired.
+  // NOTE: dd4hep::Box::z gives you the half-length so it needs to be
+  //       multiplies by 2.
+  thickness_ = static_cast<dd4hep::Box>(det_element_.solid()).z() * 2 * mm;
 }
 
 } // namespace tracker
