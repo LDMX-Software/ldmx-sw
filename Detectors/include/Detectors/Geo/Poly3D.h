07b5b071ca73a2581b254ead0343c8381760b4f5
Omar Moreno <email@omarmoreno.net>
2021-03-09 18:05:41 -0800

    Add accessors and ostream.

--- Detectors/include/Detectors/Geo/Poly3D.h
+++ Detectors/include/Detectors/Geo/Poly3D.h
@@ -11,7 +11,7 @@ namespace detectors {
 namespace geo {
 
 /**
- * Class representing a polygon. 
+ * Class representing a polygon.
  */
 class Poly3D {
 public:
@@ -27,10 +27,27 @@ public:
   ~Poly3D() = default;
 
   /// @return a vector of Vector3D objects with the vertices of this polygon
-  std::vector<Vector3D> vertices();
+  std::vector<Vector3D> vertices() const { return vertices_; };
 
   /// Update the normal such that the polygon is facing away from the origin.
   void faceOutward();
+
+  /// @return The normal to this polygon
+  Vector3D normal() const { return normal_; }
+
+  /// @return The distance from the origin
+  double distance() const { return distance_; }
+
+  friend std::ostream &operator<<(std::ostream &output, const Poly3D &poly) {
+    output << "[ detectors::geo::Poly3D ]:\n Normal = "
+           << poly.normal().format(simple) << std::endl
+           << " Distance = " << poly.distance() << " mm " << std::endl
+           << " Vertices: " << std::endl;
+    for (auto &vertex : poly.vertices())
+      output << "\t" << vertex.format(simple) << " mm " << std::endl;
+
+    return output;
+  }
 
 private:
   /// Vector containing the vertices
