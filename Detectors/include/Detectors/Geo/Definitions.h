95620fe95ad8163cc7ab010bcbf1fac95f665aa5
Omar Moreno <email@omarmoreno.net>
2021-03-09 18:07:05 -0800

    Define typedef for an eigen based 3D transformation.

--- Detectors/include/Detectors/Geo/Definitions.h
+++ Detectors/include/Detectors/Geo/Definitions.h
@@ -3,9 +3,18 @@
 
 //---< Eigen >---//
 #include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+/// Format for priting out Eigen vectors
+static Eigen::IOFormat simple(Eigen::FullPrecision, 0, ", ", ", ", "", "", "[ ", " ]"); 
 
 /// typedef for a 3D eigen vector
 typedef Eigen::Matrix<double, 3, 1> Vector3D;
+
+/// typedef for a 3D translation
+namespace eigen {
+  typedef Eigen::Transform<double, 3, Eigen::Affine> Transform3D; 
+}
 
 namespace detectors {
 namespace geo {
