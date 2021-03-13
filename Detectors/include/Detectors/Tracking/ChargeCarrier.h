0d0b2c83be11e077ba4b4b92a61ccf8eb8f9e887
Omar Moreno <email@omarmoreno.net>
2021-03-12 16:27:38 -0800

    Fix namespace typo. Add static instances representing an electron and hole.

--- Detectors/include/Detectors/Tracking/ChargeCarrier.h
+++ Detectors/include/Detectors/Tracking/ChargeCarrier.h
@@ -5,7 +5,7 @@
 #include <cmath>
 
 namespace detectors {
-namespace tracker {
+namespace tracking {
 
 /**
  * Class used to describe a charge carrier (Electron or Hole).
@@ -46,58 +46,77 @@ public:
   ~ChargeCarrier() = default;
 
   /// @return The charge of this carrier
-  int charge() { return charge_; }
+  int charge() const { return charge_; }
 
   /// @return The mobility parameter mu_0 at the temperature T [K]
-  double mu0(double temperature) {
+  double mu0(const double temperature) {
     return mu_0_factor_ * pow(temperature / 300.0, mu_0_exp_);
   }
 
   ///  @return The mobility parameter mu_min at the temperature T [K]
-  double muMin(double temperature) {
-    return mu_min_factor_ * pow(temperature / 300.0, mu_min_exponenent_);
+  double muMin(const double temperature) {
+    return mu_min_factor_ * pow(temperature / 300.0, mu_min_exp_);
   }
 
   /// @return The impurity factor at the given temperature T [K]
-  double nRef(double temperature) {
+  double nRef(const double temperature) {
     return n_ref_factor_ * pow(temperature / 300.0, n_ref_exp_);
   }
 
   /// @return The alpha parameter at the given temperature T [K]
-  double alpha(double temperature) {
+  double alpha(const double temperature) {
     return alpha_factor_ * pow(temperature / 300.0, alpha_exp_);
+  }
+
+  /**
+   * Relational operator used to compare two charge carriers by charge.
+   *
+   * @param carrier The charge carrier to compare to this one.
+   * @return True if this objects charge is less than the carrier being compared
+   *  against.
+   */
+  bool operator<(const ChargeCarrier &carrier) const {
+    return charge() < carrier.charge();
   }
 
 private:
   /// Charge The charge of this carrier
   int charge_;
-  
+
   /// Mobility parameter extracted from fit.
   double mu_0_factor_;
-  
+
   /// Mobility parameter extracted from fit.
   double mu_0_exp_;
-  
+
   /// Mobility parameter extracted from fit.
   double mu_min_factor_;
-  
+
   /// Mobility parameter extracted from fit.
   double mu_min_exp_;
-  
+
   /// Impurity parameter
   double n_ref_factor_;
-  
+
   /// Impurity parameter
   double n_ref_exp_;
- 
-  /// Mobility parameter 
+
+  /// Mobility parameter
   double alpha_factor_;
-  
-  /// Mobility parameter 
+
+  /// Mobility parameter
   double alpha_exp_;
 };
 
-} // namespace tracker
+/// Global declaration of electron charge carrier.
+static ChargeCarrier ELECTRON(-1, 1268.0, -2.33, 92.0, -0.57, 1.3e17, 2.4, 0.91,
+                              -0.146);
+
+/// Global declaration of hole charge carrier.
+static ChargeCarrier HOLE(1, 406.9, -2.23, 54.3, -0.57, 2.35e17, 2.4, 0.88,
+                          -0.146);
+
+} // namespace tracking
 } // namespace detectors
 
 #endif // DETECTORS_TRACKER_CHARGECARRIERS_H
