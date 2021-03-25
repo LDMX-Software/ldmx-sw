1ba48c4f481ae5ecbdfd11f337f87d3d8ada0be9
Matthew Solt <mrsolt@alumni.stanford.edu>
2021-03-25 14:16:09 -0400

    clean up debugs

--- src/SimCore/BiasOperators/GammaToMuPair.cxx
+++ src/SimCore/BiasOperators/GammaToMuPair.cxx
@@ -13,37 +13,18 @@ GammaToMuPair::GammaToMuPair(std::string name, const framework::config::Paramete
 
 G4VBiasingOperation* GammaToMuPair::ProposeOccurenceBiasingOperation(
     const G4Track* track, const G4BiasingProcessInterface* callingProcess) {
-  /*std::cout << "[ ElectroNuclearXsecBiasingOperator ]: "
-            << "Track ID: " << track->GetTrackID() << ", "
-            << "Parent ID: " << track->GetParentID() << ", "
-            << "Created within " << track->GetLogicalVolumeAtVertex()->GetName()
-     << ", "
-            << "Currently in volume " << track->GetVolume()->GetName()
-            << std::endl;*/
 
   if (track->GetKineticEnergy() < threshold_) return 0;
-
-  /*std::cout << "[ ElectroNuclearXsecBiasingOperator ]: "
-            << "Calling process: "
-            << callingProcess->GetWrappedProcess()->GetProcessName()
-            << std::endl;*/
 
   std::string currentProcess =
       callingProcess->GetWrappedProcess()->GetProcessName();
   if (currentProcess.compare(this->getProcessToBias()) == 0) {
     G4double interactionLength =
         callingProcess->GetWrappedProcess()->GetCurrentInteractionLength();
-    /*std::cout << "[ ElectroNuclearXsecBiasingOperator ]: "
-              << "EN Interaction length: "
-              << interactionLength << std::endl;*/
 
     double enXsecUnbiased = 1. / interactionLength;
-    /*std::cout << "[ ElectroNuclearXsecBiasingOperator ]: Unbiased EN xsec: "
-              << enXsecUnbiased << std::endl;*/
 
     double enXsecBiased = enXsecUnbiased * factor_;
-    /*std::cout << "[ ElectroNuclearXsecBiasingOperator ]: Biased EN xsec: "
-              << enXsecBiased << std::endl;*/
 
     return BiasedXsec(enXsecBiased);
   } else
