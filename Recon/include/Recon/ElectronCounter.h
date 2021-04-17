c7a197810cfac7a2182bc17274368947e5d0a18c
Omar Moreno <email@omarmoreno.net>
2021-04-16 17:02:22 -0700

    Format, update namespace.

--- Recon/include/Recon/ElectronCounter.h
+++ Recon/include/Recon/ElectronCounter.h
@@ -1,90 +1,85 @@
 
-#ifndef EVENTPROC_ELECTRONCOUNTER_H_
-#define EVENTPROC_ELECTRONCOUNTER_H_
+#ifndef RECON_ELECTRONCOUNTER_H
+#define RECON_ELECTRONCOUNTER_H
 
-/***************/
-/*   ldmx-sw   */
-/***************/
-#include "Event/TrigScintTrack.h"
-#include "Event/TrigScintCluster.h"
+//---< Framework >---//
+#include "Framework/Configure/Parameters.h"
 #include "Framework/EventProcessor.h"
-#include "Framework/Configure/Parameters.h" 
 
-namespace ldmx { 
+//---< TrigScint >---//
+#include "TrigScint/TrigScintCluster.h"
+#include "TrigScint/TrigScintTrack.h"
 
-    /**
-     * Electron counting processor.
-     *
-     * This processor will use objects reconstructed in the trigger scintillators, or truth info on the number of electrons, 
-	 * and set the electron count in the event.
-     */
-    class ElectronCounter : public Producer { 
-    
-        public: 
-        
-            /** 
-             * Constructor.
-             *
-             * @param name Name for this instance of the class.
-             * @param process The Process class associated with EventProcessor, 
-             * provided by the framework.
-             */
-            ElectronCounter(const std::string &name, Process &process); 
+namespace recon {
 
-            /// Destructor
-            ~ElectronCounter(); 
+/**
+ * Electron counting processor.
+ *
+ * This processor will use objects reconstructed in the trigger scintillators,
+ * or truth info on the number of electrons, and set the electron count in the
+ * event.
+ */
+class ElectronCounter : public Producer {
+ public:
+  /**
+   * Constructor.
+   *
+   * @param name Name for this instance of the class.
+   * @param process The Process class associated with EventProcessor,
+   * provided by the framework.
+   */
+  ElectronCounter(const std::string &name, Process &process);
 
-            /** 
-             * Configure the processor using the given user specified parameters.
-             *
-             * The user specified parameters that are availabed are defined 
-             * in the python configuration class. Look at the my_processor.py
-             * module of the EventProc python for the python structure.
-             * 
-             * @param parameters Set of parameters used to configure this processor.
-             */
-            void configure(Parameters& parameters) final override;
+  /// Destructor
+  ~ElectronCounter();
 
-            /**
-             * Process the event and put new data products into it. 
-             *
-             * @param event The event to process.
-             */
-            void produce(Event &event); 
-    
-	private:
+  /**
+   * Configure the processor using the given user specified parameters.
+   *
+   * The user specified parameters that are availabed are defined
+   * in the python configuration class. Look at the my_processor.py
+   * module of the EventProc python for the python structure.
+   *
+   * @param parameters Set of parameters used to configure this processor.
+   */
+  void configure(Parameters &parameters) final override;
 
-	  /**
-	   * The name of the input collection used for counting electrons 
-	   */
-	  std::string inputColl_;
+  /**
+   * Process the event and put new data products into it.
+   *
+   * @param event The event to process.
+   */
+  void produce(Event &event);
 
-	  /**
-	   * The pass name of the input collection used for counting electrons 
-	   */
-	  std::string inputPassName_;
+ private:
+  /**
+   * The name of the input collection used for counting electrons
+   */
+  std::string inputColl_;
 
-	   /**
-	   * The name of the output collection used to save some electron counting variables 
-	   */
-	  std::string outputColl_;
+  /**
+   * The pass name of the input collection used for counting electrons
+   */
+  std::string inputPassName_;
 
-	  /**
-	   * The number of electrons actually simulated in the event
-	   */
-	  int nElectronsSim_{-1};
+  /**
+   * The name of the output collection used to save some electron counting
+   * variables
+   */
+  std::string outputColl_;
 
-	   /**
-	   * Use the number of electrons actually simulated in the event as the electron count 
-	   */
-	  int useSimElectronCount_{false};
+  /**
+   * The number of electrons actually simulated in the event
+   */
+  int nElectronsSim_{-1};
 
-	  
+  /**
+   * Use the number of electrons actually simulated in the event as the electron
+   * count
+   */
+  int useSimElectronCount_{false};
 
-	}; // ElectronCounter
-  
+};  // ElectronCounter
+}  // namespace recon
 
-
-} // ldmx
-
-#endif // EVENTPROC_ELECTRONCOUNTER_H_
+#endif  // RECON_ELECTRONCOUNTER_H
