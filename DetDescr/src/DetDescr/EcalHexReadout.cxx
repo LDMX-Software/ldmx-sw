88323e09d2eec79ee25e78c3d102488f74314090
Hongyin Liu <hongyinliu@Hongyin-Mac-21.local>
2021-05-02 03:31:49 -0700

    This is the version with just the rotation, no structural changes to EcalHexReadout.h (The old branch is changed to iss904-rotate-shift, which contains some structural changes on a few maps having xy positions as a function of z)

--- DetDescr/src/DetDescr/EcalHexReadout.cxx
+++ DetDescr/src/DetDescr/EcalHexReadout.cxx
@@ -3,6 +3,7 @@
 #include "TGeoPolygon.h"
 #include "TGraph.h"
 #include "TList.h"
+#include "TMath.h"
 #include "TMultiGraph.h"
 
 #include <assert.h>
@@ -52,224 +53,113 @@ void EcalHexReadout::buildModuleMap() {
         << moduler_ << "    and gap of " << gap_ << std::endl;
   }
 
-  // module IDs are 0 for ecal center, 1 at 12 o'clock, and clockwise till 6 at
-  // 11 o'clock.
+  // module IDs are 0 for ecal center, 1 at right, and clockwise till 6
   double C_PI = 3.14159265358979323846;  // or TMath::Pi(), #define, atan(), ...
-  modulePositionMap_[0] = std::pair<double, double>(0., 0.);
-  for (unsigned id = 1; id < 7; id++) {
-    double x = (2. * moduler_ + gap_) * sin((id - 1) * (C_PI / 3.));
-    double y = (2. * moduler_ + gap_) * cos((id - 1) * (C_PI / 3.));
-    modulePositionMap_[id] = std::pair<double, double>(x, y);
-    if (verbose_ > 2)
-      std::cout << TString::Format("   id %d is at (%.2f, %.2f)", id, x, y)
-                << std::endl;
-  }
+    
+    modulePositionMap_[0] = std::pair<double, double>(0., 0.);
+      for (unsigned id = 1; id < 7; id++) {
+          double x = (2. * moduler_ + gap_) * cos((1 - id) * (C_PI / 3.));
+          double y = (2. * moduler_ + gap_) * sin((1 - id) * (C_PI / 3.));
+        modulePositionMap_[id] = std::pair<double, double>(x, y);
+        if (verbose_ > 2)
+          std::cout << TString::Format("   id %d is at (%.2f, %.2f)", id, x, y)
+                    << std::endl;
+      }
+    
   if (verbose_ > 0) std::cout << std::endl;
 }
 
 void EcalHexReadout::buildCellMap() {
   /** STRATEGY
-   * use native ROOT HoneyComb method to build large hexagonal grid.
-   * then copy from it the polygons which cover a module.
+   * Hard code
    */
-  TH2Poly gridMap;
+    Double_t a = cellR_;  // cell long radius (or hexagon flat side length)
+    Double_t xCenter = 0.;
+    Double_t yCenter = 0.;
+    Double_t numberOfCellsFlatSide = 12;
+    Double_t x[6], y[6];
 
-  // make hexagonal grid [boundary is rectangle] larger than the module
-  double gridMinX = 0., gridMinY = 0.;  // start at the origin
-  int numXCells = 0, numYCells = 0;
+    //(xLoop,yLoop)-coordinate of the first starting vertex in the "for" loops
+    //(xTemp,yTemp)-the changing starting vertex as loops through columns/rows
+    Double_t xLoop, yLoop, xTemp, yTemp;
+    int yColNum;
+    int ecalMapID = 0;
 
-  // first x-cell is only a half
-  gridMinX -= cellr_;
-  numXCells++;
-  while (gridMinX > -1 * moduleR_) {
-    gridMinX -= 2 * cellr_;  // decrement x by cell center-to-flat diameter
-    numXCells++;
-  }
-  while (gridMinY > -1 * moduler_) {
-    // decrement y by cell center-to-corner radius
-    //  alternate between a full corner-to-corner diameter
-    //  and a side of a cell (center-to-corner radius)
-    if (numYCells % 2 == 0)
-      gridMinY -= 1 * cellR_;
-    else
-      gridMinY -= 2 * cellR_;
-    numYCells++;
-  }
-  // only counted one half of the cells
-  numXCells *= 2;
-  numYCells *= 2;
+    // The hexagon grid divides into three sections: two trapezoids
+    // Cell map starts from top left
 
-  gridMap.Honeycomb(gridMinX, gridMinY, cellR_, numXCells, numYCells);
-
-  if (verbose_ > 0) {
-    std::cout << std::endl;
-    std::cout << std::setprecision(2) << "[buildCellMap] cell rmin: " << cellr_
-              << " cell rmax: " << cellR_ << " (gridMinX,gridMinY) = ("
-              << gridMinX << "," << gridMinY << ")"
-              << " (numXCells,numYCells) = (" << numXCells << "," << numYCells
-              << ")" << std::endl;
-  }
-
-  // copy cells lying within module boundaries to a module grid
-  TListIter next(gridMap.GetBins());  // a TH2Poly is a TList of TH2PolyBin
-  TH2PolyBin* polyBin = 0;
-  TGraph* poly = 0;   // a polygon returned by TH2Poly is a TGraph
-  int ecalMapID = 0;  // ecalMap cell IDs go from 0 to N-1, not equal to
-                      // original grid cell ID.
-  while ((polyBin = (TH2PolyBin*)next())) {
-    // these bins are coming from the honeycomb
-    //  grid so we assume that they are regular
-    //  hexagons i.e. has 6 vertices
-    poly = (TGraph*)polyBin->GetPolygon();
-
-    // decide whether to copy polygon to new map.
-    // use all vertices in case of cut-off edge polygons.
-    int numVerticesInside = 0;
-    double vertex_x[6], vertex_y[6];  // vertices of the cell
-    bool isinside[6];                 // which vertices are inside
-    if (verbose_ > 2) std::cout << "[buildCellMap] Cell vertices" << std::endl;
-    for (unsigned i = 0; i < 6; i++) {
-      poly->GetPoint(i, vertex_x[i], vertex_y[i]);
-      if (verbose_ > 2) {
-        std::cout << "     vtx # " << i << std::endl;
-        std::cout << "     vtx x,y " << vertex_x[i] << " " << vertex_y[i]
-                  << std::endl;
+    // Section 1: Left trapezoid (13cell short base, 24cell long base, 12cell
+    // lateral side
+    xLoop = xCenter - a * 18.0;
+    yLoop = yCenter + a * 18.0 / TMath::Sqrt(3);
+    for (int xCounter = 0; xCounter < numberOfCellsFlatSide;
+         xCounter++) {  // lateral side->12cells
+      yTemp = yLoop;    // resets the temporary variable
+      yColNum =
+          numberOfCellsFlatSide + xCounter + 1;  // number of cells in each column
+      for (int yCounter = 0; yCounter < yColNum; yCounter++) {
+        // Create a cell: go clockwise around the hexagon vertices starting from
+        // the left-most one
+        x[0] = xLoop;
+        y[0] = yTemp;
+        x[1] = x[0] + a / 2.0;
+        y[1] = y[0] + a * TMath::Sqrt(3) / 2.0;
+        x[2] = x[1] + a;
+        y[2] = y[1];
+        x[3] = x[2] + a / 2.0;
+        y[3] = y[0];
+        x[4] = x[2];
+        y[4] = y[3] - a * TMath::Sqrt(3) / 2.0;
+        x[5] = x[1];
+        y[5] = y[4];
+        ecalMap_.AddBin(6, x, y);
+        double cellx = (x[0] + x[3]) / 2;
+        double celly = (y[1] + y[4]) / 2;
+        cellPositionMap_[ecalMapID] = std::pair<double, double>(cellx, celly);
+        ecalMapID++;
+        // Go down
+        yTemp -= a * TMath::Sqrt(3);
       }
-      isinside[i] = isInside(vertex_x[i] / moduleR_, vertex_y[i] / moduleR_);
-      if (isinside[i]) numVerticesInside++;
+      // Increment the starting position from column to column
+      yLoop += a * TMath::Sqrt(3) / 2.0;
+      xLoop += 1.5 * a;
     }
 
-    if (numVerticesInside > 1) {
-      // Include this cell if more than one of its vertices is inside the module
-      // hexagon
-      double actual_x[8], actual_y[8];
-      int num_vertices{0};
-      if (numVerticesInside < 6) {
-        // This cell is stradling the edge of the module
-        // and is NOT cleanly cut by module edge
-
-        if (verbose_ > 1) {
-          std::cout << "[buildCellMap] Polygon " << ecalMapID
-                    << " has vertices poking out of module hexagon."
-                    << std::endl;
-        }
-
-        // loop through vertices
-        for (int i = 0; i < 6; i++) {
-          int up = i == 5 ? 0 : i + 1;
-          int dn = i == 0 ? 5 : i - 1;
-          if (isinside[i] and (not isinside[up] or not isinside[dn])) {
-            // this vertex is inside the module hexagon and is adjacent to a
-            // vertex outside
-            // ==> project this vertex onto the nearest edge of the module
-            // hexagon
-
-            // determine which side of hexagon we should project onto
-            double edge_origin_x, edge_origin_y;
-            double edge_dest_x, edge_dest_y;
-            if (vertex_x[i] < -moduleR_ / 2.) {
-              // sloped edge on negative-x side
-              edge_origin_x = -1. * moduleR_;
-              edge_origin_y = 0.;
-              edge_dest_x = -0.5 * moduleR_;
-              edge_dest_y = moduler_;
-            } else if (vertex_x[i] > moduleR_ / 2.) {
-              // sloped edge on positive-x side
-              edge_origin_x = 0.5 * moduleR_;
-              edge_origin_y = moduler_;
-              edge_dest_x = moduleR_;
-              edge_dest_y = 0.;
-            } else {
-              // flat edge at top
-              edge_origin_x = 0.5 * moduleR_;
-              edge_origin_y = moduler_;
-              edge_dest_x = -0.5 * moduleR_;
-              edge_dest_y = moduler_;
-            }
-
-            // flip to bottom half if below x-axis
-            if (vertex_y[i] < 0) {
-              edge_dest_y *= -1;
-              edge_origin_y *= -1;
-            }
-
-            // get edge slope vector
-            double edge_slope_x = edge_dest_x - edge_origin_x;
-            double edge_slope_y = edge_dest_y - edge_origin_y;
-
-            if (verbose_ > 2) {
-              std::cout
-                  << "Vertex " << i
-                  << " is inside and adjacent to a vertex outside the module."
-                  << std::endl;
-              std::cout << "Working on edge with slope (" << edge_slope_x << ","
-                        << edge_slope_y << ")"
-                        << " and origin (" << edge_origin_x << ","
-                        << edge_origin_y << ")" << std::endl;
-            }
-
-            // project vertices adjacent to the vertex outside the module onto
-            // the module edge
-            double projection_factor =
-                ((vertex_x[i] - edge_origin_x) * edge_slope_x +
-                 (vertex_y[i] - edge_origin_y) * edge_slope_y) /
-                (edge_slope_x * edge_slope_x + edge_slope_y * edge_slope_y);
-
-            double proj_x = edge_origin_x + projection_factor * edge_slope_x;
-            double proj_y = edge_origin_y + projection_factor * edge_slope_y;
-
-            if (not isinside[up]) {
-              // the next point is outside
-              actual_x[num_vertices] = vertex_x[i];
-              actual_y[num_vertices] = vertex_y[i];
-              actual_x[num_vertices + 1] = proj_x;
-              actual_y[num_vertices + 1] = proj_y;
-            } else {
-              // the previous point was outside
-              actual_x[num_vertices] = proj_x;
-              actual_y[num_vertices] = proj_y;
-              actual_x[num_vertices + 1] = vertex_x[i];
-              actual_y[num_vertices + 1] = vertex_y[i];
-            }
-            num_vertices += 2;
-
-            if (verbose_ > 2) {
-              std::cout << "New Vertex " << i << " : (" << vertex_x[i] << ","
-                        << vertex_y[i] << ")" << std::endl;
-            }
-          } else {
-            actual_x[num_vertices] = vertex_x[i];
-            actual_y[num_vertices] = vertex_y[i];
-            num_vertices++;
-          }  // should we project or not
-        }    // loop through vertices
-      } else {
-        // all 6 inside, just copy the vertices over
-        num_vertices = 6;
-        for (int i = 0; i < 6; i++) {
-          actual_x[i] = vertex_x[i];
-          actual_y[i] = vertex_y[i];
-        }
-      }  // if numVerticesInside is less than 5
-
-      // ecalMap_ needs to have its own copy of the polygon TGraph
-      //  otherwise, we get a seg fault when EcalHexReadout is destructed
-      //  because the polygon that was copied over from gridMap is deleted at
-      //  the end of this function
-      ecalMap_.AddBin(num_vertices, actual_x, actual_y);
-
-      double x = (polyBin->GetXMax() + polyBin->GetXMin()) / 2.;
-      double y = (polyBin->GetYMax() + polyBin->GetYMin()) / 2.;
-      if (verbose_ > 1) {
-        std::cout << "[buildCellMap] Copying poly with ID "
-                  << polyBin->GetBinNumber() << " and (x,y) ("
-                  << std::setprecision(2) << x << "," << y << ")" << std::endl;
+    // Section 2: Right trapezoid (12cell short base, 23cell long base, 12cell
+    // lateral side
+    xLoop = xCenter;
+    yLoop = yCenter + a * 11.0 * TMath::Sqrt(3);
+    for (int xCounter = 0; xCounter < numberOfCellsFlatSide; xCounter++) {
+      yTemp = yLoop;
+      yColNum = 2 * numberOfCellsFlatSide - xCounter -
+                1;  // number of cells in each column
+      for (int yCounter = 0; yCounter < yColNum; yCounter++) {
+        // Create a cell: go clockwise around the hexagon vertices starting from
+        // the left-most one
+        x[0] = xLoop;
+        y[0] = yTemp;
+        x[1] = x[0] + a / 2.0;
+        y[1] = y[0] + a * TMath::Sqrt(3) / 2.0;
+        x[2] = x[1] + a;
+        y[2] = y[1];
+        x[3] = x[2] + a / 2.0;
+        y[3] = y[0];
+        x[4] = x[2];
+        y[4] = y[3] - a * TMath::Sqrt(3) / 2.0;
+        x[5] = x[1];
+        y[5] = y[4];
+        ecalMap_.AddBin(6, x, y);
+        double cellx = (x[0] + x[3]) / 2;
+        double celly = (y[1] + y[4]) / 2;
+        cellPositionMap_[ecalMapID] = std::pair<double, double>(cellx, celly);
+        ecalMapID++;
+        // Go down
+        yTemp -= a * TMath::Sqrt(3);
       }
-      // save cell location as center of ENTIRE hexagon
-      cellPositionMap_[ecalMapID] = std::pair<double, double>(x, y);
-      ecalMapID++;  // incrememnt cell ID
-    }               // if num vertices inside is > 1
-  }                 // loop over larger grid spanning module hexagon
+      // Increment the starting position from column to column
+      yLoop -= a * TMath::Sqrt(3) / 2.0;
+      xLoop += 1.5 * a;
+    }
 
   if (verbose_ > 0) std::cout << std::endl;
   return;
