12b21028f5c1c6b96c205d905db3f46943eb5270
Matthew Solt <mrsolt@alumni.stanford.edu>
2021-05-03 17:16:20 -0400

    add K0 Long inelastic biasing operators

---
+++ src/SimCore/BiasOperators/K0LongInelastic.cxx
@@ -0,0 +1,37 @@
+
+#include "SimCore/BiasOperators/K0LongInelastic.h"
+
+namespace simcore {
+namespace biasoperators {
+
+K0LongInelastic::K0LongInelastic(std::string name, const framework::config::Parameters& p)
+    : XsecBiasingOperator(name, p) {
+  volume_ = p.getParameter<std::string>("volume");
+  factor_ = p.getParameter<double>("factor");
+  threshold_ = p.getParameter<double>("threshold");
+}
+
+G4VBiasingOperation* K0LongInelastic::ProposeOccurenceBiasingOperation(
+    const G4Track* track, const G4BiasingProcessInterface* callingProcess) {
+
+  if (track->GetKineticEnergy() < threshold_) return 0;
+
+  std::string currentProcess =
+      callingProcess->GetWrappedProcess()->GetProcessName();
+  if (currentProcess.compare(this->getProcessToBias()) == 0) {
+    G4double interactionLength =
+        callingProcess->GetWrappedProcess()->GetCurrentInteractionLength();
+
+    double k0LongInXsecUnbiased = 1. / interactionLength;
+
+    double k0LongInXsecBiased = k0LongInXsecUnbiased * factor_;
+
+    return BiasedXsec(k0LongInXsecBiased);
+  } else
+    return 0;
+}
+
+}  // namespace biasoperators
+}  // namespace simcore
+
+DECLARE_XSECBIASINGOPERATOR(simcore::biasoperators, K0LongInelastic)
