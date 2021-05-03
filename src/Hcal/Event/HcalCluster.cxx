473cee252292fc979e01944599d1cc8caed2c15f
Tom E <tom.eichlersmith@gmail.com>
2021-05-03 09:39:57 -0500

    Google-style formate new clustering code

--- src/Hcal/Event/HcalCluster.cxx
+++ src/Hcal/Event/HcalCluster.cxx
@@ -2,31 +2,31 @@
 
 ClassImp(ldmx::HcalCluster)
 
-namespace ldmx {
-    HcalCluster::HcalCluster() {}
+    namespace ldmx {
+  HcalCluster::HcalCluster() {}
 
-    HcalCluster::~HcalCluster() { Clear(); }
+  HcalCluster::~HcalCluster() { Clear(); }
 
-    void HcalCluster::Print() const {
-        std::cout << "HcalCluster { "
-        << "Energy: " << energy_ << ", "
-        << "Number of hits: " << nHits_ << " }" << std::endl;
+  void HcalCluster::Print() const {
+    std::cout << "HcalCluster { "
+              << "Energy: " << energy_ << ", "
+              << "Number of hits: " << nHits_ << " }" << std::endl;
+  }
+
+  void HcalCluster::Clear() {
+    hitIDs_.clear();
+    energy_ = 0;
+    nHits_ = 0;
+    centroidX_ = 0;
+    centroidY_ = 0;
+    centroidZ_ = 0;
+  }
+
+  void HcalCluster::addHits(const std::vector<const HcalHit *> hitsVec) {
+    std::vector<unsigned int> vecIDs;
+    for (unsigned int iHit = 0; iHit < hitsVec.size(); iHit++) {
+      vecIDs.push_back(hitsVec[iHit]->getID());
     }
-
-    void HcalCluster::Clear() {
-        hitIDs_.clear();
-        energy_ = 0;
-        nHits_ = 0;
-        centroidX_ = 0;
-        centroidY_ = 0;
-        centroidZ_ = 0;
-    }
-
-    void HcalCluster::addHits(const std::vector<const HcalHit *> hitsVec) {
-        std::vector<unsigned int> vecIDs;
-        for (unsigned int iHit = 0; iHit < hitsVec.size(); iHit++) {
-            vecIDs.push_back(hitsVec[iHit]->getID());
-        }
-        setIDs(vecIDs);
-    }
+    setIDs(vecIDs);
+  }
 }  // namespace ldmx
