0f15b6fc17b14eb22d14611afce271e23f3ee771
Matthew Solt <mrsolt@alumni.stanford.edu>
2021-05-03 17:15:46 -0400

    add neutron inelastic biasing operators

---
+++ src/SimCore/BiasOperators/NeutronInelastic.cxx
@@ -0,0 +1,37 @@
+
+#include "SimCore/BiasOperators/NeutronInelastic.h"
+
+namespace simcore {
+namespace biasoperators {
+
+NeutronInelastic::NeutronInelastic(std::string name, const framework::config::Parameters& p)
+    : XsecBiasingOperator(name, p) {
+  volume_ = p.getParameter<std::string>("volume");
+  factor_ = p.getParameter<double>("factor");
+  threshold_ = p.getParameter<double>("threshold");
+}
+
+G4VBiasingOperation* NeutronInelastic::ProposeOccurenceBiasingOperation(
+    const G4Track* track, const G4BiasingProcessInterface* callingProcess) {
+
+  if (track->GetKineticEnergy() < threshold_) return 0;
+
+  std::string currentProcess =
+      callingProcess->GetWrappedProcess()->GetProcessName();
+  if (currentProcess.compare(this->getProcessToBias()) == 0) {
+    G4double interactionLength =
+        callingProcess->GetWrappedProcess()->GetCurrentInteractionLength();
+
+    double neutInXsecUnbiased = 1. / interactionLength;
+
+    double neutInXsecBiased = neutInXsecUnbiased * factor_;
+
+    return BiasedXsec(neutInXsecBiased);
+  } else
+    return 0;
+}
+
+}  // namespace biasoperators
+}  // namespace simcore
+
+DECLARE_XSECBIASINGOPERATOR(simcore::biasoperators, NeutronInelastic)
