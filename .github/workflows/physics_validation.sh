444f22e0de61aaef6c1c8589fea0669fbe7304ec
Tom E <tom.eichlersmith@gmail.com>
2021-05-14 12:50:02 -0500

    add image and dev branch inputs to action

--- .github/workflows/physics_validation.sh
+++ .github/workflows/physics_validation.sh
@@ -1,9 +1,9 @@
 
 ###############################################################################
-# comp.sh
-#   Compare the current compiled version of ldmx-sw against the latest push
-#   of trunk. This is a very specific script meant to be used by the GitHub
-#   Action, so only tamper with it if you know what you are doing!
+# physics_validation.sh
+#   Compare the current compiled version of ldmx-sw against the provided
+#   production image.  This is a very specific script meant to be used by 
+#   the GitHub Action, so only tamper with it if you know what you are doing!
 ###############################################################################
 
 export LDMX_BASE=$(cd .. && pwd)
@@ -36,35 +36,32 @@ __docker_run() {
 # Compare the event trees of the two input container tags
 #   and the input event file post-fix (defined in configs).
 __compare() {
-  local _trunk="$1"
+  local _base="$1"
   local _dev="$2"
   local _sample_id="$3"
-  __docker_run ${_trunk} fire "${_sample_id}.py" ${_trunk} || return $?
+  __docker_run ${_base} fire "${_sample_id}.py" ${_base} || return $?
   __docker_run ldmx/dev:latest fire "${_sample_id}.py" ${_dev} || return $?
-  __docker_run ldmx/dev:latest python3 compare.py ${_trunk} ${_dev} ${_sample_id} || return $?
-  if pdfunite plots/${_sample_id}/* plots/${_sample_id}.pdf; then
-    rm -r plots/${_sample_id}/*
-  fi
+  __docker_run ldmx/dev:latest python3 compare.py ${_base} ${_dev} ${_sample_id} || return $?
   return $?
 }
 
 # Full Main
 #   0. Get CLI parameters 
 #   1. Go to the directory this script is in
-#   2. Make sure we have both containers we need
+#   2. Make sure we have the containers we need
 #   3. Run through different sample IDs and compare them
 __main() {
   local _dev="$(__deduce_label)"
-  local _trunk="$1"
+  local _base="$1"
 
   local _old_pwd=$OLDPWD
   cd $(dirname ${BASH_SOURCE[0]})
 
   # Make sure we have both containers locally
-  docker pull ${_trunk} || return $?
+  docker pull ${_base} || return $?
   
   for sample_id in inclusive; do
-    __compare ${_trunk} ${_dev} ${sample_id} || return $?
+    __compare ${_base} ${_dev} ${sample_id} || return $?
   done
 
   cd - &> /dev/null
