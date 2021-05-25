098d4b9ccad31c8020f6e4ecb6c76c480248d4c2
tomeichlersmith <eichl008@umn.edu>
2021-05-25 13:47:18 -0500

    manually build production container using v1.7-ldmx-sw dev

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+###############################################################################
+# This dockerfile is meant to build the production image of ldmx-sw
+#   for the development image, look at the LDMX-Software/docker repo
+###############################################################################
+
+ARG DEV_TAG=v1.7-ldmx-sw
+FROM ldmx/dev:${DEV_TAG}
+
+# install ldmx-sw into the container at /usr/local
+COPY . /code
+RUN mkdir /code/build &&\
+    ./home/ldmx.sh /code/build cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. &&\
+    ./home/ldmx.sh /code/build make install &&\
+    rm -rf code
+
+COPY ./scripts/docker_entrypoint.sh /home/docker_entrypoint.sh
+RUN chmod 755 /home/docker_entrypoint.sh
+ENTRYPOINT ["/home/docker_entrypoint.sh"]
---
+++ scripts/docker_entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+###############################################################################
+# Entry point for the ldmx production container
+#   The basic idea is that we want to go into the container,
+#   setup the ldmx-sw working environment, and then
+#   run the application with any provided arguments.
+#
+#   We mount and run inside the present working directory.
+###############################################################################
+
+## Bash environment script for use **within** the docker container
+## Assuming the following environment variables are already defined by Dockerfile:
+#   XercesC_DIR      - install of xerces-c
+#   ONNX_DIR         - install of onnx runtime
+#   ROOTDIR          - install of root
+#   G4DIR            - install of Geant4
+
+source $ROOTDIR/bin/thisroot.sh
+source $G4DIR/bin/geant4.sh
+
+# add ldmx-sw and ldmx-analysis installs to the various paths
+export LDMX_SW_INSTALL=/usr/local/
+export LD_LIBRARY_PATH=$LDMX_SW_INSTALL/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$LDMX_SW_INSTALL/python:$PYTHONPATH
+export PATH=$LDMX_SW_INSTALL/bin:$PATH
+
+# add externals installed along side ldmx-sw
+# TODO this for loop might be very slow... might want to hard-code the externals path
+for _external_path in $LDMX_SW_INSTALL/external/*/lib
+do
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$_external_path
+done
+
+# go to first argument
+cd "$1"
+
+# run the rest of the arguments depending on the command
+if [[ "$2" =~ .*".py" ]]
+then
+    # command ends in '.py'
+    #   assume that we are running the ldmx application
+    fire "${@:2}"
+else
+    # otherwise just run everything like normal
+    eval "${@:2}"
+fi
