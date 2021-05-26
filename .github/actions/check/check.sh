1432280b8b7d451fa0ca02405408fd4294799a0d
tomeichlersmith <eichl008@umn.edu>
2021-05-26 12:51:32 -0500

    isolate checking script to make sure it works

---
+++ .github/actions/check/check.sh
@@ -0,0 +1,21 @@
+
+###############################################################################
+# check.sh
+#   Check the input plots package for any failed comparison plots
+###############################################################################
+
+__main__() {
+  local _archive="$1"
+
+  local _failed_plots=($(tar -tf ${_archive} | grep "fail/.*.pdf"))
+  if [[ ${#_failed_plots[@]} -gt 0 ]]; then 
+    echo "::error ::${#_failed_plots[@]} plots failed the KS test against gold."
+    echo "::group::List of Plots Failing KS Test"
+    for p in ${_failed_plots[@]}; do
+      echo $(basename ${p})
+    done
+    echo "::endgroup::"
+  fi
+}
+
+__main__ $@
