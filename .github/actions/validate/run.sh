7c8fdbd02d344afeeba2f7762e9b8d6a87779d92
tomeichlersmith <eichl008@umn.edu>
2021-05-26 12:30:52 -0500

    more verbose printing using workflow commands

--- .github/actions/validate/run.sh
+++ .github/actions/validate/run.sh
@@ -25,18 +25,35 @@ __docker_run__() {
 __set_output__() {
   local _key="$1"
   local _val="$2"
+  echo "${_key} = ${_val}"
   echo "::set-output name=${_key}::${_val}"
 }
 
+__start_group__() {
+  echo "::group::$@"
+}
+
+__end_group__() {
+  echo "::endgroup::"
+}
+
 __main__() {
+  __start_group__ Input Deduction
+  echo "::group::Input Deduction"
   local _sample="$1"
-  
   __deduce_ldmx_base__
   __deduce_sample_dir__ $_sample
+  echo "Sample Name: ${_sample}"
+  echo "LDMX_BASE: ${LDMX_BASE}"
+  echo "Sample Dir: ${LDMX_SAMPLE_DIR_PATH}"
+  __end_group__
 
   # assume sample directory has its config called 'config.py'
+  __start_group__ Run config.py
   __docker_run__ fire config.py || return $?
+  __end_group__
 
+  __start_group__ Compare to Golden Histograms
   # assume sample directory has its gold histogram called 'gold.root'
   #   compare has 4 CLI inputs:
   #    gold_f, gold_label, test_f, test_label
@@ -47,11 +64,14 @@ __main__() {
   #   Package them up for upload
   cd ${LDMX_SAMPLE_DIR_PATH}/plots || return $?
   tar -czf ${_sample}_recon_validation_plots.tar.gz * || return $?
+  __end_group__
 
   # Share paths to plot archive
+  __start_group__ Share Paths to Outputs
   __set_output__ plots $(pwd)/${_sample}_recon_validation_plots.tar.gz
   __set_output__ hists ${LDMX_SAMPLE_DIR_PATH}/hist.root
   __set_output__ events ${LDMX_SAMPLE_DIR_PATH}/events.root
+  __end_group__
 }
 
 __main__ $@
