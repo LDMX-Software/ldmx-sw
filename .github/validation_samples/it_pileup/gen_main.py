b07e994455d109c0f9127a973d484e8d060d05b3
tomeichlersmith <eichl008@umn.edu>
2021-06-02 11:18:46 -0500

    increase number of events and test gold gen by pushing

--- .github/validation_samples/it_pileup/gen_main.py
+++ .github/validation_samples/it_pileup/gen_main.py
@@ -2,7 +2,7 @@ from LDMX.Framework import ldmxcfg
 p = ldmxcfg.Process('test')
 
 p.maxTriesPerEvent = 1000
-p.maxEvents = 100
+p.maxEvents = 1000
 
 from LDMX.Biasing import ecal
 from LDMX.SimCore import generators as gen
