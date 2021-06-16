39d54f5e16a2d5064849786145369129194f8d76
tomeichlersmith <eichl008@umn.edu>
2021-06-16 08:58:40 -0500

    add beamSpotSmear to simulations

--- .github/validation_samples/inclusive/config.py
+++ .github/validation_samples/inclusive/config.py
@@ -7,6 +7,7 @@ mySim = sim.simulator( "mySim" )
 mySim.setDetector( 'ldmx-det-v12' )
 from LDMX.SimCore import generators as gen
 mySim.generators.append( gen.single_4gev_e_upstream_tagger() )
+mySim.beamSpotSmear = [20.,80.,0.]
 mySim.description = 'Basic test Simulation'
 
 p.sequence = [ mySim ]
@@ -54,5 +55,5 @@ p.sequence.extend([
         hcal_digi.HcalRecProducer(),
         tsDigisUp, tsDigisTag, tsDigisDown,
         clTag, clUp, clDown,
-        trigScintTrack, dqm.SimObjects()
+        trigScintTrack, dqm.SimObjects(), dqm.HCalDQM()
         ] + dqm.trigScint_dqm + dqm.ecal_dqm + dqm.recoil_dqm)
