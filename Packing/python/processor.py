735c65c88be3d6027bfb6b741451ec62f656c352
tomeichlersmith <eichl008@umn.edu>
2021-06-28 12:35:07 -0500

    compiling template for translator/unpacker ecosystem
    No translation has been implemented, but there is an example translator
    that fits our structure.

--- Packing/python/processor.py
+++ Packing/python/processor.py
@@ -2,33 +2,7 @@
 
 from LDMX.Framework import ldmxcfg
 
-class Processor(ldmxcfg.Producer) :
-    """The base packing processor configuration class.
-
-    Do not interact with this directly.
-
-    The instance name of the processor is determined
-    by the decode parameter.
-
-    Parameters
-    ----------
-    decode : bool
-        True if we are decoding, false if encoding
-    translators : list[Translator]
-        List of translators for encoding/decoding
-    """
-    def __init__(self, decode, translators = []) :
-        if decode :
-            name = 'unpacker'
-        else :
-            name = 'packer'
-
-        super().__init__(name,'packing::Processor','Packing')
-
-        self.decode = decode
-        self.translators = translators
-
-class Unpack(Processor) :
+class Unpacker(Processor) :
     """Processor to use to unpack from raw to digi.
 
     Parameters
@@ -37,17 +11,8 @@ class Unpack(Processor) :
         List of translators for decoding
     """
 
-    def __init__(self, translators = []) :
-        super().__init__(True, translators)
-
-class Pack(Processor) :
-    """Processor to use to pack digi into raw.
-
-    Parameters
-    ----------
-    translators : list[Translator]
-        List of translators for encoding
-    """
-
-    def __init__(self, translators = []) :
-        super().__init__(False, translators)
+    def __init__(self, raw_name, translators = []) :
+        super().__init__('unpacker','packing::Unpacker','Packing')
+        self.translators = translators
+        self.raw_name = raw_name
+        self.raw_pass = ''
