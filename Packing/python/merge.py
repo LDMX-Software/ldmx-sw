cb597ee3f25296f95b22716ad3b1fd6080a5ba62
tomeichlersmith <eichl008@umn.edu>
2021-06-28 14:54:08 -0500

    remove classes - not very helpful

--- Packing/python/merge.py
+++ Packing/python/merge.py
@@ -7,64 +7,42 @@ import ROOT
 LDMXBufferType = ROOT.std.vector('unsigned char')
 LDMXEventEndWord = 0b00000000
 
-class RawDataStream(file) :
-    """Each 'stream' of raw data is housed in an instance
-    of this class. We pretend streams/files of raw data are
-    stacks in order of events and each stream/file has exactly
-    one block of data per event that can be uniquely identified.
+def build(self, streams, output_file = 'raw_events.root', output_name = 'RawData', output_pass = 'raw') :
+    
+    # convert file names to binary files being read
+    for name in streams :
+        streams[name] = open(streams[name], 'rb')
+    
+    # start reading in data
+    tfile = ROOT.TFile(output_file, 'recreate')
+    tree  = ROOT.TTree('LDMX_Events','LDMX_Events')
+ 
+    raw_data = ROOT.std.map(ROOT.std.string,LDMXBufferType)()
+    tree.Branch('RawData_raw', ROOT.addressof(raw_data))
+ 
+    while True :
+        raw_data.clear()
+        done = True
+        for name, buff in streams.items() :
+            cpp_obj_buffer = LDMXBufferType()
+            byte = buff.read(1)
+            while byte :
+                if byte[0] == LDMXEventEndWord :
+                    break
+                cpp_obj_buffer.push_back(byte[0])
 
-    Parameters
-    ----------
-    data : str
-        Name of file with binary data inside of it
-    name : str
-        Unique identification name for this data stream
-    """
+            if not cpp_obj_buffer.empty() :
+                done = False
+ 
+            raw_data[name] = cpp_obj_buffer
 
-    def __init__(self, data, name) :
-        super().__init__(data, 'rb')
-        self.__name = name
+        if done :
+            break
+ 
+        tree.Fill()
+ 
+    tfile.Write()
+    tfile.Close()
 
-    def name(self) :
-        """Get the name of this data stream"""
-        return self.__name
-
-    def pop(self) :
-        """Get the next chunk of data to be inserted into the map.
-
-        We keep reading 8-bit words until we reach a stopping condition.
-        """  
-
-        cpp_obj_buffer = LDMXBufferType()
-
-        while True:
-            [word] = self.read(1)
-            if word == LDMXEventEndWord :
-                break
-            cpp_obj_buffer.push_back(word)
-
-        return cpp_obj_buffer
-
-class EventBuilder() :
-    """Here we hold multiple raw data streams and merge them
-    event-by-event while writing out a *correctly formatted*
-    event file to be given to ldmx-sw."""
-
-    def __init__(self, output_file = 'raw_events.root') :
-        self.streams = {}
-
-        self.tfile = ROOT.TFile(output_file, 'recreate')
-        self.tree = ROOT.TTree('LDMX_Events','LDMX_Events')
-
-        self.raw_data = ROOT.std.map(ROOT.std.string,LDMXBufferType)()
-        self.tree.Branch('RawData_raw', ROOT.addressof(raw_data))
-
-    def next(self) :
-        """Iterate one more event, grabbing one more event from each stream"""
-
-        self.raw_data.clear()
-
-        for key, val in self.streams :
-            self.raw_data[key] = val.pop()
-
-        self.tree.Fill()
+    for name in streams :
+        streams[name].close()
