a6be797cd0b6c9438faf792907ef5c9aef4f693e
tomeichlersmith <eichl008@umn.edu>
2021-06-29 08:49:52 -0500

    notes on questions needed to be answered

--- Packing/python/merge.py
+++ Packing/python/merge.py
@@ -1,13 +1,22 @@
 """Python module to merge simultaneous raw files
 into a single ROOT file with many branches of encoded
-data."""
+data.
+
+Open Questions
+--------------
+- Is there a uniform word (or multi-word) condition that is used to
+  separate events across all subsystems?
+- Is it ok to assume that all input raw files have the same number
+  of events?
+- Where will the RunHeader/EventHeader information come from?
+"""
 
 import ROOT
 
 LDMXBufferType = ROOT.std.vector('unsigned char')
 LDMXEventEndWord = 0b00000000
 
-def build(self, streams, output_file = 'raw_events.root', output_name = 'RawData', output_pass = 'raw') :
+def build(self, streams, output_file = 'raw_events.root') :
     
     # convert file names to binary files being read
     for name in streams :
@@ -15,19 +24,19 @@ def build(self, streams, output_file = 'raw_events.root', output_name = 'RawData
     
     # start reading in data
     tfile = ROOT.TFile(output_file, 'recreate')
-    tree  = ROOT.TTree('LDMX_Events','LDMX_Events')
+    event_tree = ROOT.TTree('LDMX_RawData','LDMX_RawData')
  
     raw_data = ROOT.std.map(ROOT.std.string,LDMXBufferType)()
-    tree.Branch('RawData_raw', ROOT.addressof(raw_data))
- 
+    event_tree.Branch('data', ROOT.addressof(raw_data))
+
     while True :
         raw_data.clear()
         done = True
         for name, buff in streams.items() :
             cpp_obj_buffer = LDMXBufferType()
-            byte = buff.read(1)
-            while byte :
-                if byte[0] == LDMXEventEndWord :
+            while True :
+                byte = buff.read(1)
+                if not byte or byte[0] == LDMXEventEndWord :
                     break
                 cpp_obj_buffer.push_back(byte[0])
 
@@ -39,10 +48,14 @@ def build(self, streams, output_file = 'raw_events.root', output_name = 'RawData
         if done :
             break
  
-        tree.Fill()
- 
+        event_tree.Fill()
+
+    run_tree = ROOT.TTree('LDMX_RawRun','LDMX_RawRun')
+
+    # Run Data
+
     tfile.Write()
     tfile.Close()
 
-    for name in streams :
-        streams[name].close()
+    for f in streams.values() :
+        f.close()
