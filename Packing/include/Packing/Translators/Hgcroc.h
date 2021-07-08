bb39579ce90568bbc0a07f2159bee0e692ec7324
tomeichlersmith <eichl008@umn.edu>
2021-07-08 16:17:11 -0500

    WIP: more thorough outlining
    Sort grouping of data by bunches into grouping by EID
    and then worry about translating EID to DetID.

--- Packing/include/Packing/Translators/Hgcroc.h
+++ Packing/include/Packing/Translators/Hgcroc.h
@@ -14,16 +14,71 @@ namespace translators {
  */
 class Hgcroc : public Translator {
  public:
+  /**
+   * Constructor
+   *
+   * Currently, we have no parameters specific for this translator.
+   */
   Hgcroc(const framework::config::Parameters& ps);
-  virtual ~Hgcroc() {}
 
+  /// default destructor
+  virtual ~Hgcroc() = default;
+
+  /**
+   * We can translate the "precision digis" output by the HGC ROC.
+   *
+   * They are called "precision" in order to make them distinct
+   * from the "trigger" digis which are merged into groups for 
+   * speed.
+   *
+   * @param[in] name Name of data stream to check
+   * @return true if name has 'PrecisionHgcroc' as a sub string
+   */
   bool canTranslate(const std::string& name) const final override;
+  
+  /**
+   * Decode the input buffer into an event object and put that event
+   * object onto the event bus.
+   *
+   * We assume that the data is in the format specified by the
+   * ECal data format specifications.
+   *
+   * @TODO Can we assume that the bunches are ordered correctly?
+   * @TODO Electronics ID from link, channel, ROC, FPGA
+   * @TODO ELectronics to Detector ID
+   *
+   * @param[in,out] event EventBus to put object onto
+   * @param[in] buffer RawData buffer to decode
+   */
   void decode(framework::Event& event, const BufferType& buffer) final override;
+  
+  /**
+   * Encoding the C++ digi into a raw event type is delayed
+   * until we more strictly define our resulting raw data format.
+   */
   BufferType encode(const ldmx::HgcrocDigiCollection& data) {
     EXCEPTION_RAISE("NoImp",
         "Hgcroc Translator hasn't implemented encode yet.");
     return {};
   }
+
+ private:
+  /**
+   * Decode a single bunch.
+   *
+   * The raw data is readout on a bunch-by-bunch basis,
+   * so that is the fundamental unit of translation for us.
+   *
+   * @throws Exception if there is an error in decoding the format
+   *
+   * @param[in] i_word buffer iterator the bunch starts on
+   * @param[out] data container that holds the map of electronic IDs to words
+   * @return index of buffer 
+   */
+  std::vector<uint64_t>::iterator
+    decodeBunch(std::vector<uint64_t>::iterator start,
+        std::map<uint64_t, std::vector<ldmx::HgcrocDigiCollection::Sample>> data);
+
 };
 
 }  // namespace translators
--- Packing/src/Packing/Translators/Hgcroc.cxx
+++ Packing/src/Packing/Translators/Hgcroc.cxx
@@ -1,94 +1,169 @@
 
 #include "Packing/Translators/Hgcroc.h"
 
-
 namespace packing {
 namespace translators {
 
-Hgcroc::Hgcroc(const framework::config::Parameters& ps) : Translator(ps) {
-}
+Hgcroc::Hgcroc(const framework::config::Parameters& ps) : Translator(ps) {}
 
 bool Hgcroc::canTranslate(const std::string& name) const {
-  static const std::string sub_str_match{"hgcroc"};
+  static const std::string sub_str_match{"PrecisionHgcroc"};
   return (name.find(sub_str_match) != std::string::npos);
 }
 
 void Hgcroc::decode(framework::Event& event, const BufferType& buffer) {
-
-/** Old Decoding Code - will be helpful for Hgcroc Translator
-  chip = rocdata.chip();
-  for (half = 0; half < 2; half++) {
-    // header = 0xaaBXCWADD101 : BXC 12 bit bunch crossing counter; WADD : 9 bit
-    // column address of the triggered event;
-    std::vector<uint32_t> data = rocdata.data(half);
-    uint32_t header = data[0];
-    uint32_t head = (header >> 24) & 0xff;
-    corruption = head == 0xAA || head == 0x9A ? 0 : 1;
-    bxcounter = (header >> 12) & 0xfff;
-    wadd = (header & 0xfff) >> 3;
-    for (int ichan = 0; ichan < N_READOUT_CHANNELS; ichan++) {
-      if (ichan == 18) {
-        channel = 37;
-        adc = (data[ichan + 1] >> 10) & 0x3ff;
-        tot = 0;
-        toa = 0;
-        outtree->Fill();
-        channel = 38;
-        adc = data[ichan + 1] & 0x3ff;
-        tot = 0;
-        toa = 0;
-        outtree->Fill();
-        continue;
-      }
-      if (ichan < 18)
-        channel = ichan;
-      else if (ichan > 19)
-        channel = ichan - 2;
-      else if (ichan == 19)
-        channel = 36;
-      adc = data[ichan + 1] & 0x3ff;
-      toa = (data[ichan + 1] >> 10) & 0x3ff;
-      tot = (data[ichan + 1] >> 20) & 0xfff;
-      outtree->Fill();
-    }
-  }
- */
-  // construct header from buffer (in 8-bit words)
-  uint32_t header = buffer.at(0) + buffer.at(1) << 8 + buffer.at(2) << 16 + buffer.at(3) << 24;
-
+  /** Re-sort the data from grouped by bunch to by channel
+   * The readout chip streams the data off of it, so it doesn't
+   * have time to re-group the signals across multiple bunches (samples)
+   * by their channel ID. We need to do that here.
+   */
   // fill map of **electronic** IDs to the digis that were read out
   std::map<uint32_t, std::vector<ldmx::HgcrocDigiCollection::Sample>> digis;
-
-  // look through zero-suppresion map to get the channels that have been read out
-  //  right now the index of a channel is equal to its channel ID, but that won't
-  //  be the case generally
-  for (unsigned int i_chan{0}; i_chan < size_of_zero_supp_map; i_chan++) {
-    digis[i_chan];
-  }
-
-  // loop through rest of buffer adding digis in parallel to the list of digis
-  auto current_channel{digis.begin()};
-  for (unsigned int i_data{1}; i_data <= buffer.size()/4-1; i_data++) {
-    uint32_t data = buffer.at(4*i_data)
-                  + buffer.at(4*i_data+1) << 8
-                  + buffer.at(4*i_data+2) << 16
-                  + buffer.at(4*i_data+3) << 24;
-    int channel{-1};
-    digis[i_chan].emplace_back(data);
-    if (next channel) {
-      current_channel++;
+  auto word{buffer.begin()};
+  while (word != buffer.end()) {
+    try {
+      // decodeBunch moves the iterator,
+      // we just are here making sure to catch any out_of_range
+      // exceptions due to poorly-formatted raw data
+      word = decodeBunch(word, digis);
+    } catch (std::out_of_range&) {
+      EXCEPTION_RAISE("MisFormat",
+          "Recieved raw data that was not formatted correctly.");
     }
   }
-  
-  ldmx::HgcrocDigiCollection unpacked_data;
 
+  /** Translation
+   * The actual translation done here is the translation from electronic IDs
+   * to detector IDs. The unpacking of the 32-bit word samples for each channel
+   * is done by the HgcrocDigiCollection::Sample class and is done on-the-fly
+   * in order to save disk-space.
+   */
+  ldmx::HgcrocDigiCollection unpacked_data;
   // Electronic ID <-> Detector ID while copying into collection data structure
-  for (auto const& [channel, digi] : digis) {
+  for (auto const& [eid, digi] : digis) {
     // TODO: Instert EID --> DetID mapping here
+    auto channel = eid;
     unpacked_data.addDigi(channel, digi);
   }
 
   event.add("EcalDigis", unpacked_data);
+}
+
+std::vector<uint64_t>::iterator Hgcroc::decodeBunch(
+    std::vector<uint64_t>::iterator start,
+    std::map<uint64_t, std::vector<ldmx::HgcrocDigiCollection::Sample>> data) {
+  /** Decode Bunch Header
+   * We have a few words of header material before the actual data.
+   * This header material is assumed to be encoded as in Table 3
+   * of the DAQ specs.
+   *
+   * <name> (bits)
+   *
+   * VERSION (4) | FPGA_ID (8) | NLINKS (6) | 0 | LEN (12)
+   * BX ID (12) | RREQ (10) | OR (10)
+   * RID ok (1) | CDC ok (1) | LEN3 (6) |
+   *  RID ok (1) | CDC ok (1) | LEN2 (6) |
+   *  RID ok (1) | CDC ok (1) | LEN1 (6) |
+   *  RID ok (1) | CDC ok (1) | LEN0 (6)
+   * ... other listing of links ...
+   */
+  auto word{start};
+  unsigned int version{(*word >> 12 + 1 + 6 + 8) & mask<4>::m};
+  if (version != 1)
+    EXCEPTION_RAISE("VersMis", "Hgcroc Translator only knows version 1.");
+
+  unsigned int fpga{(word >> 12 + 1 + 6) & mask<8>::m};
+  unsigned int nlinks{(word >> 12 + 1) & mask<6>::m};
+  unsigned int len{word & mask<12>::m};
+
+  word++;
+
+  unsigned int bx_id{(word >> 10 + 10) & mask<12>::m};
+  unsigned int rreq{(word >> 10) & mask<10>::m};
+  unsigned int orbit{word & mask<10>::m};
+
+  std::vector<unsigned int> num_channels_per_link;
+  for (unsigned int i_link{0}; i_link < nlinks; i_link++) {
+    if (i_link % 4 == 0) word++;
+    unsigned int shift_in_word{8 * i_link % 4};
+    bool rid_ok{(*word >> shift_in_word + 7) & mask<1>::m == 1};
+    bool cdc_ok{(*word >> shift_in_word + 6) & mask<1>::m == 1};
+    num_channels_per_link[i_link] = (*word >> shift_in_word) & mask<6>::m;
+  }
+
+  /** Decode Each Link in Sequence
+   * Now we should be decoding each link serially
+   * where each link was encoded as in Table 4 of
+   * the DAQ specs
+   *
+   * ROC_ID (16) | CRC ok (1) | 00000 | RO Map (8)
+   * RO Map (32)
+   */
+
+  for (unsigned int i_link{0}; i_link < nlinks; i_link++) {
+    // move on from last word counting links or previous link
+    word++;
+    unsigned int roc_id{(*word >> 8 + 5 + 1) & mask<16>::m};
+    bool crc_ok{(*word >> 8 + 5) & mask<1>::m == 1};
+    unsigned int ro_map_39_32{*word & mask<8>::m};
+    word++;
+    unsigned int ro_map_31_0{*word & mask<32>::m};
+
+    // loop through channels on this link, 
+    //  check if they have been readout before saving word
+    for (unsigned int i_chan{0}; i_chan < 40; i_chan++) {
+      bool has_been_read{false};
+      if (i_chan < 32) {
+        has_been_read = ((ro_map_31_0 >> i_chan & mask<1>::m) == 1)
+      } else {
+        has_been_read = ((ro_map_39_32 >> (i_chan - 31) & mask<1>::m) == 1)
+      }
+
+      // skip zero-suppressed channels
+      if (not has_been_read) continue;
+
+      // next word is this channel
+      word++;
+
+      if (i_chan == 0) { 
+        /** Special "Header" Word from ROC
+         * 0101 | BXID (12) | RREQ (6) | OR (3) | HE (3) | 0101
+         */
+      } else if (i_chan == 1) {
+        /** Common Mode Channels
+         * 10 | 0000000000 | Common Mode ADC 0 (10) | Common Mode ADC 1 (10)
+         */
+      } else if (i_chan == 39) {
+        /** CRC Checksum form ROC
+         */
+        unsigned int roc_crc{*word & mask<32>::m};
+      } else {
+        /// DAQ Channels
+  
+        /** Generate Electronics ID
+         * Link Index i_link
+         * Channel Index i_chan
+         * ROC ID roc_id
+         * FPGA ID fpga
+         * are all available.
+         * For now, we just generate a dummy mapping
+         * using the link and channel indices.
+         */
+        unsigned int eid{i_link * 100 + i_chan};
+  
+        // copy data into EID->sample map
+        data[eid].emplace_back(*word);
+      }  // type of channel
+    }    // loop over channels (j in Table 4)
+  }      // loop over links
+
+  // next word is CDC checksum
+  word++;
+  unsigned int cdc{*word & mask<32>::m};
+
+  // need to go one word past the last word we used.
+  word++;
+  return word;
 }
 
 }  // namespace translators
