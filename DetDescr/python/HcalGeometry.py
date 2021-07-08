0f633f8c8fadf9bd7407c9b3363a986430b16f28
Einar Elén <einar.elen@gmail.com>
2021-07-08 15:02:23 +0200

    Copy the make_v12 function

--- DetDescr/python/HcalGeometry.py
+++ DetDescr/python/HcalGeometry.py
@@ -113,3 +113,71 @@ class HcalGeometry() :
                                    (self.v12.NumLayers[1]*self.v12.LayerThickness[1]+self.v12.EcalDy)/2,
                                    (self.v12.NumLayers[2]*self.v12.LayerThickness[2]+self.v12.EcalDy)/2,]
         self.v12.detectors_valid = ["ldmx-det-v12","ldmx-det-v12[.].*","ldmx-det-v9","ldmx-det-v10","ldmx-det-v11"]
+    def make_prototype(self) :
+        """Create the HcalGeometry with the v12 geometry parameters
+
+        Only sets parameters that must align with the Hcal gdml constants.
+
+        - ThicknessScint: Scintillator thickness (in z).
+          @param gdml see: `scint_thick`.
+
+        - WidthScint: Scintillator width (in x).
+          Can be obtained by: hcal_length/n_strips.
+
+        - ZeroLayer: Position of the first layer
+          Back Hcal first layer (in z) starts after Ecal+Side-Hcal_z
+          Side Hcal first layer (in x or y) starts after Ecal_x/y / 2
+          For back Hcal: ecal_front_z + hcal_side_dz (in z)
+          For side Hcal: hcal_side_dz/2 (in x/y)
+          @param gdml see: `ecal_front_z` and `hcal_side_dz`
+
+        - ZeroStrip: Position of the first strip.
+          For back Hcal: back_transverse_width/2 (in x)
+          For side Hcal: ecal_front_z (in z)
+          @param gdml see: `hcal_back_dx` and `ecal_front_z`
+
+        - LayerThickness:
+          Layer thickness (in z)
+          Can be obtained by: absorber_thickness + scint_thickness + 2.0*air_thickness
+          @param gdml see: `side_abso_thick` and `back_abso2_thick` and `air_thick`
+
+        - NumLayers:
+          Number of layers per section.
+
+        - NumStrips:
+          Number of strips per layer.
+
+        - NumSections:
+          Number of sections.
+          Encoded in HcalID.
+          Back, Top, Left, Right, Bottom.
+
+        - EcalDx/EcalDy:
+          Length of Ecal needed for side Hcal dimensions.
+
+        - HalfTotalWidth:
+          Half length of a bar
+          For back Hcal: nstrips * scint_width / 2 (in x/y)
+          For side Hcal: (nlayers_otherside * layer_thick + Ecal_dx(y))/2
+          @param gdml see: sideTB_dx, sideTB_dy
+        """
+        self.v12=HcalReadoutGeometry()
+
+        self.v12.ThicknessScint = 20.0
+        self.v12.WidthScint = 50.0
+        self.v12.ZeroLayer = [220.+600.,600./2,600./2,600./2,600./2]
+        self.v12.ZeroStrip = [3100./2,220.,220.,220.,220.]
+        self.v12.LayerThickness = [25. + self.v12.ThicknessScint + 2*2.,
+                                   20. + self.v12.ThicknessScint + 2*2., 20. + self.v12.ThicknessScint + 2*2.,
+                                   20. + self.v12.ThicknessScint + 2*2., 20. + self.v12.ThicknessScint + 2*2.]
+        self.v12.NumSections = 5
+        self.v12.NumLayers = [100,28,28,26,26]
+        self.v12.NumStrips = [62,12,12,12,12]
+        self.v12.EcalDx = 800.0
+        self.v12.EcalDy = 600.0
+        self.v12.HalfTotalWidth = [(self.v12.NumStrips[0]*self.v12.WidthScint)/2,
+                                   (self.v12.NumLayers[3]*self.v12.LayerThickness[3]+self.v12.EcalDx)/2,
+                                   (self.v12.NumLayers[4]*self.v12.LayerThickness[4]+self.v12.EcalDx)/2,
+                                   (self.v12.NumLayers[1]*self.v12.LayerThickness[1]+self.v12.EcalDy)/2,
+                                   (self.v12.NumLayers[2]*self.v12.LayerThickness[2]+self.v12.EcalDy)/2,]
+        self.v12.detectors_valid = ["ldmx-det-v12","ldmx-det-v12[.].*","ldmx-det-v9","ldmx-det-v10","ldmx-det-v11"]
