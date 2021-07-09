bbae903605c36d890241ac9cfd3038d2b4c273d1
tomeichlersmith <eichl008@umn.edu>
2021-07-09 08:57:30 -0500

    add debug mode for verbose printing out of buffer nonsense

--- Packing/HexaReformat/src/HGCROCv2RawData.cxx
+++ Packing/HexaReformat/src/HGCROCv2RawData.cxx
@@ -9,16 +9,14 @@ std::ostream& operator<<(std::ostream& out, const HGCROCv2RawData& rawdata) {
   out << "event = " << std::dec << rawdata.m_event << " "
       << "chip = " << std::dec << rawdata.m_chip << std::endl;
 
-  out << "first half : \n";
-  ;
+  out << "first half :";
   for (auto d : rawdata.m_data0)
-    out << "\t" << std::hex << std::setfill('0') << std::setw(8) << d;
+    out << "  " << std::hex << std::setfill('0') << std::setw(8) << d;
   out << std::endl;
-  out << "second half : \n";
-  ;
+  out << "second half :";
   for (auto d : rawdata.m_data1)
-    out << "\t" << std::hex << std::setfill('0') << std::setw(8) << d;
-  out << std::endl;
+    out << "  " << std::hex << std::setfill('0') << std::setw(8) << d;
+  out << std::dec << std::endl;
 
   return out;
 }
