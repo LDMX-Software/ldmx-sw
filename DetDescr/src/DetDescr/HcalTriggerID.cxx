24e2abfe8eb9044211f7f260747c082d3a494adc
Theodor Christian Herwig <herwig@cern.ch>
2021-08-03 23:46:20 -0500

    prefer superstrips over strips for clarity

--- DetDescr/src/DetDescr/HcalTriggerID.cxx
+++ DetDescr/src/DetDescr/HcalTriggerID.cxx
@@ -19,8 +19,8 @@ void HcalTriggerID::createInterpreters() {
       new IDField("layer", 2, LAYER_SHIFT,
                   LAYER_SHIFT + IDField::countOnes(LAYER_MASK) - 1));
   fields.push_back(
-      new IDField("strip", 3, STRIP_SHIFT,
-                  STRIP_SHIFT + IDField::countOnes(STRIP_MASK) - 1));
+      new IDField("superstrip", 3, SUPERSTRIP_SHIFT,
+                  SUPERSTRIP_SHIFT + IDField::countOnes(SUPERSTRIP_MASK) - 1));
 
   DetectorIDInterpreter::registerInterpreter(
       SD_HCAL, HcalAbstractID::BAR_TYPE_MASK << HcalAbstractID::BAR_TYPE_SHIFT,
