88901e1bf6e4c06cd5253bcb34131e791e3c5f1c
Theodor Christian Herwig <herwig@cern.ch>
2021-08-04 17:56:30 -0500

    add end attribute

--- DetDescr/src/DetDescr/HcalTriggerID.cxx
+++ DetDescr/src/DetDescr/HcalTriggerID.cxx
@@ -3,7 +3,7 @@
 
 std::ostream& operator<<(std::ostream& s, const ldmx::HcalTriggerID& id) {
   s << "HcalTrig(" << id.section() << ',' << id.layer() << ','
-    << id.superstrip() << ')';
+    << id.superstrip() << ',' << id.end() << ')';
   return s;
 }
 
@@ -21,6 +21,9 @@ void HcalTriggerID::createInterpreters() {
   fields.push_back(
       new IDField("superstrip", 3, SUPERSTRIP_SHIFT,
                   SUPERSTRIP_SHIFT + IDField::countOnes(SUPERSTRIP_MASK) - 1));
+  fields.push_back(
+      new IDField("end", 4, END_SHIFT,
+                  END_SHIFT + IDField::countOnes(END_MASK) - 1));
 
   DetectorIDInterpreter::registerInterpreter(
       SD_HCAL, HcalAbstractID::BAR_TYPE_MASK << HcalAbstractID::BAR_TYPE_SHIFT,
