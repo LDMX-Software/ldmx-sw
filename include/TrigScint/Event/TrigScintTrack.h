f0a2dc8ff292ac3547dc0d2fef65fd5b72e95938
Lene Kristian Bryngemark <lene.bryngemark@cern.ch>
2021-08-05 11:51:20 -0700

    Add in all x,y tracking capabilities
    This includes
    - setters and getters for track x,y and their sigmas
    - centroidX and Y for clusters and tracks
    - a method for matching tracks well measured in x and y, respectively
    - setting the y coordinate of tracks from vertical bars to quad center
    - skipping tracks in vertical bars that are from impossible quad combos
    
    In addition, there's now a bool for if one wants to make tracks even
    if the last pad didn't have a matching cluster. This is experimental and
    defaults to false.

--- include/TrigScint/Event/TrigScintTrack.h
+++ include/TrigScint/Event/TrigScintTrack.h
@@ -48,13 +48,13 @@ class TrigScintTrack {
 
   /**
    * Get the x centroid of the track.
-   * @return The x centroid of the track.
+   * @return The x centroid of the track in units of bars.
    */
   float getCentroidX() const { return centroidX_; };
 
   /**
    * Get the y centroid of the track.
-   * @return The y centroid of the track.
+   * @return The y centroid of the track in units of bars.
    */
   float getCentroidY() const { return centroidY_; };
 
@@ -88,6 +88,13 @@ class TrigScintTrack {
    */
   float getResidualZ() const { return residualZ_; };
 
+  
+  /**
+   * Get the (average) pe of the track.
+   * @return The cluster pe count averaged over the track.
+   */
+  float getPE() const { return PE_; };
+
   /**
    * Get the number of clusters forming the track.
    * @return The number of clusters in the track.
@@ -95,9 +102,32 @@ class TrigScintTrack {
   int getNclusters() const { return nClusters_; };
 
   /**
-   * Get the XYZ momentum of the particle at the position at which
-   * the track took place [MeV].
-   * @return The momentum of the particle.
+   * Get the x coordinate of the track.
+   * @return The x coordinate of the track in mm [mm].
+   */
+  float getX() const { return x_; };
+
+  /**
+   * Get the y coordinate of the track.
+   * @return The y coordinate of the track [mm].
+   */
+  float getY() const { return y_; };
+
+    /**
+   * Get the uncertainty on the x coordinate of the track.
+   * @return The uncertainty on the x coordinate of the track in mm [mm].
+   */
+  float getSigmaX() const { return sx_; };
+
+  /**
+   * Get the uncertainty on the y coordinate of the track.
+   * @return The uncertainty on the y coordinate of the track in mm [mm].
+   */
+  float getSigmaY() const { return sy_; };
+
+  /**
+   * Get the cluster constituents of the track.
+   * @return The list of track constituents.
    */
   std::vector<ldmx::TrigScintCluster> getConstituents() const {
     return constituents_;
@@ -159,6 +189,11 @@ class TrigScintTrack {
   void setResidualZ(float resid) { residualZ_ = resid; };
 
   /**
+   * Set the average cluster pe of  the track.
+   */
+  void setPE(float pe) { PE_ = pe; };
+
+  /**
    * Set the number of clusters forming the track.
    */
   void setNclusters(uint nCl) { nClusters_ = nCl; };
@@ -172,10 +207,33 @@ class TrigScintTrack {
    * Set the position of the track [mm].
    * @param x The X position.
    * @param y The Y position.
-   * @param z The Z position.
    */
-  void setPosition(const float x, const float y, const float z);
+  void setPosition(const float x, const float y) { x_ = x ; y_ = y; } ;
 
+  /**
+   * Set the uncertainty on the position of the track [mm].
+   * @param x The X position uncertainty.
+   * @param y The Y position uncertainty.
+   */
+  void setSigmaXY(const float sx, const float sy) { sx_ = sx ; sy_ = sy;} 
+
+  
+  /**
+   * Set the uncertainty on the position of the track [mm].
+   * @param x The X position uncertainty.
+   */
+  void setSigmaX(const float sx) { sx_ = sx ;} 
+
+  
+  /**
+   * Set the uncertainty on the position of the track [mm].
+   * @param y The Y position uncertainty.
+   */
+  void setSigmaY(const float sy) { sy_ = sy;} 
+
+  
+
+  
   /**
    * Set the momentum of the particle at the position at which
    * the track took place [GeV].
@@ -196,22 +254,22 @@ class TrigScintTrack {
   /**
    * The detector centroid.
    */
-  float centroid_{0};
+  float centroid_{-1};
 
   /**
-   * The x coordinate centroid.
+   * The detector x coordinate centroid.
    */
-  float centroidX_{0};
+  float centroidX_{-1};
 
   /**
-   * The y coordinate centroid.
+   * The detector y coordinate centroid.
    */
-  float centroidY_{0};
+  float centroidY_{-1};
 
   /**
-   * The z coordinate centroid.
+   * The detector z coordinate centroid.
    */
-  float centroidZ_{0};
+  float centroidZ_{-99999};
 
   /**
    * The detector residual.
@@ -243,8 +301,41 @@ class TrigScintTrack {
    */
   std::vector<ldmx::TrigScintCluster> constituents_{0};
 
+  /**
+   * The fraction of the energy deposited in the track constituents 
+   * that was deposited by a beam electron.
+   * WARNING: this is a "truth" variable and will never be 
+   * measureable in actual data.
+   */
   float beamEfrac_{0.};
 
+  /**
+   * The average pe count of the clusters making up the track.
+   */
+  float PE_{0.};
+
+  
+  /**
+   * The X position.
+   */
+  float x_{-99999.};
+
+  /**
+   * The Y position.
+   */
+  float y_{-99999.};
+
+  /**
+   * The uncertainty on the X position.
+   */
+  float sx_{-9999.};
+
+  /**
+   * The uncertainty on the Y position.
+   */
+  float sy_{-9999.};
+
+  
   // these below here i don't think i'll use.
 
   /**
@@ -261,16 +352,6 @@ class TrigScintTrack {
    * The Z momentum.
    */
   float pz_{0};
-
-  /**
-   * The X position.
-   */
-  float x_{0};
-
-  /**
-   * The Y position.
-   */
-  float y_{0};
 
   /**
    * The Z position.
