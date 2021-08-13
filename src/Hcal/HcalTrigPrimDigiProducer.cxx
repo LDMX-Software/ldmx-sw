9c140037536c0dff180ef2a1ade42c6b2d4eee34
Theodor Christian Herwig <herwig@cern.ch>
2021-08-13 12:05:44 -0400

    make STQs linear. code format

--- src/Hcal/HcalTrigPrimDigiProducer.cxx
+++ src/Hcal/HcalTrigPrimDigiProducer.cxx
@@ -1,6 +1,8 @@
 #include "Hcal/HcalTrigPrimDigiProducer.h"
 
+#include "DetDescr/HcalGeometry.h"
 #include "Hcal/HcalTriggerGeometry.h"
+#include "Recon/Event/CaloTrigPrim.h"
 #include "Recon/Event/HgcrocDigiCollection.h"
 #include "Recon/Event/HgcrocTrigDigi.h"
 #include "Tools/HgcrocTriggerCalculations.h"
@@ -30,11 +32,11 @@ void HcalTrigPrimDigiProducer::produce(framework::Event& event) {
 
   // construct the calculator...
   ldmx::HgcrocTriggerCalculations calc(conditions);
-  
+
   // Loop over the digis
   for (unsigned int ix = 0; ix < hcalDigis.getNumDigis(); ix++) {
     const ldmx::HgcrocDigiCollection::HgcrocDigi pdigi = hcalDigis.getDigi(ix);
-    ldmx::HcalTriggerID tid = geom.belongsToQuad( ldmx::HcalDigiID(pdigi.id()) );
+    ldmx::HcalTriggerID tid = geom.belongsToQuad(ldmx::HcalDigiID(pdigi.id()));
 
     if (!tid.null()) {
       int tot = 0;
@@ -42,17 +44,23 @@ void HcalTrigPrimDigiProducer::produce(framework::Event& event) {
       calc.addDigi(pdigi.id(), tid.raw(), pdigi.soi().adc_t(), tot);
     }
   }
-  
-  // // Now, we compress the digis
+
+  // Now, we compress the digis
   calc.compressDigis(4);
+  const float hgc_compress_factor = 2;
 
   // const std::map<unsigned int, uint8_t> results;
   const std::map<unsigned int, uint8_t>& results = calc.compressedEnergies();
   ldmx::HgcrocTrigDigiCollection tdigis;
+  // ldmx::CaloTrigPrimCollection tdigisUC; // sums without any compression
+  // applied
 
   for (auto result : results) {
     if (result.second > 0) {
       tdigis.push_back(ldmx::HgcrocTrigDigi(result.first, result.second));
+      // tdigisUC.push_back( ldmx::CaloTrigPrim(result.first,
+      //         hgc_compress_factor*ldmx::HgcrocTrigDigi::compressed2Linear(result.second))
+      //         );
     }
   }
 
@@ -61,30 +69,39 @@ void HcalTrigPrimDigiProducer::produce(framework::Event& event) {
   for (auto result : results) {
     if (result.second > 0) {
       const ldmx::HcalTriggerID quad_id(result.first);
-      const std::vector<ldmx::HcalDigiID> precisions_ids = geom.contentsOfQuad(quad_id);
-      if (precisions_ids.size()==0) {
-        EXCEPTION_RAISE("TriggerIDLookupMismatch",
-                        "Attempted to lookup a nonexistent HcalDigiID from an HcalTriggerID");
+      const std::vector<ldmx::HcalDigiID> precisions_ids =
+          geom.contentsOfQuad(quad_id);
+      if (precisions_ids.size() == 0) {
+        std::cout << "Failing HcalTriggerID(" << quad_id.section() << ','
+                  << quad_id.layer() << ',' << quad_id.superstrip() << ','
+                  << quad_id.end() << ')' << std::endl;
+        EXCEPTION_RAISE("TrigToPrecIDMismatch",
+                        "Attempted to lookup a nonexistent HcalDigiID from an "
+                        "HcalTriggerID");
       }
-      const ldmx::HcalDigiID prec_id( precisions_ids.front().raw() );
-      const ldmx::HcalTriggerID stq_id = geom.belongsToSTQ( prec_id );
+      const ldmx::HcalDigiID prec_id(precisions_ids.front().raw());
+      const ldmx::HcalTriggerID stq_id = geom.belongsToSTQ(prec_id);
       auto ptr = stq_tps.find(stq_id.raw());
-      if (ptr != stq_tps.end()) ptr->second += result.second;
-      else stq_tps[stq_id.raw()] = result.second;
-    }    
+      int linear_charge =
+          hgc_compress_factor *
+          ldmx::HgcrocTrigDigi::compressed2Linear(result.second);
+      if (ptr != stq_tps.end()) {
+        ptr->second += linear_charge;
+      } else {
+        stq_tps[stq_id.raw()] = linear_charge;
+      }
+    }
   }
 
-  ldmx::HgcrocTrigDigiCollection stq_digis;
+  ldmx::CaloTrigPrimCollection stq_digis;
   for (auto result : stq_tps) {
     if (result.second > 0) {
-      stq_digis.push_back(ldmx::HgcrocTrigDigi(result.first, result.second));
-      // [Later] can apply calibration from ADC to MeV
-      // 1.2 PE/ADC * 1/(68 PE/MIP) * 4.66 MeV/MIP
-      //const float mev_per_adc = 1.2*4.66/68.;
+      stq_digis.push_back(ldmx::CaloTrigPrim(result.first, result.second));
     }
   }
 
   event.add(getName(), tdigis);
+  // event.add(getName()+"Uncompress", tdigisUC);
   event.add("hcalTrigPrimDigiSTQs", stq_digis);
 }
 }  // namespace hcal
