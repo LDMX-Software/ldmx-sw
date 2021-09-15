a2ca7bbf39f00e3ad8e7591dab52da71793d8863
tomeichlersmith <eichl008@umn.edu>
2021-09-15 09:52:58 -0500

    compiling and passing a simple test

--- Packing/include/Packing/RawDataFile/Reader.h
+++ Packing/include/Packing/RawDataFile/Reader.h
@@ -1,6 +1,7 @@
 #ifndef PACKING_RAWDATAFILE_READER_H_
 #define PACKING_RAWDATAFILE_READER_H_
 
+#include <iostream> //debuggin
 #include <fstream>
 #include <string>
 #include <type_traits>
@@ -19,9 +20,21 @@ namespace rawdatafile {
  */
 class Reader {
  public:
-  /// open the input file stream and get general info
-  Reader(const std::string& file_name) : file_{file_name, std::ios::in | std::ios::binary} {
+  /// default constructor
+  Reader() {
     file_.unsetf(std::ios::skipws);
+  }
+
+  /**
+   * Open a file with this reader
+   */
+  void open(const std::string& file_name) {
+    file_.open(file_name, std::ios::in | std::ios::binary);
+  }
+
+  /// open the input file stream and get general info
+  Reader(const std::string& file_name) : Reader() {
+    this->open(file_name);
   }
 
   /// destructor, close the input file stream
@@ -59,9 +72,47 @@ class Reader {
   template <typename WordType>
   Reader& read(WordType* w, std::size_t count) {
     static_assert(std::is_integral<WordType>::value, "Integral type required for Reader::read.");
-    file_.read(reinterpret_cast<char*>(w), sizeof(WordType)*count);
-    file_.seekg(sizeof(WordType)*count, std::ios::cur);
+    std::cout << sizeof(WordType)*count << " num bytes to be read...";
+    if (file_.read(reinterpret_cast<char*>(w), sizeof(WordType)*count)) {
+      std::cout << "success: " << std::hex << *w;
+    } else {
+      std::cout << "failure";
+    }
+    std::cout << std::endl;
+    // not sure if we need to pair seekg with read
+    //file_.seekg(sizeof(WordType)*count, std::ios::cur);
     return *this;
+  }
+
+  /**
+   * Read the next 'count' words into the input vector of words
+   *  
+   * This is common enough, I wanted to specialize the read function.
+   *
+   * The std::vector::resize is helpful for avoiding additional 
+   * copies while the vector is being expanded.
+   */
+  template <typename WordType>
+  Reader& read(std::vector<WordType>& vec, std::size_t count) {
+    vec.resize(count);
+    for (auto& w : vec) {
+      if(!this->read(&w, 1)) return *this;
+    }
+    return *this;
+  }
+
+  /**
+   * Check if reader is in a fail state
+   */
+  bool operator!() const {
+    return file_.fail();
+  }
+
+  /**
+   * Check if reader is in good/bad state
+   */
+  operator bool() const {
+    return !file_.fail();
   }
 
  private:
@@ -70,7 +121,7 @@ class Reader {
 };  // RawDataFile
 
 template <typename WordType>
-Reader& operator>>(Reader& r, WordType w) {
+Reader& operator>>(Reader& r, WordType& w) {
   return r.read(&w, 1);
 }
 
