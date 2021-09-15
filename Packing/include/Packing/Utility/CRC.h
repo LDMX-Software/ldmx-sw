e2cf177e68be44918ceec8941a25cb9c71639c4c
tomeichlersmith <eichl008@umn.edu>
2021-09-15 18:38:10 -0500

    lots of documentation about utility classes

--- Packing/include/Packing/Utility/CRC.h
+++ Packing/include/Packing/Utility/CRC.h
@@ -7,7 +7,7 @@ namespace packing {
 namespace utility {
 
 /**
- * @struct CRC
+ * @class CRC
  *
  * The HGC ROC and FPGA use a CRC checksum to double check that the
  * data transfer has been done correctly. Boost has a CRC checksum
@@ -16,31 +16,100 @@ namespace utility {
  * Idea for this helper struct was found on StackOverflow
  * https://stackoverflow.com/a/63237679
  * I've actually simplified it by limiting its use-case to our
- * type of data.
+ * type of data which is integral types, classes with the 'add'
+ * method defined, and vectors of those types.
+ *
+ * This is a very light class and is meant to be used as a calculator.
+ * It involves some fancy-shamncy template specialization using
+ * std::enable_if in order for the streaming operator to function.
+ *
+ * ## Example
+ *
+ *  class MyObject {
+ *   public:
+ *    CRC& add(CRC& c) {
+ *      c << my_member_int_;
+ *    }
+ *   private:
+ *    uint64_t my_member_int_;
+ *  } my_object; 
+ *
+ *  CRC c;
+ *  c << 0xffff << std::vector<uint16_t>({0xffff, 0x00ff}) << my_object;
+ *  uint32_t the_sum = c.get();
+ *
+ * ## Limitations
+ *
+ * This design limits the inputs to the calculator to two main categories.
+ *  1. Integral types (e.g. bool, int, unsigned int, long, char, ...)
+ *  2. Classes with the 'CRC& add(CRC&)' method defined
+ *  3. A std::vector of objects in (1) or (2)
+ * This means you will get a compiler error if you attempt to stream an
+ * object not fitting into one of these categories.
  */
-struct CRC {
-  // the object from Boost doing the summing
-  boost::crc_32_type crc;
-  // add a word to the sum
+class CRC {
+ public:
+  /**
+   * Stream an integral type into the calculator.
+   *
+   * This is only enabled for integral types i.e.
+   * it looks like this definition doesn't exist for
+   * types that are non-integral. Look at the C++ reference
+   * for std::is_integral to see a full list of integral types.
+   *
+   * @param w integral-type word to insert into calculator
+   * @return CRC modified calculator
+   */
   template <typename WordType, std::enable_if_t<std::is_integral<WordType>::value,bool> = true>
   CRC& operator<<(const WordType& w) { 
     crc.process_bytes(&w, sizeof(WordType));
     return *this;
   }
-  // add a word to the sum
-  template <typename ContentType>
-  CRC& operator<<(const std::vector<ContentType>& vec) { 
-    for (auto const& w : vec) (*this) << w;
-  }
-  // all other classes have to have a 'add' method defined
+
+  /**
+   * Stream an instance of a class into the calculator
+   *
+   * Here, we assume that the class we are streaming
+   * into the calculator has a specific public method defined.
+   *
+   *  CRC& add(CRC& c)
+   *
+   * and the user can insert the subcomponents to the calculator
+   * within this method.
+   *
+   * @param o instance of an arbitrary class
+   * @return CRC modified calculator
+   */
   template <typename ObjectType, std::enable_if_t<std::is_class<ObjectType>::value,bool> = true>
   CRC& operator<<(const ObjectType& o) {
-    o.add(*this);
+    return o.add(*this);
+  }
+  
+  /**
+   * Stream a vector of objects into the calculator
+   *
+   * When the compiler deduces that the input to the stream is a vector,
+   * we simply call the stream operator on all members of the vector in
+   * sequence.
+   *
+   * @param[in] vec vector of objects to insert into calculator
+   * @return CRC modified calculator
+   */
+  template <typename ContentType>
+  CRC& operator<<(const std::vector<ContentType>& vec) { 
+    for (auto const& w : vec) *this << w;
     return *this;
   }
-  // get the checksum
-  auto get() { return crc.checksum(); }
-};
+
+  /**
+   * Get the calculate checksum from the calculator
+   * @return uint32_t checksum
+   */
+  uint32_t get() { return crc.checksum(); }
+ private:
+  /// the object from Boost doing the summing
+  boost::crc_32_type crc;
+};  // CRC
 
 }  // namespace utility
 }  // namespace packing
--- Packing/include/Packing/Utility/Writer.h
+++ Packing/include/Packing/Utility/Writer.h
@@ -11,28 +11,38 @@ namespace utility {
 
 /**
  * @class Writer
- * Reading a raw data file.
+ * Writing a raw data file.
  *
- * We wrap a basic std::ifstream in order to make the retrieving
- * of specific-width words easier for ourselves. Each reader has
- * a defined 'WordType' which makes us able to read-out data files,
- * getting words one or more at a time.
+ * We wrap a basic std::ifstream in order to make the writing
+ * of specific-width words easier for ourselves.
  */
 class Writer {
  public:
-  /// default constructor, make sure we don't skip "whitespace"
+  /**
+   * default constructor
+   *
+   * make sure we don't skip "whitespace"
+   */
   Writer() {
     file_.unsetf(std::ios::skipws);
   }
 
   /**
    * Open a file with this writer
+   *
+   * We open the file stream in output, binary mode.
+   *
+   * @param[in] file_name name of file to open
    */
   void open(const std::string& file_name) {
     file_.open(file_name, std::ios::out | std::ios::binary);
   }
 
-  /// open the input file stream and get general info
+  /**
+   * Open the input file name upon construction of this writer.
+   *
+   * @param[in] file_name name of file to open
+   */
   Writer(const std::string& file_name) : Writer() {
     this->open(file_name);
   }
@@ -41,29 +51,79 @@ class Writer {
   ~Writer() = default;
 
   /**
-   * Write the 'num' words stored in array 'w'
+   * Write a certain number of words from the input array to the output file stream.
+   *
+   * This method is only enabled for integral types so we can safely reinterpret
+   * the underlying data as an array of characters.
+   *
+   * @tparam[in] WordType integral-type to write out
+   * @param[in] w pointer to array of words to write
+   * @param[in] num number of words in array
+   * @return *this
    */
   template <typename WordType, std::enable_if_t<std::is_integral<WordType>::value,bool> = true>
-  Writer& write(WordType* w, std::size_t num) {
+  Writer& write(const WordType* w, std::size_t num) {
     file_.write(reinterpret_cast<const char*>(w), sizeof(WordType)*num);
     return *this;
   }
 
+  /**
+   * Write a single integral-type word to the output file stream.
+   *
+   * @see write
+   *
+   * @tparam[in] WordType integral-type to write out
+   * @param[in] w single word to write out
+   * @return *this
+   */
   template <typename WordType, std::enable_if_t<std::is_integral<WordType>::value,bool> = true>
-  Writer& operator<<(WordType& w) {
+  Writer& operator<<(const WordType& w) {
     return write(&w, 1);
   }
 
+  /**
+   * Write out a vector of integral-type objects.
+   *
+   * This is a short-cut in order to help speed up writing.
+   *
+   * @tparam[in] WordType integral-type to write out
+   * @param[in] vec vector of integral words to write out
+   * @return *this
+   */
   template <typename WordType, std::enable_if_t<std::is_integral<WordType>::value,bool> = true>
   Writer& operator<<(const std::vector<WordType>& vec) {
     return write(vec.data(), vec.size());
   }
 
+  /**
+   * Write out a class object.
+   *
+   * We assume that the input class object has a specific method defined.
+   *
+   *  Writer& write(Writer&) const;
+   *
+   * With this method defined, then the class can be streamed out through
+   * this object.
+   *
+   * @tparam[in] ObjectType class-type to write out
+   * @param[in] o object to write out
+   * @return *this
+   */
   template <typename ObjectType, std::enable_if_t<std::is_class<ObjectType>::value,bool> = true>
   Writer& operator<<(const ObjectType& o) {
     return o.write(*this);
   }
 
+  /**
+   * Write out a vector fo class objects
+   *
+   * We make the same assumptions as when streaming out a single class object.
+   * We leave early if any write action changes the file to a fail state.
+   *
+   * @tparam[in] ObjectType class-type inside vector to write out
+   * @param[in] vec vector of objects to write out
+   * @return *this
+   */
   template <typename ObjectType, std::enable_if_t<std::is_class<ObjectType>::value,bool> = true>
   Writer& operator<<(const std::vector<ObjectType>& vec) {
     for (auto const& o : vec) if (!o.write(*this)) return *this;
@@ -72,6 +132,10 @@ class Writer {
 
   /**
    * Check if writer is in a fail state
+   *
+   * Uses std::ofstream::fail to check on stream.
+   *
+   * @return true if stream is in fail state
    */
   bool operator!() const {
     return file_.fail();
@@ -79,6 +143,10 @@ class Writer {
 
   /**
    * Check if writer is in a good/bad state
+   *
+   * Uses std::ofstream::fail to check on stream.
+   *
+   * @return true if stream is in good state
    */
   operator bool() const {
     return !file_.fail();
