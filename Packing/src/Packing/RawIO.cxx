e4deead2cf60142eea21c00dc5121d68dd54e7e3
tomeichlersmith <eichl008@umn.edu>
2021-09-27 10:53:02 -0500

    outline processor to interface with rawdatafile

---
+++ Packing/src/Packing/RawIO.cxx
@@ -0,0 +1,27 @@
+
+#include "Packing/RawIO.h"
+
+namespace packing {
+
+void RawIO::configure(framework::config::Parameters& ps) {
+  raw_file_ = std::make_unique<rawdatafile::File>(ps.getParameter<framework::config::Parameters>("raw_file"));
+}
+
+void RawIO::beforeNewRun(ldmx::RunHeader& header) {
+  raw_file_->writeRunHeader(header);
+}
+
+void RawIO::produce(framework::Event& event) {
+  raw_file->connect(event);
+  if (not raw_file_->nextEvent()) {
+    abortEvent();
+  }
+}
+
+void RawIO::onProcessEnd() {
+  raw_file_->close();
+}
+
+}  // namespace packing
+
+DECLARE_PRODUCER_NS(packing, RawIO)
