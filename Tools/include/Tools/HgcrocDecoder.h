9f8685e6a7f9a1aaff2a3bc9ae454e2585602f69
tomeichlersmith <eichl008@umn.edu>
2021-09-29 10:29:18 -0500

    move decoding to a tools class,
    EID translating will be handled by subsystems

---
+++ Tools/include/Tools/HgcrocDecoder.h
@@ -0,0 +1,35 @@
+#ifndef TOOLS_HGCROCDECODER_H_
+#define TOOLS_HGCROCDECODER_H_
+
+// LDMX
+#include "Framework/Configure/Parameters.h"
+#include "Framework/EventProcessor.h"
+
+namespace tools {
+
+/**
+ * @class HgcrocDecoder
+ * @brief Decodes the buffer output from an HGC ROC into the HgcrocDigiCollection object.
+ */
+class HgcrocDecoder {
+ public:
+  /**
+   * Class constructor.
+   */
+  HgcrocDecoder(int version = 2) : roc_version_{version} {}
+
+  /**
+   * Decode the input buffer of encoded data into a map
+   * of **electron IDs** to the digi samples.
+   */
+  std::map<uint32_t, std::vector<ldmx::HgcrocDigiCollection::Sample>>
+    decode(const std::vector<uint32_t>& encoded_data);
+
+ private:
+  /// version of the ROC that is being decoded
+  int roc_version_;
+};
+
+}  // namespace tools
+
+#endif  // TOOLS_HGCROCDECODER_H_
