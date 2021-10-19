f301196e77a3489f7b1d94d670b0686d8a14ff1b
tomeichlersmith <eichl008@umn.edu>
2021-10-18 19:28:52 -0500

    add reading file as option

--- python/hgcrocFormat.py
+++ python/hgcrocFormat.py
@@ -8,14 +8,12 @@ class HcalRawDecoder(Producer) :
     Parameters
     """
 
-    def __init__(self, output_name, roc_version = 2, input_name = 'HcalRawData', translate_eid = False) :
+    def __init__(self, output_name, roc_version = 2, input_name = 'HcalRawData', input_file = '', translate_eid = False) :
         super().__init__('hcalrawdecode','hcal::HcalRawDecoder','Hcal')
 
         self.input_name = input_name
         self.input_pass = ''
-
+        self.input_file = input_file
         self.output_name = output_name
-
         self.roc_version = roc_version
-
         self.translate_eid = translate_eid
