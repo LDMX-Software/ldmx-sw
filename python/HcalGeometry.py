aaa4b7e6ac0778eefe2fffc068bdfe8e652abfc2
tomeichlersmith <eichl008@umn.edu>
2021-10-20 10:04:53 -0500

    move detector map configuration class to its own file
    necessary because auto-loaded geometry conditions have requirements on
    the run header that we can't fullfill right now

--- python/HcalGeometry.py
+++ python/HcalGeometry.py
@@ -86,21 +86,3 @@ class HcalTriggerGeometryProvider(ldmxcfg.ConditionsObjectProvider):
 
 # make sure global instance is created, this registers the condition
 HcalTriggerGeometryProvider.getInstance()
-
-class HcalDetectorMap(ldmxcfg.ConditionsObjectProvider) :
-    """Provider of the Hcal detector map allowing translations between
-    electronics IDs and detector IDs.
-
-    Parameters
-    ----------
-    connections_table : str
-        Path to table of connections in Hcal
-    want_d2e : bool
-        Flag determining if we should spend the time to create a detID->EID LUT
-    """
-
-    def __init__(self, connections_table, want_d2e = False) :
-        super().__init__('HcalDetectorMap','hcal::HcalDetectorMapLoader','Hcal')
-        self.connections_table = connections_table
-        self.want_d2e = want_d2e
-
