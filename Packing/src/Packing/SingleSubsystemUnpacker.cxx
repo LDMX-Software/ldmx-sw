643fa56a4ac495ef36b9784e026d10541aa38cda
tomeichlersmith <eichl008@umn.edu>
2021-10-20 16:15:06 -0500

    new producer to unpack a single subsystems raw data file
    assumption is that a specific number of words (each of a specifc size)
    is read out on each event

---
+++ Packing/src/Packing/SingleSubsystemUnpacker.cxx
@@ -0,0 +1,39 @@
+
+#include "Packing/SingleSubsystemUnpacker.h"
+
+
+namespace packing {
+
+void SingleSubsystemUnpacker::configure(framework::config::Parameters& ps) {
+  reader_.open(ps.getParameter<std::string>("raw_file"));
+  num_words_per_event_ = ps.getParameter<int>("num_words_per_event");
+  num_bytes_per_word_ = ps.getParameter<int>("num_bytes_per_word");
+  output_name_ = ps.getParameter<std::string>("output_name");
+}
+
+void SingleSubsystemUnpacker::produce(framework::Event& event) {
+  if (!reader_ or reader_.eof()) abortEvent();
+
+  switch(num_bytes_per_word_) {
+    case 1 :
+      read<uint8_t>(event);
+      break;
+    case 2 :
+      read<uint16_t>(event);
+      break;
+    case 4 :
+      read<uint32_t>(event);
+      break;
+    case 8 :
+      read<uint64_t>(event);
+      break;
+    default:
+      EXCEPTION_RAISE("BadConfig",
+          "Unable to handle "+std::to_string(num_bytes_per_word_)
+          +" bytes per word.");
+  }
+}
+
+}  // namespace packing
+
+DECLARE_PRODUCER_NS(packing, SingleSubsystemUnpacker)
