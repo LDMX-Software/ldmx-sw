8b701ab7ac61324f71127541eb537181ee649f39
tomeichlersmith <eichl008@umn.edu>
2021-10-21 09:35:57 -0500

    load conditions from decoder constructor

--- python/hgcrocFormat.py
+++ python/hgcrocFormat.py
@@ -8,7 +8,7 @@ class HcalRawDecoder(Producer) :
     Parameters
     """
 
-    def __init__(self, output_name, roc_version = 2, input_name = 'HcalRawData', num_packets_per_event = 1, input_file = '', translate_eid = False) :
+    def __init__(self, output_name, roc_version = 2, input_name = 'HcalRawData', num_packets_per_event = 1, input_file = '', connections_table = None) :
         super().__init__('hcalrawdecode','hcal::HcalRawDecoder','Hcal')
 
         self.input_name = input_name
@@ -17,4 +17,11 @@ class HcalRawDecoder(Producer) :
         self.output_name = output_name
         self.roc_version = roc_version
         self.num_packets_per_event = num_packets_per_event
-        self.translate_eid = translate_eid
+
+        if connections_table is None :
+            self.translate_eid = False
+        else :
+            # load connections table into conditions system
+            from LDMX.Hcal.DetectorMap import HcalDetectorMap
+            HcalDetectorMap(connections_table)
+            self.translate_eid = True
