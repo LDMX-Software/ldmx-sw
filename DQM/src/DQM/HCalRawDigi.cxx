12263d5daf87c45080c647f5dad4d5ff876f619f
cmantill <criss.ms7@gmail.com>
2021-10-20 19:51:33 -0500

    change histograms to adc_by_strip given layer/end

--- DQM/src/DQM/HCalRawDigi.cxx
+++ DQM/src/DQM/HCalRawDigi.cxx
@@ -1,7 +1,7 @@
 
 #include "DQM/HCalRawDigi.h"
 #include "Recon/Event/HgcrocDigiCollection.h"
-#include "DetDescr/HcalElectronicsID.h"
+#include "DetDescr/HcalDigiID.h"
 
 namespace dqm {
 
@@ -12,24 +12,33 @@ void HCalRawDigi::configure(framework::config::Parameters& ps) {
 
 void HCalRawDigi::onProcessStart() {
   getHistoDirectory();
-  histograms_.create("adc_by_channel",
-      "HCal EID Index", 9960-9720+1, 9720, 9960, "ADC Counts in SOI", 200, 0, 200);
-  histograms_.get("adc_by_channel")->SetCanExtend(TH1::kAllAxes);
+  int nlayers = 20;
+  int nstrips = 4;
+  for (uint32_t i_layer{0}; i_layer < nlayers; i_layer++) {
+    std::string layer = std::to_string(i_layer);
+    for (uint32_t i_end{0}; i_end < 2; i_end++) {
+      std::string end = std::to_string(i_end);
+      histograms_.create("adc_by_strip_end"+end+"_layer"+layer,
+			 "HCal Strip", nstrips, 0, nstrips, "ADC Counts in SOI", 200, 0, 200);
+      histograms_.get("adc_by_strip_end"+end+"_layer"+layer)->SetCanExtend(TH1::kAllAxes);
+    }
+  }
 }
 
 void HCalRawDigi::analyze(const framework::Event& event) {
   // need to tell collection to decode in v2 style
   // TODO make configurable
   ldmx::HgcrocDigiCollection::setVersion(2);
-
+  
   auto digis{event.getObject<ldmx::HgcrocDigiCollection>(input_name_,input_pass_)};
   for (auto const& digi : digis) {
-    ldmx::HcalElectronicsID eid(digi.id());
-    histograms_.fill("adc_by_channel",eid.index(),digi.soi().adc_t());
+    ldmx::HcalDigiID did(digi.id());
+    std::string layer = std::to_string(did.layer());
+    std::string end = std::to_string(did.end());
+    histograms_.fill("adc_by_strip_end"+end+"_layer"+layer,did.strip(),digi.soi().adc_t());
   }
-
 }
-
+  
 }  // namespace dqm
 
 DECLARE_ANALYZER_NS(dqm, HCalRawDigi)
