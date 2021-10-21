39de9fd43c4c11a614a44f4ebaca9b0bdf0a2f26
tomeichlersmith <eichl008@umn.edu>
2021-10-21 14:36:12 -0500

    cleanup single subsys unpacker - remove unneeded word length option

--- Packing/src/Packing/SingleSubsystemUnpacker.cxx
+++ Packing/src/Packing/SingleSubsystemUnpacker.cxx
@@ -10,8 +10,7 @@ void SingleSubsystemUnpacker::beforeNewRun(ldmx::RunHeader& rh) {
 
 void SingleSubsystemUnpacker::configure(framework::config::Parameters& ps) {
   reader_.open(ps.getParameter<std::string>("raw_file"));
-  num_words_per_event_ = ps.getParameter<int>("num_words_per_event");
-  num_bytes_per_word_ = ps.getParameter<int>("num_bytes_per_word");
+  num_bytes_per_event_ = ps.getParameter<int>("num_bytes_per_event");
   output_name_ = ps.getParameter<std::string>("output_name");
   detector_name_=ps.getParameter<std::string>("detector_name");
 }
@@ -19,24 +18,13 @@ void SingleSubsystemUnpacker::configure(framework::config::Parameters& ps) {
 void SingleSubsystemUnpacker::produce(framework::Event& event) {
   if (!reader_ or reader_.eof()) abortEvent();
 
-  switch(num_bytes_per_word_) {
-    case 1 :
-      read<uint8_t>(event);
-      break;
-    case 2 :
-      read<uint16_t>(event);
-      break;
-    case 4 :
-      read<uint32_t>(event);
-      break;
-    case 8 :
-      read<uint64_t>(event);
-      break;
-    default:
-      EXCEPTION_RAISE("BadConfig",
-          "Unable to handle "+std::to_string(num_bytes_per_word_)
-          +" bytes per word.");
+  std::vector<uint8_t> buff;
+  if(!reader_.read(buff, num_bytes_per_event_)) {
+    EXCEPTION_RAISE("MalForm",
+      "Raw file provided was unable to read "+std::to_string(num_bytes_per_event_)
+      +" bytes in an event.");
   }
+  event.add(output_name_, buff);
 }
 
 }  // namespace packing
