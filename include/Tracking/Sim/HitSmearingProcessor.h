328e6024ac5140f79271dc7217e0ad6c29ebc459
Omar Moreno <email@omarmoreno.net>
2021-11-01 16:34:21 -0700

    Format and rename.

--- include/Tracking/Sim/HitSmearingProcessor.h
+++ include/Tracking/Sim/HitSmearingProcessor.h
@@ -1,36 +1,24 @@
-/*
-  author: pf
-  email: pbutti@slac.stanford.edu
-*/
-
-
 #ifndef TRACKING_SIM_HITSMEARINGPROCESSOR_H_
 #define TRACKING_SIM_HITSMEARINGPROCESSOR_H_
 
-/*~~~~~~~~~~~~~~~*/
-/*   Framework   */
-/*~~~~~~~~~~~~~~~*/
-//#include "Framework/Event.h" //needed??!
+//---< C++ >---//
+#include <iostream>
+#include <random>
+
+//---< Framework >---//
+#include "Framework/Event.h"
 #include "Framework/Configure/Parameters.h"
 #include "Framework/EventProcessor.h"
 
-
-/*~~~~~~~~~~~~~*/
-/*   SimCore   */
-/*~~~~~~~~~~~~~*/
+//---< SimCore >---//
 #include "SimCore/Event/SimTrackerHit.h"
-
-
-/*~~~~~~~~~~~~~*/
-/*   stdlib    */
-/*~~~~~~~~~~~~~*/
-
-#include <iostream>
-#include <random>
 
 namespace tracking {
 namespace sim {
 
+/**
+ * @brief Class that performs simulated hit smearing in the tracker
+ */
 class HitSmearingProcessor : public framework::Producer {
 
 public:
@@ -65,21 +53,26 @@ public:
   void produce(framework::Event &event);
 
 private:
-  std::shared_ptr<std::normal_distribution<float> > distN;  
-  std::vector<std::string> m_inputHitCollections;
-  std::vector<std::string> m_outputHitCollections;
-  
-  /* smearing sigmas */
-  double m_taggerSigma_u, m_taggerSigma_v;
-  double m_recoilSigma_u, m_recoilSigma_v;
-     
-  ldmx::SimTrackerHit smearSimHit(const ldmx::SimTrackerHit& hit);
+  ///
+  std::shared_ptr<std::normal_distribution<float>> normal_;
 
-  /* random engine */
-  std::default_random_engine m_generator;
-    
+  ///
+  std::vector<std::string> input_hit_coll_;
+  
+  /// 
+  std::vector<std::string> output_hit_coll_;
+
+  /// smearing sigmas 
+  double tagger_sigma_u_, tagger_sigma_v_;
+  double recoil_sigma_u_, recoil_sigma_v_;
+
+  /// 
+  ldmx::SimTrackerHit smearSimHit(const ldmx::SimTrackerHit &hit);
+
+  /// random engine 
+  std::default_random_engine generator_;
+
 }; // HitSmearingProcessor
-    
 
 } // namespace sim
 } // namespace tracking
