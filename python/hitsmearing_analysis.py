f594b63acd3eb9b5f722c80e93f3298dd1b54aa0
pbutti <pierfrancesco.butti@gmail.com>
2021-11-03 16:27:06 +0100

    Removed Analysis package

--- python/hitsmearing_analysis.py
+++
@@ -1,6 +0,0 @@
-from LDMX.Framework.ldmxcfg import Analyzer
-
-class HitSmearingAnalysis(Analyzer):
-
-    def __init__(self,name="HitSmearingAnalysis") :
-        super().__init__(name,'tracking::analysis::HitSmearingAnalysis','Tracking')
