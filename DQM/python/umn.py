ddb20293c48968d8af9c9cbcd8effcc00090b4b6
tomeichlersmith <eichl008@umn.edu>
2022-01-14 15:18:25 -0600

    allow for NO pedestals to be given

--- DQM/python/umn.py
+++ DQM/python/umn.py
@@ -5,7 +5,7 @@ for our test stand setup"""
 from LDMX.Framework import ldmxcfg
 
 class TestHgcRoc(ldmxcfg.Analyzer) :
-    def __init__(self,input_name, pedestal_table, input_pass = '',name = 'hgcroc') :
+    def __init__(self,input_name, pedestal_table = 'NO_PEDESTALS', input_pass = '',name = 'hgcroc') :
         super().__init__(name,'dqm::umn::TestHgcRoc','DQM')
         self.input_name = input_name
         self.input_pass = input_pass
@@ -13,5 +13,8 @@ class TestHgcRoc(ldmxcfg.Analyzer) :
 
         from LDMX.Conditions.SimpleCSVTableProvider import SimpleCSVIntegerTableProvider
         t = SimpleCSVIntegerTableProvider(pedestal_table,["PEDESTAL"])
-        t.validForever(f'file://{pedestal_table}')
+        if pedestal_table == 'NO_PEDESTALS' :
+            t.validForAllRows([0])
+        else :
+            t.validForever(f'file://{pedestal_table}')
 
