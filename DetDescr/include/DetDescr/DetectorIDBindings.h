06a746278b73446bd302aca531c7ac819e428ac5
Einar Elén <einar.elen@gmail.com>
2022-01-21 20:45:35 +0100

    Move things to header file

---
+++ DetDescr/include/DetDescr/DetectorIDBindings.h
@@ -0,0 +1,13 @@
+
+#ifndef DETDESCR_DETECTORID_BINDINGS_H_
+#define DETDESCR_DETECTORID_BINDINGS_H_
+
+#include <boost/python.hpp>
+
+#include "DetDescr/DetectorID.h"
+#include "DetDescr/HcalAbstractID.h"
+#include "DetDescr/HcalDigiID.h"
+#include "DetDescr/HcalElectronicsID.h"
+#include "DetDescr/HcalID.h"
+
+#endif /* DETDESCR_DETECTORID_BINDINGS_H_ */
