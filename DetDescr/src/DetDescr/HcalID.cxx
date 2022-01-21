fd6e0b90fb9b7ea18c0bc471686d0c2774c59d1a
Einar Elén <einar.elen@gmail.com>
2022-01-21 15:40:52 +0100

    Sufficient to compile with Boost.Python

--- DetDescr/src/DetDescr/HcalID.cxx
+++ DetDescr/src/DetDescr/HcalID.cxx
@@ -28,3 +28,11 @@ void HcalID::createInterpreters() {
 }
 
 }  // namespace ldmx
+
+#include <boost/python.hpp>
+
+char const* greet() { return "Hello, world!\n"; }
+BOOST_PYTHON_MODULE(hello_ext) {
+  using namespace boost::python;
+  def("greet", greet);
+}
