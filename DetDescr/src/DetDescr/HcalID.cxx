b5eb0a66efea41d5cdafb13d8f2a4ee0aae5e280
Einar Elén <einar.elen@gmail.com>
2022-01-21 15:58:29 +0100

    Use same name as the shared object

--- DetDescr/src/DetDescr/HcalID.cxx
+++ DetDescr/src/DetDescr/HcalID.cxx
@@ -32,7 +32,7 @@ void HcalID::createInterpreters() {
 #include <boost/python.hpp>
 
 char const* greet() { return "Hello, world!\n"; }
-BOOST_PYTHON_MODULE(hello_ext) {
+BOOST_PYTHON_MODULE(libDetDescr) {
   using namespace boost::python;
   def("greet", greet);
 }
