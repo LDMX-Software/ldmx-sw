7b07448b2ab0daed0eca9edb02acc05c5c848369
Einar Elén <einar.elen@gmail.com>
2022-01-22 11:12:28 +0100

    Add in the remaining ID headers

--- DetDescr/include/DetDescr/DetectorIDBindings.h
+++ DetDescr/include/DetDescr/DetectorIDBindings.h
@@ -5,9 +5,14 @@
 #include <boost/python.hpp>
 
 #include "DetDescr/DetectorID.h"
+#include "DetDescr/EcalAbstractID.h"
+#include "DetDescr/EcalElectronicsID.h"
+#include "DetDescr/EcalID.h"
+#include "DetDescr/EcalTriggerID.h"
 #include "DetDescr/HcalAbstractID.h"
 #include "DetDescr/HcalDigiID.h"
 #include "DetDescr/HcalElectronicsID.h"
 #include "DetDescr/HcalID.h"
+#include "DetDescr/HcalTriggerID.h"
 
 #endif /* DETDESCR_DETECTORID_BINDINGS_H_ */
