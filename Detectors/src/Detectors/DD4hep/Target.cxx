4868f1d80c6a56a620f905e8535c6595fb7187b2
Omar Moreno <email@omarmoreno.net>
2022-01-25 20:22:42 -0800

    Add target builder.

---
+++ Detectors/src/Detectors/DD4hep/Target.cxx
@@ -0,0 +1,95 @@
+
+//---< DD4hep >---//
+#include "XML/Helper.h"
+#include "DD4hep/DetFactoryHelper.h"
+
+//---< Detectors >---//
+#include "Detectors/Geo/Box.h"
+
+//---< Framework >---//
+#include "Framework/Exception/Exception.h"
+
+static Ref_t create_target(Detector &lcdd, xml::Handle_t xml_handle,
+                            SensitiveDetector sens_det) {
+  
+   // Detector xml handle
+  xml::DetElement det_handle = xml_handle;
+
+  // Store the name and ID of the subdetector
+  auto name{det_handle.nameStr()};
+  auto subdet_id{det_handle.id()};
+
+  // Set the sensitive detector type. This will match the subdetector ID type
+  // within Detectors/DD4hep/DetectorID.h.
+  sens_det.setType(name);
+
+  // Create the target detector element
+  DetElement target(name, subdet_id);
+
+  // Get the dimensions of the target envelope and construct a box shape made
+  // out of air.  This volume will be used to contain the target.
+  auto env_dims(det_handle.dimensions());
+  Box env_box(env_dims.dx(), env_dims.dy(), env_dims.dz());
+  Volume env_vol(name + "_envelope", env_box, lcdd.air());
+  // Set the attributes (region, visualization) of the envelope
+  env_vol.setAttributes(lcdd, det_handle.regionStr(), det_handle.limitsStr(),
+                        det_handle.visStr());
+
+  // The placed volume
+  PlacedVolume pv;
+
+  // Loop over all of the modules and create the sensor volumes.
+  for (xml::Collection_t imodule(det_handle, _U(module)); imodule; ++imodule) {
+    xml::Component xml_module(imodule);
+
+    // Start by creating an assembly for the modules. An assembly will act as
+    // bounding box for the two silicon layers it encloses.
+    Assembly module_assembly(name + "_module_" +
+                             std::to_string(xml_module.id()) + "_assembly");
+
+    for (xml::Collection_t ilayer(xml_module, _U(layer)); ilayer; ++ilayer) {
+      xml::Component xml_layer(ilayer);
+
+      // Create the detector element for the target
+      DetElement layer(target,
+                       name + "_layer_" + std::to_string(xml_layer.id()),
+                       sensor_id.raw());
+      layer.setType("target");
+
+      // Create the box shape representing the target.  If a box can't be
+      // created, throw an exception.
+      // Box sensor_box{xml_layer.createShape()};
+      auto sensor_dims(xml_layer.dimensions());
+      detectors::geo::Box sensor_box(
+          name + "_layer_" + std::to_string(xml_layer.id()) + "_solid",
+          sensor_dims.dx(), sensor_dims.dy(), sensor_dims.dz());
+      if (!sensor_box.isValid()) {
+        EXCEPTION_RAISE("FailedToCreate", "Failed to create a box volume.");
+      }
+
+      // Create a volume out of the box and set the material it's made from.
+      auto sensor_mat{lcdd.material(xml_layer.materialStr())};
+      auto position{xml_layer.position()};
+      Volume layer_vol("module_" + std::to_string(xml_module.id()) + "_layer_" +
+                           std::to_string(xml_layer.id()),
+                       sensor_box, sensor_mat);
+
+    // Get the position of the module and place it inside of the target
+    // envelope.
+    auto module_position{xml_module.position()};
+    pv = env_vol.placeVolume(module_assembly,
+                             Position(module_position.x(), module_position.y(),
+                                      module_position.z()));
+  }
+
+  // Get the global position of the target envelope and place it in the mother
+  // volume.
+  auto env_pos{det_handle.position()};
+  auto env_placed_vol(lcdd.pickMotherVolume(target).placeVolume(
+      env_vol, Position(env_pos.x(), env_pos.y(), env_pos.z())));
+  target.setPlacement(env_placed_vol);
+
+  return target;
+}
+
+DECLARE_DETELEMENT(target, create_target)
