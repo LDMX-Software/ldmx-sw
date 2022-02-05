fbb7e1db8da9e5266b6fdf74e174bb6751db714e
Omar Moreno <email@omarmoreno.net>
2022-02-04 16:00:28 -0800

    Parameters specific to the recoil and tagger tracker are no longer required.  Add docs.

--- python/HitSmearingProcessor.py
+++ python/HitSmearingProcessor.py
@@ -1,16 +1,25 @@
 from LDMX.Framework.ldmxcfg import Producer
 
 class HitSmearingProcessor(Producer) :
+    """Producer that smears simulated tracker hits.
+
+    Parameters
+    ----------
+    instance_name : str
+        Unique name for this instance.
+    """
+
     def __init__(self, instance_name = "HitSmearingProcessor"):
         super().__init__(instance_name, 'tracking::sim::HitSmearingProcessor','Tracking')
 
-        self.inputHitCollections  = [""]
-        self.outputHitCollections = [""] 
+        # Name of the input sim hit collection. 
+        self.input_hit_coll = ''
 
-        self.taggerSigma_u = 0.05 #50um
-        self.taggerSigma_v = 0.25 #250um
-        
-        self.recoilSigma_u = 0.05 #50um
-        self.recoilSigma_v = 0.25 #250um
+        # Name of the output sim hit collection.
+        self.output_hit_coll = ''
 
-        
+        # The deviation in the u direction. 
+        self.sigma_u = 0.05 # 50 um
+
+        # The deviation in the v direction.
+        self.sigma_v = 0.25 # 250 um
