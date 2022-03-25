e6036a16fd30a209ffc7e6dbda26846d1bf07347
Tom Eichlersmith <eichl008@umn.edu>
2022-03-25 14:32:52 -0500

    change hcal EIDs to ROC and roc channels
    as opposed to link and link channels, roc and roc channels are more
    sustainable for developing daq maps and threshold files

--- DetDescr/src/DetDescr/HcalElectronicsID.cxx
+++ DetDescr/src/DetDescr/HcalElectronicsID.cxx
@@ -2,6 +2,6 @@
 #include "DetDescr/DetectorIDInterpreter.h"
 
 std::ostream& operator<<(std::ostream& s, const ldmx::HcalElectronicsID& id) {
-  s << "HcalElectronics(" << id.fiber() << ',' << id.elink() << ',' << id.channel() << ')';
+  s << "HcalElectronics(" << id.polarfire() << ',' << id.roc() << ',' << id.channel() << ')';
   return s;
 }
