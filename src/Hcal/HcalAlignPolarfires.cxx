a238f46a605cfc71b9cdbf79a3ff1b16ef47ef33
Tom Eichlersmith <eichl008@umn.edu>
2022-04-19 18:02:50 -0500

    looks to be operational, more testing required

--- src/Hcal/HcalAlignPolarfires.cxx
+++ src/Hcal/HcalAlignPolarfires.cxx
@@ -9,8 +9,9 @@ namespace hcal {
 /**
  * Align the two polarfires with drop/keep hints signalling successful merge
  *
- * Only checking for /dropped/ events, assuming that ticks and spills are already
- * in correct order.
+ * - Only checking for /dropped/ events
+ * - assuming that ticks and spills are already in correct ORDER
+ * - assuming spill numbering is NOT the same between the two DPMs
  */
 class HcalAlignPolarfires : public framework::Producer {
   /// input decoded objects (vector index == polarfire index)
@@ -23,27 +24,35 @@ class HcalAlignPolarfires : public framework::Producer {
   static int max_tick_diff_;
  public:
   struct PolarfireQueueEntry {
+    /// the i'th spill
     int spill;
+    /// ticks since spill
     int ticks;
     ldmx::HgcrocDigiCollection digis;
     PolarfireQueueEntry(const framework::Event& event, 
-        const std::string& input_name, const std::string& input_pass) {
-      spill = event.getObject<int>(input_name+"Spill", input_pass);
+        const std::string& input_name, const std::string& input_pass, 
+        std::pair<int,int>& spill_counter) {
+      int spilln = event.getObject<int>(input_name+"Spill", input_pass);
+      if (spilln != spill_counter.second) {
+        spill_counter.first++;
+        spill_counter.second = spilln;
+      }
+      spill = spill_counter.first;
       ticks = event.getObject<int>(input_name+"Ticks", input_pass);
       digis = event.getObject<ldmx::HgcrocDigiCollection>(input_name,input_pass);
     }
-    bool same_event(const PolarfireQueueEntry& other) {
-      return spill == other.spill and abs(ticks - other.ticks) < max_tick_diff_;
+    bool same_event(const PolarfireQueueEntry& rhs) {
+      return (spill == rhs.spill and abs(ticks - rhs.ticks) < max_tick_diff_);
     }
-    bool earlier_event(const PolarfireQueueEntry& other) {
-      if (spill != other.spill) return spill < other.spill;
-      return ticks < other.ticks;
+    bool earlier_event(const PolarfireQueueEntry& rhs) {
+      if (spill == rhs.spill) return ticks < rhs.ticks;
+      return spill < rhs.spill;
     }
   };
-  /// queue of unmatched digis within a spill
+  /// queue of unmatched digis 
   std::queue<PolarfireQueueEntry> pf0_queue, pf1_queue;
-  /// spill we are on currently
-  int spill_{-1};
+  /// spill counter
+  std::pair<int,int> pf0_spill_counter{0,-1}, pf1_spill_counter{0,-1};
  public:
   HcalAlignPolarfires(const std::string& n, framework::Process& p)
     : framework::Producer(n,p) {}
@@ -63,50 +72,64 @@ void HcalAlignPolarfires::configure(framework::config::Parameters& ps) {
 
 void HcalAlignPolarfires::produce(framework::Event& event) {
   // put next package of decoding into the queues
-  pf0_queue.emplace(event, input_names_[0], input_pass_);
-  pf1_queue.emplace(event, input_names_[1], input_pass_);
+  pf0_queue.emplace(event, input_names_[0], input_pass_, pf0_spill_counter);
+  pf1_queue.emplace(event, input_names_[1], input_pass_, pf1_spill_counter);
 
-  // remove empty events from front of queues
+  // remove empty events from front of queues for end-of-file condition
   while (pf0_queue.size() > 0 and pf0_queue.front().digis.getNumDigis() == 0) pf0_queue.pop();
   while (pf1_queue.size() > 0 and pf1_queue.front().digis.getNumDigis() == 0) pf1_queue.pop();
 
-  // if both queues are empty, abort event
-  if (pf0_queue.size() == 0 and pf1_queue.size() == 1) abortEvent();
-
-  // we have non-empty events in the queues, start alignment procedure
-  if (pf0_queue.front().same_event(pf1_queue.front())) {
-    // add them together and put them into same object
-    auto merged = pf0_queue.front().digis;
-    const auto& unmerged{pf1_queue.front().digis};
-    for (int i{0}; i < unmerged.getNumDigis(); i++) {
-      auto digi{unmerged.getDigi(i)};
-      std::vector<ldmx::HgcrocDigiCollection::Sample> samples;
-      for (int j{0}; j < unmerged.getNumSamplesPerDigi(); j++) {
-        samples.push_back(digi.at(i));
+  bool aligned{false};
+  ldmx::HgcrocDigiCollection merged;
+  if (pf0_queue.size() > 0 and pf1_queue.size() > 0) {
+    // we have non-empty events in the queues, start alignment procedure
+    if (pf0_queue.front().same_event(pf1_queue.front())) {
+      // same spill and ticks within max_tick_diff_ of each other
+      // add them together and put them into same object
+      merged = pf0_queue.front().digis;
+      const auto& unmerged{pf1_queue.front().digis};
+      for (int i{0}; i < unmerged.getNumDigis(); i++) {
+        auto digi{unmerged.getDigi(i)};
+        std::vector<ldmx::HgcrocDigiCollection::Sample> samples;
+        for (int j{0}; j < unmerged.getNumSamplesPerDigi(); j++) {
+          samples.push_back(digi.at(i));
+        }
+        merged.addDigi(digi.id(), samples);
       }
-      merged.addDigi(digi.id(), samples);
+      aligned = true;
+      pf0_queue.pop();
+      pf1_queue.pop();
+      setStorageHint(framework::hint_shouldKeep);
+    } else if (pf0_queue.front().earlier_event(pf1_queue.front())) {
+      // should add pf0 but signal event is unmerged
+      merged = pf0_queue.front().digis;
+      pf0_queue.pop();
+      setStorageHint(framework::hint_shouldDrop);
+    } else {
+      // should add pf1 but signal event is unmerged
+      merged = pf1_queue.front().digis;
+      pf1_queue.pop();
+      setStorageHint(framework::hint_shouldDrop);
     }
-    event.add(output_name_,merged);
-    bool aligned{true};
-    event.add(output_name_+"Aligned",aligned);
-    pf0_queue.pop();
-    pf1_queue.pop();
-    setStorageHint(framework::hint_shouldKeep);
-  } else if (pf0_queue.front().earlier_event(pf1_queue.front())) {
-    // should add pf0 but signal event is unmerged
-    event.add(output_name_, pf0_queue.front().digis);
-    bool aligned{false};
-    event.add(output_name_+"Aligned",aligned);
-    pf0_queue.pop();
-    setStorageHint(framework::hint_shouldDrop);
+  } else if (pf0_queue.size() > 0) {
+    // only pf0 has non-empty events left
+      // should add pf0 but signal event is unmerged
+      merged = pf0_queue.front().digis;
+      pf0_queue.pop();
+      setStorageHint(framework::hint_shouldDrop);
+  } else if (pf1_queue.size() > 0) {
+    // only pf1 has non-empty events left
+      // should add pf1 but signal event is unmerged
+      merged = pf1_queue.front().digis;
+      pf1_queue.pop();
+      setStorageHint(framework::hint_shouldDrop);
   } else {
-    // should add pf1 but signal event is unmerged
-    event.add(output_name_, pf1_queue.front().digis);
-    bool aligned{false};
-    event.add(output_name_+"Aligned",aligned);
-    pf1_queue.pop();
-    setStorageHint(framework::hint_shouldDrop);
+    // no more events, both decoders are returning empty events
+    abortEvent();
   }
+
+  event.add(output_name_,merged);
+  event.add(output_name_+"Aligned",aligned);
 } // produce
 
 }
