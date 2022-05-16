8a1b64139497a2d11d50a9fbf645943f097109ff
Tom Eichlersmith <eichl008@umn.edu>
2022-05-16 09:38:32 -0500

    WIP move recon conditions to be a CO and start provider

--- src/Hcal/HcalReconConditions.cxx
+++ src/Hcal/HcalReconConditions.cxx
@@ -4,34 +4,43 @@ namespace hcal {
 
 const std::string HcalReconConditions::CONDITIONS_NAME = "HcalReconConditions";
 
-/**
- * The order of these column names needs to
- * match the indices listed in the header.
- */
-const std::vector<std::string> HcalReconConditions::EXPECTED_COLUMNS = {
-    "ADC_PEDESTAL", "ADC_GAIN", "TOT_PEDESTAL", "TOT_GAIN"};
-
-HcalReconConditions::HcalReconConditions(
-    const conditions::DoubleTableCondition& table, bool validate)
-    : the_table_{table} {
-  // leave early if we don't want to validate
-  if (!validate) return;
-
-  if (the_table_.getColumnCount() != EXPECTED_COLUMNS.size()) {
-    EXCEPTION_RAISE(
-        "ConditionsException",
-        "Inconsistent condition for HcalReconConditions :" + table.getName());
-  }
-
-  // not using fancy C++17 to shorten this because we want the error to
-  // tell us where the failure is
-  for (size_t i = 0; i < EXPECTED_COLUMNS.size(); i++) {
-    if (the_table_.getColumnNames().at(i) != EXPECTED_COLUMNS.at(i)) {
-      EXCEPTION_RAISE("ConditionsException",
-                      "Expected column '" + EXPECTED_COLUMNS.at(i) +
-                          "', got '" + the_table_.getColumnNames().at(i) + "'");
-    }  // does name match?
-  }    // loop through columns
+HcalReconConditions(const conditions::DoubleTableCondition& adc_ped, 
+    const conditions::DoubleTableCondition& adc_gain,
+    const conditions::DoubleTableCondition& tot_ped,
+    const conditions::DoubleTableCondition& tot_gain)
+  : framework::ConditionsObject(CONDITIONS_NAME),
+    adc_pedestals_{adc_ped}, adc_gains_{adc_gain},
+    tot_pedestals_{tot_ped}, tot_gains_{tot_gain} {}
 }
+
+class HcalReconConditionsProvider : public framework::ConditionsObjectProvider {
+  std::map<std::string,std::unique_ptr<conditions::DoubleTableCondition*>>
+    recon_conditions_;
+ public:
+  HcalReconConditionsProvider(const std::string& name, const std::string& tagname,
+                              const framework::condfig::Parmaeters& parameters,
+                              framework::Process& proc)
+    : ConditionsObjectProvider(HcalReconConditions::CONDITIONS_NAME,
+                               tagname, parameters, proc) {
+      recon_conditions_[parameters.getParameter<std::string>("adc_gain")] = nullptr;
+      recon_conditions_[parameters.getParameter<std::string>("adc_ped")] = nullptr;
+      recon_conditions_[parameters.getParameter<std::string>("tot_gain")] = nullptr;
+      recon_conditions_[parameters.getParameter<std::string>("tot_ped")] = nullptr;
+    }
+
+  /// take no action on release becuase smart pointers
+  virtual void releaseConditionsObject(const framework::ConditionsObject* co) {}
+
+  virtual std::pair<const framework::ConditionsObject*, framework::ConditionsIOV>
+  getCondition(const ldmx::EventHeader& context) {
+    for (auto& [name, table] : recon_conditions_) {
+      if (!table) {
+        // need to get name
+      }
+    }
+
+
+  }
+};
 
 }  // namespace hcal
