d74278239d7a91f146230ec2fab2b003a76bbbf6
pbutti <pierfrancesco.butti@gmail.com>
2022-06-02 11:09:05 +0200

    Added seed tracks to output file. Cleaned up log and fixed layerID

--- python/examples/SeedFinding.py
+++ python/examples/SeedFinding.py
@@ -8,6 +8,7 @@ p.libraries.append("libTracking.so")
 from LDMX.Tracking import tracking_seedfinder
 
 seeder = tracking_seedfinder.SeedFinderProcessor()
+seeder.debug = False
 
 p.sequence = [seeder]
 
@@ -17,8 +18,8 @@ print(p.sequence)
 #/nfs/slac/g/ldmx/data/mc20/v12/4.0GeV/v2.2.1-batch8/mc_v12-4GeV-1e-ecal_photonuclear_run1075000_t1600764279.root
 
 p.inputFiles = [os.environ["LDMX_BASE"]+"/data_ldmx/mc_v12-4GeV-1e-inclusive_run1310001_t1601628859_reco.root"]
-p.outputFiles = [os.environ["LDMX_BASE"]+'/data_ldmx/seeds.root']
+p.outputFiles = ["./seeds.root"]
 
 p.termLogLevel=0
 
-p.maxEvents = 10000
+p.maxEvents = 1000
