5c5c6ec13ca5cf2035b7ef90cfc02df01fa0199c
Tom Eichlersmith <eichl008@umn.edu>
2022-06-14 09:11:31 -0500

    update paths so that index is loaded properly

--- python/hcal_testbeam0422_conditions.py
+++ python/hcal_testbeam0422_conditions.py
@@ -1,10 +1,24 @@
-"""Load the April 2022 Testbeam conditions for HCal"""
+"""Load the April 2022 Testbeam conditions for HCal
+
+We assume that the conditions-data repository has been cloned into LDMX_BASE.
+
+    cd ${LDMX_BASE}
+    git clone https://github.com/LDMX-Software/conditions-data
+
+You can modify this path if you have your conditions somewhere else.
+**Make sure these conditions are available to the container.
+
+    import hcal_testbeam0422_conditions
+    hcal_testbeam0422_conditions.adc_pedestal.conditions_baseURL = 'file:///full/path/my/url/'
+
+"""
 
 from LDMX.Conditions.SimpleCSVTableProvider import SimpleCSVIntegerTableProvider, SimpleCSVDoubleTableProvider
+import os
 
-adc_pedestal = SimpleCSVDoubleTableProvider("hcal_adc_pedestal",["ADC_PEDESTAL","ADC_RMS"])
-adc_pedestal.conditions_baseURL = os.environ['LDMX_BASE']+'/conditions-data/'
-adc_pedestal.entriesURL = '${LDMX_CONDITIONS_BASEURL}/Hcal/testbeam04-22/pedestals/index-v1_0_0.csv'
+adc_pedestal = SimpleCSVDoubleTableProvider("hcal_adc_pedestal",["PEDESTAL_ADC","PEDESTAL_RMS_ADC"])
+adc_pedestal.conditions_baseURL = f'file://{os.environ["LDMX_BASE"]}/conditions-data/'
+adc_pedestal.entriesURL = '${LDMX_CONDITION_BASEURL}/Hcal/testbeam04-2022/pedestals/index_v1_0_0.csv'
 
 adc_gain = SimpleCSVDoubleTableProvider("hcal_adc_gain",["gain"])
 adc_gain.validForAllRows([1.2]) # 4 ADCs per PE - maxADCRange/readoutPadCapacitance/1024
