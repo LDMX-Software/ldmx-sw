5de2981148c6d80eaecf4d07ccd529659814e420
Einar Elén <einar.elen@gmail.com>
2022-07-15 11:45:53 +0200

    Update GDML thickness parameters

--- DetDescr/python/HcalGeometry.py
+++ DetDescr/python/HcalGeometry.py
@@ -128,11 +128,16 @@ class HcalGeometry() :
         """
         self.prototype=HcalReadoutGeometry()
         # GDML-parameters
-        air_thickness=2.
-        absorber_thickness=25
-        scint_thickness=20.
-        scint_bar_length=2000.
-        layer_thickness = absorber_thickness + scint_thickness + 2 * air_thickness
+        absorber_thickness = 20.
+        scint_thickness = 20.
+        scint_bar_length = 2000.
+        scint_bar_cover_thickness = 0.5
+        layer_thickness = 45.
+        bar_mounting_plate_thickness = 3.
+        air_thickness = layer_thickness - (absorber_thickness +
+                                           bar_mounting_plate_thickness +
+                                           scint_thickness +
+                                           scint_bar_cover_thickness)
         num_layers_front_vertical = 5
         num_layers_front_horizontal = 4
         num_layers_front=num_layers_front_vertical + num_layers_front_horizontal
