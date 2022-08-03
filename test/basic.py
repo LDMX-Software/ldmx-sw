039c6186f395a4f5556f42ae2cb6e3fc50eaf6ed
tomeichlersmith <eichl008@umn.edu>
2022-08-03 09:58:54 -0500

    try out different SD using Scoring planes

--- test/basic.py
+++ test/basic.py
@@ -11,7 +11,16 @@ p.outputFiles = [ "justSim_" + str(p.maxEvents) + "_events.root" ]
 from LDMX.SimCore import simulator as sim
 from LDMX.Ecal import EcalGeometry
 mySim = sim.simulator( "mySim" )
-mySim.setDetector( 'ldmx-det-v12' )
+mySim.setDetector( 'ldmx-det-v12' , True )
+
+from LDMX.SimCore import simcfg
+class ScoringPlaneSD(simcfg.SensitiveDetector) :
+    def __init__(self) :
+        super().__init__('ecal_sp','simcore::ScoringPlaneSD')
+        self.collection_name = 'EcalDynamicSDHits'
+
+mySim.sensitive_detectors = [ ScoringPlaneSD() ]
+
 # Set a run number
 mySim.runNumber = 9001
 # Get a pre-written generator
