9054273cfa1607088315416dd6d1ff6c4547f509
tomeichlersmith <eichl008@umn.edu>
2022-08-03 09:43:20 -0500

    updates to PluginFactory (mainly using const Parameters&)

--- src/SimCore/PluginFactory.cxx
+++ src/SimCore/PluginFactory.cxx
@@ -5,7 +5,6 @@
 /*   C++ StdLib   */
 /*~~~~~~~~~~~~~~~~*/
 #include <dlfcn.h>
-
 #include <algorithm>
 #include <string>
 #include <vector>
@@ -17,17 +16,17 @@
 
 namespace simcore {
 
-PluginFactory& PluginFactory::getInstance() {
-  // the_factory is created on first call to getInstance
+PluginFactory& PluginFactory::getInstance() { 
+  //the_factory is created on first call to getInstance
   //  and is guaranteed to be destroyed
   static PluginFactory the_factory;
-  return the_factory;
+  return the_factory; 
 }
 
 void PluginFactory::registerGenerator(const std::string& className,
-                                      PrimaryGeneratorBuilder* builder) {
-  auto it{registeredGenerators_.find(className)};
-  if (it != registeredGenerators_.end()) {
+                                      simcore::PrimaryGeneratorBuilder* builder) {
+
+  if (registeredGenerators_.find(className) != registeredGenerators_.end()) {
     EXCEPTION_RAISE(
         "ExistingGeneratorDefinition",
         "The primary generator " + className + " has already been registered.");
@@ -38,38 +37,33 @@ void PluginFactory::registerGenerator(const std::string& className,
 
 void PluginFactory::createGenerator(const std::string& className,
                                     const std::string& instanceName,
-                                    framework::config::Parameters& parameters) {
-  auto it{registeredGenerators_.find(className)};
-  if (it == registeredGenerators_.end()) {
+                                    const framework::config::Parameters& parameters) {
+  if (registeredGenerators_.find(className) == registeredGenerators_.end()) {
     EXCEPTION_RAISE("CreateGenerator",
                     "Failed to create generator '" + className + "'.");
   }
 
-  auto generator{it->second(instanceName, parameters)};
+  auto generator{registeredGenerators_[className](instanceName, parameters)};
 
   // now that the generator is built --> put it on active list
   generators_.push_back(generator);
-
-  ldmx_log(info) << "Primary Generator '" << instanceName << "' of class '"
-                 << className << "' has been created.";
 }
 
 actionMap PluginFactory::getActions() {
   if (actions_.empty()) {
-    actions_[TYPE::RUN] = new UserRunAction();
-    actions_[TYPE::EVENT] = new UserEventAction();
-    actions_[TYPE::TRACKING] = new UserTrackingAction();
-    actions_[TYPE::STEPPING] = new USteppingAction();
-    actions_[TYPE::STACKING] = new UserStackingAction();
+    actions_[simcore::TYPE::RUN] = new simcore::g4user::RunAction();
+    actions_[simcore::TYPE::EVENT] = new simcore::g4user::EventAction();
+    actions_[simcore::TYPE::TRACKING] = new simcore::g4user::TrackingAction();
+    actions_[simcore::TYPE::STEPPING] = new simcore::g4user::SteppingAction();
+    actions_[simcore::TYPE::STACKING] = new simcore::g4user::StackingAction();
   }
 
   return actions_;
 }
 
 void PluginFactory::registerAction(const std::string& className,
-                                   UserActionBuilder* builder) {
-  auto it{registeredActions_.find(className)};
-  if (it != registeredActions_.end()) {
+                                   simcore::UserActionBuilder* builder) {
+  if (registeredActions_.find(className) != registeredActions_.end()) {
     EXCEPTION_RAISE(
         "ExistingActionDefinition",
         "The user action " + className + " has already been registered.");
@@ -81,40 +75,35 @@ void PluginFactory::registerAction(const std::string& className,
 void PluginFactory::createAction(const std::string& className,
                                  const std::string& instanceName,
                                  framework::config::Parameters& parameters) {
-  auto it{registeredActions_.find(className)};
-  if (it == registeredActions_.end()) {
+  if (registeredActions_.find(className) == registeredActions_.end()) {
     EXCEPTION_RAISE("PluginFactory", "Failed to create " + className);
   }
 
-  auto act{it->second(instanceName, parameters)};
+  auto act{registeredActions_[className](instanceName, parameters)};
 
-  std::vector<TYPE> types = act->getTypes();
+  std::vector<simcore::TYPE> types = act->getTypes();
   for (auto& type : types) {
-    if (type == TYPE::RUN)
-      std::get<UserRunAction*>(actions_[TYPE::RUN])->registerAction(act);
-    else if (type == TYPE::EVENT)
-      std::get<UserEventAction*>(actions_[TYPE::EVENT])->registerAction(act);
-    else if (type == TYPE::TRACKING)
-      std::get<UserTrackingAction*>(actions_[TYPE::TRACKING])
+    if (type == simcore::TYPE::RUN)
+      std::get<simcore::g4user::RunAction*>(actions_[simcore::TYPE::RUN])->registerAction(act);
+    else if (type == simcore::TYPE::EVENT)
+      std::get<simcore::g4user::EventAction*>(actions_[simcore::TYPE::EVENT])->registerAction(act);
+    else if (type == simcore::TYPE::TRACKING)
+      std::get<simcore::g4user::TrackingAction*>(actions_[simcore::TYPE::TRACKING])
           ->registerAction(act);
-    else if (type == TYPE::STEPPING)
-      std::get<USteppingAction*>(actions_[TYPE::STEPPING])->registerAction(act);
-    else if (type == TYPE::STACKING)
-      std::get<UserStackingAction*>(actions_[TYPE::STACKING])
+    else if (type == simcore::TYPE::STEPPING)
+      std::get<simcore::g4user::SteppingAction*>(actions_[simcore::TYPE::STEPPING])->registerAction(act);
+    else if (type == simcore::TYPE::STACKING)
+      std::get<simcore::g4user::StackingAction*>(actions_[simcore::TYPE::STACKING])
           ->registerAction(act);
     else
       EXCEPTION_RAISE("PluginFactory", "User action type doesn't exist.");
   }
-
-  // now that the biasing is built --> put it on active list
-  ldmx_log(info) << "User Action '" << instanceName << "' of class '"
-                 << className << "' has been created.";
 }
 
 void PluginFactory::registerBiasingOperator(
     const std::string& className, XsecBiasingOperatorBuilder* builder) {
-  auto it{registeredOperators_.find(className)};
-  if (it != registeredOperators_.end()) {
+
+  if (registeredOperators_.find(className) != registeredOperators_.end()) {
     EXCEPTION_RAISE(
         "ExistingOperatorDefinition",
         "The biasing operator " + className + " has already been registered.");
@@ -123,21 +112,47 @@ void PluginFactory::registerBiasingOperator(
   registeredOperators_[className] = builder;
 }
 
-void PluginFactory::createBiasingOperator(
-    const std::string& className, const std::string& instanceName,
-    framework::config::Parameters& parameters) {
-  auto it{registeredOperators_.find(className)};
-  if (it == registeredOperators_.end()) {
+void PluginFactory::createBiasingOperator(const std::string& className,
+                                          const std::string& instanceName,
+                                          const framework::config::Parameters& parameters) {
+  if (registeredOperators_.find(className) == registeredOperators_.end()) {
     EXCEPTION_RAISE("CreateBiasingOperator",
                     "Failed to create biasing '" + className + "'.");
   }
 
-  auto bop{it->second(instanceName, parameters)};
+  auto bop{registeredOperators_[className](instanceName, parameters)};
 
   // now that the biasing is built --> put it on active list
+  std::cout << "[ PluginFactory ]: Biasing operator '"
+    << instanceName << "' of class '"
+    << className << "' has been created." << std::endl;
   biasing_operators_.push_back(bop);
-  ldmx_log(info) << "Biasing operator '" << instanceName << "' of class '"
-                 << className << "' has been created.";
+}
+
+void PluginFactory::registerSensitiveDetector(
+    const std::string& className, SensitiveDetectorBuilder* builder) {
+
+  if (registeredDetectors_.find(className) != registeredDetectors_.end()) {
+    EXCEPTION_RAISE(
+        "ExistingOperatorDefinition",
+        "The biasing operator " + className + " has already been registered.");
+  }
+
+  registeredDetectors_[className] = builder;
+}
+
+void PluginFactory::createSensitiveDetector(const std::string& className,
+                                            const std::string& instanceName,
+                                            simcore::ConditionsInterface& ci,
+                                            const framework::config::Parameters& parameters) {
+  if (registeredDetectors_.find(className) == registeredDetectors_.end()) {
+    EXCEPTION_RAISE("CreateSensitiveDetector",
+                    "Failed to create detector '" + className + "'.");
+  }
+
+  auto det{registeredDetectors_[className](instanceName, ci, parameters)};
+
+  sensitive_detectors_.push_back(det);
 }
 
 }  // namespace simcore
