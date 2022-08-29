6910c94a11da14309fce9f0cd945a065a6b63524
tomeichlersmith <eichl008@umn.edu>
2022-08-29 15:46:15 -0500

    combine layer weights into layer stack util script

--- Detectors/util/ecal_layer_stack.py
+++ Detectors/util/ecal_layer_stack.py
@@ -1,48 +1,10 @@
 """Calculate the depth parameters for the ECal absorber layers"""
 
-
-# dictionary of properties for material layers
-# Units: MeV/mm
-# Calculations are (dEdx [MeV cm^2/g] * density [g/cm^3]) / 10 [mm/cm]
-dEdx = {
-    'Al'     : (2.699 * 1.615) / 10,
-    'Air'    : (1.815 * 1.205) / 10,
-    'PCB'    : (1.815 * 1.205) / 10,
-    'Si'     : (2.329 * 1.664) / 10,
-    'W'      : (19.3  * 1.145) / 10,
-    'Carbon' : (1.742 * 2.210) / 10,
-    'Glue'   : (1.815 * 1.205) / 10
-}
-
-# Units: mm
-# Calculations are (X0 [g/cm^2] / density [g/cm^3]) * 10 [mm/cm]
-X0 = {
-    'Al'     : ( 24.01 / 2.699 ) * 10,
-    'Air'    : ( 36.62 / 1.205 ) * 10,
-    'PCB'    : ( 36.62 / 1.205 ) * 10,
-    'Si'     : ( 21.82 / 2.329 ) * 10,
-    'W'      : ( 6.76  / 19.3  ) * 10,
-    'Carbon' : ( 42.70 / 2.210 ) * 10,
-    'Glue'   : ( 36.62 / 1.205 ) * 10
-}
-
-# Units: mm
-# Calculations are (NucLen [g/cm^2] / density [g/cm^3]) * 10 [mm/cm]
-nuclen = {
-    'Al'     : ( 107.2 / 2.699 ) * 10,
-    'Air'    : ( 90.1  / 1.205 ) * 10,
-    'PCB'    : ( 90.1  / 1.205 ) * 10,
-    'Si'     : ( 108.4 / 2.329 ) * 10,
-    'W'      : ( 191.9 / 19.3  ) * 10,
-    'Carbon' : ( 85.8  / 2.210 ) * 10,
-    'Glue'   : ( 90.1  / 1.205 ) * 10
-}
-
+import sys
 
 def print_gdml_list(**kwargs) :
     for name, l in kwargs.items() :
-        newline_list = f'{l[0]:.1f}\n'
-                     + '\n'.join([f'                {v:.1f}' for v in l[1:]])
+        newline_list = f'{l[0]:.1f}\n' + '\n'.join([f'                {v:.1f}' for v in l[1:]])
         print(f'<matrix name="{name}"')
         print( '        coldim="1"')
         print(f'        values="{newline_list}"/>')
@@ -59,10 +21,52 @@ def enumerate_absorber_dz(sections) :
     return cooling_tungsten_dz, front_tungsten_dz, bilayer_absorber_cumulative
 
 class Layer :
-    def __init__(name, thickness, sensitive = False) :
+    # dictionary of properties for material layers
+    # Units: MeV/mm
+    # Calculations are (dEdx [MeV cm^2/g] * density [g/cm^3]) / 10 [mm/cm]
+    dEdx = {
+        'Al'     : (2.699 * 1.615) / 10,
+        'Air'    : (1.815 * 1.205) / 10,
+        'PCB'    : (1.815 * 1.205) / 10,
+        'Si'     : (2.329 * 1.664) / 10,
+        'W'      : (19.3  * 1.145) / 10,
+        'Carbon' : (1.742 * 2.210) / 10,
+        'Glue'   : (1.815 * 1.205) / 10
+    }
+    
+    # Units: mm
+    # Calculations are (X0 [g/cm^2] / density [g/cm^3]) * 10 [mm/cm]
+    X0 = {
+        'Al'     : ( 24.01 / 2.699 ) * 10,
+        'Air'    : ( 36.62 / 1.205 ) * 10,
+        'PCB'    : ( 36.62 / 1.205 ) * 10,
+        'Si'     : ( 21.82 / 2.329 ) * 10,
+        'W'      : ( 6.76  / 19.3  ) * 10,
+        'Carbon' : ( 42.70 / 2.210 ) * 10,
+        'Glue'   : ( 36.62 / 1.205 ) * 10
+    }
+    
+    # Units: mm
+    # Calculations are (NucLen [g/cm^2] / density [g/cm^3]) * 10 [mm/cm]
+    nuclen = {
+        'Al'     : ( 107.2 / 2.699 ) * 10,
+        'Air'    : ( 90.1  / 1.205 ) * 10,
+        'PCB'    : ( 90.1  / 1.205 ) * 10,
+        'Si'     : ( 108.4 / 2.329 ) * 10,
+        'W'      : ( 191.9 / 19.3  ) * 10,
+        'Carbon' : ( 85.8  / 2.210 ) * 10,
+        'Glue'   : ( 90.1  / 1.205 ) * 10
+    }
+
+    SensDetThickness = 0.5
+
+    def __init__(self, name, thickness, sensitive = False) :
         self.name = name
         self.thickness = thickness
         self.sensitive = sensitive
+        self.nuclen = Layer.nuclen[self.name]
+        self.x0 = Layer.X0[self.name]
+        self.dEdx = Layer.dEdx[self.name]
 
     def air(t) :
         return Layer('Air',t)
@@ -77,7 +81,7 @@ class Layer :
         return Layer('Glue', t)
 
     def silicon() :
-        return Layer('Si',0.5,sensitive=True)
+        return Layer('Si',Layer.SensDetThickness,sensitive=True)
 
     def carbon() :
         return Layer('Carbon',5.7)
@@ -89,7 +93,7 @@ class Layer :
                 layers.append(Layer.air(0.5))
                 if front > 0 :
                     layers.append(Layer.tungsten(front))
-                    layers.append(Layer.air(0.5)
+                    layers.append(Layer.air(0.5))
                 layers.append(Layer.pcb())
                 if cooling == 0 :
                     layers.append(Layer.air(0.5))
@@ -112,6 +116,71 @@ class Layer :
     
         return layers
 
+def average(raw_weights) :
+    averaged = []
+    # first to (n-1)th layers where n is the total number of layers
+    for i_layer in range(0,len(raw_weights)-1) :
+        averaged.append(0.5*(raw_weights[i_layer] + raw_weights[i_layer+1]))
+    averaged.append(raw_weights[-1])
+    return averaged
+
+def normlize(weights) :
+    norm = len(weights)/sum(weights)
+    return [ norm*w for w in weights ]
+
+def materials_between_sensdet(layer_stack) :
+    mbs = []
+    current = []
+    for layer in layer_stack :
+        if layer.sensitive :
+            mbs.append(current)
+            current = []
+        else :
+            current.append(layer)
+    if len(current) > 0 :
+        mbs.append(current)
+    return mbs
+
+def calc_weights(layers_partitioned_by_sensdet) :
+    # Does not include sensitive detector layers
+    dE_between_sensdet = [ ]
+    X0_between_sensdet = [ ]
+    L_between_sensdet  = [ ]
+    # Does include sensitive detector layers
+    Zpos_layer = [ ]
+    for section in layers_partitioned_by_sensdet :
+        [dE_section, X0_section, L_section, Zdepth_section] = list(map(sum,
+            zip(*[(l.thickness*l.dEdx, l.thickness / l.x0, l.thickness / l.nuclen, l.thickness)
+                    for l in section ])))
+        dE_between_sensdet.append(dE_section)
+        X0_between_sensdet.append(X0_section)
+        L_between_sensdet.append(L_section)
+    
+        last_layer_pos = 0.0
+        if len(Zpos_layer) > 0:
+            last_layer_pos = Zpos_layer[-1] + Layer.SensDetThickness
+        Zpos_layer.append( last_layer_pos + Zdepth_section )
+    #endfor - sections
+
+    dE_between_sensdet = average(dE_between_sensdet)
+    X0_between_sensdet = average(X0_between_sensdet)
+    L_between_sensdet  = average(L_between_sensdet)
+
+    return dE_between_sensdet, X0_between_sensdet, L_between_sensdet, Zpos_layer
+
+def print_weights(dE_between_sensdet, X0_between_sensdet, L_between_sensdet, Zpos_layer, 
+            output = sys.stdout) :
+    output.write('{0:>5s} {1:>7s} {2:>6s} {3:>6s} {4:>6s}\n'.format('Layer', 'dE', 'X0', 'Lambda', 'Zpos'))
+    for layer in range(len(dE_between_sensdet)-1):
+        output.write('{0:5d} {1:7.3f} {2:6.3f} {3:6.3f} {4:6.3f}\n'.format(
+            layer+1, dE_between_sensdet[layer], X0_between_sensdet[layer], L_between_sensdet[layer], Zpos_layer[layer]))
+    #endfor - layers
+    output.write('{0:>5s} {1:7.3f} {2:6.3f} {3:6.3f} {4:6.3f}\n'.format(
+        'Sum', sum(dE_between_sensdet[:-1]), sum(X0_between_sensdet[:-1]), sum(L_between_sensdet[:-1]), Zpos_layer[-1] ))
+    output.write('{0:>5s} {1:7.3f} {2:6.3f} {3:6.3f} {4:6.3f}\n'.format(
+        'Back', dE_between_sensdet[-1], X0_between_sensdet[-1], L_between_sensdet[-1], Zpos_layer[-1]))
+    output.flush()
+
 def main() :
     # Section, bilayers, front, cooling
     absorber_sections = [
@@ -126,5 +195,10 @@ def main() :
                     front_tungsten_dz = ft,
                     bilayer_absorber_cumulative = bac)
 
-    layers = Layer.enumerate_full_stack(('ps',1,0,0)+absorber_sections)
+    layers = Layer.enumerate_full_stack([('ps',1,0,0)]+absorber_sections)
+    mbs = materials_between_sensdet(layers)
+    weights = calc_weights(mbs)
+    print_weights(*weights)
 
+if __name__ == '__main__' :
+    main()
