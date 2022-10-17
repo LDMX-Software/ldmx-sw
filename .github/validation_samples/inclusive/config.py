381d62154b99f9a36c165f50bedcec3547261be5
tomeichlersmith <eichl008@umn.edu>
2022-10-17 08:58:52 -0500

    update validation samples to v14 detector geometry

--- .github/validation_samples/inclusive/config.py
+++ .github/validation_samples/inclusive/config.py
@@ -3,7 +3,7 @@ p = ldmxcfg.Process('test')
 
 from LDMX.SimCore import simulator as sim
 mySim = sim.simulator( "mySim" )
-mySim.setDetector( 'ldmx-det-v12' )
+mySim.setDetector( 'ldmx-det-v14' )
 from LDMX.SimCore import generators as gen
 mySim.generators.append( gen.single_4gev_e_upstream_tagger() )
 mySim.beamSpotSmear = [20.,80.,0.]
