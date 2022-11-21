66d9186d61ac790d49c4ef76451b33def8ac022a
tomeichlersmith <eichl008@umn.edu>
2022-11-21 09:54:32 -0600

    more documentation

--- Validation/src/Validation/_differ.py
+++ Validation/src/Validation/_differ.py
@@ -24,25 +24,23 @@ class Differ :
     grp_name : str
         Name to include in legend title to differentiate
         this group of plots from another
-    args : list of 2-tuples
-        Each entry is a 2-tuple (file_path, name) where file_path
+    args : list of tuples or Files
+        Each entry is a tuple (file_path, name, *args) where file_path
         specifies the file to open and name is what should appear
-        in plot legends
-    kwargs : dict
-        These keyword arguments are passed to plt.figure for creationg
-        of the figure we draw on.
+        in plot legends.
+        Alternatively, each entry can just be the already constructed File
 
     Example
     -------
     Opening a differ is pretty quick and lightweight.
-    We do open the files with `uproot` and access the event tree.
+    We do open the files with `uproot`.
 
         d = Differ('v3.2.0-alpha',('path/to/v12.root','v12'),('path/to/v14.root','v14'))
 
     Without any other options, the plotting with show the plot as if
     we are in an interactive notebook.
 
-        d.plot1d('EcalSimHits_valid/EcalSimHits_valid.edep_')
+        d.plot1d('EcalSimHits_valid/EcalSimHits_valid.edep_', 'Sim E Dep [MeV]')
     """
 
     def __init__(self, grp_name, *args) :
@@ -52,7 +50,7 @@ class Differ :
             elif isinstance(arg, File) :
                 return arg
             else :
-                raise KeyError(f'Argument provided {arg} is not a differ.File or a tuple of arguments for its constructor')
+                raise KeyError(f'Argument provided {arg} is not a Validation.File or a tuple of arguments for its constructor')
                 
         self.grp_name = grp_name
         self.files = list(map(open_file, args))
@@ -63,6 +61,37 @@ class Differ :
               ylim = (None,None),
               out_dir = None, file_name = None,
               **hist_kwargs) :
+        """Plot a 1D histogram, overlaying the File entries
+
+        We overlay the same 'column' of data of each File onto
+        the same figure, generating a legend with the title defined by
+        grp_name from the constructor.
+
+        If out_dir is not provided, we assume we are in a notebook and
+        simply `plt.show()` the figure. If out_dir is not None (i.e. it
+        was defined), we assume we are in a non-interactive script and
+        write the figure to a PDF in the output file and then clear
+        the figure.
+
+        Parameters
+        ----------
+        column : str or Callable
+            Determines the array of data from each File to histogram and plot
+        xlabel : str
+            Label of X axis
+        ylabel : str, optional
+            Label for Y axis (default: Count)
+        yscale : {'linear', 'log', 'symlog', 'logit', ...}, optional
+            Scale to use for y-axis (default: 'log')
+        ylim : 2-tuple
+            Limits to set for the y-axis (default: deduced by matplotlib)
+        out_dir : str
+            Directory in which to write the plotting file
+        file_name : str
+            Name of file, no extension (default: column name with directory separators removed)
+        hist_kwargs : dict
+            All other key-word arguments are passed into each File.plot1d
+        """
         fig = plt.figure('differ',figsize=(11,8))
         ax = fig.subplots()
 
@@ -87,7 +116,7 @@ class Differ :
     def load(self, **kwargs) :
         """Load all of the event data frames into memory
         
-        The key-word arguments are used in each FileEntry's events call
+        The key-word arguments are used in each File's events call
         to specify which branches (if not all of them) should be loaded
         into memory and what manipulation (if any) to do.
         """
@@ -95,5 +124,12 @@ class Differ :
             f.load(**kwargs)
             
     def manipulate(self, manipulation) :
+        """Manipulate all of the File data frames
+
+        Parameters
+        ----------
+        manipulation : Callable (e.g. a function)
+            Function operating on the data frame to manipuate it **in place**
+        """
         for f in self.files :
             f.manipulate(manipulation)
