ba5f1b46bf62700881852740b5237c15d5b5b621
tomeichlersmith <eichl008@umn.edu>
2023-01-09 12:57:58 -0600

    patch dark brem interaction ntuplizer
    it is difficult (especially in the ECal case) to make sure the parent of
    the dark brem is saved, so we instead reconstruct the parent kinematics
    using knowledge of how the process is defined

--- DQM/src/DQM/NtuplizeDarkBremInteraction.cxx
+++ DQM/src/DQM/NtuplizeDarkBremInteraction.cxx
@@ -42,9 +42,26 @@ void NtuplizeDarkBremInteraction::onProcessStart() {
   ntuple_.addVar<double>("dbint","beam_energy");
 }
 
+static double energy(const std::vector<double>& p, const double& m) {
+  return sqrt(p.at(0)*p.at(0)+ p.at(1)*p.at(1)+ p.at(2)*p.at(2)+ m*m);
+}
+
+/**
+ * extract the kinematics of the dark brem interaction from the SimParticles
+ *
+ * Sometimes the electron that undergoes the dark brem is not in a region 
+ * where it should be saved (i.e. it is a shower electron inside of the ECal).
+ * In this case, we need to reconstruct the incident momentum from the outgoing
+ * products (the recoil electron and the dark photon) which should be saved by
+ * the biasing filter used during the simulation.
+ *
+ * Since the dark brem model does not include a nucleus, it only is able to 
+ * conserve momentum, so we need to reconstruct the incident particle's 3-momentum
+ * and then use the electron mass to calculate its total energy.
+ */
 void NtuplizeDarkBremInteraction::analyze(const framework::Event& e) {
   const auto& particle_map{e.getMap<int,ldmx::SimParticle>("SimParticles")};
-  const ldmx::SimParticle *recoil{nullptr}, *aprime{nullptr}, *beam{nullptr}, *incident{nullptr};
+  const ldmx::SimParticle *recoil{nullptr}, *aprime{nullptr}, *beam{nullptr};
   for (const auto& [track_id, particle] : particle_map) {
     if (track_id == 1) beam = &particle;
     if (particle.getProcessType() == ldmx::SimParticle::ProcessType::eDarkBrem) {
@@ -55,24 +72,49 @@ void NtuplizeDarkBremInteraction::analyze(const framework::Event& e) {
         aprime = &particle;
       } else {
         recoil = &particle;
-        auto parent_id = particle.getParents().at(0);
-        if (particle_map.find(parent_id) != particle_map.end()) {
-          incident = &(particle_map.at(parent_id));
-        }
       }
     }
   }
 
-  if (recoil == nullptr or aprime == nullptr or beam == nullptr or incident == nullptr) {
-    EXCEPTION_RAISE("BadEvent","Unable to find all necessary particles.");
+  if (recoil == nullptr and aprime == nullptr) {
+    /* dark brem did not occur during the simulation
+     *    IF PROPERLY CONFIGURED, this occurs because the simulation
+     *    exhausted the maximum number of tries to get a dark brem
+     *    to occur. We just leave early so that the entries in the
+     *    ntuple are the unphysical numeric minimum.
+     *
+     * This can also happen during development, so I leave a debug
+     * printout here to be uncommented when developing the dark
+     * brem simulation.
+    std::cout << "Event " << e.getEventNumber()
+      << " did not have a dark brem occur within it." << std::endl;
+     */
+    return;
   }
 
-  static auto energy = [](const std::vector<double>& p, const double& m) {
-    return sqrt(p.at(0)*p.at(0)+ p.at(1)*p.at(1)+ p.at(2)*p.at(2)+ m*m);
-  };
+  if (recoil == nullptr or aprime == nullptr or beam == nullptr) {
+    // we are going to end processing so let's take our time to
+    // construct a nice error message
+    std::stringstream err_msg;
+    err_msg 
+      << "Unable to final all necessary particles for DarkBrem interaction."
+      << " Missing: [ "
+      << (recoil == nullptr ? "recoil " : "")
+      << (aprime == nullptr ? "aprime " : "")
+      << (beam == nullptr ? "beam " : "")
+      << "]" << std::endl;
+    EXCEPTION_RAISE("BadEvent", err_msg.str());
+    return;
+  }
 
-  double incident_energy = energy(incident->getEndPointMomentum(), incident->getMass());
-  double recoil_energy = energy(recoil->getMomentum(), recoil->getMass());
+  const auto& recoil_p = recoil->getMomentum();
+  const auto& aprime_p = aprime->getMomentum();
+
+  std::vector<double> incident_p = recoil_p;
+  for (std::size_t i{0}; i < recoil_p.size(); ++i) incident_p[i] += aprime_p.at(i);
+
+  double incident_energy = energy(incident_p, recoil->getMass());
+  double recoil_energy = energy(recoil_p, recoil->getMass());
   double visible_energy = (beam->getEnergy() - incident_energy) + recoil_energy;
 
   ntuple_.setVar<double>("x", aprime->getVertex().at(0));
@@ -83,23 +125,23 @@ void NtuplizeDarkBremInteraction::analyze(const framework::Event& e) {
   ntuple_.setVar<int>("incident_genstatus", -1);
   ntuple_.setVar<double>("incident_mass", recoil->getMass());
   ntuple_.setVar<double>("incident_energy", incident_energy);
-  ntuple_.setVar<double>("incident_px", incident->getEndPointMomentum().at(0));
-  ntuple_.setVar<double>("incident_py", incident->getEndPointMomentum().at(1));
-  ntuple_.setVar<double>("incident_pz", incident->getEndPointMomentum().at(2));
+  ntuple_.setVar<double>("incident_px", incident_p.at(0));
+  ntuple_.setVar<double>("incident_py", incident_p.at(1));
+  ntuple_.setVar<double>("incident_pz", incident_p.at(2));
   ntuple_.setVar<int>("recoil_pdg", recoil->getPdgID());
   ntuple_.setVar<int>("recoil_genstatus", recoil->getGenStatus());
   ntuple_.setVar<double>("recoil_mass", recoil->getMass());
   ntuple_.setVar<double>("recoil_energy", recoil_energy);
-  ntuple_.setVar<double>("recoil_px", recoil->getMomentum().at(0));
-  ntuple_.setVar<double>("recoil_py", recoil->getMomentum().at(1));
-  ntuple_.setVar<double>("recoil_pz", recoil->getMomentum().at(2));
+  ntuple_.setVar<double>("recoil_px", recoil_p.at(0));
+  ntuple_.setVar<double>("recoil_py", recoil_p.at(1));
+  ntuple_.setVar<double>("recoil_pz", recoil_p.at(2));
   ntuple_.setVar<int>("aprime_pdg", aprime->getPdgID());
   ntuple_.setVar<int>("aprime_genstatus", aprime->getGenStatus());
   ntuple_.setVar<double>("aprime_mass", aprime->getMass());
-  ntuple_.setVar<double>("aprime_energy", energy(aprime->getMomentum(),aprime->getMass()));
-  ntuple_.setVar<double>("aprime_px", aprime->getMomentum().at(0));
-  ntuple_.setVar<double>("aprime_py", aprime->getMomentum().at(1));
-  ntuple_.setVar<double>("aprime_pz", aprime->getMomentum().at(2));
+  ntuple_.setVar<double>("aprime_energy", energy(aprime_p,aprime->getMass()));
+  ntuple_.setVar<double>("aprime_px", aprime_p.at(0));
+  ntuple_.setVar<double>("aprime_py", aprime_p.at(1));
+  ntuple_.setVar<double>("aprime_pz", aprime_p.at(2));
   ntuple_.setVar<double>("beam_energy", beam->getEnergy());
   ntuple_.setVar<double>("visible_energy", visible_energy);
 }
