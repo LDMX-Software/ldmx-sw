2207e746d3cc392b0f2c63048ed4b09e8adbdd4c
Omar Moreno <email@omarmoreno.net>
2023-01-09 23:16:59 -0800

    Add defaults to CKFProcessor.

--- python/tracking.py
+++ python/tracking.py
@@ -1,4 +1,5 @@
 from LDMX.Framework.ldmxcfg import Producer
+from LDMX.Tracking.make_path import makeFieldMapPath
 
 
 class DigitizationProcessor(Producer):
@@ -93,73 +94,109 @@ class CKFProcessor(Producer):
     instance_name : str
         Unique name for this instance.
 
-    Arguments
+    Attributes
     ---------
 
     dumpobj : bool
-        Activate dump of the tracking geometry into obj/mtl files for visualization purposes.
-        The files can be opened via an open source application as mesh lab <functionality to be moved>
+        <functionality to be moved>
+        If true, dump the tracking geometry into obj/mtl files for
+        visualization purposes. The files can be opened via an open source
+        application such as mesh lab.
     pionstates : int
-        Can be used to define the number of pion states generated with uniform distributions to be
-        propagated through the tracking geometry for debugging purposes. <functionality to be moved>
+        Can be used to define the number of pion states generated with uniform
+        distributions to be propagated through the tracking geometry for
+        debugging purposes. <functionality to be moved>
     steps_output_file_path_ : string
         DEPRECATED TO BE REMOVED
-    trackID : int
-        Only keep the simulated hits with that particular track ID <functionality to be removed>
-    pdgID : int
-        Only keep the simulated hits with that particular pdg ID <functionality to be removed>
+    track_id : int
+        <functionality to be removed>
+        Only keep the simulated hits with that particular track ID.
+    pdg_id : int
+        <functionality to be removed>
+        Only keep the simulated hits with that particular pdg ID.
     bfield : float
-        If using a constant bfield, this is the BZ component <functionality to be removed>
+        <functionality to be removed>
+        If using a constant bfield, this is the BZ component. 
     const_b_field : bool
-        Activate the usage of constant bField <functionality to be removed>
-    bfieldMap_ : string
-        Path to the location of the magnetic field map
+        <functionality to be removed>
+        Activate the usage of constant magnetic field.
+    field_map_ : string
+        Path to the location of the magnetic field map.
     propagator_step_size : float
-        Size of each RK propagator step
+        Size of each RK propagator step.
     propagator_maxSteps : int
         Maximum number of steps for the propagator
     perigee_location : list[double]
         DEPRECATED
-    debug : bool
-        Activate the debug printout
     hit_collection : string
-        The hit collection for pattern reconstruction
-    removeStereo : bool
-        Remove stereo hits from track fitting
+        The hit collection for pattern reconstruction.
+    remove_stereo : bool
+        Remove stereo hits from track fitting.
     use1Dmeasurements : bool
-        Use single strip measurements and not 3D points. <remove functionality and leave it to experts only>
-    minHits : int
+        <remove functionality and leave it to experts only>
+        Use single strip measurements and not 3D points.
+    min_hits : int
         Minimum number of measurements on track to accept the trajectory.
     use_extrapolate_location : bool
-        Activate the usage of extrapolate location for returning the track parameters
+        Activate the usage of extrapolate location for returning the track 
+        parameters.
     extrapolate_location : list[double]
-        Location of the extrapolation for the trajectory (perigee representation)
+        Location of the extrapolation for the trajectory (perigee 
+        representation).
     use_seed_perigee : bool
-        Uses the seed perigee as extrapolation location
+        Uses the seed perigee as extrapolation location.
     seed_coll_name : string
-        Seed collection for initiate the track finding
+        Seed collection for initiate the track finding.
     out_trk_collection : string
-        Name of the output Track collection
+        Name of the output Track collection.
     do_smearing : bool
-       Activate the hit smearing <functionality to be removed>
+       <functionality to be removed>
+       Activate the hit smearing.
     sigma_u : float
-       Smearing in the sensitive direction <functionality to be removed>
+       <functionality to be removed>
+       Smearing in the sensitive direction.
     sigma_v : float
-       Smearing in the unsensitive direction < functionality to be removed>
-
-    kfRefit : bool
+       < functionality to be removed>
+       Smearing in the unsensitive direction.
+    kf_refit : bool
        Activate kalman filter track refitting of the found trajectories
-    gsfRefit : bool
-       Refit tracks with Gaussian Sum Filter <experimental>
+    gsf_refit : bool
+       <experimental>
+       Refit tracks with Gaussian Sum Filter 
         
     """
     def __init__(self, instance_name = 'CKFProcessor'): 
         super().__init__(instance_name, 'tracking::sim::CKFProcessor', 'Tracking')
-        
+
+        self.dumpobj = False
+        self.pionstates = 0
+        self.track_id = -1
+        self.pdg_id = 11
+        self.bfield = 0.
+        self.const_b_field = True
+        self.field_map = makeFieldMapPath()
+        self.propagator_step_size = 200.
+        self.propagator_maxSteps = 10000
+        self.hit_collection = 'RecoilSimHits'
+        self.remove_stereo = False
+        self.use_extrapolate_location = True
+        self.extrapolate_location = [0., 0., 0.]
+        self.use_seed_perigee = False
+        self.seed_coll_name = 'SeedTracks'
+        self.out_trk_collection = 'Tracks'
+        self.do_smearing = False
+        self.sigma_u = 0.01
+        self.sigma_v = 0.
+        self.kf_refit = False
+        self.gsf_refit = False
+        self.min_hits = 6
+
 class TruthSeedProcessor(Producer) :
     """ Producer that returns truth seeds to feed the KF based track finding.
-    Seeds are not smeared, so the fits will be too optimistic, especially the residuals of the
-    estimated locations w.r.t. simulated hits on each surface.
+    Seeds are not smeared, so the fits will be too optimistic, especially the
+    residuals of the estimated locations w.r.t. simulated hits on each surface.
+    The default parameters assume electron seeds are being found in the recoil 
+    tracker with loose requirements on momentum and z position. 
 
     Parameters
     ----------
@@ -169,50 +206,44 @@ class TruthSeedProcessor(Producer) :
     Attributes
     ----------
 
-    debug : bool
-        Activate debug printouts
     out_trk_coll_name : string
-        Name of the output seed collection
-    pdgIDs : list[int]
-        Only use the scoring plane hits with this particle ID to form initial seeds
+        Name of the output seed collection.
+    pdg_ids : list[int]
+        List of particle IDs whose scoring plane hits will be used to form 
+        initial seeds.
     scoring_hits : string
-        The name of the scoring plane hits from where to get the truth parameters
-    sim_hit : string
-        The name of the recoil sim hits collection 
+        The name of the scoring plane hits from where to get the truth 
+        parameters.
+    sim_hits : string
+        The name of the sim tracker hits collection.
     n_min_hits : int
-        Request a seed to have at least
+        The minimum number of hits to create a seed from.
     z_min : double
-        Request a minimum z for the scoring plan hits
+        Request a minimum z (mm) for the scoring plane hits.
     track_id : int
-        If positive, select only scoring hits with that particular track id
+        If positive, select only scoring hits with that particular track ID.
     pz_cut : double
-        Minimum cut on the momentum of the seed along the beam axis
+        Minimum cut on the momentum (MeV) of the seed along the beam axis.
     p_cut : double
-        Minimum cut on the momentum of the seed
+        Minimum cut on the momentum (MeV) of the seed.
     p_cut_Max : double
-        Maximum cut on the momentum of the seed
-    k0_sel : bool
-        Deprecated and to be removed
-    p_cutEcal : double
-        Minimum seed track momentum at the ECAL scoring plane
+        Maximum cut on the momentum of the seed.
+    p_cut_ecal : double
+        Minimum seed track momentum (MeV) at the ECAL scoring plane
        
     
     """
     def __init__(self, instance_name = "TruthSeedProcessor"):
         super().__init__(instance_name, 'tracking::reco::TruthSeedProcessor','Tracking')
 
-        self.debug = false;
-        self.out_trk_coll_name = "TruthSeeds"
+        self.out_trk_coll_name = 'RecoilTruthSeeds'
         self.pdgIDs = [11]
-        self.scoring_hits = "TargetScoringPlaneHits_sim"
-        self.sim_hits = "RecoilSimHits"
+        self.scoring_hits = 'TargetScoringPlaneHits'
+        self.sim_hits = 'RecoilSimHits'
         self.n_min_hits = 7
-        self.track_id = -999
-        self.pz_cut = 0.
-        self.p_cut = 0.
-        self.p_cutMax = 8.
-        self.k0_sel = False
-        self.p_cutEcal = -1
-
-
-
+        self.z_min = -9999. # mm
+        self.track_id = -9999
+        self.pz_cut = -9999. # MeV
+        self.p_cut = 0. # MeV
+        self.p_cut_max = 100000. # MeV
+        self.p_cut_ecal = -1 # MeV
