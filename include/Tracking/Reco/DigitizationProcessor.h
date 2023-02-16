73128cf0a5408035217019a247d4558a99cd210f
Omar Moreno <email@omarmoreno.net>
2023-02-16 14:12:46 -0800

    Replace hard coded detector path with a parameter.

--- include/Tracking/Reco/DigitizationProcessor.h
+++ include/Tracking/Reco/DigitizationProcessor.h
@@ -1,18 +1,16 @@
-#ifndef TRACKING_RECO_DIGITIZATIONPROCESSOR_H_
-#define TRACKING_RECO_DIGITIZATIONPROCESSOR_H_
-
+#pragma once
 
 //--- Framework ---//
 #include "Framework/Configure/Parameters.h"
 #include "Framework/EventProcessor.h"
 
 //--- ACTS ---//
+#include "Acts/Definitions/Units.hpp"
 #include "Acts/Digitization/CartesianSegmentation.hpp"
 #include "Acts/Digitization/DigitizationModule.hpp"
 #include "Acts/Digitization/PlanarModuleStepper.hpp"
 #include "Acts/Surfaces/RectangleBounds.hpp"
 #include "Acts/Surfaces/Surface.hpp"
-#include "Acts/Definitions/Units.hpp"
 
 //--- LDMX ---//
 #include "Tracking/Reco/TrackersTrackingGeometry.h"
@@ -26,61 +24,58 @@
 
 #include "Tracking/Reco/TrackersTrackingGeometry.h"
 
-
-namespace tracking {
-namespace reco {
+namespace tracking::reco {
 
 class DigitizationProcessor : public framework::Producer {
  public:
-  DigitizationProcessor(const std::string &name, framework::Process &process);
+  DigitizationProcessor(const std::string& name, framework::Process& process);
   ~DigitizationProcessor();
 
   void onProcessStart() final override;
   void onProcessEnd() final override;
 
-  void configure(framework::config::Parameters &parameters) final override ;
-      
-  void produce(framework::Event &event);
+  void configure(framework::config::Parameters& parameters) final override;
+
+  void produce(framework::Event& event);
 
   /// The detector
   Acts::GeometryContext gctx_;
 
-  void digitizeHits(const std::vector<ldmx::SimTrackerHit> &sim_hits, std::vector<ldmx::LdmxSpacePoint*>& ldmxsps);
+  void digitizeHits(const std::vector<ldmx::SimTrackerHit>& sim_hits,
+                    std::vector<ldmx::LdmxSpacePoint*>& ldmxsps);
 
-  //TODO avoid copies and use references
-  bool mergeSimHits(const std::vector<ldmx::SimTrackerHit>& sim_hits, std::vector<ldmx::SimTrackerHit>& merged_hits);
-  bool mergeHits(const std::vector<ldmx::SimTrackerHit>& sihits, std::vector<ldmx::SimTrackerHit>& mergedHits);
-  
+  // TODO avoid copies and use references
+  bool mergeSimHits(const std::vector<ldmx::SimTrackerHit>& sim_hits,
+                    std::vector<ldmx::SimTrackerHit>& merged_hits);
+  bool mergeHits(const std::vector<ldmx::SimTrackerHit>& sihits,
+                 std::vector<ldmx::SimTrackerHit>& mergedHits);
+
  private:
-
-  
   std::shared_ptr<tracking::reco::TrackersTrackingGeometry> ldmx_tg;
 
+  /// The path to the GDML description of the detector
+  std::string detector_{""};
   /// Input hit collection to smear.
   std::string hit_collection_;
   /// Output hit collection name.
   std::string out_collection_;
   /// Minimum energy deposition cut.
-  double min_e_dep_; 
+  double min_e_dep_;
   /// Select a particular track ID
-  int track_id_;   
+  int track_id_;
   /// Merge the sim hits before digitizing.
   bool merge_hits_{false};
-  /// Flag to enable/disable smearing. 
+  /// Flag to enable/disable smearing.
   bool do_smearing_{true};
   /// u-direction sigma
   double sigma_u_{0};
   /// v-direction sigma
   double sigma_v_{0};
 
-
   //--- Smearing ---//
 
   std::default_random_engine generator_;
   std::shared_ptr<std::normal_distribution<float>> normal_;
-  
-}; //Digitization Processor
-}//reco
-}//tracking
 
-#endif
+};  // Digitization Processor
+}  // namespace tracking::reco
