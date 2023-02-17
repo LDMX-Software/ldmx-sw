7b6c0f58fe7019d8e2c74ecfa61d5e450180979d
Omar Moreno <email@omarmoreno.net>
2023-02-17 14:27:43 -0800

    Tracker digi DQM.

---
+++ include/Tracking/dqm/TrackerDigiDQM.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Framework/Event.h"
+#include "Framework/EventProcessor.h"
+
+namespace tracking::dqm {
+
+class TrackerDigiDQM : public framework::Analyzer {
+ public:
+  /**
+   */
+  TrackerDigiDQM(const std::string& name, framework::Process& process)
+      : framework::Analyzer(name, process){};
+
+  /// Destructor
+  ~TrackerDigiDQM() = default;
+
+  /**
+   *
+   */
+  void analyze(const framework::Event& event) final override;
+};
+}  // namespace tracking::dqm
