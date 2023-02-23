b3723b03cfb6a9f4d20d5588ddfb53fab7bdd64f
Einar Elén <einar.elen@gmail.com>
2023-02-23 17:04:20 +0100

    Add class skeleton for PN model

--- include/SimCore/PhotonuclearModel.h
+++ include/SimCore/PhotonuclearModel.h
@@ -1,3 +1,19 @@
 #ifndef SIMCORE_PHOTONUCLEAR_MODEL_H
 #define SIMCORE_PHOTONUCLEAR_MODEL_H
+#include <G4ProcessManager.hh>
+#include <string>
+#include <utility>
+
+#include "Framework/Configure/Parameters.h"
+#include "SimCore/Factory.h"
+namespace simcore {
+class PhotonuclearModel {
+ public:
+};
+}  // namespace simcore
+
+#define DECLARE_PHOTONUCLEAR_MODEL(CLASS)                                 \
+  namespace {                                                             \
+  auto v = ::simcore::PhotonuclearModel::Factory::get().declare<CLASS>(); \
+  }
 #endif /* SIMCORE_PHOTONUCLEAR_MODEL_H */
