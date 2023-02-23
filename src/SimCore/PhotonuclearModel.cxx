166ba19a0e19864818502b67f55923066aa2153b
Einar Elén <einar.elen@gmail.com>
2023-02-23 17:08:11 +0100

    Add a default function to remove existing models

--- src/SimCore/PhotonuclearModel.cxx
+++ src/SimCore/PhotonuclearModel.cxx
@@ -1,1 +1,14 @@
 #include "SimCore/PhotonuclearModel.h"
+
+namespace simcore {
+void PhotonuclearModel::removeExistingModel(G4ProcessManager* processManager) {
+  const auto processes{processManager->GetProcessList()};
+  for (int i{0}; i < processes->size(); ++i) {
+    const auto process{(*processes)[i]};
+    const auto processName{process->GetProcessName()};
+    if (processName == "photonNuclear") {
+      processManager->RemoveProcess(i);
+    }
+  }
+}
+}  // namespace simcore
