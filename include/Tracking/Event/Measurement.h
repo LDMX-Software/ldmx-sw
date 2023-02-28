6f791275af41731ffff3c18d52572ced0cd2340c
Omar Moreno <email@omarmoreno.net>
2023-02-28 12:21:08 -0800

    Add constructor that takes a SimTrackerHit. Add variables to persist the local convariance, energy deposition and time.

--- include/Tracking/Event/Measurement.h
+++ include/Tracking/Event/Measurement.h
@@ -2,52 +2,77 @@
 #define TRACKING_EVENT_MEASUREMENT_H_
 
 #include <array>
+
+#include "SimCore/Event/SimTrackerHit.h"
 #include "TObject.h"
 
 namespace ldmx {
 
 class Measurement {
-  
  public:
-  
-  Measurement(){};
-  
-  virtual ~Measurement(){};
-  
-  void setGlobalPosition(float x, float y, float z) {x_ = x; y_ = y; z_ = z;};
-  std::array<float,3>  getGlobalPosition() const {return std::array<float,3>{{x_,y_,z_}};};
+  /// Default constructor
+  Measurement() = default;
 
-  void setLocalPosition(float u, float v) {u_ = u; v_ = v;};
-  std::array<float,2>  getLocalPosition() const {return std::array<float,2>{{u_, v_}};};
-  
-  void setTime(float t) {t_ = t; };
-  float getTime() const {return t_;};
+  /**
+   */
+  Measurement(const ldmx::SimTrackerHit& hit, const float& v_r = 0.05,
+              const float& v_z = 1.0);
 
-  void setLayer(int layerid) {layerid_ = layerid;};
-  int getLayer() const{return layerid_;};
+  /// Default destructor
+  ~Measurement() = default;
+
+  void setGlobalPosition(float x, float y, float z) {
+    x_ = x;
+    y_ = y;
+    z_ = z;
+  };
+  std::array<float, 3> getGlobalPosition() const {
+    return std::array<float, 3>{{x_, y_, z_}};
+  };
+
+  void setLocalPosition(float u, float v) {
+    u_ = u;
+    v_ = v;
+  };
+  std::array<float, 2> getLocalPosition() const {
+    return std::array<float, 2>{{u_, v_}};
+  };
+
+  void setLocalCovariance(const float& v_r, const float& v_z) {
+    var_r_ = v_r;
+    var_z_ = v_z;
+  }
+
+  void setTime(float t) { t_ = t; };
+  float getTime() const { return t_; };
+
+  void setLayer(int layerid) { layerid_ = layerid; };
+  int getLayer() const { return layerid_; };
 
   int getLyID() const {
     int lyID = (getLayer() / 100) % 10;
-    int sID  = getLayer() % 2;
+    int sID = getLayer() % 2;
     return (lyID - 1) * 2 + sID;
   }
 
-  static bool compareXLocation(ldmx::Measurement& m1,
-                               ldmx::Measurement& m2) {
+  static bool compareXLocation(ldmx::Measurement& m1, ldmx::Measurement& m2) {
     return m1.getGlobalPosition()[0] < m2.getGlobalPosition()[0];
   }
-  
- private:
 
+ private:
   float x_, y_, z_, t_;
   float u_, v_;
   int layerid_;
-    
-  ClassDef(Measurement,1);
-}; //Measurement
+  float edep_;
+  float var_r_{0};
+  float var_z_{0};
+  int id_;
+
+  ClassDef(Measurement, 1);
+};  // Measurement
 
 typedef std::vector<std::reference_wrapper<const Measurement>> Measurements;
 
-}//namespace ldmx
+}  // namespace ldmx
 
-#endif //TRACKING_EVENT_MEASUREMENT_H_
+#endif  // TRACKING_EVENT_MEASUREMENT_H_
--- src/Tracking/Event/Measurement.cxx
+++ src/Tracking/Event/Measurement.cxx
@@ -1,5 +1,26 @@
 #include "Tracking/Event/Measurement.h"
 
+#include "Tracking/Sim/TrackingUtils.h"
+
 ClassImp(ldmx::Measurement)
 
-    namespace ldmx {}
+namespace ldmx {
+Measurement::Measurement(const ldmx::SimTrackerHit& hit,
+                         const float& sigma_u,
+                         const float& sigma_v) {
+  // Get the layer ID.
+  layerid_ = tracking::sim::utils::getSensorID(hit);
+
+  // Set the global positions
+  x_ = hit.getPosition()[2];
+  y_ = hit.getPosition()[0];
+  z_ = hit.getPosition()[1];
+
+  edep_ = hit.getEdep();
+  t_ = hit.getTime();
+  id_ = hit.getID();
+
+  var_r_ = sigma_u * sigma_u;
+  var_z_ = sigma_v * sigma_v;
+}
+}
