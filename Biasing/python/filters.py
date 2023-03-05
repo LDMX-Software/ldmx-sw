2f6f760a5313bd29409a1b0cd94cea57281ffde0
Einar Elén <einar.elen@gmail.com>
2023-03-05 22:13:33 +0100

    Add default parameter for vetoing missed events

--- Biasing/python/filters.py
+++ Biasing/python/filters.py
@@ -125,15 +125,18 @@ class TaggerVetoFilter(simcfg.UserAction):
     ----------
     thresh : float
         Minimum energy [MeV] that electron should have
+    reject_events_missing_tagger : bool
+        Also veto events where the primary particle misses the tagger region
     """
     
-    def __init__(self,thresh=3800.) :
+    def __init__(self,thresh=3800., reject_events_missing_tagger=True) :
         super().__init__('tagger_veto_filter','biasing::TaggerVetoFilter')
 
         from LDMX.Biasing import include
         include.library()
 
         self.threshold = thresh
+        self.reject_events_missing_tagger = reject_events_missing_tagger
 
 class PrimaryToEcalFilter(simcfg.UserAction) :
     """ Configuration used to reject events where the primary doesn't reach the ecal with a mimimum energy
