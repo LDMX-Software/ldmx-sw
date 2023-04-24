67227330bd2650dbef61bc640e6896ee4bdb7480
Einar Elén <einar.elen@gmail.com>
2023-04-24 15:54:22 +0200

    First attempt at a inefficiency analyzer

---
+++ DQM/include/DQM/HcalNeutronInefficiencyDQM.h
@@ -0,0 +1,59 @@
+#ifndef HCALNEUTRONINEFFICIENCYDQM_H
+#define HCALNEUTRONINEFFICIENCYDQM_H
+#include "Framework/Configure/Parameters.h"
+#include "Framework/Event.h"
+#include "Framework/EventProcessor.h"
+#include "SimCore/Event/SimCalorimeterHit.h"
+#include <DetDescr/HcalID.h>
+#include <Hcal/Event/HcalHit.h>
+#include <string>
+namespace dqm {
+class HcalNeutronInefficiencyAnalyzer : public framework::Analyzer {
+public:
+  HcalNeutronInefficiencyAnalyzer(const std::string &name,
+                                  framework::Process &process)
+      : framework::Analyzer{name, process} {}
+
+  void configure(framework::config::Parameters &parameters) {}
+  bool hitPassesVeto(const ldmx::HcalHit &hit) {
+    return hit.getPE() > 0 && hit.getMinPE() > 0;
+  }
+  void analyze(const framework::Event &event) override {
+    const auto hcalSimHits = event.getCollection<ldmx::SimCalorimeterHit>(
+        hcalSimHitsCollection_, hcalSimHitsPassName_);
+    const auto hcalRecHits = event.getCollection<ldmx::HcalHit>(
+        hcalRecHitsCollection_, hcalRecHitsPassName_);
+
+    /* const auto hcalSimHits =
+     * event.getCollection<ldmx::SimCalorimeterHit>(hcalSimHitsCollection_,
+     * hcalSimHitsPassName_); */
+
+    /* int firstLayerHit {9999}; */
+    double firstHitZ{5500};
+
+    for (const auto hit : hcalRecHits) {
+      const ldmx::HcalID id{hit.getID()};
+      if (id.section() != ldmx::HcalID::HcalSection::BACK) {
+        continue;
+      }
+      const auto z{hit.getZPos()};
+
+      if (hitPassesVeto(hit)) {
+        /* const auto& [x,y,z] {hit.getPosition()}; */
+        if (z < firstHitZ) {
+          firstHitZ = z;
+        }
+      }
+    }
+    histograms_.fill("Inefficiency", firstHitZ);
+  }
+
+  std::string hcalSimHitsCollection_{"HcalSimHits"};
+  std::string hcalRecHitsCollection_{"HcalRecHits"};
+  std::string hcalSimHitsPassName_{""};
+  std::string hcalRecHitsPassName_{""};
+};
+
+} // namespace dqm
+
+#endif /* HCALNEUTRONINEFFICIENCYDQM_H */
