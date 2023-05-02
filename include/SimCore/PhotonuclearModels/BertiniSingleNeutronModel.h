6ca7aeac095a9d4245bca8767869ba9f2dbd2532
Einar Elén <einar.elen@gmail.com>
2023-05-02 16:16:41 +0200

    Add a single neutron model

---
+++ include/SimCore/PhotonuclearModels/BertiniSingleNeutronModel.h
@@ -0,0 +1,54 @@
+#ifndef SIMCORE_BERTINI_SINGLE_NEUTRON_MODEL_H
+#define SIMCORE_BERTINI_SINGLE_NEUTRON_MODEL_H
+#include "Framework/Configure/Parameters.h"
+#include "G4CrossSectionDataSetRegistry.hh"
+#include "G4Gamma.hh"
+#include "G4HadProjectile.hh"
+#include "G4HadronInelasticProcess.hh"
+#include "G4Nucleus.hh"
+#include "G4PhotoNuclearCrossSection.hh"
+#include "G4ProcessManager.hh"
+#include "SimCore/PhotonuclearModel.h"
+#include "SimCore/PhotonuclearModels/BertiniEventTopologyProcess.h"
+namespace simcore {
+class BertiniSingleNeutronProcess : public BertiniEventTopologyProcess {
+ public:
+  BertiniSingleNeutronProcess(double threshold, int Zmin, double Emin)
+      : BertiniEventTopologyProcess{},
+        threshold_{threshold},
+        Zmin_{Zmin},
+        Emin_{Emin} {}
+  virtual ~BertiniSingleNeutronProcess() = default;
+  bool acceptProjectile(const G4HadProjectile& projectile) const override {
+    return projectile.GetKineticEnergy() >= Emin_;
+  }
+  bool acceptTarget(const G4Nucleus& targetNucleus) const override {
+    return targetNucleus.GetZ_asInt() >= Zmin_;
+  }
+  bool acceptEvent() const override;
+
+ private:
+  double threshold_;
+  int Zmin_;
+  double Emin_;
+};
+
+class BertiniSingleNeutronModel : public PhotonuclearModel {
+ public:
+  BertiniSingleNeutronModel(const std::string& name,
+                            const framework::config::Parameters& parameters)
+      : PhotonuclearModel{name, parameters},
+        threshold_{parameters.getParameter<double>("hard_particle_threshold")},
+        Zmin_{parameters.getParameter<int>("zmin")},
+        Emin_{parameters.getParameter<double>("emin")} {}
+  virtual ~BertiniSingleNeutronModel() = default;
+  void ConstructGammaProcess(G4ProcessManager* processManager) override;
+
+ private:
+  double threshold_;
+  int Zmin_;
+  double Emin_;
+};
+
+}  // namespace simcore
+#endif /* SIMCORE_BERTINI_SINGLE_NEUTRON_MODEL_H */
--- python/photonuclear_models.py
+++ python/photonuclear_models.py
@@ -14,3 +14,18 @@ class BertiniModel(simcfg.PhotonuclearModel):
         super().__init__('BertiniModel',
                          'simcore::BertiniModel',
                          'SimCore_PhotonuclearModels')
+class BertiniSingleNeutronModel(simcfg.PhotonuclearModel):
+    """ A photonuclear model producing only topologies with no particles above a
+    certain threshold.
+
+    Uses the default Bertini model from Geant4.
+
+    """
+
+    def __init__(self):
+        super().__init__('BertiniSingleNeutronModel',
+                         'simcore::BertiniSingleNeutronModel',
+                         'SimCore_PhotonuclearModels')
+        self.hard_particle_threshold = 200.
+        self.zmin = 0
+        self.emin = 2500.
