a19e121b363a5d08e9b604394183c68981a2eb82
Einar Elén <einar.elen@gmail.com>
2023-05-12 21:58:26 +0200

    Switch to using enum classes

--- DQM/include/DQM/PhotoNuclearDQM.h
+++ DQM/include/DQM/PhotoNuclearDQM.h
@@ -16,6 +16,40 @@ class Event;
 class SimParticle;
 
 class PhotoNuclearDQM : public framework::Analyzer {
+
+  // Classification schemes for PN events
+  enum class CompactEventType {
+    single_neutron = 0,
+    single_charged_kaon = 1,
+    single_neutral_kaon = 2,
+    two_neutrons = 3,
+    soft = 4,
+    other = 5,
+  };
+  enum class EventType {
+    nothing_hard = 0,
+    single_neutron = 1,
+    two_neutrons = 2,
+    three_or_more_neutrons = 3,
+    single_charged_pion = 4,
+    two_charged_pions = 5,
+    single_neutral_pion = 6,
+    single_charged_pion_and_nucleon = 7,
+    single_charged_pion_and_two_nucleons = 8,
+    two_charged_pions_and_nucleon = 9,
+    single_neutral_pion_and_nucleon = 10,
+    single_neutral_pion_and_two_nucleons = 11,
+    single_neutral_pion_charged_pion_and_nucleon = 12,
+    single_proton = 13,
+    two_protons = 14,
+    proton_neutron = 15,
+    klong = 16,
+    charged_kaon = 17,
+    kshort = 18,
+    exotics = 19,
+    multibody = 20,
+  };
+
 public:
   /// Constructor
   PhotoNuclearDQM(const std::string &name, framework::Process &process);
@@ -42,15 +76,16 @@ public:
   void onProcessStart();
 
 private:
-
   /** Method used to classify events. */
-  int classifyEvent(const std::vector<const ldmx::SimParticle *> daughters,
-                    double threshold);
+  EventType
+  classifyEvent(const std::vector<const ldmx::SimParticle *> daughters,
+                double threshold);
 
   /** Method used to classify events in a compact manner. */
-  int classifyCompactEvent(
-      const ldmx::SimParticle *pnGamma,
-      const std::vector<const ldmx::SimParticle *> daughters, double threshold);
+  CompactEventType
+  classifyCompactEvent(const ldmx::SimParticle *pnGamma,
+                       const std::vector<const ldmx::SimParticle *> daughters,
+                       double threshold);
 
   bool verbose_;
   void findRecoilProperties(const ldmx::SimParticle *recoil);
@@ -64,31 +99,8 @@ private:
 
   void findSubleadingKinematics(
       const ldmx::SimParticle *pnGamma,
-      const std::vector<const ldmx::SimParticle *> &pnDaughters,
-      const int eventType);
-  enum EventType {
-    nothing_hard = 0,
-    single_neutron = 1,
-    two_neutrons = 2,
-    three_or_more_neutrons = 3,
-    single_charged_pion = 4,
-    two_charged_pions = 5,
-    single_neutral_pion = 6,
-    single_charged_pion_and_nucleon = 7,
-    single_charged_pion_and_two_nucleons = 8,
-    two_charged_pions_and_nucleon = 9,
-    single_neutral_pion_and_nucleon = 10,
-    single_neutral_pion_and_two_nucleons = 11,
-    single_neutral_pion_charged_pion_and_nucleon = 12,
-    single_proton = 13,
-    two_protons = 14,
-    proton_neutron = 15,
-    klong = 16,
-    charged_kaon = 17,
-    kshort = 18,
-    exotics = 19,
-    multibody = 20,
-  };
+      const std::vector<const ldmx::SimParticle *> &pnDaughters, //
+      const EventType eventType);
 };
 
 } // namespace dqm
