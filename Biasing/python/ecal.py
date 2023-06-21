717cf8b89b0a54d7d19dce8fb4874b33b3db3457
Tyler Horoho <tgh7hx@virginia.edu>
2023-06-21 12:53:40 -0400

    Fixed muon conversion sim issue where brems are created in tagger

--- Biasing/python/ecal.py
+++ Biasing/python/ecal.py
@@ -74,3 +74,56 @@ def photo_nuclear( detector, generator ) :
     ])
 
     return sim
+
+def gamma_mumu(detector, generator) :
+    """Example configuration for biasing gamma to mu+ mu- conversions in the ecal.
+
+    In this particular example, 4 GeV elecrons are fired upstream of the
+    tagger tracker. The TargetBremFilter filters out all events that don't
+    produce a brem in the target with an energy greater than 2.5 GeV.
+
+    Parameters
+    ----------
+
+    detector : str
+        Path to the detector
+
+    Returns
+    -------
+    Instance of the sim configured for target gamma to muon conversions.
+
+    Example
+    -------
+
+        ecal_mumu_sim = ecal.gamma_mumu('ldmx-det-v12')
+
+    """
+
+    # Initiate the sim
+    sim = simulator.simulator("ecal_gammamumu")
+
+    # Set the path to the detector to use
+    # Also tell the simulator to include scoring planes
+    sim.setDetector( detector, True )
+
+    # Set run parameters
+    sim.description = "gamma --> mu+ mu-, xsec bias 3e4"
+    sim.beamSpotSmear = [20., 80., 0.]
+
+    sim.generators.append(generator)
+
+    # Enable and configure the biasing
+    sim.biasing_operators = [ bias_operators.GammaToMuPair('ecal', 3.E4, 2500.) ]
+
+    # the following filters are in a library that needs to be included
+    includeBiasing.library()
+
+    # Configure the sequence in which user actions should be called
+    sim.actions.extend([
+            filters.TaggerVetoFilter(),
+            filters.TargetBremFilter(),
+            filters.EcalProcessFilter(process='GammaToMuPair'),
+            util.TrackProcessFilter.gamma_mumu()
+    ])
+
+    return sim
