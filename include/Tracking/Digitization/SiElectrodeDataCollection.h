84c3ab895b54201ee4ffa372f73de3e744ae379d
pbutti <pierfrancesco.butti@protonmail.com>
2023-06-26 11:15:27 +0200

    First commit to port CDF-based digi package

---
+++ include/Tracking/Digitization/SiElectrodeDataCollection.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Tracking/Digitization/SiElectrodeData.h"
+#include <map>
+
+namespace tracking {
+  namespace digitization {
+
+    class SiElectrodeDataCollection {
+
+   public :
+
+      SiElectrodeDataCollection() {};
+      ~SiElectrodeDataCollection(){collection_.clear();};
+      
+
+      SiElectrodeDataCollection(const SiElectrodeDataCollection& electrode_data);
+      
+      //Create from a map of electrode charges for a single SimTrackerHit
+      //map should always be sorted ascending
+      SiElectrodeDataCollection(const std::map<int,int>& electrode_charge,
+                                ldmx::SimTrackerHit hit);
+
+      
+      void add(const std::map<int, SiElectrodeData>& electrode_data_collection);
+      
+      void add(int cellid, SiElectrodeData electrode_data);
+      
+      std::map<int,int> getChargeMap() const ;
+      
+      std::map<int,SiElectrodeData> getCollection() const {return collection_;};
+
+      void clear() {collection_.clear();};
+      
+      
+   private :
+
+      std::map<int, SiElectrodeData> collection_;
+
+      
+    };
+    
+  }
+}
---
+++ src/Tracking/Digitization/SiElectrodeDataCollection.cxx
@@ -0,0 +1,58 @@
+#include "Tracking/Digitization/SiElectrodeDataCollection.h"
+
+namespace tracking{
+namespace digitization{
+
+
+SiElectrodeDataCollection::SiElectrodeDataCollection(const SiElectrodeDataCollection& electrode_data) {
+  collection_ = electrode_data.getCollection();
+  
+}
+
+
+SiElectrodeDataCollection::SiElectrodeDataCollection(const std::map<int,int>& electrode_charge,
+                                                     ldmx::SimTrackerHit hit){
+  
+ 
+  for (auto pair : electrode_charge) {
+    collection_[pair.first] = SiElectrodeData(pair.second, hit);
+  }
+}
+
+std::map<int,int> SiElectrodeDataCollection::getChargeMap() const {
+
+  std::map<int,int> charge_map;
+
+  for (auto pair : collection_) {
+    charge_map[pair.first] = pair.second.getCharge();
+  }
+
+  return charge_map;
+}
+
+
+void SiElectrodeDataCollection::add(const std::map<int,SiElectrodeData>& electrode_data_collection) {
+
+  for (auto pair : electrode_data_collection) {
+
+    //Check if the internal collection_ has a key
+    if (collection_.count(pair.first))
+        collection_[pair.first].add(pair.second);
+
+    else
+      collection_[pair.first] = pair.second;
+    
+  
+  }
+}
+
+void SiElectrodeDataCollection::add(int cellid, SiElectrodeData electrode_data) {
+  if (electrode_data.isValid())
+    if (collection_.count(cellid))
+      collection_[cellid].add(electrode_data);
+    else
+      collection_[cellid] = electrode_data;
+}
+
+}
+}
