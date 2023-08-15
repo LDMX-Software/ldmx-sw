1be342691a15a651fecb84e847c641b545397238
tomeichlersmith <eichl008@umn.edu>
2023-08-15 14:25:03 -0500

    move lambda function definitions into full functions
    this prevents the link-time error of multi-definitions

--- include/Tracking/Sim/BFieldXYZUtils.h
+++ include/Tracking/Sim/BFieldXYZUtils.h
@@ -11,6 +11,7 @@
 
 #include <functional>
 #include <fstream>
+#include <iostream>
 
 using InterpolatedMagneticField3 =
   Acts::InterpolatedBFieldMap<Acts::detail::Grid<
@@ -25,59 +26,19 @@ using InterpolatedMagneticField3 =
 // [3] Create the transformation for the position
 // map (z,x,y) -> (x,y,z)
 // TODO:: Remove this hardcoded value!
-auto default_transformPos = [](const Acts::Vector3& pos, float offset=400.) {
-  
-  Acts::Vector3 rot_pos;
-  rot_pos(0)=pos(1);
-  rot_pos(1)=pos(2);
-  rot_pos(2)=pos(0) + offset;
-
-  //std::cout<<"PF::DEFAULT TRANSFORM"<<std::endl;
-  //std::cout<<"PF::Check:: transforming Pos"<<std::endl;
-  //std::cout<<pos<<std::endl;
-  //std::cout<<"TO"<<std::endl;
-  //std::cout<<rot_pos<<std::endl;
-  
-  return rot_pos;
-};
+Acts::Vector3 default_transformPos(const Acts::Vector3& pos);
 
 // [4] Create the transformation for the bfield
 // map (Bx,By,Bz) -> (Bx,By,Bz)
-auto default_transformBField = [](const Acts::Vector3& field,
-                                  const Acts::Vector3& /*pos*/) {
-  
-  
-  Acts::Vector3 rot_field;
-  
-  rot_field(0) = field(2);
-  rot_field(1) = field(0);
-  rot_field(2) = field(1);
-
-  //std::cout<<"PF::DEFAULT TRANSFORM"<<std::endl;
-  //std::cout<<"PF::Check:: transforming"<<std::endl;
-  //std::cout<<field<<std::endl;
-  //std::cout<<"TO"<<std::endl;
-  //std::cout<<rot_field<<std::endl;
-  
-  return rot_field;
-};
-
-
+Acts::Vector3 default_transformBField(const Acts::Vector3& field,
+                                  const Acts::Vector3& /*pos*/);
 
 // map (Bx,By,Bz) -> (Bx,By,Bz)
 //auto transformBField = [](const Vector3& field, const Vector3&) {
 //  return field;
 //};
 
-auto localToGlobalBin_xyz = [](std::array<size_t, 3> bins,
-                               std::array<size_t, 3> sizes) {
-  return (bins[0] * (sizes[1] * sizes[2]) + bins[1] * sizes[2] +
-          bins[2]);  // xyz - field space
-  // return (bins[1] * (sizes[2] * sizes[0]) + bins[2] * sizes[0] + bins[0]);
-  // //zxy
-};
-
-
+size_t localToGlobalBin_xyz(std::array<size_t, 3> bins, std::array<size_t, 3> sizes);
 
 inline Acts::InterpolatedBFieldMap<Acts::detail::Grid<
 Acts::Vector3, Acts::detail::EquidistantAxis, Acts::detail::EquidistantAxis,
