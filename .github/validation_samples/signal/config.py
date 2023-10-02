0609192b5a0df8c1f1949e542607e468a14d0f4e
tomeichlersmith <eichl008@umn.edu>
2023-10-02 12:39:21 -0500

    init a signal validation sample
    use mA=10MeV and use the template simulation from Biasing.target module
    
    use the init.sh script infrastructure to generate the reference lbirary before attempting the simulation

---
+++ .github/validation_samples/signal/config.py
@@ -0,0 +1,71 @@
+from LDMX.Framework import ldmxcfg
+p = ldmxcfg.Process('test')
+
+p.maxTriesPerEvent = 10000
+
+from LDMX.Biasing import target
+mySim = target.dark_brem(
+    #A' mass in MeV - set in init.sh to same value in GeV
+    10.0,
+    # library path is uniquely determined by arguments given to `dbgen run` in init.sh
+    #   easiest way to find this path out is by running `. init.sh` locally to see what
+    #   is produced
+    'electron_tungsten_MaxE_8.0_MinE_4.0_RelEStep_0.1_UndecayedAP_mA_0.01_run_1',
+    'ldmx-det-v14-8gev'
+)
+
+p.sequence = [ mySim ]
+
+##################################################################
+# Below should be the same for all sim scenarios
+
+import os
+import sys
+
+p.maxEvents = int(os.environ['LDMX_NUM_EVENTS'])
+p.run = int(os.environ['LDMX_RUN_NUMBER'])
+
+p.histogramFile = f'hist.root'
+p.outputFiles = [f'events.root']
+
+import LDMX.Ecal.EcalGeometry
+import LDMX.Ecal.ecal_hardcoded_conditions
+import LDMX.Hcal.HcalGeometry
+import LDMX.Hcal.hcal_hardcoded_conditions
+import LDMX.Ecal.digi as ecal_digi
+import LDMX.Ecal.vetos as ecal_vetos
+import LDMX.Hcal.digi as hcal_digi
+
+from LDMX.TrigScint.trigScint import TrigScintDigiProducer
+from LDMX.TrigScint.trigScint import TrigScintClusterProducer
+from LDMX.TrigScint.trigScint import trigScintTrack
+ts_digis = [
+        TrigScintDigiProducer.pad1(),
+        TrigScintDigiProducer.pad2(),
+        TrigScintDigiProducer.pad3(),
+        ]
+for d in ts_digis :
+    d.randomSeed = 1
+
+from LDMX.Recon.electronCounter import ElectronCounter
+from LDMX.Recon.simpleTrigger import TriggerProcessor
+
+count = ElectronCounter(1,'ElectronCounter')
+count.input_pass_name = ''
+
+from LDMX.DQM import dqm
+
+p.sequence.extend([
+        ecal_digi.EcalDigiProducer(),
+        ecal_digi.EcalRecProducer(), 
+        ecal_vetos.EcalVetoProcessor(),
+        hcal_digi.HcalDigiProducer(),
+        hcal_digi.HcalRecProducer(),
+        *ts_digis,
+        TrigScintClusterProducer.pad1(),
+        TrigScintClusterProducer.pad2(),
+        TrigScintClusterProducer.pad3(),
+        trigScintTrack, 
+        count, TriggerProcessor('trigger'),
+        dqm.DarkBremInteraction()
+        ] + dqm.all_dqm)
