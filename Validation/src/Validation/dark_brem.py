d19dc7edf947f77539903974b7b905c7613b4597
tomeichlersmith <eichl008@umn.edu>
2023-10-03 10:28:05 -0500

    add tick labels to element/material dark brem histograms
    - move tick labels to bin centers so the number of labels is equal to
      the number of bins (and not the number of bin edges)
    - update hcal and photonuclear modules to conform to this change,
      removing the trailing empty label present in those tick label lists

--- Validation/src/Validation/dark_brem.py
+++ Validation/src/Validation/dark_brem.py
@@ -24,9 +24,43 @@ def kinematics(d : Differ, out_dir = None) :
         ('incident_energy', 'Incident Energy [MeV]'),
         ('incident_pt', 'Incident $p_T$ [MeV]'),
         ('dark_brem_z', 'Dark Brem Z Location [mm]'),
-        ('dark_brem_element', 'Element in which Dark Brem Occurred'),
-        ('dark_brem_material', 'Material in which Dark Brem Occurred')
     ]
     for h, name in features :
         log.info(f'plotting {h}')
         d.plot1d(f'db_kinematics/db_kinematics_{h}', name, out_dir = out_dir)
+
+    log.info('plotting dark_brem_element')
+    d.plot1d(
+        'db_kinematics/db_kinematics_dark_brem_element', 
+        'Element in which Dark Brem Occurred',
+        out_dir = out_dir,
+        tick_labels = [
+            "did not happen",
+            "H 1",
+            "C 6",
+            "O 8",
+            "Na 11",
+            "Si 14",
+            "Ca 20",
+            "Cu 29",
+            "W 74",
+            "unlisted",
+        ]
+    )
+
+    log.info('plotting dark_brem_material')
+    d.plot1d(
+        'db_kinematics/db_kinematics_dark_brem_material',
+        'Material in which Dark Brem Occurred',
+        out_dir = out_dir,
+        tick_labels = [
+            "Unknown",
+            "C",
+            "PCB",
+            "Glue",
+            "Si",
+            "Al",
+            "W",
+            "PVT"
+        ]
+    )
