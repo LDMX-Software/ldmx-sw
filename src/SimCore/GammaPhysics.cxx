4a6896541fc259cc1400a82fb9d4001e1c691e71
tomeichlersmith <eichl008@umn.edu>
2023-10-23 14:57:51 -0500

    refactor GammaPhysics to remove extraneous classes

--- src/SimCore/GammaPhysics.cxx
+++ src/SimCore/GammaPhysics.cxx
@@ -9,37 +9,37 @@
 
 namespace simcore {
 
-G4ProcessManager* GammaPhysics::GetGammaProcessManager() const {
-  if (G4Gamma::Gamma()->GetProcessManager()) return G4Gamma::Gamma()->GetProcessManager();
-  EXCEPTION_RAISE("GammaPhysics",
-                  "Was unable to access the process manager for photons, "
-                  "something is very wrong!");
-}
+GammaPhysics::GammaPhysics(const G4String& name,
+                           const framework::config::Parameters& parameters)
+    : G4VPhysicsConstructor(name),
+      modelParameters{parameters.getParameter<framework::config::Parameters>(
+          "photonuclear_model")} {}
 
-void GammaPhysics::SetPhotonNuclearAsFirstProcess() const {
-  auto processManager{GetGammaProcessManager()};
-  // Get the process list associated with the gamma.
-  const auto processes{processManager->GetProcessList()};
+void GammaPhysics::ConstructParticle() {}
 
-  for (int i{0}; i < processes->size(); ++i) {
-    const auto process{(*processes)[i]};
-    const auto processName{process->GetProcessName()};
-    if (processName == "photonNuclear") {
-      processManager->SetProcessOrderingToFirst(
-          process, G4ProcessVectorDoItIndex::idxAll);
-    }
-  }
-}
 void GammaPhysics::ConstructProcess() {
-  G4ProcessManager* processManager = GetGammaProcessManager();
-
+  G4ProcessManager* processManager = G4Gamma::Gamma()->GetProcessManager();
+  if (processManager == nullptr) {
+    EXCEPTION_RAISE("GammaPhysics",
+                    "Was unable to access the process manager for photons, "
+                    "something is very wrong!");
+  }
+  // configure our PN model based on runtime parameters
   auto pn = PhotoNuclearModel::Factory::get().make(
       modelParameters.getParameter<std::string>("class_name"),
       modelParameters.getParameter<std::string>("instance_name"),
       modelParameters);
   pn->removeExistingModel(processManager);
   pn->ConstructGammaProcess(processManager);
-  SetPhotonNuclearAsFirstProcess();
+  // put the PN process first in the ordering in case PN biasing is happening
+  const auto processes{processManager->GetProcessList()};
+  for (int i{0}; i < processes->size(); i++) {
+    const auto process{(*processes)[i]};
+    if (process->GetProcessName() == "photonNuclear") {
+      processManager->SetProcessOrderingToFirst(
+          process, G4ProcessVectorDoItIndex::idxAll);
+    }
+  }
   // Add the gamma -> mumu to the physics list.
   processManager->AddDiscreteProcess(&gammaConvProcess);
 }
