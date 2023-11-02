a16a4f82682a140ffc2e329a1d52d2b0f29399bd
Einar Elén <einar.elen@gmail.com>
2023-11-02 16:42:08 +0100

    Warn but don't crash on missing key in differ

--- Validation/src/Validation/_differ.py
+++ Validation/src/Validation/_differ.py
@@ -7,6 +7,7 @@ import re
 # external dependencies
 import matplotlib.pyplot as plt
 import matplotlib
+import uproot
 # us
 from ._file import File
 
@@ -110,7 +111,11 @@ class Differ :
         ax = fig.subplots()
 
         for f in self.files :
-            weights, bins, patches = f.plot1d(ax, column, **hist_kwargs)
+            try:
+                weights, bins, patches = f.plot1d(ax, column, **hist_kwargs)
+            except uproot.KeyInFileError:
+                f.log.warn(f"Key {column} doesn't exist in {self}, skipping")
+                continue
 
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
