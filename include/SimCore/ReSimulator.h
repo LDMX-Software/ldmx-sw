9f605fb2b30ca0a98c23b020bfe856ea72005633
tomeichlersmith <eichl008@umn.edu>
2023-11-13 11:48:16 -0600

    add option to filter by run number as well as event number
    This is helpful for people like me who have massive samples of
    heavily-filtered simulations. The best way to generate these samples is
    across many runs but then merge the resulting files. This results in
    several runs per file and often an event from one run is aborted by the
    simulation while the same event number from a different run is the event
    I wish to resimulate.

--- include/SimCore/ReSimulator.h
+++ include/SimCore/ReSimulator.h
@@ -10,14 +10,14 @@ class ReSimulator : public SimulatorBase {
  public:
   ReSimulator(const std::string& name, framework::Process& process)
       : SimulatorBase{name, process} {}
-  /*
+  /**
    * Callback for the processor to configure itself from the given set
    * of parameters.
    *
    * @param parameters ParameterSet for configuration.
    */
   void configure(framework::config::Parameters& parameters) final override;
-  /*
+  /**
    * Run resimulation if the event is part of the requested sets of events to
    * resimulate
    *
@@ -26,7 +26,7 @@ class ReSimulator : public SimulatorBase {
   void produce(framework::Event& event) override;
 
  private:
-  /*
+  /**
    * List of event numbers in the input files that should be resimulated if
    * `resimulate_all_events` is false.
    *
@@ -34,6 +34,24 @@ class ReSimulator : public SimulatorBase {
    * input file, it will be ignored.
    */
   std::vector<int> events_to_resimulate_;
+
+  /**
+   * List of run numbers in the input files that should be resimulated if
+   * `resimulate_all_events` is false.
+   *
+   * @note: If a run number in `runs_to_resimulate_` is not part of the
+   * input file, it will be ignored.
+   *
+   * The list of runs to resimulate comes in one of three forms:
+   * 1. Empty: If no runs are given, we only match based off the event number.
+   * 2. Single Entry: If only one run is given, then we require the run number
+   *    to be that entry for all events requested to resimulate.
+   * 3. More than one entry: In this case, we require the length of this list
+   *    to be the same as the length of the events to resimulate so that we 
+   *    only resimulate events where the event/run *pair* matches a corresponding
+   *    *pair* in the events and runs to resimulate lists.
+   */
+  std::vector<int> runs_to_resimulate_;
 
   /**
    * Whether to resimulate all events in the input files
