b359c0ff9d08a43b69f48b845fdcd366380fd4ae
Horoho <horoho@sdf-login04.slac.stanford.edu>
2023-11-14 12:44:51 -0800

    Adding A' as tick mark in pdgid plot

--- Validation/src/Validation/simparticles.py
+++ Validation/src/Validation/simparticles.py
@@ -7,7 +7,7 @@ log = logging.getLogger('8GeV')
 @plotter(hist=True, event=False)
 def beamenergy_comp(d: Differ, out_dir=None):
 
-    pdgid_labels = ['', 'e+', 'e-', 'μ+', 'μ-', 'γ', 'p', 'n', 'π+', 'π-', 'π0', 'K+', 'K-', 'K-L', 'K-S', 'nucleus', 'exotic', ''] # finish later 
+    pdgid_labels = ['', 'e+', 'e-', 'μ+', 'μ-', 'γ', 'p', 'n', 'π+', 'π-', 'π0', 'K+', 'K-', 'K-L', 'K-S', 'nucleus', 'exotic', "A'"] # finish later 
 
     d.plot1d("SampleValidation/SampleValidation_pdgid_primaries", "PDG ID, primaries",
              tick_labels=pdgid_labels,
@@ -19,6 +19,14 @@ def beamenergy_comp(d: Differ, out_dir=None):
              density=True)
 
     d.plot1d("SampleValidation/SampleValidation_pdgid_primarydaughters", "PDG ID, primary daughters",
+             tick_labels=pdgid_labels,
+             out_dir=out_dir,
+             density=True)
+    d.plot1d("SampleValidation/SampleValidation_energy_daughterphoton", "Energy spectrum of all photons from primary",
+             out_dir=out_dir,
+             density=True)
+
+    d.plot1d("SampleValidation/SampleValidation_pdgid_harddaughters" "PDG ID of hard primary daughter",
              tick_labels=pdgid_labels,
              out_dir=out_dir,
              density=True)
