33fcc97eb2ce39fb0924ae4e33718260a68560d8
tomeichlersmith <eichl008@umn.edu>
2023-12-04 09:57:47 -0600

    re-enable custom class serialization for performance logging
    I learned how to mark class members as transient [0] which made me
    realize that having a special Timer class will make the code easier to
    read and the data easier to interpret, I'm happier with this design than
    previous designs so I spent a bit more effort in documentation.
    
    [0] https://root.cern/manual/io/#excluding-data-members-from-io

---
+++ include/Framework/Performance/LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ namespace framework::performance;
+
+#pragma link C++ class framework::performance::Timer+;
+
+#endif
--- src/Framework/Performance/Timer.cxx
+++ src/Framework/Performance/Timer.cxx
@@ -3,35 +3,33 @@
 
 #include "TVectorD.h"
 
+ClassImp(framework::performance::Timer);
+
 namespace framework::performance {
 
-Timer::Timer(bool do_start) {
-  if (do_start) start();
+void Timer::reset() {
+  start_ = {};
+  end_ = {};
+  start_time_ = -1;
+  duration_ = -1;
 }
 
 void Timer::start() {
   start_ = clock::now();
+  start_time_ = std::chrono::duration_cast<std::chrono::nanoseconds>(start_.time_since_epoch()).count();
 }
 
 void Timer::end() {
   end_ = clock::now();
+  duration_ = std::chrono::duration<double>(end_ - start_).count();
 }
 
 double Timer::duration() const {
-  return std::chrono::duration<double>(end_ - start_).count();
+  return duration_;
 }
 
 void Timer::write(TDirectory* location, const std::string& name) const {
-  /**
-   * The official ROOT method for writing a single data point into a file
-   * is to wrap it as a single-element array. ¯\_(ツ)_/¯
-   *
-   * brun approved
-   * https://root-forum.cern.ch/t/writing-simple-variable-in-root-files/11094
-   */
-  TVectorD v(1);
-  v[0] = duration();
-  location->WriteObject(&v, name.c_str());
+  location->WriteObject(this, name.c_str());
 }
 
 }
