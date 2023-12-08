144f119a879c9e196f02195d8c76232f6439ba93
tomeichlersmith <eichl008@umn.edu>
2023-12-08 09:47:09 -0600

    make ProductTag printout more human-readable

--- src/Framework/ProductTag.cxx
+++ src/Framework/ProductTag.cxx
@@ -1,5 +1,5 @@
 #include "Framework/ProductTag.h"
 
 std::ostream& operator<<(std::ostream& s, const framework::ProductTag& pt) {
-  return s << pt.name() << "_" << pt.passname() << "_" << pt.type();
+  return s << "{ name = " << pt.name() << ", pass = " << pt.passname() << ", type = " << pt.type() << "}";
 }
