c76e89028083297c0fa23469a21ba8f21e8e0430
Lene Kristian Bryngemark <lene.bryngemark@cern.ch>
2024-02-22 18:44:12 +0100

    compiles and runs, sets x and y in high resolution and in binned versions

--- Recon/src/Recon/BeamElectronLocator.cxx
+++ Recon/src/Recon/BeamElectronLocator.cxx
@@ -12,12 +12,13 @@ void BeamElectronLocator::configure(framework::config::Parameters &parameters) {
   inputPassName_ = parameters.getParameter<std::string>("input_pass_name");
   outputColl_ = parameters.getParameter<std::string>("output_collection");
   granularityXmm_ =
-      parameters.getParameter<float>("granularity_X_mm");
+      parameters.getParameter<double>("granularity_X_mm");
   granularityYmm_ =
-      parameters.getParameter<float>("granularity_Y_mm");
- tolerance_ =
-      parameters.getParameter<float>("min_granularity_mm");
- 
+      parameters.getParameter<double>("granularity_Y_mm");
+  tolerance_ =
+      parameters.getParameter<double>("min_granularity_mm");
+  verbose_ =  
+    parameters.getParameter<bool>("verbose");
 
   ldmx_log(debug) << "BeamElectronLocator is using parameters: "
                   << " \n\tinput_collection = " << inputColl_
@@ -25,7 +26,8 @@ void BeamElectronLocator::configure(framework::config::Parameters &parameters) {
                   << " \n\toutput_collection = " << outputColl_
                   << " \n\tgranularity_X_mm = " << granularityXmm_
                   << " \n\tgranularity_Y_mm = " << granularityYmm_
-                  << " \n\tmin_granularity_mm = " << tolerance_ ;
+                  << " \n\tmin_granularity_mm = " << tolerance_ 
+                  << " \n\tverbose = " << verbose_ ;
 }
 
   void BeamElectronLocator::produce(framework::Event &event) {
@@ -68,30 +70,51 @@ void BeamElectronLocator::configure(framework::config::Parameters &parameters) {
 	const auto simHits{event.getCollection<ldmx::SimCalorimeterHit>(
       inputColl_, inputPassName_)};
 
+	if (verbose_) {
+		ldmx_log(info) << "Looping through simhits in event " << event.getEventNumber() << "." ;
+	    }
+	
 	for (const auto &simHit : simHits) {
 	  //check if we already caught this position, else, add it 
 	  bool isMatched = false;
 	  std::vector<float> pos=simHit.getPosition();
 	  for (auto foundElectrons : beamElectronInfo ) {
 		//this check makes it square rather than a dR circle
-		if ( fabs( pos[0] - foundElectrons.getX() )> tolerance_
-			 && fabs( pos[1] - foundElectrons.getY() )> tolerance_ ) {
+		if ( fabs( pos[0] - foundElectrons.getX() ) < tolerance_
+			 && fabs( pos[1] - foundElectrons.getY() ) < tolerance_ ) {
+		  if (verbose_) 
+		    ldmx_log(debug) << "\tHit at (x = " <<  pos[0] << ", y = " <<  pos[1]
+				    << " matches electron found at (x = " << foundElectrons.getX()
+				    << ", y = " << foundElectrons.getY() << "); skip this simhit";
 		  isMatched=true;
+		  break;  //finding a match means Move on	      
 		} // if coordinates match something we already found 
 	  }// over found electrons 
 	  if (!isMatched) {		  
-		ldmx::BeamElectronTruth electronInfo;
-		electronInfo.setXYZ( pos[0], pos[1], pos[2]);
-		//find a way to do this later
-		//electronInfo.setThreeMomentum(simHit.getPx(), simHit.getPy(), simHit.getPz()); 
-		beamElectronInfo.push_back(electronInfo);
+	    if (verbose_) {
+	      ldmx_log(info) << "\tHit at (x = " <<  pos[0] << ", y = " <<  pos[1] << " not formerly matched. Adding to collection.";
+	    }
+	    ldmx::BeamElectronTruth electronInfo;
+	    electronInfo.setXYZ( pos[0], pos[1], pos[2]);
+	    //find a way to do this later
+	    //electronInfo.setThreeMomentum(simHit.getPx(), simHit.getPy(), simHit.getPz()); 
+
+	    //for these, i should 	
+	    //set up a 2d array, with some n number of bins set by the total width and granularity
+
+	    //TODO make min and max configurable 
+	    electronInfo.setBarX( bin(pos[0], granularityXmm_, -10, 10) );
+	    electronInfo.setBarY( bin(pos[1], granularityYmm_, -40, 40) );
+	    //set coordinates to bin center 
+	    electronInfo.setBinnedX( -10. + (electronInfo.getBarX()+1/2.)*granularityXmm_ );
+	    electronInfo.setBinnedY( -40. + (electronInfo.getBarY()+1/2.)*granularityYmm_ );
+
+	    
+	    beamElectronInfo.push_back(electronInfo);
 	  }
 	}// over simhits in the collection 
  
-  
-  //for these, i should 	
-	//set up a 2d array, with some n number of bins set by the total width and granularity 
-
+	event.add(outputColl_, beamElectronInfo);
 
   }
 
@@ -101,15 +124,17 @@ void BeamElectronLocator::configure(framework::config::Parameters &parameters) {
   int BeamElectronLocator::bin(float coordinate, float binWidth, float min, float max) {
 	// a function to discretize floating points to some precision 
 	int n=0;
-	while (coordinate < min + n*binWidth ) {
+	while (coordinate > min + n*binWidth ) {
 	  n++;
-	  if  (n*binWidth > max) {
+	  if  (min + n*binWidth > max) {
 		//n--; // this moves the point back, but, perhaps better to see that we go out of bounds on return 
 		break;
 	  }
 	}
-	// the resulting coordinate is between n which is the lower edge, and the next 
-	return n;
+	// the n we have is the first bin which is too large. 
+	// the resulting coordinate is between n which is the upper edge, and the previous
+	// return the lower edge 
+	return n-1;
   }
 
 
