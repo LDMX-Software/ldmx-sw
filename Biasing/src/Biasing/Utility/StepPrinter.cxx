b47ac77481c79c146d54c3bd27c8c64ceee86e2e
Einar Elén <einar.elen@gmail.com>
2024-03-31 23:22:49 +0200

    Add a utility function to trace parentage of a track

--- Biasing/src/Biasing/Utility/StepPrinter.cxx
+++ Biasing/src/Biasing/Utility/StepPrinter.cxx
@@ -17,6 +17,26 @@ StepPrinter::StepPrinter(const std::string& name,
   depth_ = parameters.getParameter<int>("depth");
 }
 
+bool isDescendent(int trackID) const {
+  int currentDepth{0};
+  int currentTrackID{trackID};
+  // Walk the tree until we either no longer have a parent or we reach the
+  // desired depth
+  while (currentDepth < depth_ &&
+         trackParents_.find(currentTrackID) != trackParents_.end()) {
+    // See if we have encountered the parent of the current track
+    //
+    // operator[] is not const, so we need to use at()
+    currentTrackID = trackParents_.at(currentTrackID);
+    if (currentTrackID == trackID_) {
+      // If one of the parents is the track of interest, we are done!
+      return true;
+    }
+    currentTrackID = track->GetParentID();
+    currentDepth++;
+  }
+  return false;
+}
 void StepPrinter::stepping(const G4Step* step) {
   // Get the track associated with this step
   auto track{step->GetTrack()};
