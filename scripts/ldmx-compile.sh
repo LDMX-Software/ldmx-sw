a49c2c7a41482903d9f3485864393e66d5d5c9b3
Tamas Vami <tamas.almos.vami@cern.ch>
2024-04-11 15:33:41 -0700

    Add new commands to auto-complete list

--- scripts/ldmx-compile.sh
+++ scripts/ldmx-compile.sh
@@ -4,7 +4,7 @@
 # ldmx ./scripts/ldmx-compile.sh
 # for changing the defaults
 # ldmx setenv LDMX_COMPILE_CORES=<num cores to be used>, i.e.
-# ldmx setenv LDMX_COMPILE_CORES=8
+# ldmx setenv LDMX_COMPILE_CORES=16
 # or
 # ldmx setenv LDMX_COMPILE_BUILD=<build location>
 
@@ -19,5 +19,5 @@ fi
 echo "-- Compiling ldmx-sw in ${LDMX_COMPILE_BUILD} with ${LDMX_COMPILE_CORES} cores"
 
 # Compile ldmx-sw
-cmake -B ${LDMX_COMPILE_BUILD}/build -S .
+cmake -B ${LDMX_COMPILE_BUILD}/build -S ${LDMX_COMPILE_BUILD}
 cmake --build ${LDMX_COMPILE_BUILD}/build --target install -j=${LDMX_COMPILE_CORES}
