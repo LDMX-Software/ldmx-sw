737b81543fbcc4052d711f4d33de56e28bc5051d
Tamas Vami <tamas.almos.vami@cern.ch>
2024-06-07 09:09:36 -0700

    clang format DetDescr

--- DetDescr/include/DetDescr/PackedIndex.h
+++ DetDescr/include/DetDescr/PackedIndex.h
@@ -24,11 +24,11 @@ namespace ldmx {
  * The maths underlying this packing also assumes that values start
  * with 0 and count up.
  *
- * @tparam[in] m0 modulus of field 0 
+ * @tparam[in] m0 modulus of field 0
  * @tparam[in] m1 modulus of field 1, optional
  * @tparam[in] m2 modulus of field 2, optional
  */
-template <unsigned int m0, unsigned int m1=0, unsigned int m2=0>
+template <unsigned int m0, unsigned int m1 = 0, unsigned int m2 = 0>
 class PackedIndex {
  public:
   /**
@@ -36,7 +36,7 @@ class PackedIndex {
    * Put our values into a single 32-bit integer.
    *
    * We are provided two to four values to pack.
-   * 
+   *
    * @note We do not check if the values are below the maxima
    * defined by the template parameters.
    *
@@ -45,30 +45,31 @@ class PackedIndex {
    * @param[in] v2 value of field 2
    * @param[in] v3 value of field 3
    */
-  PackedIndex(unsigned int v0, unsigned int v1, unsigned int v2=0, unsigned int v3=0) {
-    index_=v0+v1*m0+v2*m0*m1+v3*m0*m1*m2;
+  PackedIndex(unsigned int v0, unsigned int v1, unsigned int v2 = 0,
+              unsigned int v3 = 0) {
+    index_ = v0 + v1 * m0 + v2 * m0 * m1 + v3 * m0 * m1 * m2;
   }
 
   /// Constructor from index value
   PackedIndex(uint32_t value) : index_{value} {}
 
   /// Get the value of field 0
-  unsigned int field0() const { return index_%m0; }
+  unsigned int field0() const { return index_ % m0; }
   /// Get the value of field 1
-  unsigned int field1() const { return (index_/m0)%m1; }
+  unsigned int field1() const { return (index_ / m0) % m1; }
   /// Get the value of field 2
-  unsigned int field2() const { return (index_/(m0*m1))%m2; }
+  unsigned int field2() const { return (index_ / (m0 * m1)) % m2; }
   /// Get the value of field 3
-  unsigned int field3() const { return (index_/(m0*m1*m2)); }
+  unsigned int field3() const { return (index_ / (m0 * m1 * m2)); }
 
   /// Get the fully packed index
   uint32_t value() const { return index_; }
-  
+
  private:
   /// The 32-bit integer that is storing our fields
   uint32_t index_;
 
 };  // PackedIndex
-}
+}  // namespace ldmx
 
-#endif // DETDESCR_PACKEDINDEX_H_
+#endif  // DETDESCR_PACKEDINDEX_H_
--- DetDescr/src/DetDescr/HcalAbstractID.cxx
+++ DetDescr/src/DetDescr/HcalAbstractID.cxx
@@ -1,15 +1,21 @@
 #include "DetDescr/HcalAbstractID.h"
-#include "DetDescr/HcalID.h"
-#include "DetDescr/HcalDigiID.h"
+
 #include <iomanip>
+
+#include "DetDescr/HcalDigiID.h"
+#include "DetDescr/HcalID.h"
 
 std::ostream& operator<<(std::ostream& s, const ldmx::HcalAbstractID& id) {
   switch (id.bar_type()) {
-    case(ldmx::HcalAbstractID::Global): return s << ldmx::HcalID(id);
-    case(ldmx::HcalAbstractID::Digi): return s << ldmx::HcalDigiID(id);
-      //    case(ldmx::HcalAbstractID::Trigger): return s << ldmx::HcalTriggerID(id);
+    case (ldmx::HcalAbstractID::Global):
+      return s << ldmx::HcalID(id);
+    case (ldmx::HcalAbstractID::Digi):
+      return s << ldmx::HcalDigiID(id);
+      //    case(ldmx::HcalAbstractID::Trigger): return s <<
+      //    ldmx::HcalTriggerID(id);
     default:
-      return s << "HcalAbstractID("<<id.bar_type()<<":0x"<<std::setfill('0') << std::setw(6) << std::right << std::hex << id.payload();
+      return s << "HcalAbstractID(" << id.bar_type() << ":0x"
+               << std::setfill('0') << std::setw(6) << std::right << std::hex
+               << id.payload();
   }
-    
 }
