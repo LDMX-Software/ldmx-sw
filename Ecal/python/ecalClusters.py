bad99610c56602360094a14ea44bb8a564f2204d
Ella Viirola <ella.viirola@outlook.com>
2024-06-28 13:37:51 +0200

    Merge

--- Ecal/python/ecalClusters.py
+++ Ecal/python/ecalClusters.py
@@ -31,3 +31,30 @@ class EcalClusterProducer(ldmxcfg.Producer) :
         # Name of the cluster algo collection to make
         self.algoCollName = "ClusterAlgoResult"
 
+        # self.loopHist = 300
+        # self.clusterHist = 20
+
+        # self.build1DHistogram("nLoops", "No of loops for clustering", self.loopHist, 50, self.loopHist)
+        # self.build1DHistogram("nClusters", "No of clusters", self.clusterHist, 0, self.clusterHist)
+        # self.build1DHistogram("nHits", "Hits per cluster", 140, 0, 140)
+        # self.build1DHistogram("cluster_energy", "Energy [MeV] per cluster", 1000, 0, 10000)
+
+class EcalClusterAnalyzer(ldmxcfg.Analyzer) :
+    """Analyze clustering"""
+
+    def __init__(self,name='EcalClusterAnalyzer') :
+        super().__init__(name,"ecal::EcalClusterAnalyzer", 'Ecal')
+
+        self.ecalSimHitColl = "EcalSimHits"
+        self.ecalSimHitPass = "" #use whatever pass is available
+
+        # Pass name for ecal digis and rec hits
+        self.recHitCollName = 'EcalRecHits'
+        self.recHitPassName = ''
+
+        self.clusterCollName = 'ecalClusters'
+        self.clusterPassName = ''
+
+        self.depth = 100
+
+        self.build1DHistogram("ancestors", "Ancestors of particles", 4, 0, 3)
