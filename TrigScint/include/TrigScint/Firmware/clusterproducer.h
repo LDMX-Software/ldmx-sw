71fd2aa918ba8a97e67bb6c36912b6960f413747
rodwyer100 <rodwyer@stanford.edu>
2024-09-18 11:04:40 -0700

    Changing to array structure, haven't validated this yes please don't push just now.

--- TrigScint/include/TrigScint/Firmware/clusterproducer.h
+++ TrigScint/include/TrigScint/Firmware/clusterproducer.h
@@ -6,7 +6,7 @@
 void copyHit1(Hit One, Hit Two);
 void copyHit2(Hit One, Hit Two);
 void clusterproducer_ref(Hit inHit[NHITS], Cluster outClus[NCLUS]);
-std::unique_ptr<Cluster[]> clusterproducer_sw(Hit inHit[NHITS]);
+std::array<Cluster,NCLUS> clusterproducer_sw(Hit inHit[NHITS]);
 void clusterproducer_hw(Hit inHit[NHITS], Cluster outClus[NCLUS]);
 
 #endif
