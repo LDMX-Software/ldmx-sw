b984b6eb1f6a8951eec97b9fdb34984371decb59
Tamas Vami <tamas.almos.vami@cern.ch>
2024-09-20 09:06:03 -0700

    Make thresholds floats in simpleTrigger

--- Recon/python/simpleTrigger.py
+++ Recon/python/simpleTrigger.py
@@ -41,7 +41,7 @@ class TriggerProcessor(ldmxcfg.Producer) :
           
         self.beamEnergy = beamEnergy
         if (self.beamEnergy == 4000.): 
-            self.thresholds = [ 3000, 10000, 17000, 24200 ]
+            self.thresholds = [ 3000., 10000., 17000., 24200. ]
         else:
             self.thresholds = [ 3000., 10790., 18540., 26250. ]
         self.mode = 0
