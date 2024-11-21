e11abc16b17132a3d91b6021dd66570f8bde55b9
Tamas Vami <Tamas.Almos.Vami@cern.ch>
2024-11-21 12:59:00 -0800

    Move radius to 35 mm

--- Ecal/python/vetos.py
+++ Ecal/python/vetos.py
@@ -21,7 +21,7 @@ class EcalVetoProcessor(ldmxcfg.Producer) :
         self.bdt_file = makeBDTPath( "segmip" )
         self.roc_file = makeRoCPath( 'RoC_v14_8gev' )
         self.beam_energy = 8000.0  # in MeV
-        self.linreg_radius = 23.0 # in mm
+        self.linreg_radius = 35.0 # in mm
         self.disc_cut = 0.99741
         self.collection_name = "EcalVeto"
         self.rec_coll_name = 'EcalRecHits'
