ec4a38f405c9568b5a7032278df4c7168b64c901
Erik Wallin <skelpdar@gmail.com>
2025-01-06 16:24:26 +0100

    clang-format

--- Hcal/src/Hcal/HcalDigiProducer.cxx
+++ Hcal/src/Hcal/HcalDigiProducer.cxx
@@ -35,8 +35,8 @@ void HcalDigiProducer::configure(framework::config::Parameters& ps) {
   iSOI_ = hgcrocParams.getParameter<int>("iSOI");
   noise_ = hgcrocParams.getParameter<bool>("noise");
 
-  //If true, ignore readout threshold
-  //and generate pedestal noise digis in every empty channel
+  // If true, ignore readout threshold
+  // and generate pedestal noise digis in every empty channel
   digitizeAllChannels_ = ps.getParameter<bool>("digitizeAllChannels");
 
   // collection names
@@ -66,7 +66,6 @@ void HcalDigiProducer::configure(framework::config::Parameters& ps) {
 }
 
 void HcalDigiProducer::produce(framework::Event& event) {
-
   // Handle seeding on the first event
   if (!noiseGenerator_->hasSeed()) {
     const auto& rseed = getCondition<framework::RandomNumberSeedService>(
@@ -254,7 +253,8 @@ void HcalDigiProducer::produce(framework::Event& event) {
       ldmx::HcalDigiID posendID(section, layer, strip, 0);
       ldmx::HcalDigiID negendID(section, layer, strip, 1);
       if ((hgcroc_->digitize(posendID.raw(), pulses_posend, digiToAddPosend) &&
-          hgcroc_->digitize(negendID.raw(), pulses_negend, digiToAddNegend)) or digitizeAllChannels_) {
+           hgcroc_->digitize(negendID.raw(), pulses_negend, digiToAddNegend)) or
+          digitizeAllChannels_) {
         hcalDigis.addDigi(posendID.raw(), digiToAddPosend);
         hcalDigis.addDigi(negendID.raw(), digiToAddNegend);
       }  // Back Hcal needs to digitize both pulses or none
@@ -270,12 +270,14 @@ void HcalDigiProducer::produce(framework::Event& event) {
       }
       if (is_posend) {
         ldmx::HcalDigiID digiID(section, layer, strip, 0);
-        if (hgcroc_->digitize(digiID.raw(), pulses_posend, digiToAdd) or digitizeAllChannels_) {
+        if (hgcroc_->digitize(digiID.raw(), pulses_posend, digiToAdd) or
+            digitizeAllChannels_) {
           hcalDigis.addDigi(digiID.raw(), digiToAdd);
         }
       } else {
         ldmx::HcalDigiID digiID(section, layer, strip, 1);
-        if (hgcroc_->digitize(digiID.raw(), pulses_negend, digiToAdd) or digitizeAllChannels_) {
+        if (hgcroc_->digitize(digiID.raw(), pulses_negend, digiToAdd) or
+            digitizeAllChannels_) {
           hcalDigis.addDigi(digiID.raw(), digiToAdd);
         }
       }
@@ -286,22 +288,22 @@ void HcalDigiProducer::produce(framework::Event& event) {
    * Noise Simulation on Empty Channels
    *****************************************************************************************/
   if (noise_) {
-
     std::vector<ldmx::HcalDigiID> channelMap;
     int numChannels = 0;
     for (int section = 0; section < hcalGeometry.getNumSections(); section++) {
       int numChannelsInSection = 0;
-      for (int layer = 1; layer <= hcalGeometry.getNumLayers(section); layer++) {
+      for (int layer = 1; layer <= hcalGeometry.getNumLayers(section);
+           layer++) {
         numChannelsInSection += hcalGeometry.getNumStrips(section, layer);
-        for (int strip = 1; strip <= hcalGeometry.getNumStrips(section, layer); strip++){
-          if (section == ldmx::HcalID::HcalSection::BACK ) {
+        for (int strip = 1; strip <= hcalGeometry.getNumStrips(section, layer);
+             strip++) {
+          if (section == ldmx::HcalID::HcalSection::BACK) {
             auto detIDend0 = ldmx::HcalDigiID(section, layer, strip, 0);
             auto detIDend1 = ldmx::HcalDigiID(section, layer, strip, 1);
             channelMap.push_back(detIDend0);
             channelMap.push_back(detIDend1);
             numChannels += 2;
-          }
-          else{
+          } else {
             auto detID = ldmx::HcalDigiID(section, layer, strip, 0);
             channelMap.push_back(detID);
             numChannels++;
@@ -310,7 +312,7 @@ void HcalDigiProducer::produce(framework::Event& event) {
       }
     }
 
-    if (!digitizeAllChannels_) { //Fast noise sim
+    if (!digitizeAllChannels_) {  // Fast noise sim
       int numEmptyChannels = numChannels - hcalDigis.getNumDigis();
       // noise generator gives us a list of noise amplitudes [mV] that randomly
       // populate the empty channels and are above the readout threshold
@@ -324,7 +326,8 @@ void HcalDigiProducer::produce(framework::Event& event) {
         int sectionID, layerID, stripID, endID;
         do {
           sectionID = noiseInjector_->Integer(hcalGeometry.getNumSections());
-          layerID = noiseInjector_->Integer(hcalGeometry.getNumLayers(sectionID));
+          layerID =
+              noiseInjector_->Integer(hcalGeometry.getNumLayers(sectionID));
           // set layer to 1 if the generator says it is 0 (geometry map starts
           // from 1)
           if (layerID == 0) layerID = 1;
@@ -370,20 +373,22 @@ void HcalDigiProducer::produce(framework::Event& event) {
             hcalDigis.addDigi(noiseID, digiToAdd);
           }
         }
-      }    // loop over noise amplitudes
-    }
-    else{ //If digitizeAllChannels_ == true, add noise digis for all bars without simhits
-      for (auto digiID : channelMap){
-        //Convert from digi ID to det ID (since simhits don't know about different ends of the bar)
+      }       // loop over noise amplitudes
+    } else {  // If digitizeAllChannels_ == true, add noise digis for all bars
+              // without simhits
+      for (auto digiID : channelMap) {
+        // Convert from digi ID to det ID (since simhits don't know about
+        // different ends of the bar)
         ldmx::HcalID detid(digiID.section(), digiID.layer(), digiID.strip());
         unsigned int rawID = detid.raw();
-        if(hitsByID.find(rawID) == hitsByID.end()){
-          std::vector<ldmx::HgcrocDigiCollection::Sample> digi = hgcroc_->noiseDigi(rawID, 0.0);
+        if (hitsByID.find(rawID) == hitsByID.end()) {
+          std::vector<ldmx::HgcrocDigiCollection::Sample> digi =
+              hgcroc_->noiseDigi(rawID, 0.0);
           hcalDigis.addDigi(rawID, digi);
         }
       }
     }
-  }    // if we should add noise
+  }  // if we should add noise
 
   event.add(digiCollName_, hcalDigis);
 
