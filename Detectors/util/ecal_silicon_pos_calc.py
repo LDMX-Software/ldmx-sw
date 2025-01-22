6f4e8cffc8a14fd2339671043d1619a644a42f5f
Tamas Vami <tamas.almos.vami@cern.ch>
2025-01-21 20:48:11 -0800

    Update Ecal geom Z values to match v14 geometry (#1524)
    * Update Ecal geom Z values to match v14 geometry
    * Update reduced v1 with the new Z numbers too
    * Added script to calculate z position of silicon modules based on GDML
    * Updated z position calculation to match between layer weight script and GDML layer position calculation script
    
    ---------
    
    Co-authored-by: Sanjit Masanam <sanjitmasanam@gmail.com>

---
+++ Detectors/util/ecal_silicon_pos_calc.py
@@ -0,0 +1,46 @@
+import numpy
+
+# Constants
+PCB_dz = 1.666
+Glue_dz = 0.1
+Si_dz = 0.3
+GlueThick_dz = 0.2
+CarbonBasePlate_dz = 0.79
+MotherBoardAssembly_dz = 8.166
+CarbonCoolingPlane_dz = 5.7
+strongback_bar_dy = 25.4
+strongback_bar_dx = 838.2-0.6815
+PCB_Motherboard_Gap = 3.5
+FrontTolerance = 0.5
+BackTolerance = 0.5
+preshower_extra_air = 0.5
+bilayer_start = 240
+sampling_section_offset = 2*preshower_extra_air
+front_pre_tungsten_dz = 2.0
+cooling_pre_tungsten_dz = 2.0
+correction_constant = 0.15
+
+# Arrays
+layerZPositions = [7.582, 16.062, 33.226, 43.206, 60.370, 71.350, 90.014, 101.594, 120.258, 131.838, 150.502, 162.082, 180.746, 192.326, 210.990, 222.570, 241.234, 252.814, 271.478, 283.058, 301.722, 313.302, 331.966, 343.546, 365.710, 380.690, 402.854, 417.834, 439.998, 454.978, 477.142, 492.122, 514.286, 529.266]
+bilayer_absorber_cumulative = [0.0, 3.0, 8.0, 15.1, 22.2, 29.3, 36.4, 43.5, 50.6, 57.7, 64.8, 71.9, 85.9, 99.9, 113.9, 127.9, 141.9]
+front_tungsten_dz = [1.0, 2.0, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 7.0, 7.0, 7.0, 7.0, 7.0]
+cooling_tungsten_dz = [1.0, 1.5, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 3.5, 3.5, 3.5, 3.5, 3.5]
+
+# Calculated constants
+Flower_dz = PCB_dz+Glue_dz+Si_dz+GlueThick_dz+CarbonBasePlate_dz
+bilayer_noabsorber_thickness = FrontTolerance + PCB_dz + PCB_Motherboard_Gap + Flower_dz + CarbonCoolingPlane_dz + Flower_dz + PCB_Motherboard_Gap + PCB_dz + BackTolerance
+
+# Calculate positions of silicon modules
+first_sc_z_pos = bilayer_start + FrontTolerance + PCB_dz + PCB_Motherboard_Gap + PCB_dz + Glue_dz + correction_constant
+second_sc_z_pos = bilayer_start + FrontTolerance + PCB_dz + PCB_Motherboard_Gap + Flower_dz + preshower_extra_air + CarbonCoolingPlane_dz + CarbonBasePlate_dz + GlueThick_dz + correction_constant
+calc_layers = [first_sc_z_pos, second_sc_z_pos]
+for i in range(len(bilayer_absorber_cumulative)-1):
+    odd_layer = bilayer_start + sampling_section_offset + bilayer_noabsorber_thickness*(i+1) + bilayer_absorber_cumulative[i] + BackTolerance + front_tungsten_dz[i] + FrontTolerance + PCB_dz + PCB_Motherboard_Gap + PCB_dz + Glue_dz + correction_constant
+    even_layer = bilayer_start + sampling_section_offset + bilayer_noabsorber_thickness*(i+1) + bilayer_absorber_cumulative[i] + BackTolerance + front_tungsten_dz[i] + FrontTolerance + PCB_dz + PCB_Motherboard_Gap + Flower_dz + cooling_tungsten_dz[i] + CarbonCoolingPlane_dz + cooling_tungsten_dz[i] + CarbonBasePlate_dz + GlueThick_dz + correction_constant
+    calc_layers.append(odd_layer)
+    calc_layers.append(even_layer)
+
+# print results
+print("GDML | EcalGeometry.py | Diff")
+for i in range(len(layerZPositions)):
+    print(round(calc_layers[i] - 240, 3), round(layerZPositions[i], 3), round(numpy.abs(calc_layers[i] - layerZPositions[i] - 240), 3))
