a0a6d7c30a3e25f33aeb6ad14c9385262ca49911
Tamas Vami <Tamas.Almos.Vami@cern.ch>
2025-01-31 13:46:23 -0800

    Add tracking to signal CI

--- .github/validation_samples/inclusive/config.py
+++ .github/validation_samples/inclusive/config.py
@@ -61,7 +61,7 @@ count.input_pass_name = ''
 
 # Load ecal veto and use tracking in it
 ecalVeto = ecal_vetos.EcalVetoProcessor()
-ecalVeto.recoil_from_tracking = False
+ecalVeto.recoil_from_tracking = True 
 
 # Load HCAL veto
 import LDMX.Hcal.hcal as hcal
