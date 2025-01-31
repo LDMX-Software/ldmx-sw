50ba617be966b1d96664a0c509d4192850e0a368
Tamas Vami <tamas.almos.vami@cern.ch>
2025-01-30 21:05:08 -0800

    Update validate.sh

--- .github/actions/validate/validate.sh
+++ .github/actions/validate/validate.sh
@@ -42,11 +42,12 @@ __main__() {
 
   start_group Compare to Golden Histograms
   if [[ "${_no_comp}" == "false" ]]; then
+    LDMXSW_VERSION=$(cat $GITHUB_ACTION_PATH/../../../../VERSION)
     # assume sample directory has its gold histogram called 'gold.root'
     #   compare has 4 CLI inputs:
     #    gold_f, gold_label, test_f, test_label
     ldmx python3 $GITHUB_ACTION_PATH/compare.py \
-      gold.root ${LDMXSW_VERSION} hist.root ${GITHUB_REF} || return $?
+      gold.root $LDMXSW_VERSION hist.root ${GITHUB_REF} || return $?
 
     # print log diff into output directory
     cp -t ${_sample_dir}/plots gold.log output.log || return $?
