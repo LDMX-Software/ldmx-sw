ad2c7b57e47a2cb1705366977ae162756b88fd7f
Tamas Vami <tamas.almos.vami@cern.ch>
2025-02-06 14:12:24 -0800

    Change back to min_hits = 6

--- Tracking/python/full_tracking_sequence.py
+++ Tracking/python/full_tracking_sequence.py
@@ -81,7 +81,7 @@ tracking_recoil.seed_coll_name = seeder_recoil.out_seed_collection
 # tracking_recoil.seed_coll_name = "RecoilTruthSeeds"
 tracking_recoil.out_trk_collection = "RecoilTracks"
 tracking_recoil.measurement_collection = digi_recoil.out_collection
-tracking_recoil.min_hits = 8
+tracking_recoil.min_hits = 6
 tracking_recoil.outlier_pval_ =  22.1
 
 # Greedy ambiguity solver for the tagger
