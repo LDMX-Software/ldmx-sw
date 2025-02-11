f4474e1e81cd2a1427584dc47adba5cddde108e8
Tamas Vami <tamas.almos.vami@cern.ch>
2025-02-11 08:01:54 -0800

    Remove RecoilTrackerDQM (#1589)

--- DQM/python/dqm.py
+++ DQM/python/dqm.py
@@ -563,39 +563,6 @@ class PhotoNuclearDQM(ldmxcfg.Analyzer) :
                            "Recoil electron vertex y (mm)", 
                            320, -80, 80)
 
-class RecoilTrackerDQM(ldmxcfg.Analyzer) :
-    """Configured RecoilTrackerDQM python object
-    
-    Contains an instance of RecoilTrackerDQM that
-    has already been configured.
-    
-    Builds the necessary histograms as well.
-    
-    Examples
-    --------
-        from LDMX.DQM import dqm
-        p.sequence.append( dqm.RecoilTrackerDQM() )
-    """
-
-    def __init__(self,name='RecoilTracker') :
-        super().__init__(name, "dqm::RecoilTrackerDQM",'DQM')
-        
-        self.build1DHistogram("track_count", "Track Multiplicity", 10, 0, 10)
-        self.build1DHistogram("loose_track_count", "Track Multiplicity", 10, 0, 10)
-        self.build1DHistogram("axial_track_count", "Track Multiplicity", 10, 0, 10)
-        
-        self.build1DHistogram("recoil_vx", "Recoil e^{-} Vertex x (mm)", 120, -30, 30) 
-        self.build1DHistogram("recoil_vy", "Recoil e^{-} Vertex y (mm)", 200, -100, 100) 
-        self.build1DHistogram("recoil_vz", "Recoil e^{-} Vertex z (mm)", 40, -2, 0)
-        
-        titles = ['', '_track_veto', '_bdt', '_hcal', '_track_bdt', '_vetoes']
-        for t in titles: 
-            self.build1DHistogram("tp%s" % t,  "Recoil e^{-} Truth p (MeV)", 255, -50, 2500)
-            self.build1DHistogram("tpt%s" % t, "Recoil e^{-} Truth p_{t} (MeV)", 300, -50, 100)
-            self.build1DHistogram("tpx%s" % t, "Recoil e^{-} Truth p_{x} (MeV)", 100, -10, 10)
-            self.build1DHistogram("tpy%s" % t, "Recoil e^{-} Truth p_{y} (MeV)", 100, -10, 10)
-            self.build1DHistogram("tpz%s" % t, "Recoil e^{-} Truth p_{z} (MeV)", 260, -100, 2500)
-
 class TrkDeDxMassEstFeatures(ldmxcfg.Analyzer) :
     """Configured TrkDeDxMassEstFeatures python object
     
@@ -779,10 +746,6 @@ hcal_dqm = [
         HcalVetoResults(),
   ]
 
-recoil_dqm = [
-        RecoilTrackerDQM()
-        ]
-
 dEdx_dqm = [
         TrkDeDxMassEstFeatures()
         ]
@@ -806,4 +769,4 @@ trigger_dqm = [
         ]
 
 
-all_dqm = ecal_dqm + hcal_dqm + recoil_dqm + trigScint_dqm + trigger_dqm
+all_dqm = ecal_dqm + hcal_dqm + trigScint_dqm + trigger_dqm
