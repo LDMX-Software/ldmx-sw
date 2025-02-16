5049a7231676ee0a2a9908eb4a4a5a9e76a7715d
Tamas Vami <Tamas.Almos.Vami@cern.ch>
2025-02-15 20:55:56 -0800

    Move to a huge bias factor

--- SimCore/src/SimCore/BiasOperators/PhotoNuclear.cxx
+++ SimCore/src/SimCore/BiasOperators/PhotoNuclear.cxx
@@ -70,7 +70,7 @@ G4VBiasingOperation* PhotoNuclear::ProposeOccurenceBiasingOperation(
         emXsecUnbiased + pnXsecUnbiased_ - pnXsecBiased_, pnXsecUnbiased_);
     auto material = track->GetMaterial();
     if ((emXsecBiased == pnXsecUnbiased_) &&
-        material->GetName() == "G4_W0x270bda0") {
+        material->GetName() == "G4_W") {
       ldmx_log(warn) << "EM XS = PN unbiased XS! The biasing factor ("
                      << factor_ << ") is too large for particle with energy "
                      << track->GetKineticEnergy()
