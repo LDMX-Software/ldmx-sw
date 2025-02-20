5288d87664bf660a2b40fd0d0d73854448338142
Tamas Vami <tamas.almos.vami@cern.ch>
2025-02-20 07:53:47 -0800

    Improve TrigScint digi random number generation (#1614)
    * Improve TrigScint digi random number generation
    
    * Remove seed input from the CI test configs
    
    * Udate comments, remove adding overlayStr to clusters
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- .github/validation_samples/ecal_pn/config.py
+++ .github/validation_samples/ecal_pn/config.py
@@ -47,8 +47,12 @@ ts_digis = [
         TrigScintDigiProducer.pad2(),
         TrigScintDigiProducer.pad3(),
         ]
-for d in ts_digis :
-    d.randomSeed = 1
+
+ts_clusters = [
+        TrigScintClusterProducer.pad1(),
+        TrigScintClusterProducer.pad2(),
+        TrigScintClusterProducer.pad3(),
+        ]
 
 # Load electron counting and trigger
 from LDMX.Recon.electronCounter import ElectronCounter
@@ -81,9 +85,7 @@ p.sequence.extend([
         hcal_digi.HcalRecProducer(),
         hcal_veto,
         *ts_digis,
-        TrigScintClusterProducer.pad1(),
-        TrigScintClusterProducer.pad2(),
-        TrigScintClusterProducer.pad3(),
+        *ts_clusters,
         trigScintTrack,
         count, TriggerProcessor('trigger', 8000.),
         dqm.PhotoNuclearDQM(verbose=True),
