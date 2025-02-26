24ce73c083d2e271adf4491df4466a929ebb3c1c
Christian Herwig <therwig@users.noreply.github.com>
2025-02-26 10:06:37 -0500

    Additional information for 'dump files' used in firmware testbenchs (#1624)

--- Trigger/Algo/include/Trigger/DiscreteInputs.h
+++ Trigger/Algo/include/Trigger/DiscreteInputs.h
@@ -11,22 +11,48 @@ namespace trigger {
 namespace ldmx_int {
 
 struct EcalTP {
+  uint8_t tp;
   uint32_t tid;
-  uint32_t tp;  // store linear version
-  // uint8_t tp;
+  // extra data for added convenience
+  uint32_t tp_lin;
+  uint32_t layer;
+  uint32_t module;
+  uint32_t cell;
 
   bool operator<(const EcalTP &other) const { return tp > other.tp; }
-  void fill(int _tid, float _tp) {
+  void fill(int _tid, int _tp) {
     tid = _tid;
     tp = _tp;
+    // derived data, optional
+    layer = 0;
+    module = 0;
+    cell = 0;
+    tp_lin = 0;
+  }
+  void fill(int _tid, int _tp, int _layer, int _module, int _cell,
+            int _tp_lin) {
+    tid = _tid;
+    tp = _tp;
+    layer = _layer;
+    module = _module;
+    cell = _cell;
+    tp_lin = _tp_lin;
   }
   void writeToFile(FILE *file) const {
+    fwrite(&tp, sizeof(uint8_t), 1, file);
     fwrite(&tid, sizeof(uint32_t), 1, file);
-    fwrite(&tp, sizeof(uint32_t), 1, file);
+    fwrite(&layer, sizeof(uint32_t), 1, file);
+    fwrite(&module, sizeof(uint32_t), 1, file);
+    fwrite(&cell, sizeof(uint32_t), 1, file);
+    fwrite(&tp_lin, sizeof(uint32_t), 1, file);
   }
   void readFromFile(FILE *file) {
+    fread(&tp, sizeof(uint8_t), 1, file);
     fread(&tid, sizeof(uint32_t), 1, file);
-    fread(&tp, sizeof(uint32_t), 1, file);
+    fread(&layer, sizeof(uint32_t), 1, file);
+    fread(&module, sizeof(uint32_t), 1, file);
+    fread(&cell, sizeof(uint32_t), 1, file);
+    fread(&tp_lin, sizeof(uint32_t), 1, file);
   }
 };
 
