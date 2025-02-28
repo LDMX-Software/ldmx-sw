033c236c86386d207c9992de7a94357783dc94d1
tomeichlersmith <eichl008@umn.edu>
2025-02-28 16:52:27 -0600

    WIP: middle of Python refactor avoiding meaningless class
    - copy over refactor from LDMX-Softwar/fire and merge in our improvements
    - build into submodule
    - still need to test that it works after refactor and update to the
      newer Python C API

--- Framework/app/fire.cxx
+++ Framework/app/fire.cxx
@@ -12,7 +12,7 @@
 //-------------//
 //   ldmx-sw   //
 //-------------//
-#include "Framework/ConfigurePython.h"
+#include "Framework/Configure/Python.h"
 #include "Framework/Process.h"
 
 /**
@@ -71,9 +71,9 @@ int main(int argc, char* argv[]) try {
 
   framework::ProcessHandle p;
   try {
-    framework::ConfigurePython cfg(argv[ptrpy], argv + ptrpy + 1,
-                                   argc - ptrpy - 1);
-    p = cfg.makeProcess();
+    framework::config::Parameters config{
+      framework::config::run("ldmxcfg.Process.lastProcess", argv[ptrpy], argv + ptrpy + 1, argc - ptrpy - 1)};
+    p = std::make_unique<framework::Process>(config);
   } catch (const framework::exception::Exception& e) {
     // Error message currently printed twice since the stack trace code
     // sometimes crashes. Once this is fixed, the output above the stack trace
--- Framework/include/Framework/Configure/Python.h
+++ Framework/include/Framework/Configure/Python.h
@@ -1,88 +1,43 @@
 #ifndef FRAMEWORK_CONFIGUREPYTHON_H
 #define FRAMEWORK_CONFIGUREPYTHON_H
 
-/*~~~~~~~~~~~~~~~*/
-/*   Framework   */
-/*~~~~~~~~~~~~~~~*/
 #include "Framework/Configure/Parameters.h"
-#include "Framework/Process.h"
-
-namespace framework {
 
 /**
- * @class ConfigurePython
- * @brief Utility class which reads/executes a python script and creates a
- *        Process object based on the input.
- * @note The configuration language is documented in the overall Framework
- *       package documentation.
+ * python execution and parameter extraction
+ *
+ * this namespace is focused on holding the necessary
+ * functions to run and extract the configuration parameters
+ * from a python script. The documentation here is focused
+ * on the C++-side of the configuration - i.e. the translation
+ * of Python objects into their C++ counterparts.
  */
-class ConfigurePython {
- public:
-  /// the root configuration module name
-  static std::string root_module;
-  /// the root configuration class name
-  static std::string root_class;
-  /// the root configuration object name
-  static std::string root_object;
+namespace framework::config {
 
- public:
-  /**
-   * Class constructor.
-   *
-   * This method contains all the parsing and execution of the python script.
-   *
-   * @throw Exception if any necessary components of the python configuration
-   * are missing. e.g. The Process class or the different members of
-   * the lastProcess object.
-   *
-   * The basic premise of this constructor is to execute the python
-   * configuration script by loading it into python as a module.
-   * Then, **after the script has been executed**, all of the parameters
-   * for the Process and EventProcessors are gathered from python.
-   * The fact that the script has been executed means that the user
-   * can get up to a whole lot of shenanigans that can help them
-   * make their work more efficient.
-   *
-   * @param pythonScript Filename location of the python script.
-   * @param args Commandline arguments to be passed to the python script.
-   * @param nargs Number of commandline arguments.
-   */
-  ConfigurePython(const std::string& pythonScript, char* args[], int nargs);
+/**
+ * run the python script and extract the parameters
+ *
+ * This method contains all the parsing and execution of the python script.
+ *
+ * @throw Exception if the python script does not exit properly
+ * @throw Exception if any necessary components of the python configuration
+ * are missing. e.g. The Process class or the different members of
+ * the lastProcess object.
+ *
+ * The basic premise of this function is to execute the python
+ * configuration script. Then, **after the script has been executed**, 
+ * all of the parameters for the Process are gathered from python.
+ * The fact that the script has been executed means that the user
+ * can get up to a whole lot of shenanigans that can help them
+ * make their work more efficient.
+ *
+ * @param[in] full pythonic path to the object to kickoff extraction
+ * @param[in] pythonScript Filename location of the python script.
+ * @param[in] args Commandline arguments to be passed to the python script.
+ * @param[in] nargs Number of commandline arguments, assumed to be >= 0
+ */
+Parameters run(const std::string& root_object, const std::string& pythonScript, char* args[], int nargs);
 
-  /**
-   * Class destructor.
-   *
-   * Does nothing at the moment.
-   */
-  ~ConfigurePython() = default;
-
-  /**
-   * Create a process object based on the python file information
-   *
-   * No python parsing actually happens in this function.
-   * All of the parsing is done in the constructor, this just
-   * copies that information from configuration_ into the Process object.
-   *
-   * @return ProcessHandle handle to process that is configured and ready to go
-   */
-  ProcessHandle makeProcess();
-
-  /// Get a handle to the configuration
-  const framework::config::Parameters get() const { return configuration_; }
-
- private:
-  /**
-   * The entire configuration for this process
-   *
-   * Contains all parameters that were passed in the python
-   * in a recursive manner. For example, this would contain
-   * a key called 'sequence' that has a value of a vector
-   * of Parmaeters, each one corresponding to a processor.
-   */
-  framework::config::Parameters configuration_;
-
-};  // ConfigurePython
-
-}  // namespace framework
+}  // namespace framework::config
 
 #endif  // FRAMEWORK_CONFIGURE_PYTHON_H
