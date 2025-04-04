252a7f09dcfd032385efb1eac04643cf9329a0f8
bryngemark <lene.bryngemark@cern.ch>
2025-04-04 09:46:33 +0200

    Update PFTrackProducer.cxx
    add pass name in check printout

--- Recon/src/Recon/PFTrackProducer.cxx
+++ Recon/src/Recon/PFTrackProducer.cxx
@@ -18,7 +18,7 @@ double getP(const ldmx::SimTrackerHit& tk) {
 
 void PFTrackProducer::produce(framework::Event& event) {
   if (!event.exists(inputTrackCollName_, input_pass_name_)) {
-    ldmx_log(fatal) << "Couldn't find input collection " << inputTrackCollName_;
+    ldmx_log(fatal) << "Couldn't find input collection " << inputTrackCollName_ << "_" << input_pass_name;
     return;
   }
   const auto ecalSpHits = event.getCollection<ldmx::SimTrackerHit>(
