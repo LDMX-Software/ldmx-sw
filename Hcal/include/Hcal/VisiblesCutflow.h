1f4ce02725643d7bc8fb397bc295f6567face151
Tyler Horoho <tgh7hx@virginia.edu>
2025-05-05 13:18:33 -0400

    A class to run visibles analysis

---
+++ Hcal/include/Hcal/VisiblesCutflow.h
@@ -0,0 +1,45 @@
+#ifndef HCAL_VISIBLESCUTFLOW_H_
+#define HCAL_VISIBLESCUTFLOW_H_
+
+// LDMX
+#include "Framework/Configure/Parameters.h"
+#include "Framework/EventProcessor.h"
+#include "Tools/ONNXRuntime.h"
+
+namespace hcal {
+
+  class VisiblesCutflow : public framework::Analyzer {
+  public:
+    VisiblesCutflow(const std::string& name, framework::Process& process)
+      : Analyzer(name, process) {}
+
+    ~VisiblesCutflow() override = default;
+
+    void configure(framework::config::Parameters& parameters) override;
+
+    void analyze(const framework::Event& event) override;
+
+  private:
+
+    std::unique_ptr<ldmx::Ort::ONNXRuntime> rt_;
+    double bdtCutVal_{0.};
+    std::string featureListName_;
+
+    double beamEnergyMeV_{0.};
+
+    std::string hcal_rec_collection_;
+    std::string ecal_rec_collection_;
+    bool recoil_from_tracking_{false};
+    std::string track_collection_;
+    std::string sp_collection_;
+    std::string ecal_veto_collection_;
+    std::string ecal_veto_pass_;
+    double ecalbdtCutVal_{0.};
+
+    bool in_list(std::vector<int> parents, int a);
+
+  };
+  
+} // namespace hcal
+
+#endif
