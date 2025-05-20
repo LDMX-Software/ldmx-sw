a939cad2a1e9a9157afeef2af689cf8e749d14bf
Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
2025-05-20 16:25:31 -0500

    use docker as our runner

--- .github/validation_samples/signal/init.sh
+++ .github/validation_samples/signal/init.sh
@@ -9,6 +9,8 @@
 
 start_group Produce Dark Brem Library
 curl -s https://tomeichlersmith.github.io/denv/install | sh
+# use docker so we don't have to tell podman to look at the docker.io registry
+export DENV_RUNNER=docker
 denv init ldmx/dark-brem-lib-gen:v5.1
 denv dark-brem-lib-gen \
   --run 1 \
