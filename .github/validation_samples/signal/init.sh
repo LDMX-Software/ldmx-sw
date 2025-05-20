5091ca6c4428ea58ba3468acce32ffea9b7575fd
Tamas Vami <tamas.almos.vami@cern.ch>
2025-05-20 16:53:00 -0700

    Explicitly include minor version and change to the new argument name

--- .github/validation_samples/signal/init.sh
+++ .github/validation_samples/signal/init.sh
@@ -11,10 +11,10 @@ start_group Produce Dark Brem Library
 curl -s https://tomeichlersmith.github.io/denv/install | sh
 # use docker so we don't have to tell podman to look at the docker.io registry
 export DENV_RUNNER=docker
-denv init ldmx/dark-brem-lib-gen:v5.1
+denv init ldmx/dark-brem-lib-gen:v5.1.0
 denv dark-brem-lib-gen \
   --run 1 \
-  --max_energy 8.0 \
+  --max-energy 8.0 \
   --apmass 0.01
 # cleanup denv
 rm -r .denv
