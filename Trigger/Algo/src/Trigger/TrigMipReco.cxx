20516f6db4044aa2531ba3afcc845b5ecab086e7
Charles Bell <bell.charles1402@gmail.com>
2025-06-27 13:37:52 -0400

    cleanup debug comments

--- Trigger/Algo/src/Trigger/TrigMipReco.cxx
+++ Trigger/Algo/src/Trigger/TrigMipReco.cxx
@@ -84,143 +84,103 @@ void TrigMipReco::produce(framework::Event& event) {
   } else { ///////////////////////////////////// ECAL MIP Reconstruction ///////////////////////////////////////////////
     float radiusCut = 50;  // mm
     int maxLayer = 32;
-    int minTrackLength = 5;  // example
+    int minTrackLength = 5;  // Adjustable
+    const float radiusCut2 = radiusCut * radiusCut;
+
 
     std::map<int, std::vector<TrigCaloHit>> layerHits;
     std::set<const TrigCaloHit*> usedHits;
     std::vector<std::vector<const TrigCaloHit*>> candidateTracks;
     std::map<const TrigCaloHit*, size_t> hitToBestTrack;
 
-    // Filter for section = 0, energy range, and hits < 33 layers
+    // Filter for section = 0 and hits < 33 layers
     for (const auto& hit : caloHits) {
-      // if (hit.section() > 0 || hit.energy() < minEnergy_ ||
-      //     hit.energy() > maxEnergy_ || hit.layer() > maxLayer)
-      //   continue;
       if (hit.section() > 0 || hit.layer() > maxLayer)
         continue;
       layerHits[hit.layer()].push_back(hit);
     }
 
-    // std::map<int, int> seedLayerCounts;
     // Find mip seeds
     for (const auto& [seedLayer, seeds] : layerHits) {
       for (const auto& seed : seeds) {
-        if (usedHits.count(&seed) || seed.energy() < minEnergy_ || seed.energy() > maxEnergy_) continue;  // Skip if hit already used
+        // Skip if hit already used or outside mip energy range
+        if (usedHits.count(&seed) || seed.energy() < minEnergy_ || seed.energy() > maxEnergy_) continue;
 
         std::vector<const TrigCaloHit*> track;
         track.push_back(&seed);
 
         const TrigCaloHit* last = &seed;  // Most recent hit in track
-        int holes = 0;
 
         // Look layer by layer for next hit within dR
         for (int l = seed.layer() + 1; l <= maxLayer; ++l) {
           const TrigCaloHit* bestHit = nullptr;
-          float bestdR2 = radiusCut * radiusCut;
+          float bestdR2 = radiusCut2;
 
           for (const auto& cand : layerHits[l]) {
             if (usedHits.count(&cand) || cand.energy() < minEnergy_ || cand.energy() > maxEnergy_) continue;
 
-            // corrects for layer shift in x-direction, we calculated that the
-            // shift is 4.82 mm
+            // Corrects for layer shift in x-direction which we calculated is 4.82 mm
             float layerShiftLast = (last->layer() % 2 == 0) ? 0.0f : 4.82f;
             float layerShiftCand = (cand.layer() % 2 == 0) ? 0.0f : 4.82f;
 
-            float dx =
-                (cand.x() - layerShiftCand) - (last->x() - layerShiftLast);
+            float dx = (cand.x() - layerShiftCand) - (last->x() - layerShiftLast);
             float dy = cand.y() - last->y();
             float dR2 = dx * dx + dy * dy;
             if (dR2 < bestdR2) {
               bestdR2 = dR2;
-              bestHit = &cand;  // closest unused hit in next layer
+              bestHit = &cand;  // Closest unused hit in next layer
             }
           }
 
           if (bestHit) {
-            track.push_back(bestHit);  // builds track from best hits
+            track.push_back(bestHit);  // Builds track from best hits
             last = bestHit;
-          } else {
-            holes++;
-            // if (holes > 5) break; // break if six or more holes
           }
         }
 
-        // std::cout << "  [DEBUG] Mip num holes = " << holes
-        //         << "; Mip length = " << track.size()
-        //         << "; Mip hole fraction = " << static_cast<float>(holes) / track.size() << "\n";
-        // std::cout << "\n[DEBUG] This is a new track\n";
         bool isIsolated = true;
         float isolationECut = 20; // MeV
         float isolationRadius2 = radiusCut*radiusCut;
         if ((track.size() >= minTrackLength)) {
-          //  && ((static_cast<float>(holes) / track.size()) <= 0.2)
-          // bool isIsolated = true;
-          // float isolationECut = 5; // MeV
-          // float isolationRadius2 = radiusCut*radiusCut;
-          // float totalIsolationESum = 0.0f;
-
           for (const auto* hit : track) { // Isolation area energy check
             int layer = hit->layer();
             float hitx = hit->x();
             float hity = hit->y();
-            // float layerShiftHit = (layer % 2 == 0) ? 0.0f : 4.82f;
-
             float sumE = 0.0f;
 
-            // std::cout << "[DEBUG] Hit energy = " << hit->energy()
-            //               << " at layer " << layer
-            //               << " (x, y) = (" << hitx << ", " << hity << ")\n";
-
             for (const auto& cand : layerHits[layer]) {
-                if (&cand == hit) continue; // skips self
+                if (&cand == hit) continue; // Skips self
                 float dx = cand.x() - hitx;
                 float dy = cand.y() - hity;
                 float dR2 = dx * dx + dy * dy;
 
                 if (dR2 < isolationRadius2) {
-
-                    // std::cout << "  [DEBUG] Nearby Cand energy = " << cand.energy()
-                    //           << " at layer " << cand.layer()
-                    //           << " (x, y) = (" << cand.x() << ", " << cand.y() << ") "
-                    //           << "dR = " << dR2 << "\n";
-
                     sumE += cand.energy();
                 }
             }
-            // totalIsolationESum +=sumE
 
-            // std::cout << "  [DEBUG] Sum of energy of surrounding candidates: " << sumE << "\n";
             if (sumE >= isolationECut) {
             isIsolated = false;
-            usedHits.insert(hit);  // adds used hits to vector so they cannot be used again
+            usedHits.insert(hit);  // Adds used hits to vector so they cannot be used again
             break;
             }
           }
           if (!isIsolated) {
-            continue; // reject track if any hit is not isolated
+            continue; // Reject track if any hit is not isolated
           }
 
           size_t i = candidateTracks.size();
           candidateTracks.push_back(track);
 
-          // ensures that only the longest track per event is kept
           for (const auto* hit : track) {
-            if (!hitToBestTrack.count(hit) ||
-                candidateTracks[i].size() >
-                    candidateTracks[hitToBestTrack[hit]].size()) {
+            if (!hitToBestTrack.count(hit) || candidateTracks[i].size() > candidateTracks[hitToBestTrack[hit]].size()) {
               hitToBestTrack[hit] = i;
-              usedHits.insert(hit);  // adds used hits to vector so they cannot be used again
+              usedHits.insert(hit);  // Adds used hits to vector so they cannot be used again
             }
           }
         }
       }
     }
-
-    // std::cout << "\n=== Seed Counts by Layer ===\n";
-    // for (int layer = 0; layer <= maxLayer; ++layer) {
-    //     int count = seedLayerCounts.count(layer) ? seedLayerCounts[layer] : 0;
-    //     std::cout << "Layer " << layer << ": " << count << " seeds\n";
-    // }
 
     std::set<size_t> validTrackIDs;
     for (const auto& [hit, idx] : hitToBestTrack) {
@@ -239,10 +199,7 @@ void TrigMipReco::produce(framework::Event& event) {
       mip.setLength(track.back()->layer() - track.front()->layer());
       mip.setNHoles(mip.length() - mip.nHits());
 
-      // std::cout << "  \n[DEBUG] Mip num holes = " << mip.nHoles()
-      //           << "; Mip length = " << mip.length()
-      //           << "; Mip hole fraction = " << static_cast<float>(mip.nHoles()) / mip.length() << "\n";
-      if ((static_cast<float>(mip.nHoles()) / mip.length()) >= 0.2) continue; // remove mips with hole fraction > 0.2
+      if ((static_cast<float>(mip.nHoles()) / mip.length()) >= 0.2) continue; // Remove mips with hole fraction > 0.2
 
       float totalIsolationESum = 0.0f;
       for (const auto* hit : track) {
@@ -266,31 +223,6 @@ void TrigMipReco::produce(framework::Event& event) {
 
     event.add(passCollName_, mips);
   }
-
-  // std::sort(sortedHits.begin(), sortedHits.end(),
-  // 	    [](TrigCaloHit a, TrigCaloHit b) {
-  // 	      return a.layer() > b.layer();
-  // 	    });
-
-  // run inside-out tracking
-  // std::cout << "new"() << std::endl;
-  // for (const auto& tp : caloHits) {
-  //   std::cout << tp.layer() << std::endl;
-
-  // }
-
-  // double x{0}, y{0}, z{0}; // todo
-  // ldmx::HcalTriggerID combo_id(tp.getId());
-
-  // int adc = tp.getPrimitive();
-  // double e = adc * 1.2 / 72.961; // ADC to MeV
-  // passTrigHits.emplace_back(x, y, z, e);
-
-  // passTrigHits.back().setLayer(combo_id.layer());
-  // passTrigHits.back().setStrip(combo_id.superstrip());
-  // passTrigHits.back().setSection(combo_id.section());
-
-  // event.add(passCollName_ + "Hits", passTrigHits);
 }
 
 }  // namespace trigger
