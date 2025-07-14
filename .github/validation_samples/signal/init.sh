c6ab6d7d4273513c473f1c39febca573dbbfc191
Tamas Vami <tamas.almos.vami@cern.ch>
2025-07-14 07:12:22 -0700

    Update dark-brem-lib-gen to use v5.2.0 (#1769)

--- .github/validation_samples/signal/init.sh
+++ .github/validation_samples/signal/init.sh
@@ -11,7 +11,7 @@ start_group Produce Dark Brem Library
 curl -s https://tomeichlersmith.github.io/denv/install | sh
 # use docker so we don't have to tell podman to look at the docker.io registry
 export DENV_RUNNER=docker
-denv init ldmx/dark-brem-lib-gen:v5.1.0
+denv init ldmx/dark-brem-lib-gen:v5.2.0
 denv dark-brem-lib-gen \
   --run 1 \
   --max-energy 8.0 \
