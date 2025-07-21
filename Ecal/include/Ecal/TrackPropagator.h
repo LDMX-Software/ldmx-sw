aa71d2342e64acbbbffae12343fb0723a5c922d5
Azuya334 <115593847+Azuya334@users.noreply.github.com>
2025-07-21 09:24:06 -0700

    Allows using tracking for ParticleNet inputs (#1778)
    * Allows using tracking for ParticleNet inputs
    
    * Refactor trackProp function
    
    * Dont read the event in in the makeInput, but rather prep the input in advance and pass that
    
    * Dont have trackProp in a class
    
    ---------
    
    Co-authored-by: Tamas Vami <Tamas.Almos.Vami@cern.ch>

---
+++ Ecal/include/Ecal/TrackPropagator.h
@@ -0,0 +1,33 @@
+
+/**
+ * @file TrackPropagator.h
+ * @brief Class that propagates tracks to the ECAL face
+ * @author Tamas Vami (UCSB)
+ */
+
+#ifndef ECAL_TRACKPROPAGATOR_H_
+#define ECAL_TRACKPROPAGATOR_H_
+
+// LDMX
+#include "Tracking/Event/Track.h"
+
+// C++
+#include <cmath>
+
+namespace ecal {
+
+/**
+ * Return a vector of parameters for a propagated recoil track
+ * @param[in] tracks The track collection
+ * @param[in] ts_type The track state type, i.e. tracks state at the ECAL face
+ * @param[in] ts_title The track state title, most likely "ecal"
+ * @returns Vector of parameters for a propagated recoil track
+ */
+
+std::vector<float> trackProp(const ldmx::Tracks& tracks,
+                             ldmx::TrackStateType ts_type,
+                             const std::string& ts_title);
+
+}  // namespace ecal
+
+#endif
