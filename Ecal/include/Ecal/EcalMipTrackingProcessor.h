37da363b857a4001c2da5160ea1a8a7007ccb0ea
JYoo001 <yoojj0517@gmail.com>
2025-07-23 09:24:31 -0700

    Remove function declarations from headers

--- Ecal/include/Ecal/EcalMipTrackingProcessor.h
+++ Ecal/include/Ecal/EcalMipTrackingProcessor.h
@@ -73,39 +73,6 @@ class EcalMipTrackingProcessor : public framework::Producer {
  private:
   void clearProcessor();
 
-  // MIP tracking
-  /**
-   * Returns the distance between the lines v and w, with v defined to pass
-   * through the points (v1,v2) (and similarly for w).
-   *
-   * @param[in] v1 An arbitrary point on line v
-   * @param[in] v2 A second, distinct point on line v
-   * @param[in] w1 An arbitrary point on line w
-   * @param[in] w2 A second, distinct point on line w
-   * @returns Closest distance of approach of lines u and v
-   */
-  float distTwoLines(ROOT::Math::XYZVector v1, ROOT::Math::XYZVector v2,
-                     ROOT::Math::XYZVector w1, ROOT::Math::XYZVector w2);
-  /**
-   * Return the minimum distance between the point h1 and the line passing
-   * through points p1 and p2.
-   *
-   * @param[in] h1 Point to find the distance to
-   * @param[in] p1 An arbitrary point on the line
-   * @param[in] p2 A second, distinct point on the line
-   * @returns Minimum distance between h1 and the line
-   */
-  float distPtToLine(ROOT::Math::XYZVector h1, ROOT::Math::XYZVector p1,
-                     ROOT::Math::XYZVector p2);
-
-  /**
-   * Return a vector of parameters for a propagated recoil track
-   * @param[in] tracks The track collection
-   * @param[in] ts_type The track state type, i.e. tracks state at the ECAL face
-   * @param[in] ts_title The track state title, most likely "ecal"
-   * @returns Vector of parameters for a propagated recoil track
-   */
-
  private:
   int nevents_{0};
   double processing_time_{0.};
