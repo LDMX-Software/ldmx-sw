3ff937c97a7a254b00ff7ea0f9b6a63505a3888e
Tamas Vami <Tamas.Almos.Vami@cern.ch>
2025-07-29 16:14:12 -0700

    Add distance measures to EcalClusterAnalyzer DQM

--- DQM/src/DQM/EcalClusterAnalyzer.cxx
+++ DQM/src/DQM/EcalClusterAnalyzer.cxx
@@ -1,37 +1,22 @@
-/**
- * @file EcalClusterAnalyzer.cxx
- * @brief Analysis of cluster performance
- * @author Ella Viirola, Lund University
- */
-
 #include "DQM/EcalClusterAnalyzer.h"
-
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-
-#include "DetDescr/SimSpecialID.h"
-#include "Ecal/Event/EcalCluster.h"
-#include "Ecal/Event/EcalHit.h"
-#include "SimCore/Event/SimCalorimeterHit.h"
-#include "SimCore/Event/SimTrackerHit.h"
 
 namespace dqm {
 
 void EcalClusterAnalyzer::configure(framework::config::Parameters& ps) {
-  nbr_of_electrons_ = ps.getParameter<int>("nbr_of_electrons");
+  use_simulated_electron_number_ =
+      ps.get<bool>("use_simulated_electron_number");
+  nbr_of_electrons_ = ps.get<int>("nbr_of_electrons");
 
-  ecal_sim_hit_coll_ = ps.getParameter<std::string>("ecal_sim_hit_coll");
-  ecal_sim_hit_pass_ = ps.getParameter<std::string>("ecal_sim_hit_pass");
+  ecal_sim_hit_coll_ = ps.get<std::string>("ecal_sim_hit_coll");
+  ecal_sim_hit_pass_ = ps.get<std::string>("ecal_sim_hit_pass");
 
-  rec_hit_coll_name_ = ps.getParameter<std::string>("rec_hit_coll_name");
-  rec_hit_pass_name_ = ps.getParameter<std::string>("rec_hit_pass_name");
+  rec_hit_coll_name_ = ps.get<std::string>("rec_hit_coll_name");
+  rec_hit_pass_name_ = ps.get<std::string>("rec_hit_pass_name");
 
-  cluster_coll_name_ = ps.getParameter<std::string>("cluster_coll_name");
-  cluster_pass_name_ = ps.getParameter<std::string>("cluster_pass_name");
+  cluster_coll_name_ = ps.get<std::string>("cluster_coll_name");
+  cluster_pass_name_ = ps.get<std::string>("cluster_pass_name");
 
-  ecal_sp_hits_passname_ =
-      ps.getParameter<std::string>("ecal_sp_hits_passname");
+  ecal_sp_hits_passname_ = ps.get<std::string>("ecal_sp_hits_passname");
   return;
 }
 
@@ -43,45 +28,70 @@ void EcalClusterAnalyzer::analyze(const framework::Event& event) {
   const auto& ecal_clusters{event.getCollection<ldmx::EcalCluster>(
       cluster_coll_name_, cluster_pass_name_)};
 
-  if (ecal_clusters.size() == nbr_of_electrons_)
-    histograms_.fill("correctly_predicted_events", 1);  // correct
-  else if (ecal_clusters.size() < nbr_of_electrons_)
-    histograms_.fill("correctly_predicted_events", 0);  // undercounting
-  else if (ecal_clusters.size() > nbr_of_electrons_)
-    histograms_.fill("correctly_predicted_events", 2);  // overcounting
+  // Determine the number of recoil electrons in the event
+  // By default from the TS track counting
+  int nbr_of_electrons{event.getElectronCount()};
+  // If configured to use the simulated electron number, use that instead
+  if (use_simulated_electron_number_) {
+    nbr_of_electrons = nbr_of_electrons_;
+  }
 
-  std::unordered_map<int, std::pair<int, std::vector<double>>> hitInfo;
-  hitInfo.reserve(ecal_rec_hits.size());
+  int n_ecal_clusters = ecal_clusters.size();
+  ldmx_log(info) << "Number of ECal CLUE clusters: " << n_ecal_clusters
+                 << ", Number of electrons: " << nbr_of_electrons;
+  // Fill histograms with the number of clusters
+  histograms_.fill("number_of_clusters", n_ecal_clusters);
+  // Fill simplied 3-bin histogram to check the prediction
+  if (n_ecal_clusters == nbr_of_electrons) {
+    // correct
+    histograms_.fill("correctly_predicted_events", 1);
+  } else if (n_ecal_clusters < nbr_of_electrons) {
+    // undercounting
+    histograms_.fill("correctly_predicted_events", 0);
+  } else if (n_ecal_clusters > nbr_of_electrons) {
+    // overcounting
+    histograms_.fill("correctly_predicted_events", 2);
+  }
 
-  double dist;
-  if (nbr_of_electrons_ == 2) {
-    // Measures distance between two electrons in the ECal scoring plane
+  std::unordered_map<int, std::pair<int, std::vector<double>>> hit_info;
+  hit_info.reserve(ecal_rec_hits.size());
+
+  // Determine the truth information for the recoil electron
+  std::vector<std::vector<float>> sp_electron_positions;
+  const auto& ecal_sp_hits{event.getCollection<ldmx::SimTrackerHit>(
+      "EcalScoringPlaneHits", ecal_sp_hits_passname_)};
+
+  std::vector<ldmx::SimTrackerHit> sorted_sp_hits = ecal_sp_hits;
+  std::sort(sorted_sp_hits.begin(), sorted_sp_hits.end(),
+            [](const ldmx::SimTrackerHit& a, const ldmx::SimTrackerHit& b) {
+              return a.getTrackID() < b.getTrackID();
+            });
+
+  // Collect positions of all electrons on the SP
+  for (const ldmx::SimTrackerHit& sp_hit : sorted_sp_hits) {
+    if (sp_hit.getTrackID() <= nbr_of_electrons) {
+      sp_electron_positions.push_back(sp_hit.getPosition());
+    }
+  }
+
+  double sp_ele_dist{9999.};
+  if (nbr_of_electrons == 2) {
+    // Measures sp_ele_distance between two electrons in the ECal scoring plane
     // TODO: generalize for n electrons
     std::vector<float> pos1;
     std::vector<float> pos2;
-    bool p1 = false;
-    bool p2 = false;
+    pos1 = sp_electron_positions[0];
+    pos2 = sp_electron_positions[1];
+    sp_ele_dist = std::sqrt((pos1[0] - pos2[0]) * (pos1[0] - pos2[0]) +
+                            (pos1[1] - pos2[1]) * (pos1[1] - pos2[1]));
 
-    const auto& ecal_sp_hits{event.getCollection<ldmx::SimTrackerHit>(
-        "EcalScoringPlaneHits", ecal_sp_hits_passname_)};
-    for (const ldmx::SimTrackerHit& spHit : ecal_sp_hits) {
-      if (spHit.getTrackID() == 1) {
-        pos1 = spHit.getPosition();
-        p1 = true;
-      } else if (spHit.getTrackID() == 2) {
-        pos2 = spHit.getPosition();
-        p2 = true;
-      }
-    }
-    if (p1 && p2)
-      dist = std::sqrt(std::pow((pos1[0] - pos2[0]), 2) +
-                       std::pow((pos1[1] - pos2[1]), 2));
-  }
+  }  // end block about the scoring plane hits
 
+  // Loop over the rechits and find the matching simhits
   for (const auto& hit : ecal_rec_hits) {
     auto it = std::find_if(
         ecal_sim_hits.begin(), ecal_sim_hits.end(),
-        [&hit](const auto& simHit) { return simHit.getID() == hit.getID(); });
+        [&hit](const auto& sim_hit) { return sim_hit.getID() == hit.getID(); });
     if (it != ecal_sim_hits.end()) {
       // if found a simhit matching this rechit
       int ancestor = 0;
@@ -89,14 +99,14 @@ void EcalClusterAnalyzer::analyze(const framework::Event& event) {
       bool tagged = false;
       int tag = 0;
       std::vector<double> edep;
-      edep.resize(nbr_of_electrons_ + 1);
+      edep.resize(nbr_of_electrons + 1);
       for (int i = 0; i < it->getNumberOfContribs(); i++) {
         // for each contrib in this simhit
-        const auto& c = it->getContrib(i);
+        const auto& contrib = it->getContrib(i);
         // get origin electron ID
-        ancestor = c.originID;
+        ancestor = contrib.originID;
         // store energy from this contrib at index = origin electron ID
-        if (ancestor <= nbr_of_electrons_) edep[ancestor] += c.edep;
+        if (ancestor <= nbr_of_electrons) edep[ancestor] += contrib.edep;
         if (!tagged && i != 0 && prevAncestor != ancestor) {
           // if origin electron ID does not match previous origin electron ID
           // this hit has contributions from several electrons, ie mixed case
@@ -110,77 +120,113 @@ void EcalClusterAnalyzer::analyze(const framework::Event& event) {
         tag = prevAncestor;
       }
       histograms_.fill("ancestors", tag);
-      hitInfo.insert({hit.getID(), std::make_pair(tag, edep)});
-    }
-  }
+      hit_info.insert({hit.getID(), std::make_pair(tag, edep)});
+    }  // end if simhit found
+  }  // end loop on the rechits
 
-  int clusteredHits = 0;
-
+  // Loop over the clusters
+  int clustered_hits = 0;
   for (const auto& cl : ecal_clusters) {
+    auto cluster_centroid_x = cl.getFirstLayerCentroidX();
+    auto cluster_centroid_y = cl.getFirstLayerCentroidY();
+
+    // Find the closest sp_electron_positions to the cluster centroid
+    double min_distance = 9999.;
+    double sp_clue_x_residuals = 9999.;
+    double sp_clue_y_residuals = 9999.;
+    for (const auto& sp_pos : sp_electron_positions) {
+      double distance = std::sqrt(
+          (sp_pos[0] - cluster_centroid_x) * (sp_pos[0] - cluster_centroid_x) +
+          (sp_pos[1] - cluster_centroid_y) * (sp_pos[1] - cluster_centroid_y));
+      if (distance < min_distance) {
+        min_distance = distance;
+        sp_clue_x_residuals = sp_pos[0] - cluster_centroid_x;
+        sp_clue_y_residuals = sp_pos[1] - cluster_centroid_y;
+      }
+    }
+    // Fill histogram with the distance to the closest scoring plane electron
+    ldmx_log(trace) << "Cluster centroid: (" << cluster_centroid_x << ", "
+                    << cluster_centroid_y
+                    << "), min distance to SP electron: " << min_distance;
+    histograms_.fill("sp_clue_distance", min_distance);
+    histograms_.fill("sp_clue_x_residual", sp_clue_x_residuals);
+    histograms_.fill("sp_clue_y_residual", sp_clue_y_residuals);
+
     // for each cluster
     // total number of hits coming from electron, index = electron ID
-    std::vector<double> n;
-    n.resize(nbr_of_electrons_ + 1);
+    std::vector<double> n_hits_from_electron;
+    n_hits_from_electron.resize(nbr_of_electrons + 1);
     // total number of energy coming from electron, index = electron ID
-    std::vector<double> e;
-    e.resize(nbr_of_electrons_ + 1);
-    double eSum = 0.;
-    double nSum = 0.;
+    std::vector<double> energy_from_electron;
+    energy_from_electron.resize(nbr_of_electrons + 1);
+    double energy_sum = 0.;
+    double n_sum = 0.;
 
-    const auto& hitIDs = cl.getHitIDs();
-    for (const auto& id : hitIDs) {
+    const auto& hit_ids = cl.getHitIDs();
+    for (const auto& id : hit_ids) {
       // for each hit in cluster, find previously stored info
-      auto it = hitInfo.find(id);
-      if (it != hitInfo.end()) {
+      auto it = hit_info.find(id);
+      if (it != hit_info.end()) {
         auto t = it->second;
-        auto eId = t.first;        // origin electron ID (or 0 for mixed)
-        auto energies = t.second;  // energy vector
-        n[eId]++;  // increment number of hits coming from this electron
-        nSum++;
+        // origin electron ID (or 0 for mixed)
+        auto id_electron = t.first;
+        // energy vector
+        auto energies = t.second;
+        // increment number of hits coming from this electron
+        n_hits_from_electron[id_electron]++;
+        n_sum++;
 
-        double hitESum = 0.;
-        for (int i = 1; i < nbr_of_electrons_ + 1; i++) {
+        double hit_energy_sum = 0.;
+        for (int i = 1; i < nbr_of_electrons + 1; i++) {
           // loop through energy vector
           if (energies[i] > 0.) {
-            hitESum += energies[i];
+            energy_sum += energies[i];
             // add energy from electron i in this hit to total energy from
             // electron i in cluster
-            e[i] += energies[i];
+            energy_from_electron[i] += energies[i];
           }
         }
         // if mixed hit, add the total energy of this hit to mixed hit energy
         // counter
-        if (eId == 0) e[0] += hitESum;
-        eSum += hitESum;
+        if (id_electron == 0) energy_from_electron[0] += hit_energy_sum;
+        energy_sum += hit_energy_sum;
 
-        clusteredHits++;
-      }
-    }
+        clustered_hits++;
+      }  // end if hit info found
+    }  // end loop on the hit IDs in the cluster
 
-    if (eSum > 0) {
+    if (energy_sum > 0) {
       // get largest energy contribution
-      double eMax = *max_element(e.begin(), e.end());
+      double max_energy_contribution = *max_element(
+          energy_from_electron.begin(), energy_from_electron.end());
       // energy purity = largest contribution / all energy
-      histograms_.fill("energy_percentage", 100. * (eMax / eSum));
-      if (e[0] > 0.) histograms_.fill("mixed_hit_energy", 100. * (e[0] / eSum));
+      histograms_.fill("energy_percentage",
+                       100. * (max_energy_contribution / energy_sum));
+      if (energy_from_electron[0] > 0.)
+        histograms_.fill("mixed_hit_energy",
+                         100. * (energy_from_electron[0] / energy_sum));
 
-      histograms_.fill("total_energy_vs_hits", eSum, cl.getHitIDs().size());
-      histograms_.fill("total_energy_vs_purity", eSum, 100. * (eMax / eSum));
+      histograms_.fill("total_energy_vs_hits", energy_sum,
+                       cl.getHitIDs().size());
+      histograms_.fill("total_energy_vs_purity", energy_sum,
+                       100. * (max_energy_contribution / energy_sum));
 
-      if (nbr_of_electrons_ == 2)
-        histograms_.fill("distance_energy_purity", dist, 100. * (eMax / eSum));
+      if (nbr_of_electrons == 2)
+        histograms_.fill("sp_ele_distance_energy_purity", sp_ele_dist,
+                         100. * (max_energy_contribution / energy_sum));
     }
-    if (nSum > 0) {
-      double nMax = *max_element(n.begin(), n.end());
-      histograms_.fill("same_ancestor", 100. * (nMax / nSum));
+    if (n_sum > 0) {
+      double n_max = *max_element(n_hits_from_electron.begin(),
+                                  n_hits_from_electron.end());
+      histograms_.fill("same_ancestor", 100. * (n_max / n_sum));
     }
-  }
+  }  // end loop on the clusters
 
-  histograms_.fill("clusterless_hits", (ecal_rec_hits.size() - clusteredHits));
+  histograms_.fill("clusterless_hits", (ecal_rec_hits.size() - clustered_hits));
   histograms_.fill("total_rechits_in_event", ecal_rec_hits.size());
   histograms_.fill(
       "clusterless_hits_percentage",
-      100. * (ecal_rec_hits.size() - clusteredHits) / ecal_rec_hits.size());
+      100. * (ecal_rec_hits.size() - clustered_hits) / ecal_rec_hits.size());
 }
 
 }  // namespace dqm
