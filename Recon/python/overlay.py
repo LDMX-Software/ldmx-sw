91a62e100bfa9f04200d4f9e480a1d8951b9e5fd
Tamas Vami <Tamas.Almos.Vami@cern.ch>
2025-07-31 20:25:04 -0700

    Misc improvements to OverlayProducer

--- Recon/python/overlay.py
+++ Recon/python/overlay.py
@@ -2,48 +2,48 @@
 
 Parameters
 ----------------
-fileName : string
+file_name : string
      The name of the file containing the pileup events to overlay on the sim event
 
 Attributes:
 -------------
-passName : string
+sim_passname : string
     Pass name of the sim events 
-overlayPassName : string
+overlay_passname : string
     Pass name of the pileup events 
-overlayCaloHitCollections : string
+calo_collections : string
     List of SimCalorimeterHit collections to pull from the sim and pileup events and combine
-overlayTrackerHitCollections : string
+tracker_collections_ : string
     List of SimTrackerHit collections to pull from the sim and pileup events and combine
-totalNumberOfInteractions : int 
+poisson_mu : int 
     The total number of interactions combined (including the sim event)
-doPoissonIntime : bool
+do_poisson_in_time : bool
     Specifies whether to sample a Poisson(totalNumberOfInteractions) to obtain the number of events to combine in-time 
     with the sim event (doPoissonIntime=true), 
     or deterministically set nOverlay=totalNumberOfInteractions-1 (doPoissonIntime = false)
-doPoissonOutoftime : bool
+do_poisson_out_of_time : bool
     Specifies whether to sample a Poisson(totalNumberOfInteractions) to obtain the number of events to put in 
     bunches that are out-of-time with the sim event (doPoissonOutoftime=true), 
     or deterministically set nOverlay=totalNumberOfInteractions-1 (doPoissonOutoftime = false)
-timeSpread : float
+time_sigma : float
     The width of a single bunch in time (expressed in sigma) [ns]
-timeMean : float
+time_mean : float
     The average time, relative to the sim time, of the pileup events [ns]. 
     Note that this should generally be 0. A non-zero number combined with a 0 spread is useful for debugging.
-nEarlierBunchesToSample : int
+n_earlier : int
     The number of preceding bunches sampled for out-of-time pileup. 
     Furthermore, pileup will be uniformly distributed among this number of bunches m = -N_earlier, -N_earlier+1, ..., N_later 
     while the sim event is always in bunch m = 0. 
-nLatererBunchesToSample : int
+n_later : int
     The number of following bunches sampled for out-of-time pileup. 
     Furthermore, pileup will be uniformly distributed among this number of bunches m = -N_earlier, -N_earlier+1, ..., N_later 
     while the sim event is always in bunch m = 0. 
-bunchSpacing : float
+bunch_spacing : float
     The spacing in time between bunches [ns]
-verbosity : int
-    Sets the producer specific level of verbosity, up to 3 for the most verbose step-by-step debug printouts.
-
-    
+start_event_min : int
+    The minimum event number to start overlaying pileup events. 
+start_event_max : int
+    The max event number to start overlaying pileup events. 
 """
 
 from LDMX.Framework import ldmxcfg
@@ -59,24 +59,27 @@ class OverlayProducer(ldmxcfg.Producer) :
         p.sequence.append( OverlayProducer( myPileupFileName.root ) )
     """
 
-    def __init__(self,fileName,name = 'OverlayProducer') :
+    def __init__(self,file_name,name = 'OverlayProducer') :
         super().__init__(name,'recon::OverlayProducer','Recon')
 
 
-        self.overlayFileName = fileName 
-        self.passName = "sim"
-        self.overlayPassName = "sim"
-        self.overlayCaloHitCollections=[ "TriggerPadUpSimHits", "EcalSimHits"]
-        self.overlayTrackerHitCollections=[ "TaggerSimHits"]
-        self.totalNumberOfInteractions = 2.
-        self.doPoissonIntime = False
-        self.doPoissonOutoftime = False
-        self.timeSpread = 0.        # [ns]
-        self.timeMean = 0.          # [ns]
-        self.nEarlierBunchesToSample = 0
-        self.nLaterBunchesToSample = 0
-        self.bunchSpacing = 26.88   # [ns]
-        self.verbosity = 1	
+        self.overlay_filename = file_name 
+        self.sim_passname = "sim"
+        self.overlay_passname = "sim"
+        self.calo_collections =  ["TriggerPad1SimHits", "TriggerPad2SimHits", "TriggerPad3SimHits",
+                                   "TargetSimHits", "EcalSimHits", "HcalSimHits"]
+        self.tracker_collections = [ "TaggerSimHits", "RecoilSimHits" ]
+        self.out_coll_postfix = "Overlay"
+        self.poisson_mu = 2.
+        self.do_poisson_in_time = False
+        self.do_poisson_out_of_time = False
+        # realistically, 30 ps; 
+        self.time_sigma = 0.        # [ns]
+        self.time_mean = 0.          # [ns]
+        self.n_earlier = 0
+        self.n_later = 0
+        # 26.9 = 1000./37.2 ; 5.4 = 1000./186.
+        self.bunch_spacing = 26.9   # [ns] 
         self.tree_name = 'LDMX_Events'
-        self.compressionSetting = 9
-
+        self.start_event_min = 1
+        self.start_event_max = 10000
