f9af5d02777912241a669ae83f7252cadd4ac6df
Tamas Vami <Tamas.Almos.Vami@cern.ch>
2025-08-11 09:35:10 -0700

    Implement Birks law in the TS SD

--- SimCore/src/SimCore/SDs/TrigScintSD.cxx
+++ SimCore/src/SimCore/SDs/TrigScintSD.cxx
@@ -17,9 +17,12 @@ TrigScintSD::TrigScintSD(const std::string& name,
                          simcore::ConditionsInterface& ci,
                          const framework::config::Parameters& p)
     : SensitiveDetector(name, ci, p) {
-  module_id_ = p.getParameter<int>("module_id");
-  collection_name_ = p.getParameter<std::string>("collection_name");
-  vol_name_ = p.getParameter<std::string>("volume_name");
+  module_id_ = p.get<int>("module_id");
+  collection_name_ = p.get<std::string>("collection_name");
+  vol_name_ = p.get<std::string>("volume_name");
+  use_birks_law_ = p.get<bool>("use_birks_law");
+  birks_const_one_ = p.get<double>("birks_const_one");
+  birks_const_two_ = p.get<double>("birks_const_two");
 }
 
 G4bool TrigScintSD::ProcessHits(G4Step* step, G4TouchableHistory* history) {
@@ -28,7 +31,10 @@ G4bool TrigScintSD::ProcessHits(G4Step* step, G4TouchableHistory* history) {
 
   // If a non-Geantino particle doesn't deposit energy during the step,
   // skip processing it.
-  if (energy == 0 and not isGeantino(step)) return false;
+  if (energy == 0 and not isGeantino(step)) {
+    ldmx_log(debug) << "No energy deposited in step, skipping.";
+    return false;
+  }
 
   // Create a new instance of a calorimeter hit
   //  emplace_back returns a *reference* to the hit that was constructed
@@ -68,6 +74,29 @@ G4bool TrigScintSD::ProcessHits(G4Step* step, G4TouchableHistory* history) {
   auto bar{track->GetVolume()->GetCopyNo()};
   ldmx::TrigScintID id(module_id_, bar);
   hit.setID(id.raw());
+
+  // Implement Birks law
+  G4double birks_factor(1.0);
+  G4double step_length = step->GetStepLength() / CLHEP::cm;
+  // Do not apply Birks for gamma deposits!
+  // Check, cut if necessary.
+  if (step_length > 1.0e-6) {
+    G4double rho = step->GetPreStepPoint()->GetMaterial()->GetDensity() /
+                   (CLHEP::g / CLHEP::cm3);
+    G4double dedx = energy / (rho * step_length);  //[MeV*cm^2/g]
+    birks_factor =
+        1.0 / (1.0 + birks_const_one_ * dedx + birks_const_two_ * dedx * dedx);
+    // Birks law is only applied to charged particles
+    if (step->GetTrack()->GetDefinition()->GetPDGCharge() == 0) {
+      birks_factor = 1.0;
+    }
+  }
+
+  // update the energy to include birks_factor
+  if (use_birks_law_) {
+    ldmx_log(trace) << "Applying Birks law with factor: " << birks_factor;
+    energy *= birks_factor;
+  }
 
   // add single contrib to this calorimeter hit
   //  IncidentID - this track's ID
