78eb97d6f08033640f8dbe957e286c204831cdf5
tomeichlersmith <eichl008@umn.edu>
2025-10-02 09:35:03 -0500

    attempt to refactor, strugglign to get GENIE to link properly

---
+++ cmake/FindGENIE.cmake
@@ -0,0 +1,173 @@
+#
+# FindGENIE.cmake
+#
+# Find an installation of GENIE. Highly tuned to the installation
+# of GENIE that is built into the ldmx/dev image.
+#
+# This will define the following variables
+#
+# GENIE_FOUND
+# GENIE_INCLUDE_DIRS - path to the headers
+# GENIE_LIBRARIES    - CMake list of CMake targets
+#
+# and the following main target to link to
+# (other targets are defined for specific GENIE libraries)
+#
+# GENIE::GENIE
+#
+
+if (GENIE_FOUND)
+  return()
+endif()
+
+set(GENIE_INCLUDE_DIRS "/usr/local/include/GENIE")
+#set(GENIE_LIBRARIES
+#  log4cpp
+#  gsl
+#  GRwFwk
+#  GRwClc
+#  GRwIO
+#  GFwInt
+#  GFwNum
+#  GFwUtl
+#  GFwParDat
+#  GFwEG
+#  GFwAlg
+#  GFwGHEP
+#  GFwMsg
+#  xml2
+#  GFwReg
+#  ROOT::EG
+#  ROOT::EGPythia8
+#  pythia8
+#  GFwNtp
+#  GPhXSIg
+#  GPhPDF
+#  GPhNuclSt
+#  GPhCmn
+#  GPhDcy
+#  GPhHadTransp
+#  GPhHadnz
+#  GPhHadTens
+#  GPhDeEx
+#  GPhAMNGXS
+#  GPhAMNGEG
+#  GPhChmXS
+#  GPhCohXS
+#  GPhCohEG
+#  GPhDISXS
+#  GPhDISEG
+#  GPhDfrcXS
+#  GPhDfrcEG
+#  GPhHELptnXS
+#  GPhHELptnEG
+#  GPhIBDXS
+#  GPhIBDEG
+#  GPhMNucXS
+#  GPhMNucEG
+#  GPhMEL
+#  GPhNuElXS
+#  GPhNuElEG
+#  GPhQELXS
+#  GPhQELEG
+#  GPhResXS
+#  GPhResEG
+#  GPhStrXS
+#  GPhStrEG
+#  LHAPDF
+#  GPhHEDISXS
+#  GPhHEDISEG
+#  ROOT::Geom
+#  GTlGeo
+#  GTlFlx
+#  ROOT::MathCore
+#  ROOT::MathMore)
+#add_library(GENIE::GENIE INTERFACE IMPORTED GLOBAL)
+#set_target_properties(GENIE::GENIE PROPERTIES
+#  INTERFACE_LINK_LIBRARIES "${GENIE_LIBRARIES}"
+#  INTERFACE_INCLUDE_DIRECTORIES "${GENIE_INCLUDE_DIRS}"
+#)
+#message(STATUS "Found GENIE")
+#set(GENIE_FOUND TRUE)
+
+set(GENIE_LIBRARIES "")
+
+macro(add_genie_target)
+  cmake_parse_arguments(add_genie_target "" "name" "dependencies" ${ARGN})
+  add_library(GENIE::${add_genie_target_name} SHARED IMPORTED GLOBAL)
+  #set(libs "/usr/local/lib/lib${add_genie_target_name}.so" ${add_genie_target_dependencies})
+  set_target_properties(GENIE::${add_genie_target_name} PROPERTIES
+    IMPORTED_LOCATION "/usr/local/lib/lib${add_genie_target_name}.so"
+    INTERFACE_INCLUDE_DIRECTORIES "${GENIE_INCLUDE_DIRS}"
+  )
+  if ("${add_genie_target_dependencies}")
+    set_target_properties(GENIE::${add_genie_target_name} PROPERTIES
+      INTERFACE_LINK_LIBRARIES ${add_genie_target_dependencies}
+    )
+  endif()
+  list(APPEND GENIE_LIBRARIES GENIE::${add_genie_target_name})
+endmacro()
+
+include(CMakeFindDependencyMacro)
+find_dependency(ROOT CONFIG REQUIRED)
+
+add_genie_target(name GFwMsg dependencies log4cpp)
+add_genie_target(name GFwReg dependencies GENIE::GFwMsg log4cpp)
+add_genie_target(name GFwAlg)
+add_genie_target(name GFwInt dependencies GFwAlg)
+add_genie_target(name GFwGHEP)
+add_genie_target(name GFwNum dependencies ROOT::MathMore gsl)
+add_genie_target(name GFwUtl dependencies GFwInt GFwNum GFwAlg pythia8 ROOT::EGPythia8 xml2)
+add_genie_target(name GFwParDat dependencies ROOT::EG)
+add_genie_target(name GFwEG dependencies GFwParDat ROOT::EG)
+add_genie_target(name GFwNtp)
+
+add_genie_target(name GPhCmn)
+add_genie_target(name GPhDcy)
+add_genie_target(name GPhHadTens)
+add_genie_target(name GPhMEL)
+add_genie_target(name GPhPDF dependencies LHAPDF)
+add_genie_target(name GPhXSIg)
+add_genie_target(name GPhNuclSt)
+add_genie_target(name GPhDeEx)
+add_genie_target(name GPhHadnz)
+add_genie_target(name GPhHadTransp)
+add_genie_target(name GPhAMNGXS)
+add_genie_target(name GPhAMNGEG)
+add_genie_target(name GPhChmXS)
+add_genie_target(name GPhCohXS)
+add_genie_target(name GPhCohEG)
+add_genie_target(name GPhDISXS)
+add_genie_target(name GPhDISEG)
+add_genie_target(name GPhDfrcXS)
+add_genie_target(name GPhDfrcEG)
+add_genie_target(name GPhHELptnXS)
+add_genie_target(name GPhHELptnEG)
+add_genie_target(name GPhIBDXS)
+add_genie_target(name GPhIBDEG)
+add_genie_target(name GPhMNucXS)
+add_genie_target(name GPhMNucEG)
+add_genie_target(name GPhNuElXS)
+add_genie_target(name GPhNuElEG)
+add_genie_target(name GPhQELXS)
+add_genie_target(name GPhQELEG)
+add_genie_target(name GPhResXS)
+add_genie_target(name GPhResEG)
+add_genie_target(name GPhStrXS)
+add_genie_target(name GPhStrEG)
+add_genie_target(name GPhHEDISXS)
+add_genie_target(name GPhHEDISEG)
+add_genie_target(name GTlFlx)
+add_genie_target(name GTlGeo dependencies ROOT::Geom)
+
+add_genie_target(name GRwFwk)
+add_genie_target(name GRwIO)
+add_genie_target(name GRwClc)
+
+add_library(GENIE::GENIE INTERFACE IMPORTED GLOBAL)
+set_target_properties(GENIE::GENIE PROPERTIES
+  INTERFACE_LINK_LIBRARIES "${GENIE_LIBRARIES}"
+  INTERFACE_INCLUDE_DIRECTORIES "${GENIE_INCLUDE_DIRS}"
+)
+message(STATUS "Found GENIE")
+set(GENIE_FOUND TRUE)
