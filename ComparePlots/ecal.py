cbcadf764cf6e73cb60f366556993628eb3dbddd
Tamas Vami <tamas.almos.vami@cern.ch>
2025-11-14 07:49:02 -0800

    Remove dir name from hist names in ComparePlots (#1874)

--- ComparePlots/ecal.py
+++ ComparePlots/ecal.py
@@ -16,14 +16,14 @@ def digi_verify(d : Differ, out_dir = None) :
         Differ containing files that are not event files (presumably histogram files)
     """
 
-    col, name = 'EcalDigiVerify/EcalDigiVerify_num_sim_hits_per_cell', 'Number of SimHits per ECal Cell (excluding empty rec cells)'
+    col, name = 'EcalDigiVerify/num_sim_hits_per_cell', 'Number of SimHits per ECal Cell (excluding empty rec cells)'
     log.info(f'plotting {col}')
     d.plot1d(col, name, out_dir = out_dir, legend_kw = dict(loc='upper left'))
 
     features = [
-        ('EcalDigiVerify/EcalDigiVerify_num_rec_hits', 'Number of RecHits'),
-        ('EcalDigiVerify/EcalDigiVerify_num_noise_hits', 'Number of noisy RecHits'),
-        ('EcalDigiVerify/EcalDigiVerify_total_rec_energy', 'Total Reconstructed Energy in ECal [MeV]'),
+        ('EcalDigiVerify/num_rec_hits', 'Number of RecHits'),
+        ('EcalDigiVerify/num_noise_hits', 'Number of noisy RecHits'),
+        ('EcalDigiVerify/total_rec_energy', 'Total Reconstructed Energy in ECal [MeV]'),
     ]
     for col, name in features :
         log.info(f'plotting {col}')
@@ -39,21 +39,21 @@ def shower_feats(d : Differ, out_dir = None) :
         Differ containing files that are not event files (presumably histogram files)
     """
 
-    col, name = 'EcalShowerFeatures/EcalShowerFeatures_deepest_layer_hit', 'Deepest Layer Hit'
+    col, name = 'EcalShowerFeatures/deepest_layer_hit', 'Deepest Layer Hit'
     log.info(f'plotting {col}')
     d.plot1d(col, name, out_dir = out_dir, legend_kw = dict(loc='upper left'))
 
     features = [
-        ('EcalShowerFeatures/EcalShowerFeatures_num_readout_hits', 'N Readout Hits'),
-        ('EcalShowerFeatures/EcalShowerFeatures_summed_det', 'Total Rec Energy [MeV]'),
-        ('EcalShowerFeatures/EcalShowerFeatures_summed_iso', 'Total Isolated Energy [MeV]'),
-        ('EcalShowerFeatures/EcalShowerFeatures_summed_back', 'Total Back Energy [MeV]'),
-        ('EcalShowerFeatures/EcalShowerFeatures_max_cell_dep', 'Max Cell Dep [MeV]'),
-        ('EcalShowerFeatures/EcalShowerFeatures_shower_rms', 'Shower RMS [mm]'),
-        ('EcalShowerFeatures/EcalShowerFeatures_x_std', 'X Standard Deviation [mm]'),
-        ('EcalShowerFeatures/EcalShowerFeatures_y_std', 'Y Standard Deviation [mm]'),
-        ('EcalShowerFeatures/EcalShowerFeatures_avg_layer_hit', 'Avg Layer Hit'),
-        ('EcalShowerFeatures/EcalShowerFeatures_std_layer_hit', 'Std Dev Layer Hit')
+        ('EcalShowerFeatures/num_readout_hits', 'N Readout Hits'),
+        ('EcalShowerFeatures/summed_det', 'Total Rec Energy [MeV]'),
+        ('EcalShowerFeatures/summed_iso', 'Total Isolated Energy [MeV]'),
+        ('EcalShowerFeatures/summed_back', 'Total Back Energy [MeV]'),
+        ('EcalShowerFeatures/max_cell_dep', 'Max Cell Dep [MeV]'),
+        ('EcalShowerFeatures/shower_rms', 'Shower RMS [mm]'),
+        ('EcalShowerFeatures/x_std', 'X Standard Deviation [mm]'),
+        ('EcalShowerFeatures/y_std', 'Y Standard Deviation [mm]'),
+        ('EcalShowerFeatures/avg_layer_hit', 'Avg Layer Hit'),
+        ('EcalShowerFeatures/std_layer_hit', 'Std Dev Layer Hit')
     ]
     for col, name in features :
         log.info(f'plotting {col}')
@@ -69,19 +69,19 @@ def mip_tracking(d : Differ, out_dir = None) :
         Differ containing files that are not event files (presumably histogram files)
     """
 
-    col, name = 'EcalMipTrackingFeatures/EcalMipTrackingFeatures_n_straight_tracks', 'Number of Straight Tracks'
+    col, name = 'EcalMipTrackingFeatures/n_straight_tracks', 'Number of Straight Tracks'
     log.info(f'plotting {col}')
     d.plot1d(col, name, out_dir = out_dir, legend_kw = dict(loc='upper left'))
 
     features = [
-        ('EcalMipTrackingFeatures/EcalMipTrackingFeatures_n_linreg_segments', 'Number of Linear Regression Segments'),
-        ('EcalMipTrackingFeatures/EcalMipTrackingFeatures_first_near_photon_layer', 'First Near Photon Layer'),
-        ('EcalMipTrackingFeatures/EcalMipTrackingFeatures_ep_ang', 'Electron Photon Angle [degree]'),
-        ('EcalMipTrackingFeatures/EcalMipTrackingFeatures_ep_sep', 'Electron Photon Separation'),
-        ('EcalMipTrackingFeatures/EcalMipTrackingFeatures_recoil_pz', 'Recoil electron pz [MeV]'),
-        ('EcalMipTrackingFeatures/EcalMipTrackingFeatures_recoil_pt', 'Recoil electron pT [MeV]'),
-        ('EcalMipTrackingFeatures/EcalMipTrackingFeatures_recoil_x', 'Recoil electron x[mm]'),
-        ('EcalMipTrackingFeatures/EcalMipTrackingFeatures_recoil_y', 'Recoil electron y [mm]'),
+        ('EcalMipTrackingFeatures/n_linreg_segments', 'Number of Linear Regression Segments'),
+        ('EcalMipTrackingFeatures/first_near_photon_layer', 'First Near Photon Layer'),
+        ('EcalMipTrackingFeatures/ep_ang', 'Electron Photon Angle [degree]'),
+        ('EcalMipTrackingFeatures/ep_sep', 'Electron Photon Separation'),
+        ('EcalMipTrackingFeatures/recoil_pz', 'Recoil electron pz [MeV]'),
+        ('EcalMipTrackingFeatures/recoil_pt', 'Recoil electron pT [MeV]'),
+        ('EcalMipTrackingFeatures/recoil_x', 'Recoil electron x[mm]'),
+        ('EcalMipTrackingFeatures/recoil_y', 'Recoil electron y [mm]'),
     ]
     for col, name in features :
         log.info(f'plotting {col}')
@@ -98,13 +98,13 @@ def veto_results(d : Differ, out_dir = None) :
         Differ containing files that are not event files (presumably histogram files)
     """
 
-    col, name = 'EcalVetoResults/EcalVetoResults_bdt_disc', 'BDT discriminating score'
+    col, name = 'EcalVetoResults/bdt_disc', 'BDT discriminating score'
     log.info(f'plotting {col}')
     d.plot1d(col, name, out_dir = out_dir, legend_kw = dict(loc='upper left'))
 
     features = [
-        ('EcalVetoResults/EcalVetoResults_bdt_disc_log', '-log(1-BDT discriminating score)'),
-        ('EcalVetoResults/EcalVetoResults_fiducial', 'Recoil eletron fiducial')
+        ('EcalVetoResults/bdt_disc_log', '-log(1-BDT discriminating score)'),
+        ('EcalVetoResults/fiducial', 'Recoil eletron fiducial')
     ]
     for col, name in features :
         log.info(f'plotting {col}')
@@ -121,15 +121,15 @@ def clue_cluster(d : Differ, out_dir = None) :
     """
     
     features = [
-        ('EcalClusterAnalyzer/EcalClusterAnalyzer_number_of_clusters_first_layer', 'Number of CLUE clusters on the first layer'),
-        ('EcalClusterAnalyzer/EcalClusterAnalyzer_number_of_clusters_per_layer', 'Number of CLUE clusters per layer'),
-        ('EcalClusterAnalyzer/EcalClusterAnalyzer_number_of_clusters', 'Total number of CLUE clusters'),
-        ('EcalClusterAnalyzer/EcalClusterAnalyzer_energy_percentage', 'Percentage of energy in cluster'),
-        ('EcalClusterAnalyzer/EcalClusterAnalyzer_clusterless_hits_percentage', 'Percentage of hits not in a cluster'),
-        ('EcalClusterAnalyzer/EcalClusterAnalyzer_sp_clue_distance', 'CLUE centroid to SP ele distance in xy-plane [mm]'),
-        ('EcalClusterAnalyzer/EcalClusterAnalyzer_sp_clue_x_residual', 'CLUE centroid X - SP ele X [mm]'),
-        ('EcalClusterAnalyzer/EcalClusterAnalyzer_sp_clue_y_residual', 'CLUE centroid Y - SP ele Y [mm]'),
-        ('EcalClusterAnalyzer/EcalClusterAnalyzer_correctly_predicted_events', ''),
+        ('EcalClusterAnalyzer/number_of_clusters_first_layer', 'Number of CLUE clusters on the first layer'),
+        ('EcalClusterAnalyzer/number_of_clusters_per_layer', 'Number of CLUE clusters per layer'),
+        ('EcalClusterAnalyzer/number_of_clusters', 'Total number of CLUE clusters'),
+        ('EcalClusterAnalyzer/energy_percentage', 'Percentage of energy in cluster'),
+        ('EcalClusterAnalyzer/clusterless_hits_percentage', 'Percentage of hits not in a cluster'),
+        ('EcalClusterAnalyzer/sp_clue_distance', 'CLUE centroid to SP ele distance in xy-plane [mm]'),
+        ('EcalClusterAnalyzer/sp_clue_x_residual', 'CLUE centroid X - SP ele X [mm]'),
+        ('EcalClusterAnalyzer/sp_clue_y_residual', 'CLUE centroid Y - SP ele Y [mm]'),
+        ('EcalClusterAnalyzer/correctly_predicted_events', ''),
     ]
     for col, name in features :
         log.info(f'plotting {col}')
