15878b621045cdff4c30fd94602c8c8e04144ccb
Sanjit Masanam <sanjitmasanam@gmail.com>
2026-01-06 15:02:46 -0800

    Updated helper script for clarity

--- Detectors/util/ecal_silicon_pos_calc.py
+++ Detectors/util/ecal_silicon_pos_calc.py
@@ -1,10 +1,11 @@
-import numpy
+# Geometry name
+geo_name = 'reduced_v3'
 
 # Constants
 PCB_dz = 1.666
 Glue_dz = 0.1
-Si_dz = 0.12
-Si_extra_dz = 0.18
+Si_dz = 0.12 # Active Si width
+Si_extra_dz = 0.18 # Non-active Si width
 GlueThick_dz = 0.2
 CarbonBasePlate_dz = 0.79
 MotherBoardAssembly_dz = 8.166
@@ -21,11 +22,17 @@ front_pre_tungsten_dz = 2.0
 cooling_pre_tungsten_dz = 2.0
 correction_constant = 0.15
 
-# Arrays
+# Arrays for v15 geometry
 layerZPositions = [7.582, 16.062, 33.226, 43.206, 60.370, 71.350, 90.014, 101.594, 120.258, 131.838, 150.502, 162.082, 180.746, 192.326, 210.990, 222.570, 241.234, 252.814, 271.478, 283.058, 301.722, 313.302, 331.966, 343.546, 365.710, 380.690, 402.854, 417.834, 439.998, 454.978, 477.142, 492.122, 514.286, 529.266]
-bilayer_absorber_cumulative = [0.0, 0.0, 0.0, 15.1, 22.2, 29.3, 36.4, 43.5, 50.6, 57.7, 64.8, 71.9, 85.9, 99.9, 113.9, 127.9, 141.9]
-front_tungsten_dz = [0.0, 0.0, 0.0, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 7.0, 7.0, 7.0, 7.0, 7.0]
-cooling_tungsten_dz = [0.0, 0.0, 0.0, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 3.5, 3.5, 3.5, 3.5, 3.5]
+bilayer_absorber_cumulative = [0.0, 3.0, 8.0, 15.1, 22.2, 29.3, 36.4, 43.5, 50.6, 57.7, 64.8, 71.9, 85.9, 99.9, 113.9, 127.9, 141.9]
+front_tungsten_dz = [1.0, 2.0, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 7.0, 7.0, 7.0, 7.0, 7.0]
+cooling_tungsten_dz = [1.0, 1.5, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 3.5, 3.5, 3.5, 3.5, 3.5]
+
+# Arrays for reduced_v3 geo (changing geo_name will cause script to go back to v15)
+if geo_name == 'reduced_v3':
+    bilayer_absorber_cumulative = [0.0] * 3
+    front_tungsten_dz = [0.0] * 3
+    cooling_tungsten_dz = [0.0] * 3
 
 # Calculated constants
 Flower_dz = PCB_dz+Glue_dz+Si_dz+Si_extra_dz+GlueThick_dz+CarbonBasePlate_dz
@@ -42,6 +49,7 @@ for i in range(len(bilayer_absorber_cumulative)-1):
     calc_layers.append(even_layer)
 
 # print results
+print(f"For geometry: {geo_name}")
 print("GDML | EcalGeometry.py | Diff")
-for i in range(len(layerZPositions)):
-    print(round(calc_layers[i] - 240, 3), round(layerZPositions[i], 3), round(numpy.abs(calc_layers[i] - layerZPositions[i] - 240), 3))
+for i in range(len(bilayer_absorber_cumulative)*2):
+    print(round(calc_layers[i] - 240, 3), round(layerZPositions[i], 3), round(abs(calc_layers[i] - layerZPositions[i] - 240), 3))
