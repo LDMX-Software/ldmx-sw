b1f00e2008fcfaa538db620c2a50c525c9dd875e
tomeichlersmith <eichl008@umn.edu>
2026-01-28 13:31:47 -0600

    filter for subsystem in rogue-extract for inspecting packets

--- Packing/app/rogue_extract.cxx
+++ Packing/app/rogue_extract.cxx
@@ -14,8 +14,10 @@ static void usage() {
                "  rogue-extract [options] input_file.dat\n"
                "\n"
                " OPTIONS:\n"
-               "  -h,--help    : print this help and exit\n"
-               "  -n,--n-events: limit to first N events to print\n"
+               "  -h,--help      : print this help and exit\n"
+               "  -n,--nevent    : maximum number of events to unpack\n"
+               "  -s,--subsystem : which subsystem\n"
+               "                   ('ecal','hcal','ts','tdaq', or integer)\n"
             << std::endl;
 }
 
@@ -28,6 +30,7 @@ int main(int argc, char* argv[]) {
 
   int nevents{-1};
   std::string in_file;
+  int subsystem{-1}, contributor{-1};
   for (int i_arg{1}; i_arg < argc; i_arg++) {
     std::string arg{argv[i_arg]};
     if (arg[0] == '-') {
@@ -35,7 +38,7 @@ int main(int argc, char* argv[]) {
       if (arg == "-h" or arg == "--help") {
         usage();
         return 0;
-      } else if (arg == "-n" or arg == "--n-events") {
+      } else if (arg == "-n" or arg == "--nevent") {
         if (i_arg + 1 == argc or argv[i_arg + 1][0] == '-') {
           std::cerr << "The " << arg
                     << " parameter requires an argument after it.\n";
@@ -48,6 +51,37 @@ int main(int argc, char* argv[]) {
           std::cerr << "The argument to " << arg << " '" << argv[i_arg]
                     << "' is not an integer.\n";
           return 1;
+        }
+      } else if (arg == "-s" or arg == "--subsystem") {
+        if (i_arg + 1 == argc or argv[i_arg + 1][0] == '-') {
+          std::cerr << "The " << arg
+                    << " parameter requires an argument after it.\n";
+          return 1;
+        }
+        i_arg++;
+        std::string subsys_arg{argv[i_arg]};
+        if (subsys_arg == "ecal") {
+          subsystem = 5;
+          contributor = 40;
+        } else if (subsys_arg == "hcal") {
+          subsystem = 5;
+          contributor = 20;
+        } else if (subsys_arg == "ts") {
+          subsystem = 2;
+          contributor = 1;
+        } else if (subsys_arg == "tdaq") {
+          subsystem = 1;
+        } else if (subsys_arg == "tracker") {
+          subsystem = 4;
+        } else {
+          try {
+            subsystem = std::stoi(subsys_arg.c_str());
+          } catch (const std::invalid_argument& e) {
+            std::cerr << "The argument to " << arg << " '" << subsys_arg
+                      << "' is not 'ecal', 'hcal', 'ts', 'tdaq', 'tracker'"
+                      << " or a positive integer.\n";
+            return 1;
+          }
         }
       } else {
         std::cerr << "Unrecognized option " << arg << std::endl;
@@ -85,28 +119,53 @@ int main(int argc, char* argv[]) {
       r >> frame_header;
       frame_count++;
       const int frame_end = r.tell()+frame_header.size();
-      if (frame_header.channel() == 0 and not frame_header.probablyYaml()) {
-        event_count++;
-        printf("frame %d\n", frame_count);
-        printf("  channel = %d, size = %d, trailer = 0x%02x\n",
-            frame_header.channel(), frame_header.size(), frame_header.trailer());
-        r >> ror_header;
-        printf("  vers = %d, subsys = %d, contrib = %d\n",
-            ror_header.version(), ror_header.subsystem(), ror_header.contributor());
-        r.read(bytes, (frame_header.size() - packing::LDMXRoRHeader::size));
-        for (std::size_t i_row{0}; i_row < bytes.size() / 16; i_row++) {
-          printf("%8lu  ", i_row);
-          for (std::size_t i_byte{0}; i_byte < 16; i_byte ++) {
-            printf("%02x ", bytes[16*i_row+i_byte]);
-          }
-          printf("\n");
-        }
-        if (nevents > 0 and event_count >= nevents) {
-          return 0;
-        }
-      } else {
-        // skip this frame
+      if (frame_header.channel() != 0) {
+        // non-data channel in StreamWriter, skip
         r.seek(frame_end);
+        continue;
+      }
+
+      if (frame_header.probablyYaml()) {
+        // non-data channel in StreamWriter, skip
+        r.seek(frame_end);
+        continue;
+      }
+
+      r >> ror_header;
+      if (ror_header.subsystem() != subsystem) {
+        // wrong subsystem ID number
+        r.seek(frame_end);
+        continue;
+      }
+
+      if (contributor >= 0 and ror_header.contributor() != contributor) {
+        // wrong contributor ID number
+        r.seek(frame_end);
+        continue;
+      }
+
+      // correct subsystem and contributor channel
+      event_count++;
+      printf("frame %d\n", frame_count);
+      printf("  channel = %d, size = %d, trailer = 0x%02x\n",
+          frame_header.channel(), frame_header.size(), frame_header.trailer());
+      printf("  vers = %d, subsys = %d, contrib = %d\n",
+          ror_header.version(), ror_header.subsystem(), ror_header.contributor());
+
+      r.read(bytes, (frame_header.size() - packing::LDMXRoRHeader::size));
+
+      // print similar to hexdump -C
+      for (std::size_t i_row{0}; i_row < bytes.size() / 16; i_row++) {
+        printf("%8lu  ", i_row);
+        for (std::size_t i_byte{0}; i_byte < 16; i_byte ++) {
+          printf("%02x ", bytes[16*i_row+i_byte]);
+        }
+        printf("\n");
+      }
+
+      // leave early if reached number of events
+      if (nevents > 0 and event_count >= nevents) {
+        return 0;
       }
     }
   } catch (const std::runtime_error& e) {
