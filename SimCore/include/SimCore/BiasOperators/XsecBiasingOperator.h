2bde554c926ef79c904a9d47a5c2bd04f709b858
Tamas Vami <Tamas.Almos.Vami@cern.ch>
2026-03-04 14:37:20 -0800

    Enforce no-lint

--- SimCore/include/SimCore/BiasOperators/XsecBiasingOperator.h
+++ SimCore/include/SimCore/BiasOperators/XsecBiasingOperator.h
@@ -125,6 +125,7 @@ class XsecBiasingOperator : public G4VBiasingOperator {
    *
    * @param[in,out] header RunHeader to write configuration to
    */
+  // NOLINTNEXTLINE
   virtual void RecordConfig(ldmx::RunHeader& header) const = 0;
 
  protected:
@@ -142,6 +143,7 @@ class XsecBiasingOperator : public G4VBiasingOperator {
    * @param[in] biased_xsec the biased cross section
    * @return the biasing operation with the input biased cross section
    */
+  // NOLINTNEXTLINE
   G4VBiasingOperation* BiasedXsec(double biased_xsec) {
     xsec_operation_->SetBiasedCrossSection(biased_xsec);
     xsec_operation_->Sample();
