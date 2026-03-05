dd5a35af9c5c86f9f0772c21cffeb3ee73e01afc
cjbarton151 <CJ.Barton@coyotes.usd.edu>
2026-03-05 20:58:49 +0100

    New features for v15 geometry (TSPads and associated parts) (#1888)
    * New features for v15 geometry. TS Pad components added as active volumes under the TrigScintSD type. TS Pad component loops removed from other files and now generated automatically as assembly files by new script "makeassemblyfiles.py". GDML styles converted from online to local access.
    
    * Added authorship to new script. Updated simulator.py to preserve TSPad collections of reduced geometries. Updated new collection names to match convention.
    
    * Refactored assembly implementation to submodule implementation. Revised comments to match plus added detail. Made light pipe lengths and light pipe/SiPM positions variable as a function of x displacement.
    
    * Updated y dimension of the scint bars/light pipes. Unswapped the swapped index ordering of TSPad1 and 2 components.

--- SimCore/python/simulator.py
+++ SimCore/python/simulator.py
@@ -138,8 +138,11 @@ class simulator(Producer):
             trigscint = [ sds.TrigScintSD.up(), sds.TrigScintSD.tag(), sds.TrigScintSD.down() ]
         elif 'hcal-prototype' in det_name :
             trigscint = [ sds.TrigScintSD.testbeam() ]
-        else :
+        elif 'v13' in det_name or 'v14' in det_name or 'reduced' in det_name :
             trigscint = [ sds.TrigScintSD.pad1(), sds.TrigScintSD.pad2(), sds.TrigScintSD.pad3() ]
+        else:
+            trigscint = [ sds.TrigScintSD.pad1(), sds.TrigScintSD.pad2(), sds.TrigScintSD.pad3(), sds.TrigScintSD.pad1lg(), sds.TrigScintSD.pad2lg(), sds.TrigScintSD.pad3lg(), sds.TrigScintSD.pad1sipm(), sds.TrigScintSD.pad2sipm(), sds.TrigScintSD.pad3sipm() ]
+
         self.sensitive_detectors = [
                 sds.TrackerSD.tagger(),
                 sds.TrackerSD.recoil(),
