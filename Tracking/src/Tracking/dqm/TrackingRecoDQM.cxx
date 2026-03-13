f30f0587bfdefcec0799b3c0637cf09b548339b7
Tamas Vami <Tamas.Almos.Vami@cern.ch>
2026-03-13 13:06:13 -0700

    Add protection for tracking DQM if no truth track exists

--- Tracking/src/Tracking/dqm/TrackingRecoDQM.cxx
+++ Tracking/src/Tracking/dqm/TrackingRecoDQM.cxx
@@ -111,21 +111,24 @@ void TrackingRecoDQM::analyze(const framework::Event& event) {
   }
 
   // Track Extrapolation to Ecal Monitoring
+  // trackStateMonitoring requires truth_track_collection_ to be available
   ldmx_log(trace) << "Track Extrapolation to Ecal Monitoring";
-  if (std::find(track_states_.begin(), track_states_.end(), "target") !=
-      track_states_.end()) {
-    trackStateMonitoring(tracks, ldmx::TrackStateType::AtTarget, "target");
-  }
+  if (do_truth_comparison_) {
+    if (std::find(track_states_.begin(), track_states_.end(), "target") !=
+        track_states_.end()) {
+      trackStateMonitoring(tracks, ldmx::TrackStateType::AtTarget, "target");
+    }
 
-  if (std::find(track_states_.begin(), track_states_.end(), "ecal") !=
-      track_states_.end()) {
-    trackStateMonitoring(tracks, ldmx::TrackStateType::AtECAL, "ecal");
-  }
+    if (std::find(track_states_.begin(), track_states_.end(), "ecal") !=
+        track_states_.end()) {
+      trackStateMonitoring(tracks, ldmx::TrackStateType::AtECAL, "ecal");
+    }
 
-  if (std::find(track_states_.begin(), track_states_.end(), "beamOrigin") !=
-      track_states_.end()) {
-    trackStateMonitoring(tracks, ldmx::TrackStateType::AtBeamOrigin,
-                         "beamOrigin");
+    if (std::find(track_states_.begin(), track_states_.end(), "beamOrigin") !=
+        track_states_.end()) {
+      trackStateMonitoring(tracks, ldmx::TrackStateType::AtBeamOrigin,
+                           "beamOrigin");
+    }
   }
 
   // Technical Efficiency plots
