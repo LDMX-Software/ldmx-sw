af1e185ff1ac85485ec8e88a5b3b32c7f4815af1
oscarplewis <oscarplewis@ucsb.edu>
2026-03-13 17:04:56 -0700

    ruff check --fix

--- Tracking/python/full_tracking_sequence.py
+++ Tracking/python/full_tracking_sequence.py
@@ -239,11 +239,11 @@ def setOverlay(pass_name:str):
         params = vars(proc) # Python variable assignments are references by default,
                             # so this works to update the processor class variables
         for key, value in params.items():
-            if str(key) in ['input_pass_name', 'track_collection_event_passname', 
-                            'track_passname', 'meas_collection_event_passname', 'meas_passname', 
-                            'measurement_passname', 'truth_events_passname', 'truth_passname', 
-                            'track_collection_events_passname', 'input_tagger_pass_name', 
-                            'input_recoil_pass_name', 'input_collection_events_passname', 
+            if str(key) in ['input_pass_name', 'track_collection_event_passname',
+                            'track_passname', 'meas_collection_event_passname', 'meas_passname',
+                            'measurement_passname', 'truth_events_passname', 'truth_passname',
+                            'track_collection_events_passname', 'input_tagger_pass_name',
+                            'input_recoil_pass_name', 'input_collection_events_passname',
                             'tagger_trks_event_collection_passname']:
                 params[key] = pass_name
                 continue
