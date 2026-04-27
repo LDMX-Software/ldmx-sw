e4a55d97955a381e19209a564819b813f66683d1
Lucia Kvarnström <kvarnla@github.com>
2026-04-27 11:12:47 +0200

    update CVAnalyzer

--- ClusterViewerAnalyzer.cxx
+++ ClusterViewerAnalyzer.cxx
@@ -11,70 +11,68 @@ class ClusterViewerAnalyzer : public framework::Analyzer {
     public:
     
     std::ofstream outfile;
-    
+        
+        void configure(framework::config::Parameters& ps) override {
+        pass_name_ = ps.get<std::string>("pass_name");
+        }
+        
+        std::string pass_name_;
+        
         ClusterViewerAnalyzer(const std::string& name, framework::Process& p)
           : framework::Analyzer(name, p) {
-        
-        outfile.open("clusters.txt"); //Cluster combinations will be saved in txt file
+          
+        outfile.open("clusters.txt");
         }
-        
+                
         void analyze(const framework::Event& event) override {
         
-            const auto& clusters1 = 
-            event.getCollection<ldmx::TrigScintCluster>("TriggerPad1Clusters", "uptoclustering");
-            
-            const auto& clusters2 = 
-            event.getCollection<ldmx::TrigScintCluster>("TriggerPad2Clusters", "uptoclustering");
-                    
-            const auto& clusters3 = 
-            event.getCollection<ldmx::TrigScintCluster>("TriggerPad3Clusters", "uptoclustering");
-            
-            size_t Clusters = std::min({clusters1.size(), clusters2.size(), clusters3.size()});
-            
-            for (size_t i = 0; i < Clusters; i++) {
-                
-                int eventNumber = event.getEventNumber();
-                float c1 = clusters1[i].getCentroid();
-                float c2 = clusters2[i].getCentroid();
-                float c3 = clusters3[i].getCentroid();
-                
-                outfile << eventNumber << " "
-                        << c1 << " "
-                        << c2 << " "
-                        << c3 << "\n";
-            }
-    	
-            	//extra clusters should only very rarely (1 in 10.000) occur if the 
-            	//clustering_threshold is set to >3 when using TruthHitProducer.cxx.  
-            	//Without THP, extra clusters will still exist, but TrigScintTrackProducer  
-            	//seems to be able to choose the "correct" cluster for track-making anyways. 
-                	
-            for (size_t i = 1; i < clusters1.size(); i++){
-                std::cout << "In event "
-                          << event.getEventNumber() 
-                          << ", extra cluster in pad 1: " 
-                          << clusters1[i].getCentroid() 
-                          << "\n"; 
-            }	
+        const auto& clusters1 = 
+        event.getCollection<ldmx::TrigScintCluster>("TriggerPad1Clusters", pass_name_);
         
-            for (size_t i = 1; i < clusters2.size(); i++){
-                std::cout << "In event " 
-                          << event.getEventNumber() 
-                          << ", extra cluster in pad 2: " 
-                          << clusters2[i].getCentroid() 
-                          << "\n"; 
-            }	
-            
-            for (size_t i = 1; i < clusters3.size(); i++){
-                std::cout << "In event " 
-                          << event.getEventNumber() 
-                          << ", extra cluster in pad 3: " 
-                          <<  clusters3[i].getCentroid() 
-                          << "\n"; 
-            }	
+        const auto& clusters2 = 
+        event.getCollection<ldmx::TrigScintCluster>("TriggerPad2Clusters", pass_name_);
+                
+        const auto& clusters3 = 
+        event.getCollection<ldmx::TrigScintCluster>("TriggerPad3Clusters", pass_name_);
+        
+        size_t Clusters = std::min({clusters1.size(), clusters2.size(), clusters3.size()});
+
+        for (size_t i = 0; i < Clusters; i++) {
+
+            int eventNumber = event.getEventNumber();
+            float c1 = clusters1[i].getCentroid();
+            float c2 = clusters2[i].getCentroid();
+            float c3 = clusters3[i].getCentroid();
+            float c3_alt = c3;
+
+            if (clusters3.size() > i + 1) {
+                c3_alt = clusters3[i + 1].getCentroid();
+            }
+
+            outfile << eventNumber << " "
+                    << c1 << " "
+                    << c2 << " "
+                    << c3 << " "
+                    << c3_alt << "\n";
+        }
+        	
+    	
+        for (size_t i = 1; i < clusters1.size(); i++){
+            std::cout << "In event " << event.getEventNumber() <<", extra cluster in pad 1: " 
+            << clusters1[i].getCentroid() << "\n"; 
+        }	
+        
+        for (size_t i = 1; i < clusters2.size(); i++){
+            std::cout << "In event " << event.getEventNumber() <<", extra cluster in pad 2: " 
+            << clusters2[i].getCentroid() << "\n"; 
+        }	
+        
+        for (size_t i = 1; i < clusters3.size(); i++){
+            std::cout << "In event " << event.getEventNumber() <<", extra cluster in pad 3: " 
+            <<  clusters3[i].getCentroid() << "\n"; 
+        }	
         
         }
-        
         void onProcessEnd() override {outfile.close();}
     };
 
