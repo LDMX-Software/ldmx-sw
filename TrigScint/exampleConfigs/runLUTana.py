6a739f9426e3dc244a80d68b8199712324ff8c8a
kvarnla <kvarnla@gmail.com>
2026-06-04 23:01:13 +0200

    Update runLUTana.py

--- TrigScint/exampleConfigs/runLUTana.py
+++ TrigScint/exampleConfigs/runLUTana.py
@@ -1,14 +1,15 @@
-#config file to make LUT for tracking
+#config file to make LUT for tracking using PatternLUTMaker
 
 from LDMX.Framework import ldmxcfg
 
 p = ldmxcfg.Process("LUTmaker")
-p.input_files =  ["TruthSamples.root"] #necessary to define but it doesnt use it
+p.input_files =  ["TruthSamples.root"] #necessary to define but it doesnt use it, the real input file is defined below
 
-make_lut = ldmxcfg.Analyzer.from_file('LUTAnalyzer.cxx', 
-                                      needs = ['TrigScint_Event', 'SimCore_Event'])
-make_lut.lut_threshold = 1.0/1000
-make_lut.input_file = "clusters.txt"
+from LDMX.TrigScint.trig_scint import PatternLUTMaker
+
+make_lut = PatternLUTMaker("LUTmaker")
+make_lut.lut_threshold = 1.0/1000 #or whichever threshold you like
+make_lut.input_file = "clusters.txt" #here! input list of cluster triplets
 make_lut.output_file = "LUT.txt"
 
 p.sequence = [make_lut]
