74f9424500cc318c86beb9fae8a6c4a6e23e8653
Lucia Kvarnström <kvarnla@github.com>
2026-07-01 00:00:42 +0200

    Add event generator config

---
+++ TrigScint/exampleConfigs/eventGen.py
@@ -0,0 +1,22 @@
+#Creates simulation sample .root file with TS info
+
+from LDMX.Framework import ldmxcfg
+p = ldmxcfg.Process('simulation')
+
+from LDMX.SimCore import simulator
+from LDMX.SimCore import generators as gen
+
+mySim = simulator.Simulator( "mySim" )
+mySim.set_detector( 'ldmx-det-v14-8gev', True )
+mySim.generators = [ gen.single_8gev_e_upstream_tagger() ]
+gen.beamSpotSmear = [20.,80.,0.]
+mySim.description = 'Basic test Simulation'
+
+p.sequence = [ mySim ]
+p.run = 1 #different for each simulation
+p.max_events = 10
+p.output_files = [ 'SimSamples.root' ] #new output file name
+
+import LDMX.Ecal.ecal_geometry
+import LDMX.Ecal.ecal_hardcoded_conditions
+import LDMX.Hcal.hcal_geometry
