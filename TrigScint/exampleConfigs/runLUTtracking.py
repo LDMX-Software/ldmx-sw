cdf90d9f7e0a8834ead749935619ec243f2deb98
cjbarton151 <cjbarton151@gmail.com>
2026-07-21 16:47:50 +0200

    Fixed python formatting in LUT-related scripts to match ruff standards. (#2098)
    * Fixed python formatting in LUT-related scripts to match ruff standards.
    
    * apply ruff formatting and linting
    
    ---------
    
    Co-authored-by: Clay Barton <cbarton@cosmos1.int.lunarc>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- TrigScint/exampleConfigs/runLUTtracking.py
+++ TrigScint/exampleConfigs/runLUTtracking.py
@@ -1,7 +1,7 @@
-#Performs tracking with TrigScintTrackProducer, with and without LUT method for comparison
+# Performs tracking with TrigScintTrackProducer, with+without LUT method for comparison
 from LDMX.Framework import ldmxcfg
 
-p = ldmxcfg.Process('tracking')
+p = ldmxcfg.Process("tracking")
 p.input_files = ["Clusters.root"]
 p.output_files = ["Tracks.root"]
 
@@ -10,13 +10,11 @@ from LDMX.TrigScint.trig_scint import TrigScintTrackProducer
 tstp_tracks = TrigScintTrackProducer()
 lut_tracks = TrigScintTrackProducer()
 
-tstp_tracks.delta_max = 1.0 #set to 1.0 for easy comparison to lut_tracks 
+tstp_tracks.delta_max = 1.0  # set to 1.0 for easy comparison to lut_tracks
 tstp_tracks.output_collection = "maxdeltaTracks"
 
 lut_tracks.delta_max = 1.0
 lut_tracks.lut_tracking = True
 lut_tracks.output_collection = "lutTracks"
 
-p.sequence = [tstp_tracks, 
-              lut_tracks
-              ]
+p.sequence = [tstp_tracks, lut_tracks]
